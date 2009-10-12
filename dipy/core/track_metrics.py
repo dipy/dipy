e5828da9b8f229572c28e2fcf012cdf0bf88fa20
Eleftherios Garyfallidis <eg01@devel07.(none)>
2009-10-12 18:26:45 +0100

    Frenet-serret ready apart from checking torsion

--- dipy/core/track_metrics.py
+++ dipy/core/track_metrics.py
@@ -134,15 +134,18 @@ def center_of_mass(xyz):
         raise ValueError('xyz array cannot be empty')
     return np.mean(xyz,axis=0)
 
-def magn(xyz):
+def magn(xyz,n=1):
     ''' magnitude of vector
         
     '''    
-    mag=np.sum(xyz**2,axis=1)*0.5
+    mag=np.sum(xyz**2,axis=1)**0.5
     imag=np.where(mag==0)
-    mag[imag]=2.0e-100 
+    mag[imag]=np.finfo(float).eps #2.2204e-16#2.0e-100 
+
+    if n>1:
+        return np.tile(mag,(n,1)).T
+    return mag.reshape(len(mag),1)    
     
-    return mag
 
 def frenet_serret(xyz):
     ''' Frenet-Serret Space Curve Invarients
@@ -152,16 +155,16 @@ def frenet_serret(xyz):
     but the equations are still valid.
     
     Similar to
-    http://www.mathworks.com/matlabcentral/fileexchange/7256
+    http://www.mathworks.com/matlabcentral/fileexchange/11169
 
     _    r'
     T = ----  (Tangent)
         |r'|
  
-    _       T'
+    _    T'
     N = ----  (Normal)
-            |T'|
-    _      _   _
+        |T'|
+    _   _   _
     B = T x N (Binormal)
 
     k = |T'|  (Curvature)
@@ -175,34 +178,54 @@ def frenet_serret(xyz):
     
     Returns
     ---------
-    T :
-    N :
-    B :
-    k :
-    t :
+    T : array shape (N,3)
+        array representing the tangent of the curve xyz
+    N : array shape (N,3)
+        array representing the normal of the curve xyz    
+    B : array shape (N,3)
+        array representing the binormal of the curve xyz
+    k : array shape (N,1)
+        array representing the curvature of the curve xyz
+    t : array shape (N,1)
+        array representing the torsion of the curve xyz
+
+    Examples
+    --------
+    theta = 2*np.pi*np.linspace(0,2,100)
+    x=np.cos(theta)
+    y=np.sin(theta)
+    z=theta/(2*np.pi)
+    xyz=np.vstack((x,y,z)).T
+    T,N,B,k,t=frenet_serret(xyz)
+    >>>
 
     '''
     
-    dxyz=np.gradient(xyz)[0]    
+    dxyz=np.gradient(xyz)[0]        
+    
     ddxyz=np.gradient(dxyz)[0]
-        
-    #Tangent
-    T=np.divide(dxyz.T,magn(dxyz)).T
+    
+    #Tangent        
+    T=np.divide(dxyz,magn(dxyz,3))
     
     #Derivative of Tangent
     dT=np.gradient(T)[0]
     
     #Normal
-    N = np.divide(dT,magn(dT))
+    N = np.divide(dT,magn(dT,3))
     
     #Binormal
     B = np.cross(T,N)
     
-    #Curvature
-    #k = magn(np.cross(dxyz,ddxyz))
+    #Curvature 
+    k = magn(np.cross(dxyz,ddxyz),1)/(magn(dxyz,1)**3)    
     
-    #Torsion
-    t = np.dot(-B,N,2)
+    #Torsion 
+    #(In matlab was t=dot(-B,N,2))
+    t = np.sum(-B*N,axis=1)
+    
+    #return T,N,B,k,t,dxyz,ddxyz,dT   
+    return T,N,B,k,t
     
 
 def frechet_distance(xyz1,xyz2):
@@ -211,4 +234,18 @@ def frechet_distance(xyz1,xyz2):
     '''
     pass        
     
+def test_frenet():
+    theta = 2*np.pi*np.linspace(0,2,100)
+    x=np.cos(theta)
+    y=np.sin(theta)
+    z=theta/(2*np.pi)
+    xyz=np.vstack((x,y,z)).T
+    return xyz
+    #T,N,B,k,t=frenet_serret(xyz)
     
+if __name__ == "__main__":
+    pass
+
+    
+    
+
