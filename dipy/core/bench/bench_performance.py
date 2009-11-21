43a41d3db82ce17989d9e9e4754082204e45ac90
Matthew Brett <matthew.brett@gmail.com>
2009-11-20 17:29:12 -0800

    Renamed benchmark file; added cut_plane test

--- dipy/core/bench/bench_performance.py
+++ dipy/core/bench/bench_performance.py
@@ -22,7 +22,15 @@ def bench_zhang():
     #print 'reference time: %f' % ref_time
     opt_time = measure('pf.most_similar_track_zhang(tracks300)')
     print 'optimized time: %f' % opt_time
+    print
 
+def bench_cut_plane():
+    print 'Cut plane'
+    print '=' * 10
+    opt_time = measure('pf.cut_plane(tracks300, tracks300[0])')
+    print 'optimized time: %f' % opt_time
+    print
 
 if __name__ == '__main__' :
     bench_zhang()
+    bench_cut_plane()
