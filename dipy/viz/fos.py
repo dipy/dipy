8956e94194bb19dbb2d1ed10edf5e4121c17d5ad
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2009-12-08 19:10:34 +0000

    slicer option added

--- dipy/viz/fos.py
+++ dipy/viz/fos.py
@@ -1267,6 +1267,220 @@ def _closest_track(p,tracks):
     
     return int(d[imin,0])
     
+def slicer(ren,vol,voxsz=(1.0,1.0,1.0),affine=None,opacitymap=None,colormap=None):
+    
+    # This example reads a volume dataset, extracts two isosurfaces that
+    # represent the skin and bone, creates three orthogonal planes
+    # (saggital, axial, coronal), and displays them.
+    
+    im = vtk.vtkImageData()
+    im.SetScalarTypeToUnsignedChar()
+    im.SetDimensions(vol.shape[0],vol.shape[1],vol.shape[2])
+    #im.SetOrigin(0,0,0)
+    im.SetSpacing(voxsz[2],voxsz[0],voxsz[1])
+    im.AllocateScalars()        
+    
+    for i in range(vol.shape[0]):
+        for j in range(vol.shape[1]):
+            for k in range(vol.shape[2]):
+                
+                im.SetScalarComponentFromFloat(i,j,k,0,vol[i,j,k])
+                
+    #print im.GetExtent()
+    
+    #return
+
+    # An isosurface, or contour value of 500 is known to correspond to the
+    # skin of the patient. Once generated, a vtkPolyDataNormals filter is
+    # is used to create normals for smooth surface shading during rendering.
+    # The triangle stripper is used to create triangle strips from the
+    # isosurface these render much faster on may systems.
+    skinExtractor = vtk.vtkContourFilter()
+    #skinExtractor.SetInputConnection(im.GetOutputPort())
+    skinExtractor.SetInput(im)
+    skinExtractor.SetValue(0, 38)
+    skinNormals = vtk.vtkPolyDataNormals()
+    skinNormals.SetInputConnection(skinExtractor.GetOutputPort())
+    skinNormals.SetFeatureAngle(60.0)
+    skinStripper = vtk.vtkStripper()
+    skinStripper.SetInputConnection(skinNormals.GetOutputPort())
+    skinMapper = vtk.vtkPolyDataMapper()
+    skinMapper.SetInputConnection(skinStripper.GetOutputPort())
+    skinMapper.ScalarVisibilityOff()
+    skin = vtk.vtkActor()
+    skin.SetMapper(skinMapper)
+    skin.GetProperty().SetDiffuseColor(1, .49, .25)
+    skin.GetProperty().SetSpecular(.3)
+    skin.GetProperty().SetSpecularPower(20)
+
+    # An isosurface, or contour value of 1150 is known to correspond to the
+    # skin of the patient. Once generated, a vtkPolyDataNormals filter is
+    # is used to create normals for smooth surface shading during rendering.
+    # The triangle stripper is used to create triangle strips from the
+    # isosurface these render much faster on may systems.
+    boneExtractor = vtk.vtkContourFilter()
+    #boneExtractor.SetInputConnection(im.GetOutputPort())
+    boneExtractor.SetInput(im)
+    boneExtractor.SetValue(0, 418)
+    boneNormals = vtk.vtkPolyDataNormals()
+    boneNormals.SetInputConnection(boneExtractor.GetOutputPort())
+    boneNormals.SetFeatureAngle(60.0)
+    boneStripper = vtk.vtkStripper()
+    boneStripper.SetInputConnection(boneNormals.GetOutputPort())
+    boneMapper = vtk.vtkPolyDataMapper()
+    boneMapper.SetInputConnection(boneStripper.GetOutputPort())
+    boneMapper.ScalarVisibilityOff()
+    bone = vtk.vtkActor()
+    bone.SetMapper(boneMapper)
+    bone.GetProperty().SetDiffuseColor(1, 0, .9412)
+
+    # An outline provides context around the data.
+    outlineData = vtk.vtkOutlineFilter()
+    #outlineData.SetInputConnection(im.GetOutputPort())
+    outlineData.SetInput(im)
+    mapOutline = vtk.vtkPolyDataMapper()
+    mapOutline.SetInputConnection(outlineData.GetOutputPort())
+    outline = vtk.vtkActor()
+    outline.SetMapper(mapOutline)
+    outline.GetProperty().SetColor(1, 0, 0)
+
+    # Now we are creating three orthogonal planes passing through the
+    # volume. Each plane uses a different texture map and therefore has
+    # diferent coloration.
+
+    # Start by creatin a black/white lookup table.
+    bwLut = vtk.vtkLookupTable()
+    bwLut.SetTableRange(0, 2000)
+    bwLut.SetSaturationRange(0, 0)
+    bwLut.SetHueRange(0, 0)
+    bwLut.SetValueRange(0, 1)
+    bwLut.SetRampToLinear()
+    bwLut.Build()
+
+    # Now create a lookup table that consists of the full hue circle (from
+    # HSV).
+    hueLut = vtk.vtkLookupTable()
+    hueLut.SetTableRange(0, 2000)
+    hueLut.SetHueRange(0, 1)
+    hueLut.SetSaturationRange(1, 1)
+    hueLut.SetValueRange(1, 1)
+    hueLut.Build()
+
+    # Finally, create a lookup table with a single hue but having a range
+    # in the saturation of the hue.
+    satLut = vtk.vtkLookupTable()
+    satLut.SetTableRange(0, 2000)
+    satLut.SetHueRange(.6, .6)
+    satLut.SetSaturationRange(0, 1)
+    satLut.SetValueRange(1, 1)
+    satLut.Build()
+    
+    
+    x1,x2,y1,y2,z1,z2=im.GetExtent()
+
+    # Create the first of the three planes. The filter vtkImageMapToColors
+    # maps the data through the corresponding lookup table created above.
+    # The vtkImageActor is a type of vtkProp and conveniently displays an
+    # image on a single quadrilateral plane. It does this using texture
+    # mapping and as a result is quite fast. (Note: the input image has to
+    # be unsigned char values, which the vtkImageMapToColors produces.)
+    # Note also that by specifying the DisplayExtent, the pipeline
+    # requests data of this extent and the vtkImageMapToColors only
+    # processes a slice of data.
+    saggitalColors = vtk.vtkImageMapToColors()
+    #saggitalColors.SetInputConnection(im.GetOutputPort())
+    saggitalColors.SetInput(im)
+    saggitalColors.SetLookupTable(bwLut)
+    saggitalColors.Update()
+    
+    saggital = vtk.vtkImageActor()
+    saggital.SetInput(saggitalColors.GetOutput())
+    #saggital.SetDisplayExtent(32, 32, 0, 63, 0, 92)
+    saggital.SetDisplayExtent(40,40,0,79,0,79)
+
+
+    # Create the second (axial) plane of the three planes. We use the same
+    # approach as before except that the extent differs.
+    axialColors = vtk.vtkImageMapToColors()
+    #axialColors.SetInputConnection(v16.GetOutputPort())
+    axialColors.SetInput(im)
+    axialColors.SetLookupTable(hueLut)
+    axial = vtk.vtkImageActor()
+    axial.SetInput(axialColors.GetOutput())
+    #axial.SetDisplayExtent(0, 63, 0, 63, 46, 46)
+    
+
+    # Create the third (coronal) plane of the three planes. We use the same
+    # approach as before except that the extent differs.
+    coronalColors = vtk.vtkImageMapToColors()
+    #coronalColors.SetInputConnection(v16.GetOutputPort())
+    coronalColors.SetInput(im)
+    coronalColors.SetLookupTable(satLut)
+    coronal = vtk.vtkImageActor()
+    coronal.SetInput(coronalColors.GetOutput())
+    #coronal.SetDisplayExtent(0, 63, 32, 32, 0, 92)
+    
+
+    # It is convenient to create an initial view of the data. The FocalPoint
+    # and Position form a vector direction. Later on (ResetCamera() method)
+    # this vector is used to position the camera to look at the data in
+    # this direction.
+    aCamera = vtk.vtkCamera()
+    aCamera.SetViewUp(0, 0, -1)
+    aCamera.SetPosition(0, 1, 0)
+    aCamera.SetFocalPoint(0, 0, 0)
+    aCamera.ComputeViewPlaneNormal()
+
+    # Actors are added to the renderer.
+    ren.AddActor(outline)
+    ren.AddActor(saggital)
+    #ren.AddActor(axial)
+    #ren.AddActor(coronal)
+    
+    #ren.AddActor(axial)
+    #ren.AddActor(coronal)
+    
+    #ren.AddActor(skin)
+    #ren.AddActor(bone)
+
+    # Turn off bone for this example.
+    #bone.VisibilityOff()
+
+    # Set skin to semi-transparent.
+    skin.GetProperty().SetOpacity(0.5)
+
+    # An initial camera view is created.  The Dolly() method moves
+    # the camera towards the FocalPoint, thereby enlarging the image.
+    ren.SetActiveCamera(aCamera)
+    ren.ResetCamera()
+    aCamera.Dolly(1.5)
+
+    # Set a background color for the renderer and set the size of the
+    # render window (expressed in pixels).
+    ren.SetBackground(1, 1, 1)
+    #renWin.SetSize(640, 480)
+
+    # Note that when camera movement occurs (as it does in the Dolly()
+    # method), the clipping planes often need adjusting. Clipping planes
+    # consist of two planes: near and far along the view direction. The
+    # near plane clips out objects in front of the plane the far plane
+    # clips out objects behind the plane. This way only what is drawn
+    # between the planes is actually rendered.
+    #ren.ResetCameraClippingRange()
+
+    #return ren
+    
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
+    
+    ren.ResetCameraClippingRange()
+
+    # Interact with the data.
+    iren.Initialize()
+    renWin.Render()
+    iren.Start()
 
 def annotatePick(object, event):
     ''' Create a Python function to create the text for the 
@@ -1347,14 +1561,12 @@ def show(ren,title='Fos',size=(300,300),track_bf=None,ind_bf=None,color_bf=None)
     iren.SetRenderWindow(window)
     iren.SetPicker(picker)
     
-
-    
     iren.SetInteractorStyle(style)
     iren.Initialize()
     picker.Pick(85, 126, 0, ren)    
     window.Render()
     iren.Start()
-    
+       
     
 if __name__ == "__main__":
     
