0691d142dc33e0b1c2cfe6f4e5e915a780b428e2
Matthew Brett <matthew.brett@gmail.com>
2010-03-08 23:08:51 -0800

    NF - implemeted CSA1 reading, but with no tests

--- dipy/io/csareader.py
+++ dipy/io/csareader.py
@@ -20,28 +20,28 @@ def read(csa_str):
     -------
     header : dict
        header information as dict, where `header` has fields (at least)
-       ``type, n_tags, tags``.  ``tags`` is also a dictionary with one
-       key, value pair for each tag in the header.
+       ``type, n_tags, tags``.  ``header['tags']`` is also a dictionary
+       with one key, value pair for each tag in the header.
     '''
-    hdr_id = csa_str[:4]
     csa_len = len(csa_str)
     csa_dict = {'tags': {}}
+    hdr_id = csa_str[:4]
     ptr = 0
-    if hdr_id != 'SV10':
+    if hdr_id != 'SV10': # CSA1
+        hdr_type = 1
         csa_dict['type'] = 1
-        ptr, csa_dict['n_tags'] = _ptr_unpack('<I', hdr_id, ptr)
-        raise CSAReadError("We cannot read CSA1 headers")
     else:
+        hdr_type = 2
         ptr, _ = _ptr_unpack('4s', csa_str, ptr)
-        csa_dict['type'] = 2
         ptr, csa_dict['unused0'] = _ptr_unpack('4s', csa_str, ptr)
-        ptr, csa_dict['n_tags'] = _ptr_unpack('<I', csa_str, ptr)
+    ptr, csa_dict['n_tags'] = _ptr_unpack('<I', csa_str, ptr)
+    csa_dict['type'] = hdr_type
     if csa_dict['n_tags'] < 1 or csa_dict['n_tags'] > 128:
         raise CSAReadError('Number of tags should be '
                            '> 0, <= 128')
     ptr, csa_dict['check'] = _ptr_unpack('<I', csa_str, ptr)
     for tag_no in range(csa_dict['n_tags']):
-        ptr, name, vm, vr, syngodt, n_items, xx = \
+        ptr, name, vm, vr, syngodt, n_items, last3 = \
             _ptr_unpack('<64si4s3i',csa_str, ptr)
         vr = nt_str(vr)
         name = nt_str(name)
@@ -49,21 +49,32 @@ def read(csa_str):
                'vm': vm,
                'vr': vr,
                'syngodt': syngodt,
-               'last3': xx}
+               'last3': last3,
+               'tag_no': tag_no}
+        # CSA1 specific length modifier
+        if tag_no == 1:
+            tag0_n_items = n_items
         assert n_items < 100
         items = []
         for item_no in range(n_items):
-            ptr, x0,x1,x2,x3 = _ptr_unpack('<4i',csa_str, ptr)
-            item_len = x1
-            if (ptr + item_len) > csa_len:
-                raise CSAReadError('Item is too long, aborting read')
+            ptr, x0,x1,x2,x3 = _ptr_unpack('<4i', csa_str, ptr)
+            if hdr_type == 1:  # CSA1
+                item_len = x0 - tag0_n_items
+                if item_len < 0 or (ptr + item_len) > csa_len:
+                    items.append('')
+                    break
+            else: # CSA2
+                item_len = x1
+                if (ptr + item_len) > csa_len:
+                    raise CSAReadError('Item is too long, '
+                                       'aborting read')
             item = nt_str(csa_str[ptr:ptr+item_len])
+            items.append(item)
+            ptr += item_len
+            # go to 4 byte boundary
             plus4 = item_len % 4
             if plus4 != 0:
-                ptr+=item_len + (4-plus4)
-            else:
-                ptr+=item_len
-            items.append(item)
+                ptr += (4-plus4)
         tag['items'] = items
         csa_dict['tags'][name] = tag
     return csa_dict
