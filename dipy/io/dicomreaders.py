310b36e3a80f61b9b2aec9ab3ba66f7cf640fcb1
Matthew Brett <matthew.brett@gmail.com>
2010-03-10 16:35:36 -0800

    RF - merged DICOM readers with added data file

--- dipy/io/dicomreaders.py
+++ dipy/io/dicomreaders.py
@@ -1,10 +1,48 @@
+import numpy as np
+
 import dicom
 
-import csareader as csar
+import nibabel as nib
+
+from . import csareader as csar
 
 
-class CSAHeaderError(Exception):
+class MosiacError(Exception):
     pass
+
+
+def mosaic_to_nii(dcm_data):
+    ''' Get Nifti file from Siemens 
+
+    Parameters
+    ----------
+    dcm_data : ``dicom.DataSet``
+       DICOM header / image as read by ``dicom`` package
+
+    Returns
+    -------
+    img : ``Nifti1Image``
+       Nifti image object
+    '''
+    if not is_mosaic(dcm_data):
+        raise MosaicError('data does not appear to be mosaic format')
+    # read CSA headers
+    hdr_info = get_csa_header(dcm_data)
+    # get Mosaic size
+    n_o_m = hdr_info['tags']['NumberOfImagesInMosaic']
+    n_o_m = n_o_m['items'][0]
+    # reshape pixel slice array back from mosaic
+    mosaic_size = np.ceil(np.sqrt(n_o_m))
+    data = dcm_data.pixel_array
+    n_rows = data.shape[0] / mosaic_size
+    n_cols = data.shape[1] / mosaic_size
+    v4=data.reshape(mosaic_size,n_rows,
+                    mosaic_size,n_cols)
+    v4p=np.rollaxis(v4,2,1)
+    v3=v4p.reshape(mosaic_size*mosaic_size,n_rows,n_cols)
+    # delete any padding slices
+    v3 = v3[:n_o_m]
+    return nib.Nifti1Image(v3.T, None)
 
 
 def has_csa(dicom_header):
--- dipy/io/tests/test_dicomreaders.py
+++ dipy/io/tests/test_dicomreaders.py
@@ -4,6 +4,7 @@
 
 import os
 from os.path import join as pjoin
+import gzip
 
 import numpy as np
 
@@ -18,12 +19,19 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 from dipy.testing import parametric
 
-data_file = os.path.expanduser('~/data/20100114_195840/'
-                               'Series_012_CBU_DTI_64D_1A/'
-                               '1.3.12.2.1107.5.2.32.35119.'
-                               '2010011420300822323499745.dcm')
+data_path = pjoin(os.path.dirname(__file__), 'data')
 
-data = dicom.read_file(data_file)
+data_file = pjoin(data_path, 'siemens_dwi_1000.dcm.gz')
+
+data = dicom.read_file(gzip.open(data_file))
+
+
+@parametric
+def test_read_dwi():
+    img = didr.mosaic_to_nii(data)
+    arr = img.get_data()
+    yield assert_equal(arr.shape, (128,128,48))
+
 
 @parametric
 def test_read():
@@ -33,7 +41,8 @@ def test_read():
     yield assert_raises(ValueError, didr.get_csa_header, data,'xxxx')
     yield assert_true(didr.is_mosaic(data))
 
+
 def test_dwi_params():
     csa_hdr = didr.get_csa_header(data,'image')
-    params = get_dwi_params(csa_hdr)
+    #params = get_dwi_params(csa_hdr)
     
