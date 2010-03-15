0c08e612be2516c3a9b53a641b1a982c609d3913
Matthew Brett <matthew.brett@gmail.com>
2010-03-15 13:26:31 -0700

    TEST - skeleton of tests for qball

---
+++ dipy/core/tests/test_qball.py
@@ -0,0 +1,21 @@
+""" Testing qball 
+
+"""
+
+import numpy as np
+
+import dipy.core.qball as qball
+
+from nose.tools import assert_true, assert_false, \
+     assert_equal, assert_raises
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+from dipy.testing import parametric
+
+
+@parametric
+def test_real_sph_harm():
+    real_sh = qball.real_sph_harm(0, 0, 0, 0)
+    yield assert_true(True)
+    yield assert_false(True)
