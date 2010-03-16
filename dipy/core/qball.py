9fde3347ebd50547d60f2cb2f635d1fe219c8b6e
Bago Amirbekian <mrbago+git@gmail.com>
2010-03-16 15:18:12 -0700

    RF - split code for easier testing

--- dipy/core/qball.py
+++ dipy/core/qball.py
@@ -105,7 +105,6 @@ class odf():
         theta = np.arctan2(grad_table[1, dwi], grad_table[0, dwi])
         phi = np.arccos(grad_table[2, dwi])
         
-        self.b0 = data[..., np.logical_not(dwi)]
 
         m_list, n_list = sph_harm_ind_list(self.sh_order)
         comp_mat = real_sph_harm(m_list, n_list, theta, phi)
@@ -114,6 +113,9 @@ class odf():
         legendre0, junk = lpn(self.sh_order, 0)
         funk_radon = legendre0[n_list]
         self.fit_matrix *= funk_radon.T
+
+        self.dim = data.shape[:-1]
+        self.b0 = data[..., np.logical_not(dwi)]
         self.coef = np.dot(data[..., dwi], self.fit_matrix)
 
         if keep_resid:
@@ -159,87 +161,8 @@ class odf():
         res_mat = np.eye(self.n_grad) - res_mat
         return res_mat
 
-    def disp_odf(self, theta_res=64, phi_res=32, colormap='RGB', colors=256):
-        import Image
 
-        pi = np.pi
-        sin = np.sin
-        cos = np.cos
-
-        theta, phi = np.mgrid[0:2*pi:theta_res*1j, 0:pi:phi_res*1j] 
-        x = sin(phi)*cos(theta)
-        y = sin(phi)*sin(theta)
-        z = cos(phi)
-        
-        dim = self.coef.shape[:-1]
-        nvox = np.prod(dim)
-
-        x_cen, y_cen, z_cen = self.__odf_location(dim)
-        
-        odf_values = self.evaluate_at(theta, phi)
-        max_value = odf_values.max()
-
-        mlab.figure()
-        for ii in range(nvox):
-            odf_ii = odf_values.reshape(nvox, theta_res, phi_res)[ii,:,:]
-            odf_ii /= max_value * 2
-            if colormap == 'RGB':
-                rgb = np.r_['-1,3,0', x*odf_ii, y*odf_ii, z*odf_ii]
-                rgb = np.abs(rgb*255/rgb.max()).astype('uint8')
-                odf_im = Image.fromarray(rgb, mode='RGB')
-                odf_im = odf_im.convert('P', palette=Image.ADAPTIVE, colors=colors)
-                
-                lut = np.empty((colors,4),'uint8')
-                lut[:,3] = 255
-                lut[:,0:3] = np.reshape(odf_im.getpalette(),(colors,3))
-
-                oo = mlab.mesh(x*odf_ii + x_cen.flat[ii], 
-                               y*odf_ii + y_cen.flat[ii], 
-                               z*odf_ii + z_cen.flat[ii], 
-                               scalars=np.int16(odf_im))
-                oo.module_manager.scalar_lut_manager.lut.table=lut
-            else:
-                oo = mlab.mesh(x*odf_ii + x_cen.flat[ii], 
-                               y*odf_ii + y_cen.flat[ii], 
-                               z*odf_ii + z_cen.flat[ii], 
-                               scalars=odf_ii,
-                               colormap=colormap)
-
-    def __odf_location(self,dim):
-
-        if len(dim) > 3:
-            raise ValueError('cannot display 4d image')
-        elif len(dim) < 3:
-            d = [1, 1, 1]
-            d[0:len(dim)] = dim
-        else:
-            d = dim
-        
-        return np.mgrid[0:d[0], 0:d[1], 0:d[2]]
 
     def test(self):
         print self.sh_order
-
-
-
-filename='/Users/bagrata/HARDI/E1322S8I1.nii.gz'
-grad_table_filename='/Users/bagrata/HARDI/E1322S8I1.bvec'
-from nipy import load_image, save_image
-
-grad_table, b_values = read_bvec_file(grad_table_filename)
-img = load_image(filename)
-print 'input dimensions: '
-print img.ndim
-print 'image size: '
-print img.shape
-print 'image affine: '
-print img.affine
-print 'images has pixels with size: '
-print np.dot(img.affine, np.eye(img.ndim+1)).diagonal()[0:3]
-data = np.asarray(img)
-
-theta, phi = np.mgrid[0:2*np.pi:64*1j, 0:np.pi:32*1j]
-odf_i=odf(data[188:192,188:192,22:24,:],4,grad_table,b_values)
-odf_i.disp_odf()
-#odf_i.evaluate_at(theta, phi)
 
