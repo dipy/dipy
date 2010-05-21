ddb305774b626f58b82deece40ea31e3c44be5fe
Christopher Nguyen <cnguyen@oski.radiology.ucsf.edu>
2010-05-21 16:26:56 -0700

    RF - fixing up core/tensor.py

--- dipy/core/tensor.py
+++ dipy/core/tensor.py
@@ -1,46 +1,6 @@
 #!/usr/bin/python
-
-"""
-
-USAGE
-
-    As a script ... (::WARNING NOT REALLY SUGGESTED AND NOT SUPPORTED::)
-        tensor [-h,--help] [-v,--verbose] [--version] <data> <gtab> <bvalue>
-    
-    As a imported module ...
-        fit_data,scalars_map = tensor(data,gtab,bvalue,img=img,out_root=out_root,scalars=scalars)
-
-DESCRIPTION
-
-    tensor does...
-
-    INPUTS:
-        <data>          = 4D image matrix
-        <gtab>      = gradient table [3 x N]
-        <bval>      = b value vector [N x 1]
-        <scalars>   = 1 : save scalars
-
-    OUTPUTS
-        fit_data    = fitted 4D image matrix
-        scalars_map = 4D image matrix [x,y,z, scalars]
-
-EXAMPLES
-
-
-
-AUTHOR(S)
-
-    Christopher Nguyen <christopher.nguyen@radiology.ucsf.edu>
-
-
-LICENSE
-
-
-VERSION
-
-    1.0
-
-"""
+# Created by Christopher Nguyen
+# 5/17/2010
 
 #import modules
 import time
@@ -49,100 +9,106 @@ import numpy as np
 import scipy as sp
 
 #HBCAN utilities
-sys.path.append('/home/cnguyen/scripts/HBCAN/utils')
-from multi_dot import multi_dot as multi_dot
+#sys.path.append('/home/cnguyen/scripts/HBCAN/utils')
 from io import get_coord_4D_to_3D
 
-def WLS_fit (data,gtab,bval,scalars=0,mask=[],thresh=25,img=[],out_root='noroot'):    
-    #Make sure all input is correct
+class tensor(object):
+	"""
+	Tensor object that when initialized calculates single self diffusion tensor[1]_ in 
+	each voxel using selected fitting algorithm (DEFAULT: weighted least squares[1]_)
+
+	Requires a given gradient table, b value for each diffusion-weighted gradient vector,
+	and image data given all as numpy ndarrays.
+
+	Key Methods
+	-----------
+	evals : ndarray (X,Y,Z,EV1,EV2,EV3)Z
+		Returns cached eigenvalues of self diffusion tensor [1]_ for given index.
+	evecs : ndarray (X,Y,Z,EV1x,EV1y,EV1z,EV2x,EV2y,EV2z,EV3x,EV3y,EV3z)
+		Returns cached associated eigenvector of self diffusion tensor [1]_ for given 
+		index.
+	FA : ndarray
+		Calculates fractional anisotropy [2]_ for given index.
+	ADC : ndarray
+		Calculates apparent diffusion coefficient or mean diffusitivity [2]_ for given
+		index.
+
+	References
+	----------
+	..	[1] Basser, P.J., Mattiello, J., LeBihan, D., 1994. Estimation of the effective 
+		self-diffusion tensor from the NMR spin echo. J Magn Reson B 103, 247-254.
+	..	[2] Basser, P., Pierpaoli, C., 1996. Microstructural and physiological features 
+		of tissues elucidated by quantitative-diffusion-tensor MRI. Journal of Magnetic 
+		Resonance 111, 209-219.
+	
+	"""
+	def _getshape(self):
+		pass
+	def _getndim(self):
+		pass
+
+
+def WLS_fit (data,gtab,bval,scalars=0,mask=None,thresh=25,img=[],out_root='noroot'):    
+	"""
+	Computes weighted least squares (WLS) fit to calculate self-diffusion tensor. 
+	(Basser et al., 1994a)
+
+	Parameters
+	----------
+    data : ndarray (X,Y,Z,g)
+    	The image data as a numpy ndarray.
+    gtab : ndarray (g,3)
+        Diffusion gradient table found in DICOM header as a numpy ndarray.
+    bval : ndarray (g,1)
+    	Diffusion weighting factor b for each vector in gtab.
+    scalars : integer (0,1)
+        Flag that returns integers.
+
+
+	"""
+
+	#Make sure all input is correct
     try:
         start_time = time.time()
-        print 'out_root: ' + out_root
-        print 'thresh: ' + str(thresh)
-        print ''
-        if data == []:
-            raise ValueError('You need to send an image matrix, grad table, and b value vector)')
-
-        if gtab == []:
-            raise ValueError('Please provide a gradient table')
-
-        if bval == []:
-            raise ValueError('Please provide a b value vector')
-
         if np.size(gtab)/3 != np.size(bval):
-            raise ValueError('Gradient table [3 x N] must be consistent with given b value vector [N x 1]')
+            raise ValueError('Gradient table (g,3) must be consistent with given b value vector (g,1)')
     except:
         raise IOError('You need to send an image matrix, grad table, and b value vector')
 
         
     ####main part of code
-    fit_data = np.zeros(np.shape(data))
-    dims = np.shape(data)
-
-    print 'Performing tensor model fit ... '
-    print ''
+    #fit_data = np.zeros(np.shape(data))
+    dims = data.shape
 
     ###Create log of signal and reshape it to be x:y:z by grad
     fit_dim = (dims[0]*dims[1]*dims[2],dims[3]) 
-    fit_data = np.reshape(fit_data,fit_dim )
-    data = np.reshape(data,fit_dim)
+    data.shape = fit_dim # = np.reshape(data,fit_dim)
+    D = np.zeros((np.size(fit_data,axis=0),6))
+
     if scalars == 1:
-        scalar_maps = np.zeros((np.size(fit_data,axis=0),8))
-    tensor_data = np.zeros((np.size(fit_data,axis=0),6))
+        scalar_maps = np.zeros((np.size(data,axis=0),8))
 
     # Y matrix from Chris' paper
-    # [g by x*y*z]
-    data[np.where(data <= 0)] = 1
+    # enforcing positive values to allow for natural log
+	data[np.where(data <= 0)] = 1
     log_s = np.transpose(np.log(data))
 
-    ###Construct design matrix
-    #For DTI this is the so called B matrix
-    # X matrix from Chris' paper
-    print 'Constructing tensor design matrix (B matrix) ...'
-    print ''
+	###Construct design matrix
+	#For DTI this is the so called B matrix
+	# X matrix from Chris' paper
     B = design_matrix(gtab,bval) # [g by 7]
-    B_t = np.transpose(B)        # [7 by g]
-    
+
     ###Weighted Least Squares (WLS) to solve "linear" regression
     
     # Y hat OLS from Chris' paper
-    # ORIG IDL CODE: log_s_ols = B ## invert( transpose(B) ## B ,/double) ## transpose(B) ## log_s
-    # log_s_ols = np.dot(B, np.dot(np.linalg.inv(np.dot(B_t,B)), np.dot(B_t,log_s)))
-    #  [g by 7] [7 by 7 ] [7 by g] [ g by x*y*z ] = [g by x*y*z]
-    log_s_ols = np.dot(B  ,np.dot(  np.linalg.inv(np.dot(B_t,B)), np.dot( B_t, log_s ) ) )
-    #log_s_ols = multi_dot((B, np.linalg.inv(np.dot(B_t,B)), B_t, log_s)) 
+    #  [g by 7] [7 by g ] [ g by x*y*z ] = [g by x*y*z]
+    log_s_ols =  np.dot(np.dot(B, np.linalg.pinv(B)), log_s)
    
-    ##inv_B_t_W_B = np.zeros((fit_dim[0],7,7))
-    ##for i in range(np.size(log_s,axis=1):
-    ##    #Weighting factor from WLS
-    ##    # should be [x*y*z by g by g]
-    ##    W = sp.diag(np.exp(log_s_ols[:,i])**2)
-    ##
-    ##    #inv[[7 by g] [g by g] [g by 7]] = [7 by 7] 
-    ##    inv_B_t_W_B[i,:,:] = np.linalg.inv(multidot((B_t,W,B)))
-    ##
-    ## Beta matrix from Chris' paper
-    ## D = [x*y*z by 6] : the 2nd dim 6 parameters are [Dxx,Dyy,Dzz,Dxy,Dxz,Dyz]
-    ## [x*y*z by 7 by 7] [7 by g] [g by g] [g by x*y*z] = [7 by x*y*z]
-    #D = multi_dot((inv_B_t_W_B, B_t, W, log_s))
-    ##
-    ## Y hat matrix from Chris' paper
-    ##fit_data = np.dot(B, D)
-
     # This step is because we cannot vectorize diagonal vector and tensor fit
-    print 'Calculating voxelwise diagonal weighting matrix and tensor fit ... '
-    print
-    time_diff = list((0,0))
-    for i in log_s[mask>0] #range(np.size(log_s,axis=1)):
-        #check every 5 slices (pretty robust time left...prob make into separate module if have time)
-        if np.mod(i,dims[0]*dims[1]*5) == 0:
-            slice = i/dims[0]/dims[1]+1.
-            time_diff.append(time.time()-start_time)
-            min = np.mean(time_diff[2:len(time_diff)]) / 60.0 /5 *(dims[2] - slice)
-            sec = np.round((min - np.fix(min)) * 60.0/5)
-            min = np.fix(min)
-            percent = 100.*slice/dims[2]
-            print str(np.round(percent)) + '% ... Approx time left ' + str(min) + ' MIN ' + str(sec) + ' SEC'
+    for i in range(np.size(log_s,axis=1)):
+        
+		if mask.flat[i] == 0:
+	    continue
 
         if data[i,0] < thresh:
             continue
@@ -151,35 +117,28 @@ def WLS_fit (data,gtab,bval,scalars=0,mask=[],thresh=25,img=[],out_root='noroot'
         if not(np.unique(np.isfinite(log_s[:,i]))[0]) :
             continue
 
-        #Weighting factor from WLS
-        #[g by g]
-        W = sp.diag(np.exp(log_s_ols[:,i])**2)
-
-        # Beta matrix from Chris' paper
-        # D = [Dxx,Dyy,Dzz,Dxy,Dxz,Dyz,dummy]
-        # inv[[7 by g] [g by g] [g by 7]] [7 by g] [g by g] [g by 1] = [7 by 1]
-        #ORIG IDL CODE: D = invert( transpose(B) ## W ## B ,/double) ## transpose(B) ## W ## log_s[i,:]
-        D = multi_dot((np.linalg.inv(multi_dot((B_t,W,B))), B_t, W, log_s[:,i]))  
-
-        # Y hat matrix from Chris' paper
-        fit_data[i,:] = np.dot(B, D)
-        tensor_data[i,:] = D[0:6]*1000    
+		#Split up weighting vector into little w to perform pinv
+		w = np.exp(log_s_ols[:,i])[:,np.newaxis]
+	
+		#pointwise broadcasting to avoid diagonal matrix multiply!
+		D[:,i] = np.dot(np.linalg.pinv(B*w), w*log_s[:,i])
         
         ###Calculate scalar maps
         if scalars == 1:
             scalar_maps[i,:] = calc_dti_scalars(D)
-
-    # Reshape the output images
-    fit_data = np.reshape(fit_data,dims)
-    data = np.reshape(data,dims)
-    tensor_data = np.reshape(tensor_data,(dims[0],dims[1],dims[2],6))
     
-    #Reshape the scalar map array
-    scalar_maps = np.reshape(scalar_maps,(dims[0],dims[1],dims[2],8))
+    #Fit the data with estimate of D
+    fit_data = np.dot(B,D) 
+    
+    # Reshape the output images
+    fit_data.shape = dims
+    data.shape = dims
 
     #If requesting to save scalars ...
     if scalars == 1:
-	save_scalar_maps(scalar_maps)
+    	#Reshape the scalar map array
+    	scalar_maps = np.reshape(scalar_maps, dims[0:3]+(-1,))
+		#save_scalar_maps(scalar_maps)
    
     #Report how long it took to make the fit  
     min = (time.time() - start_time) / 60.0
@@ -208,10 +167,15 @@ def calc_dti_scalars(D):
     if np.size(eigenvals) != 3:
         raise ValueError('not 3 eigenvalues : ' + str(eigenvals))
 
+	#need to sort the eigenvalues and associated eigenvectors
+	eigenvecs = eigenvecs[:,eigenvals.argsort()[::-1]]
+	eigenvals.sort() #very fast
+	eigenvals = eigenvals[::-1]
+
+	#assign primary, secondary, and tertiary eigenvalues
     ev1 = eigenvals[0]
     ev2 = eigenvals[1]
     ev3 = eigenvals[2]
-    #eigenvecs = np.transpose(eigenvecs)
     
     #calculate scalars
     adc = (ev1+ev2+ev3)/3
@@ -225,8 +189,8 @@ def calc_dti_scalars(D):
         tsi = (ev1-ev2)/(ev1-ev3)
 
     # fyi b ~ 10^3 s/mm^2 and D ~ 10^-4 mm^2/s
-    dti_parameters = np.round([	ev1*10000, ev2*10000, ev3*10000, adc*10000,fa*1000,
-					eigenvecs[0,0]*100+1000, eigenvecs[1,0]*100+1000,eigenvecs[2,0]*100+1000]) 
+    dti_parameters = [np.round([ev1*10000, ev2*10000, ev3*10000, adc*10000,fa*1000,]),
+					eigenvecs.T.flat[:]] 
                     #,evt,tsi]
     return(dti_parameters)
 
