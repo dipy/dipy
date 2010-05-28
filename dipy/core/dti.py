fde9b98d5a63b536401477e606973caa4c62fc82
Christopher Nguyen <christopher.t.nguyen@gmail.com>
2010-05-28 13:47:25 -0700

    RF - introducing maskedview into dti.py to allow for efficient memory handling

--- dipy/core/dti.py
+++ dipy/core/dti.py
@@ -8,9 +8,9 @@ import sys, os, traceback, optparse
 import numpy as np
 import scipy as sp
 
-#HBCAN utilities
-#sys.path.append('/home/cnguyen/scripts/HBCAN/utils')
-#from io import get_coord_4D_to_3D
+#dipy modules
+from dipy.core.maskedview import MaskedView
+from dipy.core.memory import memory
 
 class tensor(object):
     """
@@ -19,6 +19,19 @@ class tensor(object):
 
     Requires a given gradient table, b value for each diffusion-weighted gradient vector,
     and image data given all as numpy ndarrays.
+
+    Parameters
+    ----------
+    data : ndarray (X,Y,Z,g)
+        The image data as a numpy ndarray.
+    gtab : ndarray (3,g)
+        Diffusion gradient table found in DICOM header as a numpy ndarray.
+    bval : ndarray (g,1)
+        Diffusion weighting factor b for each vector in gtab.
+    mask : ndarray (X,Y,Z)
+        Mask that excludes fit in voxels where mask == 0
+    thresh : integer (0,np.max(data))
+        Simple threshold to exclude fit in voxels where b0 < thresh
 
     Key Methods
     -----------
@@ -52,12 +65,38 @@ class tensor(object):
         pass
     
     def __init__(self, data, grad_table, b_values, mask=None,thresh=5,verbose=False):
-        eig_decomp, design_mat = WLS_fit(data,grad_table,b_values,verbose=verbose,mask=mask,thresh=thresh)
+        if mask == None:
+            mask = data[:,:,:,0]
+            mask[mask<thresh] = 0
+            
+        dims = data.shape
+        if mask != None and dims[0:3] != mask.shape:
+            raise ValueError('Data image and mask MUST have same 3D volume shape!')
+       
+        mask4d = mask[:,:,:,np.newaxis]
+        for i in range(dims[3]-1):
+            mask4d = np.concatenate((mask4d,mask[:,:,:,np.newaxis]),axis=3)
+
+        #no support from maskedview to handle 3D mask for 4D data
+        #   therefore input mask must be 4D
+        #no maskedview.reshape
+        #   need to reshape the 4d mask
+        #also data needs to be masked first
+        #   mask needs to be broadcasted to 4D to match and then mask data
+        data = MaskedView(mask4d.reshape((np.prod(dims[0:3]),dims[3])),data[mask4d>0]) #mask[:,:,:,np.newaxis]>0]) 
+
+        eig_decomp, design_mat = WLS_fit(data,grad_table,b_values,verbose=verbose)
+        
+        mask = mask.reshape(dims)
+        eig_decomp = MaskedView(mask,eig_decomp)
         self.evals = eig_decomp[:,:,:,0:3]
         self.evecs = eig_decomp[:,:,:,3:12]
         self.prime_evec = eig_decomp[:,:,:,3:6]
+        
+        #this is for convenience (does not add much memory)
         self.adc = self.calc_adc()
         self.fa = self.calc_fa()
+        #self.D = self.calc_D()
     
     def calc_D(self):
         D = np.dot(np.dot(Q,delta),np.linalg.pinv(Q))
@@ -81,48 +120,53 @@ class tensor(object):
         return fa 
 
 
-def WLS_fit (data,gtab,bval,mask=None,thresh=None,verbose=False):    
+def WLS_fit (data,gtab,bval,verbose=False):    
     """
     Computes weighted least squares (WLS) fit to calculate self-diffusion tensor. 
     (Basser et al., 1994a)
 
     Parameters
     ----------
-    data : ndarray (X,Y,Z,g)
+    data : ndarray (X,Y,Z,g) OR Maskedview (X*Y*Z,g) [preferred]
         The image data as a numpy ndarray.
     gtab : ndarray (3,g)
         Diffusion gradient table found in DICOM header as a numpy ndarray.
     bval : ndarray (g,1)
         Diffusion weighting factor b for each vector in gtab.
-    mask : ndarray (X,Y,Z)
-        Mask that excludes fit in voxels where mask == 0
-    thresh : integer (0,np.max(data))
-        Simple threshold to exclude fit in voxels where b0 < thresh
     verbose : boolean
         Boolean to indicate verbose output such as timing.
 
+    Returns
+    -------
+    eig_decomp : ndarray (X,Y,Z,12) OR Maskedview (X*Y*Z,g)
+        Eigenvalues and eigenvectors from eigen decomposition of the tensor
+    design_mat : ndarray (g,7)
+        DTI design matrix to reconstruct fitted data if desired
 
     """
     start_time = time.time()
     ####main part of code
     dims = data.shape
-    
-    if mask != None and dims[0:3] != mask.shape :
-        raise ValueError('Data image and mask MUST have same 3D volume shape!')
+        
+    if len(dims) == 4:
+        fit_dim = (dims[0]*dims[1]*dims[2],dims[3])
+        # Y matrix from Chris' paper
+        data = data.reshape(fit_dim) #direct reshape for some reason does not work
+    elif len(dims) == 2:
+        fit_dim = dims
 
     ###Create log of signal and reshape it to be x:y:z by grad
-    fit_dim = (dims[0]*dims[1]*dims[2],dims[3])
-    data = data.reshape(fit_dim) #direct reshape for some reason does not work
-    #log_s = np.zeros(fit_dim,dtype='float32')
-    #log_s_ols = np.zeros(fit_dim,dtype='int16')
-    
-    # Y matrix from Chris' paper
-    # enforcing positive values to allow for natural log
-    data[data <= 0] = 1
-    # need to set this seperately to enforce int16 precision
-    # set precision to 3 significant figures...to save memory
-    # instead of later calculating it with log_s_ols
-    log_s = np.int16(np.log(data) * 1000)
+    if isinstance(data,np.ndarray):
+        data[data <= 0] = 1 # enforcing positive values to allow for natural log
+        ###Create log of signal and reshape it to be x:y:z by grad
+        # need to set this seperately to enforce int16 precision
+        # set precision to 3 significant figures...to save memory
+        # instead of later calculating it with log_s_ols
+        log_s = np.int16(np.log(data) * 1000)
+        scale=1/1000.
+    else:
+        log_s = np.log(data)
+        scale=1
 
     ###Construct design matrix
     #For DTI this is the so called B matrix
@@ -132,8 +176,12 @@ def WLS_fit (data,gtab,bval,mask=None,thresh=None,verbose=False):
     ###Weighted Least Squares (WLS) to solve "linear" regression
     # Y hat OLS from Chris' paper
     #  ( [x*y*z by g] [g by 7] [7 by g ] ) = [x*y*z by g]
-    log_s_ols = np.int16( np.dot(log_s, np.dot(B, np.linalg.pinv(B))) )
-    #del log_s #freeing up memory
+    log_s_ols = np.dot(log_s, np.dot(B, np.linalg.pinv(B)))
+    del log_s #freeing up memory
+    
+    #handling if ndarray is sent
+    if isinstance(data,np.ndarray):
+        log_s_ols = np.int16(log_s_ols)
 
     #Setting these arrays later to allow the previous step to have all memory
     #fit_data = np.zeros(fit_dim,dtype='int16') #original data is int16
@@ -142,7 +190,7 @@ def WLS_fit (data,gtab,bval,mask=None,thresh=None,verbose=False):
     time_diff = list((0,0))
     time_iter = time.time()
     # This step is because we cannot vectorize diagonal vector and tensor fit
-    for i in range(np.size(log_s_ols,axis=0)):
+    for i in range(log_s_ols.shape[0]): #range(np.size(log_s_ols,axis=0)):
         #Check every 1 slices
         if verbose and i % (dims[0]*dims[1]*1) == 0:
             slice = i/dims[0]/dims[1]+1.
@@ -151,31 +199,25 @@ def WLS_fit (data,gtab,bval,mask=None,thresh=None,verbose=False):
             sec = np.round((min - np.fix(min)) * 60.0/5)
             min = np.fix(min)
             percent = 100.*slice/dims[2]
-            print str(np.round(percent)) + '% ... time left ' + str(min) + ' MIN ' + str(sec) + ' SEC'
+            print str(np.round(percent)) + '% ... time left: ' + str(min) + ' MIN ' \
+                                + str(sec) + ' SEC ... memory: ' + memory()/1024. + 'MB'
             time_iter=time.time()
  
-        if mask != None and mask.flat[i] == 0:
-            continue
-
-        if thresh != None and np.exp(log_s[i,0]/1000.) < thresh:
-            continue
-
         #Split up weighting vector into little w to perform pinv
-        w = np.exp(log_s_ols[i,:]/1000.)[:,np.newaxis]
+        w = np.exp(log_s_ols[i,:]*scale)[:,np.newaxis]
     
         #pointwise broadcasting to avoid diagonal matrix multiply!
-        D = np.dot(np.linalg.pinv(B*w), w.ravel()*log_s[i,:]/1000.) #np.log(data[:,i]))
-        #fit_data[i,:] = np.int16(np.exp(np.dot(B,D))) 
+        D = np.dot(np.linalg.pinv(B*w), w.ravel()*np.log(data[:,i])*scale) #log_s[i,:]
         
         ###Obtain eigenvalues and eigenvectors
         eig_decomp[i,:] = decompose_tensor(D[0:6],scale=1)
 
     #clear variables not needed to save memory
     del log_s_ols
-    del log_s
 
     # Reshape the output
-    eig_decomp = eig_decomp.reshape((dims[0],dims[1],dims[2],12))
+    if len(dims) == 4:
+        eig_decomp = eig_decomp.reshape((dims[0],dims[1],dims[2],12))
    
     #Report how long it took to make the fit  
     if verbose:
@@ -188,7 +230,8 @@ def WLS_fit (data,gtab,bval,mask=None,thresh=None,verbose=False):
 
 def decompose_tensor(D,scale=1):
     """
-    Computes tensor eigen decomposition to calculate eigenvalues and eigenvectors of self-diffusion tensor. Assumes D has units on order of ~ 10^-4 mm^2/s
+    Computes tensor eigen decomposition to calculate eigenvalues and eigenvectors of 
+    self-diffusion tensor. Assumes D has units on order of ~ 10^-4 mm^2/s
     (Basser et al., 1994a)
 
     Parameters
@@ -251,8 +294,10 @@ def design_matrix(gtab,bval,dtype='float32'):
     G = gtab
     
     if gtab.shape[1] != bval.shape[0] :
-        print 'Gradient table size is not consistent with bval vector... could be b/c of b0 images'
-        print 'Will try to set nonzero bval index with gradient table to construct B matrix'
+        print 'Gradient table size is not consistent with bval vector... could be b/c \
+               of b0 images'
+        print 'Will try to set nonzero bval index with gradient table to construct \
+               B matrix'
         
         G = np.zeros((3,np.size(bval)))
         G[:,np.where(bval > 0)]=gtab
