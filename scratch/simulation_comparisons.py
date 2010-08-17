911ac2064bd5593eb81b4a20ed46be8b7019cf38
iannimmosmith <ian@monksilver.com>
2010-08-17 21:41:03 +0100

    NF - simulation_comparisons

--- scratch/simulation_comparisons.py
+++ scratch/simulation_comparisons.py
@@ -3,15 +3,35 @@ import dipy as dp
 import dipy.io.pickles as pkl
 import scipy as sp
 from matplotlib.mlab import find
-import dipy.core.sphere_plots as splot
+import dipy.core.sphere_plots as splots
+import dipy.core.sphere_stats as sphats
 import dipy.core.geometry as geometry
 import get_vertices as gv
 
 fname='/home/ian/Data/SimData/results_SNR030_1fibre'
+#fname='/home/eg01/Data_Backup/Data/Marta/DSI/SimData/results_SNR030_isotropic'
 
 sim_data=np.loadtxt(fname)
 
-v = gv.get_vertex_set('dsi101')
+''' file  has one row for every voxel, every voxel is repeating 1000
+times with the same noise level , then we have 100 different
+directions. 1000 * 100 is the number of all rows.
+
+'''
+
+marta_table_fname='/home/ian/Data/SimData/Dir_and_bvals_DSI_marta.txt'
+#bvalsf='/home/eg01/Data_Backup/Data/Marta/DSI/SimData/bvals101D_float.txt'
+
+b_vals_dirs=np.loadtxt(marta_table_fname)
+
+bvals=b_vals_dirs[:,0]*1000
+gradients=b_vals_dirs[:,1:]
+
+#splots.plot_sphere(gradients, 'Marta DSI gradients')
+
+v = gv.get_vertex_set('dsi102')
+
+#splots.plot_sphere(v, 'dsi102 axes')
 
 gqfile = '/home/ian/Data/SimData/gq_SNR030_1fibre.pkl'
 gq = pkl.load_pickle(gqfile)
@@ -47,38 +67,31 @@ times with the same noise level , then we have 100 different
 directions. 1000 * 100 is the number of all rows.
 '''
 
+results = []
 
-dt_first_directions = tn.evecs[:,:,0]
+dt_first_directions = tn.evecs[:,:,0].reshape((100,1000,3))
+# these are the principal direcections for the full set of simulations
 
-for direction in range(2):
 
-    dt_batch = dt_first_directions[1000*direction:1000*(direction+1),:]
+for direction in range(100):
 
-    up = dt_batch[:,2]>= 0
+    dt_batch = dt_first_directions[direction,:,:]
+    
+    #up = dt_batch[:,2]>= 0
 
-    #splot.plot_sphere(batch[up],'batch '+str(direction))
+    #splots.plot_sphere(dt_batch[up], 'batch '+str(direction))
 
-    cross = np.dot(dt_batch.T,dt_batch)/1000
-
-    evals, evecs = np.linalg.eig(cross)
-
-    order = np.argsort(evals)
-
-    centre = geometry.cart2sphere(*evecs[:,order[2]])
-
-    splot.plot_sphere(dt_batch[up], 'batch '+str(direction))
-
-    splot.plot_lambert(dt_batch[up],'batch '+str(direction), centre)
+    #splots.plot_lambert(dt_batch[up],'batch '+str(direction), centre)
     
     #spread = gq.q2odf_params e,v = np.linalg.eigh(np.dot(spread,spread.transpose())) effective_dimension = len(find(np.cumsum(e) > 0.05*np.sum(e))) #95%
 
-    rotated = np.dot(dt_batch,evecs)
+    #rotated = np.dot(dt_batch,evecs)
 
-    rot_evals, rot_evecs =  np.linalg.eig(np.dot(rotated.T,rotated)/rotated.shape[0])
+    #rot_evals, rot_evecs =  np.linalg.eig(np.dot(rotated.T,rotated)/rotated.shape[0])
 
-    eval_order = np.argsort(rot_evals)
+    #eval_order = np.argsort(rot_evals)
 
-    rotated = rotated[:,eval_order]
+    #rotated = rotated[:,eval_order]
 
     #up = rotated[:,2]>= 0
 
@@ -86,3 +99,10 @@ for direction in range(2):
 
     #splot.plot_lambert(rotated[up],'batch '+str(direction))
 
+    c,b = sphats.eigenstats(dt_batch)
+
+    results.append(np.concatenate((c,b)))
+
+np.set_printoptions(precision=6, suppress=True)
+    
+print np.vstack(results)
