c23e131c3f80e6c5a6152d9bd7b1676afd053c0c
Ian Nimmo-Smith <iannimmosmith@gmail.com>
2010-08-29 23:54:02 +0100

    NF - DT and GQ comparison stats

--- dipy/core/sphere_stats.py
+++ dipy/core/sphere_stats.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import dipy.core.geometry as geometry
 
@@ -35,16 +37,28 @@ def eigenstats(points, alpha=0.05):
     e[0,1] = np.sum((p0*p1*(p2**2))/(n*(tau[0]-tau[2])*(tau[1]-tau[2])))
     e[1,0] = e[0,1]
 
-    try:
-        f = np.linalg.inv(e)
+    b = np.array([np.NaN,np.NaN])
 
-    except np.linalg.LinAlgError, sing:
-        return centre, np.array([0,0])
-    
-    b = np.array([0,0])
-    t, y = np.linalg.eig(f)
-    d = -2*np.log(alpha)/n
-    g = np.sqrt(d/t)
-    b = np.arcsin(g)*rad2deg
+    try:
+        #print 'in try'
+        if np.abs(np.linalg.det(e)) < 10**-20:
+            b = np.array([0,0])
+        else:
+            f = np.linalg.inv(e)
+            t, y = np.linalg.eig(f)
+            d = -2*np.log(alpha)/n
+            g = np.sqrt(d/t)
+            b= np.arcsin(g)*rad2deg
+        
+    except np.linalg.LinAlgError as detail:
+        print 'in linalgerror'
+        #print type(detail)
+        #print detail
+        b = np.array([0,0])
+
+    except:
+        print 'in except'
+        print 'Unexpected error:', sys.exc_info()[0]
+        raise        
 
     return centre, b
