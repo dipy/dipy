0584d43484969e0fb1f833822219f990d210a0a8
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-09-01 22:39:33 +0100

    working on bvecs correction

--- dipy/core/correction.py
+++ dipy/core/correction.py
@@ -4,17 +4,54 @@ import numpy as np
 import nibabel as ni
 import dipy as dp
 
-def add_padding(data,even):
+def add_padding(data,pad=10,value=0):
     ''' some times the registration could take you a bit out of the
-    field of view of the image. If that is the case zero-padding can be helpfull.
-    '''
-    pass
+    field of view of the image. If that is the case zero-padding can be
+    helpfull.
 
-def remove_padding(data,even):
-    ''' well if you added padding before the registration you should
-    better remove it at the end to return to your proper size of the volume.
+    Parameters
+    ----------
+    data: array, shape (X,Y,Z,...)
+    pad: int, number extra margin voxels on each side
+    value: scalar, value to padd    
+
+    Example
+    -------
+    >>> data=np.ones((30,30,30))
+    >>> ndata=add_padding(data,10)
+    >>> ndata.shape
+    (50,50,50)
+    >>> data=rm_padding(ndata,10)
+    >>> data.shape
+    (30,30,30)
+    
     '''
-    pass
+    new_data=value*np.ones((data.shape[0]+2*pad,data.shape[1]+2*pad,data.shape[2]+2*pad),dtype=data.dtype)
+    new_data[pad:pad+data.shape[0],pad:pad+data.shape[1],pad:pad+data.shape[2]]=data
+    return new_data
+        
+
+def rm_padding(data,pad=10):
+    ''' well if you added padding before the registration you should
+    better remove it at the end to return to the proper size of the
+    volume.
+
+    Example
+    -------
+    >>> data=np.ones((30,30,30))
+    >>> ndata=add_padding(data,10)
+    >>> ndata.shape
+    (50,50,50)
+    >>> data=rm_padding(ndata,10)
+    >>> data.shape
+    (30,30,30)
+
+    See also
+    --------
+    add_padding
+    
+    '''
+    return data[pad:data.shape[0]-pad,pad:data.shape[1]-pad,pad:data.shape[2]-pad]
 
 
 def eliminate_negatives(data):
@@ -26,13 +63,56 @@ def eliminate_negatives(data):
 
 
 def bvecs_correction(bvecs,mats):
-    ''' after motion correction it is crucial to correct the bvecs
+    ''' after motion correction it might be necessary to correct the
+    bvecs.
+
+    Parameters
+    ----------
+
+    bvecs: array,shape (N,3)
+    mats : dictionary, len(N), containing rotation information. This
+    function is returned by motion_correction
+
+    Returns
+    ------
+
+    nbvecs: array, shape (N,3), new corrected bvecs
+    
+    Notes
+    -----
+    
+    Only the rotation component of the affine transformation is
+    necessary.
+
+    See also
+    --------
+    motion_correction
+    
+    
     '''
-    pass
+    nbvecs=bvecs
+
+    for m in mats:
+
+        try:
+            a=mats[m].shape
+        except:
+            mi=mats[m].inv()
+            R=dp._rotation_vec2mat(mi.vec12[3:6])
+            b=bvecs[m]
+            b.shape=(3,1)
+            #print b.shape, R.shape, b, R
+            #print R
+            nb=np.dot(R,b)
+            nbvecs[m]=nb.ravel()
+
+    
+    return nbvecs
+    
 
 
 def motion_correction(data,affine,ref=0,similarity='cr',interp='tri',subsampling=None,search='affine',optimizer='powel',order=0):
-    ''' Correct for motion and eddy current correction using the nipy
+    ''' Correct for motion and eddy current correction using the nipy affine
     registration tools.    
     
     Parameters
@@ -86,21 +166,24 @@ def motion_correction(data,affine,ref=0,similarity='cr',interp='tri',subsampling
             A_mats[s]=A#._get_param()
 
             #USE dp.rotation_vec2mat to get matrix from rotation perhaps
-            #you need the inverse first
+            #you probably need the inverse first
         
     return ND, A_mats
-       
+
 
 #dname = '/home/eg01/Data_Backup/Data/Eleftherios/CBU090133_METHODS/20090227_145404/Series_003_CBU_DTI_64D_iso_1000'
 #dname =  '/home/eg01/Data_Backup/Data/Frank_Eleftherios/frank/20100511_m030y_cbu100624/08_ep2d_advdiff_101dir_DSI'
 #data,affine,bvals,gradients=dp.load_dcm_dir(dname)
 
+#'''
+
 img=ni.load('/tmp/testing_features/18620_0006.nii')
 data=img.get_data().astype('uint16')
 affine=img.get_affine()
-data_corr,mats=motion_correction(data,affine,similarity='mi',order=3)
+data_corr,mats=motion_correction(data,affine,similarity='cr',order=3)
 
 img=ni.Nifti1Image(data_corr,affine)
-ni.save(img,'/tmp/saved5.nii.gz')
+ni.save(img,'/tmp/18620_0006_cr_order3.nii.gz')
 
+#'''
 
