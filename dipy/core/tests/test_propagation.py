fb2f00f0780ce447b985aedf5d13a36c4fcff4b1
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-09-04 23:07:44 +0100

    TEST - new test file added for propagation

---
+++ dipy/core/tests/test_propagation.py
@@ -0,0 +1,14 @@
+import numpy as np
+import dipy as dp
+import nibabel as ni
+
+from nose.tools import assert_true, assert_false, \
+     assert_equal, assert_raises
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+
+def test_fact():
+
+    pass
+
