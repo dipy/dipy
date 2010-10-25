c9ef63319e0fbd0d610453b9063e52edc07939e9
Matthew Brett <matthew.brett@gmail.com>
2010-10-26 00:17:38 +0100

    NF - add test function to top level

--- dipy/__init__.py
+++ dipy/__init__.py
@@ -32,6 +32,11 @@ try:
 except ImportError:
     raise ImportError('nibabel.nicom.dicomreaders cannot be found')
 
+# Test callable
+from numpy.testing import Tester
+test = Tester().test
+del Tester
+
 # Plumb in version etc info stuff
 from .pkg_info import get_pkg_info as _get_pkg_info
 get_info = lambda : _get_pkg_info(os.path.dirname(__file__))
