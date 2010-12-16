69b6392c49865cc7d52f24b94399f5877ed694c0
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-12-16 12:11:11 +0000

    RF data and reconstruction

---
+++ dipy/data/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+def get_sphere(name='symmetric362'):    
+    if name=='symmetric362':
+        return os.path.join(os.path.dirname(__file__),'evenly_distributed_sphere_362.npz')
+    if name=='symmetric642':
+        return os.path.join(os.path.dirname(__file__),'evenly_distributed_sphere_642.npz')
+    
+def get_data(name='small_64D'):    
+    if name=='small_64D':
+        fbvals=os.path.join(os.path.dirname(__file__),'small_64D.bvals.npy')
+        fbvecs=os.path.join(os.path.dirname(__file__),'small_64D.gradients.npy')
+        fimg =os.path.join(os.path.dirname(__file__),'small_64D.nii')        
+        return fimg,fbvals, fbvecs
+    
+        
+    
+    
---
+++ dipy/reconstruction/dandelion.py
@@ -0,0 +1,178 @@
+import numpy as np
+from dipy.core.reconstruction_performance import peak_finding
+import os
+from os.path import join as opj
+from dipy.data import get_sphere
+
+class SphericalDandelion():
+    ''' Garyfallidis E., Nimmo-Smith I. TMI 2011 (to appear)    
+    '''
+
+    def __init__(self,data,bvals,gradients,smoothing=1.,odfsphere=None,mask=None):
+
+        '''
+
+        Parameters
+        -----------
+        data: array, shape(X,Y,Z,D)        
+        bvals: array, shape (N,)
+        gradients: array, shape (N,3) also known as bvecs
+        smoothing: float, smoothing parameter
+
+        See also
+        --------
+        Tensor, GeneralizedQSampling
+
+        '''
+        
+        if odfsphere == None:            
+            eds=np.load(get_sphere('symmetric362'))
+        else:
+            eds=np.load(odfsphere)
+            # e.g. odfsphere = evenly_distributed_sphere_642.npz
+
+        odf_vertices=eds['vertices']
+        odf_faces=eds['faces']
+
+        self.odf_vertices=odf_vertices
+        self.bvals=bvals
+        
+        # 0.01506 = 6*D where D is the free water diffusion coefficient 
+        # l_values sqrt(6 D tau) D free water diffusion coefficient and
+        # tau included in the b-value
+        #scaling = np.sqrt(bvals*0.01506)
+        #tmp=np.tile(scaling, (3,1))
+        #the b vectors might have nan values where they correspond to b
+        #value equals with 0
+        gradients[np.isnan(gradients)]= 0.
+        
+        #gradsT = gradients.T
+        #b_vector=gradsT*tmp # element-wise also known as the Hadamard product
+        #q2odf_params=np.sinc(np.dot(b_vector.T, odf_vertices.T) * Lambda/np.pi)
+        #q2odf_params=np.real(np.sinc(np.dot(b_vector.T, odf_vertices.T) * Lambda/np.pi))        
+        #q2odf_params[np.isnan(q2odf_params)]= 1.
+       
+        self.gradients=gradients
+        self.weighting=np.dot(gradients,self.odf_vertices.T)     
+        
+        '''
+        r=[[],[],[]]
+        for si in range(len(sL)):
+            for i in range(len(L)):
+                if sL[si]-L[i]==0: 
+                    r[si].append(i)
+        '''            
+
+        
+                  
+        S=data
+        datashape=S.shape #initial shape
+        msk=None #tmp mask
+
+        if len(datashape)==4:
+            x,y,z,g=S.shape        
+            S=S.reshape(x*y*z,g)
+            #QA = np.zeros((x*y*z,5))
+            #IN = np.zeros((x*y*z,5))
+            if mask != None:
+                if mask.shape[:3]==datashape[:3]:
+                    msk=mask.ravel().copy()
+                    #print 'msk.shape',msk.shape
+        if len(datashape)==2:
+            x,g= S.shape
+            #QA = np.zeros((x,5))
+            #IN = np.zeros((x,5))            
+        #glob_norm_param = 0
+        #self.q2odf_params=q2odf_params
+        
+        if mask !=None:
+            for (i,s) in enumerate(S):                            
+                if msk[i]>0:
+                    pass
+                    #Q to ODF
+                    #odf=np.dot(s,q2odf_params)            
+                    #peaks,inds=peak_finding(odf,odf_faces)            
+                    #glob_norm_param=max(np.max(odf),glob_norm_param)
+                    #remove the isotropic part
+                    #peaks = peaks - np.min(odf)
+                    #l=min(len(peaks),5)
+                    #QA[i][:l] = peaks[:l]
+                    #IN[i][:l] = inds[:l]
+
+        if mask==None:
+            for (i,s) in enumerate(S):                            
+                #Q to ODF
+                pass
+                #odf=np.dot(s,q2odf_params)            
+                #peaks,inds=rp.peak_finding(odf,odf_faces)            
+                #glob_norm_param=max(np.max(odf),glob_norm_param)
+                #remove the isotropic part
+                #peaks = peaks - np.min(odf)
+                #l=min(len(peaks),5)
+                #QA[i][:l] = peaks[:l]
+                #IN[i][:l] = inds[:l]
+
+        #normalize
+        #QA/=glob_norm_param
+       
+        if len(datashape) == 4:
+            pass
+            #self.QA=QA.reshape(x,y,z,5)    
+            #self.IN=IN.reshape(x,y,z,5)            
+        if len(datashape) == 2:
+            pass
+            #self.QA=QA
+            #self.IN=IN            
+        #self.glob_norm_param = glob_norm_param
+        
+    def spherical_signal(self,s):
+        #signal distribution function                
+        #sr=s[1:].reshape(1,len(shape))
+        sr=s[1:].reshape(1,len(s))        
+        return np.abs(np.dot(sr,self.mapping))
+    
+    def spherical_diffusivity(self,s):
+        ob=-1/self.bvals[1:]
+        d=ob*np.log(s[1:])/np.log(s[0])
+        
+        d=d.reshape(1,len(d))
+        
+        np.set_printoptions(3)
+        print 'gradients'
+        print self.gradients.shape
+        print self.gradients
+        print 'sphere'
+        print (self.odf_vertices.T).shape
+        print self.odf_vertices.T
+        print 'weighting'
+        print self.weighting.shape
+        print self.weighting
+        print np.sqrt(np.sum(self.weighting**2,axis=1))
+        print np.sqrt(np.sum(self.weighting**2,axis=0))
+        print 'd'
+        print d.shape
+        print d
+        print 'spherical diffusivity'
+        print np.round(10000*np.abs(np.dot(d,self.weighting[1:,:]))).astype('i8')
+        
+        return np.abs(np.dot(d,self.weighting[1:,:]))
+            
+    def odf(self,s):
+        '''
+        Parameters
+        ----------
+        s: array, shape(D) diffusion signal for one point in the dataset
+
+        Returns
+        -------
+        odf: array, shape(len(odf_vertices)), orientation distribution function
+
+        '''
+        #return np.dot(s,self.q2odf_params)
+        pass
+    
+    
+    
+    
+    
+
