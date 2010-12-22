8ab8d7f16633c0575af2e1ee5e46b19bc5feefb5
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2010-12-22 11:18:42 +0200

    FX - cdivision in propspeed

--- dipy/tracking/propspeed.pyx
+++ dipy/tracking/propspeed.pyx
@@ -35,6 +35,7 @@ cdef inline float* asfp(cnp.ndarray pt):
 cdef inline double* asdp(cnp.ndarray pt):
     return <double *>pt.data
 
+@cython.cdivision(True)
 cdef  long offset(long *indices,long *strides,int lenind, int typesize) nogil:
 
     ''' Very general way to access any element of any ndimensional numpy array
