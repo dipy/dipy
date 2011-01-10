b95e1b15805ac2b88a879e9614ffacf7cf6bb617
Ian Nimmo-Smith <iannimmosmith@gmail.com>
2011-01-10 09:39:23 +0000

    changes to nii2tracks example

--- doc/examples/nii_2_tracks.py
+++ doc/examples/nii_2_tracks.py
@@ -1,116 +1,151 @@
+
 """ 
+.. include:: ../links_names.txt
+
 =============================
 From niftis to tractographies
 =============================
 
-First import the necessary modules
+Overview
+========
 
-numpy is for numerical computation
+**This example gives a tour of many of the features of ``dipy``.**
+
+First import the necessary modules
+==================================
+
+* ``numpy`` is for numerical computation
 
 """
 
 import numpy as np
 
 """
-nibabel is for data formats
+* ``nibabel`` is for data formats**
 """
 
 import nibabel as nib
 
 """
-dipy.reconst is for reconstruction algorithms
+* ``dipy.reconst`` is for the reconstruction algorithms which we use to create directionality models 
+for a voxel from the raw data. 
 """
 
 import dipy.reconst.gqi as gqi
 import dipy.reconst.dti as dti
 
 """
-dipy.tracking is for tractography algorithms.
+* ``dipy.tracking`` is for tractography algorithms which create sets of tracks by integrating 
+directionality models across voxels.
 """
 
 from dipy.tracking.propagation import EuDX
 
 """
-dipy.data is for using small test and example datasets.
+* ``dipy.data`` is for small datasets for use in tests and examples.
 """
 
 from dipy.data import get_data
 
 
 """ 
-The datasets used need to have isotropic voxel size. If you have datasets with anisotropic voxel size 
-then you can have a look at the example resample_aniso_2_iso.py
+Isotropic voxel sizes required
+==============================
+``dipy`` requires its datasets to have isotropic voxel size. If you have datasets with anisotropic voxel size 
+then you need to resample with isotropic voxel size. We have provided an algorithm for this. 
+You can have a look at the example ``resample_aniso_2_iso.py``
 
-get_data is a simple function which brings here a small region of interest to play with. In order to make this work with your data you should comment the 
-line below and add the paths for your nifti file (*.nii or *.nii.gz) and your *.bvec and *.bval files. If you are not using nifti files or you don't
-know how to create the *.bvec and *.bval files from your raw dicom (*.dcm) data then you can either try the example called dcm_2_tracks.py or use mricron
-to convert the dicom files to nii, bvec and bval files using dcm2nii. 
+Accessing the necessary datasets
+================================
+``get_data`` is provides data for a small region of interest from a real
+diffusion weighted MR dataset acquired with 101 gradient. 
+In order to make this work with your data you should comment out the line below and add the paths 
+for your nifti file (``*.nii`` or ``*.nii.gz``) and your ``*.bvec`` and ``*.bval files``. If you are not using 
+nifti files or you don't know how to create the ``*.bvec`` and ``*.bval`` files from your raw dicom (``*.dcm``) 
+data then you can either try the example called ``dcm_2_tracks.py`` or use _mricron
+to convert the dicom files to nii, bvec and bval files using ``dcm2nii``. 
 """
 
 fimg,fbvals,fbvecs=get_data('small_101D')
 
 """ 
-Load the nifti file found at path fimg as an Nifti1Image. 
+* **Load the nifti file found at path ``fimg`` as an Nifti1Image.**
 """
 
 img=nib.load(fimg)
 
 """ 
-Read the datasets from the Nifti1Image.
+* **Read the datasets from the Nifti1Image.**
 """
 
 data=img.get_data()
 print('data.shape (%d,%d,%d,%d)' % data.shape)
 
 """ 
-as you would expect data is 4-dimensional as you have one 3d volume for every gradient direction.
-Read the affine matrix which gives the mapping between volume indices (voxel coordinates) and world coordinates.
+As you would expect, your raw diffusion weighted MR data is 4-dimensional as 
+you have one 3d volume for each gradient direction.
+
+* **Read the affine matrix**
+which gives the mapping between volume indices (voxel coordinates) and world coordinates.
 """
 
 affine=img.get_affine()
 
 """ 
-Read the b-values - a function of the strength, duration, temporal spacing and timing parameters of the
-specific paradigm used in the scanner. 
+* **Read the b-values**
+these are a function of the strength, duration, temporal spacing and timing parameters of the 
+specific paradigm used in the scanner, one per gradient direction.
 """
 
 bvals=np.loadtxt(fbvals)
 
 """ 
-Read the b-vectors - unit gradient directions.
+* **Read the b-vectors - unit gradient directions.**
 """
 
 gradients=np.loadtxt(fbvecs).T
 
 """ 
-Calculate the single Tensor.  
+Calculating models and parameters of directionality
+---------------------------------------------------
+We are now set up with all the data and parameters to start calculating directional models 
+for voxels and their associated parameters, e.g. anisotropy.
+
+* **Calculate the single tensor model.**  
 """
 
 ten=dti.Tensor(data,bvals,gradients,thresh=50)
 
 """ 
-Calculate Fractional Anisotropy (FA) from the single tensor
+* **Calculate Fractional Anisotropy (FA) from the single tensor model**
 """
 
 FA=ten.fa()
 print('FA.shape (%d,%d,%d)' % FA.shape)
 
 """ 
-Generate tractography using Euler Delta Crossings (EuDX)
-
-The main parameters of EuDX is an anisotropic scalar metric e.g. FA and the indices for the peaks on the sampling sphere.
-Other import options are the number of random seeds where the track will start propagate and the stopping criteria for
-example a low threshold for anisotropy for example if we are using FA a_low=.2    
+Generate a tractography 
+-----------------------
+Here we use the Euler Delta Crossings (EuDX) alggorithm.
+The main parameters of ``EuDX`` are 
+  - an anisotropic scalar metric e.g. FA, and 
+  - the indices for the peaks on the sampling sphere. 
+Other important options are 
+  - the number of random seeds where the track propagation is initiated, and 
+  - the stopping criteria, for example a low threshold for anisotropy. For instance 
+if we are using fractional anisotropy (FA) a typical threshold value might be ``a_low=.2``    
 """
 
 eu=EuDX(a=FA,ind=ten.ind(),seed_no=10000,a_low=.2)
 
 """ 
-EuDX returns a generator class which yields a track each time this class is called. 
-With that way we can generate millions of tracks without using any substantial amount of memory. 
-For an example of what to do when you want to generate millions of tracks have a look at save_dpy.py in examples.
+EuDX returns a generator class which yields a further track each time this class is called. 
+In this way we can generate millions of tracks without using a substantial amount of memory. 
+For an example of what to do when you want to generate millions of tracks have a look at 
+``save_dpy.py`` in the ``examples`` directory.
 
-However, in this example that we only have 10000 seeds we have loaded all tracks in a list using list comprehension([]) without worry much about memory.  
+However, in the current example that we only have 10000 seeds, we have loaded all tracks in a list 
+using list comprehension([]) without worry much about memory.  
 """
 
 ten_tracks=[track for track in eu]
@@ -122,8 +157,11 @@ In dipy we usually represent tractography as a list of tracks where every track 
 print('Number of FA tracks %d' % len(ten_tracks))
 
 """ 
-Another way to represent tractography is as a numpy array of numpy objects. This way has an additional advantage that it can be saved very easily using the numpy. 
-In theory in a list is faster to append an element and in an array is faster to access. In other words both representations have different + and -. Other representations are possible too e.g. graphtheoretic etc.
+Another way to represent tractography is as a numpy array of numpy objects. 
+This way has an additional advantage that it can be saved very easily using the numpy. 
+In theory in a list is faster to append an element and in an array is faster to access. 
+In other words both representations have different + and -. 
+Other representations are possible too e.g. graphtheoretic etc.
 """
 
 ten_tracks_asobj=np.array(ten_tracks,dtype=np.object)
@@ -131,10 +169,12 @@ np.save('ten_tracks.npy',ten_tracks_asobj)
 print('FA tracks saved in ten_tracks.npy')
 
 """ 
-You probably have heard about the problem of crossings in diffusion MRI. 
-The single tensor model cannot detect a crossing.
-With Generalized Q-Sampling this is possible even up to a quadruple crossing 
-or even higher as long as your datasets are able to provide that resolution.
+Crossings and GQS
+-----------------
+You probably have heard about the problem of crossings in diffusion MRI. a metric rather
+The single tensor model cannot detect a simple crossing of two fibres.
+With Generalized Q-Sampling (GQS) this is possible even up to a quadruple crossing 
+or even higher so long as your datasets are able to provide that resolution.
 """
 
 gqs=gqi.GeneralizedQSampling(data,bvals,gradients)
@@ -142,9 +182,18 @@ QA=gqs.qa()
 print('QA.shape (%d,%d,%d,%d)' % QA.shape)
 
 """ 
-The outcome of gqs is Quantitative Anisotropy a metric much different than FA even in shape however this can be feeded again at the
-same EuDX class with no problem. This is one of the advantages of EuDX that it can be used with all known model-based methods for example Single Tensor, Multiple Tensor, Stick & Ball, Higher Order Tensor and model-free methods DSI, QBall, GQI etc.
-We created this so we can compare tractographies generated from very different models.  
+A useful metric derived from GQS is *Quantitative Anisotropy* (QA). The QA function on the sphere is 
+significantly different in shape from FA, however it too can be directly input to the EuDX class.
+ 
+This is one of the advantages of EuDX that it can be used with a wide range of model-based methods, such as 
+  - Single Tensor,
+  - Multiple Tensor, 
+  - Stick & Ball, 
+  - Higher Order Tensor, and 
+  - model-free methods such as DSI, QBall, GQI etc.
+
+We designed this algorithm so we that we can compare directly tractographies generated 
+from very different models or choices of threshold.  
 """
 
 eu2=EuDX(a=QA,ind=gqs.ind(),seed_no=10000,a_low=.0239)
@@ -152,10 +201,11 @@ gqs_tracks=[track for track in eu2]
 print('Number of QA tracks %d' % len(gqs_tracks))
 
 """ 
-Do you see the difference between the number of gqs_tracks and ten_tracks? Can you think a reason why? Correct, CROSSINGS!!!
-When crossings are supported from the underlying model then an equal number of different tracks can start propagating from the seed towards different directions.
+Do you see the difference between the number of gqs_tracks and ten_tracks? Can you think of a reason? 
+Correct, *crossings*! When the underlying directionality model supports crossings then 
+distinct tracks will be propagated from a seed towards the different directions in equal abundance.
   
-In dipy it is very easy to count the number of crossings in a voxel, volume or region of interest
+In ``dipy`` it is very easy to count the number of crossings in a voxel, volume or region of interest
 
 """
 
@@ -164,8 +214,8 @@ np.save('gqs_tracks.npy',gqs_tracks_asobj)
 print('QA tracks saved in gqs_tracks.npy')
 
 """
-This is the end of this very simple example you can load again the saved tracks using np.load from your current directory. You can optionaly install python-vtk
-and visualize the tracks using fvtk
+ **This is the end of this very simple example you can load again the saved tracks using ``np.load`` from your current directory. You can optionaly install python-vtk
+and visualize the tracks using ``fvtk``.**
 """
 
 from dipy.viz import fvtk
@@ -176,7 +226,7 @@ fvtk.add(r,fvtk.line(gqs_tracks2,fvtk.green,opacity=0.1))
 #fvtk.show(r)
 
 """
-Thank you!
+**Thank you!**
+--------------
 """
-
 
