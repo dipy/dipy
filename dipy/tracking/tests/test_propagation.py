021f431cada8d9588b79f68720c8a5ccaa483725
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2011-01-17 20:50:58 +0000

    TEST - tiny fix

--- dipy/tracking/tests/test_propagation.py
+++ dipy/tracking/tests/test_propagation.py
@@ -11,7 +11,7 @@ from dipy.tracking.metrics import length
 import nibabel as ni
 
 from nose.tools import assert_true, assert_false, \
-     assert_equal, assert_raises
+     assert_equal, assert_raises, assert_almost_equal
 
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 
