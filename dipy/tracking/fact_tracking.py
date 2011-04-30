d1e80c81de8975d81421fc5158f870c5e303a5ab
Bago Amirbekian <Bago.Amirbekian@ucsf.edu>
2011-04-29 17:10:24 -0700

    NF - adding fact_tracking

---
+++ dipy/tracking/fact_tracking.py
@@ -0,0 +1,106 @@
+from numpy import abs, array, asarray, atleast_3d, cos, dot, empty, mgrid, \
+                  pi, round, sqrt
+
+pn_edge = array([[0], [1]])
+small_number = 1e-1
+
+def propagate(voxel_location, cur_step, prev_step, vox_size):
+    """takes a step just past the edge of the next voxel along cur_step
+
+    given a location and a cur_step, finds the smallest step needed to move 
+    into the next voxel
+    """
+
+    if dot(cur_step, prev_step) < 0:
+        cur_step = -cur_step
+
+    #change cur_step from mm to vox dim
+    vox_step = cur_step/vox_size
+
+    space = (voxel_location - .5) % 1
+    dist = pn_edge - space
+    step_size = dist/vox_step
+    step_size = step_size.ravel()
+    step_size.sort()
+    #because all values in space are > 0 and < 1, 3 step_sizes are < 0 and 3
+    #are > 0, take the smallest step > 0
+    smallest_step = step_size[3] + small_number
+    new_loc = voxel_location + smallest_step*vox_step
+
+    #we need to know cur_step to pass to the next propagation step
+    return new_loc, cur_step
+
+def track_tensor(tensor, start_loc, start_step, vox_size, fa_limit, 
+                 angle_limit):
+    """makes tracks using tensor starting from stat_loc"""
+
+    evec1_vol = tensor.evecs[...,0]
+    fa_vol = tensor.fa()
+    dot_limit = cos(pi*angle_limit/180)
+
+    all_tracks = []
+    prev_step = start_step
+
+    for vox_loc in start_loc:
+        track = [vox_loc]
+        cur_step, fa = get_tensor_info(vox_loc, evec1_vol, fa_vol)
+        if dot(cur_step, start_step) > 0:
+            prev_step = cur_step
+        else:
+            prev_step = -cur_step
+        while keep_tracking(cur_step, prev_step, fa, fa_limit, dot_limit):
+            vox_loc, prev_step = propagate(vox_loc, cur_step, prev_step, 
+                                           vox_size)
+            track.append(vox_loc)
+            cur_step, fa = get_tensor_info(vox_loc, evec1_vol, fa_vol)
+        if len(track) > 1:
+            all_tracks.append((array(track)*vox_size, None, None))
+
+    return all_tracks
+
+def get_tensor_info(vox_loc, evec1_vol, fa_vol):
+    """returns ev1 and fa for a given location"""
+    ind = round(vox_loc)
+    step = evec1_vol[ind[0], ind[1], ind[2]]
+    fa = fa_vol[ind[0], ind[1], ind[2]]
+    return step, fa
+
+def keep_tracking(cur_step, prev_step, fa, fa_limit, dot_limit):
+    """checks the fa_limit and dot_limit for the current location"""
+
+    if fa < fa_limit:
+        return False
+
+    d = dot(cur_step, prev_step)
+    if abs(d) < dot_limit:
+        return False
+
+    return True
+
+def seeds_from_mask(mask, density):
+    """Takes a binary mask and returns seeds in voxels != 0
+
+    places evanly spaced points in nonzero voxels of mask, spaces the points
+    based on density. For example if density is [1, 2, 3], there will be 6
+    points in each voxel, at x=.5, y=[.25, .7] and z=[.166, .5, .833]. 
+    density = a is the same as density = [a, a, a]
+
+    """
+    mask = atleast_3d(mask)
+    if mask.ndim != 3:
+        raise ValueError('mask cannot be more than 3d')
+    sp = empty(3)
+    sp[:] = 1/asarray(density)
+
+    voxels = mask.nonzero()
+    mg = mgrid[0:1:sp[0], 0:1:sp[1], 0:1:sp[2]]
+
+    seeds = [] 
+    for ii, jj, kk in zip(voxels, mg, sp):
+        s = ii[:,None] + jj.ravel() + kk/2
+        seeds.append(s.ravel())
+    
+    seeds = array(seeds).T
+    return seeds
+
+
---
+++ dipy/tracking/tests/fact_tracking.py
@@ -0,0 +1,30 @@
+from numpy import array, prod
+from nibabel import load
+from nibabel.trackvis import aff_to_hdr, empty_header, write
+from dipy.reconst.dti import Tensor
+from dipy.io.bvectxt import read_bvec_file
+from dipy.tracking.fact_tracking import seeds_from_mask, track_tensor
+
+img = load('E8885S6I1.nii.gz')
+hdr = img.get_header()
+vox_size = hdr.get_zooms()[:3]
+data = img.get_data()
+data_mask = data[..., 0] > 250
+print((mask.sum()+0.)/prod(mask.shape))
+bvec, bval = read_bvec_file('E8885S6I1.bvec')
+roi_img = load('E8885S6I1_tracking_roi.nii.gz')
+roi_mask = roi_img.get_data()
+seeds = seeds_from_mask(roi_mask, 1)
+
+#making the tensors can be slow for large images
+tensor = Tensor(data, bval, bvec.T, data_mask, min_signal=1)
+start_step = array([0, 0, -1])
+#the tracking step can be slow for a large number of seeds
+tracks = track_tensor(tensor, seeds, start_step, vox_size, .25, 45)
+tkvis_hdr = empty_header()
+tkvis_hdr['voxel_order']='LPI'
+tkvis_hdr['dim'] = tensor.shape
+tkvis_hdr['voxel_size'] = vox_size
+write('tracks.trk', tracks, tkvis_hdr)
+
+
