e092e35673ffd098423726ea1e029ffe2799733e
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2011-07-08 14:32:27 +0100

    updated test_dsi still problem with qtable

--- dipy/reconst/tests/test_dsi.py
+++ dipy/reconst/tests/test_dsi.py
@@ -10,6 +10,8 @@ from dipy.reconst.gqi import GeneralizedQSampling
 from dipy.reconst.dsi import DiffusionSpectrumImaging
 from dipy.reconst.sims import SticksAndBall
 from scipy.fftpack import fftn, fftshift
+from dipy.core.triangle_subdivide import create_unit_sphere, create_half_unit_sphere 
+from scipy.ndimage import map_coordinates
 
 def test_dsi():
        
@@ -23,44 +25,64 @@ if __name__ == '__main__':
     
     bvals=np.loadtxt('/home/eg309/Data/tp2/NIFTI/dsi_bvals.txt')
     bvecs=np.loadtxt('/home/eg309/Data/tp2/NIFTI/dsi_bvects.txt')
-    bvecs[-1]=np.array([0,0,0])
     
+    #b0 first and remove nans
+    bvecs[-1]=np.array([0,0,0])    
     swap=bvals[0];bvals[0]=bvals[-1];bvals[-1]=swap
     swap=bvecs[0];bvecs[0]=bvecs[-1];bvecs[-1]=swap
         
-    S,stics=SticksAndBall(bvals, bvecs, d=0.0015, S0=100, angles=[(0, 0),(90,0),(90,90)], fractions=[0,0,0], snr=None)
+    S,stics=SticksAndBall(bvals, bvecs, d=0.0015, S0=100, angles=[(0, 0),(90,0),(90,90)], fractions=[100,0,0], snr=None)
     
-    #duplicate
-    S=np.concatenate([S,S[1:]])
+    """
+    #show projected signal
+    Bvecs=np.concatenate([bvecs[1:],-bvecs[1:]])
+    X0=np.dot(np.diag(np.concatenate([S[1:],S[1:]])),Bvecs)    
+    ren=fvtk.ren()
+    fvtk.add(ren,fvtk.point(X0,fvtk.yellow,1,2,16,16))    
+    fvtk.show(ren)
+    """
     
-    bvals=np.concatenate([bvals,bvals[1:]])
-    bvecs=np.concatenate([bvecs,-bvecs[1:]])
+    #use symmetry to cover the entire space 
+    #SS=np.concatenate([S,S[1:]])
+    #bvals=np.concatenate([bvals,bvals[1:]])
+    #bvecs=np.concatenate([bvecs,-bvecs[1:]])
     
+    #approximate qtable from bvals and bvecs 
     bmin=np.sort(bvals)[1]
-    bv=np.sqrt(np.floor(bvals/bmin +0.5))
-    
+    bv=np.sqrt(np.floor(bvals/bmin + 0.5))    
     qtable=np.vstack((bv,bv,bv)).T * bvecs
-    qtable=np.floor(qtable+0.5)
-       
+    qtable=np.floor(qtable + 0.5)
+    
+    #calculate radius for the hanning filter
     r = np.sqrt(qtable[:,0]**2+qtable[:,1]**2+qtable[:,2]**2)
     
-    #r=fvtk.ren()
-    #fvtk.add(r,fvtk.point(qtable,fvtk.red,1,0.1,6,6))
-    #fvtk.show(r)
+    #"""
+    #plot q-table
+    ren=fvtk.ren()
+    fvtk.add(ren,fvtk.point(qtable,fvtk.red,1,0.1,6,6))
+    fvtk.show(ren)
+    #"""
+
+    #setting hanning filter width and hanning    
+    filter_width=16.        
+    hanning=.5*np.cos(2*np.pi*r/filter_width)
     
-    filter_width=16        
-    hanning=.5*np.cos(2*r*np.pi/filter_width)
-    
+    #center and index in q space volume
     q=qtable+9
     q=q.astype('i8')
-    n=515+514
     
-    value=S*hanning
-        
+    #number of signal sampling points
+    n=515#*2-1
+    
+    #apply the hanning filter
+    values=S#SS*hanning
+    
+    #create the signal volume
     Sq=np.zeros((16,16,16))
     for i in range(n):
-        Sq[q[i]]=value[i]
-        
+        Sq[q[i]]+=values[i]
+    
+    #apply fourier transform
     Pr=fftshift(np.abs(np.real(fftn(fftshift(Sq),(16,16,16)))))
 
     #"""
@@ -70,32 +92,27 @@ if __name__ == '__main__':
     fvtk.show(ren)
     #"""
     
-    #from enthought.mayavi import mlab    
-    #Pr=np.interp(Pr,[Pr.min(),Pr.max()],[0,1])    
-    #mlab.pipeline.volume(mlab.pipeline.scalar_field(Pr))
-    #mlab.show()
-    
-    from dipy.core.triangle_subdivide import create_unit_sphere, create_half_unit_sphere    
-    vertices, edges, faces  = create_unit_sphere(5)
-    
-    from scipy.ndimage import map_coordinates
-    #sp.ndimage.map_coordinates(a, inds, order=1, cval=0, output=bool)
-    
+    #from enthought.mayavi import mlab        
+    #mlab.pipeline.volume(mlab.pipeline.scalar_field(Sq))
+    #mlab.show()    
+       
+    vertices, edges, faces  = create_unit_sphere(5)    
     odf = np.zeros(len(vertices))
-    #r=np.arange(2.1,6,.2)
-    r=np.arange(.1,6,.1)
-    
+    radius=np.arange(2.1,6,.2) #suggested
+    #radius=np.arange(.1,6,.1)
     origin=9
     
     for m in range(len(vertices)):
         
         xi=origin+r*vertices[m,0]
         yi=origin+r*vertices[m,1]
-        zi=origin+r*vertices[m,2]        
+        zi=origin+r*vertices[m,2]
         
         PrI=map_coordinates(Pr,np.vstack((xi,yi,zi)),order=1)
-        for i in range(len(r)):
+        for i in range(len(radius)):
             odf[m]=odf[m]+PrI[i]*r[i]**2
+            
+    
         
 
     
