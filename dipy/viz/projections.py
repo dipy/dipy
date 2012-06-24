d3aa1550279bf0eb86c7b0c53afbd81a5f5668b9
arokem <arokem@gmail.com>
2012-06-23 17:35:54 -0700

    RF: Refactored sph_proj to remove dependency on basemap.

--- dipy/viz/projections.py
+++ dipy/viz/projections.py
@@ -10,24 +10,12 @@ import scipy.interpolate as interp
 
 import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import matplotlib.tri as tri
 
 import dipy.core.geometry as geo
 
-def sph2latlon(theta, phi):
-    """Convert spherical coordinates to latitude and longitude.
-
-    Returns
-    -------
-    lat, lon : ndarray
-        Latitude and longitude.
-
-    """
-    return np.rad2deg(theta - np.pi/2), np.rad2deg(phi - np.pi)
-
 def sph_project(vertices, val, ax=None, vmin=None, vmax=None,
-            cmap=matplotlib.cm.hot, cbar=True, tri=False,
-            boundary=True, **basemap_args):
+		cmap=matplotlib.cm.hot, cbar=True, triang=False):
 
     """Draw a signal on a 2D projection of the sphere.
 
@@ -37,29 +25,23 @@ def sph_project(vertices, val, ax=None, vmin=None, vmax=None,
     vertices : (N,3) ndarray
                 unit vector points of the sphere
 
-    r : (M, N) ndarray
+    val: (N) ndarray
         Function values.
-
-    theta : (M,) ndarray
-        Inclination / polar angles of function values.
-
-    phi : (N,) ndarray
-        Azimuth angles of function values.
 
     ax : mpl axis, optional
         If specified, draw onto this existing axis instead.
+
+    vmin, vmax: floats
+       Values to cut the z
 
     cmap: mpl colormap
 
     cbar: Whether to add the color-bar to the figure
 
-    basemap_args : dict
-        Parameters used to initialise the basemap, e.g. ``projection='ortho'``.
+    triang: Whether to display the plot triangulated as a pseudo-color plot.
 
     Returns
     -------
-    m : basemap
-        The newly created matplotlib basemap.
     fig : figure
         Matplotlib figure
 
@@ -70,32 +52,11 @@ def sph_project(vertices, val, ax=None, vmin=None, vmax=None,
     >>> sph_project(verts,np.random.rand(len(verts)))
 
     """
-    bvecs=bvecs.T
-
     if ax is None:
         fig, ax = plt.subplots(1)
 
-    basemap_args.setdefault('projection', 'ortho')
-    basemap_args.setdefault('lat_0', 0)
-    basemap_args.setdefault('lon_0', 0)
-    basemap_args.setdefault('resolution', 'c')
-
-    m = Basemap(**basemap_args)
-    if boundary:
-        m.drawmapboundary()
-
-    # Rotate the coordinate system so that you are looking from the north pole:
-    bvecs_rot = np.array(np.dot(np.matrix([[0,0,-1],[0,1,0],[1,0,0]]), bvecs))
-
-    # To get the orthographic projection, when the first coordinate is positive:
-    neg_idx = np.where(bvecs_rot[0]>0)
-
-    # rotate the entire bvector around to point in the other direction:
-    bvecs_rot[:, neg_idx] *= -1
-
-    _, theta, phi = geo.cart2sphere(bvecs_rot[0], bvecs_rot[1], bvecs_rot[2])
-    lat, lon = sph2latlon(theta, phi)
-    x, y = m(lon, lat)
+    x = vertices[:, 0]
+    y = vertices[:, 1]
 
     my_min = np.nanmin(val)
     if vmin is not None:
@@ -105,29 +66,34 @@ def sph_project(vertices, val, ax=None, vmin=None, vmax=None,
     if vmax is not None:
         my_max = vmax
 
-    if tri:
-        m.pcolor(x, y, val, vmin=my_min, vmax=my_max, tri=True, cmap=cmap)
+    r = (val - my_min)/float(my_max-my_min)
+
+    # Enforce the maximum and minumum boundaries, if there are values
+    # outside those boundaries:
+    r[r<0]=0
+    r[r>1]=1
+
+    if triang:
+	triang = tri.Triangulation(x, y)
+	plt.tripcolor(triang, r, cmap=cmap)
     else:
-        cmap_data = cmap._segmentdata
-        red_interp, blue_interp, green_interp = (
-        interp.interp1d(np.array(cmap_data[gun])[:,0],
-                        np.array(cmap_data[gun])[:,1]) for gun in
-                                                  ['red', 'blue','green'])
+	cmap_data = cmap._segmentdata
+	red_interp, blue_interp, green_interp = (
+		interp.interp1d(np.array(cmap_data[gun])[:,0],
+				np.array(cmap_data[gun])[:,1]) for gun in
+		                ['red', 'blue','green'])
 
-        r = (val - my_min)/float(my_max-my_min)
 
-        # Enforce the maximum and minumum boundaries, if there are values
-        # outside those boundaries:
-        r[r<0]=0
-        r[r>1]=1
+	for this_x, this_y, this_r in zip(x,y,r):
+	    red = red_interp(this_r)
+	    blue = blue_interp(this_r)
+	    green = green_interp(this_r)
+	    ax.plot(this_x, this_y, 'o',
+		    c=[red.item(), green.item(), blue.item()])
 
-        for this_x, this_y, this_r in zip(x,y,r):
-            red = red_interp(this_r)
-            blue = blue_interp(this_r)
-            green = green_interp(this_r)
-            m.plot(this_x, this_y, 'o',
-                   c=[red.item(), green.item(), blue.item()])
 
+    plt.axis('equal')
+    plt.axis('off')
     if cbar:
         mappable = matplotlib.cm.ScalarMappable(cmap=cmap)
         mappable.set_array([my_min, my_max])
@@ -138,4 +104,4 @@ def sph_project(vertices, val, ax=None, vmin=None, vmax=None,
         cax = fig.add_axes([l+w+0.075, b, 0.05, h], frameon=False)
         fig.colorbar(mappable, cax=cax) # draw colorbar
 
-    return m, fig
+    return fig
