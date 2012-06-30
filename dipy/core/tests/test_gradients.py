c5053c0bdc771daf38d2c7b6dd4861dcda3e42d6
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-06-30 21:41:33 +0100

    TEST for gradients

--- dipy/core/tests/test_gradients.py
+++ dipy/core/tests/test_gradients.py
@@ -1,27 +1,23 @@
+import numpy as np
 from nose.tools import assert_true
 from numpy.testing import assert_array_equal
-import numpy as np
 from dipy.data import get_data
-from dipy.core.gtable import GradientTable
+from dipy.core.gradients import DiffusionGradients
+
 
 def test_gtab():
 
     fimg,fbvals,fbvecs = get_data('small_64D')
-
     bvals=np.load(fbvals)
     bvecs=np.load(fbvecs)
-
-    gt = GradientTable((bvals,bvecs))
+    gt = DiffusionGradients((bvals,bvecs))
     assert_array_equal(gt.bvecs,bvecs)
-
-    gt2 = GradientTable((bvals,bvecs.T))
+    gt2 = DiffusionGradients((bvals,bvecs.T))
     assert_array_equal(gt2.bvecs, bvecs.T)
-
     gtab = np.concatenate((bvals[:,None], bvecs),axis=1)
-    gt3 = GradientTable(gtab)
+    gt3 = DiffusionGradients(gtab)
     assert_array_equal(gt3.bvecs, bvecs)
     assert_array_equal(gt3.bvals, bvals)
-
-    gt4 = GradientTable(gtab.T)
+    gt4 = DiffusionGradients(gtab.T)
     assert_array_equal(gt4.bvecs, bvecs)
     assert_array_equal(gt4.bvals, bvals)
