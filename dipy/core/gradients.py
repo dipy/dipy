97b4af1ecc62fa8fb3bc54131100e0661ebd0a6e
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-06-30 21:41:33 +0100

    TEST for gradients

--- dipy/core/gradients.py
+++ dipy/core/gradients.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-class GradientTable(object):
-    def __init__(self, gtab):
-        """ A general class for handling diffusion gradient tables
+
+class DiffusionGradients(object):
+    def __init__(self, gtab, b0_thr=20):
+        """ A general class for handling diffusion MRI gradients
 
         Parameters
         ----------
@@ -12,36 +13,48 @@ class GradientTable(object):
               or an (N, 4) or (4, N) array with b-values in the first 
             column and b-vectors in the last 3 columns.             
 
+        b0_thr: float 
+            all b-values with values lower than this threshold
+            are considered as b0s i.e. without diffusion weighting
+            (See notes).
+
         Attributes
         ----------
         bvals: array, shape (N,)
                 b-values
         bvecs: array, shape (N,3)
                 b-vectors
+        info: short information string
 
         Methods
         -------
-        expose_b0s: show which bvalues are smaller 
-                    than a low threshold
+        expose_b0s: show which b-values are smaller 
+                    than a low threshold (default 20)
         
-        reorient: align the bvecs to a different coordinate system
+        transform: align the b-vectors to a different coordinate system
 
         Examples
         --------
-        >>> from dipy.core.gtable import GradientTable
-        >>> from dipy.data import get_data
-        >>> fimg,fbvals,fbvecs = get_data('small_64D')
-        >>> bvals=np.load(fbvals)
-        >>> bvecs=np.load(fbvecs)
-        >>> gt = GradientTable((bvals, bvecs))
+        >>> from dipy.core.gradients import DiffusionGradients
+        >>> bvals=1500*np.ones(4)
+        >>> bvals[0]=0
+        >>> bvecs=np.array([[0,0,0],[1,0,0],[0,1,0],[0,0,1]])
+        >>> gt = DiffusionGradients((bvals, bvecs))
         >>> gt.bvecs.shape == bvecs.shape
         True
-        >>> gt = GradientTable((bvals, bvecs.T))
+        >>> gt = DiffusionGradients((bvals, bvecs.T))
         >>> gt.bvecs.shape == bvecs.T.shape
         False
 
-        """
+        Notes
+        -----
+        Often b0s (b-values which correspond to images without diffusion
+        weighting) have 0 values however in some cases the scanner cannot 
+        provide b0s of an exact 0 value and it gives a bit higher values 
+        e.g. 6 or 12. This is the purpose of the b0_thr in the __init__.
 
+        """
+        self.b0_thr=b0_thr
         if isinstance(gtab, (tuple, list)):
             self.bvals=gtab[0]
             self.bvecs=gtab[1]
@@ -63,22 +76,54 @@ class GradientTable(object):
                     self.bvals=self.gtab[:, 0]
                     self.bvecs=self.gtab[:, 1:]
 
-    def qvecs_from_bvecs(self, thr=20):
-        bv=self.bvals[self.bvals>thr]
+        #check if bvecs are unit vectors
+        sqrtb=np.sqrt(self.bvecs[:,0]**2+self.bvecs[:,1]**2+self.bvecs[:,2]**2)
+        sqrtb=sqrtb[self.expose_non_b0s()]
+        if not np.allclose(sqrtb,np.ones(len(sqrtb)),atol=1e-2):
+            raise ValueError('B-vectors should be unit vectors')
+
+
+    def norm_q_vectors(self, b0_thr=None):
+        ''' normalized q_vectors from bvals and bvecs
+        '''
+        if b0_thr==None:
+            b0_thr=self.b0_thr
+        bv=self.bvals[self.bvals>b0_thr]
         bmin=np.sort(bv)[1]
         bv=np.sqrt(bv/bmin)
-        qtable=np.vstack((bv, bv, bv)).T*self.bvecs
-        qtable=np.floor(qtable+.5)
-        self.qvecs=qtable
-        return self.qvecs
+        return np.vstack((bv, bv, bv)).T*self.bvecs
 
-    def expose_b0s(self, thr=20):
+    def expose_b0s(self, b0_thr=None):
         """ find where b0s are held
         """
-        return np.where(self.bvals<=thr)[0]
+        if b0_thr==None:
+            b0_thr=self.b0_thr
+        return np.where(self.bvals<=b0_thr)[0]
 
-    def reorient(self):
-        pass
+    def expose_non_b0s(self, b0_thr=None):
+        """ find where b0s are held
+        """
+        if b0_thr==None:
+            b0_thr=self.b0_thr
+        return np.where(self.bvals>b0_thr)[0]
 
+    def transform(self):
+        """ reorient gradients to a different coordinate system
+        """
+        raise NotImplementedError('Transformation of Gradients not implemented yet')
+
+    @property
     def info(self):
-        pass
+        print('B-values shape (%d, %d)',self.bvals.shapem)
+        print('         min %f ', self.bvals.min())
+        print('         max %f ', self.bvals.max())
+        print('B-vectors shape (%d, %d)',self.bvecs.shapem)
+        print('         min %f ', self.bvecs.min())
+        print('         max %f ', self.bvecs.max())
+
+
+
+
+
+        
+
