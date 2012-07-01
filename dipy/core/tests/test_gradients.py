2f516cab8d3e475dceecd6632d3cbf69e216df85
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-07-01 01:21:33 +0100

    Added more checks for DiffusionGradients some tests don't pass still.

--- dipy/core/tests/test_gradients.py
+++ dipy/core/tests/test_gradients.py
@@ -7,6 +7,19 @@ from dipy.core.gradients import DiffusionGradients
 
 def test_gtab():
 
+    sq2=np.sqrt(2)/2.
+    bvals=1500*np.ones(7)
+    bvals[0]=0
+    bvecs=np.array([[0, 0, 0],
+                    [1, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 1],
+                    [sq2, sq2, 0],
+                    [sq2, 0, sq2],
+                    [0, sq2, sq2]])
+    gt = DiffusionGradients((bvals,bvecs))
+    assert_array_equal(gt.bvecs,bvecs)
+    gt.info
     fimg,fbvals,fbvecs = get_data('small_64D')
     bvals=np.load(fbvals)
     bvecs=np.load(fbvecs)
