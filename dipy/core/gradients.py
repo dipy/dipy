b6b68789d527b98b4ab60faaa677c962dd99e881
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-07-02 15:04:20 +0100

    New file dipy.io.bvalues added

--- dipy/core/gradients.py
+++ dipy/core/gradients.py
@@ -1,8 +1,8 @@
 import numpy as np
-from os.path import splitext
+from dipy.io.bvalues import read_bvals_bvecs_files, read_btable_file
 
-class DiffusionGradients(object):
-    def __init__(self, bvals, bvecs=None, b0_thr=20, atol=1e-2):
+class BTable(object):
+    def __init__(self, bvals, bvecs=None, b0_threshold=20, atol=1e-2):
         """ A general class for handling information about diffusion MR gradients .
 
         This class is especially useful as an input for signal reconstruction methods. 
@@ -24,7 +24,7 @@ class DiffusionGradients(object):
             1. an array of shape (N, 3) or (3, N) with the b-vectors.
             2. a path for the file wich contains an array like the previous.
 
-        b0_thr: float 
+        b0_threshold: float 
             all b-values with values lower than this threshold
             are considered as b0s i.e. without diffusion weighting
             (See notes).
@@ -37,21 +37,20 @@ class DiffusionGradients(object):
                 b-values
         bvecs: array, shape (N,3)
                 b-vectors
-        info: short information string
 
-        Methods
-        -------
-        expose_b0s: show which b-values are smaller or equal
+        b0s_indices: show which b-values are smaller or equal
                     than a low diffusion signal threshold (default 20)
 
-        expose_nonb0s: show which b-values are smaller 
+        nonb0s_indices: show which b-values are smaller 
                     than a low diffusion signal threshold (default 20)
         
+        Methods
+        -------
         transform: align the b-vectors to a different coordinate system
 
         Examples
         --------
-        >>> from dipy.core.gradients import DiffusionGradients
+        >>> from dipy.core.gradients import BTable
         >>> bvals=1500*np.ones(7)
         >>> bvals[0]=0
         >>> sq2=np.sqrt(2)/2
@@ -62,10 +61,10 @@ class DiffusionGradients(object):
                             [sq2, sq2, 0],\
                             [sq2, 0, sq2],\
                             [0, sq2, sq2]])
-        >>> gt = DiffusionGradients(bvals, bvecs)
+        >>> gt = BTable(bvals, bvecs)
         >>> gt.bvecs.shape == bvecs.shape
         True
-        >>> gt = DiffusionGradients(bvals, bvecs.T)
+        >>> gt = BTable(bvals, bvecs.T)
         >>> gt.bvecs.shape == bvecs.T.shape
         False
 
@@ -74,7 +73,7 @@ class DiffusionGradients(object):
         1. Often b0s (b-values which correspond to images without diffusion
         weighting) have 0 values however in some cases the scanner cannot 
         provide b0s of an exact 0 value and it gives a bit higher values 
-        e.g. 6 or 12. This is the purpose of the b0_thr in the __init__.
+        e.g. 6 or 12. This is the purpose of the b0_threshold in the __init__.
 
         2. We assume that the minimum number of b-values is 7.
 
@@ -85,14 +84,14 @@ class DiffusionGradients(object):
         dipy.reconst.dti.Tensor
 
         """
-        self.b0_thr=b0_thr
+        self.b0_threshold=b0_threshold
         if isinstance(bvals,str) and isinstance(bvecs,str):
             self.bvals, self.bvecs=read_bvals_bvecs_files(bvals, bvecs)
-        if isinstance(bvals,str) and bvecs==None:
+        if isinstance(bvals,str) and bvecs is None:
             self.bvals, self.bvecs=read_btable_file(bvals)
         if hasattr(bvals,'shape') and hasattr(bvals,'shape'):
             self.bvals, self.bvecs=bvals, bvecs 
-        if hasattr(bvals,'shape') and bvecs==None:
+        if hasattr(bvals,'shape') and bvecs is None:
             if bvals.shape[-1]==4:
                 self.bvals=np.squeeze(bvals[:, 0])
                 self.bvecs=bvals[:, 1:]
@@ -105,34 +104,32 @@ class DiffusionGradients(object):
             self.bvecs=self.bvecs.T      
         #check if bvecs are unit vectors
         sqrtb=np.sqrt(self.bvecs[:,0]**2+self.bvecs[:,1]**2+self.bvecs[:,2]**2)
-        sqrtb=sqrtb[self.expose_nonb0s()]
+        sqrtb=sqrtb[self.nonb0s_indices]
         if not np.allclose(sqrtb,np.ones(len(sqrtb)),atol=atol):
             raise ValueError('B-vectors should be unit vectors')
 
 
-    def norm_q_vectors(self, b0_thr=None):
+    def norm_q_vectors(self, b0_threshold=None):
         ''' normalized q_vectors from bvals and bvecs
         '''
-        if b0_thr==None:
-            b0_thr=self.b0_thr
-        bv=self.bvals[self.bvals>b0_thr]
+        if b0_threshold is None:
+            b0_threshold=self.b0_threshold
+        bv=self.bvals[self.bvals>b0_threshold]
         bmin=np.sort(bv)[1]
         bv=np.sqrt(bv/bmin)
         return np.vstack((bv, bv, bv)).T*self.bvecs
 
-    def expose_b0s(self, b0_thr=None):
+    @property
+    def b0s_indices(self):
         """ find where b0s are held
         """
-        if b0_thr==None:
-            b0_thr=self.b0_thr
-        return np.where(self.bvals<=b0_thr)[0]
+        return np.where(self.bvals<=self.b0_threshold)[0]
 
-    def expose_nonb0s(self, b0_thr=None):
+    @property
+    def nonb0s_indices(self):
         """ find where b0s are held
         """
-        if b0_thr==None:
-            b0_thr=self.b0_thr
-        return np.where(self.bvals>b0_thr)[0]
+        return np.where(self.bvals>self.b0_threshold)[0]
 
     def transform(self):
         """ reorient gradients to a different coordinate system
@@ -148,80 +145,6 @@ class DiffusionGradients(object):
         print('         min %f ' % self.bvecs.min())
         print('         max %f ' % self.bvecs.max())
 
-
-def read_bvals_bvecs_files(fbvals,fbvecs):
-    """ Read b-values and b-vectors from the disk
-
-    Parameters
-    ----------
-    fbvals: str
-            path of file with b-values
-    fbvecs: str
-            path of file with b-vectors
-
-    Returns
-    -------
-    bvals: array, (N,)
-    bvecs: array, (N, 3)
-
-    """
-    if isinstance(fbvals,str) and isinstance(fbvecs,str):         
-        base, ext = splitext(fbvals)
-        if ext in ['.bvals', '.bval', '.txt', '']:
-            bvals=np.squeeze(np.loadtxt(fbvals))
-            bvecs=np.loadtxt(fbvecs)
-        if ext=='.npy':
-            bvals=np.squeeze(np.load(fbvals))
-            bvecs=np.load(fbvecs)
-        if bvecs.shape[1]>bvecs.shape[0]:
-            bvecs=bvecs.T
-        if min(bvecs.shape) != 3:
-            raise IOError('bvec file should have three rows')
-        if bvecs.ndim != 2:
-            raise IOError('bvec file should be saved as a two dimensional array')
-        if max(bvals.shape)!=max(bvecs.shape):
-            raise IOError('b-values and b-vectors shapes do not correspond')
-    else:
-        raise ValueError('Two strings with the full filepaths are required')
- 
-    return bvals, bvecs
-
-def read_btable_file(fbtab):
-    """ Read b-table from the disk
-
-    B-table is a 2D array which contains b-values in the 
-    first column and b-vectors in the last column.
-
-    Parameters
-    ----------
-    fbtab: str,
-        path of file with b-table
-
-    Returns
-    -------
-    bvals: array, (N,)
-    bvecs: array, (N, 3)
-
-    """
-    if isinstance(fbtab,str):         
-        base, ext = splitext(fbvals)
-        if ext in ['.txt', '']:
-            btable=np.loadtxt(fbtab)
-        if ext=='.npy':
-            btable=np.load(fbvecs)
-        if bvecs.shape[1]>bvecs.shape[0]:
-            btable=btable.T
-        if min(bvecs.shape) != 4:
-            raise IOError('btable file should have 4 rows')
-        bvals=np.squeeze(btable[:,0])
-        bvecs=btable[:,1:]
-        if bvals.ndim!=1 and bvecs.ndim != 2:
-            raise IOError('b-table was not loaded correctly')
-        if max(bvals.shape)!=max(bvecs.shape):
-            raise IOError('b-values and b-vectors shapes do not correspond')
-    else:
-        raise ValueError('A string with the full filepath is required')
-     
 
 
            
--- dipy/core/tests/test_gradients.py
+++ dipy/core/tests/test_gradients.py
@@ -2,10 +2,10 @@ import numpy as np
 from nose.tools import assert_true
 from numpy.testing import assert_array_equal
 from dipy.data import get_data
-from dipy.core.gradients import DiffusionGradients
+from dipy.core.gradients import BTable
 
 
-def test_gtab():
+def test_btable_prepare():
 
     sq2=np.sqrt(2)/2.
     bvals=1500*np.ones(7)
@@ -17,30 +17,47 @@ def test_gtab():
                     [sq2, sq2, 0],
                     [sq2, 0, sq2],
                     [0, sq2, sq2]])
-    gt = DiffusionGradients(bvals,bvecs)
-    assert_array_equal(gt.bvecs,bvecs)
-    gt.info
+    bt = BTable(bvals,bvecs)
+    assert_array_equal(bt.bvecs,bvecs)
+    bt.info
     fimg,fbvals,fbvecs = get_data('small_64D')
     bvals=np.load(fbvals)
     bvecs=np.load(fbvecs)
-    gt = DiffusionGradients(bvals,bvecs)
-    assert_array_equal(gt.bvecs,bvecs)
-    gt2 = DiffusionGradients(bvals,bvecs.T)
-    assert_array_equal(gt2.bvecs, bvecs)
-    gtab = np.concatenate((bvals[:,None], bvecs),axis=1)
-    gt3 = DiffusionGradients(gtab)
-    assert_array_equal(gt3.bvecs, bvecs)
-    assert_array_equal(gt3.bvals, bvals)
-    gt4 = DiffusionGradients(gtab.T)
-    assert_array_equal(gt4.bvecs, bvecs)
-    assert_array_equal(gt4.bvals, bvals)
-    gt5 = DiffusionGradients(fbvals,fbvecs)
-    assert_array_equal(gt4.bvecs, bvecs)
-    assert_array_equal(gt4.bvals, bvals)
+    bt = BTable(bvals,bvecs)
+    assert_array_equal(bt.bvecs,bvecs)
+    bt2 = BTable(bvals,bvecs.T)
+    assert_array_equal(bt2.bvecs, bvecs)
+    btab = np.concatenate((bvals[:,None], bvecs),axis=1)
+    bt3 = BTable(btab)
+    assert_array_equal(bt3.bvecs, bvecs)
+    assert_array_equal(bt3.bvals, bvals)
+    bt4 = BTable(btab.T)
+    assert_array_equal(bt4.bvecs, bvecs)
+    assert_array_equal(bt4.bvals, bvals)
+    bt5 = BTable(fbvals,fbvecs)
+    assert_array_equal(bt4.bvecs, bvecs)
+    assert_array_equal(bt4.bvals, bvals)
     fimg,fbvals,fbvecs = get_data('small_101D')
-    gt5 = DiffusionGradients(fbvals,fbvecs)
-    assert_array_equal(gt4.bvecs, bvecs)
-    assert_array_equal(gt4.bvals, bvals)
+    bt5 = BTable(fbvals,fbvecs)
+    assert_array_equal(bt4.bvecs, bvecs)
+    assert_array_equal(bt4.bvals, bvals)
 
+def test_b0s():
+
+    sq2=np.sqrt(2)/2.
+    bvals=1500*np.ones(8)
+    bvals[0]=0
+    bvals[-1]=0
+    bvecs=np.array([[0, 0, 0],\
+                    [1, 0, 0],\
+                    [0, 1, 0],\
+                    [0, 0, 1],\
+                    [sq2, sq2, 0],\
+                    [sq2, 0, sq2],\
+                    [0, sq2, sq2],\
+                    [np.nan, np.nan, np.nan]])
+    bt = BTable(bvals,bvecs)
+    print bt.b0s_indices
     
+
  
