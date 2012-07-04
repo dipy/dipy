4e994b6b87eaaff105b33e347963671d8da35948
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-07-05 00:34:26 +0100

    added option so that the tests file can be run separately

--- dipy/core/tests/test_gradients.py
+++ dipy/core/tests/test_gradients.py
@@ -54,3 +54,7 @@ def test_b0s():
     assert_array_equal(np.where(bt.b0s_mask==0)[0], np.arange(1,7))
 
 
+if __name__ == "__main__":
+    from numpy.testing import run_module_suite
+    run_module_suite()
+
