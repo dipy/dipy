572333849bbecf38cc8c12069b60ab22d59b4b32
arokem <arokem@gmail.com>
2012-07-06 20:04:05 -0700

    STY + BF: PEP8 and also removed a syntanx error that was towards the end there.

--- dipy/reconst/dsi.py
+++ dipy/reconst/dsi.py
@@ -25,8 +25,10 @@ class DiffusionSpectrumFit(OdfFit):
 class DiffusionSpectrumModel(OdfModel):
     ''' Calculate the PDF and ODF using Diffusion Spectrum Imaging
     
-    Based on the paper "Mapping Complex Tissue Architecture With Diffusion Spectrum Magnetic Resonance Imaging"
-    by Van J. Wedeen,Patric Hagmann,Wen-Yih Isaac Tseng,Timothy G. Reese, and Robert M. Weisskoff, MRM 2005
+    Based on the paper "Mapping Complex Tissue Architecture With Diffusion
+    Spectrum Magnetic Resonance Imaging"
+    by Van J. Wedeen,Patric Hagmann,Wen-Yih Isaac Tseng,Timothy G. Reese, and
+    Robert M. Weisskoff, MRM 2005
         
     '''
     def __init__(self, bvals, gradients, odf_sphere='symmetric642',
@@ -46,20 +48,20 @@ class DiffusionSpectrumModel(OdfModel):
         dipy.reconst.dti.Tensor, dipy.reconst.gqi.GeneralizedQSampling
         '''
         b0 = 0
-        self.bvals=bvals
-        self.gradients=gradients
+        self.bvals = bvals
+        self.gradients = gradients
         #3d volume for Sq
-        self.sz=16
+        self.sz = 16
         #necessary shifting for centering
-        self.origin=8
+        self.origin = 8
         #hanning filter width
-        self.filter_width=32.                     
+        self.filter_width = 32.
         #odf collecting radius
-        self.radius=np.arange(2.1,6,.2)
+        self.radius = np.arange(2.1,6,.2)
         #odf sphere
         odf_vertices, odf_faces = sphere_vf_from(odf_sphere)
         self.set_odf_vertices(odf_vertices,None,odf_faces)
-        self.odfn=len(self._odf_vertices)
+        self.odfn = len(self._odf_vertices)
         #number of single sampling points
         self.dn = (bvals > b0).sum()
         self.num_b0 = len(bvals) - self.dn
@@ -69,45 +71,46 @@ class DiffusionSpectrumModel(OdfModel):
     def create_qspace(self):
         
         #create the q-table from bvecs and bvals        
-        bv=self.bvals
-        bmin=np.sort(bv)[1]
-        bv=np.sqrt(bv/bmin)
-        qtable=np.vstack((bv,bv,bv)).T*self.gradients
-        qtable=np.floor(qtable+.5)
-        self.qtable=qtable
+        bv = self.bvals
+        bmin = np.sort(bv)[1]
+        bv = np.sqrt(bv/bmin)
+        qtable = np.vstack((bv,bv,bv)).T*self.gradients
+        qtable = np.floor(qtable+.5)
+        self.qtable = qtable
         
-        self.radiusn=len(self.radius)
+        self.radiusn = len(self.radius)
         #calculate r - hanning filter free parameter
         r = np.sqrt(qtable[:,0]**2+qtable[:,1]**2+qtable[:,2]**2)    
         #setting hanning filter width and hanning        
-        self.filter=.5*np.cos(2*np.pi*r/self.filter_width)                
+        self.filter = .5*np.cos(2*np.pi*r/self.filter_width)
         #center and index in qspace volume
-        self.q=qtable+self.origin
-        self.q=self.q.astype('i8')
+        self.q = qtable + self.origin
+        self.q = self.q.astype('i8')
         #peak threshold
-        self.peak_thr=.7
-        self.iso_thr=.4  
+        self.peak_thr = .7
+        self.iso_thr = .4
 
         #precompute coordinates for pdf interpolation
         self.precompute_interp_coords()    
    
 
     def pdf(self,s):
-        values=s*self.filter
+        values = s * self.filter
         #create the signal volume    
-        Sq=np.zeros((self.sz,self.sz,self.sz))
+        Sq = np.zeros((self.sz,self.sz,self.sz))
         #fill q-space
         for i in range(self.dn):
-            qx,qy,qz=self.q[i]            
-            Sq[qx,qy,qz]+=values[i]
+            qx, qy, qz = self.q[i]
+            Sq[qx, qy, qz] += values[i]
         #apply fourier transform
-        Pr=fftshift(np.abs(np.real(fftn(fftshift(Sq),(self.sz,self.sz,self.sz)))))
+        Pr=fftshift(np.abs(np.real(fftn(fftshift(Sq),
+                                        (self.sz, self.sz, self.sz)))))
         return Pr
     
     def odf(self,s):
-        Pr=self.pdf(s)       
+        Pr = self.pdf(s)
         #calculate the orientation distribution function        
-        odf=self.pdf_odf(Pr)
+        odf = self.pdf_odf(Pr)
         return odf        
         
     def pdf_odf(self,Pr):
@@ -127,24 +130,24 @@ class DiffusionSpectrumModel(OdfModel):
             for i in range(self.radiusn):
                 odf[m]=odf[m]+PrI[i]*self.radius[i]**2
         """
-        PrIs=map_coordinates(Pr,self.Xs,order=1)        
+        PrIs = map_coordinates(Pr,self.Xs,order=1)
         """ in pdf_to_odf an optimized version of the function below
         for m in range(self.odfn):
             for i in range(self.radiusn):
                 odf[m]=odf[m]+PrIs[m*self.radiusn+i]*self.radius[i]**2
         """
-        pdf_to_odf(odf,PrIs, self.radius,self.odfn,self.radiusn) 
+        pdf_to_odf(odf, PrIs, self.radius, self.odfn, self.radiusn)
         return odf
     
     
     def precompute_interp_coords(self):
-        Xs=[]
+        Xs = []
         for m in range(self.odfn):
-            xi=self.origin+self.radius*self.odf_vertices[m,0]
-            yi=self.origin+self.radius*self.odf_vertices[m,1]
-            zi=self.origin+self.radius*self.odf_vertices[m,2]
-            Xs.append(np.vstack((xi,yi,zi)).T)
-        self.Xs=np.concatenate(Xs).T
+            xi = self.origin + self.radius * self.odf_vertices[m, 0]
+            yi = self.origin + self.radius * self.odf_vertices[m, 1]
+            zi = self.origin + self.radius * self.odf_vertices[m, 2]
+            Xs.append(np.vstack((xi, yi, zi)).T)
+        self.Xs = np.concatenate(Xs).T
 
     def fit(self, data, mask=None, return_odf=False, gfa_thr=0.02, 
                 normalize_peaks=False):
@@ -201,11 +204,6 @@ class DiffusionSpectrumModel(OdfModel):
             qa_array = qa_array.reshape(shape + (npeaks,)) / global_max
             peak_values = peak_values.reshape(shape + (npeaks,))
             peak_indices = peak_indices.reshape(shape + (npeaks,))
-
-
-            #Call peak metrics from odf.py
-
-            odffit =
 
             #Unpack values to dsfit
             dsfit = DiffusionSpectrumFit()
