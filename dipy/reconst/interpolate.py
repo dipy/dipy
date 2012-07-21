5c012c95145f84ca3bad2534bd3f51520281401a
Bago Amirbekian <Bago.Amirbekian@ucsf.edu>
2012-07-20 18:04:53 -0700

    RF - Reworked integration to be more clear. Using explicit terminate callable      in generate_streamlines now.

--- dipy/reconst/interpolate.py
+++ dipy/reconst/interpolate.py
@@ -7,48 +7,35 @@ dipy.tracking.utils
 dipy.tracking.integration
 dipy.reconst.interpolate
 """
-from numpy import asarray
+from numpy import array
 from dipy.reconst.recspeed import trilinear_interp
+
+class OutsideImage(Exception):
+    pass
 
 class Interpolator(object):
     """Class to be subclassed by different interpolator types"""
-    def __init__(self, data, voxel_size, mask=None):
-        self._data = data
-        self._voxel_size = asarray(voxel_size, 'float')
-        if mask is not None:
-            if mask.ndim != len(self._voxel_size):
-                raise ValueError()
-            self._mask = asarray(mask, 'bool')
-        else:
-            self._mask = None
+    def __init__(self, data, voxel_size):
+        self.data = data
+        self.voxel_size = array(voxel_size, 'float')
 
 class NearestNeighborInterpolator(Interpolator):
     """Interpolates data using nearest neighbor interpolation"""
 
     def __getitem__(self, index):
-        index = tuple(index // self._voxel_size)
+        index = tuple(index // self.voxel_size)
         if min(index) < 0:
-            raise IndexError('Negative Index')
-        if self._mask is not None:
-            if not self._mask[index]:
-                raise StopIteration('outside mask')
-        return self._data[index]
+            raise OutsideImage('Negative Index')
+        try:
+            return self.data[index]
+        except IndexError:
+            raise OutsideImage
 
 class TriLinearInterpolator(Interpolator):
-    def __init__(self, data, voxel_size, mask=None, thresh=0):
-        self._data = data
-        self._voxel_size = asarray(voxel_size, 'float')
-        self.thresh = thresh
-        if mask is not None:
-            mask = asarray(mask, 'float')
-            mask = mask.reshape(mask.shape + (1,))
-            self._mask = mask
-        else:
-            self._mask = None
-
+    """Interpolates data using trilinear interpolation"""
     def __getitem__(self, index):
-        if mask is not None:
-            mask_value = trilinear_interp(self._mask, index, self._voxel_size)
-            if mask_value < 1:
-                raise StopIteration('outside mask')
-        return trilinear_interp(self._mask, index, self.voxel_size)
+        index = array(index, copy=False)
+        try:
+            return trilinear_interp(self.data, index, self.voxel_size)
+        except IndexError:
+            raise OutsideImage
