dc2ce3525b3235503c91b3d93f6c3cc030010384
Stefan van der Walt <stefan@sun.ac.za>
2012-10-11 13:11:17 -0700

    Add fast ndindex.

---
+++ dipy/core/ndindex.py
@@ -0,0 +1,47 @@
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+
+class ndindex(object):
+    """
+    An N-dimensional iterator object to index arrays.
+
+    Given the shape of an array, an `ndindex` instance iterates over
+    the N-dimensional index of the array. At each iteration a tuple
+    of indices is returned, the last dimension is iterated over first.
+
+    Parameters
+    ----------
+    `*args` : ints
+      The size of each dimension of the array.
+
+    Examples
+    --------
+    >>> for index in np.ndindex(3, 2, 1):
+    ...     print index
+    (0, 0, 0)
+    (0, 1, 0)
+    (1, 0, 0)
+    (1, 1, 0)
+    (2, 0, 0)
+    (2, 1, 0)
+
+    """
+    def __init__(self, *shape):
+        x = as_strided(np.zeros(1), shape=shape, strides=np.zeros_like(shape))
+        self._it = np.nditer(x, flags=['multi_index'], order='C')
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """
+        Standard iterator method, updates the index and returns the index tuple.
+
+        Returns
+        -------
+        val : tuple of ints
+            Returns a tuple containing the indices of the current iteration.
+
+        """
+        self._it.next()
+        return self._it.multi_index
