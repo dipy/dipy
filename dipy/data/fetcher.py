043bc708aabc439da738ca89c19cda8a019a7318
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-12-05 13:38:19 -0500

    BF changed to os.path.expanduser

--- dipy/data/fetcher.py
+++ dipy/data/fetcher.py
@@ -9,13 +9,13 @@ from dipy.io.gradients import read_bvals_bvecs
 def fetch_beijing_dti():
     """ Download a DTI dataset with 64 gradient directions
     """
-    DIPY_HOME = pjoin(os.getenv('HOME'), '.dipy')
+    dipy_home = pjoin(os.path.expanduser('~'), '.dipy')
     url = 'https://www.dropbox.com/sh/ybl8vquht8pdc3s/'
     uraw = url + 'NfhK5WsYRO/DTI64.nii.gz?dl=1'
     ubval = url + 'HyX6GEQSSV/DTI64.bval?dl=1'
     ubvec = url + 'Xtrvr5jSOs/DTI64.bvec?dl=1'
     ureadme = url + 'HoGsocT6hZ/beijingEnhanced.txt?dl=1'
-    folder = pjoin(DIPY_HOME, 'beijing_dti')
+    folder = pjoin(dipy_home, 'beijing_dti')
 
     if not os.path.exists(folder):
         print('Creating new directory %s' % folder)
@@ -46,9 +46,16 @@ def fetch_beijing_dti():
 
 def read_beijing_dti():
     """ Load Beijing dataset
-    """ 
-    DIPY_HOME = pjoin(os.getenv('HOME'), '.dipy')
-    folder = pjoin(DIPY_HOME, 'beijing_dti')
+
+    Returns
+    -------
+    img : obj,
+        Nifti1Image
+    gtab : obj,
+        GradientTable
+    """
+    dipy_home = pjoin(os.path.expanduser('~'), '.dipy')
+    folder = pjoin(dipy_home, 'beijing_dti')
     fraw = pjoin(folder, 'DTI64.nii.gz')
     fbval = pjoin(folder, 'DTI64.bval')
     fbvec = pjoin(folder, 'DTI64.bvec')
@@ -61,12 +68,12 @@ def read_beijing_dti():
 def fetch_taiwan_ntu_dsi():
     """ Download a DSI dataset with 203 gradient directions
     """
-    DIPY_HOME = pjoin(os.getenv('HOME'), '.dipy')
+    dipy_home = pjoin(os.path.expanduser('~'), '.dipy')
     uraw = 'http://dl.dropbox.com/u/2481924/taiwan_ntu_dsi.nii.gz'
     ubval = 'http://dl.dropbox.com/u/2481924/tawian_ntu_dsi.bval'
     ubvec = 'http://dl.dropbox.com/u/2481924/taiwan_ntu_dsi.bvec'
     ureadme = 'http://dl.dropbox.com/u/2481924/license_taiwan_ntu_dsi.txt'
-    folder = pjoin(DIPY_HOME, 'taiwan_ntu_dsi')
+    folder = pjoin(dipy_home, 'taiwan_ntu_dsi')
     if not os.path.exists(folder):
         print('Creating new directory %s' % folder)
         os.makedirs(folder)
@@ -95,9 +102,16 @@ def fetch_taiwan_ntu_dsi():
 
 def read_taiwan_ntu_dsi():
     """ Load Tawian NTU dataset
+
+    Returns
+    -------
+    img : obj,
+        Nifti1Image
+    gtab : obj,
+        GradientTable
     """
-    DIPY_HOME = pjoin(os.getenv('HOME'), '.dipy')
-    folder = pjoin(DIPY_HOME, 'taiwan_ntu_dsi')
+    dipy_home = pjoin(os.path.expanduser('~'), '.dipy')
+    folder = pjoin(dipy_home, 'taiwan_ntu_dsi')
     fraw = pjoin(folder, 'DSI203.nii.gz')
     fbval = pjoin(folder, 'DSI203.bval')
     fbvec = pjoin(folder, 'DSI203.bvec')
