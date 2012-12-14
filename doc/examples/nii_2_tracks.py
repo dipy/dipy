0422d0e820cac093e7d776ff53af01c0d1343944
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-12-13 20:34:20 -0500

    DOC added option to write the files as trk

--- doc/examples/nii_2_tracks.py
+++ doc/examples/nii_2_tracks.py
@@ -4,15 +4,11 @@
 Lets make some streamlines
 ==========================
 
-Overview
---------
+This example shows how to generate streamlines with Dipy_. First we will reconstruct
+the datasets using a single Tensor model and next will use a Constant Solid
+Angle ODF (Q-Ball) model. EuDX is the algorithm that we will use for fiber tracking.
 
-We will show how to generate streamlines with Dipy. First we will reconstruct
-the datasets using a single Tensor model and next we will use a Constant Solid
-Angle (QBall) model. EuDX will be used for the fiber tracking bits.
-
-First import the necessary modules
-----------------------------------
+First import the necessary modules:
 
 ``numpy`` is for numerical computation
 
@@ -33,7 +29,7 @@ import nibabel as nib
 from dipy.reconst.dti import TensorModel
 
 """
-``dipy.tracking`` is for tractography algorithms which create sets of tracks by integrating directionality models across voxels.
+``dipy.tracking`` is for fiber tracking.
 """
 
 from dipy.tracking.eudx import EuDX
@@ -42,7 +38,8 @@ from dipy.tracking.eudx import EuDX
 ``dipy.data`` is used for small datasets that we use in tests and examples.
 """
 
-from dipy.data import fetch_beijing_dti, read_beijing_dti
+from dipy.data import fetch_beijing_dti
+
 
 """
 Fetch will download the raw dMRI dataset of a single subject. The size of the dataset is 51 MBytes.
@@ -55,10 +52,17 @@ fetch_beijing_dti()
 Next, we read the saved dataset
 """
 
+from dipy.data import read_beijing_dti
+
 img, gtab = read_beijing_dti()
 
+"""
+img contains a nibabel Nifti1Image object (data) and gtab contains a GradientTable
+object (gradient information e.g. b-values).
+"""
+
 data = img.get_data()
-print('data.shape (%d,%d,%d,%d)' % data.shape)
+print('data.shape (%d, %d, %d, %d)' % data.shape)
 
 """
 This dataset has anisotropic voxel sizes, therefore reslicing is necessary
@@ -71,19 +75,17 @@ Load and show the zooms which hold the voxel size.
 """
 
 zooms = img.get_header().get_zooms()[:3]
-zooms
 
 """
-``(1.79, 1.79, 2.5)``
+The voxel size here is ``(1.79, 1.79, 2.5)``.
 
-Set the required new voxel size.
+We now set the required new voxel size.
 """
 
 new_zooms = (2., 2., 2.)
-new_zooms
 
 """
-``(2.0, 2.0, 2.0)``
+Which is ``(2.0, 2.0, 2.0)``
 
 Start reslicing. Trilinear interpolation is used by default.
 """
@@ -91,46 +93,69 @@ Start reslicing. Trilinear interpolation is used by default.
 from dipy.align.aniso2iso import resample
 
 data2, affine2 = resample(data, affine, zooms, new_zooms)
-data2.shape
+
+print('data2.shape (%d, %d, %d, %d)' % data2.shape)
 
 """
-Initiate your Model
+Lets also create a simple mask.
 """
-
-tenmodel = TensorModel(gtab)
 
 mask = data2[..., 0] > 50
 
 """
-Fit your data
+Now that we have prepared the datasets we can go forward with the voxel
+reconstruction. First, we instantiate the Tensor model in the following way.
+"""
+
+tenmodel = TensorModel(gtab)
+
+"""
+Fitting the data is very simple. We just need to calling the fit method of the
+TensorModel in the followin way:
 """
 
 tenfit = tenmodel.fit(data2, mask)
 
+"""
+The fit method creates a TensorFit object which contains the fitting parameters
+and other attributes of the model. For example we can generate fractional
+anisotropy.
+"""
+
 FA = tenfit.fa
 
 """
-Remove nans
+In the background of the image the fitting will not be accurate there is no
+signal and possibly we will find FA values with nans (not a number). We can
+easily remove these in the following way.
 """
 
 FA[np.isnan(FA)] = 0
 
 """
-EuDX takes as input discretized directions on a unit sphere. Therefore,
+EuDX takes as input discretized voxel directions on a unit sphere. Therefore,
 it is necessary to discretize the eigen vectors before feeding them in EuDX.
-"""
 
-from dipy.reconst.dti import quantize_evecs
+For the discretization procedure we use an evenly distributed sphere of 724
+points which we can access using the get_sphere function.
+"""
 
 from dipy.data import get_sphere
 
 sphere = get_sphere('symmetric724')
 
+"""
+We use quantize_evecs (evecs here stands for eigen vectors) to apply the
+discretization.
+"""
+
+from dipy.reconst.dti import quantize_evecs
+
 peak_indices = quantize_evecs(tenfit.evecs, sphere.vertices)
 
 """
 EuDX is the fiber tracking algorithm that we use in this example.
-The most important parameters is the first one which represents the
+The most important parameters are the first one which represents the
 magnitude of the peak of a scalar anisotropic function, the 
 second which represents the indices of the discretized directions of 
 the peaks and odf_vertices are the vertices of the input sphere.
@@ -171,14 +196,12 @@ print('Saving illustration as tensor_tracks.png')
 fvtk.record(r, n_frames=1, out_path='tensor_tracking.png', size=(600, 600))
 
 """
-Lets now repeat the same procedure with a CSA model
+Lets now repeat the same procedure with a CSA ODF model
 """
 
 from dipy.reconst.shm import CsaOdfModel, normalize_data
 
-data2 = data2.astype('f8')
-
-#normalize_data(data2, gtab.bvals, 1., out=data2)
+#data2 = data2.astype('f8')
 
 """
 We instantiate our CSA model with sperical harmonic order of 4
@@ -197,19 +220,20 @@ from dipy.reconst.odf import peaks_from_model
 peaks = peaks_from_model(model=csamodel, 
                          data=data2, 
                          sphere=sphere, 
-                         relative_peak_threshold=.8,
+                         relative_peak_threshold=.8, 
                          min_separation_angle=45, 
                          mask=mask, 
                          normalize_peaks=True)
 
 """
 This time we will not use FA as input to EuDX but we will use directly
-the maximum peaks of the ODF. 
+the maximum peaks of the ODF. The a_low threshold is the 
 """
 
 eu = EuDX(peaks.peak_values, 
           peaks.peak_indices, 
-          odf_vertices = sphere.vertices, a_low=0.2)
+          odf_vertices = sphere.vertices, 
+          a_low=0.2)
 
 csa_streamlines = [streamline for streamline in eu]
 
@@ -229,4 +253,46 @@ eigen_vectors and for the CSA case we used peaks_from_model as the CSA is a
 model that creates ODFs. However, the Tensor creates ODFs too which are always
 ellipsoids. So, we could have used the Tensor with peaks_from_model.
 """
+
+"""
+We can now save the results in the disk. For this purpose we can use the
+TrackVis format (*.trk). First, we need to create a header.
+"""
+
+hdr = nib.trackvis.empty_header()
+hdr['voxel_size'] = new_zooms
+hdr['voxel_order'] = 'LAS'
+hdr['dim'] = data2.shape[:3]
+
+"""
+Then we need to fill the streamlines.
+"""
+
+tensor_streamlines = ((sl, None, None) for sl in tensor_streamlines)
+
+ten_sl_fname = 'tensor_streamline.trk'
+
+"""
+Save the streamlines.
+"""
+
+nib.trackvis.write(ten_sl_fname, tensor_streamlines, hdr)
+
+"""
+Lets repeat the same steps for the csa_streamlines.
+"""
+
+csa_streamlines = ((sl, None, None) for sl in csa_streamlines)
+
+csa_sl_fname = 'csa_streamline.trk'
+
+nib.trackvis.write(csa_sl_fname, csa_streamlines, hdr)
+
+
+"""
+.. include:: ../links_names.inc
+
+"""
+
+
 
