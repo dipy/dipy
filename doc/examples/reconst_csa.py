091b197ace6e96c519d4771a563e0ee405b117ce
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-01-22 22:18:43 -0500

    A bit more information on the output of peaks_from_model

--- doc/examples/reconst_csa.py
+++ doc/examples/reconst_csa.py
@@ -1,11 +1,10 @@
 """
-
 =================================================
 Reconstruction using Constant Solid Angle (QBall)
 =================================================
 
 We show how to apply a Constant Solid Angle ODF (Q-Ball) model from Aganj et.
-al to your datasets.
+al (MRM 2010) to your datasets.
 
 First import the necessary modules:
 """
@@ -25,22 +24,25 @@ img, gtab = read_beijing_dti()
 
 """
 img contains a nibabel Nifti1Image object (data) and gtab contains a GradientTable
-object (gradient information e.g. b-values).
+object (gradient information e.g. b-values). For example to read the b-values
+it is possible to write print(gtab.bvals).
+
+Load the raw diffusion data and the affine.
 """
 
 data = img.get_data()
 print('data.shape (%d, %d, %d, %d)' % data.shape)
 
 """
-data.shape (128, 128, 49, 65)
+data.shape ``(128, 128, 49, 65)``
 
-This dataset has anisotropic voxel sizes, therefore reslicing is necessary
+This dataset has anisotropic voxel sizes, therefore reslicing is necessary.
 """
 
 affine = img.get_affine()
 
 """
-Load and show the zooms which hold the voxel size.
+Read the voxel size from the image header.
 """
 
 zooms = img.get_header().get_zooms()[:3]
@@ -64,9 +66,9 @@ data2, affine2 = resample(data, affine, zooms, new_zooms)
 print('data2.shape (%d, %d, %d, %d)' % data2.shape)
 
 """
-data2.shape (115, 115, 61, 65)
+data2.shape ``(115, 115, 61, 65)``
 
-Mask out most of the background.
+Mask out most of the background in the following simple way.
 """
 mask = data2[..., 0] > 50
 
@@ -87,21 +89,29 @@ grid where the ODF values will be evaluated.
 
 sphere = get_sphere('symmetric724')
 
-peaks = peaks_from_model(model=csamodel,
+csapeaks = peaks_from_model(model=csamodel,
                          data=data2,
                          sphere=sphere,
                          relative_peak_threshold=.8,
                          min_separation_angle=45,
                          mask=mask,
+                         return_odf=False,
                          normalize_peaks=True)
 
-GFA = peaks.gfa
+GFA = csapeaks.gfa
 
 print('GFA.shape (%d, %d, %d)' % GFA.shape)
 
 """
-GFA.shape (115, 115, 61)
+GFA.shape ``(115, 115, 61)``
 
+Apart from GFA csapeaks has also the attributes peak_values, peak_indices and ODF. peak_values
+shows the maxima values of the ODF and peak_indices gives us their position on the
+discrete sphere that was used to do the reconstruction of the ODF. In order to
+obtain the full ODF return_odf should be True. Before enabling this option make sure that you have enough memory.
+"""
+
+"""
 .. include:: ../links_names.inc
 
 """
