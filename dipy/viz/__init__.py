038ea982b400966959178532ab87919c1e8a72c3
arokem <arokem@gmail.com>
2013-02-06 13:56:11 -0800

    Removed mayavi dependencies in the viz module.

--- dipy/viz/__init__.py
+++ dipy/viz/__init__.py
@@ -1,21 +1,7 @@
 # Init file for visualization package
 
 
-# We make the visualization requirements (mayavi, matplotlib) optional
-# imports:
-
-try: 
-    # Mayavi appears in many guises:
-    try:
-        from enthought.mayavi import mlab
-    except ImportError:
-        from mayavi import mlab
-    has_mayavi = True
-except ImportError:
-    e_s = "You do not have Mayavi installed. Some visualization functions"
-    e_s += " might not work."
-    print(e_s)
-    has_mayavi = False
+# We make the visualization requirements optional imports:
 
 try:
     import matplotlib
@@ -25,9 +11,6 @@ except ImportError:
     e_s += " might not work for you."
     print(e_s)
     has_mpl = False
-
-if has_mayavi:
-    from .mayavi.spheres import show_odfs
 
 if has_mpl:
     import projections
