ad10ecf0f752e6895783a29897cdd78fe37b8fdb
arokem <arokem@gmail.com>
2013-02-18 16:22:44 -0800

    DOC: Added some meat to the tensor example.

--- doc/examples/reconst_dti.py
+++ doc/examples/reconst_dti.py
@@ -1,10 +1,42 @@
 """
 
-=========================
-Reconstruct with a Tensor
-=========================
+============================================================
+Reconstruction of the diffusion signal with the tensor model
+============================================================
 
-This example shows how to reconstruct your diffusion datasets using a single Tensor model.
+The diffusion tensor model is a phenomenological model of the diffusion within
+a voxel. First proposed by Basser and colleagues _[Basser1994], it has been
+extremely influential in demonstrating the utility of diffusion MRI in
+characterizing the micro-structure of white matter tissue and of the
+biophysical properties of tissue, inferred from local diffusion properties.
+
+The diffusion tensor models the diffusion signal as:
+
+.. math::
+
+    \frac{S(\theta, b)}{S_0} = e^{-b\theta^t Q \theta}
+
+Where $\theta$ is a unit vector in 3 space indicating the direction of
+measurement and b are the parameters of measurement, such as the strength and
+duration of diffusion-weighting gradient. $S(\theta, b)$ is the
+diffusion-weighted signal measured and $S_0$ is the signal conducted in a
+measurement with no diffusion weighting. Q is a positive-definite quadratic
+form, which contains six free parameters to be fit. These six parameters are:
+
+.. math::
+
+   Q = \begin{pmatrix} D_{xx} & D_{xy} & D_{xz} \\
+                       D_{yx} & D_{yy} & D_{yz} \\
+                       D_{zx} & D_{zy} & D_{zz} \\ \end{pmatrix}
+
+This matrix is a variance/covariance matrix of the diffusivity along the three
+spatial dimensions. Note that we can assume that diffusivity has antipodal
+symmetry, so elements across the diagonal are equal. For example:
+$D_{xy} = D_{yx}$. This is why there are only 6 free parameters to estimate
+here.
+
+In the following example we show how to reconstruct your diffusion datasets
+using a single tensor model.
 
 First import the necessary modules:
 
@@ -21,7 +53,8 @@ import numpy as np
 import nibabel as nib
 
 """
-``dipy.reconst`` is for the reconstruction algorithms which we use to create voxel models from the raw data.
+``dipy.reconst`` is for the reconstruction algorithms which we use to create
+voxel models from the raw data.
 """
 
 from dipy.reconst.dti import TensorModel
@@ -33,8 +66,8 @@ from dipy.reconst.dti import TensorModel
 from dipy.data import fetch_stanford_hardi
 
 """
-Fetch will download the raw dMRI dataset of a single subject. The size of the dataset is 51 MBytes.
-You only need to fetch once.
+Fetch will download the raw dMRI dataset of a single subject. The size of the
+dataset is 51 MBytes.  You only need to fetch once.
 """
 
 fetch_stanford_hardi()
@@ -48,8 +81,9 @@ from dipy.data import read_stanford_hardi
 img, gtab = read_stanford_hardi()
 
 """
-img contains a nibabel Nifti1Image object (with the data) and gtab contains a GradientTable
-object (information about the gradients e.g. b-values and b-vectors).
+img contains a nibabel Nifti1Image object (with the data) and gtab contains a
+GradientTable object (information about the gradients e.g. b-values and
+b-vectors).
 """
 
 data = img.get_data()
@@ -115,7 +149,26 @@ tenfit = tenmodel.fit(data2, mask)
 """
 The fit method creates a TensorFit object which contains the fitting parameters
 and other attributes of the model. For example we can generate fractional
-anisotropy from the eigen values of the single tensor.
+anisotropy (FA) from the eigen-values of the tensor. FA is used to characterize
+the degree to which the distribution of diffusion in a voxel is
+directional. That is, whether there is relatively unrestricted diffusion in one
+particular direction.
+
+Mathematically, FA is defined as the normalized variance of the eigen-values of
+the tensor:
+
+.. math::
+
+        FA = \sqrt{\frac{1}{2}\frac{(\lambda_1-\lambda_2)^2+(\lambda_1-
+                    \lambda_3)^2+(\lambda_2-\lambda_3)^2}{\lambda_1^2+
+                    \lambda_2^2+\lambda_3^2}}
+
+Note that FA should be interperted carefully. It may be an indication of the
+density of packing of fibers in a voxel, and the amount of myelin wrapping these
+axons, but it is not always a measure of "tissue integrity". For example, FA
+may decrease in locations in which there is fanning of white matter fibers, or
+where more than one population of white matter fibers crosses.
+
 """
 
 from dipy.reconst.dti import fractional_anisotropy
@@ -170,6 +223,22 @@ fvtk.record(r, n_frames=1, out_path='tensor_odfs.png', size=(600, 600))
    :align: center
 
    **Tensor ODFs**.
+
+Note that while the tensor model is an accurate and reliable model of the
+diffusion signal in the white matter, it has the drawback that it only has one
+principal diffusion direction. Therefore, in locations in the brain that
+contain multiple fiber populations crossing each other, the tensor model may
+indicate that the principal diffusion direction is intermediate to these
+directions. Therefore, using the principal diffusion direction for tracking in
+these locations may be misleading and may lead to errors in defining the
+tracks. Fortunately, other reconstruction methods can be used to represent the
+diffusion and fiber orientations in those locations. These are presented in
+other examples.
+
+
+
+.. [Basser1994] Basser PJ, Mattielo J, LeBihan (1994). MR diffusion tensor
+                spectroscopy and imaging.
 
 .. include:: ../links_names.inc
 
