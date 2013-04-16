2fa7f74161763cb898c25e63cce63a1a69065e37
arokem <arokem@gmail.com>
2013-04-16 13:13:23 -0700

    RF: This function is superseded by read_bvals_bvecs.

--- dipy/io/gradients.py
+++ dipy/io/gradients.py
@@ -69,39 +69,3 @@ def read_bvals_bvecs(fbvals, fbvecs):
     return bvals, bvecs
 
 
-def read_btable(fbtab):
-    """ Read b-table from the disk
-
-    B-table is a 2D array which contains b-values in the
-    first column and b-vectors in the last column.
-
-    Parameters
-    ----------
-    fbtab : str,
-        path of file with b-table
-
-    Returns
-    -------
-    bvals : array, (N,)
-    bvecs : array, (N, 3)
-
-    """
-    if isinstance(fbtab, basestring):
-        base, ext = splitext(fbvals)
-        if ext in ['.txt', '']:
-            btable=np.loadtxt(fbtab)
-        if ext == '.npy':
-            btable = np.load(fbvecs)
-        if bvecs.shape[1] > bvecs.shape[0]:
-            btable = btable.T
-        if min(bvecs.shape) != 4:
-            raise IOError('btable file should have 4 rows')
-        bvals = np.squeeze(btable[:, 0])
-        bvecs = btable[:, 1:]
-        if bvals.ndim != 1 and bvecs.ndim != 2:
-            raise IOError('b-table was not loaded correctly')
-        if max(bvals.shape) != max(bvecs.shape):
-            raise IOError('b-values and b-vectors shapes do not correspond')
-    else:
-        raise ValueError('A string with the full filepath is required')
-    return bvals, bvecs
