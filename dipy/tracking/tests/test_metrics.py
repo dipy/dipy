b0c694ac48c496b37130ed6d92636539d9b7abe7
Matthew Brett <matthew.brett@gmail.com>
2013-04-15 19:19:07 -0700

    RF: remove unused StringIO import
    Helps with Python 3 compatibility as well.

--- dipy/tracking/tests/test_metrics.py
+++ dipy/tracking/tests/test_metrics.py
@@ -1,6 +1,5 @@
 ''' Testing track_metrics module '''
 
-from StringIO import StringIO
 import numpy as np
 from nose.tools import assert_true, assert_false, assert_equal, assert_almost_equal
 from numpy.testing import assert_array_equal, assert_array_almost_equal
