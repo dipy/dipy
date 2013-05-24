dc341213951b36c72ae6b0fa5044a1c82251252a
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-05-24 16:11:55 -0400

    wip

--- dipy/reconst/tests/csa_odf_example_fulldata.py
+++ dipy/reconst/tests/csa_odf_example_fulldata.py
@@ -1,7 +1,7 @@
 import numpy as np
 import nibabel as nib
 from dipy.data import fetch_stanford_hardi, read_stanford_hardi, get_sphere
-from dipy.reconst.shm import CsaOdfModel, QballModel, normalize_data
+from dipy.reconst.shm import CsaOdfModel, QballModel, OpdtModel, normalize_data
 from dipy.reconst.odf import gfa, odf_remove_negative_values, minmax_normalize
 
 fetch_stanford_hardi()
@@ -43,6 +43,23 @@ gfa_sh[np.isnan(gfa_sh)] = 0
 print 'Saving nifti...'
 nib.save(nib.Nifti1Image(gfa_sh.astype('float32'), affine), 'gfa_qball_full_brain.nii.gz')    
 nib.save(nib.Nifti1Image(coeff.astype('float32'), affine), 'qball_odf_sh.nii.gz')
+
+
+opdtmodel = OpdtModel(gtab, 4, smooth=0.006)
+print 'Computing the OPDT odf...'
+coeff = opdtmodel._get_shm_coef(data)
+print coeff.shape
+
+print 'Computing GFA...'
+print np.min(coeff[:,:,:,0]),np.max(coeff[:,:,:,0])
+gfa_sh = np.sqrt(1.0 - (coeff[:,:,:,0] ** 2 / ( np.sum(np.square(coeff), axis=3) ) ) )
+gfa_sh[np.isnan(gfa_sh)] = 0
+
+print 'Saving nifti...'
+nib.save(nib.Nifti1Image(gfa_sh.astype('float32'), affine), 'gfa_opdt_full_brain.nii.gz')    
+nib.save(nib.Nifti1Image(coeff.astype('float32'), affine), 'opdt_odf_sh.nii.gz')
+
+
 
 
 
