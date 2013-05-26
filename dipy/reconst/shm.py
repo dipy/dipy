BF - fix scaling for Csa Model

dipy/reconst/shm.py
@@ -388,14 +388,17 @@ class CsaOdfModel(SphHarmModel):
         invB = smooth_pinv(B, sqrt(smooth) * L)
         L = L[:, None]
         F = F[:, None]
-        self._fit_matrix = F * L * invB
+        self._fit_matrix =  (F * L) / (8 * np.pi) * invB
+        self._const = .5 / np.sqrt(np.pi)
 
     def _get_shm_coef(self, data, mask=None):
         """Returns the coefficients of the model"""
         data = data[..., self._where_dwi]
         data = data.clip(self.min, self.max)
         loglog_data = np.log(-np.log(data))
-        return dot(loglog_data, self._fit_matrix.T)
+        sh_coef = dot(loglog_data, self._fit_matrix.T)
+        sh_coef[..., 0] = self._const
+        return sh_coef
 
 
 class OpdtModel(SphHarmModel):