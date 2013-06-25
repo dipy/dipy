12e8076443e6764af035f6e06948f9140e356f74
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-06-25 10:15:07 -0400

    Added many brains for checks

--- doc/examples/brain_extraction.py
+++ doc/examples/brain_extraction.py
@@ -2,17 +2,29 @@ import nibabel as nib
 from dipy.data import fetch_stanford_hardi, read_stanford_hardi
 from dipy.segment.mask import hist_mask
 
-img, gtab = read_stanford_hardi()
+#img, gtab = read_stanford_hardi()
 
-data = img.get_data()
-affine = img.get_affine()
 
-b0 = np.mean(data[..., 0:9], axis=-1)
+files = ['./test_bench/Stanford/b0.nii.gz', 
+         './test_bench/3T/GE/b0.nii.gz', 
+         './test_bench/3T/Siemens/b0.nii.gz', 
+         './test_bench/3T/Philips/b0.nii.gz',
+         './test_bench/1.5T/GE/b0.nii.gz', 
+         './test_bench/1.5T/Siemens/b0.nii.gz']
 
-mask = hist_mask(b0)
+for f in files:
 
-nib.save(nib.Nifti1Image(b0, affine), 'b0.nii.gz')
-nib.save(nib.Nifti1Image(mask.astype('byte'), affine), 'mask.nii.gz')
+    img = nib.load(f)
+
+    b0 = img.get_data().squeeze()
+    affine = img.get_affine()
+
+    #b0 = np.mean(data[..., 0:9], axis=-1)
+
+    mask = hist_mask(b0)
+
+    #nib.save(nib.Nifti1Image(b0, affine), f + 'b0.nii.gz')
+    nib.save(nib.Nifti1Image(mask.astype('byte'), affine), f + '_mask.nii.gz')
 
 
 
