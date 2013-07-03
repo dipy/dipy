ea181b1b8762de3b1824611fd015c3be6fc62da5
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-07-03 14:10:34 -0400

    brain extraction modifications

--- doc/examples/brain_extraction.py
+++ doc/examples/brain_extraction.py
@@ -5,12 +5,12 @@ from dipy.segment.mask import dwi_bet_filter
 from scipy.ndimage import generate_binary_structure, binary_dilation
 from dipy.segment.mask import hist_mask
 
-files = ['./test_bench/Stanford/b0.nii.gz', 
-         './test_bench/3T/GE/b0.nii.gz', 
-         './test_bench/3T/Siemens/b0.nii.gz', 
-         './test_bench/3T/Philips/b0.nii.gz',
-         './test_bench/1.5T/GE/b0.nii.gz', 
-         './test_bench/1.5T/Siemens/b0.nii.gz']
+files = ['./test_datasets_multicite_all_companies/Stanford/b0.nii.gz', 
+         './test_datasets_multicite_all_companies/3T/GE/b0.nii.gz', 
+         './test_datasets_multicite_all_companies/3T/Siemens/b0.nii.gz', 
+         './test_datasets_multicite_all_companies/3T/Philips/b0.nii.gz',
+         './test_datasets_multicite_all_companies/1.5T/GE/b0.nii.gz', 
+         './test_datasets_multicite_all_companies/1.5T/Siemens/b0.nii.gz']
 
 for f in files :
     print(f)
@@ -19,10 +19,10 @@ for f in files :
 
     b0 = data[:,:,:, 0]
         
-#     dwi_mask, mask = dwi_bet_filter(data, 4, 4, autocrop=False)
-#     mask_img = nib.Nifti1Image(mask.astype(np.float32), img.get_affine())
-#     print(f+'_mask.nii.gz')
-#     nib.save(mask_img, f+'_mask.nii.gz')
+    dwi_mask, mask = dwi_bet_filter(data, 4, 4, autocrop=False)
+    mask_img = nib.Nifti1Image(mask.astype(np.float32), img.get_affine())
+    print(f+'_mask.nii.gz')
+    nib.save(mask_img, f+'_mask.nii.gz')
 
     epi_mask = hist_mask(b0)
     epi_mask_img = nib.Nifti1Image(epi_mask.astype(np.float32), img.get_affine())
