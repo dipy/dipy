d2f527c58491c143445b8e9b6864076fb661e697
arokem <arokem@gmail.com>
2013-07-17 18:32:06 -0700

    Another profiling script (more concise).

---
+++ scratch/profile_dti.py
@@ -0,0 +1,19 @@
+import nibabel as nib
+import dipy.core.gradients as grad
+import dipy.data as dpd
+import dipy.reconst.dti as dti
+
+dd = nib.load(data).get_data()
+data, bvals, bvecs = dpd.get_data('small_25')
+gtab = grad.gradient_table(bvals, bvecs)
+
+def func_jac():
+    tm = dti.TensorModel(gtab, fit_method='NLLS', jac=True)
+    tf = tm.fit(dd)
+
+def func_no_jac():
+    tm = dti.TensorModel(gtab, fit_method='NLLS', jac=False)
+    tf = tm.fit(dd)
+
+if __name__=="__main__":
+    func()
