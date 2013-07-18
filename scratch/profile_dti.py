b8d31b4e06b32c927d30c3a80db163921f260682
arokem <arokem@gmail.com>
2013-07-17 18:39:58 -0700

    BF: the other order doesn't make much sense :-)

--- scratch/profile_dti.py
+++ scratch/profile_dti.py
@@ -3,8 +3,8 @@ import dipy.core.gradients as grad
 import dipy.data as dpd
 import dipy.reconst.dti as dti
 
-dd = nib.load(data).get_data()
 data, bvals, bvecs = dpd.get_data('small_25')
+dd = nib.load(data).get_data()
 gtab = grad.gradient_table(bvals, bvecs)
 
 def func_jac():
