8542c38196a3c01a428cdca0fbe603d460044522
Mauro <mauro@Mauro.(none)>
2013-07-26 18:18:56 -0400

    tests fixed

--- dipy/reconst/tests/test_canal.py
+++ dipy/reconst/tests/test_canal.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pylab as py
 from dipy.data import get_data, two_shells_voxels, three_shells_voxels, get_sphere
+from dipy.data.fetcher import (fetch_isbi2013_2shell, read_isbi2013_2shell,
+                               fetch_sherbrooke_3shell, read_sherbrooke_3shell)
 from dipy.reconst.canal import ShoreModel
+from dipy.reconst.shm import QballModel
 from dipy.reconst.odf import gfa, peak_directions
 from dipy.io import read_bvals_bvecs
 from dipy.core.gradients import gradient_table
@@ -20,24 +23,31 @@ import nibabel as nib
 
 
 def test_canal():
+    fetch_isbi2013_2shell()
+    img, gtab=read_isbi2013_2shell()
+
     # load symmetric 724 sphere
     sphere = get_sphere('symmetric724')
+
     # load icosahedron sphere
-    sphere2 = create_unit_sphere(5)
-    fimg, fbvals, fbvecs = get_data('ISBI_testing_2shells_table')
-    bvals, bvecs = read_bvals_bvecs(fbvals, fbvecs) 
-    gtab = gradient_table(bvals[1:], bvecs[1:,:])
-    data, golden_directions = SticksAndBall(gtab, d=0.0015,
+    sphere2 = create_unit_sphere(5)    
+    # must remove the b=0 image
+    gtab2 = gradient_table(gtab.bvals[1:], gtab.bvecs[1:,:])
+
+    data, golden_directions = SticksAndBall(gtab2, d=0.0015,
                                             S0=100, angles=[(0, 0), (90, 0)],
                                             fractions=[50, 50], snr=None)
 
-    asm = ShoreModel(gtab)
 
+            
+    
+    asm = ShoreModel(gtab2)
     # symmetric724
     asmfit = asm.fit(data)
     Cshore = asmfit.l2estimation(radialOrder=6, zeta=700, lambdaN=1e-8, lambdaL=1e-8)
     Csh = asmfit.odf()
     odf = sh_to_sf(Csh, sphere, 6, basis_type="fibernav")
+
     directions, _, _ = peak_directions(odf, sphere, .35, 25)
     assert_equal(len(directions), 2)
     assert_almost_equal(angular_similarity(directions, golden_directions), 2, 1)
@@ -51,7 +61,7 @@ def test_canal():
     assert_equal(len(directions), 2)
     assert_almost_equal(angular_similarity(directions, golden_directions), 2, 1)
 
-    sb_dummies = sticks_and_ball_dummies(gtab)
+    sb_dummies = sticks_and_ball_dummies(gtab2)
     for sbd in sb_dummies:
         data, golden_directions = sb_dummies[sbd]
         asmfit = asm.fit(data)
@@ -65,13 +75,17 @@ def test_canal():
             assert_equal(gfa(odf) < 0.1, True)
 
 
-def test_multivox_canal():
+def test_multivox_canal():    
+    fetch_sherbrooke_3shell()
+    img, gtab=read_sherbrooke_3shell()
+    
+    gtab2 = gradient_table(gtab.bvals[1:], gtab.bvecs[1:,:])
 
-  
-    # data, affine, gtab = two_shells_voxels(10, 40, 10, 40, 25, 26)
-    data, affine, gtab = three_shells_voxels(45, 65, 35, 65, 33, 34)
+    # must remove the b=0 image
+    test = img.get_data()
+    data = test[45:65, 35:65, 33:34, 1:]
 
-    asm = ShoreModel(gtab)
+    asm = ShoreModel(gtab2)
     asmfit = asm.fit(data)
     radialOrder = 4
     zeta = 700
@@ -83,5 +97,6 @@ def test_multivox_canal():
 
 if __name__ == '__main__':
     run_module_suite()
-    #test_multivox_canal()
     #test_canal()
+    #test_multivox_canal()
+    
