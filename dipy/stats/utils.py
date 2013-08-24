6d290f1eccac6de6536d7e6074d8ff22ec767b7d
arokem <arokem@gmail.com>
2013-08-23 17:32:34 -0700

    WIP: Started implementing cross-validation from model predictions.

---
+++ dipy/stats/utils.py
@@ -0,0 +1,39 @@
+"""
+Utility functions for the boots module
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+def coeff_of_determination(data, model, axis=-1):
+    """
+
+    Notes
+    ------
+
+    See: http://en.wikipedia.org/wiki/Coefficient_of_determination
+
+    The coefficient of determination is calculated as:
+
+    .. math::
+
+
+    R^2 = 100 * (1 - \frac{SSE}{SSD})
+
+    where SSE is the sum of the squared error between the model and the data
+    (sum of the squared residuals) and SSD is the sum of the squares of the
+    deviations of the (variance * N)
+    """
+
+    residuals = data - model
+    ss_err = np.sum(residuals ** 2, axis=axis)
+
+    demeaned_data = data - np.mean(data, axis=axis)[...,np.newaxis]
+    ss_tot = np.sum(demeaned_data **2, axis=axis)
+
+    # Don't divide by 0:
+    if np.all(ss_tot==0.0):
+        return np.nan
+
+    return 100 * (1 - (ss_err/ss_tot))
