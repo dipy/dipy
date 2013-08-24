28d073e505c196f281f06219d85a452283621e06
Mauro Zucchelli <mauro.zucchelli88@gmail.com>
2013-08-24 17:37:39 -0400

    fix the code deleting the rtop_signal non norm

--- doc/examples/reconst_dsi_metrics.py
+++ doc/examples/reconst_dsi_metrics.py
@@ -8,7 +8,7 @@ from dipy.core.ndindex import ndindex
 from dipy.data import get_sphere
 import dipy.reconst.dti as dti
 from dipy.data import get_data, dsi_voxels
-from dipy.reconst.dsi import (DiffusionSpectrumModel,DiffusionSpectrumDeconvModel)
+from dipy.reconst.dsi import DiffusionSpectrumModel
 from dipy.core.sphere import Sphere
 from dipy.core.gradients import gradient_table
 from dipy.io.gradients import read_bvals_bvecs
@@ -17,60 +17,57 @@ fetch_taiwan_ntu_dsi()
 img, gtab = read_taiwan_ntu_dsi()
 data = img.get_data()
 affine = img.get_affine()
+data = data / (data[..., 0, None]).astype(np.float)
 
 print('data.shape (%d, %d, %d, %d)' % data.shape)
 
-dsmodel = DiffusionSpectrumModel(gtab, qgrid_size=35, r_start=0.4*17, r_end=0.7*17, filter_width=18.5)
+dsmodel = DiffusionSpectrumModel(
+    gtab, qgrid_size=35, r_start=0.4 * 17, r_end=0.7 * 17, filter_width=18.5)
 
 dataslice = data[30:70, 20:80, data.shape[2] / 2]
 
 # for the signal the normalization means that is divided by the b0
-print('Calculating... rtop_signal_norm') 
-rtop_signal_norm=dsmodel.fit(dataslice).rtop_signal()
-print('Calculating... rtop_signal')
-rtop_signal=dsmodel.fit(dataslice).rtop_signal(normalized=False)
+print('Calculating... rtop_signal_norm')
+rtop_signal_norm = dsmodel.fit(dataslice).rtop_signal()
+
 
 # for the pdf the normalization means that is divided by pdf.sum()
 print('Calculating... rtop_pdf_norm')
-rtop_pdf_norm=dsmodel.fit(dataslice).rtop_pdf()
+rtop_pdf_norm = dsmodel.fit(dataslice).rtop_pdf()
 print('Calculating... rtop_pdf')
-rtop_pdf=dsmodel.fit(dataslice).rtop_pdf(normalized=False)
+rtop_pdf = dsmodel.fit(dataslice).rtop_pdf(normalized=False)
 
 # for the pdf the normalization means that is divided by pdf.sum()
 print('Calculating... MSD_norm')
-MSD_norm=dsmodel.fit(dataslice).MSD_discrete()
+MSD_norm = dsmodel.fit(dataslice).MSD_discrete()
 print('Calculating... MSD')
-MSD=dsmodel.fit(dataslice).MSD_discrete(normalized=False)
+MSD = dsmodel.fit(dataslice).MSD_discrete(normalized=False)
 
 # plot rtop
-fig=plt.figure()
-ax1=fig.add_subplot(2,2,1)
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
 ax1.set_title('rtop_signal_norm')
-ind=ax1.imshow(rtop_signal_norm.T,interpolation='nearest',origin='lower')
+ind = ax1.imshow(rtop_signal_norm.T, interpolation='nearest', origin='lower')
 plt.colorbar(ind)
-ax2=fig.add_subplot(2,2,2)
-ax2.set_title('rtop_signal')
-ind=ax2.imshow(rtop_signal.T,interpolation='nearest',origin='lower')
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.set_title('rtop_pdf_norm')
+ind = ax2.imshow(rtop_pdf_norm.T, interpolation='nearest', origin='lower')
 plt.colorbar(ind)
-ax3=fig.add_subplot(2,2,3)
-ax3.set_title('rtop_pdf_norm')
-ind=ax3.imshow(rtop_pdf_norm.T,interpolation='nearest',origin='lower')
-plt.colorbar(ind)
-ax4=fig.add_subplot(2,2,4)
-ax4.set_title('rtop_pdf')
-ind=ax4.imshow(rtop_pdf.T,interpolation='nearest',origin='lower')
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.set_title('rtop_pdf')
+ind = ax3.imshow(rtop_pdf.T, interpolation='nearest', origin='lower')
 plt.colorbar(ind)
 plt.show()
 
 # plot MSD
-fig=plt.figure()
-ax1=fig.add_subplot(2,1,1)
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 1, 1)
 ax1.set_title('MSD_norm')
-ind=ax1.imshow(MSD_norm.T,interpolation='nearest',origin='lower')
+ind = ax1.imshow(MSD_norm.T, interpolation='nearest', origin='lower')
 plt.colorbar(ind)
-ax2=fig.add_subplot(2,1,2)
+ax2 = fig.add_subplot(2, 1, 2)
 ax2.set_title('MSD')
-ind=ax2.imshow(MSD.T,interpolation='nearest',origin='lower')
+ind = ax2.imshow(MSD.T, interpolation='nearest', origin='lower')
 plt.colorbar(ind)
 plt.show()
 
