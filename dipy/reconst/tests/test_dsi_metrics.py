afc377ab38c62c6d25e7144ddbde27efe11a0ec8
Mauro Zucchelli <mauro.zucchelli88@gmail.com>
2013-08-24 17:36:30 -0400

    delited the rtop_signal non norm

--- dipy/reconst/tests/test_dsi_metrics.py
+++ dipy/reconst/tests/test_dsi_metrics.py
@@ -8,7 +8,8 @@ from dipy.core.ndindex import ndindex
 from dipy.data import get_sphere
 import dipy.reconst.dti as dti
 from dipy.data import get_data, dsi_voxels
-from dipy.reconst.dsi import (DiffusionSpectrumModel,DiffusionSpectrumDeconvModel)
+from dipy.reconst.dsi import (
+    DiffusionSpectrumModel, DiffusionSpectrumDeconvModel)
 from dipy.core.sphere import Sphere
 from dipy.core.gradients import gradient_table
 from dipy.io.gradients import read_bvals_bvecs
@@ -28,32 +29,27 @@ from numpy.testing import assert_equal
 from dipy.core.subdivide_octahedron import create_unit_sphere
 from dipy.core.sphere_stats import angular_similarity
 
+
 def test_dsi():
-    #load symmetric 724 sphere
+    # load symmetric 724 sphere
     sphere = get_sphere('symmetric724')
-    #load icosahedron sphere
+    # load icosahedron sphere
     sphere2 = create_unit_sphere(5)
     btable = np.loadtxt(get_data('dsi515btable'))
-    gtab = gradient_table(btable[:,0], btable[:,1:])
-    data, golden_directions = SticksAndBall(gtab, d=0.0015, 
+    gtab = gradient_table(btable[:, 0], btable[:, 1:])
+    data, golden_directions = SticksAndBall(gtab, d=0.0015,
                                             S0=100, angles=[(0, 0), (90, 0)],
                                             fractions=[50, 50], snr=None)
-
+    data = data / float(data[0])
     dsmodel = DiffusionSpectrumModel(gtab)
+    rtop_signal_norm = dsmodel.fit(data).rtop_signal()
+    rtop_pdf_norm = dsmodel.fit(data).rtop_pdf()
+    rtop_pdf = dsmodel.fit(data).rtop_pdf(normalized=False)
+    MSD_norm = dsmodel.fit(data).MSD_discrete()
+    MSD = dsmodel.fit(data).MSD_discrete(normalized=False)
+    assert_almost_equal(rtop_signal_norm, rtop_pdf, 10)
 
-    rtop_signal_norm=dsmodel.fit(data).rtop_signal()
-    rtop_signal=dsmodel.fit(data).rtop_signal(normalized=False)
-
-    rtop_pdf_norm=dsmodel.fit(data).rtop_pdf()
-    rtop_pdf=dsmodel.fit(data).rtop_pdf(normalized=False)
-
-    MSD_norm=dsmodel.fit(data).MSD_discrete()
-    MSD=dsmodel.fit(data).MSD_discrete(normalized=False)
-    
-    assert_almost_equal(rtop_signal, rtop_pdf, 10)
-    
     # We need a test for MSD!!!
-    
 
 
 if __name__ == '__main__':
