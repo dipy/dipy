4b8a35691227bf7ee1f1499472424a2fa682e953
Samuel St-Jean <stjeansam@gmail.com>
2013-09-03 18:50:00 -0400

    Fixed comments and added more ot the intro

--- doc/examples/segment_corpus_callosum_from_rgb.py
+++ doc/examples/segment_corpus_callosum_from_rgb.py
@@ -1,10 +1,12 @@
-"""
-==============================
+"""==============================
 Corpus callosum segmentation 
 ==============================
 
-This example shows how to segment the corpus callosum from a raw DWI
-using the Colored Fractional Anisotropy as a threshold reference. 
+This example shows how to roughly segment the corpus callosum from a raw DWI.
+The method uses the Colored Fractional Anisotropy as a threshold reference. 
+Thepurpose of the kind of segmentation is not to clearly separate the structure, 
+but rather to compute an automatic mask in order to compute various metrics
+in the region of interest later.
 
 First import the necessary modules:
 """
@@ -16,8 +18,7 @@ import numpy as np
 
 from dipy.data import fetch_stanford_hardi, read_stanford_hardi
 
-"""
-Download and read the data for this tutorial.
+"""Download and read the data for this tutorial.
 Let's first load the data. We will use a dataset with 10 b0s and 
 150 non-b0s with b-value 2000.
 """
@@ -25,8 +26,7 @@ Let's first load the data. We will use a dataset with 10 b0s and
 fetch_stanford_hardi()
 img, gtab = read_stanford_hardi()
 
-"""
-img contains a nibabel Nifti1Image object (data) and gtab contains a GradientTable
+"""img contains a nibabel Nifti1Image object (data) and gtab contains a GradientTable
 object (gradient information e.g. b-values). For example to read the b-values
 it is possible to write print(gtab.bvals).
 
@@ -37,12 +37,10 @@ data = img.get_data()
 affine = img.get_affine()
 print('data.shape (%d, %d, %d, %d)' % data.shape)
 
-"""
-data.shape ``(81, 106, 76, 160)`` 
+"""data.shape ``(81, 106, 76, 160)`` 
 """
 
-"""
-To reduce the computation time, we will only estimate the tensor model 
+"""To reduce the computation time, we will only estimate the tensor model 
 inside the brain region by creating a mask without the background.
 (See the masking example for more details about this step)
 """
@@ -50,8 +48,7 @@ inside the brain region by creating a mask without the background.
 from dipy.segment.mask import median_otsu
 b0_mask, mask = median_otsu(data[..., 0], 4, 4)
 
-"""
-We can now do a first segmentation of the data using the Colored Fractionnal 
+"""We can now do a first segmentation of the data using the Colored Fractional 
 Anisotropy (or cfa). It encodes in a 3D volume the orientation of the diffusion.
 Red means that the principal direction of the tensor is in x, Green is for the 
 y direction and the z direction is encoded with blue.
@@ -64,7 +61,7 @@ Let's pick a threshold of 0.7 in the x axis and 0.1 in the y and z axis as a
 segmentation threshold. 
 
 We will also define a rough roi, since noisy pixels could be considered in the
-maks if it's not bounded properly. Adjusting the cfa threshold and the roi 
+mask if it's not bounded properly. Adjusting the cfa threshold and the roi 
 location enables the function to segment any part of the brain based on 
 an orientation and spatial location criterion.
 
@@ -85,8 +82,10 @@ CC_box[roi[0]:roi[1], roi[2]:roi[3], roi[4]:roi[5]] = 1
 mask_corpus_callosum, cfa = segment_from_dwi(data, gtab, CC_box, 
                                              threshold, mask=mask, return_cfa=True)
 
-"""
-We can save the produced dataset with nibabel to visualize them later on.
+print ("Size of the mask :", np.count_nonzero(mask_corpus_callosum), \
+       "voxels out of", np.size(CC_box))
+
+"""We can save the produced dataset with nibabel to visualize them later on.
 This way, we can also redo a segmentation of another region using the cfa instead
 of recomputing everything everytime.
 
@@ -102,8 +101,7 @@ nib.save(cfa_img, 'cfa.nii.gz')
 nib.save(mask_corpus_callosum_img, 'mask_corpus_callosum.nii.gz')
 
 
-"""
-The mask is a bit small, so let's change the threshold and restart segmenting
+"""The mask is a bit small, so let's change the threshold and restart segmenting
 from the cfa using the corresponding function. Both of them do the same thing,
 but the segmentation from cfa is faster when you have it, since it doesn't need 
 to recompute the tensors in order to compute the cfa.
@@ -117,9 +115,10 @@ mask_corpus_callosum_from_cfa = segment_from_cfa(cfa, CC_box, threshold2)
 mask_corpus_callosum_from_cfa_img = nib.Nifti1Image(mask_corpus_callosum_from_cfa, affine)
 nib.save(mask_corpus_callosum_from_cfa_img, 'mask_corpus_callosum_from_cfa.nii.gz')
 
+print ("Size of the mask :", np.count_nonzero(mask_corpus_callosum_from_cfa), \
+       "voxels out of", np.size(CC_box))
 
-"""
-Let's check the result of the segmentation using matplotlib.
+"""Let's check the result of the segmentation using matplotlib.
 """
 
 import matplotlib.pyplot as plt
