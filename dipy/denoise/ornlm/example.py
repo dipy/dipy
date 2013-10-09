16633ade3c5eed930d868d7ee13346c5bb114cea
omarocegueda <jomaroceguedag@gmail.com>
2013-10-09 16:16:02 -0500

    Added an S0 denoising example.

--- dipy/denoise/ornlm/example.py
+++ dipy/denoise/ornlm/example.py
@@ -1,26 +1,22 @@
-import dipy
 import numpy as np
-import nibabel as nib
-from dipy.denoise.ornlm.ornlm_module import ornlmpy
-from dipy.denoise.ornlm.hsm import hsm
-from dipy.denoise.ornlm.ascm import ascm
-from dipy.data import get_data
+import ornlm_module as ornml
+from hsm import hsm
+from ascm import ascm
+from dipy.data import fetch_stanford_hardi
+from dipy.data import read_stanford_hardi
+fetch_stanford_hardi()
 
-fimg, fbval, fbvec = get_data('small_101D')
-nib_image = nib.load(fimg)
-image=nib_image.get_data().astype(np.double)
-
-fima1=np.empty_like(image, order='F')#Filter using ornlm, search-volume size=3, block size=1
-fima2=np.empty_like(image, order='F')#Filter using ornlm, search-volume size=3, block size=2
-fima3=np.empty_like(image, order='F')#Filter using hsm
-fima4=np.empty_like(image, order='F')#Filter using ascm
-#filter each volume (corresponding to each gradient) separately
-for i in xrange(image.shape[3]):
-    print "Filtering volume",i+1,"/",image.shape[3]
-    mv=image[:,:,:,i].max()
-    h=0.05*mv #Parameter: the amount of filtering, it is set to 5% of the maximum value in the current volume
-    fima1[:,:,:,i]=np.array(ornlmpy(image[:,:,:,i], 3, 1, h))
-    fima2[:,:,:,i]=np.array(ornlmpy(image[:,:,:,i], 3, 2, h))
-    fima3[:,:,:,i]=np.array(hsm(fima1[:,:,:,i],fima2[:,:,:,i]))
-    fima4[:,:,:,i]=np.array(ascm(image[:,:,:,i], fima1[:,:,:,i],fima2[:,:,:,i], h))
-
+img, gtab = read_stanford_hardi()
+data = img.get_data()
+S0 = data[..., 0].astype(np.float64)
+mv=np.max(S0)
+#Note: in P. Coupe et al. the ricial noise was simulated as 
+#sqrt((f+x)^2 + (y)^2) where f is the pixel value and x and y are 
+#independent realizations of a random variable with Normal distribution, 
+#with mean=0 and standard deviation=h. The user must tune the 'h' parameter 
+#taking that into consideration
+h=0.01*mv
+f1=np.array(ornml.ornlm_pyx(S0, 3, 1, h))
+f2=np.array(ornml.ornlm_pyx(S0, 3, 2, h))
+fhsm=hsm(f1,f2)
+filterd=ascm(S0,f1,f2,h)#this is reported to have the highest SNR in synthetic experiments
