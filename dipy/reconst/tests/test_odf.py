75a0100516c0bfd6ce5365717bf52a9c3e8e8374
Bago Amirbekian <mrbago@gmail.com>
2013-11-18 16:04:08 -0800

    RF - pass dummy gradient table to OdfModel/OdfFit tests

--- dipy/reconst/tests/test_odf.py
+++ dipy/reconst/tests/test_odf.py
@@ -5,19 +5,19 @@ from dipy.reconst.odf import (OdfFit, OdfModel, minmax_normalize)
 from dipy.core.subdivide_octahedron import create_unit_hemisphere
 from dipy.sims.voxel import multi_tensor, multi_tensor_odf
 from dipy.data import get_sphere
-from dipy.core.gradients import gradient_table
+from dipy.core.gradients import gradient_table, GradientTable
 
 
 _sphere = create_unit_hemisphere(4)
 _odf = (_sphere.vertices * [1, 2, 3]).sum(-1)
+_gtab = GradientTable(np.ones((64, 3)))
 
 
 class SimpleOdfModel(OdfModel):
     sphere = _sphere
 
     def fit(self, data):
-        fit = SimpleOdfFit(_sphere, data)
-        fit.model = self
+        fit = SimpleOdfFit(self, data)
         return fit
 
 
@@ -32,17 +32,7 @@ class SimpleOdfFit(OdfFit):
 
 
 def test_OdfFit():
-    bvalue = 3000
-    S0 = 1
-    SNR = 100
-
-    sphere = get_sphere('symmetric362')
-    bvecs = np.concatenate(([[0, 0, 0]], sphere.vertices))
-    bvals = np.zeros(len(bvecs)) + bvalue
-    bvals[0] = 0
-    gtab = gradient_table(bvals, bvecs)
-
-    m = SimpleOdfModel(gtab)
+    m = SimpleOdfModel(_gtab)
     f = m.fit(None)
     odf = f.odf(_sphere)
     assert_equal(len(odf), len(_sphere.theta))
