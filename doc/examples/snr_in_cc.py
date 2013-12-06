4195deb1ee8f8ba19b0047408775f12f3695998b
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2013-12-06 16:18:52 -0500

    correcting typos

--- doc/examples/snr_in_cc.py
+++ doc/examples/snr_in_cc.py
@@ -11,30 +11,24 @@ the gradient direction corresponding to each DWI.
 
 In classical MRI, SNR can be defined as the ratio of the mean 
 of the signal divided by the standard deviation of the
-noise, that is SNR = mean(signal) / std(noise). The noise standard deviation can be 
-computed from the background in any of the DW images. But how do we computed 
+underlying Gaussian noise, that is SNR = mean(signal) / std(noise). 
+The noise standard deviation can be 
+computed from the background in any of the DW images. How do we compute
 the mean of the signal, and what signal?
 
 
 The strategy here is to compute a 'worst-case' SNR for DWI. Several white matter  
 structures such as the corpus callosum (CC), corticospinal tract (CST), or
 the superior longitudinal fasciculus (SLF) can be easily identified from
-the colored-FA (cfa) map. In this example, we will use voxels from the CC, 
-which have the characteristic of being highly RED in the cfa map because mainly oriented in  
-the left-right direction.
-
-
-Hence, the purpose here is not to get a valid 
-segmentation of the CC but voxels where we are very confident that
-the underlying fiber population is in left-right (x-direction). These voxels
-will be used to compute the mean signal of all DW images. We know that the DW image
-closest to the x-direction will be the one with \emph{most attenuated} diffusion signal.
-Therefore,  this will produce a worst-case SNR estimation for the given DWI dataset. This is
-the strategy adopted in several recent papers (see [1]_ and [2]_). It gives a good
+the colored-FA (cfa) map. In this example, we will use voxels from the CC,
+which have the characteristic of being highly RED in the cfa map since they are mainly oriented in 
+the left-right direction. We know that the DW image
+closest to the x-direction will be the one with the most attenuated diffusion signal.
+This is the strategy adopted in several recent papers (see [1]_ and [2]_). It gives a good 
 indication of the quality of the DWI data.
 
 
-First, we compute the tensor model in a brain mask (see the DTI example for more explanation)
+First, we compute the tensor model in a brain mask (see the DTI example for more explanation).
 
 """
 
@@ -54,7 +48,7 @@ print('Computing tensors...')
 tenmodel = TensorModel(gtab)
 tensorfit = tenmodel.fit(data, mask=mask)
 
-"""Next, we set our red-blue-green thresholds to (0.7, 1) in the x axis
+"""Next, we set our red-blue-green thresholds to (0.6, 1) in the x axis
 and (0, 0.1) in the y and z axes respectively.
 These values work well in practice to isolate the very RED voxels of the cfa map.
 
@@ -106,10 +100,8 @@ fig.savefig("CC_segmentation.png")
 .. figure:: CC_segmentation.png
 """
 
-"""Now that we are happy with our crude mask that selected voxels in x-direction, 
+"""Now that we are happy with our crude mask that selected voxels in the x-direction, 
 we can use all the voxels to estimate the mean signal in this region.
-(\emph{recall that we did not want a perfect CC segmentation but just several voxels in the
-x-direction})
 
 """
 
@@ -117,9 +109,9 @@ mean_signal = np.mean(data[mask_cc_part], axis=0)
 
 """Now, we need a good background estimation. We will re-use the brain mask
 computed before and invert it to catch the outside of the brain. This could
-also be determined manually with a box ROI in the background.
-(certain MR manufacturers mask out the outside of the brain with 0's, you then
-have to carefully define a box ROI manually).
+also be determined manually with a ROI in the background.
+[Warning: Certain MR manufacturers mask out the outside of the brain with 0's. 
+One thus has to be careful how the noise ROI is defined].
 """
 
 from scipy.ndimage.morphology import binary_dilation
@@ -131,7 +123,7 @@ nib.save(mask_noise_img, 'mask_noise.nii.gz')
 
 noise_std = np.std(data[mask_noise, :])
 
-"""We can now compute the SNR for each DWI using the formula defined above.
+"""We can now compute the SNR for each DWI.
 Let's find the position of the gradient direction that lies the closest to
 the X, Y and Z axes.
 """
@@ -142,10 +134,6 @@ gtab.bvecs[idx] = np.inf
 axis_X = np.argmin(np.sum((gtab.bvecs-np.array([1, 0, 0]))**2, axis=-1))
 axis_Y = np.argmin(np.sum((gtab.bvecs-np.array([0, 1, 0]))**2, axis=-1))
 axis_Z = np.argmin(np.sum((gtab.bvecs-np.array([0, 0, 1]))**2, axis=-1))
-
-"""Now let's compute their respective SNR and compare them to the SNR of the
-b0 image SNR. 
-"""
 
 for direction in [0, axis_X, axis_Y, axis_Z]:
 	SNR = mean_signal[direction]/noise_std
@@ -163,8 +151,9 @@ for direction in [0, axis_X, axis_Y, axis_Z]:
 
 Since the CC is aligned with the X axis, it is the lowest SNR in all of
 the DWIs, where the DW signal is the most attenuated. In comparison, the DW images in
-perpendical Y and Z axes have a high SNR. The b0 still exhibits the highest SNR,
-since there is no signal attenuation. Hence, we can say the Stanford diffusion data has a 'worst-case' SNR of approximately 5, a  
+the perpendical Y and Z axes have a high SNR. The b0 still exhibits the highest SNR,
+since there is no signal attenuation. Hence, we can say the Stanford diffusion 
+data has a 'worst-case' SNR of approximately 5, a  
 'best-case' SNR of approximately 24, and a SNR of 44 on the b0 image. 
 
 """
