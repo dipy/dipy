4f11475230a12f127c0aee78e3d70c2b5424cf02
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-12-06 15:06:19 -0500

    DOC: new example for real beginners who don't know much about data analysis in python and they want to start using Dipy

--- doc/examples/quick_start.py
+++ doc/examples/quick_start.py
@@ -2,30 +2,213 @@
 =========================
 Getting started with Dipy
 =========================
+
+In diffusion MRI (dMRI) usually use three types of files the a Nifti file with the
+diffusion weighted data (*.nii.gz), and two text files one with b-values and
+one for the b-vectors.
+
+In Dipy we provide tools to load and process these files and we also provide
+access to publically available datasets for those who haven't acquired yet
+their own datasets.
+
+With the followin commands we will download a dMRI dataset
 """
 
+from dipy.data import fetch_sherbrooke_3shell
+fetch_sherbrooke_3shell()
 
-from dipy.data import get_data
-fimg, fbval, fbvec = get_data('small_101D')
+"""
+By default these datasets will go in the .dipy folder inside your home directory.
+Here is how you can access them.
+"""
+
+from os.path import expanduser, join
+home = expanduser('~')
+
+"""
+``dname`` holds the directory name where the 3 files are in.
+"""
+
+dname = join(home, '.dipy', 'sherbrooke_3shell') 
+
+"""
+Here, we show the complete filenames of the 3 files
+"""
+
+fdwi = join(dname, 'HARDI193.nii.gz')
+
+print(fdwi)
+
+fbval = join(dname, 'HARDI193.bval')
+
+print(fbval)
+
+fbvec = join(dname, 'HARDI193.bvec')
+
+print(fbvec)
+
+"""
+/home/username/.dipy/sherbrooke_3shell/HARDI193.nii.gz
+
+/home/username/.dipy/sherbrooke_3shell/HARDI193.bval
+
+/home/username/.dipy/sherbrooke_3shell/HARDI193.bvec
+
+Now, that we have the filenames we can start doing some processing
+
+Lets, start first by loading the dMRI datasets. For this purpose we
+can use nibabel.
+"""
 
 import nibabel as nib
-img = nib.load(fimg)
+img = nib.load(fdwi)
 data = img.get_data()
+
+"""
+``data`` is a 4D array where the first 3 dimensions are the i, j, k voxel
+positions and the last dimension is the number of non-weighted (S0s) and
+diffusion waited volumes.
+
+We can very easily check the size of ``data`` in the following way
+"""
+
+print(data.shape)
+
+"""
+``(128, 128, 60, 194)``
+
+We can also check the dimensions of each voxel in the following way
+
+"""
+
+print(img.get_header().get_zooms()[:3])
+
+"""
+``(2.0, 2.0, 2.0)``
+
+We can quickly visualize the results using ``matplotlib``. For example,
+let's show here the middle axial slices of volume 0 and volume 10.
+"""
+
+import matplotlib.pyplot as plt
+
+axial_middle = data.shape[2] / 2
+plt.figure('Showing the datasets')
+plt.subplot(1, 2, 1).set_axis_off()
+plt.imshow(data[:, :, axial_middle, 0].T, cmap='gray', origin='lower')
+plt.subplot(1, 2, 2).set_axis_off()
+plt.imshow(data[:, :, axial_middle, 10].T, cmap='gray', origin='lower')
+plt.show()
+plt.savefig('data.png', bbox_inches='tight')
+
+"""
+.. figure:: data.png
+   :align: center
+
+   **Showing the middle axial without (left) and with (right) diffusion weighting**.
+
+The next step is to load the b-values and b-vectors from the disk using 
+the function ``read_bvals_bvecs``.
+"""
 
 from dipy.io import read_bvals_bvecs
 bvals, bvecs = read_bvals_bvecs(fbval, fbvec)
 
+"""
+In Dipy we use an object called GradientTable which holds all in good form all the 
+acquision specific parameters e.g. b-values, b-vectors, timings and others. To create this 
+object you can use the function ``gradient_table``.
+"""
+
 from dipy.core.gradients import gradient_table
 gtab = gradient_table(bvals, bvecs)
 
-from dipy.reconst.dti import TensorModel
-ten = TensorModel(gtab)
-tenfit = ten.fit(data)
+"""
+Finally, you can use ``gtab`` (the GradientTable object) to show some information about the
+acquisition parameters
+"""
 
-from dipy.reconst.dti import fractional_anisotropy
-fa = fractional_anisotropy(tenfit.evals)
+print(gtab.info)
 
-from dipy.reconst.dti import color_fa
-cfa = color_fa(fa, tenfit.evecs)
+"""
+B-values shape (193,)
+         min 0.000000 
+         max 3500.000000 
+B-vectors shape (193, 3)
+         min -0.964050 
+         max 0.999992 
 
+You, can also see the b-values using:
+"""
 
+print(gtab.bvals)
+
+"""
+
+::
+
+    [    0.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.  1000.
+      1000.  1000.  1000.  1000.  1000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.
+      2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  2000.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.  3500.
+      3500.  3500.  3500.]
+
+Or, for example the 10 first b-vectors using:
+"""
+
+print(gtab.bvecs[:10, :])
+
+"""
+
+::
+
+    array([[ 0.        ,  0.        ,  0.        ],
+           [ 0.999979  , -0.00504001, -0.00402795],
+           [ 0.        ,  0.999992  , -0.00398794],
+           [-0.0257055 ,  0.653861  , -0.756178  ],
+           [ 0.589518  , -0.769236  , -0.246462  ],
+           [-0.235785  , -0.529095  , -0.815147  ],
+           [-0.893578  , -0.263559  , -0.363394  ],
+           [ 0.79784   ,  0.133726  , -0.587851  ],
+           [ 0.232937  ,  0.931884  , -0.278087  ],
+           [ 0.93672   ,  0.144139  , -0.31903   ]])
+
+``gtab`` can tell in which place in your dataset you had S0 volumes 
+(volumes which correspond to b-values of 0).
+"""
+
+S0s = data[:, :, :, gtab.b0s_mask]
+
+"""
+Here, we had only 1 S0 as we can see by looking at the dimension of S0s
+"""
+
+print(S0s.shape)
+
+"""
+``(128, 128, 60, 1)``
+
+Just, for fun let's save this in a new Nifti1 file.
+"""
+
+nib.save(nib.Nifti1Image(S0s, img.get_affine()), 'HARDI193_S0.nii.gz')
+
+"""
+Now, that we learned how to load dMRI datasets we can start the analysis. 
+See example :ref:`example_reconst_dti` to learn how to create Tensors.
+"""
