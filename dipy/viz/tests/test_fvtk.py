1d5a45e691748813b7f16fac88b63eeba12fb471
Stefan van der Walt <stefan@sun.ac.za>
2013-12-11 20:30:20 +0200

    Increase FVTK test coverage.

--- dipy/viz/tests/test_fvtk.py
+++ dipy/viz/tests/test_fvtk.py
@@ -4,15 +4,20 @@ import numpy as np
 
 from dipy.viz import fvtk
 
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_
 import numpy.testing as npt
+
+import tempfile
+import os
+import shutil
+
 
 @npt.dec.skipif(not fvtk.have_vtk)
 @npt.dec.skipif(not fvtk.have_vtk_colors)
 def test_fvtk_functions():
 
     # Create a renderer
-    r = fvtk.ren()
+    r = fvtk.renderer()
 
     # Create 2 lines with 2 different colors
     lines = [np.random.rand(10, 3), np.random.rand(20, 3)]
@@ -28,7 +33,7 @@ def test_fvtk_functions():
     # Create a volume and return a volumetric actor using volumetric rendering
     vol = 100 * np.random.rand(100, 100, 100)
     vol = vol.astype('uint8')
-    r = fvtk.ren()
+    r = fvtk.renderer()
     v = fvtk.volume(vol)
     fvtk.add(r, v)
 
@@ -51,6 +56,32 @@ def test_fvtk_functions():
 
 @npt.dec.skipif(not fvtk.have_vtk)
 @npt.dec.skipif(not fvtk.have_vtk_colors)
+def test_fvtk_record():
+    d = tempfile.mkdtemp(prefix='dipy_fvtk')
+    try:
+        ren = fvtk.renderer()
+        fvtk.record(ren, os.path.join(d, 'snapshot'), size=(10, 10))
+        assert_(os.path.exists(os.path.join(d, 'snapshot.png')))
+
+        fvtk.record(ren, os.path.join(d, 'snapshot'), size=(10, 10),
+                    path_numbering=True)
+        assert_(os.path.exists(os.path.join(d, 'snapshot0000000.png')))
+
+        fvtk.record(ren, os.path.join(d, 'snapshot'), size=(10, 10),
+                    path_numbering=True, n_frames=3)
+        for i in range(3):
+            assert_(os.path.exists(os.path.join(d, 'snapshot000000%d.png' % i)))
+
+        fvtk.record(ren, os.path.join(d, 'snapshot'), size=(10, 10),
+                    magnification=2)
+
+
+    finally:
+        shutil.rmtree(d)
+
+
+@npt.dec.skipif(not fvtk.have_vtk)
+@npt.dec.skipif(not fvtk.have_vtk_colors)
 def test_fvtk_ellipsoid():
 
     evals = np.array([1.4, .35, .35]) * 10 ** (-3)
@@ -66,11 +97,10 @@ def test_fvtk_ellipsoid():
 
     sphere = get_sphere('symmetric724')
 
-    ren = fvtk.ren()
+    ren = fvtk.renderer()
 
     fvtk.add(ren, fvtk.tensor(mevals, mevecs, sphere=sphere))
 
     fvtk.add(ren, fvtk.tensor(mevals, mevecs, np.ones(mevals.shape), sphere=sphere))
 
     assert_equal(ren.GetActors().GetNumberOfItems(), 2)
-
