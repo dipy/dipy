d232d89aea7d9918418e8d280ed4b76cbceef136
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-12-18 18:51:13 -0500

    BF: corrected parameters order in simulate_multi_tensor example

--- doc/examples/simulate_multi_tensor.py
+++ doc/examples/simulate_multi_tensor.py
@@ -82,11 +82,7 @@ resolution by subdividing the triangles of one of the Dipy's default spheres.
 sphere = get_sphere('symmetric724')
 sphere = sphere.subdivide(2)
 
-
-
-
-odf = multi_tensor_odf_new(sphere, fractions, mevals, angles)
-
+odf = multi_tensor_odf(sphere, mevals, angles, fractions)
 
 from dipy.viz import fvtk
 
