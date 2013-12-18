de17abaac330c5aaeec37e31dd162274f6cba009
Samuel St-Jean <Samuel.St-Jean@usherbrooke.ca>
2013-12-18 18:11:46 -0500

    padding added

--- dipy/denoise/nlmeans.py
+++ dipy/denoise/nlmeans.py
@@ -8,9 +8,11 @@ def get_block(arr, center, radius):
 
     cx, cy, cz = center
 
-    return arr[cx - radius : cx + radius + 1,
-               cy - radius : cy + radius + 1,
-               cz - radius : cz + radius + 1]
+   # print(cx - radius - 1, cx + radius, center, radius)
+
+    return arr[cx - radius - 1: cx + radius,
+               cy - radius - 1: cy + radius,
+               cz - radius - 1: cz + radius]
 
 
 def padding(A, radius):
@@ -21,8 +23,17 @@ def padding(A, radius):
     shape = np.array(A.shape)
     padded = np.zeros(shape + 2*(radius - 1))
 
-    padded[:radius-1, :radius-1, :radius-1] = A[:radius-1:-1, :radius-1:-1, :radius-1:-1]
-    padded[-1:radius-shape[0]:-1, -1:radius-shape[1]:-1, -1:radius-shape[2]:-1] = A[-1:radius:-1, -1:radius:-1, -1:radius:-1]
+    padded[:radius-1, :radius-1, :radius-1] = A[:radius-1, :radius-1, :radius-1:][::-1]
+    padded[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1] = A[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1][::-1]
+
+    padded[:radius-1, :radius-1, :radius-1] = A[:radius-1, :radius-1, :radius-1:][::-1]
+    padded[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1] = A[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1][::-1]
+
+    padded[:radius-1, :radius-1, :radius-1] = A[:radius-1, :radius-1, :radius-1:][::-1]
+    padded[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1] = A[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1][::-1]
+
+    padded[:radius-1, :radius-1, :radius-1] = A[:radius-1, :radius-1, :radius-1:][::-1]
+    padded[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1] = A[-1:-radius:-1, -1:-radius:-1, -1:-radius:-1][::-1]
 
     return padded
 
@@ -33,22 +44,35 @@ def nlmeans(image, patch_size=3, block_size=11, sigma=None, ncoils=1):
         sigma = 5 # Call piesno instead
 
     # do padding so we can cut exactly the number of required blocks
-    print(image.shape)
-    image = padding(image, block_size)
-    print(image.shape)
+    #print(image.shape)
+    #image = padding(image, block_size)
+    #print(image.shape)
+
+    shape = image.shape
+    padding = (block_size-1, block_size-1) * image.ndim
+    image = np.pad(image, zip(padding[::2], padding[1::2]), mode='reflect')
     out = np.zeros_like(image)
+
+    #print("le print")
+    #print(out.shape)
+    #print("fin")
 
     for idx in ndindex(image.shape):
 
-        neighborhood = get_block(image, idx, block_size)
-        x = get_block(neighborhood, idx, patch_size)
+        idx = np.array(idx) + block_size - 1
+        neighborhood = get_block(image, idx, (block_size-1)/2)
+        print(image.shape,idx,block_size)
+        x = get_block(neighborhood, idx, (patch_size-1)/2)
         w = np.zeros_like(neighborhood)
 
+        #print("2e print")
         print(x.shape, w.shape, neighborhood.shape)
 
-        for pos in ndindex(neighborhood.shape):
+        for pos in ndindex(np.array(neighborhood.shape) - patch_size):
 
-            y = get_block(neighborhood, pos, patch_size)
+            pos = np.array(pos) + patch_size - 1
+            y = get_block(neighborhood, pos, (patch_size-1)/2)
+            #print(x.shape,y.shape,pos,idx)
             w = np.exp(-np.sqrt(np.mean((x - y)**2))/sigma)
             w /= np.sum(w)
 
@@ -59,6 +83,6 @@ def nlmeans(image, patch_size=3, block_size=11, sigma=None, ncoils=1):
         out[out < 0] = 0
 
     # unpad out
-    out = out[radius:out.shape[0]-radius, radius:out.shape[1]-radius, radius:out.shape[2]-radius]
+    out = out[radius:shape[0]-radius, radius:shape[1]-radius, radius:shape[2]-radius]
 
     return np.sqrt(out)
