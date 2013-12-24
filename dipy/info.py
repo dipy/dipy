fbde103afacaa9f11a0daf66d1ed07004589592f
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-12-24 01:46:45 -0500

    Dipy info updated

--- dipy/info.py
+++ dipy/info.py
@@ -8,8 +8,8 @@ docs.  In setup.py in particular, we exec this file, so it cannot import dipy
 # version
 _version_major = 0
 _version_minor = 7
-_version_micro = 0
-_version_extra = ''
+_version_micro = 1
+_version_extra = '.dev'
 #_version_extra = ''
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
