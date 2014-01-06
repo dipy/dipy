cfa0944809df36e0d0364b88b82775928eef0772
Matthew Brett <matthew.brett@gmail.com>
2014-01-06 00:47:23 +0000

    RF: refactor dpy io tests to fix windows error
    Try to fix windows access error by using InTemporaryDirectory rather
    than a temporary file.

--- dipy/io/tests/test_dpy.py
+++ dipy/io/tests/test_dpy.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
-from tempfile import mkstemp
+
+from nibabel.tmpdirs import InTemporaryDirectory
 
 from ..dpy import Dpy, have_tables
+
 
 from nose.tools import assert_true, assert_false, \
      assert_equal, assert_raises
@@ -16,24 +18,22 @@ iftables = npt.dec.skipif(not have_tables,
 
 @iftables
 def test_dpy():
-    fd,fname = mkstemp()    
-    dpw = Dpy(fname,'w')    
-    A=np.ones((5,3))
-    B=2*A.copy()
-    C=3*A.copy()        
-    dpw.write_track(A)
-    dpw.write_track(B)
-    dpw.write_track(C)            
-    dpw.write_tracks([C,B,A])    
-    dpw.close()    
-    dpr = Dpy(fname,'r')    
-    assert_equal(dpr.version()=='0.0.1',True)    
-    T=dpr.read_tracksi([0,1,2,0,0,2])    
-    print(T)    
-    T2=dpr.read_tracks()    
-    assert_equal(len(T2),6)    
-    dpr.close()
-    assert_array_equal(A,T[0])
-    assert_array_equal(C,T[5])
-    # This might cause problems on windows
-    os.remove(fname)
+    fname = 'test.bin'
+    with InTemporaryDirectory():
+        dpw = Dpy(fname, 'w')
+        A=np.ones((5,3))
+        B=2*A.copy()
+        C=3*A.copy()
+        dpw.write_track(A)
+        dpw.write_track(B)
+        dpw.write_track(C)
+        dpw.write_tracks([C,B,A])
+        dpw.close()
+        dpr = Dpy(fname, 'r')
+        assert_equal(dpr.version()=='0.0.1',True)
+        T=dpr.read_tracksi([0,1,2,0,0,2])
+        T2=dpr.read_tracks()
+        assert_equal(len(T2),6)
+        dpr.close()
+        assert_array_equal(A,T[0])
+        assert_array_equal(C,T[5])
