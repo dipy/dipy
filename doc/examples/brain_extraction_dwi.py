368b78167eb6edd1e11beef771e94ebb3a5b0eaf
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2014-02-18 12:55:22 -0500

    Fix line break format.

--- doc/examples/brain_extraction_dwi.py
+++ doc/examples/brain_extraction_dwi.py
@@ -64,12 +64,12 @@ from dipy.core.histeq import histeq
 sli = data.shape[2] / 2
 plt.figure('Brain segmentation')
 plt.subplot(1, 2, 1).set_axis_off()
-plt.imshow(
-    histeq(data[:, :, sli].astype('float')).T, cmap='gray', origin='lower')
+plt.imshow(histeq(data[:, :, sli].astype('float')).T,
+           cmap='gray', origin='lower')
 
 plt.subplot(1, 2, 2).set_axis_off()
-plt.imshow(
-    histeq(b0_mask[:, :, sli].astype('float')).T, cmap='gray', origin='lower')
+plt.imshow(histeq(b0_mask[:, :, sli].astype('float')).T,
+           cmap='gray', origin='lower')
 
 plt.savefig('median_otsu.png')
 
