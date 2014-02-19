7514025b0d2ad7b9a39ecb742b956a0e31f80da6
Mauro Zucchelli <mauro.zucchelli88@gmail.com>
2014-02-19 10:47:47 +0100

    add small and big delta

--- dipy/core/gradients.py
+++ dipy/core/gradients.py
@@ -232,7 +232,7 @@ def gradient_table(bvals, bvecs=None, big_delta=None, small_delta=None,
         bvecs = np.asarray(bvecs)
         if (bvecs.shape[1] > bvecs.shape[0])  and bvecs.shape[0]>1:
             bvecs = bvecs.T
-    return gradient_table_from_bvals_bvecs(bvals, bvecs, big_delta=None,
-                                           small_delta=None,
+    return gradient_table_from_bvals_bvecs(bvals, bvecs, big_delta=big_delta,
+                                           small_delta=small_delta,
                                            b0_threshold=b0_threshold,
                                            atol=1e-2)
