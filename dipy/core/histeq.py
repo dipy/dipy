d5765d0d961aa32f783f6c2a61c86a6adf282b62
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2014-02-24 09:44:27 -0500

    Fix comment format and input var name.

--- dipy/core/histeq.py
+++ dipy/core/histeq.py
@@ -1,15 +1,14 @@
 import numpy as np
 
 
-def histeq(im, num_bins=256):
-    """
-    Performs an histogram equalization on ``img``.
+def histeq(arr, num_bins=256):
+    """ Performs an histogram equalization on ``arr``.
     This was taken from:
     http://www.janeriksolem.net/2009/06/histogram-equalization-with-python-and.html
 
     Parameters
     ----------
-    im : ndarray
+    arr : ndarray
         Image on which to perform histogram equalization.
     num_bins : int
         Number of bins used to construct the histogram.
@@ -20,11 +19,11 @@ def histeq(im, num_bins=256):
         Histogram equalized image.
     """
     #get image histogram
-    histo, bins = np.histogram(im.flatten(), num_bins, normed=True)
+    histo, bins = np.histogram(arr.flatten(), num_bins, normed=True)
     cdf = histo.cumsum()
     cdf = 255 * cdf / cdf[-1]
 
     #use linear interpolation of cdf to find new pixel values
-    result = np.interp(im.flatten(), bins[:-1], cdf)
+    result = np.interp(arr.flatten(), bins[:-1], cdf)
 
-    return result.reshape(im.shape)
+    return result.reshape(arr.shape)
