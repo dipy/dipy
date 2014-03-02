496cba0d09e222aa5f6d827b3af9027ae8bcccf8
omarocegueda <jomaroceguedag@gmail.com>
2014-03-01 19:10:24 -0500

    Init function for SymmetricDiffeomorphicRegistration

--- dipy/align/imwarp.py
+++ dipy/align/imwarp.py
@@ -3,29 +3,43 @@ import numpy as np
 
 class SymmetricDiffeomorphicRegistration(object):
 
-    def __init__(self, metric, iters):
+    def __init__(   self,
+                    metric,
+                    opt_iters = [25, 100, 100],
+                    opt_tol = 1e-4,
+                    inv_iters = 20,
+                    inv_tol = 1e-3):
         """ Symmetric Diffeomorphic Registration
 
         Parameters
         ----------
         metric : Similarity object
 
-        iters : int
+        opt_iters : list
+            maximum number of iterations at each level of the Gaussian Pyramid 
+            (multi-resolution), opt_iters[0] corresponds the finest resolution
+
+        opt_tol: float
+            tolerance for the optimization algorithm
 
         inv_iters : int
+            maximum number of iterations of the displacement field inversion 
+            algorithm 
 
-        tol_inv : float
-
-        tol_opt : float
+        inv_tol : float
+            tolerance for the displacement field inversion algorithm
 
         """
 
-        self.iters = iters
+        self.metric = metric
+        self.opt_iters = opt_iters
+        self.opt_tol = opt_tol
+        self.inv_iters = inv_iters
+        self.inv_tol = inv_tol
 
     def optimize(self, static, moving):
 
         return SymmetricDiffeomorficMap(self)
-
 
 class SymmetricDiffeomorficMap(object):
 
