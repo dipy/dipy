e55cd938553d4dec2b5667cbd8b28aa646d9c65b
Bago Amirbekian <mrbago@gmail.com>
2014-04-30 11:45:33 -0700

    NF - added trilinear interpolation function for tracking

--- dipy/tracking/localtrack.pyx
+++ dipy/tracking/localtrack.pyx
@@ -5,7 +5,7 @@ from propspeed cimport _propagation_direction
 
 import numpy as np
 
-from libc.math cimport round
+from libc.math cimport round, floor
 from libc.stdio cimport printf
 
 
@@ -168,4 +168,63 @@ def _testCheckPoint(TissueClassifier tc, double[::1] point not None):
         int tissue
     tissue = tc.check_point(&point[0])
     return tissue
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef int _trilinear_interpolate4d(double[:, :, :, :] data, double[:] point,
+                                  double[::1] result) nogil:
+    """Tri-linear interpolation along the last dimension of a 4d array
+
+    Parameters
+    ----------
+    point : 1d array (3,)
+        3 doubles representing a 3d point in space. If point has integer values
+        ``[i, j, k]``, the result will be the same as ``data[i, j, k]``.
+    data : 4d array
+        Data to be interpolated.
+    result : 1d array
+        The result of interpolation. Should have length equal to the
+        ``data.shape[3]``.
+
+    Returns
+    -------
+    err : int
+         0 : successful interpolation.
+        -1 : point is outside the data area, meaning round(point) is not a
+             valid index to data.
+        -2 : mismatch between data, result and/or point.
+
+    """
+    cdef:
+        np.npy_intp index[3][2], flr, N
+        double weight[3][2], w, rem
+
+    N = result.shape[0]
+    if data.shape[3] != N or point.shape[0] != 3:
+        return -2
+
+    for i in range(3):
+        if point[i] < -.5 or point[i] >= (data.shape[i] - .5):
+            return -1
+
+        flr = <np.npy_intp> floor(point[i])
+        rem = point[i] - flr
+
+        index[i][0] = flr + (flr == -1)
+        index[i][1] = flr + (flr != (data.shape[i] - 1))
+        weight[i][0] = 1 - rem
+        weight[i][1] = rem
+
+    for i in range(N):
+        result[i] = 0
+
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                w = weight[0][i] * weight[1][j] * weight[2][k]
+                for L in range(N):
+                    result[L] += w * data[index[0][i], index[1][j],
+                                          index[2][k], L]
+    return 0
 
