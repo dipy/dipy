8b6a66916f83934c243a74580452bb58829421fc
Bago Amirbekian <mrbago@gmail.com>
2014-07-24 12:24:38 -0700

    NF - added boundary stepping method

--- dipy/tracking/local/localtrack.pyx
+++ dipy/tracking/local/localtrack.pyx
@@ -5,7 +5,72 @@ from .tissue_classifier cimport (TissueClassifier, TissueClass, TRACKPOINT,
                                  ENDPOINT, OUTSIDEIMAGE, INVALIDPOINT)
 
 
+cdef extern from "dpy_math.h" nogil:
+    int signbit(double x)
+    double round(double x)
+    double abs(double)
+
+
+@cython.cdivision(True)
+cdef inline double stepsize(double point, double increment) nogil:
+    """Compute the step size to the closest boundary in units of increment."""
+    cdef:
+        double dist
+    dist = round(point) + .5 - signbit(increment) - point
+    if dist == 0:
+        # Point is on an edge, return step size to next edge.  This is most
+        # likely to come up if overstep is set to 0.
+        return 1. / abs(increment)
+    else:
+        return dist / increment
+
+
+cdef void step_to_boundry(double *point, double *direction,
+                          double overstep) nogil:
+    """Takes a step from point in along direction just past a voxel boundary.
+
+    Parameters
+    ----------
+    direction : c-pointer to double[3]
+        The direction along which the step should be taken.
+    point : c-pointer to double[3]
+        The tracking point which will be updated by this function.
+    overstep : double
+        It's often useful to have the points of a streamline lie inside of a
+        voxel instead of having them lie on the boundary. For this reason,
+        each step will overshoot the boundary by ``overstep * direction``.
+        This should not be negative.
+
+    """
+    cdef:
+        double step_sizes[3], smallest_step
+
+    for i in range(3):
+        step_sizes[i] = stepsize(point[i], direction[i])
+
+    smallest_step = step_sizes[0]
+    for i in range(1, 3):
+        if step_sizes[i] < smallest_step:
+            smallest_step = step_sizes[i]
+
+    smallest_step += overstep
+    for i in range(3):
+        point[i] += smallest_step * direction[i]
+
+
 cdef void fixed_step(double *point, double *direction, double stepsize) nogil:
+    """Updates point by stepping in direction.
+
+    Parameters
+    ----------
+    direction : c-pointer to double[3]
+        The direction along which the step should be taken.
+    point : c-pointer to double[3]
+        The tracking point which will be updated by this function.
+    step_size : double
+        The size of step in units of direction.
+
+    """
     for i in range(3):
         point[i] += direction[i] * stepsize
 
@@ -35,6 +100,12 @@ def local_tracker(DirectionGetter dg, TissueClassifier tc,
         TissueClass tssuclass
         double point[3], dir[3], vs[3], voxdir[3]
         double[::1] pview = point, dview = dir
+        void (*step)(double*, double*, double) nogil
+
+    if fixedstep:
+        step = fixed_step
+    else:
+        step = step_to_boundry
 
     for i in range(3):
         point[i] = seed[i]
@@ -48,7 +119,7 @@ def local_tracker(DirectionGetter dg, TissueClassifier tc,
             break
         for j in range(3):
             voxdir[j] = dir[j] / vs[j]
-        fixed_step(point, voxdir, stepsize)
+        step(point, voxdir, stepsize)
         tssuclass = tc.check_point(pview)
         if tssuclass == TRACKPOINT:
             continue
