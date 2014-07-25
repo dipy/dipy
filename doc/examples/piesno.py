8120c20832d699cba6110f21d870d373afd06e4c
Maxime Descoteaux <m.descoteaux@usherbrooke.ca>
2014-07-25 09:38:20 -0400

    made all small corrections and modifications asked for in the PR

--- doc/examples/piesno.py
+++ doc/examples/piesno.py
@@ -3,27 +3,26 @@
 Noise estimation using PIESNO
 =============================
 
-Using PIESNO [Koay2009]_ one can detect the noise's standard deviation from
-diffusion-weighted imaging (DWI). PIESNO also works from multiple channel
+Using PIESNO [Koay2009]_ one can detect the standard deviation of the noise from
+diffusion-weighted imaging (DWI). PIESNO also works with multiple channel
 DWI datasets that are acquired from N array coils for both SENSE and
 GRAPPA reconstructions.
 
-The PIESNO paper [Koay2009]_ is mathetically quite involved.
+The PIESNO paper [Koay2009]_ is mathematically quite involved.
 PIESNO works in two steps. 1) First, it finds voxels that are most likely
 background voxels. Intuitively, these voxels have very similar
 diffusion-weighted intensities (up to some noise) in the fourth dimension
-of the DWI dataset, as opposed to tissue voxels that have diffuison intensities
+of the DWI dataset. White matter, gray matter or CSF voxels have diffusion intensities
 that vary quite a lot across different directions.
 2) From these esimated background voxels and
-the input number of coils N, PIESNO finds what sigma each Gaussian distributed
-image profile from each of the N coils would have generated the observed Rician (N=1)
+the input number of coils N, PIESNO finds what sigma each Gaussian
+from each of the N coils would have generated the observed Rician (N=1)
 or non-central Chi (N>1) distributed noise profile in the DWI datasets.
-[Koay2009]_ gives all the glory details.
+[Koay2009]_ gives all the details.
 
 PIESNO makes an important assumption: the
-Gaussian noise standard deviation is assumed to be uniform either
-across multiple slice locations or across multiple images of the same location,
-e.g., if the readout bandwidth is maintained at the same level for all the images.
+Gaussian noise standard deviation is assumed to be uniform. The noise
+is uniform across multiple slice locations or across multiple images of the same location.
 
 """
 
@@ -39,13 +38,13 @@ data = img.get_data()
 
 """
 
-Now that we have fetched a dataset, we must call PIESNO with right number N
+Now that we have fetched a dataset, we must call PIESNO with right number
 of coil used to acquire this dataset. It is also important to know what
 was the parallel reconstruction algorithm used.
 Here, the data comes from a GRAPPA reconstruction from
-a 12-element head coil available on the Tim Trio Siemens, for which
+a 12-elements head coil available on the Tim Trio Siemens, for which
 the 12 coil elements are combined into 4 groups of 3 coil elements
-each. These groups are received through 4 distinct receiver channels,
+each. The signal is received through 4 distinct groups of receiver channels,
 yielding N = 4. Had we used a GE acquisition, we would have used N=1 even
 if multiple channel coils are used because GE uses a SENSE reconstruction,
 which has a Rician noise nature and thus N is always 1.
@@ -63,9 +62,10 @@ ax[0].imshow(axial, cmap='gray', origin='lower')
 ax[0].set_title('Axial slice of the b=0 data')
 ax[1].imshow(axial_piesno, cmap='gray', origin='lower')
 ax[1].set_title('Background voxels from the data')
-for i in range(2):
-    ax[i].set_axis_off()
+for a in ax:
+    a.set_axis_off()
 
+# Uncomment the coming line if you want a window to show the images.
 #plt.show()
 plt.savefig('piesno.png', bbox_inches='tight')
 
@@ -85,8 +85,8 @@ print('The std of the background is =', np.std(data[mask[...,None].astype(np.boo
 """
 
 Here, we obtained a noise standard deviation of 7.26. For comparison, a simple
-standard deviation of all voxels in the estimated mask (as done in the previous example)
-gives a value of 6.1.
+standard deviation of all voxels in the estimated mask (as done in the previous
+example :ref:`example_snr_in_cc`) gives a value of 6.1.
 
 """
 
