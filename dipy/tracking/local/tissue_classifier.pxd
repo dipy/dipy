2d13a4bc50dd27d17a6dd4df89980fb2081e2ad0
Gabriel Girard <girard.gabriel@gmail.com>
2014-09-09 19:11:11 -0400

    NF - added anatomically-constrained tractography (ACT) tissue classifier

--- dipy/tracking/local/tissue_classifier.pxd
+++ dipy/tracking/local/tissue_classifier.pxd
@@ -9,12 +9,19 @@ cdef class TissueClassifier:
     # Please update doc-string in tissue_classifer.pyx if you change these
     # declarations
     cdef:
-        double threshold, interp_out_double[1]
-        double[::1]  interp_out_view
-        double[:, :, :] metric_map
+        double interp_out_double[1]
+        double[::1] interp_out_view
     cpdef TissueClass check_point(self, double[::1] point) except PYERROR
 
 
 cdef class ThresholdTissueClassifier(TissueClassifier):
+    cdef:
+        double threshold        
+        double[:, :, :] metric_map
+    pass
+
+cdef class ActTissueClassifier(TissueClassifier):
+    cdef:        
+        double[:, :, :] include_map, exclude_map
     pass
 
--- dipy/tracking/local/tissue_classifier.pyx
+++ dipy/tracking/local/tissue_classifier.pyx
@@ -30,14 +30,6 @@ cdef class ThresholdTissueClassifier(TissueClassifier):
             double result
             int err
 
-        """
-        try:
-            trilinear_interpolate4d(self.metric_map[..., None], point,
-                                    self.interp_out_view)
-            result = self.interp_out_view[0]
-        except IndexError:
-            return OUTSIDEIMAGE
-        """
         err = _trilinear_interpolate_c_4d(self.metric_map[..., None], point,
                                           self.interp_out_view)
         if err == -1:
@@ -54,4 +46,51 @@ cdef class ThresholdTissueClassifier(TissueClassifier):
             return TRACKPOINT
         else:
             return ENDPOINT
+
+cdef class ActTissueClassifier(TissueClassifier):
+    """
+    Anatomically-Constained Tractography from Smith et al. (NeuroImage 2012).
+
+    cdef:
+        double interp_out_double[1]
+        double[:]  interp_out_view = interp_out_view
+        double[:, :, :] include_map, exclude_map
+    """
+
+    def __cinit__(self, include_map, exclude_map):
+        self.interp_out_view = self.interp_out_double
+        self.include_map = include_map
+        self.exclude_map = exclude_map
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.initializedcheck(False)
+    cpdef TissueClass check_point(self, double[::1] point):
+        cdef:
+            double include_result, exclude_result
+            int err
+
+        include_err = _trilinear_interpolate_c_4d(self.include_map[..., None], point,
+                                          self.interp_out_view)
+        include_result = self.interp_out_view[0]
+
+        exclude_err = _trilinear_interpolate_c_4d(self.exclude_map[..., None], point,
+                                          self.interp_out_view)        
+        exclude_result = self.interp_out_view[0]
+
+        if include_err == -1 or exclude_err == -1:
+            return OUTSIDEIMAGE
+        elif include_err == -2 or exclude_err == -2:
+            raise ValueError("Point has wrong shape")
+        elif include_err != 0 or exclude_err != 0:
+            # This should never happen
+            raise RuntimeError("You seem to have found a bug in dipy")
+
+        if include_result > 0.5:
+            return ENDPOINT
+        elif exclude_result > 0.5:
+            return INVALIDPOINT
+        else:
+            return TRACKPOINT
+
 
