55e8f92edaf3d466eb73dec78208bc535838b147
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2014-09-12 10:50:20 -0400

    Pep 8

--- dipy/reconst/shm.py
+++ dipy/reconst/shm.py
@@ -182,8 +182,8 @@ def spherical_harmonics(m, n, theta, phi):
         return sph_harm(m, n, theta, phi)
     x = np.cos(phi)
     val = lpmv(m, n, x).astype(complex)
-    val *= np.sqrt((2*n + 1) / 4.0 / np.pi)
-    val *= np.exp(0.5*(gammaln(n-m+1)-gammaln(n+m+1)))
+    val *= np.sqrt((2 * n + 1) / 4.0 / np.pi)
+    val *= np.exp(0.5 * (gammaln(n - m + 1) - gammaln(n + m + 1)))
     val = val * np.exp(1j * m * theta)
     return val
 
