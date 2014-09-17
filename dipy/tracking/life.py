d42f324b6e60dc1b6e2b25d0cb79f28f4e40ba2d
arokem <arokem@gmail.com>
2014-09-17 16:12:41 -0700

    NF: Added Sparse SGD algorithm for solution of the LiFE equations.

--- dipy/tracking/life.py
+++ dipy/tracking/life.py
@@ -7,7 +7,7 @@ and statistical inference in living connectomes. Nature Methods
 
 """
 import numpy as np
-import scipy.sparse as sparse
+import scipy.sparse as sps
 import scipy.linalg as la
 
 from dipy.reconst.base import ReconstModel, ReconstFit
@@ -17,6 +17,209 @@ import dipy.core.sphere as dps
 from dipy.tracking.utils import unique_rows, xform, move_streamlines
 import dipy.reconst.dti as dti
 import dipy.sims.voxel as sims
+
+
+def spdot(A, B):
+  """The same as np.dot(A, B), except it works even if A or B or both
+  are sparse matrices.
+
+  Parameters
+  ----------
+  A, B : arrays of shape (m, n), (n, k)
+
+  Returns
+  -------
+  The matrix product A @ B.
+
+  See discussion here:
+  http://mail.scipy.org/pipermail/scipy-user/2010-November/027700.html
+  """
+  if sps.issparse(A) and sps.issparse(B):
+      return A * B
+  elif sps.issparse(A) and not sps.issparse(B):
+      return (A * B).view(type=B.__class__)
+  elif not sps.issparse(A) and sps.issparse(B):
+      return (B.T * A.T).T.view(type=A.__class__)
+  else:
+      return np.dot(A, B)
+
+
+def rsq(ss_residuals, ss_residuals_to_mean):
+    """
+    Calculate: $R^2 = \frac{1-SSE}{\sigma^2}$
+
+    Parameters
+    ----------
+    ss_residuals : array
+        Model fit errors relative to the data
+    ss_residuals_to_mean : array
+        Residuals of the data relative to the mean of the data (variance)
+
+    Returns
+    -------
+    rsq : the variance explained.
+    """
+    return 100 * (1 - ss_residuals/ss_residuals_to_mean)
+
+
+def sparse_sgd(y, X, momentum=0,
+               prop_select=0.01,
+               step_size=0.1,
+               non_neg=True,
+               prop_bad_checks=0.1,
+               check_error_iter=10,
+               max_error_checks=10,
+               converge_on_r=0.2,
+               verbose=True,
+               plot=True,
+               lamda=0,
+               alpha=0.5):
+    """
+
+    Solve y=Xh for h, using a stochastic gradient descent, with X a sparse
+    matrix
+
+    Parameters
+    ----------
+
+    y: 1-d array of shape (N)
+        The data
+
+    X: ndarray. May be either sparse or dense. Shape (N, M)
+       The regressors
+
+    prop_select: float (0-1, default 0.01)
+        What proportion of the samples to evaluate in each iteration of the
+        algorithm.
+
+    step_size: float, optional (default: 0.05).
+        The increment of parameter update in each iteration
+
+    non_neg: Boolean, optional (default: True)
+        Whether to enforce non-negativity of the solution.
+
+    prop_bad_checks: float (default: 0.1)
+       If this proportion of error checks so far has not yielded an improvement
+       in r squared, we halt the optimization.
+
+    check_error_iter: int (default:10)
+        How many rounds to run between error evaluation for
+        convergence-checking.
+
+    max_error_checks: int (default: 10)
+        Don't check errors more than this number of times if no improvement in
+        r-squared is seen.
+
+    converge_on_r: float (default: 1)
+      a percentage improvement in rsquared that is required each time to say
+      that things are still going well.
+
+    verbose: Boolean (default: True).
+       Whether to display information in each iteration
+
+    plot: whether to generate a plot of the progression of the optimization
+
+    lamda, alpha: ElasticNet params
+
+    Returns
+    -------
+    h_best: The best estimate of the parameters.
+    """
+
+    num_data = y.shape[0]
+    num_regressors = X.shape[1]
+    n_select = np.round(prop_select * num_data)
+
+    # Initialize the parameters at the origin:
+    h = np.zeros(num_regressors)
+
+    # If nothing good happens, we'll return that in the end:
+    h_best = np.zeros(num_regressors)
+    gradient = np.zeros(num_regressors)
+
+    iteration = 1
+    count = 1
+    ss_residuals = []  # This will hold the residuals in each iteration
+    ss_residuals_min = np.inf  # This will keep track of the best solution so far
+    ss_residuals_to_mean = np.sum((y - np.mean(y))**2) # The variance of y
+    rsq_max = -np.inf   # This will keep track of the best r squared so far
+    count_bad = 0  # Number of times estimation error has gone up.
+    error_checks = 0  # How many error checks have we done so far
+
+    while 1:
+        # indices of data points to select
+        idx = np.floor(np.random.rand(n_select) * num_data).astype(int);
+
+        # Select for this round
+        y0 = y[idx]
+        X0 = X[idx]
+
+        if iteration>1:
+            # The sum of squared error given the current parameter setting:
+            sse = np.sum((y - spdot(X,h))**2)
+            # The gradient is (Kay 2008 supplemental page 27):
+            gradient = ((spdot(X0.T, spdot(X0,h) - y0))
+                        +
+                        lamda *((1-alpha) + alpha * h)
+                       )
+            gradient += momentum*gradient
+            # Normalize to unit-length
+            unit_length_gradient = gradient / np.sqrt(np.dot(gradient, gradient))
+            # Update the parameters in the direction of the gradient:
+            h -= step_size * unit_length_gradient
+
+            if non_neg:
+                # Set negative values to 0:
+                h[h<0] = 0
+
+        # Every once in a while check whether it's converged:
+        if np.mod(iteration, check_error_iter):
+            # This calculates the sum of squared residuals at this point:
+            ss_residuals.append(np.sum(np.power(y - spdot(X,h), 2)) +
+                                lamda * (alpha*np.sum(h**2) +
+                                         (1-alpha)*np.sum(h)))
+            rsq_est = rsq(ss_residuals[-1], ss_residuals_to_mean)
+            if verbose:
+                print("Itn #:%03d | SSE: %.1f | R2=%.1f "%
+                      (iteration,
+                       ss_residuals[-1],
+                          rsq_est))
+
+            # Did we do better this time around?
+            if  ss_residuals[-1]<ss_residuals_min:
+                # Update your expectations about the minimum error:
+                ss_residuals_min = ss_residuals[-1]
+                n_iterations = iteration # This holds the number of iterations
+                                        # for the best solution so far.
+                h_best = h # This holds the best params we have so far
+
+                # Are we generally (over iterations) converging on
+                # improvement in r-squared?
+                if rsq_est>rsq_max*(1+converge_on_r/100):
+                    rsq_max = rsq_est
+                    count_bad = 0 # We're doing good. Null this count for now
+                else:
+                    count_bad += 1
+            else:
+                count_bad += 1
+
+            if count_bad >= np.max([max_error_checks,
+                                    np.round(prop_bad_checks*error_checks)]):
+                print("\nOptimization terminated after %s iterations"%iteration)
+                print("R2= %.1f "%rsq_max)
+                print("Sum of squared residuals= %.1f"%ss_residuals_min)
+
+                if plot:
+                    import matplotlib.pyplot as plt
+                    fig, ax = plt.subplots()
+                    ax.plot(ss_residuals)
+                    ax.set_xlabel("Iteration #")
+                    ax.set_ylabel(r"$\sum{(\hat{y} - y)^2}$")
+                # Break out here, because this means that not enough
+                # improvement has happened recently
+                return h_best
+            error_checks += 1
+        iteration += 1
 
 
 def sl_gradients(sl):
@@ -42,9 +245,9 @@ def grad_tensor(grad, evals):
     """
     # This is the rotation matrix from [1, 0, 0] to this gradient of the sl:
     R = la.svd(np.matrix(grad), overwrite_a=True)[2]
-    # This is the quadratic form of the tensor after rotation:
-    Q = np.dot(np.dot(R, np.diag(evals)), R.T)
-    return Q
+    # This is the 3 by 3 tensor after rotation:
+    T = np.dot(np.dot(R, np.diag(evals)), R.T)
+    return T
 
 def sl_tensors(sl, evals=[0.0015, 0.0005, 0.0005]):
     """
@@ -61,7 +264,7 @@ def sl_tensors(sl, evals=[0.0015, 0.0005, 0.0005]):
 
     Returns
     -------
-    An n_nodes by 3 by 3 array with the Q form for each node in the fiber.
+    An n_nodes by 3 by 3 array with the tensor for each node in the fiber.
 
     Note
     ----
@@ -79,7 +282,8 @@ def sl_tensors(sl, evals=[0.0015, 0.0005, 0.0005]):
         tensors[grad_idx] = grad_tensor(this_grad, evals)
     return tensors
 
-def sl_signal(sl, gtab, evals):
+
+def sl_signal(sl, gtab, evals=[0.0015, 0.0005, 0.0005]):
     """
     The signal from a single streamline estimate along each of its nodes.
 
@@ -87,14 +291,12 @@ def sl_signal(sl, gtab, evals):
     ----------
     sl : a single streamline
 
-    gtab : a sphere on which to estimate the signal (presumably made from
-    your bvecs)
+    gtab : GradientTable class instance
 
-    XXX We might be able to make the whole thing go faster by feeding it a
-    sphere and bvals, instead of generating the sphere every time anew
+    evals : list of length 3 (optional. Default: [0.0015, 0.0005, 0.0005])
+        The eigenvalues of the canonical tensor used as an estimate of the
+        signal generated by each node of the streamline.
     """
-
-    sph = dps.Sphere(xyz=gtab.bvecs)
     # Gotta have those tensors:
     tensors = sl_tensors(sl, evals)
     sig = np.empty((len(sl), np.sum(~gtab.b0s_mask)))
@@ -243,12 +445,19 @@ class FiberModel(ReconstModel):
         # Fitting is done on the S0-normalized-and-demeaned diffusion-weighted
         # signal:
         relative_signal = (data[~self.gtab.b0s_mask]/
-                           np.mean(data[self.gtab.b0s_mask])
+                           np.mean(data[self.gtab.b0s_mask]))
 
         to_fit = relative_signal - np.mean(relative_signal, -1)
+        to_fit = to_fit[vox_coords]
+
+        iso_w = sparse_sgd(to_fit.ravel(),
+                           iso_matrix)
+
+        iso_w = sparse_sgd(to_fit.ravel(),
+                           iso_matrix)
 
 
-        return FiberFit(self, )
+        return FiberFit(self, params)
 
     def model_setup(self, sl, affine, evals=[0.0015, 0.0005, 0.0005]):
         """
@@ -298,20 +507,19 @@ class FiberModel(ReconstModel):
             # For each fiber:
             for f_idx in np.where(v2f[v_idx])[0]:
                 # Sum the signal from each node of the fiber in that voxel:
-                pred_sig = np.zeros(n_bvecs)
-                for n_idx in np.where(v2fn[f_idx]==v_idx)[0]:
-                    this_signal = fiber_signal[f_idx][n_idx]
-                    # Predict the signal and demean it, so that the isotropic
-                    # part can carry that:
-                    pred_sig += (this_signal - np.mean(this_signal))
+                vox_fiber_sig = np.zeros(n_bvecs)
+                for node_idx in np.where(v2fn[f_idx]==v_idx)[0]:
+                    this_signal = fiber_signal[f_idx][node_idx]
+                    vox_fiber_sig += (this_signal - np.mean(this_signal))
+                # For each fiber-voxel combination, we now store the row/column
+                # indices and the signal in the pre-allocated linear arrays
+                f_matrix_row[keep_ct1:keep_ct1+n_bvecs] =\
+                                        np.arange(n_bvecs) + v_idx * n_bvecs
+                f_matrix_col[keep_ct1:keep_ct1+n_bvecs] =\
+                                        np.ones(n_bvecs) * f_idx
+                f_matrix_sig[keep_ct1:keep_ct1+n_bvecs] = vox_fiber_sig
+                keep_ct1 += n_bvecs
 
-            # For each fiber-voxel combination, we now store the row/column
-            # indices and the signal in the pre-allocated linear arrays
-            f_matrix_row[keep_ct1:keep_ct1+n_bvecs] =\
-                np.arange(n_bvecs) + v_idx * n_bvecs
-            f_matrix_col[keep_ct1:keep_ct1+n_bvecs] = np.ones(n_bvecs) * f_idx
-            f_matrix_sig[keep_ct1:keep_ct1+n_bvecs] = pred_sig
-            keep_ct1 += n_bvecs
 
             # Put in the isotropic part in the other matrix:
             i_matrix_row[keep_ct2:keep_ct2 + n_bvecs] =\
@@ -323,52 +531,28 @@ class FiberModel(ReconstModel):
         # Allocate the sparse matrices, using the more memory-efficient 'csr'
         # format (converted from the coo format, which we rely on for the
         # initial allocation):
-        fiber_matrix = sparse.coo_matrix((f_matrix_sig,
+        fiber_matrix = sps.coo_matrix((f_matrix_sig,
                                        [f_matrix_row, f_matrix_col])).tocsr()
-        iso_matrix = sparse.coo_matrix((i_matrix_sig,
+        iso_matrix = sps.coo_matrix((i_matrix_sig,
                                        [i_matrix_row, i_matrix_col])).tocsr()
 
         return (fiber_matrix, iso_matrix, vox_coords)
 
 
+def FiberFit(ReconstFit):
+    """
+    A fit of the LiFE model to diffusion data
+    """
 
-
-    @auto_attr
-    def voxel_signal_demeaned(self):
+    def __init__(self, fiber_model, params):
         """
-        The signal in the voxels corresponding to where the fibers pass
-        through, with mean removed
-        """
-        # Get the average, broadcast it back to the original shape and demean,
-        # finally ravel again:
-        return(self.voxel_signal.ravel() -
-               (np.mean(self.voxel_signal,-1)[np.newaxis,...] +
-        np.zeros((len(self.b_idx),self.voxel_signal.shape[0]))).T.ravel())
+        Parameters
+        ----------
+        fiber_model : A FiberModel class instance
 
+        params : the parameters derived from a fit of the model to the data.
 
-    @auto_attr
-    def iso_weights(self):
         """
-        Get the isotropic weights
-        """
-        # XXX Need to replace SGD with another solver:
-        iso_w = sgd.stochastic_gradient_descent(self.voxel_signal.ravel(),
-                                                self.matrix[1],
-                                                verbose=self.verbose)
-        return iso_w
-
-    @auto_attr
-    def fiber_weights(self):
-        """
-        Get the weights for the fiber part of the matrix
-        """
-        # XXX Need to replace SGD with another solver:
-        fiber_w = sgd.stochastic_gradient_descent(self.voxel_signal_demeaned,
-                                                  self.matrix[0],
-                                                  verbose=self.verbose)
-
-        return fiber_w
-
 
     @auto_attr
     def _fiber_predict(self):
@@ -385,16 +569,6 @@ class FiberModel(ReconstModel):
         # (n_bvecs * n_vox), so we broadcast across directions in each voxel:
         return (self.iso_weights[np.newaxis,...] +
                 np.zeros((len(self.b_idx), self.iso_weights.shape[0]))).T.ravel()
-
-
-
-
-def FiberFit(ReconstFit):
-    """
-    A fit of the LiFE model to diffusion data
-    """
-
-    def __init__(self, fiber_model, params)
 
     @auto_attr
     def predict(self):
--- dipy/tracking/tests/test_life.py
+++ dipy/tracking/tests/test_life.py
@@ -3,7 +3,8 @@ import os
 import numpy as np
 import numpy.testing as npt
 import numpy.testing.decorators as dec
-import scipy.io as sio
+import scipy.sparse as sps
+import scipy.linalg as la
 
 import nibabel as nib
 
@@ -13,6 +14,22 @@ import dipy.core.gradients as dpg
 from dipy.data import get_data
 
 
+import numpy as np
+import numpy.testing as npt
+
+
+def test_sgd():
+    # Set up the regression:
+    beta = np.random.rand(10)
+    X = np.random.randn(1000,10)
+    y = np.dot(X, beta)
+    beta_hat = life.sparse_sgd(y,X, plot=False, lamda=0)
+    beta_hat_sparse = life.sparse_sgd(y, sps.csr_matrix(X), plot=False, lamda=0)
+    # We should be able to get back the right answer for this simple case
+    npt.assert_array_almost_equal(beta, beta_hat, decimal=1)
+    npt.assert_array_almost_equal(beta, beta_hat_sparse, decimal=1)
+
+
 def test_sl_gradients():
     sl = [[1,2,3], [4,5,6], [5,6,7]]
     grads = np.array([[3,3,3], [2,2,2], [1,1,1]])
@@ -20,13 +37,28 @@ def test_sl_gradients():
 
 
 def test_sl_tensors():
-    # This is the response function, canonical tensor evals:
-    evals = [1.5, 0.5, 0.5]
-    # This streamline doesn't move on the z dimension
+    # Small streamline
     sl = [[1,2,3], [4,5,3], [5,6,3]]
-    # So the resulting tensor for each of the gradients should be simply a
-    # rotation of the original canonical tensor onto 45 degrees in the x,y plain
-    sl_tensors = life.sl_tensors(sl)
+    # Non-default eigenvalues:
+    evals=[0.0012, 0.0006, 0.0004]
+    sl_tensors = life.sl_tensors(sl, evals=evals)
+    # Get the resulting eigenvalues:
+    eigvals = la.eigvals(sl_tensors[0])
+    # la.eigvals returns things in a strange order, so reorder them:
+    idx = np.argsort(eigvals)[::-1]
+    # The eigenvalues are the same:
+    npt.assert_array_almost_equal(eigvals[idx], evals)
+    eigvecs = la.eig(sl_tensors[0])[1][idx]
+    # The rotation on the first vector is 45 degrees each
+    npt.assert_almost_equal(np.rad2deg(np.arccos(np.dot(eigvecs[0], [1, 0, 0]))),
+                            45)
+
+    npt.assert_almost_equal(np.rad2deg(np.arccos(np.dot(eigvecs[1], [0, 1, 0]))),
+                            135)
+
+    # The rotation on the last vector is 0 degrees:
+    npt.assert_almost_equal(np.rad2deg(np.arccos(np.dot(eigvecs[2], [0, 0, 1]))),
+                            0)
 
 
 def test_sl_signal():
@@ -38,6 +70,8 @@ def test_sl_signal():
 
     sl = [[[1,2,3], [4,5,3], [5,6,3], [6,7,3]],
           [[1,2,3], [4,5,3], [5,6,3]]]
+
+
     sig = [life.sl_signal(s, gtab, evals) for s in sl]
 
 
@@ -63,5 +97,28 @@ def test_FiberModel_init():
     sl = [[[1,2,3], [4,5,3], [5,6,3], [6,7,3]],
           [[1,2,3], [4,5,3], [5,6,3]]]
     affine = np.eye(4)
+    fiber_matrix, iso_matrix, vox_coord = FM1.model_setup(sl, affine)
 
-    mat = FM1.model_matrix(sl, affine)
+def test_FiberFit():
+
+    data_file, bval_file, bvec_file = get_data('small_64D')
+    data_ni = nib.load(data_file)
+    data = data_ni.get_data()
+    data_aff = data_ni.get_affine()
+    bvals, bvecs = (np.load(f) for f in (bval_file, bvec_file))
+    gtab = dpg.gradient_table(bvals, bvecs)
+    FM1 = life.FiberModel(gtab)
+
+    sl = [[[1,2,3], [4,5,3], [5,6,3], [6,7,3]],
+          [[1,2,3], [4,5,3], [5,6,3]]]
+    affine = np.eye(4)
+    fiber_matrix, iso_matrix, vox_coord = FM1.model_setup(sl, affine)
+
+    evals = [0.0015, 0.0005, 0.0005]
+
+    w = [0.5, 0.5]
+    sig = [w[i] * life.sl_signal(sl[i], gtab, evals) for i in range(len(sl))]
+    #data =
+    #life.fit()
+
+
