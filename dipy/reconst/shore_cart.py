cbae0a4e14edfc330d5cc08220bc0a6855221f04
Mauro Zucchelli <mauro.zucchelli88@gmail.com>
2014-10-13 18:17:47 +0200

    fix conflicts in __init__.py

--- dipy/reconst/shore_cart.py
+++ dipy/reconst/shore_cart.py
@@ -1,13 +1,13 @@
 import numpy as np
 from dipy.reconst.cache import Cache
 from dipy.reconst.multi_voxel import multi_voxel_fit
-from scipy.special import hermite
-from scipy.misc import factorial
+from scipy.special import hermite, gamma
+from scipy.misc import factorial, factorial2
 
 
 class ShoreCartModel(Cache):
 
-    def __init__(self, gtab, radial_order=6, mu=1, lamd=None):
+    def __init__(self, gtab, radial_order=6, mu=1, lambd=None):
 
         self.bvals = gtab.bvals
         self.bvecs = gtab.bvecs
@@ -25,10 +25,60 @@ class ShoreCartModel(Cache):
     def fit(self, data):
 
         # Generate the SHORE basis
-        M = self.cache_get('shore_matrix', key=self.gtab)
+        M = self.cache_get('shore_phi_matrix', key=self.gtab)
         if M is None:
-            M = shore_matrix(self.radial_order,  self.mu, self.gtab, self.tau)
-            self.cache_set('shore_matrix', self.gtab, M)
+            M = shore_phi_matrix(
+                self.radial_order,  self.mu, self.gtab, self.tau)
+            self.cache_set('shore_phi_matrix', self.gtab, M)
+
+        ind_mat = self.cache_get('shore_index_matrix', key=self.gtab)
+        if ind_mat is None:
+            ind_mat = shore_index_matrix(self.radial_order)
+            self.cache_set('shore_index_matrix', self.gtab, ind_mat)
+
+        pseudo_inv = np.linalg.pinv(M)
+        coef = np.dot(pseudo_inv, data)
+        return ShoreCartFit(self, coef)
+
+
+class ShoreCartFit():
+
+    def __init__(self, model, shore_coef):
+        """ Calculates diffusion properties for a single voxel
+
+        Parameters
+        ----------
+        model : object,
+            AnalyticalModel
+        shore_coef : 1d ndarray,
+            shore coefficients
+        """
+
+        self.model = model
+        self._shore_coef = shore_coef
+        self.gtab = model.gtab
+        self.radial_order = model.radial_order
+        self.mu = model.mu
+
+    @property
+    def shore_coeff(self):
+        """The SHORE coefficients
+        """
+        return self._shore_coef
+
+    def odf(self, sphere, smoment=0):
+        r""" Calculates the real analytical odf for a given discrete sphere.
+
+        Eq.32
+        """
+        I_s = self.model.cache_get('shore_odf_matrix', key=sphere)
+        if I_s is None:
+            I_s = shore_odf_matrix(self.radial_order,
+                                       self.mu, smoment, sphere.vertices)
+            self.model.cache_set('shore_odf_matrix', sphere, I_s)
+
+        odf = np.dot(I_s, self._shore_coef)
+        return odf
 
 
 def shore_index_matrix(radial_order):
@@ -37,7 +87,6 @@ def shore_index_matrix(radial_order):
     for n in range(0, radial_order + 1, 2):
         for i in range(0, n + 1):
             for j in range(0, n - i + 1):
-                #print(i, j, n - i - j)
                 index_matrix.append([i, j, n - i - j])
 
     return np.array(index_matrix)
@@ -83,7 +132,7 @@ def shore_psi_1d(n, x, mu):
     H = hermite(n)(x / mu)
     f = factorial(n)
 
-    k = 1/ (np.sqrt(2 ** (n+1) * np.pi * f) * mu)
+    k = 1 / (np.sqrt(2 ** (n + 1) * np.pi * f) * mu)
     psi = k * np.exp(- x ** 2 / (2 * mu ** 2)) * H
 
     return psi
@@ -105,7 +154,92 @@ def shore_psi_3d(n, r, mu):
     return psi(n1, x, mux) * psi(n2, y, muy) * psi(n3, z, muz)
 
 
+def shore_phi_matrix(radial_order, mu, gtab, tau):
 
-def shore_matrix(radial_order, mu, gtab, tau):
+    ind_mat = shore_index_matrix(radial_order)
 
+    qvals = np.sqrt(gtab.bvals / (4 * np.pi ** 2 * tau))
+    bvecs = gtab.bvecs
+
+    qgradients = qvals[:, None] * bvecs
+
+    np.savetxt('qgradients.txt', qgradients)
+
+    n_elem = ind_mat.shape[0]
+
+    n_qgrad = qgradients.shape[0]
+
+    M = np.zeros((n_qgrad, n_elem))
+
+    for i in range(n_qgrad):
+        for j in range(n_elem):
+            M[i, j] = shore_phi_3d(ind_mat[j], qgradients[i], mu)
+
+    return M
+
+
+def shore_odf_matrix(radial_order, mu, smoment, vertices):
+    """
+    Eq. 33 (choose ux=uy=uz)
+    """
+
+    ind_mat = shore_index_matrix(radial_order)
+
+    n_vert = vertices.shape[0]
+
+    n_elem = ind_mat.shape[0]
+
+    odf_mat = np.zeros((n_vert, n_elem))
+
+    rho = mu
+
+    for i in range(n_vert):
+
+        vx, vy, vz = vertices[i]
+
+        for j in range(n_elem):
+
+            n1, n2, n3 = ind_mat[j]
+            f = np.sqrt(factorial(n1) * factorial(n2) * factorial(n3))
+
+            k = mu ** (smoment) / np.sqrt(2 ** (2 - smoment) * np.pi ** 3)
+
+            odf_mat[i, j] = k * f * _odf_cfunc(n1, n2, n3, vx, vy, vz, smoment)
+
+    return odf_mat
+
+
+def _odf_cfunc(n1, n2, n3, vx, vy, vz, smoment):
+    """
+    Eq. 34
+    """
+
+    f = factorial
+    f2 = factorial2
+
+    sumc = 0
+    for i in range(0, n1 + 1, 2):
+        for j in range(0, n2 + 1, 2):
+            for k in range(0, n3 + 1, 2):
+
+                nn = n1 + n2 + n3 - i - j - k
+
+                gam = (-1) ** ((i + j + k) / 2) * gamma((3 + smoment + nn) / 2)
+
+                num1 =  vx  ** (n1 - i)
+
+                num2 = vy  ** (n2 - j)
+
+                num3 = vz  ** (n3 - k)
+
+                num = 2 ** (nn) * num1 * num2 * num3
+
+                denom = f(n1 - i) * f(n2 - j) * f(n3 - k) * f2(i) * f2(j) * f2(k)
+
+                sumc += (gam * num) / denom
+
+    return sumc
+
+
+def laplacian_regularization(radial_order, mu):
     pass
--- dipy/reconst/tests/test_shore_odf.py
+++ dipy/reconst/tests/test_shore_odf.py
@@ -8,10 +8,24 @@ from numpy.testing import (assert_equal,
                            run_module_suite,
                            assert_array_equal,
                            assert_raises)
+<<<<<<< HEAD:dipy/reconst/tests/test_shore_odf.py
 from dipy.sims.voxel import SticksAndBall
 from dipy.core.subdivide_octahedron import create_unit_sphere
 from dipy.core.sphere_stats import angular_similarity
 from dipy.reconst.tests.test_dsi import sticks_and_ball_dummies
+=======
+from dipy.sims.voxel import (SticksAndBall, multi_tensor, all_tensor_evecs,
+                             multi_tensor_odf)
+from dipy.core.subdivide_octahedron import create_unit_sphere
+from dipy.core.sphere_stats import angular_similarity
+from dipy.reconst.tests.test_dsi import sticks_and_ball_dummies
+import nibabel as nib
+from dipy.reconst.shore_cart import (shore_index_matrix, shore_phi_1d,
+                                     shore_phi_3d, shore_psi_1d,
+                                     shore_psi_3d, shore_phi_matrix,
+                                     ShoreCartModel, ShoreCartFit)
+from dipy.io.gradients import read_bvals_bvecs
+>>>>>>> We can create create cartesian shore odfs even without regularization:dipy/reconst/tests/test_shore.py
 
 
 def test_shore_odf():
@@ -73,5 +87,128 @@ def test_multivox_shore():
     assert_equal(np.alltrue(np.isreal(c_shore)), True)
 
 
+<<<<<<< HEAD:dipy/reconst/tests/test_shore_odf.py
 if __name__ == '__main__':
     run_module_suite()
+=======
+def check_shore_index_size(radial_order):
+    f = np.floor(radial_order / 2)
+    return (f + 1) * (f + 2) * (4 * f + 3) / 6
+
+
+def test_shore_cart():
+
+    mat = shore_index_matrix(16)
+    assert_equal(mat.shape[0], check_shore_index_size(16))
+
+    phi = shore_phi_1d(50, 1.534, 0.001)
+
+    assert_almost_equal(0.333504, np.real(phi), 6)
+
+    phi3d = shore_phi_3d((3, 2, 9), (1.2, -2.35, 0.067),.004)
+
+    assert_almost_equal(-0.000136642, phi3d, 8)
+
+    psi = shore_psi_1d(2, .3, 0.32)
+
+    assert_almost_equal(0.430482, psi, 6)
+
+    psi3d = shore_psi_3d((4, 1, 5), (1.3, -2.5, 0.001), (0.3, 0.4, 0.5))
+
+    assert_almost_equal(-2.42048e-12, psi3d, 5)
+
+
+def test_shore_cart_matrix():
+
+    fbvals, fbvecs = get_data('3shells_data')
+    bvals, bvecs = read_bvals_bvecs(fbvals, fbvecs)
+    gtab = gradient_table(bvals, bvecs)
+
+    mat = shore_index_matrix(2)
+
+    coeff = np.zeros(mat.shape[0])
+    coeff[0] = 1
+
+    zeta = 700.
+    mu = 1/ (2 * np.pi * np.sqrt(zeta))
+
+    pmat = shore_phi_matrix(radial_order=2, mu=mu, gtab=gtab,
+                            tau=1 / (4 * np.pi ** 2))
+
+    E = np.dot(pmat, coeff[:, None])
+    E = np.squeeze(E)
+
+    scm = ShoreCartModel(gtab, radial_order=2, mu=mu, lambd=None)
+
+    scf = scm.fit(E)
+
+    assert_almost_equal(scf.shore_coeff, coeff, 10)
+
+
+    mat = shore_index_matrix(4)
+
+    coeff = np.random.rand(mat.shape[0])
+
+    pmat = shore_phi_matrix(radial_order=4, mu=mu, gtab=gtab,
+                            tau=1 / (4 * np.pi ** 2))
+
+    E = np.dot(pmat, coeff[:, None])
+    E = np.squeeze(E)
+
+    scm = ShoreCartModel(gtab, radial_order=4, mu=mu, lambd=None)
+
+    scf = scm.fit(E)
+
+    assert_almost_equal(scf.shore_coeff, coeff, 4)
+
+    SNR = None
+    S0 = 1
+
+    mevals = np.array(([0.0015, 0.0003, 0.0003],
+                       [0.0015, 0.0003, 0.0003]))
+
+    data, sticks = multi_tensor(gtab, mevals, S0, angles=[(0, 0), (45, 0)],
+                             fractions=[50, 50], snr=SNR)
+
+    sphere = get_sphere('symmetric724')
+
+    mevecs = [all_tensor_evecs(sticks[0]).T,
+              all_tensor_evecs(sticks[1]).T]
+
+    odf_gt = multi_tensor_odf(sphere.vertices, [0.5, 0.5], mevals, mevecs)
+
+    scm = ShoreCartModel(gtab, radial_order=8, mu=mu, lambd=None)
+
+    scf = scm.fit(data)
+
+    odf = scf.odf(sphere, smoment=4)
+
+    sm = ShoreModel(gtab, radial_order=6, zeta=700)
+
+    smf = sm.fit(data)
+
+    odf2 = smf.odf(sphere)
+
+
+
+    #assert_array_almost_equal(odf, odf_gt, 4)
+
+    from dipy.viz import fvtk
+
+    ren = fvtk.ren()
+
+    odfs = np.zeros((3, 1, 1, sphere.vertices.shape[0]))
+    odfs[0, 0, 0] = odf_gt
+    odfs[1, 0, 0] = odf
+    odfs[2, 0, 0] = odf2
+
+    fvtk.add(ren, fvtk.sphere_funcs(odfs, sphere))
+    fvtk.show(ren)
+
+
+
+
+if __name__ == '__main__':
+    # run_module_suite()
+    test_shore_cart_matrix()
+>>>>>>> We can create create cartesian shore odfs even without regularization:dipy/reconst/tests/test_shore.py
