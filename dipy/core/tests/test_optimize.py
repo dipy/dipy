29d01f342eb2d77984f519912fece967bc2e03db
arokem <arokem@gmail.com>
2014-11-22 11:51:40 -0800

    BF: This has to come from `npt` namespace.

--- dipy/core/tests/test_optimize.py
+++ dipy/core/tests/test_optimize.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse as sps
+
 from numpy.testing import (assert_equal,
                            assert_almost_equal,
                            assert_array_almost_equal,
