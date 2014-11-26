049141a6a7c563df9e17fe73dc998b8dcad42e38
arokem <arokem@gmail.com>
2014-11-25 20:36:55 -0800

    BF: For some (older?) versions of python, take care of cases in which inputs are nans (?).

--- dipy/core/optimize.py
+++ dipy/core/optimize.py
@@ -453,6 +453,9 @@ class NNLS(SKLearnLinearSolver):
         ----------
 
         """
-        coef, rnorm = opt.nnls(X, y)
-        self.coef_ = coef
+        if np.any(np.isnan(X)) or np.any(np.isnan(y)):
+            self.coef_ = np.zeros(X.shape[-1])
+        else:
+            coef, rnorm = opt.nnls(X, y)
+            self.coef_ = coef
         return self
