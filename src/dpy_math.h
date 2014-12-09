5682f4fb0e58a590db7a2f5546cf681d788a0394
omarocegueda <jomaroceguedag@gmail.com>
2014-12-08 22:09:37 -0600

    BF: round to nearest even

--- src/dpy_math.h
+++ src/dpy_math.h
@@ -7,7 +7,6 @@
 #include "numpy/npy_math.h"
 
 #define dpy_isnan npy_isnan
-#define dpy_log2 npy_log2
 #define DPY_PI NPY_PI
 
 /* From numpy npy_math.c.src */
@@ -16,6 +15,32 @@ double dpy_log2(double x)
 {
     return NPY_LOG2E*log(x);
 }
+#else
+#define dpy_log2 npy_log2
 #endif
 
-#define dpy_round(x) floor((x) + 0.5)
+/* From numpy npy_math.c.src */
+#ifndef HAVE_RINT
+double dpy_rint(double x)
+{
+    double y, r;
+
+    y = npy_floor(x);
+    r = x - y;
+
+    if (r > 0.5) {
+        y += 1.0;
+    }
+
+    /* Round to nearest even */
+    if (r == 0.5) {
+        r = y - 2.0*npy_floor(0.5*y);
+        if (r == 1.0) {
+            y += 1.0;
+        }
+    }
+    return y;
+}
+#else
+#define dpy_rint npy_rint
+#endif
