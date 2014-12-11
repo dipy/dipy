a5b55ca732787b919107653cada3f7ff317e3069
omarocegueda <jomaroceguedag@gmail.com>
2014-12-10 21:38:11 -0600

    BF: use npy_* functions directly instead of defining our own dpy_* versions

--- src/dpy_math.h
+++ src/dpy_math.h
@@ -6,42 +6,17 @@
 #include <math.h>
 #include "numpy/npy_math.h"
 
-#define dpy_isnan npy_isnan
-#define dpy_signbit npy_signbit
 #define DPY_PI NPY_PI
 
-/* From numpy npy_math.c.src */
-#ifndef HAVE_LOG2
-double dpy_log2(double x)
-{
-    return NPY_LOG2E*log(x);
-}
-#else
-#define dpy_log2 npy_log2
+/* If HAVE_LOG2 is not defined, then the replacement
+'npy_log2' was defined, and we can safely link to it,
+otherwise, we can simply call log2*/
+#if 0
+#ifdef HAVE_LOG2
+#define npy_log2 log2
 #endif
 
-/* From numpy npy_math.c.src */
-#ifndef HAVE_RINT
-double dpy_rint(double x)
-{
-    double y, r;
-
-    y = npy_floor(x);
-    r = x - y;
-
-    if (r > 0.5) {
-        y += 1.0;
-    }
-
-    /* Round to nearest even */
-    if (r == 0.5) {
-        r = y - 2.0*npy_floor(0.5*y);
-        if (r == 1.0) {
-            y += 1.0;
-        }
-    }
-    return y;
-}
-#else
-#define dpy_rint npy_rint
+#ifdef HAVE_RINT
+#define npy_rint rint
+#endif
 #endif
