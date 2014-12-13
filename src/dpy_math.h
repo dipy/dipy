771b61162b6339f26cfe002347f0526786560379
omarocegueda <jomaroceguedag@gmail.com>
2014-12-12 21:19:36 -0600

    Use NPY_PI instead of DPY_PI

--- src/dpy_math.h
+++ src/dpy_math.h
@@ -5,18 +5,3 @@
 
 #include <math.h>
 #include "numpy/npy_math.h"
-
-#define DPY_PI NPY_PI
-
-/* If HAVE_LOG2 is not defined, then the replacement
-'npy_log2' was defined, and we can safely link to it,
-otherwise, we can simply call log2*/
-#if 0
-#ifdef HAVE_LOG2
-#define npy_log2 log2
-#endif
-
-#ifdef HAVE_RINT
-#define npy_rint rint
-#endif
-#endif
