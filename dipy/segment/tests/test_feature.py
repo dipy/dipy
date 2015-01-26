caa711a3b9c04b7e53920e899bb54ba0b1659673
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2015-01-25 19:56:46 -0500

    Do not use 'keepdims' of numpy.mean as it is not available in Numpy 1.6

--- dipy/segment/tests/test_feature.py
+++ dipy/segment/tests/test_feature.py
@@ -55,7 +55,7 @@ def test_feature_center_of_mass():
             return (1, streamline.shape[1])
 
         def extract(self, streamline):
-            return np.mean(streamline, axis=0, keepdims=True)
+            return np.mean(streamline, axis=0)[None, :]
 
     for feature in [dipymetric.CenterOfMassFeature(), CenterOfMass()]:
         for s in [s1, s2, s3, s4]:
@@ -65,7 +65,7 @@ def test_feature_center_of_mass():
             # Test method extract
             features = feature.extract(s)
             assert_equal(features.shape, (1, s.shape[1]))
-            assert_array_equal(features, np.mean(s, axis=0, keepdims=True))
+            assert_array_equal(features, np.mean(s, axis=0)[None, :])
 
         # This feature type is order invariant
         assert_true(feature.is_order_invariant)
