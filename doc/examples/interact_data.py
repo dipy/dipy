ac9e7905b8062732ea2d030ed3a6d2ba52e9e044
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-03-06 14:23:06 -0500

    Example now supports native img coords or world coords

--- doc/examples/interact_data.py
+++ doc/examples/interact_data.py
@@ -1,14 +1,18 @@
 import numpy as np
 from dipy.viz import actor, window, widget
 
+
 # Change with Stanford data
 dname = '/home/eleftherios/Data/Cunnane_Elef/08-111-609-AC15/work/'
 import nibabel as nib
 from nibabel import trackvis as tv
 
+world_coords = True
+
 img = nib.load(dname + 't1_brain_warp.nii.gz')
 data = img.get_data()
 affine = img.get_affine()
+
 
 streams, hdr = tv.read(dname + 'results/bundles/cst.right.trk',
                        points_space="rasmm")
@@ -22,9 +26,17 @@ streams, hdr = tv.read(dname + 'results/bundles/cc_1.trk',
                        points_space="rasmm")
 streamlines += [s[0] for s in streams]
 
+if not world_coords:
+    from dipy.tracking.streamline import transform_streamlines
+    streamlines = transform_streamlines(streamlines, np.linalg.inv(affine))
+
 renderer = window.renderer()
 stream_actor = actor.line(streamlines)
-slicer = actor.butcher(data, affine)
+
+if not world_coords:
+    slicer = actor.butcher(data, affine=np.eye(4))
+else:
+    slicer = actor.butcher(data, affine)
 
 window.add(renderer, stream_actor)
 window.add(renderer, slicer)
