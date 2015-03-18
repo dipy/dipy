b86a6f0d6fc7af98b04c914aebf68f901b226d9b
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-03-18 17:03:56 -0400

    Changed release to 0.9.1 because pypi didn't allow an update of 0.9.0

--- dipy/info.py
+++ dipy/info.py
@@ -8,7 +8,7 @@ docs.  In setup.py in particular, we exec this file, so it cannot import dipy
 # version
 _version_major = 0
 _version_minor = 9
-_version_micro = 0
+_version_micro = 1
 #_version_extra = 'dev'
 _version_extra = ''
 
