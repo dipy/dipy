77f08fa463285d6b965a163dc2d08effe6f470d8
Gabriel Girard <girard.gabriel@gmail.com>
2015-04-18 16:51:32 -0400

    NF - added multiprocessing in multi voxel fit.

--- dipy/reconst/tests/test_multi_voxel.py
+++ dipy/reconst/tests/test_multi_voxel.py
@@ -105,28 +105,29 @@ def test_CallableArray():
     npt.assert_array_equal(callarray(4), expected)
 
 
+class SillyModel(object):
+
+    @multi_voxel_fit
+    def fit(self, data, mask=None):
+        return SillyFit(self)
+
+
+class SillyFit(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    model_attr = 2.
+
+    def odf(self, sphere):
+        return np.ones(len(sphere.phi))
+
+    @property
+    def directions(self):
+        n = np.random.randint(0, 10)
+        return np.zeros((n, 3))
+
 def test_multi_voxel_fit():
-
-    class SillyModel(object):
-
-        @multi_voxel_fit
-        def fit(self, data, mask=None):
-            return SillyFit(model)
-
-    class SillyFit(object):
-
-        def __init__(self, model):
-            self.model = model
-
-        model_attr = 2.
-
-        def odf(self, sphere):
-            return np.ones(len(sphere.phi))
-
-        @property
-        def directions(self):
-            n = np.random.randint(0, 10)
-            return np.zeros((n, 3))
 
     # Test the single voxel case
     model = SillyModel()
@@ -148,6 +149,41 @@ def test_multi_voxel_fit():
     mask = np.eye(3).astype('bool')
     data = np.zeros((3, 3, 64))
     fit = model.fit(data, mask)
+    npt.assert_array_equal(fit.model_attr, np.eye(3) * 2)
+    odf = fit.odf(unit_icosahedron)
+    npt.assert_equal(odf.shape, (3, 3, 12))
+    npt.assert_array_equal(odf[~mask], 0)
+    npt.assert_array_equal(odf[mask], 1)
+
+    # Test fit.shape
+    npt.assert_equal(fit.shape, (3, 3))
+
+    # Test indexing into a fit
+    npt.assert_equal(type(fit[0, 0]), SillyFit)
+    npt.assert_equal(fit[:2, :2].shape, (2, 2))
+
+def test_multi_voxel_fit_parallel():
+
+    # Test the single voxel case
+    model = SillyModel()
+    single_voxel = np.zeros(64)
+    fit = model.fit(single_voxel, nbr_processes=4)
+    npt.assert_equal(type(fit), SillyFit)
+
+    # Test without a mask
+    many_voxels = np.zeros((2, 3, 4, 64))
+    fit = model.fit(many_voxels, nbr_processes=4)
+    expected = np.empty((2, 3, 4))
+    expected[:] = 2.
+    npt.assert_array_equal(fit.model_attr, expected)
+    expected = np.ones((2, 3, 4, 12))
+    npt.assert_array_equal(fit.odf(unit_icosahedron), expected)
+    npt.assert_equal(fit.directions.shape, (2, 3, 4))
+
+    # Test with a mask
+    mask = np.eye(3).astype('bool')
+    data = np.zeros((3, 3, 64))
+    fit = model.fit(data, mask, nbr_processes=4)
     npt.assert_array_equal(fit.model_attr, np.eye(3) * 2)
     odf = fit.odf(unit_icosahedron)
     npt.assert_equal(odf.shape, (3, 3, 12))
