bff6db2fe62d38a54f60336f8e988c55272d6fc8
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2015-05-05 15:07:25 -0400

    Brain extraction workflow and call script

--- dipy/workflow/segment.py
+++ dipy/workflow/segment.py
@@ -1,4 +1,44 @@
 from __future__ import division, print_function, absolute_import
 
-from warnings import warn
+from glob import glob
+from os.path import join, basename, splitext
 
+import nibabel as nib
+import numpy as np
+
+from dipy.segment.mask import median_otsu
+
+
+def median_otsu_bet(input_file, out_dir, save_masked=False, median_radius=4,
+                numpass=4, autocrop=False, vol_idx=None, dilate=None):
+
+    inputs = glob(input_file)
+    out_dirs = glob(out_dir)
+    if len(out_dirs) == 1:
+        out_dirs = [out_dirs] * len(inputs)
+
+    print(out_dirs)
+    #print(inputs, out_dirs)
+
+    for (fpath, out_dir_path) in zip(inputs, out_dirs):
+        img = nib.load(fpath)
+        volume = img.get_data()
+        masked, mask = median_otsu(volume, median_radius, numpass, autocrop,
+                                   vol_idx, dilate)
+
+        fname, ext = splitext(basename(fpath))
+        if(fname.endswith('.nii')):
+            fname, _ = splitext(fname)
+            ext = '.nii.gz'
+
+
+        mask_fname = fname + '_mask' + ext
+
+        mask_img = nib.Nifti1Image(mask.astype(np.float32), img.get_affine())
+        print(out_dir_path)
+        mask_img.to_filename(join(out_dir_path, mask_fname))
+
+        if save_masked:
+            masked_fname = fname + '_masked' + ext
+            masked_img = nib.Nifti1Image(masked, img.get_affine(), img.get_header())
+            masked_img.to_filename(join(out_dir_path, masked_fname))
