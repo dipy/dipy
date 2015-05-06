3c478c185cd05d7927a743341c9ead11b599d713
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-05-05 21:46:49 -0400

    BF: corrected problem with axes at VTK 6

--- dipy/viz/tests/test_fvtk_window.py
+++ dipy/viz/tests/test_fvtk_window.py
@@ -1,20 +1,18 @@
 import numpy as np
-from dipy.viz import actor
-from dipy.viz import window
-from dipy.viz import widget
+from dipy.viz import actor, window, fvtk
 from dipy.data import fetch_viz_icons, read_viz_icons
 import numpy.testing as npt
 
 
 @npt.dec.skipif(not actor.have_vtk)
 @npt.dec.skipif(not actor.have_vtk_colors)
-def test_window_module():
+def test_renderer():
 
     ren = window.Renderer()
 
     ren.background((1, 0.5, 0))
 
-    window.show(ren)
+    # window.show(ren)
 
     arr = window.snapshot(ren)
 
@@ -24,120 +22,27 @@ def test_window_module():
     npt.assert_equal(report.objects, 1)
     npt.assert_equal(report.colors_found, [True, False])
 
+    axes = fvtk.axes()
 
-@npt.dec.skipif(not actor.have_vtk)
-@npt.dec.skipif(not actor.have_vtk_colors)
-def test_slider_widget():
+    ren.add(axes)
 
-    renderer = window.renderer()
+    window.show(ren)
 
-    # Create 2 lines with 2 different colors
-    lines = [np.random.rand(10, 3), np.random.rand(20, 3)]
-    colors = np.array([[1., 0., 0.], [0.8, 0., 0.]])
-    c = actor.line(lines, colors, linewidth=3)
-    window.add(renderer, c)
+    ren.rm(axes)
 
-    from dipy.viz.window import vtk
+    window.show(ren)
 
-    ren_win = vtk.vtkRenderWindow()
-    ren_win.AddRenderer(renderer)
+    window.add(ren, axes)
 
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(ren_win)
+    window.show(ren)
 
-    def print_status(obj, event):
-        print(obj)
-        print(event)
-        renderer.SetBackground(np.random.rand(3))
+    ren.rm_all()
 
-    slider = widget.slider(iren=iren, callback=print_status)
-    # text = widget.text(iren, None)
+    window.show(ren)
 
-    iren.Initialize()
-
-    ren_win.Render()
-    iren.Start()
-    arr = window.snapshot(renderer, size=(600, 600))
-    report = window.analyze_snapshot(arr)
-
-
-@npt.dec.skipif(not actor.have_vtk)
-@npt.dec.skipif(not actor.have_vtk_colors)
-def test_button_widget():
-
-    from dipy.viz.window import vtk
-
-    renderer = window.renderer()
-
-    lines = [np.random.rand(10, 3), np.random.rand(20, 3)]
-    colors = np.array([[1., 0., 0.], [0.8, 0., 0.]])
-    stream_actor = actor.streamtube(lines, colors)
-
-    window.add(renderer, stream_actor)
-
-    renderer.ResetCamera()
-
-    ren_win = vtk.vtkRenderWindow()
-    ren_win.AddRenderer(renderer)
-    ren_win.SetSize(600, 600)
-
-    iren = vtk.vtkRenderWindowInteractor()
-    iren.SetRenderWindow(ren_win)
-
-    def callback(obj, event):
-        print(obj)
-        print('Pressed')
-
-    fetch_viz_icons()
-    button_png = read_viz_icons(fname='home3.png')
-
-    button = widget.button(iren, callback,
-                           button_png, (.8, 1.2), (50, 50))
-
-    button_png_plus = read_viz_icons(fname='plus.png')
-    button_plus = widget.button(iren, callback,
-                                button_png_plus, (.7, .8), (50, 50))
-
-    button_png_minus = read_viz_icons(fname='minus.png')
-    button_minus = widget.button(iren, callback,
-                                 button_png_minus, (.9, .8), (50, 50))
-
-    def print_status(obj, event):
-        print(obj)
-        print(event)
-        renderer.SetBackground(np.random.rand(3))
-
-    slider = widget.slider(iren=iren, callback=print_status)
-
-    iren.Initialize()
-
-    ren_win.Render()
-
-    button_norm_coords = (.9, 1.2)
-    button_size = (50, 50)
-
-    button.place(renderer)
-    button_plus.place(renderer)
-    button_minus.place(renderer)
-
-    def win_callback(obj, event):
-        # print(obj)
-        print(event)
-        print(obj.GetSize())
-
-        button.place(renderer)
-        button_plus.place(renderer)
-        button_minus.place(renderer)
-
-    ren_win.AddObserver(vtk.vtkCommand.ModifiedEvent, win_callback)
-
-    ren_win.Render()
-    iren.Start()
-
-    arr = window.snapshot(renderer, size=(600, 600))
 
 
 if __name__ == '__main__':
 
-    test_window_module()
+    test_renderer()
     # npt.run_module_suite()
