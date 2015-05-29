ffd5156c1695926c61b227bcc5544ed9de29b912
RafaelNH <rafaelnh21@gmail.com>
2015-05-29 17:34:52 +0100

    DOC: Figure directive corrected

--- doc/examples/simulate_dki.py
+++ doc/examples/simulate_dki.py
@@ -107,7 +107,7 @@ plt.plot(signal, label='noiseless')
 plt.plot(signal_noisy, label='with noise')
 plt.legend()
 plt.show()
-plt.savefig('simulated_signal.png')
+plt.savefig('simulated_dki_signal.png')
 
 
 """
