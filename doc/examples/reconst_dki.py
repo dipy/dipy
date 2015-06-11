2c8b6f96cff26f8d24621698d73ae9c5a3d4e187
RafaelNH <rafaelnh21@gmail.com>
2015-06-11 13:44:26 +0100

    NF: Selection of an appropriate dataset for DKI reconstruction and selection of valid b-values on DKI reconstruction example (doc.example.reconst_dki.py)

--- doc/examples/reconst_dki.py
+++ doc/examples/reconst_dki.py
@@ -71,44 +71,69 @@ import nibabel as nib
 voxel models from the raw data.
 """
 
-import dipy.reconst.dti as dti
+import dipy.reconst.dki as dki
 
 """
-``dipy.data`` is used for small datasets that we use in tests and examples.
+``dipy.data`` is used for small datasets that we use in tests and examples. DKI
+required multi shell data, i.e. data acquired from more than one non-zero
+b-value.
 """
 
-from dipy.data import fetch_stanford_hardi
+from dipy.data import fetch_sherbrooke_3shell
 
 """
-Fetch will download the raw dMRI dataset of a single subject. The size of the
-dataset is 87 MBytes.  You only need to fetch once.
+Fetch will download the raw HARDI dMRI dataset of a single subject. The size of
+the dataset is 188 MBytes, however you only need to fetch it once.
 """
 
-fetch_stanford_hardi()
+fetch_sherbrooke_3shell()
 
 """
 Next, we read the saved dataset
 """
 
-from dipy.data import read_stanford_hardi
+from dipy.data import read_sherbrooke_3shell
 
-img, gtab = read_stanford_hardi()
+img, gtab = read_sherbrooke_3shell()
 
 """
 img contains a nibabel Nifti1Image object (with the data) and gtab contains a
-GradientTable object (information about the gradients e.g. b-values and
-b-vectors).
+GradientTable object with information about the b-values and b-vectors. The
+b-values used on the loaded dataset are visualized above
 """
+
+import matplotlib.pyplot as plt
+
+plt.plot(gtab.bvals, label='b-values')
+plt.legend()
+plt.show()
+plt.savefig('HARDI193_bvalues.png')
+
+"""
+.. figure:: HARDI193_bvalues.png
+   :align: center
+   **b-values of the loaded dataset**.
+   
+From the figure above we can check that the loaded dataset containing three
+non-zero b-values as required for DKI. However the highest b-value of 3500
+$s.mm^{-2}$ is higher than normally used on DKI. Since DKI negletes diffusion
+signal components higher than the 4th order KT, a upper bound of b-value < 3000
+$s.mm^{-2}$ is normally implied to insure the fitting viability[Jensen2010]_. 
+Following this, we discard the b-value shell of 3500 $s.mm^{-2}$ before DKI
+fitting
+"""
+
+select_ind = gtab.bvals < 3000
+
+selected_bvals = gtab.bvals[select_ind]
+selected_bvecs = gtab.bvecs[select_ind, :]
 
 data = img.get_data()
-print('data.shape (%d, %d, %d, %d)' % data.shape)
+selected_data = data[:, :, :, select_ind]
 
 """
-data.shape ``(81, 106, 76, 160)``
-
-First of all, we mask and crop the data. This is a quick way to avoid
-calculating Tensors on the background of the image. This is done using dipy's
-mask module.
+Before fitting the data some data pre-processing is done. First, we mask and
+crop the data to avoid calculating Tensors on the background of the image.
 """
 
 from dipy.segment.mask import median_otsu
@@ -134,157 +159,7 @@ TensorModel in the following way:
 tenfit = tenmodel.fit(maskdata)
 
 """
-The fit method creates a TensorFit object which contains the fitting parameters
-and other attributes of the model. For example we can generate fractional
-anisotropy (FA) from the eigen-values of the tensor. FA is used to characterize
-the degree to which the distribution of diffusion in a voxel is
-directional. That is, whether there is relatively unrestricted diffusion in one
-particular direction.
-
-Mathematically, FA is defined as the normalized variance of the eigen-values of
-the tensor:
-
-.. math::
-
-        FA = \sqrt{\frac{1}{2}\frac{(\lambda_1-\lambda_2)^2+(\lambda_1-
-                    \lambda_3)^2+(\lambda_2-\lambda_3)^2}{\lambda_1^2+
-                    \lambda_2^2+\lambda_3^2}}
-
-Note that FA should be interperted carefully. It may be an indication of the
-density of packing of fibers in a voxel, and the amount of myelin wrapping these
-axons, but it is not always a measure of "tissue integrity". For example, FA
-may decrease in locations in which there is fanning of white matter fibers, or
-where more than one population of white matter fibers crosses.
-"""
-
-print('Computing anisotropy measures (FA, MD, RGB)')
-from dipy.reconst.dti import fractional_anisotropy, color_fa, lower_triangular
-
-FA = fractional_anisotropy(tenfit.evals)
-
-"""
-In the background of the image the fitting will not be accurate there is no
-signal and possibly we will find FA values with nans (not a number). We can
-easily remove these in the following way.
-"""
-
-FA[np.isnan(FA)] = 0
-
-"""
-Saving the FA images is very easy using nibabel. We need the FA volume and the
-affine matrix which transform the image's coordinates to the world coordinates.
-Here, we choose to save the FA in float32.
-"""
-
-fa_img = nib.Nifti1Image(FA.astype(np.float32), img.get_affine())
-nib.save(fa_img, 'tensor_fa.nii.gz')
-
-"""
-You can now see the result with any nifti viewer or check it slice by slice
-using matplotlib_'s imshow. In the same way you can save the eigen values, the
-eigen vectors or any other properties of the Tensor.
-"""
-
-evecs_img = nib.Nifti1Image(tenfit.evecs.astype(np.float32), img.get_affine())
-nib.save(evecs_img, 'tensor_evecs.nii.gz')
-
-"""
-Other tensor statistics can be calculated from the `tenfit` object. For example,
-a commonly calculated statistic is the mean diffusivity (MD). This is simply the
-mean of the  eigenvalues of the tensor. Since FA is a normalized
-measure of variance and MD is the mean, they are often used as complimentary
-measures. In `dipy`, there are two equivalent ways to calculate the mean
-diffusivity. One is by calling the `mean_diffusivity` module function on the
-eigen-values of the TensorFit class instance:
-"""
-
-MD1 = dti.mean_diffusivity(tenfit.evals)
-nib.save(nib.Nifti1Image(MD1.astype(np.float32), img.get_affine()), 'tensors_md.nii.gz')
-
-"""
-The other is to call the TensorFit class method:
-"""
-
-MD2 = tenfit.md
-
-"""
-Obviously, the quantities are identical.
-
-We can also compute the colored FA or RGB-map [Pajevic1999]_. First, we make sure
-that the FA is scaled between 0 and 1, we compute the RGB map and save it.
-"""
-
-FA = np.clip(FA, 0, 1)
-RGB = color_fa(FA, tenfit.evecs)
-nib.save(nib.Nifti1Image(np.array(255 * RGB, 'uint8'), img.get_affine()), 'tensor_rgb.nii.gz')
-
-"""
-Let's try to visualize the tensor ellipsoids of a small rectangular
-area in an axial slice of the splenium of the corpus callosum (CC).
-"""
-
-print('Computing tensor ellipsoids in a part of the splenium of the CC')
-
-from dipy.data import get_sphere
-sphere = get_sphere('symmetric724')
-
-from dipy.viz import fvtk
-ren = fvtk.ren()
-
-evals = tenfit.evals[13:43, 44:74, 28:29]
-evecs = tenfit.evecs[13:43, 44:74, 28:29]
-
-"""
-We can color the ellipsoids using the ``color_fa`` values that we calculated
-above. In this example we additionally normalize the values to increase the contrast.
-"""
-
-cfa = RGB[13:43, 44:74, 28:29]
-cfa /= cfa.max()
-
-fvtk.add(ren, fvtk.tensor(evals, evecs, cfa, sphere))
-
-print('Saving illustration as tensor_ellipsoids.png')
-fvtk.record(ren, n_frames=1, out_path='tensor_ellipsoids.png', size=(600, 600))
-
-"""
-.. figure:: tensor_ellipsoids.png
-   :align: center
-
-   **Tensor Ellipsoids**.
-"""
-
-fvtk.clear(ren)
-
-"""
-Finally, we can visualize the tensor orientation distribution functions
-for the same area as we did with the ellipsoids.
-"""
-
-tensor_odfs = tenmodel.fit(data[20:50, 55:85, 38:39]).odf(sphere)
-
-fvtk.add(ren, fvtk.sphere_funcs(tensor_odfs, sphere, colormap=None))
-#fvtk.show(r)
-print('Saving illustration as tensor_odfs.png')
-fvtk.record(ren, n_frames=1, out_path='tensor_odfs.png', size=(600, 600))
-
-"""
-.. figure:: tensor_odfs.png
-   :align: center
-
-   **Tensor ODFs**.
-
-Note that while the tensor model is an accurate and reliable model of the
-diffusion signal in the white matter, it has the drawback that it only has one
-principal diffusion direction. Therefore, in locations in the brain that
-contain multiple fiber populations crossing each other, the tensor model may
-indicate that the principal diffusion direction is intermediate to these
-directions. Therefore, using the principal diffusion direction for tracking in
-these locations may be misleading and may lead to errors in defining the
-tracks. Fortunately, other reconstruction methods can be used to represent the
-diffusion and fiber orientations in those locations. These are presented in
-other examples.
-
+References:
 
 .. [Jensen2005] Jensen JH, Helpern JA, Ramani A, Lu H, Kaczynski K (2005).
                 Diffusional Kurtosis Imaging: The Quantification of
