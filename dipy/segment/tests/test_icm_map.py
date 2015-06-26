b5bf3b73ffcf7aab11dad6e60041a4895e3b6774
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-06-26 14:51:33 -0400

    Updated test

--- dipy/segment/tests/test_icm_map.py
+++ dipy/segment/tests/test_icm_map.py
@@ -8,22 +8,28 @@ from dipy.core.ndindex import ndindex
 from dipy.segment.rois_stats import seg_stats
 from dipy.segment.energy_mrf import total_energy
 
-img = nib.load('/Users/jvillalo/Documents/GSoC_2015/Code/Data/3587_BL_T1_to_MNI_Linear_6p.nii.gz')
+# dname = '/Users/jvillalo/Documents/GSoC_2015/Code/Data/'
+dname = '/home/eleftherios/Dropbox/DIPY_GSoC_2015/'
+
+img = nib.load(dname + '3587_BL_T1_to_MNI_Linear_6p.nii.gz')
 dataimg = img.get_data()
 
-mask_image = nib.load('/Users/jvillalo/Documents/GSoC_2015/Code/Data/3587_mask.nii.gz')
+mask_image = nib.load(dname + '3587_mask.nii.gz')
 datamask = mask_image.get_data()
 
 masked_img = applymask(dataimg, datamask)
 print('masked_img.shape (%d, %d, %d)' % masked_img.shape)
 shape = masked_img.shape[:3]
 
-seg = nib.load('/Users/jvillalo/Documents/GSoC_2015/Code/Data/FAST/3587_BL_T1_to_MNI_Linear_6p_seg.nii.gz')
+seg = nib.load(dname + '3587_BL_T1_to_MNI_Linear_6p_seg.nii.gz')
 seg_init = seg.get_data()
 seg_init_masked = applymask(seg_init, datamask)
 
 print("computing the statistics of the ROIs (WM, GM, CSF)")
-seg_stats(masked_img, seg_init, 3)
+mu, std = seg_stats(masked_img, seg_init_masked, 3)
+
+print(mu)
+print(std)
 
 nclass = 3
 L = range(1, nclass+1)
@@ -43,7 +49,7 @@ while True:
         #print(idx)
         if not masked_img[idx]:
             continue
-        for l in range(0, nclass-3):
+        for l in range(0, nclass):
 
             totalE[l] = total_energy(masked_img, seg_init_masked, mu, var, idx, l, beta)
 
