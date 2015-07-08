6f8554f9defc974a8b15800068e2decd55061f90
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-07-08 16:56:12 -0400

    Replacing with map_coordinates

---
+++ dipy/viz/tests/test_fvtk_utils.py
@@ -0,0 +1,83 @@
+import numpy as np
+import numpy.testing as npt
+
+
+def trilinear_interp(input_array, indices):
+    """ Evaluate the input_array data at the given indices
+    """
+
+    if input_array.ndim <= 2 or input_array.ndim >= 5:
+        raise ValueError("Input array can only be 3d or 4d")
+
+    x_indices = indices[:, 0]
+    y_indices = indices[:, 1]
+    z_indices = indices[:, 2]
+
+    x0 = x_indices.astype(np.integer)
+    y0 = y_indices.astype(np.integer)
+    z0 = z_indices.astype(np.integer)
+    x1 = x0 + 1
+    y1 = y0 + 1
+    z1 = z0 + 1
+
+    # Check if xyz1 is beyond array boundary:
+    x1[np.where(x1 == input_array.shape[0])] = x0.max()
+    y1[np.where(y1 == input_array.shape[1])] = y0.max()
+    z1[np.where(z1 == input_array.shape[2])] = z0.max()
+
+    if input_array.ndim == 3:
+        x = x_indices - x0
+        y = y_indices - y0
+        z = z_indices - z0
+
+    elif input_array.ndim == 4:
+        x = np.expand_dims(x_indices - x0, axis=1)
+        y = np.expand_dims(y_indices - y0, axis=1)
+        z = np.expand_dims(z_indices - z0, axis=1)
+
+    output = (input_array[x0, y0, z0] * (1 - x) * (1 - y) * (1 - z) +
+              input_array[x1, y0, z0] * x * (1 - y) * (1 - z) +
+              input_array[x0, y1, z0] * (1 - x) * y * (1-z) +
+              input_array[x0, y0, z1] * (1 - x) * (1 - y) * z +
+              input_array[x1, y0, z1] * x * (1 - y) * z +
+              input_array[x0, y1, z1] * (1 - x) * y * z +
+              input_array[x1, y1, z0] * x * y * (1 - z) +
+              input_array[x1, y1, z1] * x * y * z)
+
+    return output
+
+
+def test_trilinear_interp():
+
+    A = np.zeros((5, 5, 5))
+    A[2, 2, 2] = 1
+
+    indices = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2], [1.5, 1.5, 1.5]])
+
+    values = trilinear_interp(A, indices)
+    print(values)
+
+    values2 = map_coordinates(A, indices.T, order=1)
+    print(values2)
+
+    npt.assert_almost_equal(values, values2)
+
+    B = np.zeros((5, 5, 5, 3))
+    B[2, 2, 2] = np.array([1, 1, 1])
+
+    values = trilinear_interp(B, indices)
+    print(values)
+
+    values_4d = []
+    for i in range(B.shape[-1]):
+        values_tmp = map_coordinates(B[..., i], indices.T, order=1)
+        values_4d.append(values_tmp)
+    values_4d = np.ascontiguousarray(np.array(values_4d).T)
+
+    print(values_4d)
+    npt.assert_almost_equal(values, values_4d)
+
+
+if __name__ == '__main__':
+
+    test_trilinear_interp()
--- dipy/viz/utils.py
+++ dipy/viz/utils.py
@@ -3,6 +3,7 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from dipy.core.ndindex import ndindex
+from scipy.ndimage import map_coordinates
 
 # Conditional import machinery for vtk
 from dipy.utils.optpkg import optional_package
@@ -129,47 +130,31 @@ def rotation_from_lines(lines, use_line_dir=True, use_full_eig=False):
 
 def trilinear_interp(input_array, indices):
     """ Evaluate the input_array data at the given indices
+
+    Parameters
+    ----------
+    input_array : ndarray,
+        3D or 4D array
+
+    Returns
+    -------
+    output : ndarray
+        1D or 2D array
     """
 
     if input_array.ndim <= 2 or input_array.ndim >= 5:
         raise ValueError("Input array can only be 3d or 4d")
 
-    x_indices = indices[:, 0]
-    y_indices = indices[:, 1]
-    z_indices = indices[:, 2]
-
-    x0 = x_indices.astype(np.integer)
-    y0 = y_indices.astype(np.integer)
-    z0 = z_indices.astype(np.integer)
-    x1 = x0 + 1
-    y1 = y0 + 1
-    z1 = z0 + 1
-
-    # Check if xyz1 is beyond array boundary:
-    x1[np.where(x1 == input_array.shape[0])] = x0.max()
-    y1[np.where(y1 == input_array.shape[1])] = y0.max()
-    z1[np.where(z1 == input_array.shape[2])] = z0.max()
-
     if input_array.ndim == 3:
-        x = x_indices - x0
-        y = y_indices - y0
-        z = z_indices - z0
+        return map_coordinates(input_array, indices.T, order=1)
 
-    elif input_array.ndim == 4:
-        x = np.expand_dims(x_indices - x0, axis=1)
-        y = np.expand_dims(y_indices - y0, axis=1)
-        z = np.expand_dims(z_indices - z0, axis=1)
-
-    output = (input_array[x0, y0, z0] * (1 - x) * (1 - y) * (1 - z) +
-              input_array[x1, y0, z0] * x * (1 - y) * (1 - z) +
-              input_array[x0, y1, z0] * (1 - x) * y * (1-z) +
-              input_array[x0, y0, z1] * (1 - x) * (1 - y) * z +
-              input_array[x1, y0, z1] * x * (1 - y) * z +
-              input_array[x0, y1, z1] * (1 - x) * y * z +
-              input_array[x1, y1, z0] * x * y * (1 - z) +
-              input_array[x1, y1, z1] * x * y * z)
-
-    return output
+    if input_array.ndim == 4:
+        values_4d = []
+        for i in range(input_array.shape[-1]):
+            values_tmp = map_coordinates(input_array[..., i],
+                                         indices.T, order=1)
+            values_4d.append(values_tmp)
+        return np.ascontiguousarray(np.array(values_4d).T)
 
 
 def rescale_to_uint8(data):
