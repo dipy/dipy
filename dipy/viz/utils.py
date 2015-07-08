e98db4f4c8f0954dd255068fe78486b2f3823bfb
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-07-08 18:49:48 -0400

    RF: removed unused function

--- dipy/viz/utils.py
+++ dipy/viz/utils.py
@@ -80,54 +80,6 @@ def evec_from_lines(lines, use_line_dir=True):
     return e_vec
 
 
-def rotation_from_lines(lines, use_line_dir=True, use_full_eig=False):
-    """ Get the rotation from lines directions in vtk.vtkTransform() object
-
-    if use_line_dir is set to False
-        only use the points position information
-
-    if use_full_eig is set to True
-        the rotation will be the full eigen_vector matrix
-        (not only the Yaw and Pitch)
-
-    Example
-    ----------
-    >>> camera = renderer.GetActiveCamera()
-    >>> rotation = rotation_from_lines(lines)
-    >>> camera.ApplyTransform(rotation)
-    >>> fvtk.show(renderer)
-    """
-    e_vec = evec_from_lines(lines, use_line_dir)
-
-    matrix = vtk.vtkMatrix4x4()
-
-    if use_full_eig:
-        for (i, j) in ndindex((3, 3)) :
-            matrix.SetElement(j, i, e_vec[i,j])
-
-    else:
-        v1 = e_vec[2]
-        v2 = np.array([0,0,1])
-        v3 = np.cross(v1,v2)
-        v3 = v3/(np.sqrt(np.sum(v3**2)))
-        v4 = np.cross(v3,v1)
-        v4 = v4/(np.sqrt(np.sum(v4**2)))
-
-        m1 = np.array([v1,v4,v3])
-        cos = np.dot(v2,v1)
-        sin = np.dot(v2,v4)
-        m2 = np.array([[cos,sin,0],[-sin,cos,0],[0,0,1]])
-
-        m = np.dot( np.dot(m1.T,m2), m1)
-
-        for (i, j) in ndindex((3, 3)) :
-            matrix.SetElement(i, j, m[i,j])
-
-    transform = vtk.vtkTransform()
-    transform.SetMatrix(matrix)
-    return transform
-
-
 def map_coordinates_3d_4d(input_array, indices):
     """ Evaluate the input_array data at the given indices
     using trilinear interpolation
