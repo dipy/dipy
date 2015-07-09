1a178b6ffd653791b03252a2ec8c3f6f31b9da7f
arokem <arokem@gmail.com>
2015-07-09 16:28:23 -0500

    RF: To avoid duplication of code, and circular imports, move dki_design_matrix
    Created a new utils.py module under the reconst namespace to put things like
    this.

--- dipy/reconst/dki.py
+++ dipy/reconst/dki.py
@@ -16,6 +16,7 @@ from dipy.reconst.dti import (TensorFit, fractional_anisotropy,
                               apparent_diffusion_coef, from_lower_triangular,
                               lower_triangular, decompose_tensor)
 
+from dipy.reconst.utils import dki_design_matrix as design_matrix
 from dipy.utils.six.moves import range
 from dipy.data import get_sphere
 from ..core.gradients import gradient_table
@@ -695,55 +696,6 @@ def split_dki_param(dki_params):
     kt = dki_params[..., 12:]
 
     return evals, evecs, kt
-
-
-def design_matrix(gtab):
-    r""" Constructs B design matrix for DKI
-
-    Parameters
-    ---------
-    gtab : GradientTable
-        Measurement directions.
-
-    Returns
-    -------
-    B : array (N, 22)
-        Design matrix or B matrix for the DKI model
-        B[j, :] = (Bxx, Bxy, Bzz, Bxz, Byz, Bzz,
-                   Bxxxx, Byyyy, Bzzzz, Bxxxy, Bxxxz,
-                   Bxyyy, Byyyz, Bxzzz, Byzzz, Bxxyy,
-                   Bxxzz, Byyzz, Bxxyz, Bxyyz, Bxyzz,
-                   BlogS0)
-    """
-    b = gtab.bvals
-    bvec = gtab.bvecs
-
-    B = np.zeros((len(b), 22))
-    B[:, 0] = -b * bvec[:, 0] * bvec[:, 0]
-    B[:, 1] = -2 * b * bvec[:, 0] * bvec[:, 1]
-    B[:, 2] = -b * bvec[:, 1] * bvec[:, 1]
-    B[:, 3] = -2 * b * bvec[:, 0] * bvec[:, 2]
-    B[:, 4] = -2 * b * bvec[:, 1] * bvec[:, 2]
-    B[:, 5] = -b * bvec[:, 2] * bvec[:, 2]
-    B[:, 6] = b * b * bvec[:, 0]**4 / 6
-    B[:, 7] = b * b * bvec[:, 1]**4 / 6
-    B[:, 8] = b * b * bvec[:, 2]**4 / 6
-    B[:, 9] = 4 * b * b * bvec[:, 0]**3 * bvec[:, 1] / 6
-    B[:, 10] = 4 * b * b * bvec[:, 0]**3 * bvec[:, 2] / 6
-    B[:, 11] = 4 * b * b * bvec[:, 1]**3 * bvec[:, 0] / 6
-    B[:, 12] = 4 * b * b * bvec[:, 1]**3 * bvec[:, 2] / 6
-    B[:, 13] = 4 * b * b * bvec[:, 2]**3 * bvec[:, 0] / 6
-    B[:, 14] = 4 * b * b * bvec[:, 2]**3 * bvec[:, 1] / 6
-    B[:, 15] = b * b * bvec[:, 0]**2 * bvec[:, 1]**2
-    B[:, 16] = b * b * bvec[:, 0]**2 * bvec[:, 2]**2
-    B[:, 17] = b * b * bvec[:, 1]**2 * bvec[:, 2]**2
-    B[:, 18] = 2 * b * b * bvec[:, 0]**2 * bvec[:, 1] * bvec[:, 2]
-    B[:, 19] = 2 * b * b * bvec[:, 1]**2 * bvec[:, 0] * bvec[:, 2]
-    B[:, 20] = 2 * b * b * bvec[:, 2]**2 * bvec[:, 0] * bvec[:, 1]
-    B[:, 21] = np.ones(len(b))
-
-    return B
-
 
 common_fit_methods = {'WLS': wls_fit_dki,
                       'OLS': ols_fit_dki,
--- dipy/sims/voxel.py
+++ dipy/sims/voxel.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy import dot
 from dipy.core.geometry import sphere2cart
 from dipy.core.geometry import vec2vec_rotmat
-
+from dipy.reconst.utils import dki_design_matrix
 
 # Diffusion coefficients for white matter tracts, in mm^2/s
 #
@@ -519,7 +519,7 @@ def DKI_signal(gtab, dt, kt, S0=150, snr=None):
     dt = np.array(dt)
     kt = np.array(kt)
 
-    A = design_matrix(gtab)
+    A = dki_design_matrix(gtab)
 
     # define vector of DKI parameters
     MD = (dt[0] + dt[2] + dt[5]) / 3
@@ -532,53 +532,6 @@ def DKI_signal(gtab, dt, kt, S0=150, snr=None):
 
     return S
 
-
-def design_matrix(gtab):
-    r""" Constructs B design matrix for DKI
-
-    Parameters
-    ---------
-    gtab : GradientTable
-        Measurement directions.
-
-    Returns
-    -------
-    design_matrix : array (N,22)
-          Design matrix or B matrix for the DKI model
-            design_matrix[j, :] = (Bxx, Bxy, Bzz, Bxz, Byz, Bzz,
-                                   Bxxxx, Byyyy, Bzzzz, Bxxxy, Bxxxz,
-                                   Bxyyy, Byyyz, Bxzzz, Byzzz, Bxxyy,
-                                   Bxxzz, Byyzz, Bxxyz, Bxyyz, Bxyzz,
-                                   BlogS0)
-    """
-    b = gtab.bvals
-    bvec = gtab.bvecs
-
-    B = np.zeros((len(b), 22))
-    B[:, 0] = -b * bvec[:, 0] * bvec[:, 0]
-    B[:, 1] = -2 * b * bvec[:, 0] * bvec[:, 1]
-    B[:, 2] = -b * bvec[:, 1] * bvec[:, 1]
-    B[:, 3] = -2 * b * bvec[:, 0] * bvec[:, 2]
-    B[:, 4] = -2 * b * bvec[:, 1] * bvec[:, 2]
-    B[:, 5] = -b * bvec[:, 2] * bvec[:, 2]
-    B[:, 6] = b * b * bvec[:, 0]**4 / 6
-    B[:, 7] = b * b * bvec[:, 1]**4 / 6
-    B[:, 8] = b * b * bvec[:, 2]**4 / 6
-    B[:, 9] = 4 * b * b * bvec[:, 0]**3 * bvec[:, 1] / 6
-    B[:, 10] = 4 * b * b * bvec[:, 0]**3 * bvec[:, 2] / 6
-    B[:, 11] = 4 * b * b * bvec[:, 1]**3 * bvec[:, 0] / 6
-    B[:, 12] = 4 * b * b * bvec[:, 1]**3 * bvec[:, 2] / 6
-    B[:, 13] = 4 * b * b * bvec[:, 2]**3 * bvec[:, 0] / 6
-    B[:, 14] = 4 * b * b * bvec[:, 2]**3 * bvec[:, 1] / 6
-    B[:, 15] = b * b * bvec[:, 0]**2 * bvec[:, 1]**2
-    B[:, 16] = b * b * bvec[:, 0]**2 * bvec[:, 2]**2
-    B[:, 17] = b * b * bvec[:, 1]**2 * bvec[:, 2]**2
-    B[:, 18] = 2 * b * b * bvec[:, 0]**2 * bvec[:, 1] * bvec[:, 2]
-    B[:, 19] = 2 * b * b * bvec[:, 1]**2 * bvec[:, 0] * bvec[:, 2]
-    B[:, 20] = 2 * b * b * bvec[:, 2]**2 * bvec[:, 0] * bvec[:, 1]
-    B[:, 21] = np.ones(len(b))
-
-    return B
 
 
 def single_tensor_odf(r, evals=None, evecs=None):
