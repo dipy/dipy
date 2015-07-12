a09d813649375dc159bcf6e1daf98ecc4cf4d980
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-07-12 16:02:26 -0400

    DOC: updated simple visualization example. Needs more text.

--- doc/examples/viz_slice.py
+++ doc/examples/viz_slice.py
@@ -1,57 +1,39 @@
 
 """
-==========================
-Simple image visualization
-==========================
+============================
+Simple volume visualization
+============================
+
+Here we present an example
+
 """
-import numpy as np
+import os
 import nibabel as nib
-from dipy.data import fetch_sherbrooke_3shell, read_sherbrooke_3shell
+from dipy.data import fetch_bundles_2_subjects
 from dipy.viz import window, actor
-from dipy.core.histeq import histeq
-from dipy.align.reslice import reslice
-
-#fetch_sherbrooke_3shell()
-#img, gtab = read_sherbrooke_3shell()
 
 
-fraw = '/home/eleftherios/Data/MPI_Elef/fa_1x1x1.nii.gz'
-fraw = '/home/eleftherios/Data/trento_processed/subj_01/MPRAGE_32/rawbet.nii.gz'
-fraw = '/home/eleftherios/Data/trento_processed/subj_01/MPRAGE_32/T1_flirt_out.nii.gz'
-# fraw = '/home/eleftherios/Data/Jorge_Rudas/tensor_fa.nii.gz'
-img = nib.load(fraw)
+fetch_bundles_2_subjects()
 
-# import nibabel as nib
+fname = os.path.join(os.path.expanduser('~'), '.dipy', 'exp_bundles_and_maps',
+                     'bundles_2_subjects', 'subj_1', 't1_warped.nii.gz')
 
+img = nib.load(fname)
 data = img.get_data()
 affine = img.get_affine()
 
-affine = np.dot(np.diag([2., 2., 2., 1]), affine)
-
-print(affine)
-print(data.shape)
-
-reslice = False
-
-if reslice:
-    zooms = img.get_header().get_zooms()[:3]
-    new_zooms = (2, 2, 2.)
-    data, affine = reslice(data, affine, zooms, new_zooms)
-
-    print(affine)
-    print(data.shape)
-
 renderer = window.Renderer()
 
-# S0 = data[..., 0]
+mean, std = data[data > 0].mean(), data[data > 0].std()
 
-vol = histeq(data)
+value_range = (mean - 0.5 * std, mean + 1.5 * std)
 
 world_coord = True
+
 if world_coord:
-    slice_actor = actor.slice(vol, affine)
+    slice_actor = actor.slice(data, affine, value_range)
 else:
-    slice_actor = actor.slice(vol)
+    slice_actor = actor.slice(data, value_range=value_range)
 
 renderer.add(slice_actor)
 
@@ -61,6 +43,6 @@ slice_actor2.display(slice_actor2.shape[0]/2, None, None)
 
 renderer.background((1, 1, 1))
 
-#renderer.add(slice_actor2)
+renderer.add(slice_actor2)
 
-window.show(renderer)
+window.show(renderer, size=(600, 600))
