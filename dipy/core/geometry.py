6f5e2ed0f89dd32f8929464b9622f7f45eb03213
RafaelNH <rafaelnh21@gmail.com>
2015-07-15 15:49:38 +0100

    DOC: Use the rst link syntax as suggsted by @arokem

--- dipy/core/geometry.py
+++ dipy/core/geometry.py
@@ -979,10 +979,8 @@ def perpendicular_directions(v, num=30, half=False):
         \frac{v_{y}v_{z}\sin(a_{i})+v_{x}\cos(a_{i})}
         {\sqrt{{v_{x}}^{2}+{v_{z}}^{2}}} \right  ]
 
-    For more details see:
-
-    http://gsoc2015dipydki.blogspot.it/2015/07/rnh-post-8-computing-perpendicul
-    ar.html
+    For more details `click here <http://gsoc2015dipydki.blogspot.it/2015/07/
+    rnh-post-8-computing-perpendicular.html>`_.
     """
     v = np.array(v, dtype=float)
 
