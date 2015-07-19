c90053470644b41afa40eff2c2926fcba4a30f96
arokem <arokem@gmail.com>
2015-07-18 18:04:54 -0700

    DOC: Deal with some non-unicode characters.

--- dipy/data/fetcher.py
+++ dipy/data/fetcher.py
@@ -578,13 +578,13 @@ mni_notes = \
 
     License for the MNI templates:
     -----------------------------
-    Copyright (C) 1993–2004 Louis Collins, McConnell Brain Imaging Centre,
+    Copyright (C) 1993-2004, Louis Collins McConnell Brain Imaging Centre,
     Montreal Neurological Institute, McGill University. Permission to use,
     copy, modify, and distribute this software and its documentation for any
     purpose and without fee is hereby granted, provided that the above
     copyright notice appear in all copies. The authors and McGill University
     make no representations about the suitability of this software for any
-    purpose. It is provided “as is” without express or implied warranty. The
+    purpose. It is provided "as is" without express or implied warranty. The
     authors are not responsible for any data loss, equipment damage, property
     loss, or injury to subjects or patients resulting from the use or misuse
     of this software package.
