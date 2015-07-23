c9e92b58e22498f34698a541b89c42e5e2f0417a
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-07-23 13:59:50 -0400

    DOC: adding visualization of multiple bundles

--- doc/examples/align_bundle_registration.py
+++ doc/examples/align_bundle_registration.py
@@ -1,39 +1,8 @@
 from dipy.align.streamlinear import StreamlineLinearRegistration
-from dipy.tracking.streamline import set_number_of_points
+from dipy.tracking.streamline import set_number_of_points, select_random_set_of_streamlines
 from dipy.viz import fvtk
 from dipy.data.fetcher import fetch_bundles_2_subjects, read_bundles_2_subjects
 
-fetch_bundles_2_subjects()
-
-"""
-The following function outputs a dictionary with the required bundles e.g. af
-left and maps, e.g. FA for a specific subject.
-"""
-
-subj1 = read_bundles_2_subjects('subj_1', ['fa', 't1'],
-                               ['af.left', 'cst.right', 'cc_1'])
-
-subj2 = read_bundles_2_subjects('subj_2', ['fa', 't1'],
-                               ['af.left', 'cst.right', 'cc_1'])
-
-
-"""
-We will use 3 bundles, FA and the affine transformation that brings the voxel
-cordinates to world coordinates (RAS 1mm).
-"""
-
-bundle1 = subj1['af.left']
-bundle2 = subj2['af.left']
-
-sbundle1 = set_number_of_points(bundle1, 20)
-sbundle2 = set_number_of_points(bundle2, 20)
-
-
-slr = StreamlineLinearRegistration(x0='affine')
-slm = slr.optimize(static=sbundle1, moving=sbundle2)
-
-
-sbundle2_moved = slm.transform(sbundle2)
 
 def show_both_bundles(bundles, colors=None, show=False, fname=None):
 
@@ -52,13 +21,38 @@ def show_both_bundles(bundles, colors=None, show=False, fname=None):
         fvtk.record(ren, n_frames=1, out_path=fname, size=(900, 900))
 
 
-show_both_bundles([sbundle1, sbundle2],
-                  colors=[fvtk.colors.orange, fvtk.colors.red],
-                  show=True,
-                  fname='before_registration.png')
+fetch_bundles_2_subjects()
+
+subj1 = read_bundles_2_subjects('subj_1', ['fa', 't1'],
+                               ['af.left', 'cst.right', 'cc_1'])
+
+subj2 = read_bundles_2_subjects('subj_2', ['fa', 't1'],
+                               ['af.left', 'cst.right', 'cc_1'])
 
 
-show_both_bundles([sbundle1, sbundle2_moved],
-                  colors=[fvtk.colors.orange, fvtk.colors.red],
-                  show=True,
-                  fname='after_registration.png')
+for bundle_type in ['af.left', 'cst.right', 'cc_1']:
+
+
+    bundle1 = subj1[bundle_type]
+    bundle2 = subj2[bundle_type]
+
+    sbundle1 = set_number_of_points(bundle1, 20)
+    sbundle2 = set_number_of_points(bundle2, 20)
+
+    sbundle1 = select_random_set_of_streamlines(sbundle1, 400)
+    sbundle2 = select_random_set_of_streamlines(sbundle2, 400)
+
+    slr = StreamlineLinearRegistration(x0='affine')
+    slm = slr.optimize(static=sbundle1, moving=sbundle2)
+
+    sbundle2_moved = slm.transform(sbundle2)
+
+
+    show_both_bundles([sbundle1, sbundle2],
+                      colors=[fvtk.colors.orange, fvtk.colors.red],
+                      show=True)
+
+
+    show_both_bundles([sbundle1, sbundle2_moved],
+                      colors=[fvtk.colors.orange, fvtk.colors.red],
+                      show=True)
