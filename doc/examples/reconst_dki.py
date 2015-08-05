8dbad437ff0457897f634de19fe2c2c522369b18
RafaelNH <rafaelnh21@gmail.com>
2015-08-05 19:48:50 +0100

    DOC: replace the Sherbrooke's data on DKI usage example by the HCP-like dataset

--- doc/examples/reconst_dki.py
+++ doc/examples/reconst_dki.py
@@ -7,12 +7,14 @@ The diffusion kurtois model is an expansion of the diffusion tensor model
 (see :ref:`example_reconst_dti`). In addition to the diffusion tensor (DT), the
 diffusion kurtosis model quantifies the degree to which water diffusion in 
 biologic tissues is non-Gaussian using the kurtosis tensor (KT) [Jensen2005]_.
-Measurements of non-Gaussian diffusion are of interest because they can be used
-to charaterize tissue microstructural heterogeneity [Jensen2010]_ and to derive
-concrete biophysical parameters as the density of axonal fibres and diffusion
-tortuosity [Fierem2011]_. Moreover, DKI can be used to resolve crossing fibers
-in tractography and to obtain invariant rotational measures not limited to well
-aligned fiber populations [NetoHe2015]_.
+
+Measurements of non-Gaussian diffusion by the diffusion kurtosis model are of
+interest because they can be used to charaterize tissue microstructural
+heterogeneity [Jensen2010]_ and to derive concrete biophysical parameters as
+the density of axonal fibres and diffusion tortuosity [Fierem2011]_.
+Moreover, DKI can be used to resolve crossing fibers in tractography and to
+obtain invariant rotational measures not limited to well aligned fiber
+populations [NetoHe2015]_.
 
 The diffusion kurtosis model express the diffusion-weighted signal as:
 
@@ -37,10 +39,10 @@ and
      K(n)=\frac{MD^{2}}{D(n)^{2}}\sum_{i=1}^{3}\sum_{j=1}^{3}\sum_{k=1}^{3}
      \sum_{l=1}^{3}n_{i}n_{j}n_{k}n_{l}W_{ijkl}
 
-where $D_{ij}$ and $W_{ijkl}$ are the elements of the second-order DT and the
-fourth-order KT tensors, respectively, and $MD$ is the mean diffusivity.
-As the DT, KT has antipodal symmetry and thus only 15 Wijkl elemments are
-needed to fully characterize the KT:
+where $D_{ij}$ are the elements of the second-order DT, and $W_{ijkl}$ the
+elements of the fourth-order KT and $MD$ is the mean diffusivity. As the DT,
+KT has antipodal symmetry and thus only 15 Wijkl elemments are needed to fully
+characterize the KT:
 
 .. math::
    \begin{matrix} ( & W_{xxxx} & W_{yyyy} & W_{zzzz} & W_{xxxy} & W_{xxxz}
@@ -58,91 +60,58 @@ First, we import all relevant modules:
 """
 
 import dipy.reconst.dki as dki
+import dipy.reconst.dti as dti
+import matplotlib.pyplot as plt
+from dipy.data import fetch_cenir_multib
+from dipy.data import read_cenir_multib
+from dipy.segment.mask import median_otsu
 
 """
 DKI requires multi-shell data, i.e. data acquired from more than one non-zero
-b-value. Here, we use fetch to download the raw HARDI of a dMRI dataset
-cotaining three non-zero b-values. The size of the dataset is 188 MBytes,
-however you only need to fetch it once.
+b-value. Here, we use fetch to download a multi-shell dataset which parameters
+are similar to the used on the Human Connectome Project (HCP). The total size
+of the dowloaded data is 188 MBytes, however you only need to fetch it once.
+Parameter ``with_raw`` of function ``fetch_cenir_multib`` is set to ``False``
+to only download eddy-current/motion corrected data:
 """
 
-from dipy.data import fetch_sherbrooke_3shell
-
-fetch_sherbrooke_3shell()
+fetch_cenir_multib(with_raw=False)
 
 """
-Next, we read the saved dataset
+Next, we read the saved dataset. To decrease the influence of diffusion signal 
+taylor approximation componets larger than the fourth order (componets not
+taken into account by the diffusion kurtosis tensor), we only select the
+b-values up to 2000 $s.mm^{-2}$:
 """
 
-from dipy.data import read_sherbrooke_3shell
+bvals=[200, 400, 1000, 2000]
 
-img, gtab = read_sherbrooke_3shell()
+img, gtab = read_cenir_multib(bvals)
 
 data = img.get_data()
 
 """
-img contains a nibabel Nifti1Image object (with the data) and gtab contains a
-GradientTable object with information about the b-values and b-vectors. The
-b-values used on the loaded dataset are visualized below
-"""
+Function ``read_cenir_multib`` return img and gtab which contains respectively
+a nibabel Nifti1Image object (where the data is extracted) and a GradientTable
+object with information about the b-values and b-vectors.
 
-import matplotlib.pyplot as plt
-
-plt.plot(gtab.bvals, label='b-values')
-fig1 = plt.gcf()
-plt.legend()
-plt.show()
-fig1.savefig('HARDI193_bvalues.png')
-
-"""
-.. figure:: HARDI193_bvalues.png
-   :align: center
-   **b-values of the loaded dataset**.
-
-From the figure above, we can check that the loaded dataset contains three
-non-zero b-values as required for DKI. However the highest b-value of 3500
-$s.mm^{-2}$ is higher than normally used on DKI. Since DKI neglects diffusion
-signal components higher than the 4th order KT, a upper bound of b-value < 3000
-$s.mm^{-2}$ is normally implied to decrease the influence of higher order terms
-[Jensen2010]_. Following this, we discard the b-value shell of 3500 $s.mm^{-2}$
-before DKI fitting.
-"""
-
-select_ind = gtab.bvals < 3000
-selected_bvals = gtab.bvals[select_ind]
-selected_bvecs = gtab.bvecs[select_ind, :]
-selected_data = data[:, :, :, select_ind]
-
-"""
-The selected b-values and gradient directions are then converted to Dipy's
-GradientTable format.
-"""
-
-from dipy.core.gradients import gradient_table
-
-gtab = gradient_table(selected_bvals, selected_bvecs)
-
-"""
 Before fitting the data some data pre-processing is done. First, we mask and
 crop the data to avoid calculating Tensors on the background of the image.
 """
 
-from dipy.segment.mask import median_otsu
-
-maskdata, mask = median_otsu(selected_data, 3, 1, True,
-                             vol_idx=range(10, 50), dilate=2)
+maskdata, mask = median_otsu(data, 3, 1, True, vol_idx=range(10, 50), dilate=2)
 
 """
-Now that we have prepared the datasets we can go forward with the voxel
-reconstruction. This can be done by first instantiate the DiffusinKurtosisModel
-in the following way.
+Now that we have loaded and prepared the datasets we can go forward with the
+voxel reconstruction. This can be done by first instantiate the
+DiffusinKurtosisModel in the following way:
 """
 
 dkimodel = dki.DiffusionKurtosisModel(gtab)
 
 """
-Fitting the data is very simple. We just need to call the fit method of the
-DiffusinKurtosisModel in the following way:
+To fitting the data using the defined ``dkimodel``, we just need to call the 
+fit function of the DiffusinKurtosisModel:
 """
 
 dkifit = dkimodel.fit(maskdata)
@@ -150,9 +119,11 @@ dkifit = dkimodel.fit(maskdata)
 """
 The fit method creates a DiffusionKurtosisFit object which contains all the
 diffusion and kurtosis fitting parameters and other DKI attributes. For
-instance, all diffusion standard tensor statistics can be computed from the
-DiffusionKurtosisFit instance as the fractional anisotropy (FA), the mean
-diffusivity (MD), the axial diffusivity (AD) and the radial diffusivity (RD).
+instance, since the diffusion kurtosis model also estimates the diffusion
+tensor, all diffusion standard tensor statistics can be computed from the
+DiffusionKurtosisFit instance. As example, we show below how to extract the
+fractional anisotropy (FA), the mean diffusivity (MD), the axial diffusivity
+(AD) and the radial diffusivity (RD) from the DiffusionKurtosisiFit instance.
 """
 
 FA = dkifit.fa
@@ -163,26 +134,11 @@ RD = dkifit.rd
 """
 Note that these four standard measures could also be computed from Dipy's DTI
 module. For comparison purposes, we calculate below the FA, MD, AD, and RD
-using Dipy's DTI module. For this, we first extract the data of the lower
-b-value to decrease the effects of the signal components lager than the 2nd
-order which are not taken into account by DTI.
+using Dipy's TensorModel.
 """
 
-select_dti_ind = gtab.bvals < 1500
-selected_dti_bvals = gtab.bvals[select_dti_ind]
-selected_dti_bvecs = gtab.bvecs[select_dti_ind, :]
-selected_dti_data = data[:, :, :, select_dti_ind]
-gtab_for_dti = gradient_table(selected_dti_bvals, selected_dti_bvecs)
-maskdata_for_dti, mask = median_otsu(selected_dti_data, 3, 1, True,
-                                     vol_idx=range(10, 50), dilate=2)
-
-""" Then, we carry on with the DTI fitting and standard tensor statistics
-by instantiating Dipy's TensorModel """
-
-import dipy.reconst.dti as dti
-
-tenmodel = dti.TensorModel(gtab_for_dti)
-tenfit = tenmodel.fit(maskdata_for_dti)
+tenmodel = dti.TensorModel(gtab)
+tenfit = tenmodel.fit(maskdata)
 
 dti_FA = tenfit.fa
 dti_MD = tenfit.md
@@ -191,38 +147,38 @@ dti_RD = tenfit.rd
 
 """
 The DT based measured obtain from DKI and DTI can be easly visualized using
-matplotlib. For example, we show below the middle axial slices of FA, MD, AD,
-and RD obtain from the DKI model (upper panels) and the DTI model (lower
-panels).
+matplotlib. For example, the FA, MD, AD, and RD obtain from the DKI model
+(upper panels) and the DTI model (lower panels). Images are ploted for an
+arbitary selected axial slices.
 """
 
-axial_middle = FA.shape[2] / 2
+axial_slice = 40
 
-fig2, ax = plt.subplots(2, 4, figsize=(12, 6),
+fig1, ax = plt.subplots(2, 4, figsize=(12, 6),
                         subplot_kw={'xticks': [], 'yticks': []})
 
-fig2.subplots_adjust(hspace=0.3, wspace=0.05)
+fig1.subplots_adjust(hspace=0.3, wspace=0.05)
 
-ax.flat[0].imshow(FA[:, :, axial_middle], cmap='gray')
+ax.flat[0].imshow(FA[:, :, axial_slice], cmap='gray')
 ax.flat[0].set_title('FA (DKI)')
-ax.flat[1].imshow(MD[:, :, axial_middle], cmap='gray')
+ax.flat[1].imshow(MD[:, :, axial_slice], cmap='gray')
 ax.flat[1].set_title('MD (DKI)')
-ax.flat[2].imshow(AD[:, :, axial_middle], cmap='gray')
+ax.flat[2].imshow(AD[:, :, axial_slice], cmap='gray')
 ax.flat[2].set_title('AD (DKI)')
-ax.flat[3].imshow(RD[:, :, axial_middle], cmap='gray')
+ax.flat[3].imshow(RD[:, :, axial_slice], cmap='gray')
 ax.flat[3].set_title('RD (DKI)')
 
-ax.flat[4].imshow(dti_FA[:, :, axial_middle], cmap='gray')
+ax.flat[4].imshow(dti_FA[:, :, axial_slice], cmap='gray')
 ax.flat[4].set_title('FA (DTI)')
-ax.flat[5].imshow(dti_MD[:, :, axial_middle], cmap='gray')
+ax.flat[5].imshow(dti_MD[:, :, axial_slice], cmap='gray')
 ax.flat[5].set_title('MD (DTI)')
-ax.flat[6].imshow(dti_AD[:, :, axial_middle], cmap='gray')
+ax.flat[6].imshow(dti_AD[:, :, axial_slice], cmap='gray')
 ax.flat[6].set_title('AD (DTI)')
-ax.flat[7].imshow(dti_RD[:, :, axial_middle], cmap='gray')
+ax.flat[7].imshow(dti_RD[:, :, axial_slice], cmap='gray')
 ax.flat[7].set_title('RD (DTI)')
 
 plt.show()
-fig2.savefig('Diffusion_tensor_measures_from_DTI_and_DKI.png')
+fig1.savefig('Diffusion_tensor_measures_from_DTI_and_DKI.png')
 
 """
 .. figure:: Diffusion_tensor_measures_from_DTI_and_DKI.png
@@ -230,6 +186,7 @@ fig2.savefig('Diffusion_tensor_measures_from_DTI_and_DKI.png')
    **Diffusion tensor measures obtain from the diffusion tensor estimated from
    DKI (upper panels) and DTI (lower panels).**.
 
+This paragraph have to be updated -----------------------------------------
 From the figure, we can see that the DT standard diffusion measures from DKI
 are noisier than the DTI measurements. This is a well known pitfall of DKI
 [NetoHe2014]_. Since it involves the estimation of a larger number of
@@ -246,20 +203,38 @@ MK = dkifit.mk
 RK = dkifit.rk
 AK = dkifit.ak
 
-fig3, ax = plt.subplots(1, 3, figsize=(12, 6),
+"""
+Kurtosis measures are very susceptible to outliers which corrupts the automatic
+visualization scale of the figure above. By setting a typical kurtosis value
+range of 0 and 2, we remove kurtosis measures outliers and reproduce the
+figures.
+"""
+
+MK[MK > 2] = 2
+MK[MK < 0] = 0
+AK[AK > 2] = 2
+AK[AK < 0] = 0
+RK[RK > 2] = 2
+RK[RK < 0] = 0
+
+"""
+We plot
+"""
+
+fig2, ax = plt.subplots(1, 3, figsize=(12, 6),
                         subplot_kw={'xticks': [], 'yticks': []})
 
-fig3.subplots_adjust(hspace=0.3, wspace=0.05)
+fig2.subplots_adjust(hspace=0.3, wspace=0.05)
 
-ax.flat[0].imshow(MK[:, :, axial_middle], cmap='gray')
+ax.flat[0].imshow(MK[:, :, axial_slice], cmap='gray')
 ax.flat[0].set_title('MK')
-ax.flat[1].imshow(RK[:, :, axial_middle], cmap='gray')
-ax.flat[1].set_title('RK')
-ax.flat[2].imshow(AK[:, :, axial_middle], cmap='gray')
-ax.flat[2].set_title('AK')
+ax.flat[2].imshow(RK[:, :, axial_slice], cmap='gray')
+ax.flat[2].set_title('RK')
+ax.flat[1].imshow(AK[:, :, axial_slice], cmap='gray')
+ax.flat[1].set_title('AK')
 
 plt.show()
-fig3.savefig('Kurtosis_standard_measures.png')
+fig2.savefig('Kurtosis_tensor_standard_measures.png')
 
 """
 .. figure:: Kurtosis_standard_measures.png
