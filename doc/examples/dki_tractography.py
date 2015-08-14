8628f9acf57d0dbcc759658a357be13379a664b1
RafaelNH <rafaelnh21@gmail.com>
2015-08-14 18:20:22 +0100

    DOC: First plot of the DKI-ODF in real data was added in the example

--- doc/examples/dki_tractography.py
+++ doc/examples/dki_tractography.py
@@ -30,8 +30,12 @@ account by the diffusion kurtosis model.
 """
 
 bvals = [200, 400, 1000, 2000]
+
 gtab, img = read_cenir_multib(bvals)
+
 data = img.get_data()
+
+affine = img.get_affine()
 
 """
 Having the acquition paramenters of the loaded data, we can define the
@@ -44,8 +48,8 @@ dkimodel = dki.DiffusionKurtosisModel(gtab)
 We first illustrate the 3D information provided by DKI from simulations. For
 this, based on the same GradientTable of the loaded data, we generate the
 signal of five voxels containing two fibers populations with increasing
-intersection angle and the same GradientTable of the loaded data (for more
-details on DKI simulates see :ref:`example_simulate_dki`):
+intersection angle (for more details on DKI simulates see
+:ref:`example_simulate_dki`):
 """
 
 # Defining the parameters that we mantain constant across the five voxels
@@ -76,8 +80,8 @@ DiffusinKurtosisModel: """
 dkifit = dkimodel.fit(signal_dki)
 
 """ To visualize the 3D information provide by the diffusion and kurtosis
-tensors, we compute and plot the apparent directional diffusivity and kurtosis
-values on 724 directions evenly sampled on a sphere. """
+tensors, we compute and plot the directional diffusivity and kurtosis values
+on 724 directions evenly sampled on a sphere. """
 
 sphere = get_sphere('symmetric724')
 
@@ -94,8 +98,8 @@ fvtk.add(ren, tensors_actor)
 
 """
 For the graphical presentation of the tensors estimated from DKI, we add the
-ground truth direction of fibers from the simulation paremeters. This is done
-using the following lines of code:
+ground truth direction of the fibers. This is done using the following lines of
+code:
 """
 
 # convert fiber directions ground truth angles to Cartesian coordinates
@@ -113,9 +117,12 @@ gt_dir_2copies = gt_dir_2copies.reshape((5, 2, 1, 2, 3), order='F')
 gt_peaks = fvtk.peaks(gt_dir_2copies, 1.05 * np.ones(gt_dir_2copies.shape))
 fvtk.add(ren, gt_peaks)
 
-""" Now we are ready to save the figure containing the tensor geometries """
+""" Now we are ready to save and show the figure containing the tensor
+geometries """
 
 fvtk.record(ren, out_path='geometry_of_dki_tensors.png', size=(1200, 1200))
+
+fvtk.show(ren, title='Geometry of DKI tensors', size=(500, 500))
 
 """ 
 .. figure:: geometry_of_DKI_tensors.png
@@ -139,8 +146,8 @@ from the DiffusionKurtosisFit object:
 
 ODF = dkifit.dki_odf(sphere)
 
-""" We plot below the DKI-ODF in the analogous way done for the apparent
-directional diffusivity and kurtosis values:
+""" We plot below the DKI-ODF in the analogous way done for the directional
+diffusivity and kurtosis values:
 """
 
 ren = fvtk.ren()
@@ -153,6 +160,8 @@ fvtk.add(ren, gt_peaks)
 
 fvtk.record(ren, out_path='DKI_ODF_geometry.png', size=(1200, 1200))
 
+fvtk.show(ren, title='DKI-ODF geometry', size=(500, 500))
+
 """ 
 .. figure:: DKI_ODF_geometry.png
    :align: center
@@ -161,31 +170,48 @@ fvtk.record(ren, out_path='DKI_ODF_geometry.png', size=(1200, 1200))
    between two simulated white matter fibers. Fiber ground truth are plotted in
    red **.
 
-We can see from figure that DKI-ODF peaks can be directly related to the ground
-truth directions. Following this, fiber direction can be estimated by finding
-the maxima of the DKI-ODF. Below we illustrate how this is done in real data
-and how this fiber estimates can be used on tractography.
+We can see from figure that DKI-ODF peaks are near to the fiber directions
+ground truth. In this way, we show that when the ground truth fibers directions
+are not known (the case of real brain data), these can estimated by finding
+the DKI-ODF maxima. Below we illustrate how this is done the HCP-like brain
+dataset.
 
 As mention in :ref:`example_reconst_dki`, diffusion kurtosis imaging requires
-some pre-processing to reduce the impact of signal artefacts. Robust procedures
-for this can be computationally expensive. To avoid reprocessing data, here we
-load the data previously denoised in :ref:`example_reconst_dki`
+that some pre-processing is done on real brain datasets to reduce the impact of
+signal artefacts. To avoid reprocessing data, here we load the data previously
+denoised in :ref:`example_reconst_dki`
 """
 
 img = nib.load('denoised_cenir_multib.nii.gz') 
-den = img.get_data()
-affine = img.get_affine()
 
-""" Now, we mask and crop the data to remove some of unnecessary background
-voxels:  
+den = img.get_data()
+
+""" Now, we mask the data to avoid processing unnecessary background voxels:  
 """
 
-maskdata, mask = median_otsu(den, 4, 2, True, vol_idx=[0, 1], dilate=1)
+maskdata, mask = median_otsu(den, 4, 2, False, vol_idx=[0, 1], dilate=1)
 
-""" To fit the diffusion kurtosis model, we just need to call again the fit
-funtion the of DiffusinKurtosisModel: """
+""" To fit the diffusion kurtosis model, we just need to call again the
+function fit of the DiffusinKurtosisModel. For illustration, we first show the
+DKI based ODF reconstructions in a small portion of the data. """
 
-dkifit = dkimodel.fit(maskdata)
+data_portion = maskdata[30:70, 50:51, 35:65]
+
+dkifit = dkimodel.fit(data_portion)
+
+"""
+Having fitted the diffusion kurtosis model, we are ready to compute and plot
+the DKI based ODFs
+"""
+
+dkiodf = dkifit.dki_odf(sphere)
+
+ren = fvtk.ren()
+odf_spheres = fvtk.sphere_funcs(dkiodf, sphere)
+odf_spheres.RotateX(-90)
+fvtk.add(ren, odf_spheres)
+fvtk.record(ren, out_path='dki_odfs.png')
+fvtk.show(ren)
 
 """
 References:
