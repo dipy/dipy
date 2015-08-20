f1b265f4bb8fde88156737237d0b649ea64f164f
Julio Villalon <neurobarranquilla@hotmail.com>
2015-08-19 17:16:05 -0700

    Removed unused files

--- dipy/segment/threshold.py
+++ dipy/segment/threshold.py
@@ -37,46 +37,4 @@ def otsu(image, nbins=256):
     idx = np.argmax(variance12)
     threshold = bin_centers[:-1][idx]
     return threshold
-    
-    def otsu_3(image, nbins=256):
-        
-            """
-    Return two threshold values based on Otsu's method.
-    It is intended to work on a masked T1 brain image.
-    It is intended to give threshold values for CSF/WM and WM/GM. 
-    
-    Copied from scikit-image to remove dependency.
 
-    Parameters
-    ----------
-    image : array
-        Input image.
-    nbins : int
-        Number of bins used to calculate histogram. This value is ignored for
-        integer arrays.
-
-    Returns
-    -------
-    threshold : float
-        Threshold values.
-    """
-
-    hist, bin_centers = np.histogram(image, nbins)
-    hist = hist.astype(np.float)
-
-    # class probabilities for all possible thresholds
-    weight1 = np.cumsum(hist)
-    weight2 = np.cumsum(hist[::-1])[::-1]
-
-    # class means for all possible thresholds
-    mean1 = np.cumsum(hist * bin_centers[1:]) / weight1
-    mean2 = (np.cumsum((hist * bin_centers[1:])[::-1]) / weight2[::-1])[::-1]
-
-    # Clip ends to align class 1 and class 2 variables:
-    # The last value of `weight1`/`mean1` should pair with zero values in
-    # `weight2`/`mean2`, which do not exist.
-    variance12 = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:])**2
-
-    idx = np.argmax(variance12)
-    threshold = bin_centers[:-1][idx]
-    return threshold
