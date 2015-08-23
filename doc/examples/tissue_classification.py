0bb49d2060ace6df5e6e0594a97a42dd8f2611c2
Julio Villalon <neurobarranquilla@hotmail.com>
2015-08-22 22:49:08 -0500

    Modified example file. Fixed figures and grammar

--- doc/examples/tissue_classification.py
+++ doc/examples/tissue_classification.py
@@ -15,16 +15,28 @@ and bias field corrected.
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
+from dipy.denoise.nlmeans import nlmeans
+from dipy.denoise.noise_estimate import estimate_sigma
+from dipy.data import fetch_tissue_data, read_tissue_data
 from dipy.segment.tissue import TissueClassifierHMRF
 
 """
 First we fetch the T1 volume from the Syn dataset and will determine its shape.
 """
 
-img = nib.load('/Users/jvillalo/.dipy/syn_test/t1_brain_denoised.nii')
-t1_img = img.get_data()
-print('t1_img.shape (%d, %d, %d)' % t1_img.shape)
-t1_img = np.interp(t1_img, [0, 400], [0, 1])
+fetch_tissue_data()
+t1_img = read_tissue_data()
+t1 = t1_img.get_data()
+print('t1.shape (%d, %d, %d)' % t1.shape)
+
+"""
+Once we have fetched the T1 volume, we proceed to denoise it with the NLM algorithm.
+"""
+
+t1 = np.interp(t1, [0, 400], [0, 1])
+sigma = estimate_sigma(t1)
+print(sigma)
+t1 = nlmeans(t1, sigma=sigma)
 
 """
 We will look at the axial and the coronal slices of the image.
@@ -32,16 +44,16 @@ We will look at the axial and the coronal slices of the image.
 
 fig = plt.figure()
 a = fig.add_subplot(1, 2, 1)
-img_ax = np.rot90(t1_img[..., 89])
+img_ax = np.rot90(t1[..., 89])
 imgplot = plt.imshow(img_ax, cmap="gray")
 a.axis('off')
 a.set_title('Axial')
 a = fig.add_subplot(1, 2, 2)
-img_cor = np.rot90(t1_img[:, 128, :])
+img_cor = np.rot90(t1[:, 128, :])
 imgplot = plt.imshow(img_cor, cmap="gray")
 a.axis('off')
 a.set_title('Coronal')
-plt.savefig('t1_image.png')
+plt.savefig('t1_image.png', bbox_inches='tight', pad_inches = 0)
 
 """
 .. figure:: t1_image.png
@@ -64,10 +76,10 @@ with values between 0 and 0.5
 beta = 0.1
 
 """
-Then we set the number of iterations. Most of the time between 10-20 iterations are enough to get a good segmentation.
+Then we set the number of iterations.
 """
 
-niter = 20
+niter = 30
 
 """
 Now we call an instace of the class TissueClassifierHMRF and its method
@@ -75,7 +87,7 @@ called classify and input the parameters defined above to perform the segmentati
 """
 
 hmrf = TissueClassifierHMRF()
-initial_segmentation, final_segmentation, PVE = hmrf.classify(t1_img,
+initial_segmentation, final_segmentation, PVE = hmrf.classify(t1,
                                                               nclass, beta,
                                                               niter)
                                                               
@@ -90,7 +102,7 @@ img_cor = np.rot90(final_segmentation[:, 128, :])
 imgplot = plt.imshow(img_cor)
 a.axis('off')
 a.set_title('Coronal')
-plt.savefig('final_seg.png')
+plt.savefig('final_seg.png', bbox_inches='tight', pad_inches = 0)
 
 """
 Now we plot the resulting segmentation.
@@ -100,6 +112,7 @@ Now we plot the resulting segmentation.
 
    **Each tissue class is color coded separately, red for the WM, yellow for the GM and light blue for the CSF**.
 
+And we will also have a look at the probability maps for each tissue class.
 """
 
 fig = plt.figure()
@@ -118,13 +131,12 @@ img_cor = np.rot90(PVE[:, :, 89, 2])
 imgplot = plt.imshow(img_cor, cmap="gray")
 a.axis('off')
 a.set_title('White Matter')
-plt.savefig('probabilities.png')
+plt.savefig('probabilities.png', bbox_inches='tight', pad_inches = 0)
 
 """
-And we will also have a look at the probability maps for each tissue class.
-
 .. figure:: probabilities.png
    :align: center
+   :scale: 120
 
    **These are the probability maps of each of the three tissue classes**.
 
