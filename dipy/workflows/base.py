447d6dbfa832c00ae3b33d71b917354918041881
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-09-01 21:57:47 -0400

    NF: the AugmentedParser can now introspect workflows and update arguments

--- dipy/workflows/base.py
+++ dipy/workflows/base.py
@@ -2,21 +2,24 @@ from dipy.fixes import argparse as arg
 import inspect
 
 
-class BaseArgumentParser(arg.ArgumentParser):
+class AugmentedArgumentParser(arg.ArgumentParser):
 
-    def set_workflow(self, workflow):
+    def add_workflow(self, workflow):
 
         specs = inspect.getargspec(workflow)
+        #print(specs)
+
         args = specs.args
         defaults = specs.defaults
+
 
         len_args = len(args)
         len_defaults = len(defaults)
 
-        print(len_args)
-        print(len_defaults)
+        #print(len_args)
+        #print(len_defaults)
 
-        # Arguments with no defaults
+        # Arguments with no defaults (Positional)
         cnt = 0
         for i in range(len_args - len_defaults):
 
@@ -25,8 +28,25 @@ class BaseArgumentParser(arg.ArgumentParser):
 
         # Arguments with defaults
         for i in range(cnt, len_args):
-            self.add_argument('--' + args[i])
+            self.add_argument('--' + args[i], help="ok")
+
+
+    def update_argument(self, *args, **kargs):
+
+        self.add_argument(*args, **kargs)
 
 
 
+#    def update_argument(self, option_string1, option_string2, dest,
+#                        nargs=None, const=None, default=None, type=None,
+#                        choices=None, help=None, metavar=None):
+#
+#        for i, act in enumerate(self._actions):
+#            act_tmp = act
+#            #self._actions[i] = arg._StoreAction(option_strings, dest,
+#            #                                    nargs, const, default, type,
+#            #                                    choices, help, metavar)
+#            self.add_argument(option_strings1, dest, nargs, const,
+#                              default, type,
+#                              choices, help, metavar)
 
