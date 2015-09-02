8351ef9d37b85d48de0361c379d71eca4d8c48d1
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-09-02 11:50:15 -0400

    Starting using numpydoc for parsing the docstrings

--- dipy/workflows/base.py
+++ dipy/workflows/base.py
@@ -1,23 +1,74 @@
 from dipy.fixes import argparse as arg
 import inspect
 
+try:
+    import numpydoc as ndoc
+    has_ndoc = True
+except ImportError:
+    print('Numpydoc is not installed. \n pip install numpydoc')
+    has_ndoc = False
 
-class AugmentedArgumentParser(arg.ArgumentParser):
+
+class IntrospectiveArgumentParser(arg.ArgumentParser):
+
+    def __init__(self, prog=None, usage=None, description=None, epilog=None,
+                 version=None, parents=[],
+                 formatter_class=arg.RawTextHelpFormatter,
+                 prefix_chars='-', fromfile_prefix_chars=None,
+                 argument_default=None, conflict_handler='resolve',
+                 add_help=True):
+
+        """ Augmenting the argument parser to allow automatic creation of
+        arguments from workflows
+
+        Parameters
+        -----------
+        prog : None
+            The name of the program (default: sys.argv[0])
+        usage : None
+            A usage message (default: auto-generated from arguments)
+        description : str
+            A description of what the program does
+        epilog : str
+            Text following the argument descriptions
+        version : None
+            Add a -v/--version option with the given version string
+        parents : list
+            Parsers whose arguments should be copied into this one
+        formatter_class : obj
+            HelpFormatter class for printing help messages
+        prefix_chars : str
+            Characters that prefix optional arguments
+        fromfile_prefix_chars : None
+            Characters that prefix files containing additional arguments
+        argument_default : None
+            The default value for all arguments
+        conflict_handler : str
+            String indicating how to handle conflicts
+        add_help : bool
+            Add a -h/-help option
+        """
+
+        iap = IntrospectiveArgumentParser
+        super(iap, self).__init__(prog, usage, description, epilog, version,
+                                  parents, formatter_class, prefix_chars,
+                                  fromfile_prefix_chars, argument_default,
+                                  conflict_handler, add_help)
 
     def add_workflow(self, workflow):
 
         specs = inspect.getargspec(workflow)
-        #print(specs)
+        doc = inspect.getdoc(workflow)
+
+        if has_ndoc:
+
+            self.doc = ndoc.docscrape.NumpyDocString(doc)
 
         args = specs.args
         defaults = specs.defaults
 
-
         len_args = len(args)
         len_defaults = len(defaults)
-
-        #print(len_args)
-        #print(len_defaults)
 
         # Arguments with no defaults (Positional)
         cnt = 0
@@ -30,23 +81,6 @@ class AugmentedArgumentParser(arg.ArgumentParser):
         for i in range(cnt, len_args):
             self.add_argument('--' + args[i], help="ok")
 
-
     def update_argument(self, *args, **kargs):
 
         self.add_argument(*args, **kargs)
-
-
-
-#    def update_argument(self, option_string1, option_string2, dest,
-#                        nargs=None, const=None, default=None, type=None,
-#                        choices=None, help=None, metavar=None):
-#
-#        for i, act in enumerate(self._actions):
-#            act_tmp = act
-#            #self._actions[i] = arg._StoreAction(option_strings, dest,
-#            #                                    nargs, const, default, type,
-#            #                                    choices, help, metavar)
-#            self.add_argument(option_strings1, dest, nargs, const,
-#                              default, type,
-#                              choices, help, metavar)
-
