52bfa1015d51ac5835909eb178caf9279530d666
arokem <arokem@gmail.com>
2015-09-25 11:57:03 -0700

    TST: Verify that output of estimate_sigma is a proper input to nlmeans.

---
+++ dipy/denoise/tests/test_denoise.py
@@ -0,0 +1,15 @@
+import numpy as np
+import numpy.testing as npt
+from dipy.denoise.noise_estimate import estimate_sigma
+from dipy.denoise.nlmeans import nlmeans
+import dipy.data as dpd
+import nibabel as nib
+
+def test_denoise():
+    """
+
+    """
+    fdata, fbval, fbvec = dpd.get_data()
+    data = nib.load(fdata).get_data()
+    sigma = estimate_sigma(data, N=4)
+    denoised = nlmeans(data, sigma=sigma)
