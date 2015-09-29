61ea5860856f3c48a8f9a094a655802cc919052c
arokem <arokem@gmail.com>
2015-09-29 07:50:09 -0400

    Smoke test the 3D version, as well as the 4D version.

--- dipy/denoise/tests/test_denoise.py
+++ dipy/denoise/tests/test_denoise.py
@@ -10,6 +10,12 @@ def test_denoise():
 
     """
     fdata, fbval, fbvec = dpd.get_data()
+    # Test on 4D image:
     data = nib.load(fdata).get_data()
+    sigma = estimate_sigma(data)
+    denoised = nlmeans(data, sigma=sigma)
+
+    # Test on 3D image:
+    data = data[..., 0]
     sigma = estimate_sigma(data)
     denoised = nlmeans(data, sigma=sigma)
