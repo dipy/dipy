2b404caae8d6424933f7888138bcf2bef90a06d7
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2015-10-15 14:32:42 -0400

    NF: disabling mesa offscreen by default

--- dipy/viz/window.py
+++ dipy/viz/window.py
@@ -681,7 +681,7 @@ def snapshot(ren, fname=None, size=(300, 300)):
 
     graphics_factory = vtk.vtkGraphicsFactory()
     graphics_factory.SetOffScreenOnlyMode(1)
-    graphics_factory.SetUseMesaClasses(1)
+    # graphics_factory.SetUseMesaClasses(1)
 
     render_window = vtk.vtkRenderWindow()
     render_window.SetOffScreenRendering(1)
