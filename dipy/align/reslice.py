4dc161e3b8ebdd5c62f2dd4fa8a176568a92659d
Dimitris Rozakis <dimrozakis@gmail.com>
2015-10-31 23:33:01 +0200

    Optimize dipy.align.reslice mem usage for 4Darrays

--- dipy/align/reslice.py
+++ dipy/align/reslice.py
@@ -56,22 +56,16 @@ def reslice(data, affine, zooms, new_zooms, order=1, mode='constant', cval=0):
     """
     R = np.array(new_zooms) / np.array(zooms)
     new_shape = np.array(zooms)/np.array(new_zooms) * np.array(data.shape[:3])
-    new_shape = np.round(new_shape).astype('i8')
+    new_shape = tuple(np.round(new_shape).astype('i8'))
     if data.ndim == 3:
-        data2 = affine_transform(input=data, matrix=R, offset=np.zeros(3,),
-                                 output_shape=tuple(new_shape),
+        data2 = affine_transform(input=data, matrix=R, output_shape=new_shape,
                                  order=order, mode=mode, cval=cval)
     if data.ndim == 4:
-        data2l=[]
+        data2 = np.zeros(new_shape+(data.shape[-1],), data.dtype)
         for i in range(data.shape[-1]):
-            tmp = affine_transform(input=data[..., i], matrix=R,
-                                   offset=np.zeros(3,),
-                                   output_shape=tuple(new_shape),
-                                   order=order, mode=mode, cval=cval)
-            data2l.append(tmp)
-        data2 = np.zeros(tmp.shape+(data.shape[-1],), data.dtype)
-        for i in range(data.shape[-1]):
-            data2[..., i] = data2l[i]
+            affine_transform(input=data[..., i], matrix=R,
+                             output=data2[..., i], output_shape=new_shape,
+                             order=order, mode=mode, cval=cval)
 
     Rx = np.eye(4)
     Rx[:3, :3] = np.diag(R)
