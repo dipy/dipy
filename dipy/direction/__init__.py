d7bf45a2f58ca6dbe059bc9400b39cbc01780b31
arokem <arokem@gmail.com>
2015-11-15 12:03:21 -0800

    BF: Tracking expects the name peaks_from_model to be available in directions.

--- dipy/direction/__init__.py
+++ dipy/direction/__init__.py
@@ -2,3 +2,4 @@
 from .probabilistic_direction_getter import (ProbabilisticDirectionGetter,
                                              DeterministicMaximumDirectionGetter)
 import peaks
+from peaks import peaks_from_model
