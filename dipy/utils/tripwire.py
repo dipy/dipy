c09851378e2e949a61f87d91757589c401aeee81
arokem <arokem@gmail.com>
2015-12-04 06:48:43 -0800

    BF: Tripwire subclass AttributeError.
    https://github.com/nipy/nibabel/pull/363/files

--- dipy/utils/tripwire.py
+++ dipy/utils/tripwire.py
@@ -1,7 +1,7 @@
 """ Class to raise error for missing modules or other misfortunes
 """
 
-class TripWireError(Exception):
+class TripWireError(AttributeError):
     """ Exception if trying to use TripWire object """
 
 
