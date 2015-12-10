c7c80b8429ab70ea3b4d17451eac9cd5020e68a8
omarocegueda <jomarocegueda@gmail.com>
2015-12-09 18:40:22 -0600

    TEST: relax imwarp tests. Instead of comparing the energy evolution, verify that the quality of the final registration

--- dipy/align/tests/test_imwarp.py
+++ dipy/align/tests/test_imwarp.py
@@ -417,23 +417,13 @@ def test_ssd_2d_demons():
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    subsampled_energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(subsampled_energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([312.6813333, 80.74625551, 49.43591374, 34.08871301,
-                      25.18286981, 17.78955273, 25.91334939, 20.16932281,
-                      43.86083145, 79.0966558 ])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([312.6813333, 98.17321941, 60.98300837, 47.75387157,
-                      34.11067498, 122.91901409, 19.75599298, 14.28763847,
-                      36.33599718, 88.62426913])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(subsampled_energy_profile,
-                              expected_profile, decimal=5)
+    assert(reduced > 0.9)
     assert_equal(optimizer.OPT_START_CALLED, 1)
     assert_equal(optimizer.OPT_END_CALLED, 1)
     assert_equal(optimizer.SCALE_START_CALLED, 1)
@@ -491,29 +481,75 @@ def test_ssd_2d_gauss_newton():
     mapping = optimizer.optimize(static, moving, id, id, id)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    subsampled_energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(subsampled_energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([312.68133316, 70.17782995, 21.38508088, 96.41054776,
-                      49.990781, 43.11867579, 24.53952718, 51.0786643,
-                      143.24848252, 150.48349573])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([312.68133316, 70.17782938, 21.26798507, 96.51765054,
-                      51.1495088, 37.86204803, 21.62425293, 49.44868302,
-                      121.6643917, 137.91427228])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(subsampled_energy_profile, expected_profile,
-                              decimal = 5)
+    assert(reduced > 0.9)
     assert_equal(optimizer.OPT_START_CALLED, 0)
     assert_equal(optimizer.OPT_END_CALLED, 0)
     assert_equal(optimizer.SCALE_START_CALLED, 0)
     assert_equal(optimizer.SCALE_END_CALLED, 0)
     assert_equal(optimizer.ITER_START_CALLED, 0)
     assert_equal(optimizer.ITER_END_CALLED, 0)
+
+
+def get_warped_stacked_image(image, nslices, b, m):
+    r""" Deform `image` with invertible field and for a 3D image with it
+
+    The image is deformed under an invertible field, and a 3D volume is
+    generated as follows:
+    the first and last `nslices`//3 slices are filled with zeros
+    to simulate background. The remaining middle slices are filled with
+    copies of the deformed `image` under the action of the invertible
+    field.
+
+    Parameters
+    ----------
+    image: 2d array shape(r, c)
+        the image to be deformed
+    nslices: int
+        the number of slices in the final volume
+    b, m : float
+        parameters of the harmonic field (as in [1]).
+
+    Returns
+    -------
+    vol : array shape(r, c, `nslices`)
+        the volumed generated using the undeformed image
+    wvol :
+        the volumed generated using the warped image
+
+    References
+    ----------
+    [1] Chen, M., Lu, W., Chen, Q., Ruchala, K. J., & Olivera, G. H. (2008).
+        A simple fixed-point approach to invert a deformation field.
+        Medical Physics, 35(1), 81. doi:10.1118/1.2816107
+    """
+    shape = image.shape
+    #create a synthetic invertible map and warp the circle
+    d, dinv = vfu.create_harmonic_fields_2d(shape[0], shape[1], b, m)
+    d = np.asarray(d, dtype=floating)
+    dinv = np.asarray(dinv, dtype=floating)
+    mapping = DiffeomorphicMap(2, shape)
+    mapping.forward, mapping.backward = d, dinv
+    wimage = mapping.transform(image)
+
+    if(nslices == 1):
+        return image, wimage
+
+    #normalize and form the 3d by piling slices
+    image = image.astype(floating)
+    image = (image-image.min())/(image.max() - image.min())
+    zero_slices = nslices // 3
+    vol = np.zeros(shape=image.shape + (nslices,))
+    vol[..., zero_slices:(2 * zero_slices)] = image[..., None]
+    wvol = np.zeros(shape=image.shape + (nslices,))
+    wvol[..., zero_slices:(2 * zero_slices)] = wimage[..., None]
+
+    return vol, wvol
 
 
 def get_synthetic_warped_circle(nslices):
@@ -574,8 +610,8 @@ def test_ssd_3d_demons():
                                           step_type=step_type)
 
     #Create the optimizer
-    level_iters = [10, 5]
-    step_length = 0.25
+    level_iters = [10, 10]
+    step_length = 0.1
     opt_tol = 1e-4
     inv_iter = 20
     inv_tol = 1e-3
@@ -586,22 +622,13 @@ def test_ssd_3d_demons():
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([312.22706987, 154.65556884, 53.88543188, 9.11484007,
-                      36.46592407, 13.20522299, 48.65663399, 14.91579802,
-                      49.82954704, 14.92646254])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([312.22706987, 154.65556885, 53.88455398, 9.11770682,
-                      36.48642824, 13.21706748, 48.67710635, 14.91782047,
-                      49.84142899, 14.92531294])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=4)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -630,8 +657,8 @@ def test_ssd_3d_gauss_newton():
     similarity_metric = metrics.SSDMetric(3, smooth, inner_iter, step_type)
 
     #Create the optimizer
-    level_iters = [10, 5]
-    step_length = 0.25
+    level_iters = [10, 10]
+    step_length = 0.1
     opt_tol = 1e-4
     inv_iter = 20
     inv_tol = 1e-3
@@ -642,22 +669,14 @@ def test_ssd_3d_gauss_newton():
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([348.3204721, 143.480757, 44.30003405, 8.73624842,
-                      3.13227203, 14.70806563, 6.48360268, 23.52491883,
-                      17.25669088, 48.99709064])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([348.3204721, 143.48075646, 44.30003413, 8.73624841,
-                      3.13227181, 14.70806845, 6.48360884, 23.52499421,
-                      17.25667176, 48.997691])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=4)
+    assert(reduced > 0.9)
+
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -669,37 +688,33 @@ def test_cc_2d():
     version of CC in 2D, and this test checks that the current energy profile
     matches the saved one.
     '''
+    fname = get_data('t1_coronal_slice')
+    nslices = 1
+    b = 0.1
+    m = 4
 
-    moving, static = get_synthetic_warped_circle(1)
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
+
     #Configure the metric
     sigma_diff = 3.0
     radius = 4
     metric = metrics.CCMetric(2, sigma_diff, radius)
 
     #Configure and run the Optimizer
-    level_iters = [10, 5]
+    level_iters = [15, 5]
     optimizer = imwarp.SymmetricDiffeomorphicRegistration(metric, level_iters)
     optimizer.verbosity = VerbosityLevels.DEBUG
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            [-681.02276193, -910.57721051, -1012.76781394, -1021.24181308,
-             -1016.97233745, -977.35458126, -1013.90114894, -989.04516449,
-             -1021.72431465, -988.46698723]
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            [-681.02276236, -920.57714783, -1008.82241171, -1021.91021701,
-             -994.86961164, -1026.52978164, -1015.83587405, -1020.02780802,
-             -993.8576053, -1026.4369566 ]
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    expected_profile = np.asarray(expected_profile)
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=5)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -718,20 +733,26 @@ def test_cc_3d():
     iterations. Any modification that produces a change in the energy profile
     should be carefully validated to ensure no accuracy loss.
     '''
-    moving, static = moving, static = get_synthetic_warped_circle(20)
+    fname = get_data('t1_coronal_slice')
+    nslices = 21
+    b = 0.1
+    m = 4
+
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
 
     #Create the CC metric
     sigma_diff = 2.0
-    radius = 4
+    radius = 2
     similarity_metric = metrics.CCMetric(3, sigma_diff, radius)
 
     #Create the optimizer
-    level_iters = [20, 10]
+    level_iters = [20, 5]
     step_length = 0.25
     opt_tol = 1e-4
     inv_iter = 20
     inv_tol = 1e-3
-    ss_sigma_factor = 0.5
+    ss_sigma_factor = 0.2
     optimizer = imwarp.SymmetricDiffeomorphicRegistration(similarity_metric,
         level_iters, step_length, ss_sigma_factor, opt_tol, inv_iter, inv_tol)
     optimizer.verbosity = VerbosityLevels.DEBUG
@@ -739,21 +760,13 @@ def test_cc_3d():
     mapping = optimizer.optimize(static, moving, None, None, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)*1e-4
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            [-0.17336006, -0.20516197, -0.20448353, -0.20630727, -0.20652892,
-             -0.2073403, -3.0046531, -3.43771429, -3.47262116, -3.51383381]
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            [-0.17136006, -0.20632291, -0.2038927, -0.20688352, -0.20821154,
-             -0.20909298, -0.20872891, -0.20933514, -3.06861497, -3.07851062]
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    expected_profile = np.asarray(expected_profile)
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=4)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -772,14 +785,16 @@ def test_em_3d_gauss_newton():
     that produces a change in the energy profile should be carefully validated
     to ensure no accuracy loss.
     '''
-    moving, static = get_synthetic_warped_circle(30)
-    moving[...,:8] = 0
-    moving[...,-1:-9:-1] = 0
-    static[...,:8] = 0
-    static[...,-1:-9:-1] = 0
+    fname = get_data('t1_coronal_slice')
+    nslices = 21
+    b = 0.1
+    m = 4
+
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
 
     #Create the EM metric
-    smooth=25.0
+    smooth=2.0
     inner_iter=20
     step_length=0.25
     q_levels=256
@@ -789,33 +804,24 @@ def test_em_3d_gauss_newton():
         3, smooth, inner_iter, q_levels, double_gradient, iter_type)
 
     #Create the optimizer
-    level_iters = [10, 5]
+    level_iters = [20, 5]
     opt_tol = 1e-4
     inv_iter = 20
     inv_tol = 1e-3
-    ss_sigma_factor = 0.5
+    ss_sigma_factor = 1.0
     optimizer = imwarp.SymmetricDiffeomorphicRegistration(similarity_metric,
         level_iters, step_length, ss_sigma_factor, opt_tol, inv_iter, inv_tol)
     optimizer.verbosity = VerbosityLevels.DEBUG
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([144.03694724, 63.06874155, 51.84694887, 39.6374044,
-                      31.84981429, 44.3778833, 37.84961761, 38.00509734,
-                      38.67423812, 38.47003306])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([144.03694724, 63.06874148, 51.84694881, 39.63740417,
-                      31.84981481, 44.37788414, 37.84961844, 38.00509881,
-                      38.67423954, 38.47003339])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=4)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -828,10 +834,16 @@ def test_em_2d_gauss_newton():
     matches the saved one.
     '''
 
-    moving, static = get_synthetic_warped_circle(1)
+    fname = get_data('t1_coronal_slice')
+    nslices = 1
+    b = 0.1
+    m = 4
+
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
 
     #Configure the metric
-    smooth=25.0
+    smooth=5.0
     inner_iter=20
     q_levels=256
     double_gradient=False
@@ -846,21 +858,13 @@ def test_em_2d_gauss_newton():
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            [2.50773392, 0.41762978, 0.30900322, 0.14818498, 0.44620725,
-             1.53134054, 1.42115728, 1.66358267, 1.184265, 46.13635772]
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            [2.50773392, 0.41763383, 0.30908578, 0.06241115, 0.11573476,
-             2.48475885, 1.10053769, 0.9270271, 49.37186785, 44.72643467]
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, np.array(expected_profile),
-                              decimal=5)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -879,14 +883,16 @@ def test_em_3d_demons():
     that produces a change in the energy profile should be carefully validated
     to ensure no accuracy loss.
     '''
-    moving, static = get_synthetic_warped_circle(30)
-    moving[...,:8] = 0
-    moving[...,-1:-9:-1] = 0
-    static[...,:8] = 0
-    static[...,-1:-9:-1] = 0
+    fname = get_data('t1_coronal_slice')
+    nslices = 21
+    b = 0.1
+    m = 4
+
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
 
     #Create the EM metric
-    smooth=25.0
+    smooth=2.0
     inner_iter=20
     step_length=0.25
     q_levels=256
@@ -896,33 +902,24 @@ def test_em_3d_demons():
         3, smooth, inner_iter, q_levels, double_gradient, iter_type)
 
     #Create the optimizer
-    level_iters = [10, 5]
+    level_iters = [20, 5]
     opt_tol = 1e-4
     inv_iter = 20
     inv_tol = 1e-3
-    ss_sigma_factor = 0.5
+    ss_sigma_factor = 1.0
     optimizer = imwarp.SymmetricDiffeomorphicRegistration(similarity_metric,
         level_iters, step_length, ss_sigma_factor, opt_tol, inv_iter, inv_tol)
     optimizer.verbosity = VerbosityLevels.DEBUG
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            np.array([144.03694708, 122.39512307, 111.31925381, 90.9100989,
-                      93.93705232, 104.22993997, 110.57817867, 140.45262039,
-                      133.87804571, 119.20794977])
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            np.array([144.03694708, 122.39512227, 111.31924572, 90.91010482,
-                      93.93707059, 104.22996918, 110.57822649, 140.45298465,
-                      133.87831302, 119.20826433])
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, expected_profile, decimal=4)
+    assert(reduced > 0.9)
 
 
 @npt.dec.skipif(NO_SSE2)
@@ -934,11 +931,16 @@ def test_em_2d_demons():
     version of EM in 2D, and this test checks that the current energy profile
     matches the saved one.
     '''
+    fname = get_data('t1_coronal_slice')
+    nslices = 1
+    b = 0.1
+    m = 4
 
-    moving, static = get_synthetic_warped_circle(1)
+    image = np.load(fname)
+    moving, static = get_warped_stacked_image(image, nslices, b, m)
 
     #Configure the metric
-    smooth=25.0
+    smooth=2.0
     inner_iter=20
     q_levels=256
     double_gradient=False
@@ -953,21 +955,14 @@ def test_em_2d_demons():
     mapping = optimizer.optimize(static, moving, None)
     m = optimizer.get_map()
     assert_equal(mapping, m)
-    energy_profile = subsample_profile(
-        optimizer.full_energy_profile, 10)
-    print(energy_profile)
 
-    if USING_VC_SSE2:
-        expected_profile = \
-            [2.50773393, 3.26942324, 1.81684393, 5.44878881, 40.0195918,
-             31.87030788, 25.15710409, 29.82206485, 196.33114499, 213.86419995]
-    elif USING_GCC_SSE2:
-        expected_profile = \
-            [2.50773393, 3.26942352, 1.8168445, 5.44879264, 40.01956373,
-             31.65616398, 32.43115903, 35.24130742, 192.89072697, 195.456909]
+    warped = mapping.transform(moving)
+    starting_energy = np.sum((static - moving)**2)
+    final_energy = np.sum((static - warped)**2)
+    reduced = 1.0 - final_energy/starting_energy
 
-    assert_array_almost_equal(energy_profile, np.array(expected_profile),
-                              decimal=5)
+    assert(reduced > 0.9)
+
 
 if __name__=='__main__':
     test_scale_space_exceptions()
