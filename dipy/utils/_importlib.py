465b9dd0593536653938cc58236b418f7b9ad4b4
Kenny Ho <hok@vcu.edu>
2016-02-07 07:04:10 -0500

    PEP8 utils
    pep8 utils done.

--- dipy/utils/_importlib.py
+++ dipy/utils/_importlib.py
@@ -3,6 +3,7 @@
 # code compatible with Python 2.3.
 import sys
 
+
 def _resolve_name(name, package, level):
     """Return the absolute name of the module to be imported."""
     if not hasattr(package, 'rindex'):
