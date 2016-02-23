corrected small bias in isotropic scaling estimation. removed copied code in tests.

dipy/reconst/mapmri.py
@@ -282,7 +282,8 @@ class MapmriModel(Cache):
                 coef = coef / sum(coef * self.Bm)
                 return MapmriFit(self, coef, self.mu, R, self.ind_mat, lopt)
             else:
-                mumean = np.sqrt(evals.mean() * 2 * self.tau)
+                mumean = isotropic_scale_factor(evals * 2 * self.tau)
+                #mumean = np.sqrt(evals.mean() * 2 * self.tau)
                 mu = np.array([mumean, mumean, mumean])
                 q = self.gtab.bvecs * qvals[:, None]
                 M_mu_dependent = mapmri_isotropic_M_mu_dependent(
@@ -587,6 +588,33 @@ class MapmriFit(ReconstFit):
 
         return EAP
 
+
+def sigma(t, eta):
+    r"""Function used in estimating propagator anisotropy _[1] Eq. (A.7)
+
+    References
+    ----------
+    .. _[1] Avram et al. "Clinical feasibility of using mean apparent
+    propagator (MAP) MRI to characterize brain tissue microstructure".
+    NeuroImage 2015, in press.
+    """
+    return t ** (3 * eta) / (1 - 3 * t ** eta + 3 * t ** (2 * eta))
+
+
+def isotropic_scale_factor(mu_squared):
+    r"""Estimated isotropic scaling factor _[1] Eq. (49)
+
+    References
+    ----------
+    .. [1] Ozarslan E. et. al, "Mean apparent propagator (MAP) MRI: A novel
+    diffusion imaging method for mapping tissue microstructure",
+    NeuroImage, 2013.
+    """
+    X, Y, Z = mu_squared
+    coef_array = np.array([-3, -(X + Y + Z), (X * Y + X * Z + Y * Z),
+                           3 * X * Y * Z])
+    u0 = np.sqrt(np.roots(coef_array).max())
+    return u0
 
 def mapmri_index_matrix(radial_order):
     r""" Calculates the indices for the MAPMRI [1]_ basis in x, y and z.