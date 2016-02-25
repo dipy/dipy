8e53d3943a47016c2e276f4e51b64d32b9f02175
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-02-25 14:47:19 -0500

    Deterministic tracking and TDI workflows

--- dipy/workflows/tract_metrics.py
+++ dipy/workflows/tract_metrics.py
@@ -1,5 +1,6 @@
-import os
 
+from __future__ import division, print_function
+import os
 from glob import glob
 
 import numpy as np
@@ -7,12 +8,36 @@ import nibabel as nib
 import tractconverter
 
 from dipy.tracking.utils import density_map
+from dipy.workflows.utils import choose_create_out_dir
 
-def tract_density(tract_files, ref_files, up_factor=1.0, out_dir=''):
-    for tract_file, ref_file in zip(glob(tract_files), glob(ref_files)):
+def tract_density_flow(tractograms, ref_files, out_dir='', up_factor=1.0,
+                       tdi='tdi.nii.gz'):
+    """ Workflow for tract density computation.
+
+    Parameters
+    ----------
+    tractograms : string
+        Path to the tractogram on which to perform tract density computation.
+        This path may contain wildcards to process multiple inputs at once.
+    ref_files : string
+        Path to the reference volumes. This path may contain wildcards to use
+        multiple masks at once.
+    out_dir : string, optional
+        Output directory (default input file directory)
+    up_factor : float, optional
+        Factor by which to upsample the resulting volume. (default 1.0)
+    tdi : string, optional
+        Tract density file name (default 'tdi.nii.gz')
+    Outputs
+    -------
+    tdi : Nifti file
+        Track density volume.
+    """
+    for tract_file, ref_file in zip(glob(tractograms), glob(ref_files)):
         print('Computing tract density for {0}'.format(tract_file))
         ref = nib.load(ref_file)
         ref_head = ref.get_header()
+        print(up_factor)
         pos_factor = ref_head['pixdim'][1:4] / up_factor
         data_shape = np.array(ref.shape) * up_factor
         data_shape = tuple(data_shape.astype('int32'))
@@ -20,10 +45,11 @@ def tract_density(tract_files, ref_files, up_factor=1.0, out_dir=''):
         tract_format = tractconverter.detect_format(tract_file)
         tract = tract_format(tract_file, anatFile=ref_file)
 
-        streamlines = [i for i in tract]
+        streamlines = [i * up_factor for i in tract]
 
+
+        # Need to fix scaling
         affine = np.eye(4)
-
         # Need to adjust the affine to take upsampling into account
         affine[0, 0] /= up_factor
         affine[1, 1] /= up_factor
@@ -36,17 +62,9 @@ def tract_density(tract_files, ref_files, up_factor=1.0, out_dir=''):
         if len(tdi_map.shape) > 3:
             pos_factor += [1]
 
-        if out_dir == '':
-            out_dir_path = os.path.dirname(ref_file)
-        elif not os.path.isabs(out_dir):
-            out_dir_path = os.path.join(os.path.dirname(ref_file), out_dir)
-            if not os.path.exists(out_dir_path):
-                os.makedirs(out_dir_path)
-        else:
-            out_dir_path = out_dir
+        out_dir_path = choose_create_out_dir(out_dir, tractograms)
 
         map_img.get_header().set_zooms(pos_factor)
         map_img.get_header().set_qform(ref_head.get_qform())
         map_img.get_header().set_sform(ref_head.get_sform())
-        map_img.to_filename(os.path.join(out_dir_path,
-                                         'tdi{0}x.nii.gz'.format(up_factor)))
+        map_img.to_filename(os.path.join(out_dir_path, tdi))
