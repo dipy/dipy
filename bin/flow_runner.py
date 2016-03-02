50ec3847648e0e81b350994cbb9db9e86a61eb56
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-03-02 15:28:45 -0500

    Added basic logging

--- bin/flow_runner.py
+++ bin/flow_runner.py
@@ -1,7 +1,10 @@
 from __future__ import division, print_function, absolute_import
+import logging
+
 from dipy.workflows.base import IntrospectiveArgumentParser
 
 def run_flow(flow):
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     parser = IntrospectiveArgumentParser()
     parser.add_workflow(flow)
     args = parser.get_flow_args()
