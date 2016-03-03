1ed91a49af54afff515ef9d81c172ca95699e16a
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-03-03 10:20:42 -0500

    Added logging parameters to all workflows

--- bin/flow_runner.py
+++ bin/flow_runner.py
@@ -3,9 +3,31 @@ import logging
 
 from dipy.workflows.base import IntrospectiveArgumentParser
 
+def get_level(lvl):
+    try:
+        return logging._levelNames[lvl]
+    except:
+        return logging.INFO
+
 def run_flow(flow):
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     parser = IntrospectiveArgumentParser()
     parser.add_workflow(flow)
+
+    # Add logging parameters common to all workflows
+    parser.add_argument('--log_level', action='store', dest='log_level',
+                        metavar='string', required=False, default='INFO',
+                        help='Log messsages display level')
+
+    parser.add_argument('--log_file', action='store', dest='log_file',
+                        metavar='string', required=False, default='',
+                        help='Log messsages display level')
+
     args = parser.get_flow_args()
+    logging.basicConfig(filename=args['log_file'],
+                        format='%(levelname)s:%(message)s',
+                        level=get_level(args['log_level']))
+    # Keep only workflow related parameters
+    del args['log_level']
+    del args['log_file']
+
     flow(**args)
