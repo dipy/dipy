d4ad8f32fa37de372d3ce1e42225703f27481303
Matthieu Dumont <matthieu.dumont@yahoo.com>
2016-03-04 11:34:19 -0500

    Fixed tests with new flaglike optionnal bools

--- dipy/workflows/tests/test_segment.py
+++ dipy/workflows/tests/test_segment.py
@@ -18,19 +18,18 @@ def test_median_otsu_flow():
         vol_idx = [0]
         dilate = 0
 
-        median_otsu_flow(data_path, out_dir, save_masked, median_radius,
-                         numpass, autocrop, vol_idx, dilate)
+        mask_name = 'mask.nii.gz'
+        masked_name = 'masked.nii.gz'
+        median_otsu_flow(data_path, out_dir=out_dir, save_masked=save_masked, median_radius=median_radius,
+                         numpass=numpass, autocrop=autocrop, vol_idx=vol_idx, dilate=dilate, mask=mask_name,
+                         masked=masked_name)
 
         masked, mask = median_otsu(volume, median_radius,
                                    numpass, autocrop,
                                    vol_idx, dilate)
 
-        fname, _ = splitext(splitext(basename(data_path))[0])
-
-        mask_fname = fname + '_mask.nii.gz'
-        result_mask_data = nib.load(join(out_dir, mask_fname)).get_data()
+        result_mask_data = nib.load(join(out_dir, mask_name)).get_data()
         npt.assert_array_equal(result_mask_data, mask)
 
-        masked_fname = fname + '_bet.nii.gz'
-        result_masked_data = nib.load(join(out_dir, masked_fname)).get_data()
+        result_masked_data = nib.load(join(out_dir, masked_name)).get_data()
         npt.assert_array_equal(result_masked_data, masked)
