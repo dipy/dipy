a4e6f631d1a7af74ab74530bcfe33ca2609bedc6
Matthieu Dumont <matthieu.dumont@yahoo.com>
2016-03-04 14:13:37 -0500

    Make dti metrics mask optionnal

--- dipy/workflows/utils.py
+++ dipy/workflows/utils.py
@@ -1,5 +1,20 @@
 from os.path import join, dirname, isabs, exists
 from os import makedirs
+from glob import glob
+
+# FIND NEW PARAM NAMES
+def glob_or_value(globable, maybe_globable, default=None):
+    globed_globable = glob(globable)
+    try:
+        globed_maybe = glob(maybe_globable)
+    except:
+        globed_maybe = None
+
+    if globed_maybe is None or len(globed_maybe) != len(globed_globable):
+        globed_maybe = [default] * len(globed_globable)
+
+    return globed_globable, globed_maybe
+
 
 
 def choose_create_out_dir(out_dir, root_path):
