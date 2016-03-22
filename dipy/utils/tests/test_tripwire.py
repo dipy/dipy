6e54d762fe55615685d811a1b09c39bca28cc4dc
Sourav Singh <souravsingh@users.noreply.github.com>
2016-03-23 00:00:34 +0530

    Use absolute import in test_tripwire

--- dipy/utils/tests/test_tripwire.py
+++ dipy/utils/tests/test_tripwire.py
@@ -1,7 +1,7 @@
 """ Testing tripwire module.
 """
 
-from ..tripwire import TripWire, is_tripwire, TripWireError
+from dipy.utils.tripwire import TripWire, is_tripwire, TripWireError
 
 from nose import SkipTest
 from nose.tools import (assert_true, assert_false, assert_raises,
