2b0a68a4da9258763b2789b597ace01a854ea3b7
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2016-04-27 21:59:30 -0400

    TEST: adding tests with different cases of inputs and outputs

--- dipy/workflows/multi_io.py
+++ dipy/workflows/multi_io.py
@@ -1,13 +1,19 @@
-
+from glob import glob
 
 
 class MultiIOFileConnector():
 
     def set_inputs(inputs):
-        pass
+        self.inputs = inputs
 
     def set_outputs(outputs):
-        pass
+        self.outputs = outputs
 
-    def sync_input_outputs():
+    def set_out_dir(out_dir):
+        self.out_dir = out_dir
+
+    def sync_input_outputs(out_dir, sync_type='many_to_many'):
+
+        for fpath in glob(input_files), globout):
+
         pass
