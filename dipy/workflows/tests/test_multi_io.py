4354493fb781aa5abfee8a94eca3a3ff36cb3855
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2016-04-29 20:35:05 -0400

    NF: new class that allows generating specific filenames of outputs given any combination of one or multiple inputs of multiple subjects

--- dipy/workflows/tests/test_multi_io.py
+++ dipy/workflows/tests/test_multi_io.py
@@ -5,115 +5,125 @@ import numpy.testing as npt
 from glob import glob
 from ipdb import set_trace
 from dipy.workflows.multi_io import OutputCreator
-
-
-def common_start(sa, sb):
-    """ Returns the longest common substring from the beginning of sa and sb """
-    def _iter():
-        for a, b in zip(sa, sb):
-            if a == b:
-                yield a
-            else:
-                return
-
-    return ''.join(_iter())
+from nibabel.tmpdirs import TemporaryDirectory
 
 
 def test_output_generator():
 
-    d1 = '/tmp/data/s1/'
-    d2 = '/tmp/data/s2/'
-    d3 = '/tmp/data/s3/'
+    with TemporaryDirectory() as tmpdir:
 
-    if not os.path.exists(d1):
-        os.makedirs(d1)
-        os.makedirs(d2)
-        os.makedirs(d3)
+        d1 = path.join(tmpdir, 'data', 's1')
+        d2 = path.join(tmpdir, 'data', 's2')
+        d3 = path.join(tmpdir, 'data', 's3')
 
-    in1 = d1 + 'test.txt'
-    np.savetxt(in1, np.arange(10))
-    in2 = d2 + 'test.txt'
-    np.savetxt(in2, 2 * np.arange(10))
-    in3 = d3 + 'test.txt'
-    np.savetxt(in3, 3 * np.arange(10))
+        if not os.path.exists(d1):
+            os.makedirs(d1)
+            os.makedirs(d2)
+            os.makedirs(d3)
 
-    d1_2 = '/tmp/data/s1/other/'
-    d2_2 = '/tmp/data/s2/other/'
-    d3_2 = '/tmp/data/s3/other/'
+        in1 = path.join(d1, 'test.txt')
+        np.savetxt(in1, np.arange(10))
+        in2 = path.join(d2, 'test.txt')
+        np.savetxt(in2, 2 * np.arange(10))
+        in3 = path.join(d3, 'test.txt')
+        np.savetxt(in3, 3 * np.arange(10))
 
-    if not os.path.exists(d1_2):
-        os.makedirs(d1_2)
-        os.makedirs(d2_2)
-        os.makedirs(d3_2)
+        d1_2 = path.join(tmpdir, 'data', 's1', 'other')
+        d2_2 = path.join(tmpdir, 'data', 's2', 'other')
+        d3_2 = path.join(tmpdir, 'data', 's3', 'other')
 
-    in1_2 = d1_2 + 'test2.txt'
-    np.savetxt(in1_2, np.arange(10))
-    in2_2 = d2_2 + 'test2.txt'
-    np.savetxt(in2_2, 2 * np.arange(10))
-    in3_2 = d3_2 + 'test2.txt'
-    np.savetxt(in3_2, 3 * np.arange(10))
-    in3_2 = d3_2 + 'test2.txt'
-    np.savetxt(in3_2, 3 * np.arange(10))
+        if not os.path.exists(d1_2):
+            os.makedirs(d1_2)
+            os.makedirs(d2_2)
+            os.makedirs(d3_2)
 
-    template_dir = '/tmp/template/'
-    if not os.path.exists(template_dir):
-        os.makedirs(template_dir)
+        in1_2 = path.join(d1_2, 'test2.txt')
+        np.savetxt(in1_2, np.arange(10))
+        in2_2 = path.join(d2_2, 'test2.txt')
+        np.savetxt(in2_2, 2 * np.arange(10))
+        in3_2 = path.join(d3_2, 'test2.txt')
+        np.savetxt(in3_2, 3 * np.arange(10))
 
-    np.savetxt(template_dir + 'avg.txt', 10 * np.arange(10))
+        template_dir = path.join(tmpdir, 'template')
+        if not os.path.exists(template_dir):
+            os.makedirs(template_dir)
 
-    out_dir = '/tmp/out/'
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+        np.savetxt(path.join(template_dir, 'avg.txt'), 10 * np.arange(10))
 
-    out_files = ['out_info.txt', 'out_summary.txt']
+        out_dir = path.join(tmpdir, 'out')
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
 
-    print('Two wild inputs')
+        out_files = ['out_info.txt', 'out_summary.txt']
 
-    og = OutputCreator(verbose=True)
-    og.set_inputs('/tmp/data/s*/test.txt', '/tmp/data/s*/other/test2.txt')
-    og.set_out_dir(out_dir)
-    og.set_out_fnames(*out_files)
-    og.create_outputs()
+        print('Two long inputs')
 
-    npt.assert_equal(np.array(og.outputs).shape, (3, 2))
-    print(og.outputs)
-    print('\n')
+        og = OutputCreator()
+        og.set_inputs(path.join(tmpdir, 'data', 's*', 'test.txt'),
+                      path.join(tmpdir, 'data', 's*', 'other', 'test2.txt'))
 
-    print('One wild input and one single')
+        og.set_out_dir(out_dir)
+        og.set_out_fnames(*out_files)
 
-    og = OutputCreator(verbose=True)
-    og.set_inputs('/tmp/data/s*/test.txt', '/tmp/template/avg.txt')
-    og.set_out_dir(out_dir)
-    og.set_out_fnames(*out_files)
-    og.create_outputs()
+        og.create_outputs()
+        npt.assert_equal(np.array(og.outputs).shape, (3, 2))
+        print(og.outputs)
+        print('\n')
 
-    npt.assert_equal(np.array(og.outputs).shape, (3, 2))
-    print(og.outputs)
-    print('\n')
+        for i_o in og:
+            print(i_o)
+        print('\n')
 
-    print('One single and one single inputs')
+        print('One long input and one single')
 
-    og = OutputCreator(verbose=True)
-    og.set_inputs('/tmp/data/s1/test.txt', '/tmp/template/avg.txt')
-    og.set_out_dir(out_dir)
-    og.set_out_fnames(*out_files)
-    og.create_outputs()
+        og = OutputCreator()
+        og.set_inputs(path.join(tmpdir, 'data', 's*', 'test.txt'),
+                      path.join(tmpdir, 'template', 'avg.txt'))
+        og.set_out_dir(out_dir)
+        og.set_out_fnames(*out_files)
+        og.create_outputs()
 
-    npt.assert_equal(np.array(og.outputs).shape, (1, 2))
-    print(og.outputs)
-    print('\n')
+        npt.assert_equal(np.array(og.outputs).shape, (3, 2))
+        print(og.outputs)
+        print('\n')
 
-    print('One single input')
+        for i_o in og:
+            print(i_o)
+        print('\n')
 
-    og = OutputCreator(verbose=True)
-    og.set_inputs('/tmp/data/s1/test.txt')
-    og.set_out_dir(out_dir)
-    og.set_out_fnames(*out_files)
-    og.create_outputs()
+        print('One single and one single input')
 
-    npt.assert_equal(np.array(og.outputs).shape, (1, 2))
-    print(og.outputs)
-    print('\n')
+        og = OutputCreator()
+        og.set_inputs(path.join(tmpdir, 'data', 's1', 'test.txt'),
+                      path.join(tmpdir, 'template', 'avg.txt'))
+        og.set_out_dir(out_dir)
+        og.set_out_fnames(*out_files)
+        og.create_outputs()
+
+        npt.assert_equal(np.array(og.outputs).shape, (1, 2))
+        print(og.outputs)
+        print('\n')
+
+        for i_o in og:
+            print(i_o)
+        print('\n')
+
+        print('One single input')
+
+        og = OutputCreator()
+        og.set_inputs(path.join(tmpdir, 'data', 's1', 'test.txt'))
+        og.set_out_dir(out_dir)
+        og.set_out_fnames(*out_files)
+        og.create_outputs()
+
+        npt.assert_equal(np.array(og.outputs).shape, (1, 2))
+        print(og.outputs)
+        print('\n')
+
+        for in1, out1, out2 in og:
+            print(in1)
+            print(out1)
+            print(out2)
 
 
 if __name__ == '__main__':
