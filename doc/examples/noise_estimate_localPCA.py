a217645ae675fcba7b712e7c6af8b049265568c7
Riddhish Bhalodia <Riddhish@Riddhishs-MacBook-Pro.local>
2016-05-18 13:12:55 +0530

    Added localPCA noise estimation example

---
+++ doc/examples/noise_estimate_localPCA.py
@@ -0,0 +1,141 @@
+"""
+===========================================
+Estimate Noise Levels for LocalPCA Datasets
+===========================================
+
+"""
+
+import numpy as np
+import nibabel as nib
+import scipy as sp
+import matplotlib.pyplot as plt
+from time import time
+from scipy import ndimage
+from dipy.data import fetch_sherbrooke_3shell, read_sherbrooke_3shell
+from dipy.data import fetch_stanford_hardi, read_stanford_hardi
+
+
+# fetch_sherbrooke_3shell()
+# img, gtab = read_sherbrooke_3shell()
+
+fetch_stanford_hardi()
+img, gtab = read_stanford_hardi()
+
+data = img.get_data()
+affine = img.get_affine()
+# data = data[20:60,20:60,20:40,:]
+# identify the b0 images from the dataset
+
+# first identify the number of the b0 images
+K = gtab.b0s_mask[gtab.b0s_mask].size
+
+if(K>1):
+    print "MUBE Estimate"
+    # If multiple b0 values then 
+    data0 = data[...,gtab.b0s_mask]
+    # MUBE Noise Estimate
+
+    # Form their matrix
+    X = data0.reshape(data0.shape[0] * data0.shape[1] * data0.shape[2], data0.shape[3])
+    # X = NxK
+    # Now to subtract the mean
+    M = np.mean(X,axis = 0)
+    X = X - M
+    C = np.transpose(X).dot(X)
+
+    # Do PCA and find the lowest principal component
+    [d,W] = np.linalg.eigh(C)
+    # we want eigen vectors of XX^T so we do
+    V = X.dot(W)
+    # unit normalize the clolumns of V
+    # V = V / np.linalg.norm(V, ord=2, axis=0)
+    # As the W is sorted in increasing eignevalue order, so is V
+    # choose the smallest principle component
+    I = V[:,0].reshape(data.shape[0], data.shape[1], data.shape[2])
+    
+
+else:
+    print "SIBE Estimate"
+    # if only one b0 value then
+    # SIBE Noise Estimate
+    data0 = data[...,~gtab.b0s_mask]
+    # MUBE Noise Estimate
+
+    # Form their matrix
+    X = data0.reshape(data0.shape[0] * data0.shape[1] * data0.shape[2], data0.shape[3])
+    # X = NxK
+    # Now to subtract the mean
+    M = np.mean(X,axis = 0)
+    X = X - M
+    C = np.transpose(X).dot(X)
+
+    # Do PCA and find the lowest principal component
+    [d,W] = np.linalg.eigh(C)
+    # we want eigen vectors of XX^T so we do
+    V = X.dot(W)
+    # unit normalize the clolumns of V
+    # V = V / np.linalg.norm(V, ord=2, axis=0)
+    # As the W is sorted in increasing eignevalue order, so is V
+    # choose the smallest principle component
+    I = V[:,0].reshape(data.shape[0], data.shape[1], data.shape[2])
+
+
+
+sigma = np.zeros(I.shape, dtype = np.float64)
+count = np.zeros_like(data)
+mean = np.zeros_like(data).astype(np.float64)
+
+# Compute the local noise variance of the I image
+for i in range(1,I.shape[0] - 1):
+    print(i)
+    for j in range(1, I.shape[1] - 1):
+        for k in range(1, I.shape[2] - 1):
+                
+            temp = I[i-1:i+2, j-1:j+2, k-1:k+2]
+            temp = (temp - np.mean(temp)) * (temp - np.mean(temp))
+            sigma[i-1:i+2, j-1:j+2, k-1:k+2] += temp  
+            temp = data[i-1:i+2, j-1:j+2, k-1:k+2, :]
+            mean[i-1:i+2, j-1:j+2, k-1:k+2, :] +=  np.mean(np.mean(np.mean(temp,axis = 0),axis=0),axis=0)
+            count[i-1:i+2, j-1:j+2, k-1:k+2,:] += 1
+
+sigma = sigma / count[...,0]             
+
+print "Initial estimate of local noise variance"
+# Compute the local mean of for the data                    
+mean = mean / count
+
+snr = np.zeros_like(I).astype(np.float64)
+sigma_corr = np.zeros_like(data).astype(np.float64)
+
+# find the SNR and make the correction
+# SNR Correction
+
+for l in range(data.shape[3]):
+    print(l)
+    snr = mean[...,l] / np.sqrt(sigma)
+    eta = 2 + snr**2 - (np.pi / 8) * np.exp(-0.5 * (snr**2)) * ((2 + snr**2) * sp.special.j0(0.25 *(snr**2)) + (snr**2) * sp.special.j1(0.25 *(snr**2)))**2
+    sigma_corr[...,l] = sigma / eta
+
+print "SNR correction done"
+# smoothing by lpf
+sigma_corrr = ndimage.gaussian_filter(sigma_corr,5)
+sigmar = ndimage.gaussian_filter(sigma,5)
+
+# display
+fig, ax = plt.subplots(2, 3)
+ax[0,0].imshow(data[:,:,30,13], cmap='gray', origin='lower')
+ax[0,0].set_title('Original')
+ax[0,1].imshow(sigma[:,:,30], cmap='gray', origin='lower')
+ax[0,1].set_title('sigma by PCA')
+ax[0,2].imshow(sigma_corr[:,:,30,13], cmap='gray', origin='lower')
+ax[0,2].set_title('after rescaling')
+ax[1,0].imshow(sigmar[:,:,30], cmap='gray', origin='lower')
+ax[1,0].set_title('sigma by PCA + LPF')
+ax[1,1].imshow(sigma_corrr[:,:,30,13], cmap='gray', origin='lower')
+ax[1,1].set_title('after rescaling + LPF')
+ax[1,2].imshow(I[:,:,30], cmap='gray', origin= 'lower')
+ax[1,2].set_title('The least significant PC image')
+# for i in range(3):
+#     ax[i].set_axis_off()
+
+plt.show()
