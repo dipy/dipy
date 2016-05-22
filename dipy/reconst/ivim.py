c6e532ed2c9168d800ba9d26e0f3514e0fb68140
Shahnawaz Ahmed <shd339@gmail.com>
2016-05-22 21:11:58 +0530

    IvimModel class defn

--- dipy/reconst/ivim.py
+++ dipy/reconst/ivim.py
@@ -15,6 +15,7 @@ import scipy.integrate as si
 
 from dipy.core.gradients import gradient_table
 from base import ReconstModel
+from dti import _min_positive_signal
 
 import matplotlib.pylab as pl
 
@@ -66,3 +67,52 @@ def ivim_prediction(ivim_params, gtab, S0=1.0):
         f, D, D_star = ivim_params[i]
         pred_signal.append(S(f, D, D_star, bvals))
     return pred_signal
+
+
+class IvimModel(ReconstModel):
+    """Ivim model
+    """
+
+    def __init__(self, gtab, fit_method="one_stage_fit", *args, **kwargs):
+        """ A Ivim Model
+        Parameters
+        ----------
+        gtab : GradientTable class instance
+        fit_method : str or callable
+                str can be one of the following:
+            'one_stage_fit'
+                Try to fit for all b values together
+
+            'two_stage_fit'
+                Fit for bvalues > 200 first and then
+                for less than 200
+
+        min_signal : float
+            The minimum signal value. Needs to be a strictly positive
+            number. Default: minimal signal in the data provided to `fit`.
+
+        References
+        ----------
+        .. [1]
+        .. [2]
+        .. [3]
+
+        """
+        ReconstModel.__init__(self, gtab)
+
+        if not callable(fit_method):
+            try:
+                fit_method = common_fit_methods[fit_method]
+            except KeyError:
+                e_s = '"' + str(fit_method) + '" is not a known fit '
+                e_s += 'method, the fit method should either be a '
+                e_s += 'function or one of the common fit methods'
+                raise ValueError(e_s)
+        self.fit_method = fit_method
+        self.args = args
+        self.kwargs = kwargs
+        self.min_signal = self.kwargs.pop('min_signal', None)
+        if self.min_signal is not None and self.min_signal <= 0:
+            e_s = "The `min_signal` key-word argument needs to be strictly"
+            e_s += " positive."
+            raise ValueError(e_s)
