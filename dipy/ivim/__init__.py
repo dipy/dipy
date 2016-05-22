1d22bb13744ed50e6bd383b5e9273c3444bdeb1a
Shahnawaz Ahmed <shd339@gmail.com>
2016-05-22 20:45:58 +0530

    Clean slate

--- dipy/ivim/__init__.py
+++
@@ -1,9 +0,0 @@
-# init for the ivim module
-# init for reconst aka the reconstruction module
-
-# Test callable
-from numpy.testing import Tester
-test = Tester().test
-bench = Tester().bench
-
-del Tester
