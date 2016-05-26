5e19275646935a70c1cc10c41f6d7e4cf645dd3e
Shahnawaz Ahmed <shd339@gmail.com>
2016-05-26 23:26:21 +0530

    testing with Federau values

--- dipy/reconst/tests/test_ivim.py
+++ dipy/reconst/tests/test_ivim.py
@@ -23,24 +23,29 @@ from dipy.core.gradients import (gradient_table, GradientTable,
                                  gradient_table_from_bvals_bvecs,
                                  reorient_bvecs)
 from dipy.sims.voxel import multi_tensor
+from dipy.core.sphere import disperse_charges, Sphere, HemiSphere
 
 
 def test_nlls_fit():
     """
     Test the implementation of NLLS
     """
-    fimg, fbvals, fbvecs = get_data('small_101D')
-    bvals, bvecs = read_bvals_bvecs(fbvals, fbvecs)
-    gtab = gradient_table(bvals, bvecs)
+    bvals = np.array([0., 10., 20., 30., 40., 60., 80., 100.,
+                      120., 140., 160., 180., 200., 220., 240.,
+                      260., 280., 300., 350., 400., ])
+    N = len(bvals)
+    bvecs = get_bvecs(N)
+    gtab = gradient_table(bvals, bvecs.T)
 
-    f, D_star, D = 90, 0.0015, 0.0008
+    f, D_star, D = 0.06, 0.0072, 0.00097
 
     mevals = np.array(([D_star, D_star, D_star], [D, D, D]))
     # This gives an isotropic signal
 
-    signal = multi_tensor(gtab, mevals, snr=None, fractions=[f, 100 - f])
+    signal = multi_tensor(gtab, mevals, snr=None, fractions=[
+                          f * 100, 100 * (1 - f)])
     data = signal[0]
-
+    S0 = data[0]
     ivim_model = ivim.IvimModel(gtab)
     ivim_fit = ivim_model.fit(data)
 
@@ -54,3 +59,13 @@ def test_nlls_fit():
     assert_equal(est_signal.shape, data.shape)
     assert_array_almost_equal(est_signal, data)
     assert_array_almost_equal(ivim_fit.model_params, [S0, f, D_star, D])
+
+
+def get_bvecs(N):
+    """Generate bvectors for N bvalues"""
+    theta = np.pi * np.random.rand(N)
+    phi = 2 * np.pi * np.random.rand(N)
+    hsph_initial = HemiSphere(theta=theta, phi=phi)
+    hsph_updated, potential = disperse_charges(hsph_initial, 5000)
+    vertices = hsph_updated.vertices
+    return vertices
