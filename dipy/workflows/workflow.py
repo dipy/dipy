e36cc8b30622f45875a0187253c60bd4a41f1995
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-06-03 14:26:53 -0400

    Added force mecanic, finished implementing new pipeline

--- dipy/workflows/workflow.py
+++ dipy/workflows/workflow.py
@@ -1,17 +1,59 @@
 from __future__ import division, print_function, absolute_import
+
+import os
+
+import logging
+
 from dipy.workflows.multi_io import io_iterator_
 
 
 class Workflow(object):
 
-    def __init__(self, output_strategy='append', mix_names=False):
+    def __init__(self, output_strategy='append', mix_names=False,
+                 force=False):
+
         self._output_strategy = output_strategy
         self._mix_names = mix_names
+        self.last_generated_outputs = None
+        self._force_overwrite = force
 
     def get_io_iterator(self, frame):
-        return io_iterator_(frame, self.run,
-                            output_strategy=self._output_strategy,
-                            mix_names=self._mix_names)
+        io_it = io_iterator_(frame, self.run,
+                             output_strategy=self._output_strategy,
+                             mix_names=self._mix_names)
+
+        self.last_generated_outputs = io_it.outputs
+
+        if self.manage_output_overwrite():
+            return io_it
+        else:
+            return []
+
+    def manage_output_overwrite(self):
+        duplicates = []
+        for output_list in self.last_generated_outputs:
+            for output in output_list:
+                if os.path.isfile(output):
+                    duplicates.append(output)
+
+        if len(duplicates) > 0:
+            if self._force_overwrite:
+                logging.info('The following output files are about to be' +
+                             ' overwritten.')
+
+            else:
+                logging.info('The following output files already exist, the ' +
+                             'workflow will not continue processing any further.' +
+                             ' Add the --force flag to allow output files ' +
+                             ' overwrite.')
+
+            for dup in duplicates:
+                logging.info(dup)
+
+            return self._force_overwrite
+
+        return True
+
 
     def run(self):
         pass
