183c67663d0ce519232b1dcf4060564dd4d7bbb0
Gabriel Girard <girard.gabriel@gmail.com>
2016-06-05 12:58:07 -0400

    NF - added CMC tissue classifier

--- dipy/tracking/local/__init__.py
+++ dipy/tracking/local/__init__.py
@@ -1,8 +1,9 @@
 from .localtracking import LocalTracking
 from .tissue_classifier import (ActTissueClassifier, BinaryTissueClassifier,
-                                ThresholdTissueClassifier, TissueClassifier)
+                                ThresholdTissueClassifier, TissueClassifier,
+                                CmcTissueClassifier)
 from .direction_getter import DirectionGetter
 from dipy.tracking import utils
 
 __all__ = ["ActTissueClassifier", "BinaryTissueClassifier", "LocalTracking",
-           "ThresholdTissueClassifier"]
+           "ThresholdTissueClassifier", "CmcTissueClassifier"]
