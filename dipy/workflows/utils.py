3bde33f1531ccae5c1e705293818631581fa4d52
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-06-20 09:29:18 -0400

    Tests update wip

--- dipy/workflows/utils.py
+++ dipy/workflows/utils.py
@@ -1,20 +1,5 @@
-from glob import glob
 from os import makedirs
 from os.path import join, dirname, isabs, exists
-
-
-def glob_or_value(globable, maybe_globable, default=None):
-    globed_globable = glob(globable)
-    try:
-        globed_maybe = glob(maybe_globable)
-    except:
-        globed_maybe = None
-
-    if globed_maybe is None or len(globed_maybe) != len(globed_globable):
-        globed_maybe = [default] * len(globed_globable)
-
-    return globed_globable, globed_maybe
-
 
 
 def choose_create_out_dir(out_dir, root_path):
