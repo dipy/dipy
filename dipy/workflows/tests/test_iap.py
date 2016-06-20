4dd7c6978215da1057c52dd81b84f6f0a36d5bd9
Matthieu Dumont <matthieu.dumont@usherbrooke.ca>
2016-06-20 09:29:18 -0400

    Tests update wip

--- dipy/workflows/tests/test_iap.py
+++ dipy/workflows/tests/test_iap.py
@@ -3,36 +3,43 @@ import sys
 
 from dipy.workflows.base import IntrospectiveArgumentParser
 
+from dipy.workflows.workflow import Workflow
 
-def dummy_flow(positional_str, positional_bool, positional_int,
-               positional_float, optional_str='default', optional_bool=False,
-               optional_int=0, optional_float=1.0, optional_float_2=2.0):
-    """ Workflow used to test the introspective argument parser.
 
-    Parameters
-    ----------
-    positional_str : string
-        positional string argument
-    positional_bool : bool
-        positional bool argument
-    positional_int : int
-        positional int argument
-    positional_float : float
-        positional float argument
-    optional_str : string, optional
-        optional string argument (default 'default')
-    optional_bool : bool, optional
-        optional bool argument (default False)
-    optional_int : int, optional
-        optional int argument (default 0)
-    optional_float : float, optional
-        optional float argument (default 1.0)
-    optional_float_2 : float, optional
-        optional float argument #2 (default 2.0)
-    """
-    return positional_str, positional_bool, positional_int,\
-        positional_float, optional_str, optional_bool,\
-        optional_int, optional_float, optional_float_2
+class DummyFlow(Workflow):
+
+    def run(self, positional_str, positional_bool, positional_int,
+            positional_float, optional_str='default', optional_bool=False,
+            optional_int=0, optional_float=1.0, optional_float_2=2.0,
+            out_dir=''):
+        """ Workflow used to test the introspective argument parser.
+
+        Parameters
+        ----------
+        positional_str : string
+            positional string argument
+        positional_bool : bool
+            positional bool argument
+        positional_int : int
+            positional int argument
+        positional_float : float
+            positional float argument
+        optional_str : string, optional
+            optional string argument (default 'default')
+        optional_bool : bool, optional
+            optional bool argument (default False)
+        optional_int : int, optional
+            optional int argument (default 0)
+        optional_float : float, optional
+            optional float argument (default 1.0)
+        optional_float_2 : float, optional
+            optional float argument #2 (default 2.0)
+        out_dir : string
+            output directory (default '')
+        """
+        return positional_str, positional_bool, positional_int,\
+               positional_float, optional_str, optional_bool,\
+               optional_int, optional_float, optional_float_2
 
 
 def test_iap():
@@ -51,6 +58,7 @@ def test_iap():
 
     sys.argv.extend(inputs)
     parser = IntrospectiveArgumentParser()
+    dummy_flow = DummyFlow()
     parser.add_workflow(dummy_flow)
     args = parser.get_flow_args()
     all_keys = pos_keys + opt_keys
@@ -61,7 +69,7 @@ def test_iap():
         npt.assert_equal(args[k], v)
 
     # Test if **args really fits dummy_flow's arguments
-    return_values = dummy_flow(**args)
+    return_values = dummy_flow.run(**args)
     npt.assert_array_equal(return_values, all_results + [2.0])
 
 
@@ -77,29 +85,6 @@ def inputs_from_results(results, keys=None, optionnal=False):
 
     return inputs
 
-
-def nargs_flow(variable_ints, optional_int=2):
-    """ Workflow used to test the nargs argument.
-
-    Parameters
-    ----------
-    variable_ints : variable int
-        variable number of strings
-    optional_int : int
-        optional int argument
-    """
-    return variable_ints, optional_int
-
-
-def test_nargs():
-    sys.argv = [sys.argv[0]]
-    var_args = ['1', '2', '3', '4', '5', '6', '7', '8']
-    optionnals = ['--optional_int', '2']
-    sys.argv.extend(var_args + optionnals)
-
-    parser = IntrospectiveArgumentParser()
-    parser.add_workflow(nargs_flow)
-    args = parser.get_flow_args()
-    var_ints, opt_int = nargs_flow(**args)
-    npt.assert_equal(len(var_ints), len(var_args))
+if __name__ == '__main__':
+    test_iap()
 
--- dipy/workflows/tests/test_logging.py
+++ dipy/workflows/tests/test_logging.py
@@ -9,7 +9,9 @@ from dipy.workflows.segment import MedianOtsuFlow
 
 from nose.tools import assert_true
 
+
 def test_logging_consistency():
+    # nosetests need to be ran with --nologcapture
     with TemporaryDirectory() as tmpdir:
         data_path, _, _ = get_data('small_25')
         prog_log = os.path.join(tmpdir, 'prog_log.txt')
@@ -20,13 +22,19 @@ def test_logging_consistency():
                             format=format,
                             level=level)
 
-        MedianOtsuFlow(data_path, out_dir=tmpdir)
+        mo_flow = MedianOtsuFlow()
+        mo_flow.run(data_path, out_dir=tmpdir)
+
+        mask_path, _ = mo_flow.last_generated_outputs[0]
+        os.remove(mask_path)
 
         cmd_log = os.path.join(tmpdir, 'cmd_log.txt')
         cmd_line = \
             'dipy_median_otsu {0} --out_dir {1} --log_file {2} --log_level {3}'\
-             .format(data_path, tmpdir, cmd_log, level)
+            .format(data_path, tmpdir, cmd_log, level)
 
+        print(prog_log)
+        print(cmd_log)
         subprocess.call(cmd_line.split(' '))
         same_content = filecmp.cmp(prog_log, cmd_log)
         assert_true(same_content)
