de22db5766c96a836bd6b68ff94fd07573f8c2bd
Shahnawaz Ahmed <shd339@gmail.com>
2016-06-21 18:03:45 +0530

    Tests for two stage

--- dipy/reconst/tests/test_ivim.py
+++ dipy/reconst/tests/test_ivim.py
@@ -113,51 +113,34 @@ def generate_multivoxel_data(gtab, params):
     return np.array(data)
 
 
-# def test_two_stage():
-#     """Test the two stage fitting routine"""
-#     bvals = np.array([0., 10., 20., 30., 40., 60., 80., 100.,
-#                       120., 140., 160., 180., 200., 220., 240.,
-#                       260., 280., 300., 350., 400., 500., 600.,
-#                       700., 800., 900., 1000.])
-#     N = len(bvals)
-#     bvecs = get_bvecs(N)
-#     gtab = gradient_table(bvals, bvecs.T)
+def test_two_stage():
+    """Test the two stage fitting routine"""
+    bvals = np.array([0., 10., 20., 30., 40., 60., 80., 100.,
+                      120., 140., 160., 180., 200., 220., 240.,
+                      260., 280., 300., 350., 400., 500., 600.,
+                      700., 800., 900., 1000.])
+    N = len(bvals)
+    bvecs = get_bvecs(N)
+    gtab = gradient_table(bvals, bvecs.T)
 
-#     S0, f, D_star, D = 1.0, 0.2052, 0.00473, 0.00066
+    S0, f, D_star, D = 1.0, 0.2052, 0.00473, 0.00066
 
-#     mevals = np.array(([D_star, D_star, D_star], [D, D, D]))
-#     # This gives an isotropic signal
+    mevals = np.array(([D_star, D_star, D_star], [D, D, D]))
+    # This gives an isotropic signal
 
-#     signal = multi_tensor(gtab, mevals, snr=None, S0=S0, fractions=[
-#                           f * 100, 100 * (1 - f)])
-#     data = signal[0]
-#     ivim_model = IvimModel(gtab)
+    signal = multi_tensor(gtab, mevals, snr=None, S0=S0, fractions=[
+                          f * 100, 100 * (1 - f)])
+    data = signal[0]
+    ivim_model = IvimModel(gtab)
 
-#     guess_params = np.array([[1.0, .10, 0.001, 0.0001], ])
-#     ivim_fit = ivim_model.fit(data, fit_method="two_stage", routine='minimize')
+    ivim_fit = ivim_model.fit(data, fit_method="two_stage", routine='minimize')
 
-#     est_signal = ivim_function(ivim_fit.model_params, bvals)
+    est_signal = ivim_function(ivim_fit.model_params, bvals)
 
-#     assert_array_equal(est_signal.shape, data.shape)
-#     assert_array_almost_equal(est_signal, data)
-#     assert_array_almost_equal(ivim_fit.model_params, [S0, f, D_star, D])
+    assert_array_equal(est_signal.shape, data.shape)
+    assert_array_almost_equal(est_signal, data)
+    assert_array_almost_equal(ivim_fit.model_params, [S0, f, D_star, D])
 
-    # Test with multiple voxels
-
-    # params = [[1.0, 0.2052, 0.00473, 0.00066], [1.0, 0.18, 0.00555, 0.0007]]
-
-    # data_two = generate_multivoxel_data(gtab, params)
-    # ivim_model_two = IvimModel(gtab)
-
-    # guess_params = np.array([[1.0, 0.01, 0.001, 0.0009],
-    #                          [0.9, 0.04, 0.002, 0.0004]])
-    # ivim_fit_two = ivim_model_two.fit(data_two, fit_method="two_stage",
-    #                                   x0=guess_params)
-    # est_signal_two = generate_multivoxel_data(gtab, ivim_fit_two.model_params)
-
-    # assert_array_equal(est_signal_two.shape, data_two.shape)
-    # assert_array_almost_equal(est_signal_two, data_two)
-    # assert_array_almost_equal(ivim_fit_two.model_params, params)
 
 def test_predict():
     """
