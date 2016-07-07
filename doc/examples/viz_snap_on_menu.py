a052a3c6a63fdee49d5306cc5bdee24f173b6965
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-07-07 12:04:08 -0400

    Improved example on how to make a vtkAssembly that follows the camera.

--- doc/examples/viz_snap_on_menu.py
+++ doc/examples/viz_snap_on_menu.py
@@ -4,6 +4,47 @@ import numpy as np
 from dipy.viz import actor, window
 
 
+def make_assembly_follower(assembly):
+    # Usually vtkFollower works by using
+    # SetMapper(my_object.GetMapper()) but since our orbital_system
+    # is a vtkAssembly there is no Mapper. So, we have to manually
+    # update the transformation matrix of our orbital_system according to
+    # an empty vtkFollower actor that we explictly add into the assembly.
+    # By adding the vtkFollower into the assembly its transformation matrix
+    # get automatically updated so it always faces the camera. Using that
+    # trasnformation matrix and we can transform our orbital_system
+    # accordingly.
+    dummy_follower = vtk.vtkFollower()
+    orbital_system.AddPart(dummy_follower)
+    dummy_follower.SetCamera(ren.GetActiveCamera())
+
+    # Get assembly transformation matrix.
+    M = vtk.vtkTransform()
+    M.SetMatrix(assembly.GetMatrix())
+
+    # Get the inverse of the assembly transformation matrix.
+    M_inv = vtk.vtkTransform()
+    M_inv.SetMatrix(assembly.GetMatrix())
+    M_inv.Inverse()
+
+    # Create a transform object that gets updated whenever the input matrix
+    # is updated, which is whenever the camera moves.
+    dummy_follower_transform = vtk.vtkMatrixToLinearTransform()
+    dummy_follower_transform.SetInput(dummy_follower.GetMatrix())
+
+    T = vtk.vtkTransform()
+    T.PostMultiply()
+    # Bring the assembly to the origin.
+    T.Concatenate(M_inv)
+    # Change orientation of the assembly.
+    T.Concatenate(dummy_follower_transform)
+    # Bring the assembly back where it was.
+    T.Concatenate(M)
+
+    assembly.SetUserTransform(T)
+    return assembly
+
+
 def make_cube(edge=1):
     cube_src = vtk.vtkCubeSource()
     cube_src.SetXLength(edge)
@@ -44,6 +85,9 @@ def send_into_orbit(obj, satellites, r=1., show_orbit=True):
         satellite.SetPosition(coord)
         orbital_system.AddPart(satellite)
 
+    offset = (np.asarray(obj.GetCenter()) - np.asarray(orbital_system.GetCenter()))
+    orbital_system.SetPosition(offset)
+
     return orbital_system
 
 
@@ -61,20 +105,22 @@ orbital_system = send_into_orbit(earth, [s1, s2, s3],  r=1.1)
 ren = window.ren()
 
 # Make the orbit always faces the camera.
-# Usually vtkFollower works by using
-# SetMapper(my_object.GetMapper()) but since our orbital_system
-# is a vtkAssembly there is no Mapper. So, we have to manually
-# update the transformation matrix of our orbital_system according to
-# an empty vtkFollower actor that we explictly add into the assembly.
-# By adding the vtkFollower into the assembly its transformation matrix
-# get automatically updated so it always faces the camera. Using that
-# trasnformation matrix and we can transform our orbital_system accordingly.
-dummy_follower = vtk.vtkFollower()
-orbital_system.AddPart(dummy_follower)
-dummy_follower.SetCamera(ren.GetActiveCamera())
-orbital_system.SetUserMatrix(dummy_follower.GetMatrix())
-
+orbital_system = make_assembly_follower(orbital_system)
 ren.add(earth, orbital_system)
+
+# Let's get crazy and add a moon.
+moon = make_cube(edge=0.2)
+moon.AddPosition(2, 2, 2)
+moon.RotateZ(45)
+
+s4 = make_cube(edge=0.05)
+s5 = make_cube(edge=0.05)
+s6 = make_cube(edge=0.05)
+s7 = make_cube(edge=0.05)
+
+orbital_system = send_into_orbit(moon, [s4, s5, s6, s7],  r=1.1)
+orbital_system = make_assembly_follower(orbital_system)
+ren.add(moon, orbital_system)
 
 show_m = window.ShowManager(ren, size=(800, 600))
 show_m.start()
