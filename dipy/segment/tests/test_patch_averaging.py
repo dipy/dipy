2b568f1249f99b17b8355b35df20859abb4b09a5
Riddhish Bhalodia <riddhishb@gmail.com>
2016-07-20 01:57:34 +0530

    Speed improvment in tests

--- dipy/segment/tests/test_patch_averaging.py
+++ dipy/segment/tests/test_patch_averaging.py
@@ -11,12 +11,12 @@ from dipy.segment.mask import (brain_extraction, patch_averaging)
 
 def test_static():
     # concentric cube phantom
-    S0 = np.zeros([70, 70, 70], dtype=np.float64)
-    S0[10:60, 10:60, 10:60] = 50
-    S0[20:50, 20:50, 20:50] = 100
-    S0[30:40, 30:40, 30:40] = 150
-    mask = np.zeros([70, 70, 70])
-    mask[10:60, 10:60, 10:60] = 1
+    S0 = np.zeros([35, 35, 35], dtype=np.float64)
+    S0[5:30, 5:30, 5:30] = 50
+    S0[10:25, 10:25, 10:25] = 100
+    S0[15:20, 15:20, 15:20] = 150
+    mask = np.zeros([35, 35, 35])
+    mask[5:30, 5:30, 5:30] = 1
     [S0avg, S0mask] = patch_averaging(
         S0, S0, mask, patch_radius=0, block_radius=1, parameter=1, threshold=0)
 
