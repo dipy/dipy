ea08055860bc6df2f6224f7220dcc8c21de903e9
RafaelNH <rafaelnh21@gmail.com>
2016-08-30 16:54:27 +0200

    DOC: Finish a first version of the free water DTI usage example

--- doc/examples/reconst_fwdti.py
+++ doc/examples/reconst_fwdti.py
@@ -1,6 +1,6 @@
 """
 ==========================================================================
-Using the free water elimination model to remove free water contaminations
+Using the free water elimination model to remove free water contamination
 ==========================================================================
 
 As shown previously (see :ref:`example_reconst_dti`), the diffusion tensor
@@ -11,19 +11,17 @@ underestimated by partial volume effects of the cerebral spinal fluid (CSF).
 This free water contamination can particularly corrupt diffusion tensor imaging
 analysis of microstructural changes when different groups of subject show
 different brain morphology (e.g. brain ventricle enlargement associated with
-brain tissue atrophy that occurs in pathology or aging).
+brain tissue atrophy that occurs in several brain pathologies and ageing).
 
 A way to remove this free water influences is to expand the DTI model to take
-into account two compartments representing the diffusion contributions from
-the tissue and from the CSF. The expressing of the expanded free water 
-elimination DTI model is shown below:
-
+into account an extra compartment representing the contributions of free water
+diffusion. The expression of the expanded DTI model is shown below:
 
 .. math::
 
     S(\mathbf{g}, b) = S_0(1-f)e^{-b\mathbf{g}^T \mathbf{D}
     \mathbf{g}}+S_0fe^{-b D_{iso}}
-    
+
 where $\mathbf{g}$ and $b$ are diffusion grandient direction and weighted
 (more information see :ref:`example_reconst_dti`), $S(\mathbf{g}, b)$ is the
 diffusion-weighted signal measured, $S_0$ is the signal conducted in a
@@ -56,7 +54,7 @@ fetch_cenir_multib(with_raw=False)
 
 """
 From the downloaded data, we read only the data acquired with b-values up to
-2000 $s.mm^{-2} to decrease the influence of non-Gaussain (_[Hoy2014]).
+2000 $s.mm^{-2} to decrease the influence of non-Gaussain _[Hoy2014].
 """
 
 bvals = [200, 400, 1000, 2000]
@@ -70,14 +68,13 @@ affine = img.get_affine()
 """
 The free water DTI model can take some minutes to process the full data set.
 Thus, we remove the background of the image to avoid unnecessary calculations.
-Moreover, for illustration purposes we process only an axial slice of the
-data.
 """
 
 maskdata, mask = median_otsu(data, 4, 2, False, vol_idx=[0, 1], dilate=1)
 
 """
-Moreover, for illustration purposes we select an axial slice of the data.
+Moreover, for illustration purposes we process only an axial slice of the
+data.
 """
 
 axial_slice = 40
@@ -103,9 +100,8 @@ fwdtifit = fwdtimodel.fit(data, mask=mask_roi)
 """
 This 2-steps procedure will create a FreeWaterTensorFit object which contains
 all the diffusion tensor statistics free for free water contaminations. Below
-we extract the fractional anisotropy (FA), the mean diffusivity (MD), the
-axial diffusivity (AD) and  the radial diffusivity (RD) of the free water
-diffusion tensor."""
+we extract the fractional anisotropy (FA) and the mean diffusivity (MD) of the
+free water diffusion tensor."""
 
 FA = fwdtifit.fa
 MD = fwdtifit.md
@@ -113,85 +109,72 @@ AD = fwdtifit.ad
 RD = fwdtifit.rd
 
 """
-From comparison we also compute the same standard measures processed by the
+For comparison we also compute the same standard measures processed by the
 standard DTI model
 """
+
 dtimodel = dti.TensorModel(gtab)
 
 dtifit = dtimodel.fit(data, mask=mask_roi)
 
 dti_FA = dtifit.fa
 dti_MD = dtifit.md
-dti_AD = dtifit.ad
-dti_RD = dtifit.rd
 
 """
-Below the FA, MD, AD, and RD maps of the selected slice are ploted (upper
-panels) and compared to the FA, MD, AD and RD maps obtain from the standard
-DTI model (lower panels).
+Below the FA values for both free water elimnantion DTI model and standard DTI
+model are ploted in panels A and B, while the repective MD values are ploted in
+panels D and E. For a better visualization of the effect of the free water
+correction, the differences between these two metrics are shown in panels C and
+E. In addition to the standard diffusion statistics, the estimated volume
+fraction of the free water contamination is shown on panel G.
 """
 
 fig1, ax = plt.subplots(2, 4, figsize=(12, 6),
                         subplot_kw={'xticks': [], 'yticks': []})
 
 fig1.subplots_adjust(hspace=0.3, wspace=0.05)
+ax.flat[0].imshow(FA[:, :, axial_slice].T, origin='lower',
+                  cmap='gray', vmin=0, vmax=1)
+ax.flat[0].set_title('A) fwDTI FA')
+ax.flat[1].imshow(dti_FA[:, :, axial_slice].T, origin='lower',
+                  cmap='gray', vmin=0, vmax=1)
+ax.flat[1].set_title('B) standard DTI FA')
 
-ax.flat[0].imshow(FA[:, :, axial_slice], cmap='gray', vmin=0, vmax=1)
-ax.flat[0].set_title('FA (fwDTI)')
-ax.flat[1].imshow(MD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[1].set_title('MD (fwDTI)')
-ax.flat[2].imshow(AD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[2].set_title('AD (fwDTI)')
-ax.flat[3].imshow(RD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[3].set_title('RD (fwDTI)')
+FAdiff = abs(FA[:, :, axial_slice] - dti_FA[:, :, axial_slice])
+ax.flat[2].imshow(FAdiff.T, cmap='gray', origin='lower', vmin=0, vmax=1)
+ax.flat[2].set_title('C) FA difference')
 
-ax.flat[4].imshow(dti_FA[:, :, axial_slice], cmap='gray', vmin=0, vmax=1)
-ax.flat[4].set_title('FA (DTI)')
-ax.flat[5].imshow(dti_MD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[5].set_title('MD (DTI)')
-ax.flat[6].imshow(dti_AD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[6].set_title('AD (DTI)')
-ax.flat[7].imshow(dti_RD[:, :, axial_slice], cmap='gray', vmin=0, vmax=2.5e-3)
-ax.flat[7].set_title('RD (DTI)')
+ax.flat[3].axis('off')
 
-plt.show()
-fig1.savefig('Diffusion_measures_from_free_water_DTI_and_standard_DTI.png')
+ax.flat[4].imshow(MD[:, :, axial_slice].T, origin='lower',
+                  cmap='gray', vmin=0, vmax=2.5e-3)
+ax.flat[4].set_title('D) fwDTI MD')
+ax.flat[5].imshow(dti_MD[:, :, axial_slice].T, origin='lower',
+                  cmap='gray', vmin=0, vmax=2.5e-3)
+ax.flat[5].set_title('E) standard DTI MD')
 
-"""
-.. figure:: Diffusion_measures_from_free_water_DTI_and_standard_DTI.png
-   :align: center
-   **Diffusion tensor measures obtain from the diffusion tensor estimated from
-   the free water DTI (upper panels) and standard DTI (lower panels).**.
-
-From the figure, we can see that ...
-
-In addition to the standard diffusion statistics, the FreeWaterDiffusionFit
-instance can be used to display the volume fraction of the free water diffusion
-component.
-"""
+MDdiff = abs(MD[:, :, axial_slice] - dti_MD[:, :, axial_slice])
+ax.flat[6].imshow(MDdiff.T, origin='lower', cmap='gray', vmin=0, vmax=2.5e-3)
+ax.flat[6].set_title('F) MD difference')
 
 F = fwdtifit.f
-S0 = fwdtifit.S0
 
-fig2, ax = plt.subplots(1, 2, figsize=(12, 6),
-                        subplot_kw={'xticks': [], 'yticks': []})
-
-fig2.subplots_adjust(hspace=0.3, wspace=0.05)
-
-ax.flat[0].imshow(F[:, :, axial_slice], cmap='gray', vmin=0, vmax=1)
-ax.flat[0].set_title('F')
-ax.flat[1].imshow(S0[:, :, axial_slice], cmap='gray')
-ax.flat[1].set_title('S0')
+ax.flat[7].imshow(F[:, :, axial_slice].T, origin='lower',
+                  cmap='gray', vmin=0, vmax=1)
+ax.flat[7].set_title('G) free water volume')
 
 plt.show()
-fig2.savefig('Volume_fraction_of_the_water_diffusion_component.png')
+fig1.savefig('In_vivo_free_water_DTI_and_standard_DTI_measures.png')
 
 """
-.. figure:: Volume_fraction_of_the_water_diffusion_component.png
+.. figure:: In_vivo_free_water_DTI_and_standard_DTI_measures.png
    :align: center
-   **Volume fraction of the water diffusion component.**.
-
-[Insert Figure explanation]
+   ** In vivo diffusion measures obtain from the free water DTI and standard
+   DTI. The values of Fractional Anisotropy for the free water DTI model and
+   standard DTI model and their difference are shown in the upper panels (A-C),
+   while respective MD values are shown in the lower panels (D-F). In addition
+   the free water volume fraction estimated from the fwDTI model is shown in
+   panel G**.
 
 References:
 
