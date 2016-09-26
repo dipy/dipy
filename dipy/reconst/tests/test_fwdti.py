9c93e29751a82a4ab09b503f540c05ad7b3008d8
RafaelNH <rafaelnh21@gmail.com>
2016-09-26 16:38:16 +0100

    BF: Deal with negative S0 in teory we shouldn't have negative S0, however this can happen in partice. For example this case was found in the CENIR HCP like dataset

--- dipy/reconst/tests/test_fwdti.py
+++ dipy/reconst/tests/test_fwdti.py
@@ -295,3 +295,18 @@ def test_md_regularization():
     assert_array_almost_equal(fwefit.fa, FAref)
     assert_array_almost_equal(fwefit.md, MDref)
     assert_array_almost_equal(fwefit.f, GTF)
+
+
+def test_zero_s0():
+    # single voxel
+    gtf = 0.55
+    mevals = np.array([[0.0017, 0.0003, 0.0003], [0.003, 0.003, 0.003]])
+    S_conta, peaks = multi_tensor(gtab_2s, mevals, S0=100,
+                                  angles=[(90, 0), (90, 0)],
+                                  fractions=[(1-gtf) * 100, gtf*100], snr=None)
+    S_conta[gtab_2s.bvals == 0] = -100
+    fwdm = fwdti.FreeWaterTensorModel(gtab_2s, 'NLS')
+    fwefit = fwdm.fit(S_conta)
+    assert_array_almost_equal(fwefit.fa, 0.0)
+    assert_array_almost_equal(fwefit.md, 0.0)
+    assert_array_almost_equal(fwefit.f, 0.0)
