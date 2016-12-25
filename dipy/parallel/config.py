e4572c9d20d91a8e6a20ceced75ddf941559717f
Bago Amirbekian <mrbago@gmail.com>
2016-12-25 13:51:47 -0700

    RF - have parallel pool launch at startup.

--- dipy/parallel/config.py
+++ dipy/parallel/config.py
@@ -1,14 +1,27 @@
 from multiprocessing import cpu_count
 from contextlib import contextmanager
+import multiprocessing
 
 _dipy_num_cpu = 1
+manager = None
 
 def activate_multithreading(num_cpu=None):
     global _dipy_num_cpu
+    global manager
     if num_cpu is None:
         _dipy_num_cpu = cpu_count()
-    elif num_cpu > 0:
+    elif num_cpu <= 0:
+        raise ValueError("num_cpu must be positive.")
+    else:
         _dipy_num_cpu = num_cpu
+
+    if manager is not None:
+        # manager.shut_down()
+        raise NotImplemented()
+    if _dipy_num_cpu > 1:
+        manager = PoolMananger(_dipy_num_cpu)
+    else:
+        manager = None
 
 def deactivate_multithreading():
     global _dipy_num_cpu
@@ -18,6 +31,31 @@ def deactivate_multithreading():
 def multithreading_on(num_cpu=None):
     previous_state = _dipy_num_cpu
     activate_multithreading(num_cpu)
-    yield
-    activate_multithreading(previous_state)
+    try:
+        yield
+    finally:
+        activate_multithreading(previous_state)
 
+class PoolMananger(object):
+
+    active = False
+
+    def __init__(self, n_cpu):
+        self.n_cpu = n_cpu
+        self.pool = multiprocessing.Pool(n_cpu)
+        self.active = True
+
+    def shut_down(self):
+        if self.active:
+            self.pool.close()
+            self.pool.join()
+            self.active = False
+            self.pool = None
+
+def cleanup():
+    if manager is not None:
+        manager.shut_down()
+    print("dipy-parallel-cleanup done")
+
+import atexit
+atexit.register(cleanup)
--- dipy/parallel/main.py
+++ dipy/parallel/main.py
@@ -5,6 +5,7 @@ from multiprocessing import cpu_count, Pool
 import numpy as np
 
 from numpy import ndindex
+import time
 
 import dipy.parallel.config
 
@@ -57,25 +58,50 @@ class MultiVoxelFunction(object):
         self._serial(self, data, mask, *args, **kwargs)
 
 
+class TrackProgress(object):
+
+    def __init__(self, n):
+        self.count = 0
+        self.total = n
+        self.reset()
+
+    def reset(self):
+        self.start = time.time()
+
+    def task_done(self):
+        duration = time.time() - self.start
+        self.count += 1
+        msg = "task %d out of %d done in %s sec"
+        msg = msg % (self.count, self.total, duration)
+        print(msg)
+
+
 class UpdateCallback(MultiVoxelFunction):
 
-    def __init__(self, index, outputs, errors):
+    def __init__(self, index, outputs, errors, tracker):
         self.index = index
         self.outputs = outputs
         self.errors = errors
+        self.tracker = tracker
 
     def __call__(self, result):
         update_outputs(self.index, result, self.outputs)
         # Remove from errors to indicate successful completion and free memory.
         self.errors.pop(repr(self.index))
+        self.tracker.task_done()
 
 
 def _array_split_points(mask, num_chunks):
     # TODO split input based on mask values so each thread gets about the same
     # number of voxels where mask is True.
-    chunk_size = mask.size / num_chunks
-    split_points = np.arange(1, num_chunks + 1) * chunk_size
-    return np.round(split_points).astype(int)
+    cumsum = mask.cumsum()
+    num_chunks = min(num_chunks, cumsum[-1])
+    even_spacing = np.linspace(1, cumsum[-1], num_chunks + 1)
+
+    split_points = cumsum.searchsorted(even_spacing, 'left')
+    split_points[-1] += 1
+    assert cumsum[split_points[-1]] == cumsum[-1]
+    return split_points
 
 
 def call_remotely(parallel_function, *args, **kwargs):
@@ -97,16 +123,24 @@ class ParallelFunction(MultiVoxelFunction):
         outputs = self._setup_outputs(data, mask, *args, **kwargs)
 
         num_cpu = dipy.parallel.config._dipy_num_cpu
-        num_chunks = min(10 * num_cpu, mask.size)
-        end_points = _array_split_points(mask, num_chunks)
+        num_chunks = 100 * num_cpu
+        split_points = _array_split_points(mask, num_chunks)
+        start = split_points[0]
+        end_points = split_points[1:]
 
-        pool = Pool(num_cpu)
-        start = 0
+        # pool = Pool(num_cpu)
+        if dipy.parallel.config.manager is None:
+            raise ValueError()
+        pool = dipy.parallel.config.manager.pool
+        if pool is None:
+            raise ValueError()
+
         errors = {}
+        tracker = TrackProgress(len(split_points))
         for end in end_points:
             index = slice(start, end)
             chunk_args = (self, data[index], mask[index]) + args
-            callback = UpdateCallback(index, outputs, errors)
+            callback = UpdateCallback(index, outputs, errors, tracker)
             r = pool.apply_async(call_remotely, chunk_args, kwargs,
                                  callback=callback)
             # As of python 2.7, the async_result is the only way to track
@@ -115,8 +149,11 @@ class ParallelFunction(MultiVoxelFunction):
             start = end
 
         del r
-        pool.close()
-        pool.join()
+        pool_done = False
+        while not pool_done:
+            time.sleep(3)
+            result = list(errors.values())
+            pool_done = all(r.ready() for r in result)
 
         if errors:
             index, r = errors.popitem()
