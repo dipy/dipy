b47298bcce22cf7511c8be6f2668b8796c7b0e0d
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-04-06 14:09:08 +0530

    Use integer division to avoid errors in indexing
    Fixes #1215

--- doc/examples/brain_extraction_dwi.py
+++ doc/examples/brain_extraction_dwi.py
@@ -61,7 +61,7 @@ Quick view of the results middle slice using matplotlib.
 import matplotlib.pyplot as plt
 from dipy.core.histeq import histeq
 
-sli = data.shape[2] / 2
+sli = data.shape[2] // 2
 plt.figure('Brain segmentation')
 plt.subplot(1, 2, 1).set_axis_off()
 plt.imshow(histeq(data[:, :, sli].astype('float')).T,
