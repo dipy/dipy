eba44ef37ba2bfad36abb11e8084b64b0ef70b8a
arokem <arokem@gmail.com>
2017-04-07 16:42:31 -0700

    Eliminate un-needed whitespace.

--- dipy/denoise/tests/test_non_local_means.py
+++ dipy/denoise/tests/test_non_local_means.py
@@ -9,8 +9,7 @@ from dipy.denoise.non_local_means import non_local_means
 
 def test_nlmeans_static():
     S0 = 100 * np.ones((20, 20, 20), dtype='f8')
-    S0nb = non_local_means(S0, sigma=1.0,
-                           rician=False)
+    S0nb = non_local_means(S0, sigma=1.0, rician=False)
     assert_array_almost_equal(S0, S0nb)
 
 
@@ -26,8 +25,7 @@ def test_nlmeans_random_noise():
         assert_(S0nb[mask].max() < S0[mask].max())
         assert_equal(np.round(S0nb[mask].mean()), 100)
 
-        S0nb = non_local_means(S0, sigma=np.std(S0), rician=False,
-                               mask=mask)
+        S0nb = non_local_means(S0, sigma=np.std(S0), rician=False, mask=mask)
 
         assert_(S0nb[mask].min() > S0[mask].min())
         assert_(S0nb[mask].max() < S0[mask].max())
@@ -46,21 +44,11 @@ def test_scalar_sigma():
 
 def test_nlmeans_boundary():
     # nlmeans preserves boundaries
-
     S0 = 100 + np.zeros((20, 20, 20))
-
     noise = 2 * np.random.standard_normal((20, 20, 20))
-
     S0 += noise
-
     S0[:10, :10, :10] = 300 + noise[:10, :10, :10]
-
-    S0n = non_local_means(S0, sigma=np.std(noise),
-                          rician=False)
-
-    print(S0[9, 9, 9])
-    print(S0[10, 10, 10])
-
+    S0n = non_local_means(S0, sigma=np.std(noise), rician=False)
     assert_(S0[9, 9, 9] > 290)
     assert_(S0[10, 10, 10] < 110)
 
@@ -75,10 +63,8 @@ def test_nlmeans_wrong():
 
 def test_nlmeans_4D_and_mask():
     S0 = 200 * np.ones((20, 20, 20, 3), dtype='f8')
-
     mask = np.zeros((20, 20, 20))
     mask[10, 10, 10] = 1
-
     S0n = non_local_means(S0, sigma=1, mask=mask, rician=True)
     assert_equal(S0.shape, S0n.shape)
     assert_equal(np.round(S0n[10, 10, 10]), 200)
@@ -92,15 +78,12 @@ def test_nlmeans_dtype():
     mask[10:14, 10:14, 10:14] = 1
     S0n = non_local_means(S0, sigma=1, mask=mask, rician=True)
     assert_equal(S0.dtype, S0n.dtype)
-
     S0 = 200 * np.ones((20, 20, 20), dtype=np.uint16)
     mask = np.zeros((20, 20, 20))
     mask[10:14, 10:14, 10:14] = 1
-    S0n = non_local_means(S0, sigma=1, mask=mask,
-                          rician=True)
+    S0n = non_local_means(S0, sigma=1, mask=mask, rician=True)
     assert_equal(S0.dtype, S0n.dtype)
 
 
 if __name__ == '__main__':
-
     run_module_suite()
