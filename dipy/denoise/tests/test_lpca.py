d3a7d1e153d6735db30d5a7944d57acde2f5bb02
arokem <arokem@gmail.com>
2017-04-14 16:50:12 -0700

    Fix up tests to remove the word "slow"
    And to use smaller sigma in the test.

--- dipy/denoise/tests/test_lpca.py
+++ dipy/denoise/tests/test_lpca.py
@@ -6,16 +6,15 @@ from numpy.testing import (run_module_suite,
                            assert_equal,
                            assert_raises,
                            assert_array_almost_equal)
-from dipy.denoise.localpca_slow import localpca_slow
+from dipy.denoise.localpca import localpca
 from dipy.sims.voxel import multi_tensor
-from dipy.core.gradients import gradient_table
+from dipy.core.gradients import gradient_table, generate_bvecs
 from dipy.core.sphere import disperse_charges, HemiSphere
 from dipy.sims.voxel import multi_tensor
 
 
 def rfiw_phantom(gtab, snr=None):
-    """retangle fiber immersed in water"""
-
+    """rectangle fiber immersed in water"""
     # define voxel index
     slice_ind = np.zeros((10, 10, 10))
     slice_ind[4:7, 4:7, :] = 1
@@ -89,6 +88,7 @@ def gen_gtab():
     hsph_updated, potential = disperse_charges(hsph_initial, 5000)
     directions8 = hsph_updated.vertices  # directions for each shell
 
+
     # Sample 30 diffusion-weighted directions for second shell
     n_pts = 30
     theta = np.pi * np.random.rand(n_pts)
@@ -119,13 +119,13 @@ def gen_gtab():
 
 def test_lpca_static():
     S0 = 100 * np.ones((20, 20, 20, 20), dtype='f8')
-    S0ns = localpca_slow(S0, sigma=np.ones((20, 20, 20), dtype=np.float64))
+    S0ns = localpca(S0, sigma=np.ones((20, 20, 20), dtype=np.float64))
     assert_array_almost_equal(S0, S0ns)
 
 
 def test_lpca_random_noise():
     S0 = 100 + 2 * np.random.standard_normal((22, 23, 30, 20))
-    S0ns = localpca_slow(S0, sigma=np.std(S0))
+    S0ns = localpca(S0, sigma=np.std(S0))
 
     assert_(S0ns.min() > S0.min())
     assert_(S0ns.max() < S0.max())
@@ -138,7 +138,7 @@ def test_lpca_boundary_behaviour():
     S0[:, :, 0, :] = S0[:, :, 0, :] + 2 * \
         np.random.standard_normal((20, 20, 20))
     S0_first = S0[:, :, 0, :]
-    S0ns = localpca_slow(S0, sigma=np.std(S0))
+    S0ns = localpca(S0, sigma=np.std(S0))
     S0ns_first = S0ns[:, :, 0, :]
     rmses = np.sum(np.abs(S0ns_first - S0_first)) / \
         (100.0 * 20.0 * 20.0 * 20.0)
@@ -150,7 +150,7 @@ def test_lpca_boundary_behaviour():
 
 def test_lpca_rmse():
     S0 = 100 + 2 * np.random.standard_normal((22, 23, 30, 20))
-    S0ns = localpca_slow(S0, sigma=np.std(S0))
+    S0ns = localpca(S0, sigma=np.std(S0))
     rmses = np.sum(np.abs(S0ns - 100) / np.sum(100 * np.ones(S0.shape)))
     # error should be less than 5%
     assert_(rmses < 0.05)
@@ -161,7 +161,7 @@ def test_lpca_sharpness():
     S0[10:20, 10:20, 10:20, :] = 50
     S0[20:30, 20:30, 20:30, :] = 0
     S0 = S0 + 20 * np.random.standard_normal((30, 30, 30, 20))
-    S0ns = localpca_slow(S0, sigma=400.0)
+    S0ns = localpca(S0, sigma=20.0)
     # check the edge gradient
     edgs = np.abs(np.mean(S0ns[8, 10:20, 10:20] - S0ns[12, 10:20, 10:20]) - 50)
     assert_(edgs < 2)
@@ -170,18 +170,18 @@ def test_lpca_sharpness():
 def test_lpca_dtype():
 
     S0 = 200 * np.ones((20, 20, 20, 3), dtype='f4')
-    S0ns = localpca_slow(S0, sigma=1)
+    S0ns = localpca(S0, sigma=1)
     assert_equal(S0.dtype, S0ns.dtype)
 
     S0 = 200 * np.ones((20, 20, 20, 20), dtype=np.uint16)
-    S0ns = localpca_slow(S0, sigma=np.ones((20, 20, 20)))
+    S0ns = localpca(S0, sigma=np.ones((20, 20, 20)))
     assert_equal(S0.dtype, S0ns.dtype)
 
 
 def test_lpca_wrong():
 
     S0 = np.ones((20, 20))
-    assert_raises(ValueError, localpca_slow, S0, sigma=1)
+    assert_raises(ValueError, localpca, S0, sigma=1)
 
 
 def test_phantom():
@@ -195,7 +195,7 @@ def test_phantom():
         (1 + 0.5 * temp) * sps.iv(
             0, 0.25 * temp) + 0.5 * temp * sps.iv(1, 0.25 * temp))**2
 
-    DWI_den = localpca_slow(DWI, sigma, patch_radius=3)
+    DWI_den = localpca(DWI, sigma, patch_radius=3)
     rmse_den = np.sum(np.abs(DWI_clean - DWI_den)) / np.sum(np.abs(DWI_clean))
     rmse_noisy = np.sum(np.abs(DWI_clean - DWI)) / np.sum(np.abs(DWI_clean))
 
