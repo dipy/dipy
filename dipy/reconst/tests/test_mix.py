6b4960ecca616031665cd333553b74b02b730958
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2017-05-12 10:56:33 -0400

    Moved norm_meas_Aax

--- dipy/reconst/tests/test_mix.py
+++ dipy/reconst/tests/test_mix.py
@@ -33,11 +33,32 @@ signal = np.array(data[0, 0, 0])
 
 signal_param = mix.make_signal_param(signal, bvals, bvecs, G, small_delta, big_delta)
 
+
+def norm_meas_Aax(signal):
+
+    """
+    normalizing the signal based on the b0 values of each shell
+    """
+    y = signal
+    y01 = (y[0] + y[1] + y[2]) / 3
+    y02 = (y[93] + y[94] + y[95]) / 3
+    y03 = (y[186] + y[187] + y[188]) / 3
+    y04 = (y[279] + y[280] + y[281]) / 3
+    y1 = y[0:93] / y01
+    y2 = y[93:186] / y02
+    y3 = y[186:279] / y03
+    y4 = y[279:372] / y04
+    f = np.concatenate((y1, y2, y3, y4))
+    return f
+
+
 """
 normalizing the signal based on the b0 values of each shell
 """
-    
 signal = mix.norm_meas_Aax(signal)
+
+
+
 
 
 def test_activax_exvivo_model():
@@ -86,7 +107,7 @@ def test_activax_exvivo_model():
     error_one =  mix.activeax_cost_one(phi, data[0, 0, 0])
 
     assert_almost_equal(error_one, 0.00037568453377497451)
-    
+
     yhat2_cylinder, yhat2_zeppelin, yhat2_ball, yhat2_dot = \
     mix.activax_exvivo_compartments2(x_fe, bvals, bvecs, G, small_delta, big_delta,
                           gamma=gamma,
@@ -96,18 +117,18 @@ def test_activax_exvivo_model():
     assert_almost_equal(yhat2_cylinder[6], 0.56056900197243242)
     assert_almost_equal(yhat2_ball[6], 26.399529350756438)
     assert_almost_equal(yhat2_dot[6], 0)
-    
+
     exp_phi = mix.activax_exvivo_model2(x_fe, bvals, bvecs, G, small_delta, big_delta,
                          gamma=gamma,
                          D_intra=0.6 * 10 ** 3, D_iso=2 * 10 ** 3,
                          debug=False)
-    
+
     assert_almost_equal(exp_phi[6,0],0.57088413721552789)
     assert_almost_equal(exp_phi[6,1],0.04541868892016445)
     assert_almost_equal(exp_phi[6,2],3.426337015990705e-12)
     assert_almost_equal(exp_phi[6,3],1)
-    
-        
+
+
 def test_estimate_x_and_f():
     x_fe = np.array([ 0.44623926,  0.2855913 ,  0.15918695,  2.68329756,  2.89085876, 3.40398589,  0.10898249])
     x_fe = np.squeeze(x_fe)
@@ -132,13 +153,13 @@ def test_activax_exvivo_estimate():
      [ 0.21049456]
      [ 0.15899651]]
     """
-    return fe  
+    return fe
 
 def test_final():
     x_fe = np.array([ 0.44623926,  0.2855913 ,  0.15918695,  2.68329756,  2.89085876, 3.40398589,  0.10898249])
-    bounds = ([0.01, 0.01,  0.01, 0.01, 0.01, 0.1, 0.01],[0.9,  0.9,  0.9,   np.pi, np.pi, 11, 0.9])      
+    bounds = ([0.01, 0.01,  0.01, 0.01, 0.01, 0.1, 0.01],[0.9,  0.9,  0.9,   np.pi, np.pi, 11, 0.9])
     res = least_squares(mix.estimate_x_and_f, x_fe, bounds = (bounds), args=(signal_param,))
-     
+
     """
     assert_array_equal()
     [[ 0.44553814,  0.28593944,  0.15950191,  2.6832976 ,  2.89085801,
@@ -153,5 +174,4 @@ def test_dif_evol():
     assert_array_equal()
     [[ 2.68329953,  2.89083525,  3.40510099,  0.6076389 ]]
     """
-    return res_one    
-  
+    return res_one
