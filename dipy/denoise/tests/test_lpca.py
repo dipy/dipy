55d89ee367b0b131e041fd851f6efe9dc2ba7359
arokem <arokem@gmail.com>
2017-06-16 10:21:33 -0700

    Use float32 precision, except where input is float64.

--- dipy/denoise/tests/test_lpca.py
+++ dipy/denoise/tests/test_lpca.py
@@ -158,6 +158,18 @@ def test_lpca_sharpness():
     assert_(edgs < 2)
 
 
+def test_lpca_dtype():
+    # If the input is float64, we retain that precision:
+    S0 = 200 * np.ones((20, 20, 20, 3), dtype=np.float64)
+    S0ns = localpca(S0, sigma=1)
+    assert_equal(S0.dtype, S0ns.dtype)
+
+    # If the input has any dtype except float64, it's returned as float32
+    S0 = 200 * np.ones((20, 20, 20, 20), dtype=np.uint16)
+    S0ns = localpca(S0, sigma=np.ones((20, 20, 20)))
+    assert_equal(np.float32, S0ns.dtype)
+
+
 def test_lpca_wrong():
     S0 = np.ones((20, 20))
     assert_raises(ValueError, localpca, S0, sigma=1)
