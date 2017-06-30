25a359729afe0cdc393ee6a170b41dfcb0421e07
Rutger Fick <rutger.fick@inria.fr>
2017-06-30 20:30:19 +0200

    changed cvxopt dependency to cvxpy as requested

--- dipy/reconst/tests/test_shore.py
+++ dipy/reconst/tests/test_shore.py
@@ -15,9 +15,9 @@ from numpy.testing import (assert_almost_equal,
                            dec)
 
 from dipy.utils.optpkg import optional_package
-cvxopt, have_cvxopt, _ = optional_package("cvxopt")
+cvxpy, have_cvxpy, _ = optional_package("cvxpy")
 
-needs_cvxopt = dec.skipif(not have_cvxopt)
+needs_cvxpy = dec.skipif(not have_cvxpy)
 
 
 # Object to hold module global data
@@ -40,7 +40,7 @@ def setup():
     data.lambdaL = 1e-12
 
 
-@needs_cvxopt
+@needs_cvxpy
 def test_shore_positive_constrain():
     asm = ShoreModel(data.gtab,
                      radial_order=data.radial_order,
@@ -64,7 +64,7 @@ def test_shore_fitting_no_constrain_e0():
     assert_almost_equal(compute_e0(asmfit), 1)
 
 
-@needs_cvxopt
+@needs_cvxpy
 def test_shore_fitting_constrain_e0():
     asm = ShoreModel(data.gtab, radial_order=data.radial_order,
                      zeta=data.zeta, lambdaN=data.lambdaN,
