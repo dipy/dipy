03caa0cd767c6b0dc8def4e65762fe9e5dd384cf
Maryam Afzali <maryam.afzali.m@gmail.com>
2017-08-18 10:04:28 -0400

    activeax

--- dipy/reconst/recspeed.pyx
+++ dipy/reconst/recspeed.pyx
@@ -643,10 +643,7 @@ cdef double fast_func_mul(double [:] x, double [:] am2, double [:] small_delta, 
 
             num = 2 * sd - 2 + 2 * exp(-sd) + 2 * exp(-bd) - exp(-(bd - sd)) - exp(-(bd + sd))
 
-#            num = 2 * sd - 2 + 2 * (cos(-sd) + 1j*sin(-sd)) + 2 * exp(-bd) - exp(-(bd - sd)) - exp(-(bd + sd))
-
             summ_rows[j] += num * idenom[i]
-
 
     free(idenom)
 #    return num
@@ -698,56 +695,6 @@ cdef double fast_S2(double [:] x2, double [:] bvals, double [:, :] bvecs, double
         # zeppelin
         yhat_zeppelin[i] = bvals[i] * ((D_intra - D_x2) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) + D_x2)
 
-#
-#@cython.boundscheck(False)
-#@cython.wraparound(False)
-#def S1(x1, bvecs, bvals, small_delta, big_delta, G, yhat_cylinder):
-#    fast_S1(x1, bvecs, bvals, small_delta, big_delta, G, yhat_cylinder)
-#
-#@cython.boundscheck(False)
-#@cython.wraparound(False)
-#@cython.cdivision(True)
-#cdef double fast_S1(double [:] x1, double [:, :] bvecs, double [:] bvals, double [:] small_delta, double [:] big_delta, double [:] G, double [:] yhat_cylinder) nogil:
-#    cdef double x1_0, x1_1, L, am2
-#    cdef cnp.npy_intp i, j
-#    cdef double n[3]
-#    cdef cnp.npy_intp M = small_delta.shape[0]
-#    cdef double D_intra = 0.6 * 10 ** 3
-#    cdef double gamma = 2.675987 * 10 ** 8
-#    cdef double am = 1.84118307861360
-#
-#    cdef double * summ_rows = <double *> calloc(M, sizeof(double))
-##    cdef double * am2 = <double *> calloc(60, sizeof(double))
-#    cdef double * g_per = <double *> calloc(M, sizeof(double))
-#    cdef double * L2 = <double *> calloc(M, sizeof(double))
-#    cdef double * L1 = <double *> calloc(M, sizeof(double))
-#
-#    x1_0 = x1[0]
-#    x1_1 = x1[1]
-#    sinT = sin(x1_0)
-#    cosT = cos(x1_0)
-#    sinP = sin(x1_1)
-#    cosP = cos(x1_1)
-#    n[0] = cosP * sinT
-#    n[1] = sinP * sinT
-#    n[2] = cosT
-#    # Cylinder
-#    for i in range(M):
-#        L = bvals[i] * D_intra
-#        L1[i] = L * (bvecs [i, 0]* n[0] + bvecs [i, 1] * n[1] + bvecs [i, 2] *n[2]) * (bvecs [i, 0]* n[0] + bvecs [i, 1] * n[1] + bvecs [i, 2] *n[2])
-#    am2 = (am / x1[2]) * (am / x1[2])
-#
-#    func_mul(x1, am2, small_delta, big_delta, summ_rows)
-#    func_bvec(bvecs, n, g_per)
-#    for i in range(M):
-#        L2[i] = 2 * (g_per[i] * gamma * gamma) * summ_rows[i] * G[i] * G[i]
-#    yhat_cylinder = L1 + L2
-#
-#    free(summ_rows)
-#    free(g_per)
-#    free(L2)
-#    free(L1)
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -761,15 +708,9 @@ cdef double fast_S2_new(double [:] x_fe, double [:] bvals, double [:, :] bvecs, 
     cdef cnp.npy_intp i
     cdef double x_0, x_1, fe0,  sinT, cosT, sinP, cosP, D_v, fe1
     cdef double n[3]
-#    cdef double fe[4]
-#    cdef double x[3]
     cdef cnp.npy_intp M = bvals.shape[0]
     cdef double D_intra = 600
 
-#    fe[0] = x_fe[0]
-#    fe[1] = x_fe[1]
-#    x[0] = x_fe[3]
-#    x[1] = x_fe[4]
     fe1 = x_fe[1]
     fe0 = x_fe[0]
     x_0 = x_fe[3]
@@ -787,3 +728,99 @@ cdef double fast_S2_new(double [:] x_fe, double [:] bvals, double [:, :] bvecs, 
     for i in range(M):
         # zeppelin
         yhat_zeppelin[i] = bvals[i] * ((D_intra - D_v) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) + D_v)
+
+
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def S1(x1, am1, bvecs, bvals, small_delta, big_delta, G2, yhat_cylinder):
+    fast_S1(x1, am1, bvecs, bvals, small_delta, big_delta, G2, yhat_cylinder)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef double fast_S1(double [:] x1, double[:] am1, double [:, :] bvecs, double [:] bvals, double [:] small_delta, double [:] big_delta, double [:] G2, double [:] yhat_cylinder) nogil:
+    cdef double x1_0, x1_1, L, am, g_per, sd, bd, D_intra_am, x2, L2
+    cdef cnp.npy_intp i, j
+    cdef double n[3]
+    cdef cnp.npy_intp K = small_delta.shape[0]
+    cdef double D_intra = 0.6 * 10 ** 3
+    cdef double gamma = 2.675987 * 10 ** 8
+    cdef cnp.npy_intp M = am1.shape[0]
+
+    cdef double * idenom = <double *> calloc(M, sizeof(double))
+    cdef double * summ_rows = <double *> calloc(M, sizeof(double))
+    cdef double * L1 = <double *> calloc(M, sizeof(double))
+
+    x1_0 = x1[0]
+    x1_1 = x1[1]
+    sinT = sin(x1_0)
+    cosT = cos(x1_0)
+    sinP = sin(x1_1)
+    cosP = cos(x1_1)
+    n[0] = cosP * sinT
+    n[1] = sinP * sinT
+    n[2] = cosT
+    # Cylinder
+    for i in range(K):
+        L = bvals[i] * D_intra
+        L1[i] = L * (bvecs [i, 0]* n[0] + bvecs [i, 1] * n[1] + bvecs [i, 2] *n[2]) * (bvecs [i, 0]* n[0] + bvecs [i, 1] * n[1] + bvecs [i, 2] *n[2])
+
+#    func_mul(x1, am2, small_delta, big_delta, summ_rows)
+
+    for i in range(M):
+        am = (am1[i] / x1[2]) * (am1[i] / x1[2])
+        D_intra_am = D_intra * am
+        idenom[i] = 1.0 / ((D_intra * D_intra) * (am * am * am) * (x2 * x2 * am - 1))
+
+        for j in range(K):
+
+            bd = D_intra_am * big_delta[j]
+
+            sd = D_intra_am * small_delta[j]
+
+            num = 2 * sd - 2 + 2 * exp(-sd) + 2 * exp(-bd) - exp(-(bd - sd)) - exp(-(bd + sd))
+
+            summ_rows[j] += num * idenom[i]
+
+    for i in range(K):
+        g_per = 1 - (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2])
+        L2 = 2 * (g_per * gamma * gamma) * summ_rows[i] * G2[i]
+        yhat_cylinder[i] = L1[i] + L2
+#    func_bvec(bvecs, n, g_per)
+#    for i in range(M):
+#                L2[i] = 2 * (g_per[i] * gamma * gamma) * summ_rows[i] * G2[i]
+#    yhat_cylinder = L1 + L2
+
+    free(summ_rows)
+    free(L1)
+    free(idenom)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def return_L1(L, bvecs, n, L1):
+    fast_return_L1(L, bvecs, n, L1)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef double fast_return_L1(double [:] L, double [:, :] bvecs, double [:] n, double [:] L1) nogil:
+    cdef cnp.npy_intp i
+    cdef cnp.npy_intp M = bvecs.shape[0]
+
+    for i in range(M):
+        L1[i] = L[i]*(bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2]) * (bvecs[i, 0] * n[0] + bvecs[i, 1] * n[1] + bvecs[i, 2] * n[2])
+
+
+
+
+
+
+
+
+
+
+
+
