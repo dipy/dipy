26af181070a267f00102ba797324acdbf61bb53b
Maryam Afzali <maryam.afzali.m@gmail.com>
2017-08-28 09:22:18 -0400

    example_activeax

--- doc/examples/example_activeax.py
+++ doc/examples/example_activeax.py
@@ -12,12 +12,10 @@ import numpy as np
 import nibabel as nib
 from dipy.core.gradients import gradient_table
 from dipy.data import get_data
-import dipy.reconst.mix as mix
 import dipy.reconst.activeax as activeax
-from scipy.optimize import differential_evolution
 from scipy.linalg import get_blas_funcs
 gemm = get_blas_funcs("gemm")
-
+#t1 = time()
 fname, fscanner = get_data('ActiveAx_synth_2d')
 params = np.loadtxt(fscanner)
 img = nib.load(fname)
@@ -34,10 +32,8 @@ bvals = bvals
 gtab = gradient_table(bvals, bvecs, big_delta=big_delta,
                       small_delta=small_delta,
                       b0_threshold=0, atol=1e-2)
-signal = np.array(data[0, 0, 0])
-
-signal_param = mix.make_signal_param(signal, bvals, bvecs, G, small_delta,
-                                     big_delta)
+# signal_param = mix.make_signal_param(signal, bvals, bvecs, G, small_delta,
+#                                     big_delta)
 
 
 def norm_meas_Aax(signal):
@@ -57,53 +53,40 @@ def norm_meas_Aax(signal):
     f = np.concatenate((y1, y2, y3, y4))
     return f
 
-
-signal = norm_meas_Aax(signal)
-signal = np.float64(signal)
-# jit decorator tells Numba to compile this function.
-# The argument types will be inferred by Numba when function is called.
-
-#t1 = time()
-#res_one1 = mix_fast.dif_evol(signal, bvals, bvecs, G, small_delta, big_delta)
-#t2 = time()
-#res_one = mix.dif_evol(signal, bvals, bvecs, G, small_delta, big_delta)
-#t3 = time()
-#
-#fast_time = t2 - t1
-#slow_time = t3 - t2
-#speedup = slow_time/fast_time
-#print(speedup)
-# print(mix_fast.cnt_stochastic)
-
-t1 = time()
 fit_method = 'MIX'
 activeax_model = activeax.ActiveAxModel(gtab, fit_method=fit_method)
-activeax_fit = activeax_model.fit(signal)
+activeax_fit = np.zeros((10, 10, 7))
+t1 = time()
+for i in range(1):
+    for j in range(1):
+        signal = np.array(data[i, j, 0])
+        signal = norm_meas_Aax(signal)
+        signal = np.float64(signal)
+        activeax_fit[i, j, :] = activeax_model.fit(signal)
 t2 = time()
-#fit_method = 'MIX'
-#activeax_model = activeax_fast.ActiveAxModel(gtab, fit_method=fit_method)
-#activeax_fit2 = activeax_model.fit(data[0, 0, 0])
-#t3 = time()
-
-#t1 = time()
-#bounds = [(0.01, np.pi), (0.01, np.pi), (0.1, 11), (0.1, 0.8)]
-#res_one = differential_evolution(activeax_model.stoc_search_cost, bounds,
-#                                 args=(signal,))
-#t2 = time()
-
-#res_one = mix.dif_evol(signal, bvals, bvecs, G, small_delta, big_delta)
-#t3 = time()
-
 fast_time = t2 - t1
 print(fast_time)
-#print(res_one.x)
-print(activeax.overall_duration)
-#slow_time = t3 - t2
-#speedup = slow_time/fast_time
-#print(speedup)
-#x = res_one.x
-#phi = activeax.Phi(x, gtab)
-#fe = mix.estimate_f(np.array(signal), phi)
+print(activeax_fit[0,0])
+#affine = img.affine.copy()
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 0], affine), 'f1_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 1], affine), 'f2_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 2], affine), 'f3_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 3], affine), 'theta_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 4], affine), 'phi_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 5], affine), 'R_activeax.nii.gz')
+#nib.save(nib.Nifti1Image(activeax_fit[:, :, 6], affine), 'f4_activeax.nii.gz')
+#t2 = time()
+#fast_time = t2 - t1
+#print(fast_time)
 
-#yhat_zeppelin1 = np.zeros(small_delta.shape[0])
-#S2_new(x_fe, bvals,  bvecs, yhat_zeppelin1)
+#from dipy.reconst.recspeed import func_mul, func_bvec, S2, S2_new, S1
+#am = np.array([1.84118307861360])
+#G2 = G ** 2
+#D_intra = 0.6 * 10 ** 3
+#L = gtab.bvals * D_intra
+#x1 = np.array([0.5, 0.5, 10, 0.8])
+#yhat_cylinder = np.zeros(small_delta.shape[0])
+#import pstats, cProfile
+#cProfile.runctx("S1(x1, am, gtab.bvecs, gtab.bvals, small_delta, big_delta, G2, L, yhat_cylinder)", globals(), locals(), "Profile.prof")
+#s = pstats.Stats("Profile.prof")
+#s.strip_dirs().sort_stats("time").print_stats()
