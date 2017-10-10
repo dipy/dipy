4da6a2d06a68754196a45c9b8a55589c5830d289
Serge Koudoro <skab12@gmail.com>
2017-10-10 20:51:50 +0200

    adding the new streamline module for reading and saving *.trk

---
+++ dipy/io/streamline.py
@@ -0,0 +1,55 @@
+import nibabel as nib
+from nibabel.streamlines import Field
+from nibabel.orientations import aff2axcodes
+
+
+def save_trk(fname, streamlines, affine, vox_size=None, shape=None, hdr=None):
+    """ function Helper for saving trk files.
+
+    Parameters
+    ----------
+    fname : str
+        output trk filename
+    streamlines : list of 2D arrays
+        Each 2D array represents a sequence of 3D points (points, 3).
+    affine : array_like (4, 4)
+        The mapping from voxel coordinates to streamline points.
+    vox_size : array_like (3,)
+        The sizes of the voxels in the reference image.
+    shape : array, shape (dim,)
+        The shape of the reference image.
+    hdr : dict
+        header from a trk file
+
+    """
+    if vox_size and shape:
+        header = hdr if hdr else {}
+        header[Field.VOXEL_TO_RASMM] = affine.copy()
+        header[Field.VOXEL_SIZES] = vox_size
+        header[Field.DIMENSIONS] = shape
+        header[Field.VOXEL_ORDER] = "".join(aff2axcodes(affine))
+
+    tractogram = nib.streamlines.Tractogram(streamlines)
+    tractogram.affine_to_rasmm = affine
+    trk_file = nib.streamlines.TrkFile(tractogram, header=hdr)
+    nib.streamlines.save(trk_file, fname)
+
+
+def load_trk(filename):
+    """ function Helper for Loading trk files.
+
+    Parameters
+    ----------
+    filename : str
+        input trk filename
+
+    Returns:
+    -------
+    streamlines : list of 2D arrays
+        Each 2D array represents a sequence of 3D points (points, 3).
+    hdr : dict
+        header from a trk file
+
+    """
+    trk_file = nib.streamlines.load(filename)
+    return trk_file.streamlines, trk_file.header
