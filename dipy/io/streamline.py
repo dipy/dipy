42c01be317deae7d420086ab152b5bedc8c696af
skoudoro <skoudoro@gmail.com>
2017-10-19 11:14:36 -0400

    header correction in save_trk

--- dipy/io/streamline.py
+++ dipy/io/streamline.py
@@ -3,7 +3,7 @@ from nibabel.streamlines import Field
 from nibabel.orientations import aff2axcodes
 
 
-def save_trk(fname, streamlines, affine, vox_size=None, shape=None, hdr=None):
+def save_trk(fname, streamlines, affine, vox_size=None, shape=None, header=None):
     """ function Helper for saving trk files.
 
     Parameters
@@ -18,12 +18,13 @@ def save_trk(fname, streamlines, affine, vox_size=None, shape=None, hdr=None):
         The sizes of the voxels in the reference image.
     shape : array, shape (dim,)
         The shape of the reference image.
-    hdr : dict
+    header : dict
         header from a trk file
 
     """
     if vox_size and shape:
-        header = hdr if hdr else {}
+        if not isinstance(header, dict):
+            header = {}
         header[Field.VOXEL_TO_RASMM] = affine.copy()
         header[Field.VOXEL_SIZES] = vox_size
         header[Field.DIMENSIONS] = shape
@@ -31,7 +32,7 @@ def save_trk(fname, streamlines, affine, vox_size=None, shape=None, hdr=None):
 
     tractogram = nib.streamlines.Tractogram(streamlines)
     tractogram.affine_to_rasmm = affine
-    trk_file = nib.streamlines.TrkFile(tractogram, header=hdr)
+    trk_file = nib.streamlines.TrkFile(tractogram, header=header)
     nib.streamlines.save(trk_file, fname)
 
 
@@ -43,7 +44,7 @@ def load_trk(filename):
     filename : str
         input trk filename
 
-    Returns:
+    Returns
     -------
     streamlines : list of 2D arrays
         Each 2D array represents a sequence of 3D points (points, 3).
