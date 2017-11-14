114cb7619737127228b0cfc48947a1b888477caa
Nil Goyette <nil.goyette@imeka.ca>
2017-11-14 14:37:56 -0500

    Add a test for the piesno type overflow

--- dipy/denoise/nlmeans.py
+++ dipy/denoise/nlmeans.py
@@ -1,17 +1,15 @@
 from __future__ import division, print_function
 
+from datetime import datetime
+
+import nibabel as nib
 import numpy as np
 from dipy.denoise.denspeed import nlmeans_3d
-# from warnings import warn
-# import warnings
-
-# warnings.simplefilter('always', DeprecationWarning)
-# warn(DeprecationWarning("Module 'dipy.denoise.nlmeans' is deprecated,"
-#                         " use module 'dipy.denoise.non_local_means' instead"))
+from dipy.denoise.noise_estimate import estimate_sigma, piesno
 
 
 def nlmeans(arr, sigma, mask=None, patch_radius=1, block_radius=5,
-            rician=True, num_threads=None):
+            rician=False, num_threads=None):
     r""" Non-local means for denoising 3D and 4D images
 
     Parameters
@@ -45,12 +43,6 @@ def nlmeans(arr, sigma, mask=None, patch_radius=1, block_radius=5,
                       HARDI, MICCAI 2008
 
     """
-
-    # warn(DeprecationWarning("function 'dipy.denoise.nlmeans'"
-    #                        " is deprecated, use module "
-    #                        "'dipy.denoise.non_local_means'"
-    #                        " instead"))
-
     if arr.ndim == 3:
         sigma = np.ones(arr.shape, dtype=np.float64) * sigma
         return nlmeans_3d(arr, mask, sigma,
@@ -67,6 +59,7 @@ def nlmeans(arr, sigma, mask=None, patch_radius=1, block_radius=5,
             sigma = np.ones(arr.shape, dtype=np.float64) * sigma
 
         for i in range(arr.shape[-1]):
+            start = datetime.now()
             denoised_arr[..., i] = nlmeans_3d(arr[..., i],
                                               mask,
                                               sigma[..., i],
@@ -74,8 +67,41 @@ def nlmeans(arr, sigma, mask=None, patch_radius=1, block_radius=5,
                                               block_radius,
                                               rician,
                                               num_threads).astype(arr.dtype)
+            print(datetime.now() - start)
 
         return denoised_arr
 
     else:
         raise ValueError("Only 3D or 4D array are supported!", arr.shape)
+
+
+def main():
+    # input_path = '/home/nil/libs/scilpy-data/fa.nii.gz'
+    # output_path = '/home/nil/libs/scilpy-data/fa_nlm.nii.gz'
+    input_path = '/home/nil/dwi.nii.gz'
+    output_path = '/home/nil/dwi_nlm.nii.gz'
+    # input_path = '/home/nil/libs/scilpy-data/test.nii.gz'
+    # output_path = '/home/nil/libs/scilpy-data/dwi_nlmeans.nii.gz'
+
+    print('Denoising {0}'.format(input_path))
+    image = nib.load(input_path)
+    data = image.get_data()
+    print(data.shape)
+    # data = data[20:124, 20:168, 20:130]
+
+    print('Estimating sigma')
+    #sigma = estimate_sigma(data)
+    sigma = piesno(data, 1)
+    print('Found sigma {0}'.format(sigma))
+
+    start = datetime.now()
+    denoised_data = nlmeans(data, sigma, patch_radius=6, block_radius=5)
+    print('Total time:', datetime.now() - start)
+    denoised_image = nib.Nifti1Image(
+        denoised_data, image.affine, image.header)
+
+    denoised_image.to_filename(output_path)
+    print('Denoised volume saved as {0}'.format(output_path))
+
+
+main()
