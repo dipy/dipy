1e69ffb04ab19b8464c4a87430cdb8072e6a55ca
arokem <arokem@gmail.com>
2018-01-01 11:21:12 -0800

    Adds a DKI workflow.

--- dipy/workflows/reconst.py
+++ dipy/workflows/reconst.py
@@ -20,6 +20,7 @@ from dipy.reconst.dti import (TensorModel, color_fa, fractional_anisotropy,
 from dipy.reconst.peaks import peaks_from_model
 from dipy.reconst.shm import CsaOdfModel
 from dipy.workflows.workflow import Workflow
+from dipy.reconst.dki import DiffusionKurtosisModel
 
 
 class ReconstDtiFlow(Workflow):
@@ -298,8 +299,8 @@ class ReconstCSDFlow(Workflow):
         """
         io_it = self.get_io_iterator()
 
-        for dwi, bval, bvec, maskfile, opam, oshm, opeaks_dir, opeaks_values, \
-                opeaks_indices, ogfa in io_it:
+        for (dwi, bval, bvec, maskfile, opam, oshm, opeaks_dir, opeaks_values,
+             opeaks_indices, ogfa) in io_it:
 
             logging.info('Loading {0}'.format(dwi))
             img = nib.load(dwi)
@@ -456,8 +457,8 @@ class ReconstCSAFlow(Workflow):
         """
         io_it = self.get_io_iterator()
 
-        for dwi, bval, bvec, maskfile, opam, oshm, opeaks_dir, \
-                opeaks_values, opeaks_indices, ogfa in io_it:
+        for (dwi, bval, bvec, maskfile, opam, oshm, opeaks_dir,
+             opeaks_values, opeaks_indices, ogfa) in io_it:
 
             logging.info('Loading {0}'.format(dwi))
             vol = nib.load(dwi)
@@ -470,6 +471,25 @@ class ReconstCSAFlow(Workflow):
             mask_vol = nib.load(maskfile).get_data().astype(np.bool)
 
             peaks_sphere = get_sphere('repulsion724')
+
+            sh_order = 8
+            if data.shape[-1] < 15:
+                raise ValueError(
+                    'You need at least 15 unique DWI volumes to '
+                    'compute fiber odfs. You currently have: {0}'
+                    ' DWI volumes.'.format(data.shape[-1]))
+            elif data.shape[-1] < 30:
+                sh_order = 6
+
+            response, ratio = auto_response(gtab, data)
+            response = list(response)
+
+            logging.info(
+                'Eigenvalues for the frf of the input data are :{0}'
+                .format(response[0]))
+            logging.info(
+                'Ratio for smallest to largest eigen value is {0}'
+                .format(ratio))
 
             logging.info('Starting CSA computations {0}'.format(dwi))
 
@@ -504,3 +524,179 @@ class ReconstCSAFlow(Workflow):
                         'Pam5 file saved in {0}'.format(dname_))
 
             return io_it
+
+
+class ReconstDkiFlow(Workflow):
+    @classmethod
+    def get_short_name(cls):
+        return 'dki'
+
+    def run(self, input_files, bvalues, bvectors, mask_files, b0_threshold=0.0,
+            save_metrics=[],
+            out_dir='', out_tensor='tensors.nii.gz', out_fa='fa.nii.gz',
+            out_ga='ga.nii.gz', out_rgb='rgb.nii.gz', out_md='md.nii.gz',
+            out_ad='ad.nii.gz', out_rd='rd.nii.gz', out_mode='mode.nii.gz',
+            out_evec='evecs.nii.gz', out_eval='evals.nii.gz',
+            out_mk="mk.nii.gz", out_ak="ak.nii.gz", out_rk="rk.nii.gz"):
+        """ Workflow for Diffusion Kurtosis reconstruction and for computing
+        DKI metrics. Performs a DKI reconstruction on the files by 'globing'
+        ``input_files`` and saves the DTI metrics in a directory specified by
+        ``out_dir``.
+
+        Parameters
+        ----------
+        input_files : string
+            Path to the input volumes. This path may contain wildcards to
+            process multiple inputs at once.
+        bvalues : string
+            Path to the bvalues files. This path may contain wildcards to use
+            multiple bvalues files at once.
+        bvectors : string
+            Path to the bvalues files. This path may contain wildcards to use
+            multiple bvalues files at once.
+        mask_files : string
+            Path to the input masks. This path may contain wildcards to use
+            multiple masks at once. (default: No mask used)
+        b0_threshold : float, optional
+            Threshold used to find b=0 directions (default 0.0)
+        save_metrics : variable string, optional
+            List of metrics to save.
+            Possible values: fa, ga, rgb, md, ad, rd, mode, tensor, evec, eval
+            (default [] (all))
+        out_dir : string, optional
+            Output directory (default input file directory)
+        out_tensor : string, optional
+            Name of the tensors volume to be saved (default 'tensors.nii.gz')
+        out_fa : string, optional
+            Name of the fractional anisotropy volume to be saved
+            (default 'fa.nii.gz')
+        out_ga : string, optional
+            Name of the geodesic anisotropy volume to be saved
+            (default 'ga.nii.gz')
+        out_rgb : string, optional
+            Name of the color fa volume to be saved (default 'rgb.nii.gz')
+        out_md : string, optional
+            Name of the mean diffusivity volume to be saved
+            (default 'md.nii.gz')
+        out_ad : string, optional
+            Name of the axial diffusivity volume to be saved
+            (default 'ad.nii.gz')
+        out_rd : string, optional
+            Name of the radial diffusivity volume to be saved
+            (default 'rd.nii.gz')
+        out_mode : string, optional
+            Name of the mode volume to be saved (default 'mode.nii.gz')
+        out_evec : string, optional
+            Name of the eigenvectors volume to be saved
+            (default 'evecs.nii.gz')
+        out_eval : string, optional
+            Name of the eigenvalues to be saved (default 'evals.nii.gz')
+        """
+        io_it = self.get_io_iterator()
+
+        for (dwi, bval, bvec, mask, otensor, ofa, oga, orgb, omd, oad, orad,
+             omode, oevecs, oevals, omk, oak, ork) in io_it:
+
+            logging.info('Computing DKI metrics for {0}'.format(dwi))
+            img = nib.load(dwi)
+            data = img.get_data()
+            affine = img.affine
+
+            if mask is None:
+                mask = None
+            else:
+                mask = nib.load(mask).get_data().astype(np.bool)
+
+            dkfit, _ = self.get_fitted_tensor(data, mask, bval, bvec,
+                                              b0_threshold)
+
+            if not save_metrics:
+                save_metrics = ['mk', 'rk', 'ak', 'fa', 'md', 'rd', 'ad', 'ga',
+                                'rgb', 'mode', 'evec', 'eval', 'tensor']
+
+            FA = fractional_anisotropy(dkfit.evals)
+            FA[np.isnan(FA)] = 0
+            FA = np.clip(FA, 0, 1)
+
+            if 'tensor' in save_metrics:
+                tensor_vals = lower_triangular(dkfit.quadratic_form)
+                correct_order = [0, 1, 3, 2, 4, 5]
+                tensor_vals_reordered = tensor_vals[..., correct_order]
+                fiber_tensors = nib.Nifti1Image(tensor_vals_reordered.astype(
+                    np.float32), affine)
+                nib.save(fiber_tensors, otensor)
+
+            if 'fa' in save_metrics:
+                fa_img = nib.Nifti1Image(FA.astype(np.float32), affine)
+                nib.save(fa_img, ofa)
+
+            if 'ga' in save_metrics:
+                GA = geodesic_anisotropy(dkfit.evals)
+                ga_img = nib.Nifti1Image(GA.astype(np.float32), affine)
+                nib.save(ga_img, oga)
+
+            if 'rgb' in save_metrics:
+                RGB = color_fa(FA, dkfit.evecs)
+                rgb_img = nib.Nifti1Image(np.array(255 * RGB, 'uint8'), affine)
+                nib.save(rgb_img, orgb)
+
+            if 'md' in save_metrics:
+                MD = mean_diffusivity(dkfit.evals)
+                md_img = nib.Nifti1Image(MD.astype(np.float32), affine)
+                nib.save(md_img, omd)
+
+            if 'ad' in save_metrics:
+                AD = axial_diffusivity(dkfit.evals)
+                ad_img = nib.Nifti1Image(AD.astype(np.float32), affine)
+                nib.save(ad_img, oad)
+
+            if 'rd' in save_metrics:
+                RD = radial_diffusivity(dkfit.evals)
+                rd_img = nib.Nifti1Image(RD.astype(np.float32), affine)
+                nib.save(rd_img, orad)
+
+            if 'mode' in save_metrics:
+                MODE = get_mode(dkfit.quadratic_form)
+                mode_img = nib.Nifti1Image(MODE.astype(np.float32), affine)
+                nib.save(mode_img, omode)
+
+            if 'evec' in save_metrics:
+                evecs_img = nib.Nifti1Image(dkfit.evecs.astype(np.float32),
+                                            affine)
+                nib.save(evecs_img, oevecs)
+
+            if 'eval' in save_metrics:
+                evals_img = nib.Nifti1Image(dkfit.evals.astype(np.float32),
+                                            affine)
+                nib.save(evals_img, oevals)
+
+            if 'mk' in save_metrics:
+                mk_img = nib.Nifti1Image(dkfit.mk().astype(np.float32),
+                                     affine)
+                nib.save(mk_img, omk)
+
+            if 'ak' in save_metrics:
+                ak_img = nib.Nifti1Image(dkfit.ak().astype(np.float32),
+                                     affine)
+                nib.save(ak_img, oak)
+
+            if 'rk' in save_metrics:
+                rk_img = nib.Nifti1Image(dkfit.rk().astype(np.float32),
+                                     affine)
+                nib.save(rk_img, ork)
+
+            logging.info('DKI metrics saved in {0}'.
+                         format(os.path.dirname(oevals)))
+
+    def get_tensor_model(self, gtab):
+        return DiffusionKurtosisModel(gtab)
+
+    def get_fitted_tensor(self, data, mask, bval, bvec, b0_threshold=0):
+        logging.info('Diffusion kurtosis estimation...')
+        bvals, bvecs = read_bvals_bvecs(bval, bvec)
+        gtab = gradient_table(bvals, bvecs, b0_threshold=b0_threshold)
+
+        dkmodel = self.get_tensor_model(gtab)
+        dkfit = dkmodel.fit(data, mask)
+
+        return dkmodel, gtab
