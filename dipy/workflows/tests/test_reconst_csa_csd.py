21a1032fba59878a002952712ed095a9f6837c53
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2018-01-11 16:54:51 -0500

    TEST: for frf

--- dipy/workflows/tests/test_reconst_csa_csd.py
+++ dipy/workflows/tests/test_reconst_csa_csd.py
@@ -68,6 +68,11 @@ def reconst_flow_core(flow):
         npt.assert_allclose(pam.shm_coeff, shm_data)
         npt.assert_allclose(pam.gfa, gfa_data)
 
+        if isinstance(flow, ReconstCSDFlow):
+            reconst_flow = flow()
+            reconst_flow.run(data_path, bval_path, bvec_path, mask_path,
+                             out_dir=out_dir, frf=[15, 5, 5])
+
 
 if __name__ == '__main__':
     npt.run_module_suite()
