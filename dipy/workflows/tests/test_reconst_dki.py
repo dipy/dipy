b96b42d27f8a50a49590a6f0e94afd5bb4ed1a43
arokem <arokem@gmail.com>
2018-01-31 22:18:48 -0800

    assert_true => assert_equal

--- dipy/workflows/tests/test_reconst_dki.py
+++ dipy/workflows/tests/test_reconst_dki.py
@@ -5,7 +5,7 @@ from nibabel.tmpdirs import TemporaryDirectory
 
 import numpy as np
 
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_equal
 
 from dipy.data import get_data
 from dipy.workflows.reconst import ReconstDkiFlow
@@ -29,64 +29,64 @@ def test_reconst_dki():
 
         fa_path = dki_flow.last_generated_outputs['out_fa']
         fa_data = nib.load(fa_path).get_data()
-        assert_true(fa_data.shape == volume.shape[:-1])
+        assert_equal(fa_data.shape, volume.shape[:-1])
 
         tensor_path = dki_flow.last_generated_outputs['out_dt_tensor']
         tensor_data = nib.load(tensor_path)
-        assert_true(tensor_data.shape[-1] == 6)
-        assert_true(tensor_data.shape[:-1] == volume.shape[:-1])
+        assert_equal(tensor_data.shape[-1], 6)
+        assert_equal(tensor_data.shape[:-1], volume.shape[:-1])
 
         ga_path = dki_flow.last_generated_outputs['out_ga']
         ga_data = nib.load(ga_path).get_data()
-        assert_true(ga_data.shape == volume.shape[:-1])
+        assert_equal(ga_data.shape, volume.shape[:-1])
 
         rgb_path = dki_flow.last_generated_outputs['out_rgb']
         rgb_data = nib.load(rgb_path)
-        assert_true(rgb_data.shape[-1] == 3)
-        assert_true(rgb_data.shape[:-1] == volume.shape[:-1])
+        assert_equal(rgb_data.shape[-1], 3)
+        assert_equal(rgb_data.shape[:-1], volume.shape[:-1])
 
         md_path = dki_flow.last_generated_outputs['out_md']
         md_data = nib.load(md_path).get_data()
-        assert_true(md_data.shape == volume.shape[:-1])
+        assert_equal(md_data.shape, volume.shape[:-1])
 
         ad_path = dki_flow.last_generated_outputs['out_ad']
         ad_data = nib.load(ad_path).get_data()
-        assert_true(ad_data.shape == volume.shape[:-1])
+        assert_equal(ad_data.shape, volume.shape[:-1])
 
         rd_path = dki_flow.last_generated_outputs['out_rd']
         rd_data = nib.load(rd_path).get_data()
-        assert_true(rd_data.shape == volume.shape[:-1])
+        assert_equal(rd_data.shape, volume.shape[:-1])
 
         mk_path = dki_flow.last_generated_outputs['out_mk']
         mk_data = nib.load(mk_path).get_data()
-        assert_true(mk_data.shape == volume.shape[:-1])
+        assert_equal(mk_data.shape, volume.shape[:-1])
 
         ak_path = dki_flow.last_generated_outputs['out_ak']
         ak_data = nib.load(ak_path).get_data()
-        assert_true(ak_data.shape == volume.shape[:-1])
+        assert_equal(ak_data.shape, volume.shape[:-1])
 
         rk_path = dki_flow.last_generated_outputs['out_rk']
         rk_data = nib.load(rk_path).get_data()
-        assert_true(rk_data.shape == volume.shape[:-1])
+        assert_equal(rk_data.shape, volume.shape[:-1])
 
         kt_path = dki_flow.last_generated_outputs['out_dk_tensor']
         kt_data = nib.load(kt_path)
-        assert_true(kt_data.shape[-1] == 15)
-        assert_true(kt_data.shape[:-1] == volume.shape[:-1])
+        assert_equal(kt_data.shape[-1], 15)
+        assert_equal(kt_data.shape[:-1], volume.shape[:-1])
 
         mode_path = dki_flow.last_generated_outputs['out_mode']
         mode_data = nib.load(mode_path).get_data()
-        assert_true(mode_data.shape == volume.shape[:-1])
+        assert_equal(mode_data.shape, volume.shape[:-1])
 
         evecs_path = dki_flow.last_generated_outputs['out_evec']
         evecs_data = nib.load(evecs_path).get_data()
-        assert_true(evecs_data.shape[-2:] == tuple((3, 3)))
-        assert_true(evecs_data.shape[:-2] == volume.shape[:-1])
+        assert_equal(evecs_data.shape[-2:], tuple((3, 3)))
+        assert_equal(evecs_data.shape[:-2], volume.shape[:-1])
 
         evals_path = dki_flow.last_generated_outputs['out_eval']
         evals_data = nib.load(evals_path).get_data()
-        assert_true(evals_data.shape[-1] == 3)
-        assert_true(evals_data.shape[:-1] == volume.shape[:-1])
+        assert_equal(evals_data.shape[-1], 3)
+        assert_equal(evals_data.shape[:-1], volume.shape[:-1])
 
 
 if __name__ == '__main__':
