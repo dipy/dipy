9218ee40cc5806bc12c09712236499facf264e20
Gabriel Girard <girard.gabriel@gmail.com>
2018-02-11 18:28:05 +0100

    RF - Cythonize bootstrap_direction_getter

--- dipy/direction/pmf.pyx
+++ dipy/direction/pmf.pyx
@@ -2,9 +2,14 @@
 # cython: initializedcheck=False
 # cython: wraparound=False
 
-import numpy as np
+from warnings import warn
 
-from dipy.reconst.shm import order_from_ncoef, sph_harm_lookup
+import numpy as np
+cimport numpy as np
+
+from dipy.core.geometry import cart2sphere
+from dipy.reconst import shm
+
 from dipy.tracking.local.interpolation cimport trilinear_interpolate4d_c
 
 
@@ -13,7 +18,7 @@ cdef class PmfGen:
     cpdef double[:] get_pmf(self, double[::1] point):
         return self.get_pmf_c(&point[0])
 
-    cdef double[:] get_pmf_c(self, double* point) nogil:
+    cdef double[:] get_pmf_c(self, double* point):
         pass
 
 
@@ -28,9 +33,10 @@ cdef class SimplePmfGen(PmfGen):
 
         self.out = np.empty(pmf_array.shape[3])
 
-    cdef double[:] get_pmf_c(self, double* point) nogil:
-        trilinear_interpolate4d_c(self.pmf_array, point, self.out)
-        return self.out
+    cdef double[:] get_pmf_c(self, double* point):
+        if trilinear_interpolate4d_c(self.pmf_array, point, self.out) == 0:
+            return self.out
+        return None
 
 
 cdef class SHCoeffPmfGen(PmfGen):
@@ -46,29 +52,110 @@ cdef class SHCoeffPmfGen(PmfGen):
 
         self.shcoeff = shcoeff
         self.sphere = sphere
-        sh_order = order_from_ncoef(shcoeff.shape[3])
+        sh_order = shm.order_from_ncoef(shcoeff.shape[3])
         try:
-            basis = sph_harm_lookup[basis_type]
+            basis = shm.sph_harm_lookup[basis_type]
         except KeyError:
             raise ValueError("%s is not a known basis type." % basis_type)
         self.B, m, n = basis(sh_order, sphere.theta, sphere.phi)
         self.coeff = np.empty(self.shcoeff.shape[3])
         self.pmf = np.empty(self.B.shape[0])
 
-    cdef double[:] get_pmf_c(self, double* point) nogil:
+    cdef double[:] get_pmf_c(self, double* point):
         cdef:
             size_t i, j
             size_t len_pmf = self.pmf.shape[0]
             size_t len_B = self.B.shape[1]
             double _sum
 
-        trilinear_interpolate4d_c(self.shcoeff, point, self.coeff)
-        for i in range(len_pmf):
-            _sum = 0
-            for j in range(len_B):
-                _sum += self.B[i, j] * self.coeff[j]
-            self.pmf[i] = _sum
-            if self.pmf[i] < 0.0:
-                self.pmf[i] = 0.0
+        if trilinear_interpolate4d_c(self.shcoeff, point, self.coeff) == 0:
+            for i in range(len_pmf):
+                _sum = 0
+                for j in range(len_B):
+                    _sum += self.B[i, j] * self.coeff[j]
+                self.pmf[i] = _sum
+                if self.pmf[i] < 0.0:
+                    self.pmf[i] = 0.0
+            return self.pmf
+        return None
 
-        return self.pmf
+
+cdef class BootPmfGen(SHCoeffPmfGen):
+
+    def __init__(self,
+                 np.ndarray data,
+                 object model,
+                 object sphere,
+                 int sh_order=0,
+                 double tol=1e-2):
+        cdef:
+            double b_range
+            np.ndarray x, y, z, r
+            double[:] theta, phi
+            double[:, :] B
+
+        self.sh_order = sh_order
+        if self.sh_order == 0:
+            if hasattr(model, "sh_order"):
+                self.sh_order = model.sh_order
+            else:
+                self.sh_order = 4 #  DEFAULT Value
+
+        x, y, z = model.gtab.gradients[model.gtab.b0s_mask == 0].T
+        r, theta, phi = shm.cart2sphere(x, y, z)
+        b_range = (r.max() - r.min()) / r.min()
+        if b_range > tol:
+            raise ValueError("BootPmfGen only supports single shell data")
+        B, m, n = shm.real_sym_sh_basis(self.sh_order, theta, phi)
+
+        self.nbr_b0s = np.sum(model.gtab.b0s_mask == 1)
+        self.nbr_dwi = np.sum(model.gtab.b0s_mask == 0)
+        self.nbr_data = data.shape[3]
+        self.vox_b0s = np.empty(self.nbr_b0s)
+        self.vox_dwi = np.empty(self.nbr_dwi)
+        self.vox_data = np.empty(self.nbr_data)
+        self.model = model
+        self.sphere = sphere
+        self.H = shm.hat(B)
+        self.R = shm.lcr_matrix(self.H)
+        self.pmf = np.empty(self.B.shape[0])
+        self.b0s = np.asarray(data[:,:,:,model.gtab.b0s_mask == 1], "float64")
+        self.dwi = np.asarray(data[:,:,:,model.gtab.b0s_mask == 0], "float64")
+
+
+    cdef double[:] get_pmf_c(self, double* point):
+        """Produces an ODF from a SH bootstrap sample"""
+        if (trilinear_interpolate4d_c(self.dwi, point, self.vox_dwi) == 0 and
+                trilinear_interpolate4d_c(self.b0s, point, self.vox_b0s) == 0):
+            self.vox_dwi = shm.bootstrap_data_voxel(self.vox_dwi, self.H,
+                                                    self.R)
+            self._set_vox_data()
+            self.pmf = self.model.fit(self.vox_data).odf(self.sphere)
+            return self.pmf
+        return None
+
+
+    cpdef double[:] get_pmf_no_boot(self, double[::1] point):
+        return self.get_pmf_no_boot_c(&point[0])
+
+
+    cdef double[:] get_pmf_no_boot_c(self, double* point):
+        if (trilinear_interpolate4d_c(self.dwi, point, self.vox_dwi) == 0 and
+                trilinear_interpolate4d_c(self.b0s, point, self.vox_b0s) == 0):
+            self._set_vox_data()
+            self.pmf = self.model.fit(self.vox_data).odf(self.sphere)
+            return self.pmf
+        return None
+
+
+    cdef int _set_vox_data(self):
+        cdef:
+            size_t i, j
+        j = 0
+        for i in range(self.nbr_data):
+            if self.model.gtab.b0s_mask[i] == 1:
+                self.vox_data[i] = self.vox_b0s[j]
+                j = j + 1
+            else:
+                self.vox_data[i] = self.vox_dwi[i-j]
+        return 0
--- dipy/direction/tests/test_bootstrap_direction_getter.py
+++ dipy/direction/tests/test_bootstrap_direction_getter.py
@@ -5,266 +5,232 @@ from dipy.core.geometry import cart2sphere
 from dipy.core.gradients import gradient_table
 from dipy.core.sphere import HemiSphere, unit_icosahedron
 
+from dipy.direction.bootstrap_direction_getter import BootDirectionGetter
+from dipy.direction.pmf import BootPmfGen, SimplePmfGen
 from dipy.reconst import shm
 from dipy.reconst import dti
-import dipy.reconst.csdeconv as csd
-
+from dipy.reconst.csdeconv import ConstrainedSphericalDeconvModel
 from dipy.sims.voxel import single_tensor, multi_tensor
 
-from dipy.tracking import utils
-from dipy.tracking.local import LocalTracking, ThresholdTissueClassifier
-from dipy.direction import DeterministicMaximumDirectionGetter
 
-import dipy.direction.bootstrap_direction_getter as bdg
+from warnings import warn
+import numpy as np
+
+from dipy.reconst import shm
+from dipy.core.geometry import cart2sphere
+from dipy.tracking.local.interpolation import trilinear_interpolate4d
+
+from dipy.data import default_sphere
 
 
-# Set up the toy example
-def uniform_toy_data():
-    toydict = {}
+DEFAULT_SH = 4
 
-    hsph_updated = HemiSphere.from_sphere(unit_icosahedron).subdivide(2)
 
-    vertices = hsph_updated.vertices
+class BootOdfGen2(object):
 
-    bvecs = vertices
-    bvals = np.ones(len(vertices)) * 1000
+    def __init__(self, data, model, sphere, sh_order=None, tol=1e-2):
+        if sh_order is None:
+            if hasattr(model, "sh_order"):
+                sh_order = model.sh_order
+            else:
+                sh_order = DEFAULT_SH
 
-    bvecs = np.insert(bvecs, 0, np.array([0, 0, 0]), axis=0)
-    bvals = np.insert(bvals, 0, 0)
+        self.where_dwi = shm.lazy_index(~model.gtab.b0s_mask)
+        if not isinstance(self.where_dwi, slice):
+            msg = ("For optimal bootstrap tracking consider reordering the "
+                   "diffusion volumes so that all the b0 volumes are at the "
+                   "beginning")
+            warn(msg)
+        x, y, z = model.gtab.gradients[self.where_dwi].T
+        r, theta, phi = cart2sphere(x, y, z)
+        b_range = (r.max() - r.min()) / r.min()
+        if b_range > tol:
+            raise ValueError("BootOdfGen only supports single shell data")
 
-    gtab = gradient_table(bvals, bvecs)
+        B, m, n = shm.real_sym_sh_basis(sh_order, theta, phi)
+        H = shm.hat(B)
+        R = shm.lcr_matrix(H)
 
-    toy_voxel = single_tensor(gtab)
-    toy_data = np.tile(toy_voxel, (11, 11, 11, 1))
-    toy_tissue_classifier = np.ones(toy_data.shape[:-1])
-    toy_affine = np.eye(4)
+        self.data = np.asarray(data, "float64")
+        self.model = model
+        self.sphere = sphere
+        self.H = H
+        self.R = R
 
-    # make a slice roi that should track in-plane only
-    toy_roi_long_plane = np.zeros(toy_data.shape[:-1])
-    toy_roi_long_plane[:, :, 0] = 1
-    # make a slice roi that should track directly across the whole volume
-    toy_roi_radial_plane = np.zeros(toy_data.shape[:-1])
-    toy_roi_radial_plane[0, :, :] = 1
-    # make an roi that contains the center voxel only
-    toy_roi_center_vox = np.zeros(toy_data.shape[:-1])
-    toy_roi_center_vox[5, 5, 5] = 1
+    def get_pmf_boot(self, point):
+        """Produces an ODF from a SH bootstrap sample"""
+        single_vox_data = trilinear_interpolate4d(self.data, point)
 
-    toydict['gtab'] = gtab
-    toydict['toy_data'] = toy_data
-    toydict['toy_affine'] = toy_affine
-    toydict['toy_roi_long_plane'] = toy_roi_long_plane
-    toydict['toy_roi_radial_plane'] = toy_roi_radial_plane
-    toydict['toy_roi_center_vox'] = toy_roi_center_vox
-    toydict['toy_tissue_classifier'] = toy_tissue_classifier
+        non_b0_data = self.where_dwi
+        dwidata = single_vox_data[non_b0_data]
+        bootdata = shm.bootstrap_data_voxel(dwidata, self.H, self.R)
+        print "---", self.H, "---"
+        single_vox_data[non_b0_data] = bootdata
+        fit = self.model.fit(single_vox_data)
+        pmf = fit.odf(self.sphere)
+        return pmf
 
-    return toydict
+    def get_pmf(self, point):
+        data = trilinear_interpolate4d(self.data, point)
+        fit = self.model.fit(data)
+        return fit.odf(self.sphere)
+
+
+
 
 
 def test_bdg_initial_direction():
-    # test that we get one direction when we have a single tensor
+    """This test the number of inital direction."
+    """
 
     hsph_updated = HemiSphere.from_sphere(unit_icosahedron).subdivide(2)
-
     vertices = hsph_updated.vertices
-
     bvecs = vertices
     bvals = np.ones(len(vertices)) * 1000
-
     bvecs = np.insert(bvecs, 0, np.array([0, 0, 0]), axis=0)
     bvals = np.insert(bvals, 0, 0)
-
     gtab = gradient_table(bvals, bvecs)
 
-    toy_voxel = single_tensor(gtab).reshape([1, 1, 1, -1])
-
-    model = dti.TensorModel(gtab)
-
-    direction_getter = bdg.BootDirectionGetter.from_data(toy_voxel, model, 30)
-
-    initial_direction = direction_getter.initial_direction(np.zeros(3))
-
+    # test that we get one direction when we have a single tensor
+    voxel = single_tensor(gtab).reshape([1, 1, 1, -1])
+    dti_model = dti.TensorModel(gtab)
+    boot_dg = BootDirectionGetter.from_data(voxel, dti_model, 30, sh_order=6)
+    initial_direction = boot_dg.initial_direction(np.zeros(3))
     npt.assert_equal(len(initial_direction), 1)
-
     npt.assert_allclose(initial_direction[0], [1, 0, 0], atol=0.1)
-
+    bog2 = BootOdfGen2(voxel, dti_model, hsph_updated)
+    bog2.get_pmf_boot(np.zeros(3))
     # test that we get multiple directions when we have a multi-tensor
-
     mevals = np.array([[1.5, 0.4, 0.4], [1.5, 0.4, 0.4]]) * 1e-3
     fracs = [60, 40]
-    toy_voxel, primary_evecs = multi_tensor(gtab, mevals,
-                                            fractions=fracs, snr=None)
-    toy_voxel = toy_voxel.reshape([1, 1, 1, -1])
-    model = csd.ConstrainedSphericalDeconvModel(gtab, response=None)
-    direction_getter = bdg.BootDirectionGetter.from_data(toy_voxel, model, 30)
-    initial_direction = direction_getter.initial_direction(np.zeros(3))
-    print(initial_direction)
+    voxel, primary_evecs = multi_tensor(gtab, mevals, fractions=fracs,
+                                        snr=None)
+    voxel = voxel.reshape([1, 1, 1, -1])
+    csd_model = ConstrainedSphericalDeconvModel(gtab, response=(np.array([0.0015, 0.0004, 0.0004]), 1),
+                                                sh_order=4)
+    boot_dg = BootDirectionGetter.from_data(voxel, csd_model, 30)
+    initial_direction = boot_dg.initial_direction(np.zeros(3))
+    print primary_evecs
+    print initial_direction
+
+
+    bog2 = BootOdfGen2(voxel, csd_model, hsph_updated)
+    bog2.get_pmf_boot(np.zeros(3))
+
+
     npt.assert_equal(len(initial_direction), 2)
     npt.assert_allclose(initial_direction, primary_evecs, atol=0.1)
 
 
+
+
 def test_bdg_get_direction():
-    ''' test if direction getter goes 5 tries if '''
+    """This tests that the new tracking direction if founded.
+    """
 
     sphere = HemiSphere.from_sphere(unit_icosahedron.subdivide())
-
     two_neighbors = sphere.edges[0]
     direction1 = sphere.vertices[two_neighbors[0]]
     direction2 = sphere.vertices[two_neighbors[1]]
     angle = np.rad2deg(direction1.dot(direction2))
+    point = np.zeros(3)
+    prev_direction = direction2.copy()
 
-    class Fakepmf(object):
-        count = 0
-
-        def get_pmf_boot(self, point):
-            pmf = np.zeros(len(sphere.vertices))
-            pmf[two_neighbors[0]] = 1
-            self.count += 1
-            return pmf
-
-        def get_pmf(self, point):
-            pass
-
-    myfakepmf = Fakepmf()
-    mydirgetter = bdg.BootDirectionGetter(myfakepmf, angle/2., sphere=sphere)
-
-    mypoint = np.zeros(3)
-    myprev_direction = direction2.copy()
+    pmf = np.zeros([1, 1, 1, len(sphere.vertices)])
+    pmf[:, :, :, two_neighbors[0]] = 1
+    pmf_gen = SimplePmfGen(pmf)
 
     # test case in which no valid direction is found with default maxdir
-    no_valid_direction = mydirgetter.get_direction(mypoint, myprev_direction)
-
-    npt.assert_equal(no_valid_direction, 1)
-    npt.assert_equal(direction2, myprev_direction)
-    npt.assert_equal(myfakepmf.count, 5)
+    boot_dg = BootDirectionGetter(pmf_gen, angle / 2., sphere=sphere)
+    npt.assert_equal(boot_dg.get_direction(point, prev_direction), 1)
+    npt.assert_equal(direction2, prev_direction)
 
     # test case in which no valid direction is found with new max attempts
-    myfakepmf.count = 0
-    mydirgetter = bdg.BootDirectionGetter(myfakepmf, angle/2., sphere=sphere,
-                                          max_attempts=3)
-    no_valid_direction = mydirgetter.get_direction(mypoint, myprev_direction)
+    boot_dg = BootDirectionGetter(pmf_gen, angle / 2., sphere=sphere,
+                                  max_attempts=3)
+    npt.assert_equal(boot_dg.get_direction(point, prev_direction), 1)
+    npt.assert_equal(direction2, prev_direction)
 
-    npt.assert_equal(no_valid_direction, 1)
-    npt.assert_equal(direction2, myprev_direction)
-    npt.assert_equal(myfakepmf.count, 3)
+    # test case in which a valid direction is found
+    boot_dg = BootDirectionGetter(pmf_gen, angle * 2., sphere=sphere,
+                                  max_attempts=1)
+    npt.assert_equal(boot_dg.get_direction(point, prev_direction), 0)
+    npt.assert_equal(direction1, prev_direction)
 
-    # now test case in which valid direction is found
-    myfakepmf.count = 0
-    mydirgetter = bdg.BootDirectionGetter(myfakepmf, angle*2., sphere=sphere)
-    no_valid_direction = mydirgetter.get_direction(mypoint, myprev_direction)
-
-    npt.assert_equal(no_valid_direction, 0)
-    npt.assert_equal(direction1, myprev_direction)
-    npt.assert_equal(myfakepmf.count, 1)
+    # test invalid max_attempts parameters
+    npt.assert_raises(
+        ValueError,
+        lambda: BootDirectionGetter(pmf_gen, angle * 2., sphere=sphere,
+                                    max_attempts=0))
 
 
-def test_bdg_bog_pmfnoboot():
-    # does it return the right size of the sphere we give it?
-    # check the model you explicitly fit is the one that is returned
+def test_bdg_model():
+    """This tests the local model used for the bootstrapping.
+    """
 
     hsph_updated = HemiSphere.from_sphere(unit_icosahedron).subdivide(2)
-
     vertices = hsph_updated.vertices
-
     bvecs = vertices
     bvals = np.ones(len(vertices)) * 1000
-
     bvecs = np.insert(bvecs, 0, np.array([0, 0, 0]), axis=0)
     bvals = np.insert(bvals, 0, 0)
-
     gtab = gradient_table(bvals, bvecs)
-
-    toy_voxel = single_tensor(gtab)
-    toy_data = np.tile(toy_voxel, (3, 3, 3, 1))
-
+    voxel = single_tensor(gtab)
+    data = np.tile(voxel, (3, 3, 3, 1))
     tensor_model = dti.TensorModel(gtab)
 
-    mybog = bdg.BootOdfGen(toy_data, model=tensor_model, sphere=hsph_updated)
-    odf_fit = mybog.get_pmf(np.array([1., 1., 1.]))
+    boot_pmf_gen = BootPmfGen(data, model=tensor_model,
+                              sphere=hsph_updated)
+    boot_dg_pmf = boot_pmf_gen.get_pmf_no_boot(np.array([1., 1., 1.]))
 
-    myfit = tensor_model.fit(toy_voxel).odf(hsph_updated)
+    model_pmf = tensor_model.fit(voxel).odf(hsph_updated)
 
-    npt.assert_equal(len(hsph_updated.vertices), odf_fit.shape[0])
-    npt.assert_array_almost_equal(myfit, odf_fit)
+    npt.assert_equal(len(hsph_updated.vertices), boot_dg_pmf.shape[0])
+    npt.assert_array_almost_equal(boot_dg_pmf, model_pmf)
 
 
-def test_bdg_bog_pmfboot():
-    # test if residuals = 0
-    # make a perfect dataset that can be fit
+def test_bdg_residual():
+    """This tests the bootstrapping residual.
+    """
 
     hsph_updated = HemiSphere.from_sphere(unit_icosahedron).subdivide(2)
     vertices = hsph_updated.vertices
     bvecs = vertices
-
     bvals = np.ones(len(vertices)) * 1000
-
     bvecs = np.insert(bvecs, 0, np.array([0, 0, 0]), axis=0)
     bvals = np.insert(bvals, 0, 0)
-
     gtab = gradient_table(bvals, bvecs)
-
     r, theta, phi = cart2sphere(*vertices.T)
-
     B, m, n = shm.real_sym_sh_basis(6, theta, phi)
-
     shm_coeff = np.random.random(B.shape[1])
 
-    # sphere_func is samples of the spherical function for each point of the
-    # sphere
+    # sphere_func is samples of the spherical function for each point of
+    # the sphere
     sphere_func = np.dot(shm_coeff, B.T)
 
-    toy_voxel = np.concatenate((np.zeros(1), sphere_func))
-    toy_data = np.tile(toy_voxel, (3, 3, 3, 1))
+    voxel = np.concatenate((np.zeros(1), sphere_func))
+    data = np.tile(voxel, (3, 3, 3, 1))
 
-    csd_model = csd.ConstrainedSphericalDeconvModel(gtab, None, sh_order=6)
+    csd_model = ConstrainedSphericalDeconvModel(gtab, None, sh_order=6)
+    boot_pmf_gen = BootPmfGen(data, model=csd_model, sphere=hsph_updated,
+                              sh_order=6)
 
-    mybog = bdg.BootOdfGen(toy_data, model=csd_model, sphere=hsph_updated,
-                           sh_order=6)
-    # Two boot samples should be the same if there are no residuales
-    myodf1 = mybog.get_pmf_boot(np.array([1.5, 1.5, 1.5]))
-    myodf2 = mybog.get_pmf_boot(np.array([1.5, 1.5, 1.5]))
-
-    npt.assert_array_almost_equal(myodf1, myodf2)
+    # Two boot samples should be the same
+    odf1 = boot_pmf_gen.get_pmf(np.array([1.5, 1.5, 1.5]))
+    odf2 = boot_pmf_gen.get_pmf(np.array([1.5, 1.5, 1.5]))
+    npt.assert_array_almost_equal(odf1, odf2)
 
     # A boot sample with less sh coeffs should have residuals, thus the two
     # should be different
-    mybog2 = bdg.BootOdfGen(toy_data, model=csd_model, sphere=hsph_updated,
-                            sh_order=4)
-    myodf1 = mybog2.get_pmf_boot(np.array([1.5, 1.5, 1.5]))
-    myodf2 = mybog2.get_pmf_boot(np.array([1.5, 1.5, 1.5]))
+    boot_pmf_gen2 = BootPmfGen(data, model=csd_model, sphere=hsph_updated,
+                               sh_order=4)
+    odf1 = boot_pmf_gen2.get_pmf(np.array([1.5, 1.5, 1.5]))
+    odf2 = boot_pmf_gen2.get_pmf(np.array([1.5, 1.5, 1.5]))
+    npt.assert_(np.any(odf1 != odf2))
 
-    npt.assert_(np.any(myodf1 != myodf2))
-
-    # throw in a gtab with two shells and assert you get an error
+    # test with a gtab with two shells and assert you get an error
     bvals[-1] = 2000
     gtab = gradient_table(bvals, bvecs)
-
-    csd_model = csd.ConstrainedSphericalDeconvModel(gtab, None, sh_order=6)
-    npt.assert_raises(ValueError, bdg.BootOdfGen, toy_data, csd_model,
+    csd_model = ConstrainedSphericalDeconvModel(gtab, None, sh_order=6)
+    npt.assert_raises(ValueError, BootPmfGen, data, csd_model,
                       hsph_updated, 6)
-
-
-def test_num_sls():
-
-    toydict = uniform_toy_data()
-    csd_model = csd.ConstrainedSphericalDeconvModel(toydict['gtab'], None,
-                                                    sh_order=6)
-    csd_fit = csd_model.fit(toydict['toy_data'])
-
-    sltest_list = [('toy_roi_long_plane', 121),
-                   ('toy_roi_radial_plane', 121),
-                   ('toy_roi_center_vox', 1)]
-
-    classifier = ThresholdTissueClassifier(toydict['toy_tissue_classifier'],
-                                           0.1)
-    detmax_dg = DeterministicMaximumDirectionGetter.from_shcoeff(
-        csd_fit.shm_coeff, max_angle=30.)
-
-    expected_sl_length = 11
-    for roi, num_sl in sltest_list:
-        seed = utils.seeds_from_mask(toydict[roi])
-        streamlines = LocalTracking(detmax_dg, classifier, seed,
-                                    toydict['toy_affine'], step_size=1)
-        streamlines = list(streamlines)
-        npt.assert_equal(len(streamlines), num_sl)
-        for sl in streamlines:
-            npt.assert_equal(len(sl), expected_sl_length)
