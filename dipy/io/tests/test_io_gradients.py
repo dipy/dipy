150b9655c94fc1fe0d5a99b4430a6950c2464548
Kumar Ashutosh <kumarashutosh.ee@gmail.com>
2018-02-18 16:23:44 +0530

    with directive used

--- dipy/io/tests/test_io_gradients.py
+++ dipy/io/tests/test_io_gradients.py
@@ -60,34 +60,40 @@ def test_read_bvals_bvecs():
     npt.assert_raises(IOError, read_bvals_bvecs, fbvecs, fbvecs)
 
     # All possible delimiters should work
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file4:
-        bv_file4.write("66 55 33")
-        bvals_1, _ = read_bvals_bvecs(bv_file4.name, None)
+    bv_file4 = tempfile.NamedTemporaryFile()
+    with open(bv_file4.name, 'w') as f:
+        f.write("66 55 33")
+    bvals_1, _ = read_bvals_bvecs(bv_file4.name, None)
 
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file5:
-        bv_file4.write("66, 55, 33")
-        bvals_2, _ = read_bvals_bvecs(bv_file5.name, None)
+    bv_file5 = tempfile.NamedTemporaryFile()
+    with open(bv_file5.name, 'w') as f:
+        f.write("66, 55, 33")
+    bvals_2, _ = read_bvals_bvecs(bv_file5.name, None)
 
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file6:
-        bv_file4.write("66 \t 55 \t 33")
-        bvals_3, _ = read_bvals_bvecs(bv_file6.name, None)
+    bv_file6 = tempfile.NamedTemporaryFile()
+    with open(bv_file6.name, 'w') as f:
+        f.write("66 \t 55 \t 33")
+    bvals_3, _ = read_bvals_bvecs(bv_file6.name, None)
 
     ans = np.array([66., 55., 33.])
     npt.assert_array_equal(ans, bvals_1)
     npt.assert_array_equal(ans, bvals_2)
     npt.assert_array_equal(ans, bvals_3)
 
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file7:
-        bv_file5.write("66 55 33 \n 45 34 21 \n 55 32 65")
-        _, bvecs_1 = read_bvals_bvecs(None, bv_file7.name)
+    bv_file7 = tempfile.NamedTemporaryFile()
+    with open(bv_file7.name, 'w') as f:
+        f.write("66 55 33 \n 45 34 21 \n 55 32 65")
+    _, bvecs_1 = read_bvals_bvecs(None, bv_file7.name)
 
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file8:
-        bv_file5.write("66, 55, 33 \n 45, 34, 21 \n 55, 32, 65")
-        _, bvecs_2 = read_bvals_bvecs(None, bv_file8.name)
+    bv_file8 = tempfile.NamedTemporaryFile()
+    with open(bv_file8.name, 'w') as f:
+        f.write("66, 55, 33 \n 45, 34, 21 \n 55, 32, 65")
+    _, bvecs_2 = read_bvals_bvecs(None, bv_file8.name)
 
-    with open(tempfile.NamedTemporaryFile, 'wt') as bv_file9:
-        bv_file5.write("66 \t 55 \t 33 \n 45 \t 34 \t 21 \n 55 \t 32 \t 65")
-        _, bvecs_3 = read_bvals_bvecs(None, bv_file9.name)
+    bv_file9 = tempfile.NamedTemporaryFile()
+    with open(bv_file9.name, 'w') as f:
+        f.write("66 \t 55 \t 33 \n 45 \t 34 \t 21 \n 55 \t 32 \t 65")
+    _, bvecs_3 = read_bvals_bvecs(None, bv_file9.name)
 
     ans = np.array([[66., 55., 33.], [45., 34., 21.], [55., 32., 65.]])
     npt.assert_array_equal(ans, bvecs_1)
