7bed03bf0d086702c587c9d4c7c21a0845853c93
skoudoro <skab12@gmail.com>
2018-03-12 10:34:55 -0400

    update import

--- dipy/tracking/tests/test_streamline.py
+++ dipy/tracking/tests/test_streamline.py
@@ -12,7 +12,7 @@ from nose.tools import assert_true, assert_equal, assert_almost_equal
 from numpy.testing import (assert_array_equal, assert_array_almost_equal,
                            assert_raises, run_module_suite)
 
-from dipy.tracking import Streamlines
+from dipy.tracking.streamline import Streamlines
 import dipy.tracking.utils as ut
 from dipy.tracking.streamline import (set_number_of_points,
                                       length,
