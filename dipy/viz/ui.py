Fixed rotation direction of actor

dipy/viz/ui.py
@@ -1921,7 +1921,7 @@ class DiskSlider2D(UI):
         self.center = np.array(position)
         self.min_value = min_value
         self.max_value = max_value
-
+        self.initial_value = initial_value
         self.slider_inner_radius = slider_inner_radius
         self.slider_outer_radius = slider_outer_radius
         self.handle_inner_radius = handle_inner_radius
@@ -1939,6 +1939,7 @@ class DiskSlider2D(UI):
 
         # By setting the value, it also updates everything.
         self.value = initial_value
+        self.pvalue = initial_value
         self.handle_events(None)
 
     def build_actors(self):
@@ -1988,6 +1989,14 @@ class DiskSlider2D(UI):
         self.ratio = (value - self.min_value) / value_range
 
     @property
+    def pvalue(self):
+        return self._pvalue
+
+    @pvalue.setter
+    def pvalue(self, pvalue):
+        self._pvalue = pvalue
+
+    @property
     def ratio(self):
         return self._ratio
 
@@ -2021,6 +2030,10 @@ class DiskSlider2D(UI):
 
         # Compute the selected value considering min_value and max_value.
         value_range = self.max_value - self.min_value
+        try:
+            self._pvalue = self.value
+        except:
+            self._pvalue = self.initial_value
         self._value = self.min_value + self.ratio*value_range
 
         # Update text disk actor.