5abf7e3994eea041264faf538d4de2ece9a64478
RicciWoo <ricciwoo@gmail.com>
2018-03-25 07:38:02 -0400

    fix bug in example reconst_csd.py

--- doc/examples/reconst_csd.py
+++ doc/examples/reconst_csd.py
@@ -151,6 +151,7 @@ like  a pancake:
 """
 
 response_signal = response.on_sphere(sphere)
+response_signal = response_signal[None, None, None, :]
 response_actor = actor.odf_slicer(response_signal, sphere=sphere, colormap='plasma')
 
 ren = window.Renderer()
@@ -225,7 +226,7 @@ csd_peaks = peaks_from_model(model=csd_model,
                              parallel=True)
 
 window.clear(ren)
-fodf_peaks = actor.peak_slicer(csd_peaks.peak_dirs, csd_peaks.peak_values, scale=1.3)
+fodf_peaks = actor.peak_slicer(csd_peaks.peak_dirs, csd_peaks.peak_values)
 ren.add(fodf_peaks)
 
 print('Saving illustration as csd_peaks.png')
