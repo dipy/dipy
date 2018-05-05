2398d1990a40c10b2218b4a583ef8ff49fffc637
Kumar Ashutosh <kumarashutosh.ee@gmail.com>
2018-05-05 19:09:16 +0530

    np.all added

--- dipy/utils/testing.py
+++ dipy/utils/testing.py
@@ -6,37 +6,37 @@ import numpy as np
 
 def assert_equal(value1, value2):
 	try:
-		assert value1 == value2
+		assert np.all(value1 == value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' != ' + str(value2))
 
 def assert_not_equal(value1, value2):
 	try:
-		assert value1 != value2
+		assert np.all(value1 != value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' != ' + str(value2))
 
 def assert_less(value1, value2):
 	try:
-		assert value1 < value2
+		assert np.all(value1 < value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' is not less than ' + str(value2))
 
 def assert_less_equal(value1, value2):
 	try:
-		assert value1 <= value2
+		assert np.all(value1 <= value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' is not less than ' + str(value2))
 
 def assert_greater(value1, value2):
 	try:
-		assert value1 > value2
+		assert np.all(value1 > value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' is not greater than ' + str(value1))
 
 def assert_greater_equal(value1, value2):
 	try:
-		assert value1 >= value2
+		assert np.all(value1 >= value2)
 	except AssertionError:
 		raise AssertionError(str(value1) + ' is not greater than ' + str(value1))
 
