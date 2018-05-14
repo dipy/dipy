c5729fc2776da95255a28316d5262a8edb51baa1
Parichit Sharma <schmuck@149-161-162-17.dhcp-bl.indiana.edu>
2018-05-14 16:13:09 -0400

    Removed the trailing whitespaces as per the PEP8 standard.
    1) The PEP8 still complains about the maximum line width but no complaints about the
    trailing white spaces.

--- doc/examples/workflow_creation.py
+++ doc/examples/workflow_creation.py
@@ -96,10 +96,13 @@ line, you need to add this bit of code. Usually this is in a separate
 executable file located in ``bin``.
 """
 
-"""These lines will import the required files and classes into the 
-python file. The first line imports the run_flow method from the
-flow_runner class and the second line imports the AppendTextFlow 
-method from the newly created workflow class."""
+"""
+
+These lines will import the required files and classes into the python file. The
+first line imports the run_flow method from the flow_runner class and the second
+line imports the AppendTextFlow method from the newly created workflow class.
+"""
+
 from dipy.workflows.flow_runner import run_flow
 from dipy.workflows.AppendTextFlow import AppendTextFlow
 """
