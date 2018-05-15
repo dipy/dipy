4c9b4f69248abdffef5d5d5ed49edbd46e267925
Karan <karan.jkps@gmail.com>
2018-05-16 04:31:25 +0530

    Started Image widget in ui

--- dipy/viz/ui.py
+++ dipy/viz/ui.py
@@ -51,6 +51,8 @@ class UI(object):
         (i.e. pressed -> released).
     on_right_mouse_button_dragged: function
         Callback function for when dragging using the right mouse button.
+    on_key_press: function
+        Callback function for when a keyboard key is pressed.
     """
 
     def __init__(self, position=(0, 0)):
@@ -2164,6 +2166,190 @@ class RingSlider2D(UI):
         self.move_handle(click_position=click_position)
         i_ren.force_render()
         i_ren.event.abort()  # Stop propagating the event.
+
+
+class ImageHolder(UI):
+    """ A 2D container to hold an image and is of type vtkTexturedActor2D.
+    Currently Supports:
+    - png and jpg/jpeg images
+
+    Attributes
+    ----------
+    size: (float, float)
+        Image size (width, height) in pixels.
+
+    """
+
+    def __init__(self, imgPath, position=(0, 0), size=(100, 100)):
+        """
+        Parameters
+        ----------
+        imgPath : string
+            Path of the image
+        position : (float, float), optional
+            Absolute coordinates (x, y) of the lower-left corner of the image.
+        size : (int, int), optional
+            Width and height in pixels of the image.
+        """
+        super(ImageHolder, self).__init__(position)
+        self.img = self._build_image(imgPath)
+        self.resize(size)
+
+    def _build_image(self, imgPath):
+        """ Converts image path to vtkImageDataGeometryFilters.
+
+        A pre-processing step to prevent re-read of image during every
+        state change.
+
+        Parameters
+        ----------
+        imgPath : string
+            Path of the image
+
+        Returns
+        -------
+        img : vtkImageDataGeometryFilters
+            The corresponding image .
+        """
+        if imgPath.split(".")[-1] in ["png", "PNG"]:
+            png = vtk.vtkPNGReader()
+            png.SetFileName(imgPath)
+            png.Update()
+            img = png.GetOutput()
+        elif imgPath.split(".")[-1] in ["jpg","jpeg","JPG","JPEG"]:
+            jpeg = vtk.vtkJPEGReader()
+            jpeg.SetFileName(imgPath)
+            jpeg.Update()
+            img = jpeg.GetOutput()
+        else:
+            error_msg = "This file format is not supported by the Image Holder"
+            warn(Warning(error_msg))
+        return img
+
+    def _get_size(self):
+        lower_left_corner = self.texture_points.GetPoint(0)
+        upper_right_corner = self.texture_points.GetPoint(2)
+        size = np.array(upper_right_corner) - np.array(lower_left_corner)
+        return abs(size[:2])
+
+    def _setup(self):
+        """ Setup this UI Component.
+        Return an image as a 2D actor with a specific position.
+
+        Returns
+        -------
+        :class:`vtkTexturedActor2D`
+        """
+        img = self.img
+        self.texture_polydata = vtk.vtkPolyData()
+        self.texture_points = vtk.vtkPoints()
+        self.texture_points.SetNumberOfPoints(4)
+        self.size = img.GetExtent()
+
+        polys = vtk.vtkCellArray()
+        polys.InsertNextCell(4)
+        polys.InsertCellPoint(0)
+        polys.InsertCellPoint(1)
+        polys.InsertCellPoint(2)
+        polys.InsertCellPoint(3)
+        self.texture_polydata.SetPolys(polys)
+
+        tc = vtk.vtkFloatArray()
+        tc.SetNumberOfComponents(2)
+        tc.SetNumberOfTuples(4)
+        tc.InsertComponent(0, 0, 0.0)
+        tc.InsertComponent(0, 1, 0.0)
+        tc.InsertComponent(1, 0, 1.0)
+        tc.InsertComponent(1, 1, 0.0)
+        tc.InsertComponent(2, 0, 1.0)
+        tc.InsertComponent(2, 1, 1.0)
+        tc.InsertComponent(3, 0, 0.0)
+        tc.InsertComponent(3, 1, 1.0)
+        self.texture_polydata.GetPointData().SetTCoords(tc)
+
+        texture_mapper = vtk.vtkPolyDataMapper2D()
+        if major_version <= 5:
+            texture_mapper.SetInput(self.texture_polydata)
+        else:
+            texture_mapper.SetInputData(self.texture_polydata)
+
+        image = vtk.vtkTexturedActor2D()
+        image.SetMapper(texture_mapper)
+
+        self.texture = vtk.vtkTexture()
+        image.SetTexture(self.texture)
+
+        image_property = vtk.vtkProperty2D()
+        image_property.SetOpacity(1.0)
+        image.SetProperty(image_property)
+        self.set_img(img)
+
+        # Add default events listener to the VTK actor.
+        self.handle_events(self.actor)
+        
+        return image
+
+    def get_actors(self):
+        """ Returns the actors that compose this UI component.
+        """
+        return [self.actor]
+
+    def _add_to_renderer(self, ren):
+        """ Add all subcomponents or VTK props that compose this UI component.
+
+        Parameters
+        ----------
+        ren : renderer
+        """
+        ren.add(self.actor)
+
+    def resize(self, size):
+        """ Resize the image.
+
+        Parameters
+        ----------
+        size : (float, float)
+            image size (width, height) in pixels.
+        """
+        # Update actor.
+        self.texture_points.SetPoint(0, 0, 0, 0.0)
+        self.texture_points.SetPoint(1, size[0], 0, 0.0)
+        self.texture_points.SetPoint(2, size[0], size[1], 0.0)
+        self.texture_points.SetPoint(3, 0, size[1], 0.0)
+        self.texture_polydata.SetPoints(self.texture_points)
+
+    def _set_position(self, coords):
+        """ Position the lower-left corner of this UI component.
+
+        Parameters
+        ----------
+        coords: (float, float)
+            Absolute pixel coordinates (x, y).
+        """
+        self.actor.SetPosition(*coords)
+
+    def scale(self, factor):
+        """ Scales the image.
+
+        Parameters
+        ----------
+        factor : (float, float)
+            Scaling factor (width, height) in pixels.
+        """
+        self.resize(self.size * factor)
+
+    def set_img(self, img):
+        """ Modifies the image used by the vtkTexturedActor2D.
+
+        Parameters
+        ----------
+        img : imageDataGeometryFilter
+
+        """
+        if major_version <= 5:
+            self.texture.SetInput(img)
+        else:
+            self.texture.SetInputData(img)
 
 
 class ListBox2D(UI):
