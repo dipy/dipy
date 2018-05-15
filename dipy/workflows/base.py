50cc366bf5127339c470b1225f3dbae2334db9e7
Parichit Sharma <schmuck@149-161-162-17.dhcp-bl.indiana.edu>
2018-05-15 19:03:44 -0400

    Resolved the PEP8 formatting style warnings.

--- dipy/workflows/base.py
+++ dipy/workflows/base.py
@@ -264,11 +264,13 @@ class IntrospectiveArgumentParser(arg.ArgumentParser):
     def get_flow_args(self, args=None, namespace=None):
         """ Returns the parsed arguments as a dictionary that will be used
         as a workflow's run method arguments.
+
+        The function simply exits with a help message if no arguments were
+        provided by the user.
         """
 
-        #Checking if the required arguments have been provided by the user or not.
         if len(sys.argv) <= 1:
-            print("Program",sys.argv[0],"expects arguments. Type", sys.argv[0],"-h for help.")
+            print("Program", sys.argv[0], "expects arguments. Type", sys.argv[0], "-h for help.")
             exit(1)
 
         ns_args = self.parse_args(args, namespace)
