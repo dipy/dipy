6a7a857d9d6773c7312c2eaa0d2a003141b7e8bc
RicciWoo <ricciwoo@gmail.com>
2018-06-17 15:49:22 -0400

    remove change in _gradient_2d & _sparse_gradient_2d

--- dipy/align/vector_fields.pyx
+++ dipy/align/vector_fields.pyx
@@ -3302,28 +3302,21 @@ def _gradient_2d(floating[:, :] img, double[:, :] img_world2grid,
         int nrows = out.shape[0]
         int ncols = out.shape[1]
         int i, j, k, in_flag, p
-        double tmp, *x, *dx, *q
+        double tmp
+        double[:] x = np.empty(shape=(2,), dtype=np.float64)
+        double[:] dx = np.empty(shape=(2,), dtype=np.float64)
         double[:] h = np.empty(shape=(2,), dtype=np.float64)
-    with nogil, parallel():
+        double[:] q = np.empty(shape=(2,), dtype=np.float64)
+    with nogil:
         h[0] = 0.5 * img_spacing[0]
         h[1] = 0.5 * img_spacing[1]
-        x = <double *>malloc(sizeof(double) * 2)
-        if x == NULL:
-            abort()
-        dx = <double *>malloc(sizeof(double) * 2)
-        if dx == NULL:
-            abort()
-        q = <double *>malloc(sizeof(double) * 2)
-        if q == NULL:
-            abort()
-        for i in prange(nrows, schedule='guided'):
+        for i in range(nrows):
             for j in range(ncols):
                 inside[i, j] = 1
                 # Compute coordinates of index (i, j) in physical space
                 x[0] = _apply_affine_2d_x0(i, j, 1, out_grid2world)
                 x[1] = _apply_affine_2d_x1(i, j, 1, out_grid2world)
-                for p in range(2):
-                    dx[p] = x[p]
+                dx[:] = x[:]
                 for p in range(2):
                     # Compute coordinates of point dx on img's grid
                     dx[p] = x[p] - h[p]
@@ -3350,9 +3343,6 @@ def _gradient_2d(floating[:, :] img, double[:, :] img_world2grid,
                         continue
                     out[i, j, p] = (out[i, j, p] - tmp) / img_spacing[p]
                     dx[p] = x[p]
-        free(x)
-        free(dx)
-        free(q)
 
 
 def _sparse_gradient_2d(floating[:, :] img, double[:, :] img_world2grid,
@@ -3392,18 +3382,14 @@ def _sparse_gradient_2d(floating[:, :] img, double[:, :] img_world2grid,
     cdef:
         int n = sample_points.shape[0]
         int i, in_flag, p
-        double tmp, *dx, *q
+        double tmp
+        double[:] dx = np.empty(shape=(2,), dtype=np.float64)
         double[:] h = np.empty(shape=(2,), dtype=np.float64)
-    with nogil, parallel():
+        double[:] q = np.empty(shape=(2,), dtype=np.float64)
+    with nogil:
         h[0] = 0.5 * img_spacing[0]
         h[1] = 0.5 * img_spacing[1]
-        dx = <double *>malloc(sizeof(double) * 2)
-        if dx == NULL:
-            abort()
-        q = <double *>malloc(sizeof(double) * 2)
-        if q == NULL:
-            abort()
-        for i in prange(n, schedule='guided'):
+        for i in range(n):
             inside[i] = 1
             dx[0] = sample_points[i, 0]
             dx[1] = sample_points[i, 1]
@@ -3433,8 +3419,6 @@ def _sparse_gradient_2d(floating[:, :] img, double[:, :] img_world2grid,
                     continue
                 out[i, p] = (out[i, p] - tmp) / img_spacing[p]
                 dx[p] = sample_points[i, p]
-        free(dx)
-        free(q)
 
 
 def gradient(img, img_world2grid, img_spacing, out_shape,
