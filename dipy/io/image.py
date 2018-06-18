60feca2c3e86d8af867bb0ec5a642764bedf31bf
Parichit Sharma <schmuck@schmuck.attlocal.net>
2018-06-18 15:13:29 -0400

    Workflow for applying the transform on a set of images.
    1)Added the function to load the saved affine matrix that needs to be applied.
    
    2) Added the import for scipy.ndimage in the test_parzenhist.py file.
    
    3) Added the apply_transform workflow to apply the transform.

--- dipy/io/image.py
+++ dipy/io/image.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function, absolute_import
-import numpy as np
+
 import nibabel as nib
+import numpy as np
 
 
 def load_nifti(fname, return_img=False, return_voxsize=False,
@@ -8,7 +9,7 @@ def load_nifti(fname, return_img=False, return_voxsize=False,
     img = nib.load(fname)
     data = img.get_data()
     vox_size = img.header.get_zooms()[:3]
-    
+
     ret_val = [data, img.affine]
 
     if return_img:
@@ -25,8 +26,8 @@ def save_nifti(fname, data, affine, hdr=None):
     result_img = nib.Nifti1Image(data, affine, header=hdr)
     result_img.to_filename(fname)
 
-def save_affine_matrix(fname, affine):
 
+def save_affine_matrix(fname, affine):
     """
     Parameters
     ---------
@@ -36,3 +37,13 @@ def save_affine_matrix(fname, affine):
         The object containing the affine matrix.
     """
     np.savetxt(fname, affine)
+
+
+def load_affine_matrix(fname):
+    """
+    Parameters
+    ----------
+    fname : string
+        The file containing the saved affine matrix.
+    """
+    return np.loadtxt(fname)
