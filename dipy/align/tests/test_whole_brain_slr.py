1be009d88a31dab86827a56a976d191ca8264cd9
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2018-08-23 16:48:44 -0400

    Updated SLR test

--- dipy/align/tests/test_whole_brain_slr.py
+++ dipy/align/tests/test_whole_brain_slr.py
@@ -33,9 +33,7 @@ def test_whole_brain_slr():
     moved, transform, qb_centroids1, qb_centroids2 = whole_brain_slr(
             f1, f2, x0='affine', verbose=True, rm_small_clusters=1,
             greater_than=0, less_than=np.inf,
-            qbx_thr=[40, 30, 20, 15, 5, 1], progressive=False)
-
-
+            qbx_thr=[5, 2], progressive=False)
 
 #    ren = window.Renderer()
 #    ren.add(actor.line(f1, colors=(1, 0, 0)))
@@ -43,7 +41,7 @@ def test_whole_brain_slr():
 #    #ren.add(actor.line(moved, colors=(0, 0, 1)))
 #    window.show(ren)
 
-    print("transform = ", transform)
+    #print("transform = ", transform)
     # we can check the quality of registration by comparing the matrices
     # MAM streamline distances before and after SLR
     D12 = bundles_distances_mam(f1, f2)
@@ -63,12 +61,11 @@ def test_whole_brain_slr():
     mat = compose_matrix44([0, 0, 0, 15, 0, 0])
 
     f3 = f.copy()
-    old_f3 = f3.copy()
     f3 = transform_streamlines(f3, mat)
 
     moved, transform, qb_centroids1, qb_centroids2 = slr_with_qbx(
-            f1, old_f3, verbose=False, rm_small_clusters=0, greater_than=0,
-            less_than=np.inf, qbx_thr=[40, 30, 20, 15, 5, 1],
+            f1, f3, verbose=False, rm_small_clusters=0, greater_than=0,
+            less_than=np.inf, qbx_thr=[5, 2],
             progressive=True)
 
     # we can also check the quality by looking at the decomposed transform
@@ -78,7 +75,7 @@ def test_whole_brain_slr():
     moved, transform, qb_centroids1, qb_centroids2 = slr_with_qbx(
             f1, f3, verbose=False, rm_small_clusters=0, select_random=400,
             greater_than=0,
-            less_than=np.inf, qbx_thr=[40, 30, 20, 15, 5, 1],
+            less_than=np.inf, qbx_thr=[5, 2],
             progressive=True)
 
     # we can also check the quality by looking at the decomposed transform
@@ -86,4 +83,5 @@ def test_whole_brain_slr():
     assert_array_almost_equal(decompose_matrix44(transform)[3], -15, 2)
 
 if __name__ == '__main__':
-    run_module_suite()
+    # run_module_suite()
+    test_whole_brain_slr()
