409410059e95f7d71a1d0d0ad88136c316dbef09
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2018-08-31 11:10:33 -0400

    RF: fixed PEP8 in test_utils.py

--- dipy/io/tests/test_utils.py
+++ dipy/io/tests/test_utils.py
@@ -2,6 +2,7 @@ from dipy.io.utils import decfa
 from nibabel import Nifti1Image
 import numpy as np
 
+
 def test_decfa():
     data_orig = np.zeros((4, 4, 4, 3))
     data_orig[0, 0, 0] = np.array([1, 0, 0])
@@ -12,6 +13,6 @@ def test_decfa():
                                          dtype=np.dtype([('R', 'uint8'),
                                                          ('G', 'uint8'),
                                                          ('B', 'uint8')]))
-    assert data_new.dtype ==  np.dtype([('R', 'uint8'),
-                                        ('G', 'uint8'),
-                                        ('B', 'uint8')])
+    assert data_new.dtype == np.dtype([('R', 'uint8'),
+                                       ('G', 'uint8'),
+                                       ('B', 'uint8')])
