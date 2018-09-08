c738f2f0144509da8887f55fe324bdfdd0bc7641
Serge Koudoro <skab12@gmail.com>
2018-09-08 06:24:53 +0200

    remove nose on dipy.sims

--- dipy/sims/tests/test_voxel.py
+++ dipy/sims/tests/test_voxel.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-from nose.tools import assert_almost_equal
 from numpy.testing import (assert_array_equal, assert_array_almost_equal,
-                           assert_)
+                           assert_, assert_almost_equal)
 
 from dipy.sims.voxel import (_check_directions, SingleTensor, MultiTensor,
                              all_tensor_evecs, add_noise, single_tensor,
