ffb6cbbca1470c7dfa90692e744d2334f0599d72
Serge Koudoro <skab12@gmail.com>
2018-09-08 06:26:02 +0200

    remove nose on dipy.testing dipy.tests

--- dipy/testing/tests/test_decorators.py
+++ dipy/testing/tests/test_decorators.py
@@ -3,13 +3,9 @@
 
 import numpy as np
 
-from numpy.testing import (assert_almost_equal,
-                           assert_array_equal)
-
-from nose.tools import (assert_true, assert_false, assert_raises,
-                        assert_equal, assert_not_equal)
-
-
+from numpy.testing import (assert_almost_equal, assert_raises,
+                           assert_array_equal, assert_equal)
+from dipy.testing import assert_true, assert_false, assert_not_equal
 from dipy.testing.decorators import doctest_skip_parser
 
 
