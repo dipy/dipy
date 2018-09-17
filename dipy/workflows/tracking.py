8c61bb58fc7570a19bd61160b26379f0ca648bc5
Serge Koudoro <skab12@gmail.com>
2018-09-17 20:20:41 +0200

    from_pve addition

--- dipy/workflows/tracking.py
+++ dipy/workflows/tracking.py
@@ -157,9 +157,9 @@ class PFTrackingPAMFlow(LocalFiberTrackingPAMFlow):
         wm, affine, voxel_size = load_nifti(wm_path, return_voxsize=True)
         gm, _ = load_nifti(gm_path)
         csf, _ = load_nifti(csf_path)
-        classifier = CmcTissueClassifier(wm, gm, csf,
-                                         step_size=step_size,
-                                         average_voxel_size=voxel_size)
+        classifier = CmcTissueClassifier.from_pve(wm, gm, csf,
+                                                  step_size=step_size,
+                                                  average_voxel_size=voxel_size)
         logging.info('classifier done')
         seed_mask, _ = load_nifti(seeding_path)
         seeds = \
