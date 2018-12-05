796ca8caef107f2cb040875ecbdc44fd7c5c4b73
ShreyasFadnavis <shreyasfadnavis@gmail.com>
2018-12-05 14:07:44 -0500

    eigh

--- dipy/reconst/bayes_learn.py
+++ dipy/reconst/bayes_learn.py
@@ -3,7 +3,6 @@ import nibabel as nib
 import numpy as np
 from dipy.io import read_bvals_bvecs
 from scipy.io import loadmat
-import dipy.reconst.dti as dti
 from dipy.core.gradients import gradient_table
 
 
@@ -14,14 +13,79 @@ fdata = '/home/shreyasfadnavis/Downloads/baydiff.minimal/example_data/data_hex28
 
 # loading the data and creating the mask as per Matlab
 bvals, bvecs = read_bvals_bvecs(fbvals, fbvecs)
+gtab = gradient_table(bvals, bvecs)
 img = nib.load(fdata)
 dwi = img.get_data()
 mask = dwi[:, :, :, -1]
 mask[mask > 150] = 0
-# mat = loadmat('baydif_model.mat')
-gtab = gradient_table(bvals, bvecs)
-noise = 'estglobal'
-sigma_snrestimation = 1.75
+
+# generate the tensors
+T = 1
+ten = np.zeros((3, 3, 28))
+for i in range(len(bvals)):
+    gdir = T * bvecs.T[:, i]
+    gdir = gdir / (np.spacing(1) + np.linalg.norm(gdir))
+    gdir = np.expand_dims(gdir, axis=0)
+    ten[:, :, i] = gdir * gdir.T * bvals[i]
+
+ten = ten/1000
+b = np.squeeze(ten[0, 0, :] + ten[1, 1, :] + ten[2, 2, :])
+buni = np.unique(np.round(b*10))*100
+
+# raise error for  b0_threshold
+if min(buni) != 0:
+    ValueError('no b0 available ...')
+
+print('DWI Scheme')
+for i in range(len(buni)):
+    print('bval: ', buni[i], 'num: ', np.sum(np.round(buni[i]/100) ==
+          np.round(b*10)))
+
+print('Drawing Parameters')
+nz = 0.01
+B0 = np.arange(0.3, 1.01, 0.01)
+B0 = pow(B0, 4)
+
+# sample parameter space
+
+nt = 10000
+D1 = 0.2 + np.random.rand(nt, 1) * 2.8
+D2 = 0.2 + np.random.rand(nt, 1) * 2.8
+D3 = 0.2 + np.random.rand(nt, 1) * 1.3
+V = np.random.rand(nt, 1) * 1
+Vw = (1-V) * np.random.rand(nt, 1)
+
+# select those with similar traces
+idx = np.where(abs(D1-(D2+2*D3)) < 0.5)
+
+D1 = D1[idx]
+D2 = D2[idx]
+D3 = D3[idx]
+V = V[idx]
+Vw = Vw[idx]
+
+# define derived parameters
+meanD = ((D1*V) + ((D2+2*D3) * (1-(V+Vw))) + 9*Vw/3)
+microAx = ((D1*V) + ((D2)*(1-(V+Vw))) + 3*Vw)
+microRad = ((D3)*(1 - (V+Vw)) + 3*Vw)
+microFA = np.double(abs((D2-D1)/D3 + 4) > np.sqrt(40/3))
+
+# number of samples
+numsamples = len(D1)
+
+
+# computes q and b=|q| from tensor rep
+def getDirs(ten):
+    scheme = np.zeros((3, 28))
+    for i in range(ten.shape[2]):
+        D, U = sc.linalg.eigh(ten[:, :, i])
+        sorted_indices_D = np.argsort(D)[::-1]
+        sorted_indices_U = np.argsort(D)[::-1]
+        D2 = D[sorted_indices_D]
+        U2 = U[sorted_indices_U]
+        scheme[:, i] = np.sqrt(D2[0])*U2[:, 0]
+        b = pow(scheme, 2)
+        return scheme, b
 
 
 def fspecial_gauss(shape=(3, 3), sigma=0.5):
@@ -54,6 +118,7 @@ def iso_gaussian_3d(mu=5, sigma=0.2, dim=11):
                 out[i, j, k] = (denom * np.exp(numer))
     return out
 
+
 def makeGau(sigma, sz):
     ng = np.ceil(sigma * 5)
     ng = ng + np.mod(ng, 2) + 1
@@ -65,15 +130,6 @@ def makeGau(sigma, sz):
                        np.mod(sz, 2) + 1), 'constant')
     gau = sc.fft(sc.ifft(gau))
     gau = gau / gau[0]
-
-
-# generate the tensors
-T = 0
-ten = np.zeros((3, 3, 28))
-for i in range(len(bvals)):
-    gdir = T * bvecs.T[:, i]
-    gdir = gdir / (np.spacing(1) + np.linalg.norm(gdir))
-    ten[:, :, i] = gdir * gdir.T * bvals[i]
 
 sigma_smooth = 0
 b = np.squeeze(ten[0, 0, :] + ten[1, 1, :] + ten[2, 2, :])
