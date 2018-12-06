79ee4d9b10a7ddd404198aec9d5bb676ad1202dd
David <davhunt@indiana.edu>
2018-12-06 14:10:43 -0500

    pep8

--- dipy/workflows/stats.py
+++ dipy/workflows/stats.py
@@ -19,16 +19,20 @@ from dipy.segment.mask import bounding_box
 
 from dipy.workflows.workflow import Workflow
 
+
 class SNRinCCFlow(Workflow):
-    
+
     @classmethod
     def get_short_name(cls):
         return 'snrincc'
-    
-    def run(self, data_file, data_bvals, data_bvecs, mask=None, bbox_threshold=(0.6, 1, 0, 0.1, 0, 0.1), out_dir='', out_file='product.json', out_mask_cc='cc.nii.gz', out_mask_noise='mask_noise.nii.gz'):
 
-        """ Workflow for computing the signal-to-noise ratio in the corpus callosum
-            
+    def run(self, data_file, data_bvals, data_bvecs, mask=None,
+            bbox_threshold=(0.6, 1, 0, 0.1, 0, 0.1), out_dir='',
+            out_file='product.json', out_mask_cc='cc.nii.gz',
+            out_mask_noise='mask_noise.nii.gz'):
+        """ Workflow for computing the signal-to-noise ratio in the
+            corpus callosum
+
         Parameters
         ----------
         data_file : string
@@ -41,7 +45,8 @@ class SNRinCCFlow(Workflow):
         mask : string, optional
             Path of mask if desired. (default None)
         bbox_threshold : string, optional
-            Threshold for bounding box, values separated with commas for ex. [0.6,1,0,0.1,0,0.1]. (default (0.6, 1, 0, 0.1, 0, 0.1))
+            Threshold for bounding box, values separated with commas for ex.
+            [0.6,1,0,0.1,0,0.1]. (default (0.6, 1, 0, 0.1, 0, 0.1))
         out_dir : string, optional
             Where the resulting file will be saved. (default '')
         out_file : string, optional
@@ -49,30 +54,33 @@ class SNRinCCFlow(Workflow):
         out_mask_cc : string, optional
             Name of the CC mask volume to be saved (default 'cc.nii.gz')
         out_mask_noise : string, optional
-            Name of the mask noise volume to be saved (default 'mask_noise.nii.gz')
+            Name of the mask noise volume to be saved
+            (default 'mask_noise.nii.gz')
         """
-        
+
         if not isinstance(bbox_threshold, tuple):
-            b = bbox_threshold.replace("[","")
-            b = b.replace("]","")
-            b = b.replace("(","")
-            b = b.replace(")","")
-            b = b.replace(" ","")
+            b = bbox_threshold.replace("[", "")
+            b = b.replace("]", "")
+            b = b.replace("(", "")
+            b = b.replace(")", "")
+            b = b.replace(" ", "")
             b = b.split(",")
             for i in range(len(b)):
                 b[i] = float(b[i])
             bbox_threshold = tuple(b)
 
         io_it = self.get_io_iterator()
-        
-        for data_path, data_bvals_path, data_bvecs_path, out_path, cc_mask_path, mask_noise_path in io_it:
+
+        for data_path, data_bvals_path, data_bvecs_path, out_path, \
+                cc_mask_path, mask_noise_path in io_it:
             img = nib.load('{0}'.format(data_path))
-            bvals, bvecs = read_bvals_bvecs('{0}'.format(data_bvals_path), '{0}'.format(data_bvecs_path))
+            bvals, bvecs = read_bvals_bvecs('{0}'.format(
+                data_bvals_path), '{0}'.format(data_bvecs_path))
             gtab = gradient_table(bvals, bvecs)
-            
+
             data = img.get_data()
             affine = img.affine
-            
+
             logging.info('Computing brain mask...')
             b0_mask, calc_mask = median_otsu(data)
 
@@ -86,16 +94,17 @@ class SNRinCCFlow(Workflow):
             tenmodel = TensorModel(gtab)
             tensorfit = tenmodel.fit(data, mask=mask)
 
-            logging.info('Computing worst-case/best-case SNR using the corpus callosum...')
+            logging.info(
+                'Computing worst-case/best-case SNR using the CC...')
             threshold = bbox_threshold
-            
+
             if np.ndim(data) == 4:
                 CC_box = np.zeros_like(data[..., 0])
             elif np.ndim(data) == 3:
                 CC_box = np.zeros_like(data)
             else:
                 raise IOError('DWI data has invalid dimensions')
-            
+
             mins, maxs = bounding_box(mask)
             mins = np.array(mins)
             maxs = np.array(maxs)
@@ -106,12 +115,13 @@ class SNRinCCFlow(Workflow):
             CC_box[bounds_min[0]:bounds_max[0],
                    bounds_min[1]:bounds_max[1],
                    bounds_min[2]:bounds_max[2]] = 1
-                   
+
             mask_cc_part, cfa = segment_from_cfa(tensorfit, CC_box, threshold,
                                                  return_cfa=True)
-                                                 
+
             cfa_img = nib.Nifti1Image((cfa*255).astype(np.uint8), affine)
-            mask_cc_part_img = nib.Nifti1Image(mask_cc_part.astype(np.uint8), affine)
+            mask_cc_part_img = nib.Nifti1Image(
+                mask_cc_part.astype(np.uint8), affine)
             nib.save(mask_cc_part_img, cc_mask_path)
             logging.info('CC mask saved as {0}'.format(cc_mask_path))
 
@@ -119,18 +129,22 @@ class SNRinCCFlow(Workflow):
             mask_noise = binary_dilation(mask, iterations=10)
             mask_noise[..., :mask_noise.shape[-1]//2] = 1
             mask_noise = ~mask_noise
-            mask_noise_img = nib.Nifti1Image(mask_noise.astype(np.uint8), affine)
+            mask_noise_img = nib.Nifti1Image(
+                mask_noise.astype(np.uint8), affine)
             nib.save(mask_noise_img, mask_noise_path)
             logging.info('Mask noise saved as {0}'.format(mask_noise_path))
-            
+
             noise_std = np.std(data[mask_noise, :])
             logging.info('Noise standard deviation sigma= ' + str(noise_std))
 
             idx = np.sum(gtab.bvecs, axis=-1) == 0
             gtab.bvecs[idx] = np.inf
-            axis_X = np.argmin(np.sum((gtab.bvecs-np.array([1, 0, 0])) ** 2, axis=-1))
-            axis_Y = np.argmin(np.sum((gtab.bvecs-np.array([0, 1, 0])) ** 2, axis=-1))
-            axis_Z = np.argmin(np.sum((gtab.bvecs-np.array([0, 0, 1])) ** 2, axis=-1))
+            axis_X = np.argmin(
+                np.sum((gtab.bvecs-np.array([1, 0, 0])) ** 2, axis=-1))
+            axis_Y = np.argmin(
+                np.sum((gtab.bvecs-np.array([0, 1, 0])) ** 2, axis=-1))
+            axis_Z = np.argmin(
+                np.sum((gtab.bvecs-np.array([0, 0, 1])) ** 2, axis=-1))
 
             SNR_output = []
             SNR_directions = []
@@ -139,16 +153,20 @@ class SNRinCCFlow(Workflow):
                     SNR = mean_signal[0]/noise_std
                     logging.info("SNR for the b=0 image is :" + str(SNR))
                 else:
-                    logging.info("SNR for direction " + str(direction) + " " + str(gtab.bvecs[direction]) + "is :" + str(SNR))
+                    logging.info("SNR for direction " + str(direction) +
+                                 " " + str(gtab.bvecs[direction]) + "is :" +
+                                 str(SNR))
                     SNR_directions.append(direction)
                     SNR = mean_signal[direction]/noise_std
                 SNR_output.append(SNR)
-            
+
             data = []
             data.append({
-                        'data': str(SNR_output[0]) + ' ' + str(SNR_output[1]) + ' ' + str(SNR_output[2]) + ' ' + str(SNR_output[3]),
-                        'directions': 'b0' + ' ' + str(SNR_directions[0]) + ' ' + str(SNR_directions[1]) + ' ' + str(SNR_directions[2])
+                        'data': str(SNR_output[0]) + ' ' + str(SNR_output[1]) +
+                        ' ' + str(SNR_output[2]) + ' ' + str(SNR_output[3]),
+                        'directions': 'b0' + ' ' + str(SNR_directions[0]) + ' '
+                        + str(SNR_directions[1]) + ' ' + str(SNR_directions[2])
                         })
-            
+
             with open(os.path.join(out_dir, out_file), 'w') as myfile:
                 json.dump(data, myfile)
--- dipy/workflows/tests/test_stats.py
+++ dipy/workflows/tests/test_stats.py
@@ -13,6 +13,7 @@ from nose.tools import assert_true, assert_equal
 from dipy.data import get_data
 from dipy.workflows.stats import SNRinCCFlow
 
+
 def test_stats():
     with TemporaryDirectory() as out_dir:
         data_path, bval_path, bvec_path = get_data('small_101D')
@@ -22,35 +23,43 @@ def test_stats():
         mask_img = nib.Nifti1Image(mask.astype(np.uint8), vol_img.affine)
         mask_path = join(out_dir, 'tmp_mask.nii.gz')
         nib.save(mask_img, mask_path)
-        
+
         snr_flow = SNRinCCFlow(force=True)
         args = [data_path, bval_path, bvec_path]
-        
+
         snr_flow.run(*args, out_dir=out_dir)
         assert_true(os.path.exists(os.path.join(out_dir, 'product.json')))
-        assert_true(os.stat(os.path.join(out_dir, 'product.json')).st_size != 0)
+        assert_true(os.stat(os.path.join(
+            out_dir, 'product.json')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'cc.nii.gz')))
         assert_true(os.stat(os.path.join(out_dir, 'cc.nii.gz')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'mask_noise.nii.gz')))
-        assert_true(os.stat(os.path.join(out_dir, 'mask_noise.nii.gz')).st_size != 0)
-        
+        assert_true(os.stat(os.path.join(
+            out_dir, 'mask_noise.nii.gz')).st_size != 0)
+
         snr_flow._force_overwrite = True
         snr_flow.run(*args, mask=mask_path, out_dir=out_dir)
         assert_true(os.path.exists(os.path.join(out_dir, 'product.json')))
-        assert_true(os.stat(os.path.join(out_dir, 'product.json')).st_size != 0)
+        assert_true(os.stat(os.path.join(
+            out_dir, 'product.json')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'cc.nii.gz')))
         assert_true(os.stat(os.path.join(out_dir, 'cc.nii.gz')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'mask_noise.nii.gz')))
-        assert_true(os.stat(os.path.join(out_dir, 'mask_noise.nii.gz')).st_size != 0)
-        
+        assert_true(os.stat(os.path.join(
+            out_dir, 'mask_noise.nii.gz')).st_size != 0)
+
         snr_flow._force_overwrite = True
-        snr_flow.run(*args, bbox_threshold=(0.5, 1, 0, 0.15, 0, 0.2), out_dir=out_dir)
+        snr_flow.run(*args, bbox_threshold=(0.5, 1, 0,
+                                            0.15, 0, 0.2), out_dir=out_dir)
         assert_true(os.path.exists(os.path.join(out_dir, 'product.json')))
-        assert_true(os.stat(os.path.join(out_dir, 'product.json')).st_size != 0)
+        assert_true(os.stat(os.path.join(
+            out_dir, 'product.json')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'cc.nii.gz')))
         assert_true(os.stat(os.path.join(out_dir, 'cc.nii.gz')).st_size != 0)
         assert_true(os.path.exists(os.path.join(out_dir, 'mask_noise.nii.gz')))
-        assert_true(os.stat(os.path.join(out_dir, 'mask_noise.nii.gz')).st_size != 0)
+        assert_true(os.stat(os.path.join(
+            out_dir, 'mask_noise.nii.gz')).st_size != 0)
+
 
 if __name__ == '__main__':
     test_stats()
