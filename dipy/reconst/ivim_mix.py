30666f33fbec8ce76e8a4950b7997273d50628bc
ShreyasFadnavis <shreyasfadnavis@gmail.com>
2019-01-23 20:38:28 -0500

    cleaned code

--- dipy/reconst/ivim_mix.py
+++ dipy/reconst/ivim_mix.py
@@ -10,9 +10,8 @@ class IVIMModel(ReconstModel):
     def __init__(self, bvals, fit_method='MIX'):
         r""" MIX framework (MIX) [1]_.
 
-        The MIX computes the ActiveAx parameters. ActiveAx is a multi
-        compartment model, (sum of exponentials).
-        This algorithm uses three different optimizer. It starts with a
+        The MIX computes the IVIM parameters.
+        This algorithm uses three different optimizers. It starts with a
         differential evolutionary algorithm and fits the parameters in the
         power of exponentials. Then the fitted parameters in the first step are
         utilized to make a linear convex problem. Using a convex optimization,
@@ -29,7 +28,7 @@ class IVIMModel(ReconstModel):
 
         Returns
         -------
-        ActiveAx parameters
+        IVIM_MIX parameters
 
         References
         ----------
@@ -39,17 +38,15 @@ class IVIMModel(ReconstModel):
 
         """
 
-        self.maxiter = 1000  # The maximum number of generations, genetic
-#        algorithm 1000 default, 1
-        self.xtol = 1e-8  # Tolerance for termination, nonlinear least square
-#        1e-8 default, 1e-3
+        self.maxiter = 1000  # maximum no. of iter for differential evolution
+        self.xtol = 1e-8  # Tolerance for termination: nonlinear least square
         self.bvals = bvals
         self.yhat_perfusion = np.zeros(self.bvals.shape[0])
         self.yhat_diffusion = np.zeros(self.bvals.shape[0])
         self.exp_phi1 = np.zeros((self.bvals.shape[0], 2))
 
     def fit(self, data):
-        """ Fit method of the ActiveAx model class
+        """ Fit method of the IVIMModel model class
 
         Parameters
         ----------
@@ -74,19 +71,11 @@ class IVIMModel(ReconstModel):
 
     def stoc_search_cost(self, x, signal):
         """
-        Aax_exvivo_nlin
-
-        Cost function for genetic algorithm
+        Cost function for differntial evolution algorithm
 
         Parameters
         ----------
         x : array
-            x.shape = 4x1
-            x(0) theta (radian)
-            x(1) phi (radian)
-            x(2) R (micrometers)
-            x(3) v=f1/(f1+f2) (0.1 - 0.8)
-
         bvals
         bvecs
         G: gradient strength
@@ -109,13 +98,13 @@ class IVIMModel(ReconstModel):
             (signal -  S)^T(signal -  S)
         """
         phi = self.Phi(x)
-        return self.activeax_cost_one(phi, signal)
+        return self.ivim_mix_cost_one(phi, signal)
 
-    def activeax_cost_one(self, phi, signal):  # sigma
+    def ivim_mix_cost_one(self, phi, signal):  # sigma
 
         """
-        Aax_exvivo_nlin
-        to make cost function for genetic algorithm
+        ivim_mix_nlin
+        to make cost function for differential evolution algorithm
         Parameters
         ----------
         phi:
@@ -144,21 +133,17 @@ class IVIMModel(ReconstModel):
         Parameters
         ----------
         phi : array
-            phi.shape = number of data points x 4
-            signal : array
-            signal.shape = number of data points x 1
+        signal : array
 
         Returns
         -------
-        f1, f2, f3, f4 (volume fractions)
+        f1, f2 (volume fractions)
         f1 = fe[0]
         f2 = fe[1]
-        f3 = fe[2]
-        f4 = fe[3]
 
         Notes
         --------
-        cost function for genetic algorithm:
+        cost function for differential evolution algorithm:
 
         .. math::
 
@@ -189,18 +174,8 @@ class IVIMModel(ReconstModel):
         Parameters
         ----------
         x_fe : array
-            x_fe(0) x_fe(1) x_fe(2)  are f1 f2 f3
-            x_fe(3) theta
-            x_fe(4) phi
-            x_fe(5) R
-            x_fe(6) as f4
 
-        signal_param : array
-            signal_param.shape = number of data points x 7
-
-            signal_param = np.hstack([signal[:, None], bvals[:, None], bvecs,
-                                  G[:, None], small_delta[:, None],
-                                  big_delta[:, None]])
+        signal : array
 
         Returns
         -------
