88dc62b9fb9030ee4953c9e7ac69427aa7450ac8
frheault <francois.m.rheault@usherbrooke>
2019-02-23 15:57:57 -0500

    Fixed the random state of both RB object, to fix crash on python 3.6

--- dipy/segment/tests/test_refine_rb.py
+++ dipy/segment/tests/test_refine_rb.py
@@ -83,15 +83,18 @@ def test_rb_disable_slr():
 
 def test_rb_slr_threads():
 
-    rb = RecoBundles(f, greater_than=0, clust_thr=10)
+    rng_multi = np.random.RandomState(42)
+    rb_multi = RecoBundles(f, greater_than=0, clust_thr=10,
+                           rng=np.random.RandomState(42))
+    rec_trans_multi_threads, _ = rb_multi.recognize(model_bundle=f2,
+                                                    model_clust_thr=5.,
+                                                    reduction_thr=10,
+                                                    slr=True,
+                                                    slr_num_threads=None)
 
-    rec_trans_multi_threads, _ = rb.recognize(model_bundle=f2,
-                                              model_clust_thr=5.,
-                                              reduction_thr=10,
-                                              slr=True,
-                                              slr_num_threads=None)
-
-    rec_trans_single_thread, _ = rb.recognize(model_bundle=f2,
+    rb_single = RecoBundles(f, greater_than=0, clust_thr=10,
+                            rng=np.random.RandomState(42))
+    rec_trans_single_thread, _ = rb_single.recognize(model_bundle=f2,
                                               model_clust_thr=5.,
                                               reduction_thr=10,
                                               slr=True,
