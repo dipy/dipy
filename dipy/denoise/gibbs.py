dce42ebdffb6e24bbe1cb9af8a35fe3d2a7b71a5
Rafael Neto Henriques <rafaelnh21@gmail.com>
2019-03-14 14:54:10 -0400

    NF: Add a function to shift image voxel position using liner interpolation
    This function is required to convert images to the right space after gibbs corection along a given axis

--- dipy/denoise/gibbs.py
+++ dipy/denoise/gibbs.py
@@ -1,6 +1,46 @@
 import numpy as np
 
 
+def image_shift(x, s, a=0):
+    """ Shift elements of matrix x by a position difference s along axis a.
+
+    Parameters
+    ----------
+    x : 2D ndarray
+        Original values of matrix x
+    s : float
+        Value of the shift
+    a : int (0 or 1)
+        Axis along which the shift will be applied.
+        Default a is set to 0.
+
+    Returns
+    -------
+    Shifted version of matrix x.
+
+    Note
+    ----
+    The values of the new shifted matrix are calculated using linear
+    interpolation of the values of the original matrix.
+    """
+    if a:
+        xs = x.copy()
+    else:
+        xs = x.T.copy()
+
+    if s >= 1 or s <= -1:
+        raise ValueError('Shift should be a value between -1 and 1')
+    elif s > 0:
+        xs[:, :-1] = (xs[:, 1:] - xs[:, :-1]) * s + xs[:, :-1]
+    else:
+        xs[:, 1:] = (xs[:, 1:] - xs[:, :-1]) * (1+s) + xs[:, :-1]
+
+    if a:
+        return xs
+    else:
+        return xs.T
+
+
 def image_tv(x, fn=0, nn=3, a=0):
     """ Computes total variation (TV) of matrix x along axis a in two
     directions.
