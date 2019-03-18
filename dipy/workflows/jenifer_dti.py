615e7cb7639b1f81e88ee01008a05e671f16d680
ShreyasFadnavis <shreyasfadnavis@gmail.com>
2019-03-18 15:48:42 -0400

    rem files

--- dipy/workflows/jenifer_dti.py
+++
@@ -1,8 +0,0 @@
-import nibabel as nib
-
-fdwi = "dMRI_CMRR_dir98_AP_4.nii.gz"
-fbval = "dMRI_CMRR_dir98_AP_4.bval"
-fbvec = "dMRI_CMRR_dir98_AP_4.bvec"
-
-img = nib.load(fdwi)
-data = img.get_data()
