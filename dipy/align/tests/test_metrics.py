493347a4ff6d915fa91d24271a39c315704722db
skoudoro <skab12@gmail.com>
2019-05-04 17:59:28 -0400

    update tests

--- dipy/align/tests/test_metrics.py
+++ dipy/align/tests/test_metrics.py
@@ -16,20 +16,29 @@ def test_exceptions():
     assert_raises(ValueError, SSDMetric, 3, step_type='unknown_metric_name')
     assert_raises(ValueError, EMMetric, 3, step_type='unknown_metric_name')
 
-    def check_image_size_error(metric):
-        metric.set_static_image(np.arange(np.prod(shape)).reshape(shape),
-                                np.eye(4), np.ones(3), np.eye(3))
-        metric.set_moving_image(np.arange(np.prod(shape)).reshape(shape),
-                                np.eye(4), np.ones(3), np.eye(3))
-        assert_raises(IOError, metric.initialize_iteration)
+    def init_metric(shape, radius):
+        dim = len(shape)
+        metric = CCMetric(dim, radius=radius)
+        metric.set_static_image(np.arange(np.prod(shape),
+                                          dtype=np.float).reshape(shape),
+                                np.eye(4), np.ones(dim), np.eye(3))
+        metric.set_moving_image(np.arange(np.prod(shape),
+                                dtype=np.float).reshape(shape),
+                                np.eye(4), np.ones(dim), np.eye(3))
+        return metric
 
-    metric = CCMetric(2)
-    for shape in itertools.product((5, 8), (8, 5)):
-        check_image_size_error(metric)
+    # Generate many shape combinations
+    shapes_2d = itertools.product((5, 8), (8, 5))
+    shapes_3d = itertools.product((5, 8), (8, 5), (30, 50))
+    all_shapes = itertools.chain(shapes_2d, shapes_3d)
+    # expected to fail for any dimension < 2*radius + 1.
+    for shape in all_shapes:
+        metric = init_metric(shape, 4)
+        assert_raises(ValueError, metric.initialize_iteration)
 
-    metric = CCMetric(3)
-    for shape in itertools.product((5, 8, 3), (8, 5, 30)):
-        check_image_size_error(metric)
+    # expected to pass for any dimension == 2*radius + 1.
+    metric = init_metric((9, 9), 4)
+    metric.initialize_iteration()
 
 
 def test_EMMetric_image_dynamics():
