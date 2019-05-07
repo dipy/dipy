7311775bd09ba74b2214edc5c36707fb1019627f
Ariel Rokem <arokem@gmail.com>
2019-05-06 19:18:48 -0700

    Requires vol_idx in test.

--- dipy/workflows/tests/test_workflow.py
+++ dipy/workflows/tests/test_workflow.py
@@ -16,7 +16,7 @@ def test_force_overwrite():
         mo_flow = MedianOtsuFlow(output_strategy='absolute')
 
         # Generate the first results
-        mo_flow.run(data_path, out_dir=out_dir)
+        mo_flow.run(data_path, out_dir=out_dir, vol_idx=[0])
         mask_file = mo_flow.last_generated_outputs['out_mask']
         first_time = os.path.getmtime(mask_file)
 
@@ -31,7 +31,7 @@ def test_force_overwrite():
         # Make sure that at least one second elapsed, so that time-stamp is
         # different (sometimes measured in whole seconds)
         time.sleep(1)
-        mo_flow.run(data_path, out_dir=out_dir)
+        mo_flow.run(data_path, out_dir=out_dir, vol_idx=[0])
         mask_file = mo_flow.last_generated_outputs['out_mask']
         third_time = os.path.getmtime(mask_file)
         assert third_time != second_time
