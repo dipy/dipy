fb4b82e519887b8231bf67e267ef9a777b070624
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-05-25 01:01:07 -0400

    Slider color changed and picking seems working both in 3d and 4d

--- dipy/viz/panel.py
+++ dipy/viz/panel.py
@@ -97,7 +97,13 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
                                     text_template="{value:.0f}",
                                     length=140)
 
-    line_slider_z.default_color = (1, 0.5, 0)
+    def _color_slider(slider):
+        slider.default_color = (1, 0.5, 0)
+        slider.track.color = (0.8, 0.3, 0)
+        slider.active_color = (0.9, 0.4, 0)
+        slider.handle.color = (1, 0.5, 0)
+
+    _color_slider(line_slider_z)
 
     line_slider_x = ui.LineSlider2D(min_value=0,
                                     max_value=shape[0] - 1,
@@ -105,16 +111,23 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
                                     text_template="{value:.0f}",
                                     length=140)
 
+    _color_slider(line_slider_x)
+
     line_slider_y = ui.LineSlider2D(min_value=0,
                                     max_value=shape[1] - 1,
                                     initial_value=shape[1] / 2,
                                     text_template="{value:.0f}",
                                     length=140)
 
+    _color_slider(line_slider_y)
+
     opacity_slider = ui.LineSlider2D(min_value=0.0,
                                      max_value=1.0,
                                      initial_value=slicer_opacity,
-                                     length=140)
+                                     length=140,
+                                     text_template="{ratio:.0%}")
+
+    _color_slider(opacity_slider)
 
     volume_slider = ui.LineSlider2D(min_value=0,
                                     max_value=data.shape[-1] - 1,
@@ -122,11 +135,7 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
                                     length=140,
                                     text_template="{value:.0f}", shape='square')
 
-    def change_slice_z(slider):
-        z = int(np.round(slider.value))
-        change_volume.image_actor_z.display_extent(0, shape[0] - 1,
-                                                   0, shape[1] - 1, z, z)
-        change_slice_z.z = z
+    _color_slider(volume_slider)
 
     def change_slice_x(slider):
         x = int(np.round(slider.value))
@@ -134,12 +143,17 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
                                                    shape[2] - 1)
         change_slice_x.x = x
 
-
     def change_slice_y(slider):
         y = int(np.round(slider.value))
         change_volume.image_actor_y.display_extent(0, shape[0] - 1, y, y,
                                                    0, shape[2] - 1)
         change_slice_y.y = y
+
+    def change_slice_z(slider):
+        z = int(np.round(slider.value))
+        change_volume.image_actor_z.display_extent(0, shape[0] - 1,
+                                                   0, shape[1] - 1, z, z)
+        change_slice_z.z = z
 
     def change_opacity(slider):
         slicer_opacity = slider.value
@@ -208,21 +222,30 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
         picker_label.message = '({}, {}, {})'.format(str(i), str(j), str(k)) + ' ' + message
         
 
+    change_volume.vol_idx = 0
+    change_volume.image_actor_x = image_actor_x
+    change_volume.image_actor_y = image_actor_y
     change_volume.image_actor_z = image_actor_z
 
+    change_volume.image_actor_x.AddObserver('LeftButtonPressEvent',
+                                            left_click_picker_callback,
+                                            1.0)
+    change_volume.image_actor_y.AddObserver('LeftButtonPressEvent',
+                                            left_click_picker_callback,
+                                            1.0)
     change_volume.image_actor_z.AddObserver('LeftButtonPressEvent',
                                             left_click_picker_callback,
                                             1.0)
 
-    change_volume.image_actor_x = image_actor_x
-    change_volume.image_actor_y = image_actor_y
-    change_slice_z.z = int(np.round(shape[2] / 2))
+
     change_slice_x.x = int(np.round(shape[0] / 2))
     change_slice_y.y = int(np.round(shape[1] / 2))
-
-    line_slider_z.on_change = change_slice_z
-    line_slider_y.on_change = change_slice_y
+    change_slice_z.z = int(np.round(shape[2] / 2))
+    
     line_slider_x.on_change = change_slice_x
+    line_slider_y.on_change = change_slice_y
+    line_slider_z.on_change = change_slice_z
+    
     opacity_slider.on_change = change_opacity
     
     volume_slider.handle_events(volume_slider.handle.actor)
@@ -230,9 +253,10 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
 
     # volume_slider.on_right_mouse_button_released = change_volume2
 
-    line_slider_label_z = build_label(text="Z Slice")
     line_slider_label_x = build_label(text="X Slice")
     line_slider_label_y = build_label(text="Y Slice")
+    line_slider_label_z = build_label(text="Z Slice")
+    
     opacity_slider_label = build_label(text="Opacity")
     volume_slider_label = build_label(text="Volume")
     picker_label = build_label(text = '')
@@ -250,18 +274,18 @@ def slicer_panel(renderer, iren, data=None, affine=None, world_coords=False):
 
     ys = np.linspace(0, 1, 8)
 
-    panel.add_element(line_slider_x, coords=(0.4, ys[1]))
+    panel.add_element(line_slider_z, coords=(0.4, ys[1]))
     panel.add_element(line_slider_y, coords=(0.4, ys[2]))
-    panel.add_element(line_slider_z, coords=(0.4, ys[3]))
+    panel.add_element(line_slider_x, coords=(0.4, ys[3]))
     panel.add_element(opacity_slider, coords=(0.4, ys[4]))
 
     if data.ndim == 4:    
         panel.add_element(volume_slider, coords=(0.4, ys[6]))
     
     
-    panel.add_element(line_slider_label_x, coords=(0.1, ys[1]))
+    panel.add_element(line_slider_label_z, coords=(0.1, ys[1]))
     panel.add_element(line_slider_label_y, coords=(0.1, ys[2]))
-    panel.add_element(line_slider_label_z, coords=(0.1, ys[3]))
+    panel.add_element(line_slider_label_x, coords=(0.1, ys[3]))
     panel.add_element(opacity_slider_label, coords=(0.1, ys[4]))
     
     if data.ndim == 4:    
