9c8876d46682fd415730f1dcaf0c9a64f373245a
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-06-17 16:04:56 -0400

    ENH: increasing number of global memory attributes

--- dipy/viz/gmem.py
+++ dipy/viz/gmem.py
@@ -1,5 +1,30 @@
 class GlobalHorizon(object):
-    slicer_ctrls = None
-    cluster_ctrls = None
+    window_timer_cnt = 0
+    
+    slicer_opacity = 1
+    slicer_colormap = 'gray'
+    slicer_colormaps = ['gray', 'magma', 'viridis', 'Pastel1', 'disting']
+    slicer_axes = ['x', 'y', 'z']
+    
+    slicer_curr_x = None
+    slicer_curr_y = None
+    slicer_curr_z = None
+    
+    slicer_curr_actor_x = None
+    slicer_curr_actor_y = None
+    slicer_curr_actor_z = None
+    
+    slicer_orig_shape = None
+    slicer_resliced_shape = None
+    
+    slicer_vol_idx = None
+    slicer_vol = None
+
+    slicer_peaks_actor_z = None
+
+    slicer_grid = False
+    
+    cluster_thr = 15
+    
 
 HORIZON = GlobalHorizon()
