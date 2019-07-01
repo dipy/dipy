e5e66f733092d755f384c59696a300611816db54
Serge Koudoro <skab12@gmail.com>
2019-07-01 14:34:04 +0200

    remove numpy_less

--- dipy/utils/arrfuncs.py
+++ dipy/utils/arrfuncs.py
@@ -1,14 +1,7 @@
 """ Utilities to manipulate numpy arrays """
 
-import sys
-from distutils.version import LooseVersion
-
 import numpy as np
-
-from nibabel.volumeutils import endian_codes, native_code, swapped_code
-
-
-NUMPY_LESS_1_8 = LooseVersion(np.version.short_version) < '1.8'
+from nibabel.volumeutils import endian_codes, native_code
 
 
 def as_native_array(arr):
@@ -62,27 +55,17 @@ def pinv(a, rcond=1e-15):
     np.linalg.pinv
     """
     a = np.asarray(a)
-    if NUMPY_LESS_1_8:
-        if a.ndim <= 2:
-            # properly handle the case of a single 2D array
-            return np.linalg.pinv(a, rcond)
-        shape = a.shape[:-2]
-        a = a.reshape(-1, a.shape[-2], a.shape[-1])
-        result = np.empty((a.shape[0], a.shape[2], a.shape[1]))
-        for i, item in enumerate(a):
-            result[i] = np.linalg.pinv(item, rcond)
-        return result.reshape(shape + (a.shape[2], a.shape[1]))
-    else:
-        swap = np.arange(a.ndim)
-        swap[[-2, -1]] = swap[[-1, -2]]
-        u, s, v = np.linalg.svd(a, full_matrices=False)
-        cutoff = np.maximum.reduce(s, axis=-1, keepdims=True) * rcond
-        mask = s > cutoff
-        s[mask] = 1. / s[mask]
-        s[~mask] = 0
-        return np.einsum('...ij,...jk',
-                         np.transpose(v, swap) * s[..., None, :],
-                         np.transpose(u, swap))
+
+    swap = np.arange(a.ndim)
+    swap[[-2, -1]] = swap[[-1, -2]]
+    u, s, v = np.linalg.svd(a, full_matrices=False)
+    cutoff = np.maximum.reduce(s, axis=-1, keepdims=True) * rcond
+    mask = s > cutoff
+    s[mask] = 1. / s[mask]
+    s[~mask] = 0
+    return np.einsum('...ij,...jk',
+                     np.transpose(v, swap) * s[..., None, :],
+                     np.transpose(u, swap))
 
 
 def eigh(a, UPLO='L'):
@@ -116,13 +99,4 @@ def eigh(a, UPLO='L'):
     np.linalg.eigh
     """
     a = np.asarray(a)
-    if a.ndim > 2 and NUMPY_LESS_1_8:
-        shape = a.shape[:-2]
-        a = a.reshape(-1, a.shape[-2], a.shape[-1])
-        evals = np.empty((a.shape[0], a.shape[1]))
-        evecs = np.empty((a.shape[0], a.shape[1], a.shape[1]))
-        for i, item in enumerate(a):
-            evals[i], evecs[i] = np.linalg.eigh(item, UPLO)
-        return (evals.reshape(shape + (a.shape[1], )),
-                evecs.reshape(shape + (a.shape[1], a.shape[1])))
     return np.linalg.eigh(a, UPLO)
