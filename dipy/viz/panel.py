6bbfdbc04ef49fbed1a6d6249e5e77129b25a360
ShreyasFadnavis <shreyasfadnavis@gmail.com>
2019-08-22 12:37:04 -0400

    del: moving to fury

--- dipy/viz/panel.py
+++ dipy/viz/panel.py
@@ -1,15 +1,8 @@
-import os
 import numpy as np
 from dipy.utils.optpkg import optional_package
 import itertools
-from nibabel.tmpdirs import InTemporaryDirectory
 
 fury, have_fury, setup_module = optional_package('fury')
-_, have_imread, _ = optional_package('Image')
-matplotlib, have_mpl, _ = optional_package("matplotlib")
-
-if have_imread:
-    from scipy.misc import imread
 
 if have_fury:
     from dipy.viz import actor, ui, colormap
@@ -538,51 +531,3 @@ def slicer_panel(renderer, iren,
     apply_colormap(r1, r2)
 
     return panel
-
-
-def matplotlib_figure_to_numpy(fig, dpi=100, fname=None, flip_up_down=True,
-                               transparent=False):
-    r""" Convert a Matplotlib figure to a 3D numpy array with RGBA channels
-    Parameters
-    ----------
-    fig : obj,
-        A matplotlib figure object
-    dpi : int
-        Dots per inch
-    fname : str
-        If ``fname`` is given then the array will be saved as a png to this
-        position.
-    flip_up_down : bool
-        The origin is different from matlplotlib default and VTK's default
-        behaviour (default True).
-    transparent : bool
-        Make background transparent (default False).
-    Returns
-    -------
-    arr : ndarray
-        a numpy 3D array of RGBA values
-    Notes
-    ------
-    The safest way to read the pixel values from the figure was to save them
-    using savefig as a png and then read again the png. There is a cleaner
-    way found here http://www.icare.univ-lille1.fr/drupal/node/1141 where
-    you can actually use fig.canvas.tostring_argb() to get the values directly
-    without saving to the disk. However, this was not stable across different
-    machines and needed more investigation from what time permited.
-    """
-
-    if fname is None:
-        with InTemporaryDirectory() as tmpdir:
-            fname = os.path.join(tmpdir, 'tmp.png')
-            fig.savefig(fname, dpi=dpi, transparent=transparent,
-                        bbox_inches='tight', pad_inches=0)
-            arr = imread(fname)
-    else:
-        fig.savefig(fname, dpi=dpi, transparent=transparent,
-                    bbox_inches='tight', pad_inches=0)
-        arr = imread(fname)
-
-    if flip_up_down:
-        arr = np.flipud(arr)
-
-    return arr
