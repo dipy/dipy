ecccb74e9d42949598d74608c9642e5b3733ef20
skoudoro <skab12@gmail.com>
2019-09-30 14:33:59 -0400

    update install step + opengl step

---
+++ ci/azure/install.ps1
@@ -0,0 +1,24 @@
+# Powershell Install script
+
+if($env:INSTALL_TYPE -match "conda")
+{
+  # Get Anaconda path
+  Write-Output "Conda path: $env:CONDA\Scripts"
+  #gci env:*
+
+  Invoke-Expression "conda config --set always_yes yes --set changeps1 no"
+  Invoke-Expression "conda update -yq conda"
+  Invoke-Expression "conda install conda-build anaconda-client"
+  Invoke-Expression "conda config --add channels conda-forge"
+  Invoke-Expression "conda create -n testenv --yes python=$env:PYTHON_VERSION pip"
+  Invoke-Expression "conda install -yq --name testenv $DEPENDS"
+}
+else
+{
+  $env:PIPI = "pip install $env:EXTRA_PIP_FLAGS"
+  # Print and check this environment variable
+  Write-Output "Pip command: $env:PIPI"
+  Invoke-Expression "python -m pip install -U pip"
+  Invoke-Expression "pip --version"
+  Invoke-Expression "$env:PIPI $DEPENDS"
+}
