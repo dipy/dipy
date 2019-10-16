781c99d3eb357d2df2e4461daf73496d3663c9db
skoudoro <skab12@gmail.com>
2019-10-16 11:02:20 -0400

    force reloading to generate the warning

--- dipy/viz/tests/test_viz_import.py
+++ dipy/viz/tests/test_viz_import.py
@@ -1,4 +1,6 @@
 
+import sys
+import importlib
 import warnings
 import pytest
 
@@ -9,5 +11,10 @@ fury, has_fury, _ = optional_package('fury')
 @pytest.mark.skipif(has_fury, reason="Skipped because Fury is installed")
 def test_viz_import_warning():
     with warnings.catch_warnings(record=True) as w:
-        import dipy.viz
+        module_path = 'dipy.viz'
+        if module_path in sys.modules:
+            importlib.reload(sys.modules[module_path])
+        else:
+            importlib.import_module(module_path)
+
         assert len(w) == 1
