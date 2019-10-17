20cbb0920a7bb4d616b582953c9f737f6a8af6d4
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2019-10-17 12:04:09 -0400

    Updated travis

--- dipy/nn/tests/test_tf.py
+++ dipy/nn/tests/test_tf.py
@@ -1,10 +1,23 @@
-import numpy as np
-import random
+import pytest
+from distutils.version import LooseVersion
 from numpy.testing import (assert_array_almost_equal, assert_almost_equal,
                            assert_, assert_raises)
-import tensorflow as tf
+
+from dipy.utils.optpkg import optional_package
+
+tf_gpu , have_tf_gpu, _ = optional_package('tensorflow-gpu')
+tf , have_tf, _ = optional_package('tensorflow')
+
+if have_tf_gpu:
+    tf = tf_gpu
+    have_tf = have_tf_gpu
+
+if have_tf:
+    if LooseVersion(tf.__version__) < LooseVersion('2.0.0'):
+        raise ImportError('Please upgrade to TensorFlow 2+')
 
 
+@pytest.mark.skipif(not have_tf)
 def test_default_mnist():
 
     mnist = tf.keras.datasets.mnist
@@ -26,7 +39,6 @@ def test_default_mnist():
     model.fit(x_train, y_train, epochs=5)
 
     model.evaluate(x_test,  y_test, verbose=2)
-
 
 
 if __name__ == "__main__":
