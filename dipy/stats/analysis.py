b8fb18341e778b9c4e3712d324734ca66a8c9e62
bramshqamar <bramshq@gmail.com>
2020-03-24 21:41:05 -0400

    cleaning the code

--- dipy/stats/analysis.py
+++ dipy/stats/analysis.py
@@ -209,26 +209,17 @@ def bundle_analysis(model_bundle_folder, bundle_folder, orig_bundle_folder,
 
     dt = dict()
 
-    #mb = os.listdir(model_bundle_folder)
-    
-    #mb = glob(model_bundle_folder+"/*.trk")
-    # edit here remove this later, uncomment above line
     mb = glob(os.path.join(model_bundle_folder,"*.trk"))
     print(mb)
-    #edit end
+
     
     mb.sort()
-    #bd = os.listdir(bundle_folder)
-    #bd.sort()
-    
-    #bd = glob(bundle_folder+"/*.trk")
+  
     bd = glob(os.path.join(bundle_folder,"*.trk"))
     
     
     bd.sort()
     print(bd)
-    
-    #org_bd = os.listdir(orig_bundle_folder)
     org_bd = glob(os.path.join(orig_bundle_folder,"*.trk"))
     org_bd.sort()
     print(org_bd)
