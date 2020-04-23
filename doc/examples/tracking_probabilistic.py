b4c7e712452207e8d47ec59fe56aae0f2d69fe13
Shrishti Hore <40817931+ShrishtiHore@users.noreply.github.com>
2020-04-23 13:46:16 +0530

    Moved all important imports at top

--- doc/examples/tracking_probabilistic.py
+++ doc/examples/tracking_probabilistic.py
@@ -21,15 +21,24 @@ data and fitting a Constrained Spherical Deconvolution (CSD) model.
 
 from dipy.core.gradients import gradient_table
 from dipy.data import get_fnames
+from dipy.data import small_sphere
+from dipy.data import default_sphere
 from dipy.io.gradients import read_bvals_bvecs
 from dipy.io.image import load_nifti, load_nifti_data
+from dipy.io.stateful_tractogram import Space, StatefulTractogram
+from dipy.io.streamline import save_trk
+from dipy.direction import ProbabilisticDirectionGetter
+from dipy.direction import peaks_from_model
 from dipy.reconst.csdeconv import (ConstrainedSphericalDeconvModel,
                                    auto_response)
+from dipy.reconst.shm import CsaOdfModel
 from dipy.tracking import utils
 from dipy.tracking.local_tracking import LocalTracking
 from dipy.tracking.streamline import Streamlines
 from dipy.tracking.stopping_criterion import ThresholdStoppingCriterion
 from dipy.viz import window, actor, colormap, has_fury
+
+
 
 
 # Enables/disables interactive visualization
@@ -55,8 +64,6 @@ csd_fit = csd_model.fit(data, mask=white_matter)
 We use the GFA of the CSA model to build a stopping criterion.
 """
 
-from dipy.reconst.shm import CsaOdfModel
-
 csa_model = CsaOdfModel(gtab, sh_order=6)
 gfa = csa_model.fit(data, mask=white_matter).gfa
 stopping_criterion = ThresholdStoppingCriterion(gfa, .25)
@@ -71,11 +78,6 @@ need to clip the FOD to use it as a PMF because the latter cannot have negative
 values. Ideally, the FOD should be strictly positive, but because of noise
 and/or model failures sometimes it can have negative values.
 """
-
-from dipy.direction import ProbabilisticDirectionGetter
-from dipy.data import small_sphere
-from dipy.io.stateful_tractogram import Space, StatefulTractogram
-from dipy.io.streamline import save_trk
 
 fod = csd_fit.odf(small_sphere)
 pmf = fod.clip(min=0)
@@ -113,8 +115,6 @@ larger sphere, like ``default_sphere`` which has 362 directions on the
 hemisphere, without having to worry about memory limitations.
 """
 
-from dipy.data import default_sphere
-
 prob_dg = ProbabilisticDirectionGetter.from_shcoeff(csd_fit.shm_coeff,
                                                     max_angle=30.,
                                                     sphere=default_sphere)
@@ -142,8 +142,6 @@ Not all model fits have the ``shm_coeff`` attribute because not all models use
 this basis to represent the data internally. However we can fit the ODF of any
 model to the spherical harmonic basis using the ``peaks_from_model`` function.
 """
-
-from dipy.direction import peaks_from_model
 
 peaks = peaks_from_model(csd_model, data, default_sphere, .5, 25,
                          mask=white_matter, return_sh=True, parallel=True)
