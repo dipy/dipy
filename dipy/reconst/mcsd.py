d21ab0d360eb119c4b9b0a33acffa66518d7f567
karanphil <philippe.karan@usherbrooke.ca>
2020-06-01 11:20:16 -0400

    Changing threshold management in mask_for_response_msmt

--- dipy/reconst/mcsd.py
+++ dipy/reconst/mcsd.py
@@ -661,17 +661,17 @@ def mask_for_response_msmt(gtab, data, roi_center=None, roi_radii=10,
     mask_wm[fa > wm_fa_thr] = 1
     mask_wm *= roi_mask
 
-    md_mask_gm = np.ones(md.shape, dtype=np.int64)
-    md_mask_gm[(md > gm_md_thr)] = 0
+    md_mask_gm = np.zeros(md.shape, dtype=np.int64)
+    md_mask_gm[(md < gm_md_thr)] = 1
 
     fa_mask_gm = np.zeros(fa.shape, dtype=np.int64)
-    fa_mask_gm[(fa < gm_fa_thr) & (fa > 0)] = 1
+    fa_mask_gm[(fa < gm_fa_thr) & (fa > csf_fa_thr)] = 1
 
     mask_gm = md_mask_gm * fa_mask_gm
     mask_gm *= roi_mask
 
-    md_mask_csf = np.ones(md.shape, dtype=np.int64)
-    md_mask_csf[(md > csf_md_thr)] = 0
+    md_mask_csf = np.zeros(md.shape, dtype=np.int64)
+    md_mask_csf[(md < csf_md_thr) & (md > gm_md_thr)] = 1
 
     fa_mask_csf = np.zeros(fa.shape, dtype=np.int64)
     fa_mask_csf[(fa < csf_fa_thr) & (fa > 0)] = 1
