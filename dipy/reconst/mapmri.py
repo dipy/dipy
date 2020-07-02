3e07a0ae307a5d79d62b4f81eb2d063cd08aeed9
Serge Koudoro <skab12@gmail.com>
2020-07-03 00:14:34 +0200

    fix multiply for cvxpy 1.1

--- dipy/reconst/mapmri.py
+++ dipy/reconst/mapmri.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from distutils.version import LooseVersion
 from dipy.reconst.multi_voxel import multi_voxel_fit
 from dipy.reconst.base import ReconstModel, ReconstFit
 from dipy.reconst.cache import Cache
@@ -397,20 +398,27 @@ class MapmriModel(ReconstModel, Cache):
 
             data_norm = np.asarray(data / data[self.gtab.b0s_mask].mean())
             c = cvxpy.Variable(M.shape[1])
-            design_matrix = cvxpy.Constant(M)
+            if LooseVersion(cvxpy.__version__) < LooseVersion('1.1'):
+                design_matrix = cvxpy.Constant(M) * c
+            else:
+                design_matrix = cvxpy.Constant(M) @ c
             # workaround for the bug on cvxpy 1.0.15 when lopt = 0
             # See https://github.com/cvxgrp/cvxpy/issues/672
             if not lopt:
                 objective = cvxpy.Minimize(
-                    cvxpy.sum_squares(design_matrix * c - data_norm))
+                    cvxpy.sum_squares(design_matrix - data_norm))
             else:
                 objective = cvxpy.Minimize(
-                    cvxpy.sum_squares(design_matrix * c - data_norm) +
+                    cvxpy.sum_squares(design_matrix - data_norm) +
                     lopt * cvxpy.quad_form(c, laplacian_matrix)
                 )
             M0 = M[self.gtab.b0s_mask, :]
-            constraints = [(M0[0] * c) == 1,
-                           (K * c) >= -0.1]
+            if LooseVersion(cvxpy.__version__) < LooseVersion('1.1'):
+                constraints = [(M0[0] * c) == 1,
+                               (K * c) >= -0.1]
+            else:
+                constraints = [(M0[0] @ c) == 1,
+                               (K @ c) >= -0.1]
             prob = cvxpy.Problem(objective, constraints)
             try:
                 prob.solve(solver=self.cvxpy_solver)
