f67ce820020a12e2c13efa722765e84b79283c16
Serge Koudoro <skab12@gmail.com>
2020-08-18 14:32:45 +0200

    fix pep8

--- doc/examples/tracking_bootstrap_peaks.py
+++ doc/examples/tracking_bootstrap_peaks.py
@@ -84,7 +84,8 @@ save_trk(sft, "tractogram_bootstrap_dg.trk")
 if has_fury:
     scene = window.Scene()
     scene.add(actor.line(streamlines, colormap.line_colors(streamlines)))
-    window.record(scene, out_path='tractogram_bootstrap_dg.png', size=(800, 800))
+    window.record(scene, out_path='tractogram_bootstrap_dg.png',
+                  size=(800, 800))
     if interactive:
         window.show(scene)
 
