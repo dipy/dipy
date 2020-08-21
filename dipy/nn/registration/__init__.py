6c8550c6fdbae129f921a829c3519b0f898b3206
sarathknv <sarathknv@gmail.com>
2020-08-21 21:50:49 +0530

    updated __init__.py

--- dipy/nn/registration/__init__.py
+++ dipy/nn/registration/__init__.py
@@ -1,2 +1,12 @@
-from dipy.nn.registration.unet import UNet2d
-from dipy.nn.registration.dataloader import RegistrationDataLoader
+from dipy.utils.optpkg import optional_package
+from distutils.version import LooseVersion
+
+tf, have_tf, _ = optional_package("tensorflow")
+layers, _, _ = optional_package("tensorflow.keras.layers")
+
+if have_tf:
+    if LooseVersion(tf.__version__) < LooseVersion('2.0.0'):
+        raise ImportError('Please upgrade to TensorFlow 2+')
+
+    from dipy.nn.registration.unet import UNet2d
+    from dipy.nn.registration.dataloader import RegistrationDataLoader
