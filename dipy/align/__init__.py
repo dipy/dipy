6d84c6461f4936aaaef4e396ecc410451fab666d
Ariel Rokem <arokem@gmail.com>
2020-09-23 09:20:38 -0700

    PEP8 in dipy/align/__init__.py

--- dipy/align/__init__.py
+++ dipy/align/__init__.py
@@ -2,6 +2,7 @@
 import numpy as np
 floating = np.float32
 
+
 class Bunch(object):
     def __init__(self, **kwds):
         r"""A 'bunch' of values (a replacement of Enum)
@@ -23,14 +24,14 @@ registration that can be used to detect a failing component.
 DEBUG : print as much information as possible to isolate the cause of a bug.
 """
 
-from dipy.align._public import (syn_registration, register_dwi_to_template,
+from dipy.align._public import (syn_registration, register_dwi_to_template, # noqa
                                 write_mapping, read_mapping, resample,
                                 center_of_mass, translation, rigid, affine,
                                 affine_registration, register_series,
                                 register_dwi_series, streamline_registration)
 
-__all__ =  ["syn_registration", "register_dwi_to_template",
-            "write_mapping", "read_mapping", "resample",
-            "center_of_mass", "translation", "rigid", "affine",
-            "affine_registration", "register_series",
-            "register_dwi_series", "streamline_registration"]
+__all__ = ["syn_registration", "register_dwi_to_template",
+           "write_mapping", "read_mapping", "resample",
+           "center_of_mass", "translation", "rigid", "affine",
+           "affine_registration", "register_series",
+           "register_dwi_series", "streamline_registration"]
