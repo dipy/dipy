b2251448bdb4b9a0715883f0dad64f360db942e8
Serge Koudoro <skab12@gmail.com>
2020-11-05 17:27:58 +0100

    1.4.0dev serie starting

--- dipy/info.py
+++ dipy/info.py
@@ -7,10 +7,10 @@ docs.  In setup.py in particular, we exec this file, so it cannot import dipy
 # full release.  '.dev' as a _version_extra string means this is a development
 # version
 _version_major = 1
-_version_minor = 3
+_version_minor = 4
 _version_micro = 0
-# _version_extra = 'dev'
-_version_extra = ''
+_version_extra = 'dev'
+# _version_extra = ''
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 __version__ = "%s.%s.%s%s" % (_version_major,
