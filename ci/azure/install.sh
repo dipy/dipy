4fe612ffecdb38355db4c42b98aeb749ae08f3b6
Serge Koudoro <skab12@gmail.com>
2021-01-27 18:51:44 +0100

    update azure for linux

--- ci/azure/install.sh
+++ ci/azure/install.sh
@@ -15,10 +15,10 @@ if [ "$INSTALL_TYPE" == "conda" ]; then
     conda create -n venv --yes python=$PYTHON_VERSION pip
     conda install -yq --name venv $DEPENDS $EXTRA_DEPENDS pytest
 else
-    PIPI="pip install --timeout=60 --find-links=$EXTRA_WHEELS"
+    PIPI="pip install --timeout=60 "
 
     if [ -n "$USE_PRE" ]; then
-        PIPI="$PIPI --find-links=$PRE_WHEELS --pre";
+        PIPI="$PIPI --extra-index-url=$PRE_WHEELS --pre";
     fi
 
     pip install --upgrade virtualenv
