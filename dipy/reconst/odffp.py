6b705c062c2758e223c233be1fd074ea63ba12b3
filipp02 <patryk.filipiak@nyulangone.org>
2021-03-04 16:48:22 -0500

    ODF rotation to max

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -10,6 +10,7 @@ from dipy.data import Sphere
 from dipy.reconst.gqi import GeneralizedQSamplingModel
 from dipy.reconst.dsi import DiffusionSpectrumModel
 
+from dipy.direction import peaks_from_model
 from scipy.io import loadmat
 
 
@@ -29,23 +30,37 @@ class OdffpModel(object):
 
     def __init__(self, gtab):
         self.gtab = gtab
+    
+    
+    def _rotation_matrix_two_vectors(A,B):
+        cAB = np.cross(A,B)
+        sAB = np.array([[0,-cAB[2],cAB[1]], [cAB[2],0,-cAB[0]], [-cAB[1],cAB[0],0]])
+        R = np.eye(3) + sAB + np.dot(sAB,sAB) * (1-np.dot(A,B)) / np.sum(cAB**2)
+    
+        if np.all(np.isnan(R)):
+            R = np.eye(3)
+
+        return R
+    
         
     def fit(self, data):
         tessellation = dsiSphere8Fold()
         
         diff_model = GeneralizedQSamplingModel(self.gtab)
-#         diff_model = DiffusionSpectrumModel(self.gtab)
-        diff_fit = diff_model.fit(data)
-        diff_odf = diff_fit.odf(tessellation)
-        
-        # This function can return ODFs, checkout at:
-        # https://dipy.org/documentation/1.1.1./reference/dipy.direction/#id23
         diff_peaks = peaks_from_model(
-            model=diff_model, data=data, sphere=tessellation, 
+            model=diff_model, data=data, sphere=tessellation, return_odf=True,
             relative_peak_threshold=.5, min_separation_angle=25, npeaks=1
         )
         
-        return diff_odf
+        for x in range(diff_peaks.peak_dirs.shape[0]):
+            for y in range(diff_peaks.peak_dirs.shape[1]):
+                for z in range(diff_peaks.peak_dirs.shape[2]):
+
+                    R = _rotation_matrix_two_vectors(
+                        diff_peaks.peak_dirs[x,y,z,0,:], [0,0,1]
+                    )
+        
+        return diff_peaks.odf
 
         
 class OdffpFit(object):
