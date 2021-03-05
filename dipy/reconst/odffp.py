8922ef325daf89b483e1a4eabb3a352483230279
filipp02 <patryk.filipiak@nyulangone.org>
2021-03-05 09:32:15 -0500

    Rotations to main peak

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -5,6 +5,7 @@ Created on Feb 18, 2021
 '''
 
 import os.path
+import numpy as np
 
 from dipy.data import Sphere
 from dipy.reconst.gqi import GeneralizedQSamplingModel
@@ -32,7 +33,7 @@ class OdffpModel(object):
         self.gtab = gtab
     
     
-    def _rotation_matrix_two_vectors(A,B):
+    def _get_rotation_of_vector(self, A, B):
         cAB = np.cross(A,B)
         sAB = np.array([[0,-cAB[2],cAB[1]], [cAB[2],0,-cAB[0]], [-cAB[1],cAB[0],0]])
         R = np.eye(3) + sAB + np.dot(sAB,sAB) * (1-np.dot(A,B)) / np.sum(cAB**2)
@@ -42,25 +43,33 @@ class OdffpModel(object):
 
         return R
     
+    
+    def _get_rotations_to_main_peak(self, peak_dirs):
+        rotations = np.zeros(peak_dirs.shape + (3,3))
+        
+        for x in range(peak_dirs.shape[0]):
+            for y in range(peak_dirs.shape[1]):
+                for z in range(peak_dirs.shape[2]):
+
+                    rotations[x,y,z,:,:] = self._get_rotation_of_vector(
+                        peak_dirs[x,y,z,0,:], [0,0,1]
+                    )
+        
+        return rotations
+        
         
     def fit(self, data):
         tessellation = dsiSphere8Fold()
         
         diff_model = GeneralizedQSamplingModel(self.gtab)
-        diff_peaks = peaks_from_model(
+        data_peaks = peaks_from_model(
             model=diff_model, data=data, sphere=tessellation, return_odf=True,
             relative_peak_threshold=.5, min_separation_angle=25, npeaks=1
         )
         
-        for x in range(diff_peaks.peak_dirs.shape[0]):
-            for y in range(diff_peaks.peak_dirs.shape[1]):
-                for z in range(diff_peaks.peak_dirs.shape[2]):
-
-                    R = _rotation_matrix_two_vectors(
-                        diff_peaks.peak_dirs[x,y,z,0,:], [0,0,1]
-                    )
-        
-        return diff_peaks.odf
+        self._rotations = self._get_rotations_to_main_peak(data_peaks.peak_dirs)
+                
+        return data_peaks.odf
 
         
 class OdffpFit(object):
