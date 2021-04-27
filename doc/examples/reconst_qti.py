564b4657f45fa4cfdb49ab20e20fba2e9f242d80
kerkelae <leevi.kerkela@protonmail.com>
2021-04-27 19:31:49 +0100

    DOC - started the tutorial for QTI

---
+++ doc/examples/reconst_qti.py
@@ -0,0 +1,14 @@
+"""
+=================================================
+Reconstruct with Q-space Trajectory Imaging (QTI)
+=================================================
+
+This tutorial shows how to estimate the diffusion tensor and covariance tensor
+using q-space trajectory imaging by Westin et al. [1]_.
+
+References
+----------
+.. [1] Westin, Carl-Fredrik, et al. "Q-space trajectory imaging for
+   multidimensional diffusion MRI of the human brain." Neuroimage 135
+   (2016): 345-362. https://doi.org/10.1016/j.neuroimage.2016.02.039.
+"""
