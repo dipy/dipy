e82eb366a849a31e0ae5b996fc8b54e161a603e4
filipp02 <patryk.filipiak@nyulangone.org>
2021-04-28 15:20:49 -0400

    Class _DsiSphere8Fold is a singleton now.

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -17,14 +17,33 @@ from dipy.direction import peak_directions
 from dipy.reconst.shm import sf_to_sh, sh_to_sf
 from scipy.io import loadmat
 
+
+class _DsiSphere8Fold(Sphere):
+    _instance = None
+
+    @staticmethod 
+    def get_instance():
+        if _DsiSphere8Fold._instance == None:
+            _DsiSphere8Fold()
+        return _DsiSphere8Fold._instance
+    
+    
+    def __init__(self):
+        if _DsiSphere8Fold._instance != None:
+            raise Exception("The class _DsiSphere8Fold is a singleton. Call dsiSphere8Fold() function instead.")
+        else:
+            dsi_sphere = loadmat(os.path.join(
+                os.path.dirname(__file__), "../data/files/dsi_sphere_8fold.mat"
+            ))
+            Sphere.__init__(self,
+                xyz=dsi_sphere['odf_vertices'].T,
+                faces=dsi_sphere['odf_faces'].T
+            )
+            _DsiSphere8Fold._instance = self
+
+
 def dsiSphere8Fold():
-    dsi_sphere = loadmat(os.path.join(
-        os.path.dirname(__file__), "../data/files/dsi_sphere_8fold.mat"
-    ))
-    return Sphere(
-        xyz=dsi_sphere['odf_vertices'].T,
-        faces=dsi_sphere['odf_faces'].T
-    )
+    return _DsiSphere8Fold.get_instance()
 
 
 class OdffpModel(object):
