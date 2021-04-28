1a00bb13f93a152838b3f09b1c009306f70f52e5
filipp02 <patryk.filipiak@nyulangone.org>
2021-04-28 17:33:46 -0400

    Peak dirs rotation

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -12,10 +12,13 @@ from dipy.data import Sphere, HemiSphere
 from dipy.reconst.gqi import GeneralizedQSamplingModel
 from dipy.reconst.dsi import DiffusionSpectrumModel
 
+from dipy.core.geometry import sphere2cart
 from dipy.core.ndindex import ndindex
 from dipy.direction import peak_directions
 from dipy.reconst.shm import sf_to_sh, sh_to_sf
+
 from scipy.io import loadmat
+from scipy.spatial.transform import rotation
 
 
 class _DsiSphere8Fold(Sphere):
@@ -48,11 +51,20 @@ def dsiSphere8Fold():
 
 class OdffpModel(object):
  
+    @staticmethod 
+    def resample_odf(odf, in_sphere, out_sphere):
+        return sh_to_sf(
+            sf_to_sh(odf, in_sphere), # sh_order=14, basis_type='tournier07') 
+            out_sphere
+        ) #, sh_order=14, basis_type='tournier07')
+     
+
     def __init__(self, gtab, dict_file, drop_odf_amplitude=True):
         self.gtab = gtab
          
         with h5py.File(dict_file, 'r') as mat_file:
             self._dict_odf = np.array(mat_file['odfrot'])    
+            self._dict_peak_dirs = np.array(mat_file['dirrot'])
          
         self._drop_odf_amplitude = drop_odf_amplitude
         self._dict_odf = self._normalize_odf(self._dict_odf)
@@ -65,58 +77,77 @@ class OdffpModel(object):
         return odf / np.maximum(1e-8, np.sqrt(np.sum(odf**2, axis=0)))
 
      
-    def _rotate_tessellation(self, tessellation, in_direction, out_direction=[0,0,1]):
+    def _get_rotation(self, in_direction, out_direction=[0,0,1]):
         cAB = np.cross(in_direction, out_direction)
         sAB = np.array([[0,-cAB[2],cAB[1]], [cAB[2],0,-cAB[0]], [-cAB[1],cAB[0],0]])
         rotation = np.eye(3) + sAB + np.dot(sAB,sAB) * (1-np.dot(in_direction,out_direction)) / np.sum(cAB**2)
      
         if np.all(np.isnan(rotation)):
             rotation = np.eye(3)
- 
+
+        return rotation
+     
+     
+    def _rotate_tessellation(self, tessellation, rotation):
         return Sphere(
             xyz=np.dot(tessellation.vertices, rotation),
             faces=tessellation.faces
         )
+        
+    def _rotate_peak_dirs(self, peak_dirs, rotation):
+        return np.dot(
+            np.array(sphere2cart(1, np.pi/2 + peak_dirs[1,:], peak_dirs[0,:])).T, 
+            rotation
+        )
  
- 
-    def _rotate_odf(self, odf, in_tessellation, out_tessellation):
-        sh = sf_to_sh(odf, in_tessellation) #, sh_order=14, basis_type='tournier07')
-        sf = sh_to_sf(sh, out_tessellation) #, sh_order=14, basis_type='tournier07')
-        return sf
-     
-     
+   
     def fit(self, data):
         diff_model = GeneralizedQSamplingModel(self.gtab)
  
         data_shape = data.shape[:-1]
+        max_peaks_num = self._dict_peak_dirs.shape[1]
+
         tessellation = dsiSphere8Fold()
         tessellation_size = len(tessellation.vertices)
  
         self._rotations = np.zeros(data_shape + (3,3))
         output_odf = np.zeros(data_shape + (tessellation_size,))
+        output_peak_dirs = np.zeros(data_shape + (max_peaks_num,3))
  
         for idx in ndindex(data_shape):
             model_fit = diff_model.fit(data[idx])
              
             input_odf = model_fit.odf(tessellation)
-            peak_dirs,_,_ = peak_directions(input_odf, tessellation)
+            input_peak_dirs,_,_ = peak_directions(input_odf, tessellation)
  
-            rotated_tessellation = self._rotate_tessellation(tessellation, np.squeeze(peak_dirs[:1]))
-            rotated_odf = self._rotate_odf(input_odf, tessellation, rotated_tessellation)
+            rotation = self._get_rotation(np.squeeze(input_peak_dirs[:1]))
+            rotated_tessellation = self._rotate_tessellation(tessellation, rotation)
+            rotated_input_odf = OdffpModel.resample_odf(input_odf, tessellation, rotated_tessellation)
              
-            odf_trace = self._normalize_odf(rotated_odf[:int(tessellation_size/2)])
+            input_odf_trace = self._normalize_odf(rotated_input_odf[:int(tessellation_size/2)])
              
-            dict_idx = np.argmax(np.dot(odf_trace, self._dict_odf))
+            dict_idx = np.argmax(np.dot(input_odf_trace, self._dict_odf))
             
-            output_odf[idx] = self._rotate_odf(
+            output_odf[idx] = OdffpModel.resample_odf(
                 np.concatenate((self._dict_odf[:,dict_idx],self._dict_odf[:,dict_idx])), 
                 rotated_tessellation, tessellation
             )
+            
+            output_peak_dirs[idx] = self._rotate_peak_dirs(self._dict_peak_dirs[:,:,dict_idx], rotation)
  
-        return output_odf
+        return OdffpFit(output_odf, output_peak_dirs)
 
         
 class OdffpFit(object):
     
-    def __init__(self):
-        pass
+    def __init__(self, odf, peak_dirs):
+        self._odf = odf
+        self._peak_dirs = peak_dirs
+        
+    def odf(self):
+        return self._odf
+    
+    def peak_dirs(self):
+        return self._peak_dirs
+    
+    
