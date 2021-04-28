09e1fc77a02de1cac2d844da97d86a7e2f8dd9a7
kerkelae <leevi.kerkela@protonmail.com>
2021-04-28 13:03:28 +0100

    DOC - QTI tutorial first version

--- doc/examples/reconst_qti.py
+++ doc/examples/reconst_qti.py
@@ -3,9 +3,165 @@
 Reconstruct with Q-space Trajectory Imaging (QTI)
 =================================================
 
-This tutorial shows how to estimate the diffusion tensor and covariance tensor
-using q-space trajectory imaging by Westin et al. [1]_.
+Q-space trajectory imaging (QTI) by Westin et al. [1]_ is a general framework
+for analyzing diffusion-weighted MRI data acquired with tensor-valued diffusion
+encoding. This tutorial briefly summarizes the theory and provides an example of
+how to estimate the diffusion and covariance tensors using Dipy.
 
+Theory
+======
+
+QTI is based on a microstructural model, the diffusion tensor distribution
+(DTD), which represents the microscopic diffusion environments in the voxel.
+Here, DTD is denoted by $\mathbf{D}$ and the voxel-level diffusion tensor from
+DTI by $\langle\mathbf{D}\rangle$, where $\langle \ \rangle$ denotes averaging
+over the DTD. The covariance of $\mathbf{D}$ is given by a fourth-order
+covariance tensor $\mathbb{C}$ defined as
+
+.. math::
+
+   \mathbb{C} = \langle \mathbf{D} \otimes \mathbf{D} \rangle - \langle
+   \mathbf{D} \rangle \otimes \langle \mathbf{D} \rangle ,
+
+where $\otimes$ denotes a tensor outer product. $\mathbb{C}$ has 21 unique
+elements and enables the calculation of several microstructural parameters.
+
+Using the cumulant expansion, the diffusion-weighted signal can be approximated
+as
+
+.. math::
+   S \approx S_0 \exp \left(- \mathbf{b} : \langle \mathbf{D} \rangle +
+   \frac{1}{2}(\mathbf{b} \otimes \mathbf{b}) : \mathbb{C} \right) ,
+
+where $S_0$ is the signal without diffusion-weighting, $\mathbf{b}$ is the
+b-tensor used in the acquisition, and $:$ denotes a tensor inner product.
+
+The model parameters $S_0$, $\langle\mathbf{D}\rangle$, and $\mathbb{C}$ can be
+estimated by solving the following equation:
+
+.. math::
+   
+   S = \beta X,
+
+where
+
+.. math::
+
+   S = \begin{pmatrix} \ln S_1 \\ \vdots \\ \ln S_n \end{pmatrix} ,
+
+.. math::
+
+   \beta = \begin{pmatrix} \ln S_0 & \langle \mathbf{D} \rangle & \mathbb{C}
+   \end{pmatrix}^\text{T} ,
+
+.. math::
+
+   X = 
+   \begin{pmatrix}
+   1 & -\mathbf{b}_1^\text{T} & \frac{1}{2} (\mathbf{b}_1 \otimes \mathbf{b}_1)
+   \text{T} \\ 
+   \vdots & \vdots & \vdots \\ 
+   1 & -\mathbf{b}_n^\text{T} & \frac{1}{2} (\mathbf{b}_n \otimes \mathbf{b}_n)
+   ^\text{T}
+   \end{pmatrix} ,
+
+where $n$ is the number of acquisitions and $\langle\mathbf{D}\rangle$,
+$\mathbb{C}$, $\mathbf{b}_i$, and $(\mathbf{b}_i \otimes \mathbf{b}_i)$ are
+represented by column vectors using Voigt notation. Estimation of the model
+parameters requires that $\text{rank}(\mathbf{X}^\text{T}\mathbf{X}) = 28$.
+This can be achieved by combining acquisitions with b-tensors with different
+shapes, sizes, and orientations.
+
+For details, please see [1]_.
+
+Usage example
+=============
+
+QTI can be fit to data using the module `dipy.reconst.qti`, which we import as
+follows:
+"""
+import dipy.reconst.qti as qti
+"""
+As QTI requires data with tensor-valued encoding, let's load an example dataset
+acquired with q-space trajectory encoding (QTE):
+"""
+from dipy.data import read_qte_lte_pte
+
+img, mask_img, gtab = read_qte_lte_pte()  # Data is fetched just once
+data = img.get_fdata()  # Diffusion-weighted data
+mask = mask_img.get_fdata()  # Brain mask
+"""
+The dataset contains 122 volumes of which the first half were acquired with
+linear tensor encoding (LTE) and the second half with planar tensor encoding
+(PTE). We can confirm this by calculating the ranks of the b-tensors in the
+gradient table.
+"""
+import numpy as np
+
+ranks = np.array([np.linalg.matrix_rank(b) for b in gtab.btens])
+for i, l in enumerate(['b = 0', 'LTE', 'PTE']):
+    print('%s volumes with %s' % (np.sum(ranks == i), l))
+"""
+Now that we have data acquired with tensor-valued diffusion encoding and the
+corresponding gradient table containing the b-tensors, we can fit QTI to the
+data as follows:
+"""
+qtimodel = qti.QtiModel(gtab)
+qtifit = qtimodel.fit(data, mask)
+"""
+QTI parameter maps can accessed as the attributes of `qtifit`. For instance:
+"""
+fa = qtifit.fa  # Conventional fractional anisotropy from DTI
+ufa = qtifit.ufa  # Microscopic fractional anisotropy
+"""
+Finally, let's visualize the QTI parameter maps by reproducing Figure 9 from
+[1]_:
+"""
+import matplotlib.pyplot as plt
+
+z = 36
+
+fig, ax = plt.subplots(3, 4, figsize=(12, 9))
+
+background = np.zeros(data.shape[0:2])  # Black background for figures
+for i in range(3):
+    for j in range(4):
+        ax[i, j].imshow(background, cmap='gray')
+        ax[i, j].set_xticks([])
+        ax[i, j].set_yticks([])
+
+ax[0, 0].imshow(np.rot90(qtifit.md[:, :, z]), cmap='gray', vmin=0, vmax=3e-3)
+ax[0, 0].set_title('MD')
+ax[0, 1].imshow(np.rot90(qtifit.v_md[:, :, z]), cmap='gray', vmin=0, vmax=.5e-6)
+ax[0, 1].set_title('V$_{MD}$')
+ax[0, 2].imshow(np.rot90(qtifit.v_shear[:, :, z]), cmap='gray', vmin=0,
+                vmax=.5e-6)
+ax[0, 2].set_title('V$_{shear}$')
+ax[0, 3].imshow(np.rot90(qtifit.v_iso[:, :, z]), cmap='gray', vmin=0, vmax=1e-6)
+ax[0, 3].set_title('V$_{iso}$')
+
+ax[1, 0].imshow(np.rot90(qtifit.c_md[:, :, z]), cmap='gray', vmin=0, vmax=.25)
+ax[1, 0].set_title('C$_{MD}$')
+ax[1, 1].imshow(np.rot90(qtifit.c_mu[:, :, z]), cmap='gray', vmin=0, vmax=1)
+ax[1, 1].set_title('C$_{μ}$ = μFA$^2$')
+ax[1, 2].imshow(np.rot90(qtifit.c_m[:, :, z]), cmap='gray', vmin=0, vmax=1)
+ax[1, 2].set_title('C$_{M}$ = FA$^2$')
+ax[1, 3].imshow(np.rot90(qtifit.c_c[:, :, z]), cmap='gray', vmin=0, vmax=1)
+ax[1, 3].set_title('C$_{c}$')
+
+ax[2, 0].imshow(np.rot90(qtifit.mk[:, :, z]), cmap='gray', vmin=0, vmax=1.5)
+ax[2, 0].set_title('MK')
+ax[2, 1].imshow(np.rot90(qtifit.k_bulk[:, :, z]), cmap='gray', vmin=0, vmax=1.5)
+ax[2, 1].set_title('K$_{bulk}$')
+ax[2, 2].imshow(np.rot90(qtifit.k_shear[:, :, z]), cmap='gray', vmin=0,
+                vmax=1.5)
+ax[2, 2].set_title('K$_{shear}$')
+ax[2, 3].imshow(np.rot90(qtifit.k_mu[:, :, z]), cmap='gray', vmin=0, vmax=1.5)
+ax[2, 3].set_title('K$_{μ}$')
+
+fig.tight_layout()
+plt.show()
+"""
 References
 ----------
 .. [1] Westin, Carl-Fredrik, et al. "Q-space trajectory imaging for
