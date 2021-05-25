cd1bce3fc75440c77ec3d414023f821d93e97413
filipp02 <patryk.filipiak@nyulangone.org>
2021-05-25 18:35:16 -0400

    Index maps implemented.

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -68,32 +68,35 @@ class OdffpModel(object):
         """ODF-Fingerprinting reconstruction"""
         
         self.gtab = gtab
+
+        self.drop_negative_odf = drop_negative_odf 
+        self.zero_baseline_odf = zero_baseline_odf
+        self.output_dict_odf = output_dict_odf
+        self.max_chunk_size = np.maximum(1, max_chunk_size)
          
         with h5py.File(dict_file, 'r') as mat_file:
-            self._dict_odf = np.array(mat_file['odfrot']) 
+            self._dict = {}
+            
+            # Load ODF fingerprints
+            self._dict['odf'] = np.array(mat_file['odfrot'])
+            self._dict['odf_trace'],_ = self._normalize_odf(self._dict['odf'])
             
             # Load only non-empty fibers from the dictionary file   
             peak_dirs = np.array(mat_file['dirrot'])
             self._max_peaks_num = np.sum(np.any(~np.isnan(peak_dirs[0]),axis=1))
-            self._dict_peak_dirs = peak_dirs[:,:self._max_peaks_num]
-            self._dict_micro = np.array(mat_file['micro'])[:,:self._max_peaks_num+1]
-            self._dict_ratio = np.array(mat_file['rat'])[:,:self._max_peaks_num+1]
+            self._dict['peak_dirs'] = peak_dirs[:,:self._max_peaks_num]
+            self._dict['micro'] = np.array(mat_file['micro'])[:,:self._max_peaks_num+1]
+            self._dict['ratio'] = np.array(mat_file['rat'])[:self._max_peaks_num+1]
         
 #             self._dict_model = np.ravel(mat_file['libopt']['microstruct']) # TODO: convert to str
             
-        self._drop_negative_odf = drop_negative_odf 
-        self._zero_baseline_odf = zero_baseline_odf
-        self._output_dict_odf = output_dict_odf
-        self._max_chunk_size = np.maximum(1, max_chunk_size)
-        
-        self._normalized_dict_odf,_ = self._normalize_odf(self._dict_odf)
 
      
     def _normalize_odf(self, odf):
-        if self._drop_negative_odf:
+        if self.drop_negative_odf:
             odf = np.maximum(0, odf)
         
-        if self._zero_baseline_odf:
+        if self.zero_baseline_odf:
             odf -= np.min(odf, axis=0)
             
         odf_norm = np.maximum(1e-8, np.sqrt(np.sum(odf**2, axis=0)))
@@ -155,7 +158,7 @@ class OdffpModel(object):
         output_odf = np.zeros((voxels_num, tessellation_size))
         output_peak_dirs = np.zeros((voxels_num, self._max_peaks_num, 3))
         
-        for chunk_idx in np.split(range(voxels_num), range(self._max_chunk_size, voxels_num, self._max_chunk_size)):
+        for chunk_idx in np.split(range(voxels_num), range(self.max_chunk_size, voxels_num, self.max_chunk_size)):
         
             print("%.1f%%" % (100 * (np.max(chunk_idx) + 1) / voxels_num))
         
@@ -177,37 +180,37 @@ class OdffpModel(object):
             
                 input_odf_trace[i], input_odf_norm[i] = self._normalize_odf(rotated_input_odf[i][:int(tessellation_size/2)])
             
-            dict_idx[chunk_idx] = np.argmax(np.dot(input_odf_trace, self._normalized_dict_odf), axis=1)
+            dict_idx[chunk_idx] = np.argmax(np.dot(input_odf_trace, self._dict['odf_trace']), axis=1)
         
             for i, j in zip(range(chunk_size), chunk_idx):
-                if self._output_dict_odf:
+                if self.output_dict_odf:
                     output_odf[j] = OdffpModel.resample_odf(
-                        input_odf_norm[i] * np.concatenate((self._dict_odf[:,dict_idx[j]], self._dict_odf[:,dict_idx[j]])), 
+                        input_odf_norm[i] * np.concatenate((self._dict['odf'][:,dict_idx[j]], self._dict['odf'][:,dict_idx[j]])), 
                         rotated_tessellation[i], tessellation
                     )
                 else:
                     output_odf[j] = input_odf[i]
                   
-                output_peak_dirs[j] = self._rotate_peak_dirs(self._dict_peak_dirs[:,:,dict_idx[j]], rotation[i])
+                output_peak_dirs[j] = self._rotate_peak_dirs(self._dict['peak_dirs'][:,:,dict_idx[j]], rotation[i])
    
         return OdffpFit(
             data, tessellation, 
             self._unmask(output_odf, mask), 
             self._unmask(output_peak_dirs, mask),
             self._unmask(dict_idx, mask),
-            self._dict_micro
+            self._dict
         )
             
         
 class OdffpFit(OdfFit):
     
-    def __init__(self, data, tessellation, odf, peak_dirs, dict_idx, dict_micro):
+    def __init__(self, data, tessellation, odf, peak_dirs, dict_idx, odf_dict):
         self._data = data
         self._tessellation = tessellation
         self._odf = odf
         self._peak_dirs = peak_dirs
         self._dict_idx = dict_idx
-        self._dict_micro = dict_micro
+        self._dict = odf_dict
     
         
     def odf(self, sphere=None):
@@ -230,8 +233,8 @@ class OdffpFit(OdfFit):
         return matrix.reshape(new_size, order='F')
 
 
-    def _fib_index_map(self, var_data, dict_idx, voxels_num, slice_size, fa_filter):
-        index_map = np.zeros(voxels_num)
+    def _fib_index_map(self, var_data, dict_idx, slice_size, fa_filter):
+        index_map = np.zeros(dict_idx.shape)
         index_map[fa_filter] = var_data[dict_idx[fa_filter]]
         return self._fib_reshape(index_map, slice_size)
 
@@ -289,37 +292,23 @@ class OdffpFit(OdfFit):
             fib['fa%d' % i] -= np.min(fib['fa%d' % i])
             fib['nqa%d' % i] = fib['fa%d' % i] / np.maximum(1e-8, np.max(fib['fa%d' % i]))
 
-        fib['odf0'] = output_odf[fib['fa0'] > 0,:tessellation_half_size].T
+        fa_filter = fib['fa0'] > 0
+        fib['odf0'] = output_odf[fa_filter,:tessellation_half_size].T
         fib['odf0'] /= np.maximum(1e-8, np.max(fib['odf0']))
 
         # microstructure parameters
-        fa_filter = fib['fa0'] > 0
-        fib['D_iso'] = self._fib_index_map(self._dict_micro[1,0], dict_idx, voxels_num, slice_size, fa_filter)
+        fib['D_iso'] = self._fib_index_map(self._dict['micro'][1,0], dict_idx, slice_size, fa_filter)
+        fib['p_iso'] = self._fib_index_map(self._dict['ratio'][0], dict_idx, slice_size, fa_filter)
 
         for i in range(1, max_peaks_num+1):
-            fib['fib%d_Da' % i] = self._fib_index_map(self._dict_micro[0,i], dict_idx, voxels_num, slice_size, fa_filter)
-            fib['fib%d_De||' % i] = self._fib_index_map(self._dict_micro[1,i], dict_idx, voxels_num, slice_size, fa_filter)
-            fib['fib%d_De_|_' % i] = self._fib_index_map(self._dict_micro[2,i], dict_idx, voxels_num, slice_size, fa_filter)
+            index_maps = {
+                'fib%d_Da' % i: self._dict['micro'][0,i], 'fib%d_De||' % i: self._dict['micro'][1,i],
+                'fib%d_De_|_' % i: self._dict['micro'][2,i], 'fib%d_fin' % i: self._dict['micro'][3,i],
+                'fib%d_p' % i: self._dict['ratio'][i]
+            }
+            for index_name in index_maps:
+                fib[index_name] = self._fib_index_map(index_maps[index_name], dict_idx, slice_size, fa_filter)
 
-#         fib['D_iso'] = np.zeros(voxels_num)
-#         fib['D_iso'][fa_filter] = self._dict_micro[1,0,dict_idx[fa_filter]]
-#         
-#         for i in range(max_peaks_num):
-#             fib['Da_par%d' % i] = np.zeros(voxels_num)
-#             fib['Da_par%d' % i][fa_filter] = self._dict_micro[0,i,dict_idx[fa_filter]]
-#             fib['Da_par%d' % i] = self._fib_reshape(fib['Da_par%d' % i], slice_size)
-#             fib['De_par%d' % i] = np.zeros(voxels_num)
-#             fib['De_par%d' % i][fa_filter] = self._dict_micro[1,i,dict_idx[fa_filter]]
-#             fib['De_perp%d' % i] = np.zeros(voxels_num)
-#             fib['De_perp%d' % i][fa_filter] = self._dict_micro[2,i,dict_idx[fa_filter]]
-            
-#         fib['p_iso'] = np.zeros(voxels_num)
-        
- 
-        fib['D_iso'] = self._fib_reshape(fib['D_iso'], slice_size)
-#         fib['p_iso'] = self._fib_reshape(fib['p_iso'], slice_size)
-         
-        # reshape to the output sizes
         for i in range(max_peaks_num):
             fib['fa%d' % i] = self._fib_reshape(fib['fa%d' % i], slice_size)
             fib['nqa%d' % i] = self._fib_reshape(fib['nqa%d' % i], slice_size)
