105c3c72e91720b53b901df5be7ca183e6ef9586
dombas <davidrb093g@gmail.com>
2021-08-02 12:09:27 +0200

    TEST: extend coverage of bundle atlasing

--- dipy/atlasing/tests/test_bundles.py
+++ dipy/atlasing/tests/test_bundles.py
@@ -1,26 +1,21 @@
-import os
+from os import mkdir, listdir
+from os.path import isfile, join, isdir
 import zipfile
+import pytest
 import numpy as np
 from numpy.testing import assert_equal, assert_raises, run_module_suite
-import pandas as pd
-from tempfile import TemporaryDirectory
-
+from nibabel.tmpdirs import TemporaryDirectory
 from dipy.atlasing.bundles import (combine_bundles, compute_atlas_bundle,
                                    get_pairwise_tree)
 from dipy.data import get_fnames
-from dipy.io.streamline import load_tractogram
+from dipy.io.streamline import (create_tractogram_header, load_tractogram,
+                                save_trk, Space, StatefulTractogram)
 from dipy.tracking.streamline import (relist_streamlines,
                                       select_random_set_of_streamlines,
                                       unlist_streamlines)
+from dipy.utils.optpkg import optional_package
 
-
-def setup_module():
-    global in_dir
-    # Load toy data
-    in_dir = TemporaryDirectory()
-    example_tracts = get_fnames('minimal_bundles')
-    with zipfile.ZipFile(example_tracts, 'r') as zip_ref:
-        zip_ref.extractall(in_dir.name)
+pd, have_pandas, _ = optional_package("pandas")
 
 
 def test_pairwise_tree():
@@ -47,118 +42,175 @@ def test_pairwise_tree():
 
 
 def test_combine_bundle():
-    # Prepare a pair of bundles with 30 and 50 streamlines
-    bundle_obj1 = load_tractogram(f'{in_dir.name}/sub_1/AF_L.trk',
-                                  reference='same', bbox_valid_check=False)
-    points, offsets = unlist_streamlines(bundle_obj1.streamlines)
-    bundle1 = relist_streamlines(points, offsets)
-    n_stream1 = 30
-    bundle1 = select_random_set_of_streamlines(bundle1, n_stream1)
+    with TemporaryDirectory() as in_dir:
+        # Extract data
+        example_tracts = get_fnames('minimal_bundles')
+        with zipfile.ZipFile(example_tracts, 'r') as zip_ref:
+            zip_ref.extractall(in_dir)
 
-    bundle_obj2 = load_tractogram(f'{in_dir.name}/sub_2/AF_L.trk',
-                                  reference='same', bbox_valid_check=False)
-    points, offsets = unlist_streamlines(bundle_obj2.streamlines)
-    bundle2 = relist_streamlines(points, offsets)
+        # Prepare a pair of bundles with 30 and 50 streamlines
+        bundle_obj1 = load_tractogram(f'{in_dir}/sub_1/AF_L.trk',
+                                      reference='same', bbox_valid_check=False)
+        points, offsets = unlist_streamlines(bundle_obj1.streamlines)
+        bundle1 = relist_streamlines(points, offsets)
+        n_stream1 = 30
+        bundle1 = select_random_set_of_streamlines(bundle1, n_stream1)
 
-    # Test incorrect parameters
-    assert_raises(ValueError, combine_bundles, bundle1, bundle2,
-                  distance='imaginary_distance')
-    assert_raises(ValueError, combine_bundles, bundle1, bundle2,
-                  comb_method='imaginary_method')
+        bundle_obj2 = load_tractogram(f'{in_dir}/sub_2/AF_L.trk',
+                                      reference='same', bbox_valid_check=False)
+        points, offsets = unlist_streamlines(bundle_obj2.streamlines)
+        bundle2 = relist_streamlines(points, offsets)
 
-    # Test expected streamline number and correct averaging
-    for dist in ['mdf', 'mdf_se']:
-        # Test RLAP
-        combined = combine_bundles(bundle1, bundle2, 'rlap', dist)
-        assert_equal(len(combined), 30)
-        combined = combine_bundles(bundle2, bundle1, 'rlap', dist)
-        assert_equal(len(combined), 30)
-        combined = combine_bundles(bundle1, bundle1, 'rlap', dist)
-        assert_equal(combined, bundle1)
-        # Test RLAP + keep
-        combined = combine_bundles(bundle1, bundle2, 'rlap_keep', dist)
-        assert_equal(len(combined), 50)
-        combined = combine_bundles(bundle2, bundle1, 'rlap_keep', dist)
-        assert_equal(len(combined), 50)
-        combined = combine_bundles(bundle1, bundle1, 'rlap_keep', dist)
-        assert_equal(combined, bundle1)
-        # Test RLAP + closest
-        combined = combine_bundles(bundle1, bundle2, 'rlap_closest', dist)
-        assert_equal(len(combined), 50)
-        combined = combine_bundles(bundle2, bundle1, 'rlap_closest', dist)
-        assert_equal(len(combined), 50)
-        combined = combine_bundles(bundle1, bundle1, 'rlap_keep', dist)
-        assert_equal(combined, bundle1)
-        # Test random pick
-        combined = combine_bundles(bundle1, bundle2, 'random_pick', dist,
-                                   n_stream=33)
-        assert_equal(len(combined), 33)
-        combined = combine_bundles(bundle2, bundle1, 'random_pick', dist,
-                                   n_stream=33)
-        assert_equal(len(combined), 33)
+        # Test incorrect parameters
+        assert_raises(ValueError, combine_bundles, bundle1, bundle2,
+                      distance='imaginary_distance')
+        assert_raises(ValueError, combine_bundles, bundle1, bundle2,
+                      comb_method='imaginary_method')
+
+        # Test expected streamline number and correct averaging
+        for dist in ['mdf', 'mdf_se']:
+            # Test RLAP
+            combined = combine_bundles(bundle1, bundle2, 'rlap', dist)
+            assert_equal(len(combined), 30)
+            combined = combine_bundles(bundle2, bundle1, 'rlap', dist)
+            assert_equal(len(combined), 30)
+            combined = combine_bundles(bundle1, bundle1, 'rlap', dist)
+            assert_equal(combined, bundle1)
+            # Test RLAP + keep
+            combined = combine_bundles(bundle1, bundle2, 'rlap_keep', dist)
+            assert_equal(len(combined), 50)
+            combined = combine_bundles(bundle2, bundle1, 'rlap_keep', dist)
+            assert_equal(len(combined), 50)
+            combined = combine_bundles(bundle1, bundle1, 'rlap_keep', dist)
+            assert_equal(combined, bundle1)
+            # Test RLAP + closest
+            combined = combine_bundles(bundle1, bundle2, 'rlap_closest', dist)
+            assert_equal(len(combined), 50)
+            combined = combine_bundles(bundle2, bundle1, 'rlap_closest', dist)
+            assert_equal(len(combined), 50)
+            combined = combine_bundles(bundle1, bundle1, 'rlap_keep', dist)
+            assert_equal(combined, bundle1)
+            # Test random pick
+            combined = combine_bundles(bundle1, bundle2, 'random_pick', dist,
+                                       n_stream=33)
+            assert_equal(len(combined), 33)
+            combined = combine_bundles(bundle2, bundle1, 'random_pick', dist,
+                                       n_stream=33)
+            assert_equal(len(combined), 33)
 
 
+@pytest.mark.skipif(not have_pandas, reason='Requires Pandas')
 def test_compute_atlas_bundle():
-    # Test argument errors
-    assert_raises(TypeError, compute_atlas_bundle, in_dir=2)
-    assert_raises(TypeError, compute_atlas_bundle, in_dir=in_dir.name,
-                  mid_path=2)
-    assert_raises(TypeError, compute_atlas_bundle, in_dir=in_dir.name,
-                  n_point=2.78)
-    assert_raises(ValueError, compute_atlas_bundle, in_dir='imaginary_folder')
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  out_dir='imaginary_folder')
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  n_stream_max=0)
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  n_stream_min=0)
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  n_point=0)
+    # Prepare input test data
+    with TemporaryDirectory() as in_dir:
+        example_tracts = get_fnames('minimal_bundles')
+        with zipfile.ZipFile(example_tracts, 'r') as zip_ref:
+            zip_ref.extractall(in_dir)
 
-    # Test wrong mid_path
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  mid_path='fake_path')
+        # Test argument errors
+        assert_raises(TypeError, compute_atlas_bundle, in_dir=2)
+        assert_raises(TypeError, compute_atlas_bundle, in_dir=in_dir,
+                      mid_path=3.14)
+        assert_raises(TypeError, compute_atlas_bundle, in_dir=in_dir,
+                      n_point=2.78)
+        assert_raises(ValueError, compute_atlas_bundle, in_dir='fake_folder')
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      out_dir='fake_folder')
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      n_stream_max=0)
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      n_stream_min=0)
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      n_point=0)
 
-    # Test bundle duplications
-    df = pd.DataFrame(['AF_L', 'AF_L'], columns=['bundle'])
-    fname = os.path.join(in_dir.name, 'bundles.tsv')
-    df.to_csv(fname, sep='\t', index=False)
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  bundle_names=fname)
+        # Test wrong mid_path
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      mid_path='fake_path')
 
-    # Test subject duplications
-    df = pd.DataFrame({'participant': ['sub_1', 'sub_2', 'sub_2', 'sub_4']})
-    fname = os.path.join(in_dir.name, 'participants.tsv')
-    df.to_csv(fname, sep='\t', index=False)
-    assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir.name,
-                  subjects=fname)
+        # Test bundle duplications
+        df = pd.DataFrame(['AF_L', 'AF_L'], columns=['bundle'])
+        fname = join(in_dir, 'bundles.tsv')
+        df.to_csv(fname, sep='\t', index=False)
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      bundle_names=fname)
 
-    # Test full functionality
-    out_dir = TemporaryDirectory()
-    atlas, atlas_merged = compute_atlas_bundle(in_dir=in_dir.name,
-                                               out_dir=out_dir.name,
-                                               save_temp=False,
-                                               merge_out=True)
-    assert_equal(len(atlas), 3)
-    assert_equal(len(atlas_merged), len(np.concatenate(atlas)))
+        # Test bundle names and model bundle list mismatch
+        df = pd.DataFrame(['AF_L', 'AF_R'], columns=['bundle'])
+        fname = join(in_dir, 'bundles.tsv')
+        df.to_csv(fname, sep='\t', index=False)
+        model_bundle_dir = f'{in_dir}/sub_1'  # Use first subject as model
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      bundle_names=fname, model_bundle_dir=model_bundle_dir)
 
-    assert_equal(os.path.isfile(f'{out_dir.name}/AF_L.trk'), True)
-    assert_equal(os.path.isfile(f'{out_dir.name}/CST_R.trk'), True)
-    assert_equal(os.path.isfile(f'{out_dir.name}/CC_ForcepsMajor.trk'), True)
-    assert_equal(os.path.isfile(f'{out_dir.name}/whole_brain.trk'), True)
+        # Test subject duplications
+        df = pd.DataFrame(
+            {'participant': ['sub_1', 'sub_2', 'sub_2', 'sub_4']})
+        fname = join(in_dir, 'participants.tsv')
+        df.to_csv(fname, sep='\t', index=False)
+        assert_raises(ValueError, compute_atlas_bundle, in_dir=in_dir,
+                      subjects=fname)
 
-    # Test specifying subject group + temporary files
-    out_dir = TemporaryDirectory()
-    df = pd.DataFrame({'participant': ['sub_1', 'sub_2', 'sub_3', 'sub_4'],
-                       'group': ['control', 'patient', 'control', 'control']})
-    fname = os.path.join(in_dir.name, 'participants.tsv')
-    df.to_csv(fname, sep='\t', index=False)
-    _ = compute_atlas_bundle(in_dir=in_dir.name, subjects=fname,
-                             group='control', out_dir=out_dir.name,
-                             save_temp=True)
-    files = os.listdir(f'{out_dir.name}/temp/AF_L/step_0')
-    trk_files = [file for file in files if file.endswith('.trk')]
-    assert_equal(len(trk_files), 3)
+        # Test default functionality
+        with TemporaryDirectory() as out_dir:
+            atlas = compute_atlas_bundle(in_dir=in_dir, out_dir=out_dir)
+
+            assert_equal(len(atlas), 3)
+            assert_equal(isfile(f'{out_dir}/AF_L.trk'), True)
+            assert_equal(isfile(f'{out_dir}/CST_R.trk'), True)
+            assert_equal(isfile(f'{out_dir}/CC_ForcepsMajor.trk'), True)
+            assert_equal(isdir(f'{out_dir}/temp'), False)
+
+        # Test specific functionalities
+
+        # Subjects and bundles provided as tsv with different groups
+        subjects = ['sub_1', 'sub_2', 'sub_3', 'sub_4', 'sub_5']
+        group = ['control', 'patient', 'control', 'control', 'control']
+        df = pd.DataFrame({'participant': subjects, 'group': group})
+        subjects = join(in_dir, 'participants.tsv')
+        df.to_csv(subjects, sep='\t', index=False)
+
+        df = pd.DataFrame(['AF_L', 'CC_ForcepsMajor'],
+                          columns=['bundle'])
+        bundle_names = join(in_dir, 'bundles.tsv')
+        df.to_csv(bundle_names, sep='\t', index=False)
+
+        # One bundle with only 4 streamlines (to be discarded)
+        file = f'{in_dir}/sub_1/AF_L.trk'
+        bundle_obj = load_tractogram(file, reference='same',
+                                     bbox_valid_check=False)
+        header = create_tractogram_header(file, *bundle_obj.space_attributes)
+        bundle = select_random_set_of_streamlines(bundle_obj.streamlines, 4)
+        new_tractogram = StatefulTractogram(bundle, reference=header,
+                                            space=Space.RASMM)
+        save_trk(new_tractogram, file, bbox_valid_check=False)
+
+        with TemporaryDirectory() as out_dir:
+            # temp folder to be removed by the function
+            mkdir(f'{out_dir}/temp')
+
+            atlas, atlas_merged = \
+                compute_atlas_bundle(in_dir=in_dir, subjects=subjects,
+                                     group='control', out_dir=out_dir,
+                                     bundle_names=bundle_names,
+                                     model_bundle_dir=model_bundle_dir,
+                                     save_temp=True, merge_out=True,
+                                     skip_pairs=True, n_stream_min=5,
+                                     n_stream_max=10)
+
+            assert_equal(len(atlas), 2)
+            assert_equal(len(atlas_merged), len(np.concatenate(atlas)))
+            assert_equal(isfile(f'{out_dir}/AF_L.trk'), True)
+            assert_equal(isfile(f'{out_dir}/CC_ForcepsMajor.trk'), True)
+            assert_equal(isfile(f'{out_dir}/whole_brain.trk'), True)
+
+            assert_equal(isdir(f'{out_dir}/temp'), True)
+
+            temp_files = listdir(f'{out_dir}/temp/AF_L/step_0')
+            trk_files = [file for file in temp_files if file.endswith('.trk')]
+            png_files = [file for file in temp_files if file.endswith('.png')]
+
+            assert_equal(len(trk_files), 3)  # 4 controls - 1 discarded
+            assert_equal(len(png_files), 3)
 
 
 if __name__ == '__main__':
---
+++ dipy/workflows/tests/test_atlasing.py
@@ -0,0 +1,39 @@
+import zipfile
+from os import listdir
+from os.path import join, isfile
+from dipy.utils.optpkg import optional_package
+import pytest
+from numpy.testing import assert_equal
+from nibabel.tmpdirs import TemporaryDirectory
+# from dipy.testing import assert_true
+from dipy.data import get_fnames
+from dipy.workflows.atlasing import DiscreteBundleAtlasFlow
+
+_, have_pandas, _ = optional_package("pandas")
+
+
+@pytest.mark.skipif(not have_pandas, reason='Requires Pandas')
+def test_discrete_bundle_atlas():
+
+    with TemporaryDirectory() as in_dir:
+        example_tracts = get_fnames('minimal_bundles')
+        with zipfile.ZipFile(example_tracts, 'r') as zip_ref:
+            zip_ref.extractall(in_dir)
+
+        with TemporaryDirectory() as out_dir:
+            flow = DiscreteBundleAtlasFlow()
+            flow.run(in_dir, out_dir=out_dir, save_temp=True, merge_out=True)
+
+            # Check that final output was generated
+            assert_equal(isfile(join(out_dir, 'AF_L.trk')), True)
+            assert_equal(isfile(join(out_dir, 'CST_R.trk')), True)
+            assert_equal(isfile(join(out_dir, 'CC_ForcepsMajor.trk')), True)
+            assert_equal(isfile(join(out_dir, 'whole_brain.trk')), True)
+
+            # Check that temp output was generated
+            temp_files = listdir(join(out_dir, 'temp', 'AF_L', 'step_0'))
+            trk_files = [file for file in temp_files if file.endswith('.trk')]
+            png_files = [file for file in temp_files if file.endswith('.png')]
+
+            assert_equal(len(trk_files), 5)
+            assert_equal(len(png_files), 5)
