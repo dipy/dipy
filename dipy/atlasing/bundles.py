f7aec932b6dd9b76841524f224c40a314a2033ed
dombas <davidrb093g@gmail.com>
2021-08-02 12:09:55 +0200

    STYLE: improve readability of bundle atlasing

--- dipy/atlasing/bundles.py
+++ dipy/atlasing/bundles.py
@@ -10,15 +10,15 @@ Available functions:
     atlas.
 """
 
-import os
-import shutil
+from os import listdir, mkdir, makedirs, getcwd
+from os.path import join, isdir, splitext
+from shutil import rmtree, copyfile
 import numpy as np
 import pandas as pd
 from scipy.optimize import linear_sum_assignment
 from time import sleep
 from dipy.align.bundlemin import distance_matrix_mdf
 from dipy.align.streamlinear import slr_with_qbx
-from dipy.data import fetch_bundle_atlas_hcp842, get_bundle_atlas_hcp842
 from dipy.io.streamline import (create_tractogram_header, load_tractogram,
                                 save_trk, Space, StatefulTractogram)
 from dipy.tracking.streamline import (orient_by_streamline, relist_streamlines,
@@ -70,9 +70,9 @@ def get_pairwise_tree(n_item, seed=None):
         index = np.arange(n_item)
         # Compute the number of registration pairs
         n_reg.append(np.floor(n_item/2).astype('int'))
-        # Shuffle the bundles
+        # Shuffle the items
         index = np.random.permutation(n_item)
-        # If n_bundles is odd duplicate one of the others
+        # If n_item is odd duplicate one of the others
         if np.mod(n_item, 2) == 1:
             # avoid removing again an item twice (index == 0)
             lonely = np.random.randint(1, n_item)
@@ -82,7 +82,7 @@ def get_pairwise_tree(n_item, seed=None):
             alone.append(None)
         # Generate pairwise registration matrix
         index = np.reshape(index, (n_reg[-1], 2))
-        # Update bundle number
+        # Update item number
         n_item = np.ceil(n_item/2).astype('int')
         # Save matched pairs
         matched_pairs.append(index)
@@ -109,7 +109,8 @@ def combine_bundles(bundle1, bundle2, comb_method='rlap', distance='mdf',
         Method to be used to combine the two bundles. Default is 'rlap_keep'.
     distance : str, optional
         Distance used for streamline matching. Used by all methods except for
-        'random_pick'.  Default is 'mdf'.
+        'random_pick'.  Default is 'mdf'. The 'mdf_se' distance uses only the
+        start and end points.
     n_stream : int, optional
         Number of streamlines to be selected when comb_method='random_pick'.
         Default is 2000.
@@ -137,7 +138,7 @@ def combine_bundles(bundle1, bundle2, comb_method='rlap', distance='mdf',
     else:
         raise ValueError("Incorrect distance metric")
 
-    # Set as bundle 1 the one with less streamlines
+    # Set as bundle 1 the bundle with less streamlines
     if len(bundle2) < len(bundle1):
         aux = bundle1.copy()
         bundle1 = bundle2
@@ -151,7 +152,7 @@ def combine_bundles(bundle1, bundle2, comb_method='rlap', distance='mdf',
     if comb_method == 'rlap':
         # Minimize the sum of distances (RLAP)
         matched_pairs = np.asarray(linear_sum_assignment(cost)).T
-
+        # For each matched pair, reorient and average
         for ind1, ind2 in matched_pairs:
 
             stream1 = bundle1[ind1]
@@ -172,26 +173,23 @@ def combine_bundles(bundle1, bundle2, comb_method='rlap', distance='mdf',
         ind_lap1, ind_lap2 = linear_sum_assignment(cost)
 
         for ind2 in range(n_stream):
-            # Check if streamline already matched by LAP
+            # Check if streamline already matched by RLAP
             aux = np.argwhere(ind_lap2 == ind2)
             if aux.size > 0:
                 ind1 = ind_lap1[aux[0][0]]
+            # If not, find the closest streamline
             else:
-                # Find the closest streamline and save it
                 ind1 = np.argmin(cost[:, ind2])
 
-            # Get matched streamline pair
+            # Get matched streamline pair, reorient and average
             stream1 = bundle1[ind1]
             stream2 = bundle2[ind2]
 
-            # Reorient streamlines in the right order
             stream2 = orient_by_streamline([stream2], stream1)
             stream2, _ = unlist_streamlines(stream2)
 
-            # Combine matched streamlines and generate the atlas
             stream_mean = np.mean([stream1, stream2], axis=0)
 
-            # Store streamline
             combined.append(stream_mean)
 
     elif comb_method == 'rlap_keep':
@@ -202,19 +200,17 @@ def combine_bundles(bundle1, bundle2, comb_method='rlap', distance='mdf',
         ind_lap1, ind_lap2 = linear_sum_assignment(cost)
 
         for ind2 in range(n_stream):
-            # Check if streamline already matched by LAP
+            # If streamline already matched by RLAP, average them
             aux = np.argwhere(ind_lap2 == ind2)
-            # If matched compute mean
             if aux.size > 0:
                 ind1 = ind_lap1[aux[0][0]]
-                # Get matched streamline pair
+
                 stream1 = bundle1[ind1]
                 stream2 = bundle2[ind2]
-                # Reorient streamlines in the right order
+
                 stream2 = orient_by_streamline([stream2], stream1)
                 stream2, _ = unlist_streamlines(stream2)
 
-                # Combine matched streamlines and generate the atlas
                 combined.append(np.mean([stream1, stream2], axis=0))
 
             # If not matched keep it as it is
@@ -279,7 +275,7 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
                          bundle_names=None, model_bundle_dir=None,
                          out_dir=None, merge_out=False, save_temp=False,
                          n_stream_min=10, n_stream_max=5000, n_point=20,
-                         distance='mdf_se', comb_method='rlap',
+                         distance='mdf', comb_method='rlap',
                          skip_pairs=False):
     """Compute a population specific bundle atlas.
 
@@ -304,18 +300,17 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
         None, all trk files of the first subject will be considered as
         bundle_names.
     model_bundle_dir : str, optional
-        Directory with reference bundles to be used as a reference to move all
-        bundles to a standard space. If 'HCP-842' the HCP-842 atlas bundles are
-        downloaded and used. If None, bundles are assumed to be in the same
-        space and no registration is performed.
+        Directory with model bundles to be used as a reference to move all
+        bundles to a common space. If None, bundles are assumed to be in the
+        same space and no registration is performed.
     out_dir : str, optional
-        Output directory. If None, the current directory is used.
+        Output directory. If None, the current working directory is used.
     merge_out : boolean, optional
         If True the resulting atlases of all bundles are combined into a single
         file. Default is False.
     save_temp : boolean, optional
         If True the intermediate results of each tree level are saved in a temp
-        folder. Default is False.
+        folder in trk and png formats. Default is False.
     n_stream_min : int, optional
         Bundles with less than ``n_stream_min`` streamlines wont be processed.
         Default is 10.
@@ -325,9 +320,10 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
     n_point : int, optional
         All streamlines are set to have ``n_point`` points. Default is 20.
     distance : str, optional
-        Distance metric to be used to combine bundles. Default is 'mdf_se'.
+        Distance metric to be used to combine bundles. Default is 'mdf'. The
+        'mdf_se' distance uses only start/end points of streamlines.
     comb_method : str, optional
-        Method to be used to combine each bundle pair. Default is 'rlap'.
+        Method used to combine each bundle pair. Default is 'rlap'.
     skip_pairs : boolean, optional
         If true bundle combination steps are randomly skipped. This helps to
         obtain a sharper result. Default is False.
@@ -345,11 +341,11 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
         raise TypeError("mid_path must be a string")
     if type(n_point) != int:
         raise TypeError("n_point must be an int")
-    if os.path.isdir(in_dir) is False:
+    if isdir(in_dir) is False:
         raise ValueError("Input directory does not exist")
     if out_dir is None:
-        out_dir = os.getcwd()
-    if os.path.isdir(out_dir) is False:
+        out_dir = getcwd()
+    if isdir(out_dir) is False:
         raise ValueError("Output directory does not exist")
     if n_stream_min < 1:
         raise ValueError("n_stream_min must be >= 1")
@@ -361,13 +357,18 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
     print('Input directory:' + in_dir)
     print('Output directory:' + out_dir)
 
-    # Get subjects
+    # Create temporary folder
+    temp_dir = join(out_dir, 'temp')
+    if isdir(temp_dir):
+        print("There is already a /temp folder in out_dir. Deleting.")
+        rmtree(temp_dir)
+    mkdir(temp_dir)
+
+    # Get subjects (from in_dir or a BIDS-like parciticipants.tsv file)
     if subjects is None:
-        files = os.listdir(in_dir)
-        subjects = [file for file in files if os.path.isdir(
-                    os.path.join(in_dir, file))]
+        files = listdir(in_dir)
+        subjects = [file for file in files if isdir(join(in_dir, file))]
     else:
-        # Read participants.tsv file (BIDS format)
         df = pd.read_csv(subjects, delimiter='\t', dtype='object')
         if group is None:
             subjects = list(df.participant)
@@ -376,70 +377,58 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
     subjects.sort()  # necessary?
     if len(set(subjects)) < len(subjects):
         raise ValueError("Subjects cannot be duplicated")
+
     print(str(len(subjects)) + " subjects to be processed:")
     print(subjects)
 
-    # Get bundle names
+    # Get bundle names (from first subject folder or from tsv file)
     if bundle_names is None:
-        bundle_dir = os.path.join(in_dir, subjects[0], mid_path)
+        bundle_dir = join(in_dir, subjects[0], mid_path)
         print("Retrieving bundle names from " + bundle_dir)
-        if os.path.isdir(bundle_dir) is False:
+        if isdir(bundle_dir) is False:
             raise ValueError("Path to subject bundles is incorrect")
 
-        files = os.listdir(bundle_dir)
-        my_files = [file for file in files if file.endswith('.trk')]
-        bundle_names = [os.path.splitext(file)[0] for file in my_files]
+        files = listdir(bundle_dir)
+        trk_files = [file for file in files if file.endswith('.trk')]
+        bundle_names = [splitext(file)[0] for file in trk_files]
     else:
-        # Read bundles.tsv file (first column)
         df = pd.read_csv(bundle_names, delimiter='\t', dtype='object')
         bundle_names = list(df.iloc[:, 0])
     bundle_names.sort()  # necessary?
     if len(set(bundle_names)) < len(bundle_names):
         raise ValueError("Bundle names cannot be duplicated")
+
     print(str(len(bundle_names)) + " bundles to be processed:")
     print(bundle_names)
 
-    # Get bundle file list
+    # Create a dictionary with all bundle files
     bundle_files = {}
     for sub in subjects:
         for bundle in bundle_names:
-            file = os.path.join(in_dir, sub, mid_path, bundle + '.trk')
+            file = join(in_dir, sub, mid_path, bundle + '.trk')
             bundle_files[(sub, bundle)] = file
 
     # Get model bundle list
     if model_bundle_dir is None:
         model_bundles = None
     else:
-        if model_bundle_dir == 'HCP-842':
-            _, _ = fetch_bundle_atlas_hcp842()
-            _, all_bundles_files = get_bundle_atlas_hcp842()
-            model_bundle_dir = os.path.split(all_bundles_files)[0]
-
-        files = os.listdir(model_bundle_dir)
-        my_files = [file for file in files if file.endswith('.trk')]
-        model_bundle_list = [os.path.splitext(file)[0] for file in my_files]
+        files = listdir(model_bundle_dir)
+        trk_files = [file for file in files if file.endswith('.trk')]
+        model_bundle_list = [splitext(file)[0] for file in trk_files]
 
         if not all(x in model_bundle_list for x in bundle_names):
             raise ValueError("Not all the specified bundles have a model")
         model_bundles = {}
         for bundle in bundle_names:
-            model_bundles[bundle] = os.path.join(
-                model_bundle_dir, bundle + '.trk')
-
-    # Create temporary folder
-    temp_dir = os.path.join(out_dir, 'temp/')
-    if os.path.exists(temp_dir):
-        print("There is already a temp folder in out_dir. Deleting.")
-        shutil.rmtree(temp_dir)
-        os.mkdir(temp_dir)
+            model_bundles[bundle] = join(model_bundle_dir, bundle + '.trk')
 
     # Atlas building starts
     atlas = []
     for bundle in bundle_names:
         print("Processing bundle: " + bundle)
 
-        step_dir = os.path.join(temp_dir, bundle, 'step_0')
-        os.makedirs(step_dir)
+        step_dir = join(temp_dir, bundle, 'step_0')
+        makedirs(step_dir)
 
         # Load model bundle if required
         if model_bundles is not None:
@@ -450,7 +439,7 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
             header_model \
                 = create_tractogram_header(file, *bundle_obj.space_attributes)
 
-        # Preprocess all bundles
+        # Preprocess all bundles and save them as trk
         file_list = []
         for i, sub in enumerate(subjects):
             file = bundle_files[(sub, bundle)]
@@ -465,8 +454,8 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
                 print(f"{file} has {n_stream} streamlines. Discarded.")
                 continue
             elif n_stream > n_stream_max:
-                streamlines = select_random_set_of_streamlines(
-                    streamlines, n_stream_max)
+                streamlines = select_random_set_of_streamlines(streamlines,
+                                                               n_stream_max)
 
             streamlines = set_number_of_points(streamlines, n_point)
 
@@ -483,33 +472,35 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
             new_tractogram = StatefulTractogram(streamlines, reference=header,
                                                 space=Space.RASMM)
             save_trk(new_tractogram, f'{file}.trk', bbox_valid_check=False)
-            show_bundles([streamlines], f'{file}.png')
+            if save_temp:
+                show_bundles([streamlines], f'{file}.png')
 
         print("Bundle preprocessing: ok.")
 
         # Compute pairwise registration tree-structure
         tree, alone, n_reg = get_pairwise_tree(n_item=len(file_list))
 
-        # Go through all tree levels
+        # Go through all tree steps
         for i_step, pairs in enumerate(tree):
             new_file_list = list()
 
             # Create step folder
-            new_step_dir = os.path.join(
-                temp_dir, bundle, 'step_' + str(i_step+1))
-            os.mkdir(new_step_dir)
+            step_dir = join(temp_dir, bundle, 'step_' + str(i_step+1))
+            mkdir(step_dir)
 
             # A lonely bundle goes to the next level
             has_lonely = 0
             if alone[i_step] is not None:
                 has_lonely = 1
 
-                file_new = f'{new_step_dir}/bundle_0_prev_{alone[i_step]}'
+                file_prev = file_list[alone[i_step]]
+                file_new = f'{step_dir}/bundle_0_prev_{alone[i_step]}'
                 new_file_list.append(file_new)
-                shutil.copyfile(f'{file_list[alone[i_step]]}.trk',
-                                f'{file_new}.trk')
+                copyfile(f'{file_prev}.trk', f'{file_new}.trk')
+                if save_temp:
+                    copyfile(f'{file_prev}.png', f'{file_new}.png')
 
-            # Loop through all registration pairs
+            # Register and combine each pair of bundles
             for index, pair in enumerate(pairs):
                 i = pair[0]
                 j = pair[1]
@@ -539,7 +530,7 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
                 points, offsets = unlist_streamlines(moving)
                 moving = relist_streamlines(points, offsets)
 
-                # Combine bundles
+                # Randomly skip steps if speciffied to get a sharper results
                 if skip_pairs and np.random.choice([True, False], 1)[0]:
                     combined = combine_bundles(static, aligned, 'random_pick',
                                                distance)
@@ -547,25 +538,24 @@ def compute_atlas_bundle(in_dir, subjects=None, group=None, mid_path='',
                     combined = combine_bundles(static, aligned, comb_method,
                                                distance)
 
-                file = f'{new_step_dir}/bundle_{index+has_lonely}_prev_{i}_{j}'
+                file = f'{step_dir}/bundle_{index+has_lonely}_prev_{i}_{j}'
                 new_file_list.append(file)
                 new_tractogram = StatefulTractogram(Streamlines(combined),
                                                     reference=header,
                                                     space=Space.RASMM)
                 save_trk(new_tractogram, file + ".trk", bbox_valid_check=False)
-                show_bundles([static, moving, aligned], f'{file}_reg.png',
-                             colors=[(0, 0, 1), (1, 0, 0), (0, 1, 0)])
-                show_bundles([Streamlines(combined)], f'{file}.png')
+                if save_temp:
+                    show_bundles([static, moving, aligned], f'{file}_reg.png',
+                                 colors=[(0, 0, 1), (1, 0, 0), (0, 1, 0)])
+                    show_bundles([Streamlines(combined)], f'{file}.png')
 
-            step_dir = new_step_dir
             file_list = new_file_list
-
-        shutil.copyfile(file + '.trk', out_dir + '/' + bundle + '.trk')
-
+        save_trk(new_tractogram, f'{out_dir}/{bundle}.trk',
+                 bbox_valid_check=False)
         atlas.append(Streamlines(combined))
 
     if not save_temp:
-        shutil.rmtree(temp_dir)
+        rmtree(temp_dir)
 
     if merge_out:
         atlas_merged = np.concatenate(atlas)
