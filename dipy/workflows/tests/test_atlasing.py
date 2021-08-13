6e52726c96f2ee8977d4aa5eeb0a31310e285de3
dombas <davidrb093g@gmail.com>
2021-08-13 09:58:38 +0200

    BF correct workflow launcher

--- dipy/workflows/tests/test_atlasing.py
+++ dipy/workflows/tests/test_atlasing.py
@@ -1,12 +1,11 @@
-import zipfile
 from os import listdir
 from os.path import join, isfile
-from dipy.utils.optpkg import optional_package
 import pytest
+import zipfile
 from numpy.testing import assert_equal
 from nibabel.tmpdirs import TemporaryDirectory
-# from dipy.testing import assert_true
 from dipy.data import get_fnames
+from dipy.utils.optpkg import optional_package
 from dipy.workflows.atlasing import DiscreteBundleAtlasFlow
 
 _, has_fury, _ = optional_package('fury')
@@ -20,17 +19,25 @@ def test_discrete_bundle_atlas():
         with zipfile.ZipFile(example_tracts, 'r') as zip_ref:
             zip_ref.extractall(in_dir)
 
+        flow = DiscreteBundleAtlasFlow()
+
+        # Check that final output was generated with default version
         with TemporaryDirectory() as out_dir:
-            flow = DiscreteBundleAtlasFlow()
+            flow.run(in_dir, out_dir=out_dir)
+
+            assert_equal(isfile(join(out_dir, 'AF_L.trk')), True)
+            assert_equal(isfile(join(out_dir, 'CST_R.trk')), True)
+            assert_equal(isfile(join(out_dir, 'CC_ForcepsMajor.trk')), True)
+
+        # Check additional outputs (whole brain tractogram and temporary files)
+        with TemporaryDirectory() as out_dir:
             flow.run(in_dir, out_dir=out_dir, save_temp=True, merge_out=True)
 
-            # Check that final output was generated
             assert_equal(isfile(join(out_dir, 'AF_L.trk')), True)
             assert_equal(isfile(join(out_dir, 'CST_R.trk')), True)
             assert_equal(isfile(join(out_dir, 'CC_ForcepsMajor.trk')), True)
             assert_equal(isfile(join(out_dir, 'whole_brain.trk')), True)
 
-            # Check that temp output was generated
             temp_files = listdir(join(out_dir, 'temp', 'AF_L', 'step_0'))
             trk_files = [file for file in temp_files if file.endswith('.trk')]
             png_files = [file for file in temp_files if file.endswith('.png')]
