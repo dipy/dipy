def227170a8c158bc9935cb09d02a1841d9c5da0
Kenji Marshall <kenji.marshall99@gmail.com>
2021-09-22 23:02:51 -0400

    Update examples and add multishell example

---
+++ doc/examples/reconst_rumba_multishell.py
@@ -0,0 +1,166 @@
+"""
+========================================
+Multi-Shell Reconstruction with RUMBA-SD
+========================================
+
+This example shows how to use Robust and Unbiased Model-BAsed Spherical
+Deconvolution (RUMBA-SD) with multi-shell data. This model was introduced
+by Canales-Rodriguez et al [CanalesRodriguez2015]_. For a general introduction
+to the method using single-shell data, see :ref:`example_reconst_rumba`.
+
+For multi-shell data, a modified white matter response function can be used
+with shell-specific diffusivities.
+"""
+
+import numpy as np
+
+from dipy.core.gradients import gradient_table
+from dipy.io.gradients import read_bvals_bvecs
+from dipy.io.image import load_nifti
+from dipy.segment.mask import median_otsu
+from dipy.reconst.rumba import RumbaSD, global_fit
+from dipy.reconst.mcsd import auto_response_msmt
+from dipy.viz import window, actor
+
+from dipy.data import get_sphere, get_fnames
+sphere = get_sphere('symmetric724')
+
+"""
+We begin by loading multi-shell data provided by Hansen and Jespersen
+[Hansen2016]_. From this dataset, we extract only two non-zero b-values.
+"""
+
+fraw, fbval, fbvec, t1_fname = get_fnames('cfin_multib')
+
+data, affine = load_nifti(fraw)
+bvals, bvecs = read_bvals_bvecs(fbval, fbvec)
+
+sel_b = np.logical_or(np.logical_or(bvals == 0, bvals == 1000), bvals == 2000)
+data = data[..., sel_b]
+
+gtab = gradient_table(bvals[sel_b], bvecs[sel_b])
+
+"""
+To compute the response function, we first need to find segment the data into
+white matter, grey matter, and cerebrospinal fluid voxels. This is done using
+`mask_for_response_msmt`, which applies thresholds on mean diffusivity and
+fractional anisotropy to make this distinction. An optimal fit only uses
+b-values less than 1200, so a warning will be generated.
+
+Then, `response_from_mask_msmt` computes the multi-shell response function
+using these masks. These functions can be combined using `auto_response_msmt`,
+shown below.
+"""
+
+wm_response, gm_response, csf_response = \
+    auto_response_msmt(gtab, data, roi_radii=10)
+
+"""
+This white matter response can now be used in RUMBA-SD as follows. Note that
+RUMBA-SD only requires two values per shell (the axial and perpendicular
+diffusivity). There is no way to pass a multi-shell GM or CSF response.
+"""
+
+rumba = RumbaSD(gtab, wm_response=wm_response[:, :-2])
+
+"""
+Now we can compute our fODFs. For efficiency, we won't use total variation
+regularization. We will first fit a mask using `median_otsu`. The fit will
+take about 10 minutes.
+"""
+
+b0_mask, mask = median_otsu(data, median_radius=2, numpass=1, vol_idx=[0, 1])
+odf, f_gm, f_csf, f_iso, f_wm, combined = global_fit(rumba, data, sphere,
+                                                     mask=mask, use_tv=False)
+
+"""
+We can now visualize these fODFs
+"""
+
+fodf_spheres = actor.odf_slicer(combined, sphere=sphere, scale=0.9,
+                                norm=True, colormap=None)
+
+interactive = True
+scene = window.Scene()
+scene.add(fodf_spheres)
+scene.reset_camera_tight()
+
+print('Saving illustration as rumba_multishell_odf.png')
+window.record(scene, out_path='rumba_multishell_odf.png', size=(600, 600))
+
+if interactive:
+    window.show(scene)
+
+"""
+.. figure:: rumba_multishell_odf.png
+   :align: center
+
+   RUMBA-SD multi-shell fODFs
+"""
+
+scene.rm(fodf_spheres)
+
+"""
+We can also visualize the peaks computed from these fODFs.
+"""
+
+from dipy.direction.peaks import peak_directions
+shape = odf.shape[:3]
+npeaks = 5  # maximum number of peaks returned for a given voxel
+peak_dirs = np.zeros((shape + (npeaks, 3)))
+peak_values = np.zeros((shape + (npeaks,)))
+
+for idx in np.ndindex(shape):  # iterate through each voxel
+    # Get peaks of odf
+    direction, pk, _ = peak_directions(odf[idx], sphere,
+                                       relative_peak_threshold=0.5,
+                                       min_separation_angle=25)
+
+    # Calculate peak metrics
+    if pk.shape[0] != 0:
+        n = min(npeaks, pk.shape[0])
+        peak_dirs[idx][:n] = direction[:n]
+        peak_values[idx][:n] = pk[:n]
+
+peak_values = np.clip(peak_values * 25, 0, 1)  # scale up for visualization
+
+fodf_peaks = actor.peak_slicer(
+    peak_dirs[:, :, 0:1, :], peak_values[:, :, 0:1, :])
+scene.add(fodf_peaks)
+
+print('Saving illustration as rumba_multishell_peaks.png')
+window.record(scene, out_path='rumba_multishell_peaks.png', size=(600, 600))
+if interactive:
+    window.show(scene)
+
+
+"""
+.. figure:: rumba_multishell_peaks.png
+   :align: center
+
+   RUMBA-SD multishell peaks
+"""
+
+scene.rm(fodf_peaks)
+
+
+"""
+References
+----------
+
+.. [CanalesRodriguez2015] Canales-Rodríguez, E. J., Daducci, A., Sotiropoulos,
+   S. N., Caruyer, E., Aja-Fernández, S., Radua, J., Mendizabal, J. M. Y.,
+   Iturria-Medina, Y., Melie-García, L., Alemán-Gómez, Y., Thiran, J.-P.,
+   Sarró, S., Pomarol-Clotet, E., & Salvador, R. (2015). Spherical
+   Deconvolution of Multichannel Diffusion MRI Data with Non-Gaussian Noise
+   Models and Spatial Regularization. PLOS ONE, 10(10), e0138910.
+   https://doi.org/10.1371/journal.pone.0138910
+
+.. [Hansen2016] Hansen, B., & Jespersen, S. N. (2016). Data for evaluation of
+   fast kurtosis strategies, b-value optimization and exploration
+   of diffusion MRI contrast. Scientific Data, 3(1), 160072.
+   https://doi.org/10.1038/sdata.2016.72
+
+.. include:: ../links_names.inc
+
+"""
