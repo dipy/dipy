34506d1d0fadf7c7b8173c86013c1102af100127
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2022-01-21 13:54:24 -0500

    Changing reference to fury.shaders and removing direct vtk calls

--- dipy/viz/__init__.py
+++ dipy/viz/__init__.py
@@ -12,26 +12,17 @@ fury, has_fury, _ = optional_package(
 
 
 if has_fury:
-    from fury import actor, window, colormap, interactor, ui, utils
-    from fury.window import vtk
+    from fury import actor, window, colormap, interactor, ui, utils, shaders
+    from fury import lib
     from fury.data import (fetch_viz_icons, read_viz_icons,
                            DATA_DIR as FURY_DATA_DIR)
-
-    HAVE_VTK_9_PLUS = vtk.vtkVersion.GetVTKMajorVersion() >= 9
-    HAVE_FURY_0_6_LESS = LooseVersion(fury.__version__) < LooseVersion('0.6.0')
-    if HAVE_VTK_9_PLUS and HAVE_FURY_0_6_LESS:
-        msg = "You are using VTK 9+ and FURY {}. ".format(fury.__version__)
-        msg += "Some functions might not work. To have a better experience, "
-        msg += "please upgrade to FURY version 0.6.0+. "
-        msg += "pip install -U fury"
-        warnings.warn(msg)
-
 
 else:
     warnings.warn(
         "You do not have FURY installed. "
-        "Some visualization functions might not work for you. "
-        "For installation instructions, please visit: https://fury.gl/")
+        "Therefore, 3D visualization functions will not work for you. "
+        "Please install or upgrade FURY using pip install -U fury"
+        "For detailed installation instructions, please visit: https://fury.gl/")
 
 # We make the visualization requirements optional imports:
 _, has_mpl, _ = optional_package(
