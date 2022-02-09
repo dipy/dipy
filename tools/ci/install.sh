2184ac4b9cd1d396466f6a63444e50fc3158cc48
Serge Koudoro <skab12@gmail.com>
2022-02-09 19:20:49 +0100

    update ci script

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ -e venv/bin/activate ]; then
-    source venv/bin/activate
-elif [ -e venv/Scripts/activate ]; then
-    source virtenv/Scripts/activate
-elif [ "$INSTALL_TYPE" == "conda" ]; then
-    conda init bash
-    source $CONDA/etc/profile.d/conda.sh
-    conda activate venv
-else
-    echo Cannot activate virtual environment
-    ls -R venv
-    false
-fi
+source activate.sh
 
 PIPI="pip install --timeout=60"
 
--- tools/ci/install_dependencies.sh
+++ tools/ci/install_dependencies.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -ev
 
-if [ "$INSTALL_TYPE" == "conda" ]; then
+echo "Activate virtual environment"
+source tools/ci/activate.sh
 
-    conda config --set always_yes yes --set changeps1 no
-    conda update -yq conda
-    conda install conda-build anaconda-client
-    conda config --add channels conda-forge
-    conda create -n venv --yes python=$PYTHON_VERSION pip
+echo "Display Python version"
+python -c "import sys; print(sys.version)"
+python -m pip install -U pip setuptools>=30.3.0 wheel
+
+
+echo "Install Dependencies"
+if [ "$INSTALL_TYPE" == "conda" ]; then
     conda install -yq --name venv $DEPENDS $EXTRA_DEPENDS pytest
 else
     PIPI="pip install --timeout=60 "
@@ -16,22 +19,10 @@ else
         PIPI="$PIPI --extra-index-url=$PRE_WHEELS --pre";
     fi
 
-    pip install --upgrade virtualenv
-    virtualenv $VENV_ARGS venv
-    source venv/bin/activate
-    # just to check python version
-    python --version
-
     $PIPI pytest
     $PIPI numpy
     if [ -n "$DEPENDS" ]; then $PIPI $DEPENDS $EXTRA_DEPENDS; fi
     if [ "$COVERAGE" == "1" ]; then pip install coverage coveralls codecov; fi
-    if [ "$VTK" == "1" ]; then
-        sudo apt-get update;
-        sudo apt-get install -y $VTK_VER;
-        sudo apt-get install -y xvfb;
-        sudo apt-get install -y python-tk;
-        sudo apt-get install -y python-imaging;
-        $PIPI xvfbwrapper;
-    fi
 fi
+
+
