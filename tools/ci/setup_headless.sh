89c4eb14a755d9684a639a345c56ba2f90d651ae
Serge Koudoro <skab12@gmail.com>
2022-02-14 21:19:00 +0100

    export some viz env var for linux

--- tools/ci/setup_headless.sh
+++ tools/ci/setup_headless.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export DISPLAY=:99.0
+echo "DISPLAY=:99.0" >> $GITHUB_ENV
 
 if [ "$RUNNER_OS" == "Linux" ]; then
-	export LIBGL_ALWAYS_SOFTWARE=1
-    export LIBGL_ALWAYS_INDIRECT=0
+	echo "LIBGL_ALWAYS_SOFTWARE=1" >> $GITHUB_ENV
+    echo "LIBGL_ALWAYS_INDIRECT=0" >> $GITHUB_ENV
     sudo apt-get install -y libgl1-mesa-glx xvfb;
 	Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1  &
     sleep 3
