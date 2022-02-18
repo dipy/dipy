17eba69891a08d1bbf332f007225733bb3af1883
frheault <francois.m.rheault@usherbrooke.ca>
2022-02-18 11:52:28 -0600

    Switch back to load nifti

--- doc/examples/histology_resdnn.py
+++ doc/examples/histology_resdnn.py
@@ -24,7 +24,6 @@ from dipy.nn.histo_resdnn import HistoResDNN
 from dipy.reconst.shm import sh_to_sf
 from dipy.segment.mask import median_otsu
 from dipy.viz import window, actor
-import nibabel as nib
 import numpy as np
 import scipy.ndimage as ndi
 
@@ -39,8 +38,8 @@ and a gradient table is constructed from bvals/bvecs.
 
 # Fetch DWI and GTAB
 hardi_fname, hardi_bval_fname, hardi_bvec_fname = get_fnames('stanford_hardi')
-dwi_img = nib.load(hardi_fname)
-data = np.squeeze(dwi_img.get_fdata())
+data, affine = load_nifti(hardi_fname)
+data = np.squeeze(data)
 bvals, bvecs = read_bvals_bvecs(hardi_bval_fname, hardi_bvec_fname)
 gtab = gradient_table(bvals, bvecs)
 
@@ -60,12 +59,11 @@ unique, count = np.unique(mask_labeled, return_counts=True)
 val = unique[np.argmax(count[1:])+1]
 mask[mask_labeled != val] = 0
 
-nib.save(nib.Nifti1Image(mask.astype(np.uint8), dwi_img.affine),
-         'mask.nii.gz')
+save_nifti('mask.nii.gz', mask.astype(np.uint8), affine)
 
 """
 Using a ResDNN for sh_order 8 (default) and load the appropriate weights.
-Fit the data and save the resulting fODF.s
+Fit the data and save the resulting fODF.
 """
 
 resdnn_model = HistoResDNN(verbose=True)
@@ -73,7 +71,7 @@ fetch_model_weights_path = get_fnames('histo_resdnn_weights')
 resdnn_model.load_model_weights(fetch_model_weights_path)
 predicted_sh = resdnn_model.fit(data, gtab, mask=mask)
 
-nib.save(nib.Nifti1Image(predicted_sh, dwi_img.affine), 'predicted_sh.nii.gz')
+save_nifti('predicted_sh.nii.gz', predicted_sh, affine)
 
 """
 Preparing the scene using Fury. The ODF slicer expects spherical function
