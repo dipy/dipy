ffd45af6eaeb8763fbe6140dadef775d0a32e574
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2022-02-17 19:28:29 -0500

    STYLE: Remove redundant parentheses
    Remove redundant parentheses.

--- dipy/reconst/tests/test_shore_metrics.py
+++ dipy/reconst/tests/test_shore_metrics.py
@@ -59,7 +59,7 @@ def test_shore_metrics():
 
     # test if the analytical integral of the pdf is equal to one
     integral = 0
-    for n in range(int((radial_order)/2 + 1)):
+    for n in range(int(radial_order/2 + 1)):
         integral += c_shore[n] * (np.pi**(-1.5) * zeta ** (-1.5) *
                                   genlaguerre(n, 0.5)(0)) ** 0.5
 
