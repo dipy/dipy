bca0000dc1b4f5ce977b5eabd85f897b4a9ae904
Ariel Rokem <arokem@gmail.com>
2022-03-04 15:47:52 -0800

    Rename parfor to paramap to be a bit more accurate.

--- dipy/utils/tests/test_parallel.py
+++ dipy/utils/tests/test_parallel.py
@@ -1,6 +1,5 @@
 import numpy as np
 import numpy.testing as npt
-
 import dipy.utils.parallel as para
 
 
@@ -8,7 +7,7 @@ def power_it(num, n=2):
     # We define a function of the right form for parallelization
     return num ** n
 
-def test_parfor():
+def test_paramap():
     engines = ["serial"]
     if para.has_dask:
         engines.append("dask")
@@ -21,16 +20,17 @@ def test_parfor():
     my_list = list(my_array.ravel())
     for engine in engines:
         for backend in ["threading", "multiprocessing"]:
-            npt.assert_array_equal(para.parfor(power_it,
-                                         my_list,
-                                         engine=engine,
-                                         backend=backend,
-                                         out_shape=my_array.shape),
+            npt.assert_array_equal(para.paramap(
+                power_it,
+                my_list,
+                engine=engine,
+                backend=backend,
+                out_shape=my_array.shape),
                              power_it(my_array))
 
             # If it's not reshaped, the first item should be the item 0, 0:
-            npt.assert_equal(para.parfor(power_it,
-                                         my_list,
-                                         engine=engine,
-                                         backend=backend)[0],
+            npt.assert_equal(para.paramap(power_it,
+                                          my_list,
+                                          engine=engine,
+                                          backend=backend)[0],
                              power_it(my_array[0, 0]))
