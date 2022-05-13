1cf4b30fe66425d0e437317551f292c84f919a3e
Ariel Rokem <arokem@gmail.com>
2022-05-13 12:15:14 -0700

    Import the needed utility to test with optional packages.

--- dipy/reconst/tests/test_multi_voxel.py
+++ dipy/reconst/tests/test_multi_voxel.py
@@ -6,6 +6,7 @@ import numpy.testing as npt
 from dipy.core.sphere import unit_icosahedron
 from dipy.reconst.multi_voxel import CallableArray, _squash, multi_voxel_fit
 from dipy.testing.decorators import set_random_number_generator
+from dipy.utils.optpkg import optional_package
 
 joblib, has_joblib, _ = optional_package('joblib')
 dask, has_dask, _ = optional_package('dask')
