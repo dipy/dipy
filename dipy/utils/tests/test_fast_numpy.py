9393354aa2b925707a90960cde72fd84d8623a51
Gabriel Girard <girard.gabriel@gmail.com>
2023-01-26 15:21:20 -0500

    TST/BF - add missing import

--- dipy/utils/tests/test_fast_numpy.py
+++ dipy/utils/tests/test_fast_numpy.py
@@ -1,8 +1,7 @@
 import timeit
 
 import numpy as np
-
-from numpy.testing import assert_almost_equal, assert_
+from numpy.testing import assert_, assert_almost_equal, assert_array_equal 
 from dipy.utils.fast_numpy import random, norm, normalize, dot, cross
 
 
