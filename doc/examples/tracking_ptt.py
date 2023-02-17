51eb0c483660c5cd20894ae8b1c88f546a57d3a4
Gabriel Girard <girard.gabriel@gmail.com>
2023-02-17 07:16:40 -0500

    RF/DOC - simplified ptt example

--- doc/examples/tracking_ptt.py
+++ doc/examples/tracking_ptt.py
@@ -5,6 +5,13 @@ An introduction to the PTT
 
 """
 
+from dipy.direction import peaks_from_model
+from dipy.data import default_sphere
+from dipy.io.streamline import save_trk
+from dipy.io.stateful_tractogram import Space, StatefulTractogram
+from dipy.data import small_sphere
+from dipy.direction import PTTDirectionGetter
+from dipy.reconst.shm import CsaOdfModel
 from dipy.core.gradients import gradient_table
 from dipy.data import get_fnames
 from dipy.io.gradients import read_bvals_bvecs
@@ -19,7 +26,7 @@ from dipy.viz import window, actor, colormap, has_fury
 
 
 # Enables/disables interactive visualization
-interactive = True
+interactive = False
 
 hardi_fname, hardi_bval_fname, hardi_bvec_fname = get_fnames('stanford_hardi')
 label_fname = get_fnames('stanford_labels')
@@ -41,7 +48,6 @@ csd_fit = csd_model.fit(data, mask=white_matter)
 We use the GFA of the CSA model to build a stopping criterion.
 """
 
-from dipy.reconst.shm import CsaOdfModel
 
 csa_model = CsaOdfModel(gtab, sh_order=6)
 gfa = csa_model.fit(data, mask=white_matter).gfa
@@ -51,17 +57,13 @@ stopping_criterion = ThresholdStoppingCriterion(gfa, .25)
 
 """
 
-from dipy.direction import PTTDirectionGetter
-from dipy.data import small_sphere
-from dipy.io.stateful_tractogram import Space, StatefulTractogram
-from dipy.io.streamline import save_trk
 
 fod = csd_fit.odf(small_sphere)
 pmf = fod.clip(min=0)
-prob_dg = PTTDirectionGetter.from_pmf(pmf, max_angle=20,
-                                      sphere=small_sphere)
+ptt_dg = PTTDirectionGetter.from_pmf(pmf, max_angle=5,
+                                     sphere=small_sphere)
 
-streamline_generator = LocalTracking(direction_getter=prob_dg,
+streamline_generator = LocalTracking(direction_getter=ptt_dg,
                                      stopping_criterion=stopping_criterion,
                                      seeds=seeds,
                                      affine=affine,
@@ -82,79 +84,4 @@ if has_fury:
    :align: center
 
    **Corpus Callosum using ptt direction getter from PMF**
-"""
-"""
-One disadvantage of using a discrete PMF to represent possible tracking
-directions is that it tends to take up a lot of memory (RAM). The size of the
-PMF, the FOD in this case, must be equal to the number of possible tracking
-directions on the hemisphere, and every voxel has a unique PMF. In this case
-the data is ``(81, 106, 76)`` and ``small_sphere`` has 181 directions so the
-FOD is ``(81, 106, 76, 181)``. One way to avoid sampling the PMF and holding it
-in memory is to build the direction getter directly from the spherical harmonic
-(SH) representation of the FOD. By using this approach, we can also use a
-larger sphere, like ``default_sphere`` which has 362 directions on the
-hemisphere, without having to worry about memory limitations.
-"""
-
-from dipy.data import default_sphere
-
-prob_dg = PTTDirectionGetter.from_shcoeff(csd_fit.shm_coeff,
-                                          max_angle=1.,
-                                          sphere=default_sphere)
-streamline_generator = LocalTracking(prob_dg, stopping_criterion, seeds,
-                                     affine, step_size=1/20)
-streamlines = Streamlines(streamline_generator)
-sft = StatefulTractogram(streamlines, hardi_img, Space.RASMM)
-save_trk(sft, "tractogram_ptt_dg_sh.trk")
-
-if has_fury:
-    scene = window.Scene()
-    scene.add(actor.line(streamlines, colormap.line_colors(streamlines)))
-    window.record(scene, out_path='tractogram_ptt_dg_sh.png',
-                  size=(800, 800))
-    if interactive:
-        window.show(scene)
-"""
-.. figure:: tractogram_ptt_dg_sh.png
-   :align: center
-
-   **Corpus Callosum using ptt direction getter from SH**
-"""
-"""
-Not all model fits have the ``shm_coeff`` attribute because not all models use
-this basis to represent the data internally. However we can fit the ODF of any
-model to the spherical harmonic basis using the ``peaks_from_model`` function.
-"""
-
-from dipy.direction import peaks_from_model
-
-peaks = peaks_from_model(csd_model, data, default_sphere, .5, 25,
-                         mask=white_matter, return_sh=True, parallel=True)
-fod_coeff = peaks.shm_coeff
-
-prob_dg = PTTDirectionGetter.from_shcoeff(fod_coeff, max_angle=1.,
-                                          sphere=default_sphere)
-streamline_generator = LocalTracking(prob_dg, stopping_criterion, seeds,
-                                     affine, step_size=1/20)
-streamlines = Streamlines(streamline_generator)
-sft = StatefulTractogram(streamlines, hardi_img, Space.RASMM)
-save_trk(sft, "tractogram_ptt_dg_sh_pfm.trk")
-
-if has_fury:
-    scene = window.Scene()
-    scene.add(actor.line(streamlines, colormap.line_colors(streamlines)))
-    window.record(scene, out_path='tractogram_ptt_dg_sh_pfm.png',
-                  size=(800, 800))
-    if interactive:
-        window.show(scene)
-"""
-.. figure:: tractogram_ptt_dg_sh_pfm.png
-   :align: center
-
-   **Corpus Callosum using ptt direction getter from SH (
-   peaks_from_model)**
-"""
-"""
-.. include:: ../links_names.inc
-
 """
