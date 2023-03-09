998c34b70bb30fa67b323f908207e8ea3cebab5d
Javier Guaje <jrguajeg@iu.edu>
2023-03-09 11:57:11 -0500

    Added tab_ui property and tabs initialization to TabManager.

--- dipy/viz/horizon/tab/base.py
+++ dipy/viz/horizon/tab/base.py
@@ -24,14 +24,16 @@ class TabManager:
         # TODO: Handle size dynamically
         num_tabs = len(tabs)
         
-        self.tab_ui = ui.TabUI(
+        self.__tab_ui = ui.TabUI(
             position=(-25, 5), size=(320, 240), nb_tabs=num_tabs,
             active_color=(1, 1, 1), inactive_color=(0.5, 0.5, 0.5),
             draggable=False
         )
         
-        for i in range(num_tabs):
-            # TODO: Standardize tabs
-            self.tab_ui.tabs[i].title = 'Test'
-            # TODO: Loop over the elements in the custom tab
-            self.tab_ui.add_element(i, ring_slider, (0.3, 0.3))
+        for id, tab in enumerate(tabs):
+            self.__tab_ui.tabs[id].title = tab.name
+            tab.build(id, self.__tab_ui)
+    
+    @property
+    def tab_ui(self):
+        return self.__tab_ui
