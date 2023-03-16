b88190450a3bd117c5d9d4b8f2052e804e8f58ba
Javier Guaje <jrguajeg@iu.edu>
2023-03-16 10:51:42 -0400

    Indexed RoIsTab.

--- dipy/viz/horizon/tab/__init__.py
+++ dipy/viz/horizon/tab/__init__.py
@@ -1,6 +1,8 @@
 from dipy.viz.horizon.tab.base import (HorizonTab, TabManager, build_label,
                                        color_slider)
 from dipy.viz.horizon.tab.peak import PeaksTab
+from dipy.viz.horizon.tab.roi import RoIsTab
 
 __all__ = [
-    'HorizonTab', 'TabManager', 'PeaksTab', 'build_label', 'color_slider']
+    'HorizonTab', 'TabManager', 'PeaksTab', 'RoIsTab', 'build_label', 
+    'color_slider']
