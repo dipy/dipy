5887b17dba30c62547fc50137cc7cb8317d3120c
Javier Guaje <jrguajeg@iu.edu>
2023-03-21 10:26:12 -0400

    Indexed SlicesTab class.

--- dipy/viz/horizon/tab/__init__.py
+++ dipy/viz/horizon/tab/__init__.py
@@ -2,7 +2,8 @@ from dipy.viz.horizon.tab.base import (HorizonTab, TabManager, build_label,
                                        color_slider)
 from dipy.viz.horizon.tab.peak import PeaksTab
 from dipy.viz.horizon.tab.roi import ROIsTab
+from dipy.viz.horizon.tab.slice import SlicesTab
 
 __all__ = [
-    'HorizonTab', 'TabManager', 'PeaksTab', 'ROIsTab', 'build_label', 
-    'color_slider']
+    'HorizonTab', 'TabManager', 'PeaksTab', 'ROIsTab', 'SlicesTab',
+    'build_label', 'color_slider']
