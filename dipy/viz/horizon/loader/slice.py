d887b72cf5df39c2961f13e22e138fbcdec2f2a3
Javier Guaje <jrguajeg@iu.edu>
2023-04-03 18:03:20 -0400

    Adjusted output of add_slice_actors function.

--- dipy/viz/horizon/loader/slice.py
+++ dipy/viz/horizon/loader/slice.py
@@ -3,6 +3,7 @@ import warnings
 import numpy as np
 
 from dipy.utils.optpkg import optional_package
+from dipy.viz.gmem import GlobalHorizon
 
 fury, has_fury, setup_module = optional_package('fury')
 
@@ -10,24 +11,18 @@ if has_fury:
     from fury import actor
 
 
-def nifti_to_slice_actors(data, affine=None, world_coords=False):
+def add_slice_actors(data, scene, affine=None, world_coords=False):
     orig_shape = data.shape
-    print('Original shape: {}'.format(orig_shape))
+    print(f'Original shape: {orig_shape}')
     ndim = data.ndim
-    tmp = data
+    tmp_data = data
     if ndim == 4:
-        if orig_shape[-1] > 3:
-            orig_shape = orig_shape[:3]
-            # Sometimes, first volume is null, so we try the next one.
-            for i in range(orig_shape[-1]):
-                tmp = data[..., i]
-                value_range = np.percentile(data[..., i], q=[2, 98])
-                if np.sum(np.diff(value_range)) != 0:
-                    break
-        if orig_shape[-1] == 3:
-            value_range = (0, 1.)
-    if ndim == 3:
-        value_range = np.percentile(tmp, q=[2, 98])
+        tmp_data = data[..., 0]
+    elif 3 > ndim > 4:
+        raise ValueError(f'Unsupported data dimension: {ndim}')
+    print(f'{tmp_data.min()} - {tmp_data.max()}')
+    
+    value_range = np.percentile(tmp_data, [2, 98])
 
     if np.sum(np.diff(value_range)) == 0:
         warnings.warn(
@@ -38,21 +33,17 @@ def nifti_to_slice_actors(data, affine=None, world_coords=False):
         affine = np.eye(4)
 
     slice_actor_z = actor.slicer(
-        tmp, affine=affine, value_range=value_range, interpolation='nearest',
-        picking_tol=0.025)
+        tmp_data, affine=affine, value_range=value_range,
+        interpolation='nearest')
 
     tmp_new = slice_actor_z.resliced_array()
+    print(f'{tmp_new.min()} - {tmp_new.max()}')
 
-    if len(data.shape) == 4:
-        if data.shape[-1] == 3:
-            print('Resized to RAS shape: {}'.format(tmp_new.shape))
-        else:
-            print('Resized to RAS shape: {}'.format(
-                tmp_new.shape + (data.shape[-1],)))
+    if ndim == 4:
+        shape = tmp_new.shape + (data.shape[-1],)
     else:
-        print('Resized to RAS shape: {}'.format(tmp_new.shape))
-
-    shape = tmp_new.shape
+        shape = tmp_new.shape
+    print(f'Resized to RAS shape: {shape}')
 
     slice_actor_x = slice_actor_z.copy()
     x_midpoint = int(np.round(shape[0] / 2))
@@ -64,4 +55,13 @@ def nifti_to_slice_actors(data, affine=None, world_coords=False):
     slice_actor_y.display_extent(
         0, shape[0] - 1, y_midpoint, y_midpoint, 0, shape[2] - 1)
     
-    return ((slice_actor_x, slice_actor_y, slice_actor_z), tmp, value_range)
+    scene.add(slice_actor_x)
+    scene.add(slice_actor_y)
+    scene.add(slice_actor_z)
+    
+    return ((slice_actor_x, slice_actor_y, slice_actor_z), shape,
+            (data.min(), data.max()), value_range)
+
+
+def replace_slice_actors(data, scene, actors, affine=None, world_coords=False):
+    pass
