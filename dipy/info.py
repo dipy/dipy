ba3ce0c594d276dee0afa4cd4ef4da096ca01d9f
frheault <francois.m.rheault@usherbrooke.ca>
2023-04-17 11:01:57 -0400

    upgrade numpy and sync tests

--- dipy/info.py
+++ dipy/info.py
@@ -81,7 +81,7 @@ NIBABEL_MIN_VERSION = '3.0.0'
 H5PY_MIN_VERSION = '2.8.0'
 PACKAGING_MIN_VERSION = '19.0'
 TQDM_MIN_VERSION = '4.30.0'
-TRX_MIN_VERSION = '0.1'
+TRX_MIN_VERSION = '0.0'
 
 # Main setup parameters
 NAME                = 'dipy'
