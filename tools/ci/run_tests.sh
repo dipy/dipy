eab9e65725c070e33e98d3320b88b05b32395332
Serge Koudoro <skab12@gmail.com>
2023-04-17 15:05:09 -0400

    use gha to upload coverage

--- tools/ci/run_tests.sh
+++ tools/ci/run_tests.sh
@@ -20,10 +20,8 @@ if [ "$COVERAGE" == "1" ] || [ "$COVERAGE" == true ]; then
     # Run the tests and check for test coverage.
     coverage run -m pytest -svv --doctest-modules --verbose --durations=10 --pyargs dipy
     coverage report -m  # Generate test coverage report.
-    echo "START UPLOAD COVERAGE"
-    codecov    # Upload the report to codecov.
 else
     pytest -svv --doctest-modules --verbose --durations=10 --pyargs dipy
 fi
-
+cd ..
 set +ex
