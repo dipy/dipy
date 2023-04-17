dd43e577e28cb8841de2aaf00918a7fb6eba5668
frheault <francois.m.rheault@usherbrooke.ca>
2023-04-17 08:32:09 -0400

    upgrade numpy

--- dipy/info.py
+++ dipy/info.py
@@ -75,7 +75,7 @@ MIT licenses.
 # versions for dependencies
 # Check these versions against .travis.yml and requirements.txt
 CYTHON_MIN_VERSION = '0.29.24'
-NUMPY_MIN_VERSION = '1.14.5'
+NUMPY_MIN_VERSION = '1.20.3'
 SCIPY_MIN_VERSION = '1.1'
 NIBABEL_MIN_VERSION = '3.0.0'
 H5PY_MIN_VERSION = '2.8.0'
