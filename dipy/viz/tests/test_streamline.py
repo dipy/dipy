c9c84c58688acde8a3755ffc8683dcf6699e339d
BramshQamar <bramshq@gmail.com>
2023-04-19 14:37:09 -0700

    added all tests for bundlwarp and visualization functions

--- dipy/viz/tests/test_streamline.py
+++ dipy/viz/tests/test_streamline.py
@@ -4,11 +4,15 @@ from numpy.testing import assert_raises, assert_equal
 import pytest
 
 from dipy.utils.optpkg import optional_package
-from dipy.data import read_five_af_bundles
+from dipy.data import read_five_af_bundles, two_cingulum_bundles
+from dipy.tracking.streamline import set_number_of_points, unlist_streamlines
+
+from dipy.align.streamwarp import bundlewarp, bundlewarp_vector_filed
 
 fury, has_fury, _ = optional_package('fury')
 if has_fury:
-    from dipy.viz.streamline import show_bundles
+    from dipy.viz.streamline import (show_bundles, viz_two_bundles,
+                                     viz_displacement_mag, viz_vector_field)
     from dipy.viz import window
 
 bundles = read_five_af_bundles()
@@ -35,7 +39,40 @@ def test_output_created():
         report = window.analyze_snapshot(fname, find_objects=True)
         assert_equal(report.objects > 0, True)
 
+        cb1, cb2 = two_cingulum_bundles()
+
+        fname = os.path.join(temp_dir, 'test_two_bundles.png')
+        viz_two_bundles(cb1, cb2, fname=fname)
+        assert_equal(os.path.exists(fname), True)
+
 
 @pytest.mark.skipif(not has_fury, reason='Requires FURY')
 def test_incorrect_view():
     assert_raises(ValueError, show_bundles, bundles, False, 'wrong_view')
+
+
+@pytest.mark.skipif(not has_fury, reason='Requires FURY')
+def test_bundlewarp_viz():
+
+    cingulum_bundles = two_cingulum_bundles()
+
+    cb1 = cingulum_bundles[0]
+    cb1 = set_number_of_points(cb1, 20)
+
+    cb2 = cingulum_bundles[1]
+    cb2 = set_number_of_points(cb2, 20)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+
+        deformed_bundle, affine_bundle, _, _, _ = bundlewarp(cb1, cb2)
+
+        offsets, directions, colors = bundlewarp_vector_filed(cb1, cb2)
+        points_aligned, _ = unlist_streamlines(affine_bundle)
+
+        fname = os.path.join(temp_dir, 'test_vector_field.png')
+        viz_vector_field(points_aligned, directions, colors, offsets, fname)
+        assert_equal(os.path.exists(fname), True)
+
+        fname = os.path.join(temp_dir, 'test_mag_viz.png')
+        viz_displacement_mag(affine_bundle, offsets, fname)
+        assert_equal(os.path.exists(fname), True)
