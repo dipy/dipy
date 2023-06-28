de009768ca70c3046e516d0d20183926d81bb62a
shilpiprd <shilpiprd@gmail.com>
2023-06-28 13:30:02 +0530

    Updating the Predict method,and pep8Format

--- dipy/reconst/cti.py
+++ dipy/reconst/cti.py
@@ -12,20 +12,87 @@ from dipy.reconst.dki import (
 
                             )
 
+
+def cti_prediction(cti_params, gtab1, gtab2, S0=1):
+    """Predict a signal given correlation tensor imaging parameters
+
+    	Parameters
+    	----------
+      	cti_params: numpy.ndarray (...,43)
+    		All parameters estimated from the correlation tensor model.
+    		Paramters are ordered as follows::
+
+		1. Six independent elments of diffusion tensor.
+		2. Twenty-One elements of the covariance tensor
+		3. Fifteen elements of the kurtosis tensor
+	gtab1: dipy.core.gradients.GradientTable
+        A GradientTable class instance for first DDE diffusion epoch
+
+    	gtab2: dipy.core.gradients.GradientTable
+        A GradientTable class instance for second DDE diffusion epoch
+
+	S0 : float or ndarray (optional)
+            The non diffusion-weighted signal in every voxel, or across all
+            voxels. Default: 1
+
+        Returns
+        -------
+        S : ndarray
+            Simulated signal based on the CTI model:
+
+    """
+    evals, evecs = split_cti_parms(cti_params)
+	# Define CTI design matrix according to given gtabs
+    A = cti_design_matrix(gtab1, gtab2)
+
+    # Flat parameters and initialize pred_sig
+    fevals = evals.reshape((-1, evals.shape[-1]))
+    fevecs = evecs.reshape((-1,) + evecs.shape[-2:])
+    fkt = kt.reshape((-1, kt.shape[-1]))
+    pred_sig = np.zeros((len(fevals), len(gtab.bvals)))
+    if isinstance(S0, np.ndarray):
+        S0_vol = np.reshape(S0, (len(fevals)))
+    else:
+        S0_vol = S0
+    # looping for all voxels
+    for v in range(len(pred_sig)):
+        DT = np.dot(np.dot(fevecs[v], np.diag(fevals[v])), fevecs[v].T)
+        dt = lower_triangular(DT)
+        MD = (dt[0] + dt[2] + dt[5]) / 3
+        if isinstance(S0_vol, np.ndarray):
+            this_S0 = S0_vol[v]
+        else:
+            this_S0 = S0_vol
+        X = np.concatenate((dt, fkt[v] * MD * MD,
+                            np.array([-np.log(this_S0)])),
+                           axis=0)
+        pred_sig[v] = np.exp(np.dot(A, X))
+
+    # Reshape data according to the shape of cti_params
+    pred_sig = pred_sig.reshape(cti_params.shape[:-1] + (pred_sig.shape[-1],))
+
+    return pred_sig
+
+
 class CorrelationTensorModel(ReconstModel):
     """ Class for the Correlation Tensor Model
     """
-    def __init__(self, gtab, fit_method="", *args, **kwargs): #not sure about the fit method yet
-        """ Diffusion Kurtosis Tensor Model [1]
+
+    # not sure about the fit method yet
+    def __init__(self, gtab1, gtab2, fit_method="", *args, **kwargs):
+        """ Correlation Tensor Imaging Model [1]
 
         Parameters
         ----------
-        gtab : GradientTable class instance
+	gtab1: dipy.core.gradients.GradientTable
+        A GradientTable class instance for first DDE diffusion epoch
+    	gtab2: dipy.core.gradients.GradientTable
+        A GradientTable class instance for second DDE diffusion epoch
 
         fit_method : str or callable
 
 
-        args, kwargs : arguments and key-word arguments passed to the 
+        args, kwargs : arguments and key-word arguments passed to the
         fit_method.
 
         """
@@ -43,73 +110,41 @@ class CorrelationTensorModel(ReconstModel):
             should be analyzed that has the shape data.shape[-1]
 
         """
-        
-    def cti_prediction(cti_params, gtab1, gtab2, S0 = 1):
-    
-    """Predict a signal given correlation tensor imaging parameters
-    
-    	Parameters
-    	----------
-      	cti_params: numpy.ndarray (...,43)
-    		All parameters estimated from the correlation tensor model.
-    		Paramters are ordered as follows:: 
-    		
-		1. Six independent elments of diffusion tensor.
-		2. Twenty-One elements of the covariance tensor
-		3. Fifteen elements of the kurtosis tensor 
-	S0 : float or ndarray (optional)
-            The non diffusion-weighted signal in every voxel, or across all
-            voxels. Default: 1
-            
-        Returns
-        -------
-        S : ndarray 
-            Simulated signal based on the CTI model: 
-            
-    """
-    math ? : design_maatrix x cti_params
-    #were not considering the kurtosis tensor while generating signals.
-    #we need to specify gtab1 and gtab2 
-    
- 
-    def predict(self, cti_params, S0=1.):  #created
+
+    def predict(self, cti_params, S0=1.):  # created
     	"""Predict a signal for the CTI model class instance given parameteres
-    	
-    	Parameters: 
+
+    	Parameters:
     	-----------
-    	params: numpy.ndarray (...,43)
+    	cti_params: numpy.ndarray (...,43)
     		All parameters estimated from the correlation tensor model.
-    		Paramters are ordered as follows:: 
-    		
-		1. Three diffusion tensor's eigenvalues
-		2. Three lines of the eigenvector matrix each containing the
-		first, second and third coordinates of the eigenvector
-		3. Twenty-One elements of the covariance tensor
-		4. Fifteen elements of the kurtosis tensor 
+    		Paramters are ordered as follows::
+
+		1. Six independent elements of Diffusion Tensor.
+		2. Twenty-One elements of the Covariance Tensor
+		3. Fifteen elements of the Kurtosis Tensor
+	gtab1: dipy.core.gradients.GradientTable
+        A GradientTable class instance for first DDE diffusion epoch
+    	gtab2: dipy.core.gradients.GradientTable
+        A GradientTable class instance for second DDE diffusion epoch
 	S0 : float or ndarray (optional)
             The non diffusion-weighted signal in every voxel, or across all
             voxels. Default: 1
-            
+
         Returns
         -------
         S : numpy.ndarray
             Signals.
- 	""" 
-        
-        #S = qti_signal(self.gtab, D, C, S0)
-        #return S #this is from covariance matrix
-        #return dki_prediction(self.model_params, gtab, S0)
-        #this is from dki module
-   	return cti_prediction(cti_params, self.gtab, S0)
-        
-        
+ 	"""
+
+   	return cti_prediction(cti_params, self.gtab1, self.gtab2, S0)
+
 
 class CorrelationTensorFit(DiffusionKurtosisFit):
 
-    """ Class for fitting the Diffusion Kurtosis Model """ 
+    """ Class for fitting the Diffusion Kurtosis Model """
 
     def __init__(self, model, model_params):
-    
         """ Initialize a CorrelationTensorFit class instance.
 
         Since CTI is an extension of DKI, class instance is defined as subclass
@@ -119,19 +154,19 @@ class CorrelationTensorFit(DiffusionKurtosisFit):
         ----------
         model : CorrelationTensorModel Class instance
             Class instance containing the Correlation Tensor Model for the fit
-        model_params : ndarray (x, y, z, 27) or (n, 27)
+        model_params : ndarray (x, y, z, 43) or (n, 43)
             All parameters estimated from the correlation tensor model.
             Parameters are ordered as follows:
 
         """
         DiffusionKurtosisFit.__init__(self, model, model_params)
-        
-    def kt(self): #created
+
+    def kt(self):  # created
         """
         Return the 15 independent elements of the kurtosis tensor as an array
         """
-        return self.model_params[..., 27:42] #last index won't get included....?
-        
+        return self.model_params[..., 27:42]  # last index won't get included....?
+
    def dft(self):  #created
     	"""
     	Returns the 6 independent elements of the diffusion tensor as an array
@@ -158,7 +193,7 @@ class CorrelationTensorFit(DiffusionKurtosisFit):
      def split_dk_cv_param(params):
      	r""" Extract the diffusion tensor eigenvalues, the diffusion tensor
     eigenvector matrix, and the 15 independent elements of the kurtosis tensor
-    from the model parameters estimated from the DKI model
+    from the model parameters estimated from the CTI model
 
     Parameters
     ----------
@@ -172,7 +207,7 @@ class CorrelationTensorFit(DiffusionKurtosisFit):
     
     
     
-  def predict(self, gtab, S0=1.):  #created
+  def predict(self, gtab1, gtab2, S0=1.):  #created
     	"""Given a CTI model fit, predict the signal on the vertices of a gradient table 
     	
     	Parameters: 
@@ -181,11 +216,13 @@ class CorrelationTensorFit(DiffusionKurtosisFit):
     		All parameters estimated from the correlation tensor model.
     		Paramters are ordered as follows:: 
     		
-		1. Three diffusion tensor's eigenvalues
-		2. Three lines of the eigenvector matrix each containing the
-		first, second and third coordinates of the eigenvector
-		3. Twenty-One elements of the covariance tensor
-		4. Fifteen elements of the kurtosis tensor 
+		1. Six independent elements of the Diffusion Tensor
+		2. Twenty-One elements of the covariance tensor
+		3. Fifteen elements of the kurtosis tensor 
+	gtab1: dipy.core.gradients.GradientTable
+        A GradientTable class instance for first DDE diffusion epoch
+    	gtab2: dipy.core.gradients.GradientTable
+        A GradientTable class instance for second DDE diffusion epoch
 	S0 : float or ndarray (optional)
             The non diffusion-weighted signal in every voxel, or across all
             voxels. Default: 1
@@ -195,7 +232,7 @@ class CorrelationTensorFit(DiffusionKurtosisFit):
         S : numpy.ndarray
             Signals.
         """
-        return cti_prediction(self.model_params, gtab, S0)
+        return cti_prediction(self.model_params, gtab1, gtab2, S0)
         
    
    
@@ -230,11 +267,6 @@ def params_to_cvt_params(result, min_diffusivity = 0):
 
 
 
-
-
-
-
-
 def split_cti_params(cti_params): 
    r"""Extract the diffusion tensor eigenvalues, the diffusion tensor eigenvector matrix, and the 21 independent elements of the covariance tensor, and the 15 independent elements of the kurtosis tensor from the model parameters estimated from the CTI model 
    Parameters: 
@@ -259,10 +291,12 @@ def split_cti_params(cti_params):
         kt: Fifteen elemnets of the kurtosis tensor 
        
       """ 
-     dvt = cti_params[...,:6]
+      DT_elements = cti_params[...,:6]
+      evals, evecs = decompose_tensor(from_lower_triangular(DT_elements),
+                                    		min_diffusivity=min_diffusivity)
      cvt = cti_params[...,6:27]
      kvt = cti_params[..., 27:42]
-     return dvt, cvt, kvt 
+     return evals, evecs, cvt, kvt 
      
      
 def ls_fit_cti(design_matrix, data, inverse_design_matrix, weights = True, min_diffusivity = 0): 
