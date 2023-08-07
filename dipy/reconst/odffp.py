e6c0cdaadfef90a348fa8ba2c19af930186b6e87
filipp02 <69862888+filipp02@users.noreply.github.com>
2023-08-07 09:21:56 -0400

    Penalization begins at the 2nd fiber

--- dipy/reconst/odffp.py
+++ dipy/reconst/odffp.py
@@ -706,34 +706,71 @@ class OdffpModel(object):
     #     return np.argmax(dot_product, axis=1) 
 
 
+    # def _find_matching_odf_trace(self, input_odf_trace, dict_odf_trace, penalty):
+    #     input_odf_trace_num = input_odf_trace.shape[0]
+    #
+    #     max_dot_product_values = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num))
+    #     max_dot_product_dict_idx = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num), dtype=int)
+    #
+    #     for peak_id in range(self._dict.max_peaks_num+1):
+    #         peak_filter = np.array(self._dict.peaks_per_voxel == peak_id)
+    #         if ~np.any(peak_filter):
+    #             peak_filter[0] = True
+    #
+    #         peak_filter_idx = np.arange(len(self._dict.peaks_per_voxel))[peak_filter]
+    #
+    #         dot_product = np.dot(input_odf_trace, dict_odf_trace[:,peak_filter])
+    #         max_dot_product_idx = np.argmax(dot_product, axis=1)
+    #         max_dot_product_dict_idx[peak_id] = peak_filter_idx[max_dot_product_idx] 
+    #         max_dot_product_values[peak_id] = dot_product[
+    #             np.arange(input_odf_trace_num),
+    #             max_dot_product_idx
+    #         ]
+    #
+    #     if penalty > 0.0:
+    #         max_dot_product_values = np.log(max_dot_product_values) - 2 * penalty * np.atleast_2d(np.arange(self._dict.max_peaks_num+1)).T
+    #
+    #     return max_dot_product_dict_idx[
+    #         np.argmax(max_dot_product_values, axis=0),
+    #         np.arange(input_odf_trace_num)
+    #     ]
+        
     def _find_matching_odf_trace(self, input_odf_trace, dict_odf_trace, penalty):
-        input_odf_trace_num = input_odf_trace.shape[0]
     
-        max_dot_product_values = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num))
-        max_dot_product_dict_idx = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num), dtype=int)
+        try:
+            input_odf_trace_num = input_odf_trace.shape[0]
     
-        for peak_id in range(self._dict.max_peaks_num+1):
-            peak_filter = np.array(self._dict.peaks_per_voxel == peak_id)
-            if ~np.any(peak_filter):
-                peak_filter[0] = True
+            max_dot_product_values = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num))
+            max_dot_product_dict_idx = np.zeros((self._dict.max_peaks_num+1, input_odf_trace_num), dtype=int)
+    
+            for peak_id in range(self._dict.max_peaks_num+1):
+                peak_filter = np.array(self._dict.peaks_per_voxel == peak_id)
+                if ~np.any(peak_filter):
+                    peak_filter[0] = True
                 
-            peak_filter_idx = np.arange(len(self._dict.peaks_per_voxel))[peak_filter]
+                peak_filter_idx = np.arange(len(self._dict.peaks_per_voxel))[peak_filter]
     
-            dot_product = np.dot(input_odf_trace, dict_odf_trace[:,peak_filter])
-            max_dot_product_idx = np.argmax(dot_product, axis=1)
-            max_dot_product_dict_idx[peak_id] = peak_filter_idx[max_dot_product_idx] 
-            max_dot_product_values[peak_id] = dot_product[
-                np.arange(input_odf_trace_num),
-                max_dot_product_idx
-            ]
+                dot_product = np.dot(input_odf_trace, dict_odf_trace[:,peak_filter])
+                max_dot_product_idx = np.argmax(dot_product, axis=1)
+                max_dot_product_dict_idx[peak_id] = peak_filter_idx[max_dot_product_idx] 
+                max_dot_product_values[peak_id] = dot_product[
+                    np.arange(input_odf_trace_num),
+                    max_dot_product_idx
+                ]
     
-        if penalty > 0.0:
-            max_dot_product_values = np.log(max_dot_product_values) - 2 * penalty * np.atleast_2d(np.arange(self._dict.max_peaks_num+1)).T
+            # Penalization begins at the 2nd fiber
+            if penalty > 0.0:
+                max_dot_product_values = np.log(max_dot_product_values) - 2 * penalty * np.atleast_2d(
+                    np.hstack((0, np.arange(self._dict.max_peaks_num)))
+                ).T
     
+        except:
+            return self._find_matching_odf_trace(input_odf_trace, dict_odf_trace, penalty)
+
         return max_dot_product_dict_idx[
             np.argmax(max_dot_product_values, axis=0),
             np.arange(input_odf_trace_num)
-        ]
+        ]        
 
    
     def fit(self, data, mask=None, max_chunk_size=1000, penalty = DEFAULT_FIT_PENALTY):
