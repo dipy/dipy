80fc0b9747605f6aa9601e687fc6c05b6110bf77
36000 <jk6.28@outlook.com>
2023-09-08 10:44:47 -0700

    maxlen here is now 2

--- dipy/direction/tests/test_ptt_direction_getter.py
+++ dipy/direction/tests/test_ptt_direction_getter.py
@@ -64,7 +64,7 @@ def test_ptt_tracking():
                                          stopping_criterion=sc,
                                          seeds=seed_coordinates,
                                          affine=affine,
-                                         maxlen=1)
+                                         maxlen=2)
     streams = Streamlines(streamline_generator)
     npt.assert_almost_equal(np.linalg.norm(streams[0][0] - streams[0][1]), 0.2,
                             decimal=1)
@@ -76,7 +76,7 @@ def test_ptt_tracking():
                                          stopping_criterion=sc,
                                          seeds=seed_coordinates,
                                          affine=affine,
-                                         maxlen=1,
+                                         maxlen=2,
                                          fixedstep=False)
 
     # Check fixedstep ValueError
