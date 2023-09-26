5b0c567a25381a24ef168d3617c9bb835c80adae
shilpiprd <shilpiprd@gmail.com>
2023-09-26 23:03:57 +0530

    Making suggested changes_1

--- doc/examples/qti_simulation_tutorial.py
+++ doc/examples/qti_simulation_tutorial.py
@@ -3,135 +3,55 @@
 Q-SpaceTrajectory Simulation
 ============================
 
-The most basic assumption of qti states that tissues can be denoted by multiple gaussian component.
-QTI can only handle a single gtab at a time, so in this tutorial we'll start by taking 2 gtabs then combine them into a single btensor. 
-"""
+In this tutorial, we demonstrate how to generate synthetic signals using
+b-tensor valued encodings. These are invaluable for acquiring synthetic signals
+from advanced diffusion encodings, working under the premise that tissues can
+be represented as a summation of multiple Gaussian components.
 
+QTI is designed to handle a single gtab at a time. In this tutorial, we will
+start by combining two gtabs into a single btensor.
+
+First, let's import all the relevant libraries and modules for this simulation.
+"""
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
-from dipy.reconst.tests.test_qti import  _anisotropic_DTD, _isotropic_DTD 
-from dipy.core.gradients import gradient_table 
-from dipy.core.sphere import disperse_charges, Sphere, HemiSphere
+from dipy.core.gradients import gradient_table
+from dipy.core.sphere import disperse_charges, HemiSphere
 from dipy.sims.voxel import multi_tensor
 
 """
-We'll take the case of 2 gtabs and then combine them into a single btensor as for qti model, we need a single gtab.
+We'll take the case of two gtabs and combine them into a single btensor.
 """
-
-""" 
-Creating the first gtab 
-""" 
-
-n_pts = 20
-theta = np.pi * np.random.rand(n_pts) 
+n_pts = 30
+theta = np.pi * np.random.rand(n_pts)
 phi = 2 * np.pi * np.random.rand(n_pts)
 hsph_initial = HemiSphere(theta=theta, phi=phi)
-hsph_updated, potential = disperse_charges(hsph_initial, 5000) 
-bvecs1 = np.concatenate([hsph_updated.vertices] * 4)
-bvecs1 = np.append(bvecs1 , [[0, 0, 0]], axis=0)
-bvals1 = np.array([2] * 20 + [1] * 20 + [1] * 20 + [1] * 20 + [0])
-gtab1 = gradient_table(bvals1, bvecs1)
+hsph_updated, potential = disperse_charges(hsph_initial, 5000)
+# These will correspond to the main directions of the btensors.
+bvecs = np.concatenate([hsph_updated.vertices] * 4)
+bvals = np.array([1000] * 30 + [2000] * 30 + [1000] * 30 + [2000] * 30)
 
-""" 
-For the second gradient table, we'll consider a set of directions which are perpendicular relative to the generated hsph_updated. We'll call it hsph_updated90.
-Using the function perpendicular_directions, we can get the perpendicular tensors.
-"""
+# Define b-tensor encodings
+btens = np.array(['LTE' for i in range(60)] + ['PTE' for i in range(60)])
 
-def perpendicular_directions(v, num=20, half=False):
-    v = np.array(v, dtype=np.float64)
-    v = v.T 
-    er = np.finfo(v[0].dtype).eps * 1e3
-    if half is True:
-        a = np.linspace(0., math.pi, num=num, endpoint=False)
-    else:
-        a = np.linspace(0., 2 * math.pi, num=num, endpoint=False)
-    cosa = np.cos(a) #(20,)
-    sina = np.sin(a)
-    if np.any(abs(v[0] - 1.) > er):
-        sq = np.sqrt(v[1]**2 + v[2]**2)
-        psamples = np.array([- sq*sina, (v[0]*v[1]*sina - v[2]*cosa) / sq,
-                             (v[0]*v[2]*sina + v[1]*cosa) / sq])
-    else:
-        sq = np.sqrt(v[0]**2 + v[2]**2)
-        psamples = np.array([- (v[2]*cosa + v[0]*v[1]*sina) / sq, sina*sq,
-                             (v[0]*cosa - v[2]*v[1]*sina) / sq])
-    return psamples.T
-    
+# Create the gradient table with b-tensor encodings
+gtab = gradient_table(bvals, bvecs, btens=btens)
 
-"""
-Using the above function to get the perpendicular vectors.
-""" 
+# Define the eigenvalues for the tensors
+mevals = np.array([[0.0015, 0.0003, 0.0003],
+                   [0.0015, 0.0003, 0.0003]])
 
-hsph_updated90 = perpendicular_directions(hsph_updated.vertices)
+# Define the principal axis of each tensor in polar coordinates (θ, ϕ)
+angles = [(0, 0), (60, 0)]
 
-"""
-We can check that the verctors are indeed perpendicular by taking dot product.
-"""
+# Define the contribution percentage of each tensor
+fractions = [50, 50]
 
-dot_product = np.sum(hsph_updated.vertices * hsph_updated90, axis=1)
-are_perpendicular = np.isclose(dot_product, 0)
+# Generate the simulated signal using the multi_tensor function
+signal, sticks = multi_tensor(gtab, mevals, S0=100, angles=angles,
+                              fractions=fractions, snr=None)
 
-bvecs2 = np.concatenate(([hsph_updated.vertices] * 2) + [hsph_updated90] + ([hsph_updated.vertices]))
-bvecs2 = np.append(bvecs2, [[0, 0, 0]], axis=0)
-
-bvals2 =  np.array([0] * 20 + [1] * 20 + [1] * 20 + [0] * 20 + [0])
-gtab2 = gradient_table(bvals2, bvecs2)
-
-"""
-Creating the btensor.
-Defining btensor helps to capture more complex tissue microstructure information than either DTI or DKI. It also simplifies the information as now rather than having to consider 2 gtabs, we need to consider only one. 
-"""
-
-e1 = bvecs1 
-e2 = bvecs2
-e3 = np.cross(e1,e2) 
-V = np.stack((e1, e2, e3), axis=-1)
-V_transpose = np.transpose(V, axes=(0, 2, 1))
-
-"""
-Defining the Btensor
-"""
-B = np.zeros((81, 3, 3)) #initializing a btensor
-b = np.zeros((3, 3))
-for i in range(81):
-    b[0, 0] = bvals1[i]
-    b[1, 1] = bvals2[i]
-    B[i] = np.matmul(V[i], np.matmul(b, V_transpose[i]))
-    
-"""
-Creating the equivalent gradient table
-"""
-gtab = gradient_table(bvals1, bvecs1, btens = B) 
-angles = [(0, 0), (90, 0)] #both of them are perpendicular to each other
-fractions = [50, 50] 
-
-# Generate anisotropic signal
-anisotropic_DTD = _anisotropic_DTD()
-isotropic_DTD = _isotropic_DTD() 
-signal_aniso, sticks = multi_tensor(gtab, anisotropic_DTD, S0=100, angles=angles, fractions=fractions, snr=None)
-# Generate isotropic signal
-signal_iso, sticks = multi_tensor(gtab, isotropic_DTD, S0=100, angles = angles, fractions= fractions, snr=None)
-
-
-plt.plot(signal_aniso, label='anisotropic_signals')
-plt.plot(signal_iso, label = 'isotropic_signals')
+plt.plot(signal, label='signals')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
