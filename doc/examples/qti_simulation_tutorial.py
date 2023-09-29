000236795a8a6bb838aff3220a7a2d6b32f160fe
shilpiprd <shilpiprd@gmail.com>
2023-09-29 15:53:28 +0530

    Making basic changes

--- doc/examples/qti_simulation_tutorial.py
+++ doc/examples/qti_simulation_tutorial.py
@@ -21,24 +21,28 @@ from dipy.core.sphere import disperse_charges, HemiSphere
 from dipy.sims.voxel import multi_tensor
 
 """
-We'll take the case of two gtabs and combine them into a single btensor.
+Here we simulate a QTI signal based on random hemispherical directions, different b-values and b-tensor encodings.
+n_pts represents the number of points for hemisphere sampling.
 """
 n_pts = 30
 theta = np.pi * np.random.rand(n_pts)
 phi = 2 * np.pi * np.random.rand(n_pts)
 hsph_initial = HemiSphere(theta=theta, phi=phi)
+"""
+We use disperse_charges to optimize hemisphere sampling.
+"""
 hsph_updated, potential = disperse_charges(hsph_initial, 5000)
 # These will correspond to the main directions of the btensors.
 bvecs = np.concatenate([hsph_updated.vertices] * 4)
 bvals = np.array([1000] * 30 + [2000] * 30 + [1000] * 30 + [2000] * 30)
 
-# Define b-tensor encodings
+"""Here we define b-tensor encodings which include both LTE and PTE."""
 btens = np.array(['LTE' for i in range(60)] + ['PTE' for i in range(60)])
 
-# Create the gradient table with b-tensor encodings
+"""Here we create the gradient table with b-tensor encodings. """
 gtab = gradient_table(bvals, bvecs, btens=btens)
 
-# Define the eigenvalues for the tensors
+"""Here we're defining the eigenvalues for the tensors"""
 mevals = np.array([[0.0015, 0.0003, 0.0003],
                    [0.0015, 0.0003, 0.0003]])
 
@@ -48,10 +52,14 @@ angles = [(0, 0), (60, 0)]
 # Define the contribution percentage of each tensor
 fractions = [50, 50]
 
-# Generate the simulated signal using the multi_tensor function
+"""
+The signal is simulated using the 'multi_tensor' function with provided eigenvalues, angles, and fractions.
+"""
 signal, sticks = multi_tensor(gtab, mevals, S0=100, angles=angles,
                               fractions=fractions, snr=None)
-
+"""
+Here we finally plot the simulated QTI signal.
+"""
 plt.plot(signal, label='signals')
 plt.legend()
 plt.show()
