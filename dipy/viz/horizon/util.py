2edb5a7a87cdd4b3cee6b2a5aebb57bbd05b2e9f
maharshigor <gor.maharshi@gmail.com>
2023-10-11 11:18:39 -0400

    single image shape check added

--- dipy/viz/horizon/util.py
+++ dipy/viz/horizon/util.py
@@ -2,6 +2,10 @@ def check_img_shapes(images):
     """
     Checks if the images have same shapes
     """
+
+    # No need to check if there is less than 2 images
+    if len(images) < 2:
+        return False
     base_shape = images[0][0].shape[:3]
     for img in images:
         data, _ = img
