087b1294b76a60c62a3029130a4dc41d8f4c262c
Gabriel Girard <girard.gabriel@gmail.com>
2023-10-22 16:27:35 -0400

    OPT - changed PTT pmf function call to _c

--- dipy/direction/ptt_direction_getter.pyx
+++ dipy/direction/ptt_direction_getter.pyx
@@ -174,7 +174,7 @@ cdef class PTTDirectionGetter(ProbabilisticDirectionGetter):
                                    * sin(count * self.angular_separation)
                                    * self.inv_voxel_size[i])
 
-                self.last_val += self.pmf_gen.get_pmf_value(position,
+                self.last_val += self.pmf_gen.get_pmf_value_c(position,
                                                             self.frame[0])
 
 
@@ -269,7 +269,7 @@ cdef class PTTDirectionGetter(ProbabilisticDirectionGetter):
                 copy_point(&binormal[0], &frame[2][0])
 
             if self.probe_count == 1:
-                fod_amp = self.pmf_gen.get_pmf_value(position, tangent)
+                fod_amp = self.pmf_gen.get_pmf_value_c(position, tangent)
                 fod_amp = fod_amp if fod_amp > self.pmf_threshold else 0
                 self.last_val_cand = fod_amp
                 likelihood += self.last_val_cand
@@ -291,7 +291,7 @@ cdef class PTTDirectionGetter(ProbabilisticDirectionGetter):
                                            + binormal[i] * self.probe_radius
                                            * sin(c * self.angular_separation)
                                            * self.inv_voxel_size[i])
-                    fod_amp = self.pmf_gen.get_pmf_value(new_position, tangent)
+                    fod_amp = self.pmf_gen.get_pmf_value_c(new_position, tangent)
                     fod_amp = fod_amp if fod_amp > self.pmf_threshold else 0
                     self.last_val_cand += fod_amp
 
