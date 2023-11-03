529aa01b328d9925fd291538fb8b4818d9ede613
Jong Sung Park <jp109@antelope.luddy.indiana.edu>
2023-11-03 11:31:05 -0400

    NF: Updating EVAC+ model and adding util function
    This commit changes the EVAC+ model and its default weights for better
    brain extraction output. It also adds an option to only select the
    largest foreground and remove its holes, so that most prediction noises
    can be removed. Slice wise filling holes were added as well for more
    desired results. Utils functions have changed to use voxsize, and tests
    have changed accordingly as well.

--- dipy/nn/tests/test_utils.py
+++ dipy/nn/tests/test_utils.py
@@ -1,14 +1,22 @@
 import numpy as np
 from dipy.nn.utils import normalize, unnormalize, transform_img, recover_img
+from dipy.testing.decorators import set_random_number_generator
 
-def test_norm():
-    temp = np.random.random((8, 8, 8)) * 10
+
+@set_random_number_generator()
+def test_norm(rng=None):
+    temp = rng.random((8, 8, 8)) * 10
     temp2 = normalize(temp)
     temp2 = unnormalize(temp2, -1, 1, 0, 10)
     np.testing.assert_almost_equal(temp, temp2, 1)
 
-def test_transform():
-    temp = np.random.random((30, 31, 32))
-    temp2, new_affine = transform_img(temp, np.eye(4), (32, 32, 32))
-    temp2 = recover_img(temp2, new_affine, temp.shape)
+
+@set_random_number_generator()
+def test_transform(rng=None):
+    temp = rng.random((30, 31, 32))
+    temp2, new_affine, ori_shape = transform_img(temp, np.eye(4),
+                                                 init_shape=(32, 32, 32),
+                                                 voxsize=np.ones(3)*2)
+    temp2 = recover_img(temp2, new_affine, ori_shape, temp.shape,
+                        init_shape=(32, 32, 32), voxsize=np.ones(3)*2)
     np.testing.assert_almost_equal(np.array(temp.shape), np.array(temp2.shape))
