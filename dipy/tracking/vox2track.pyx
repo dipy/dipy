5f43d4e85f6276cfde2e1b8a40a160b7c9b72808
Serge Koudoro <skab12@gmail.com>
2023-11-28 10:45:47 -0500

    [FIX] int_t to npy_intp

--- dipy/tracking/vox2track.pyx
+++ dipy/tracking/vox2track.pyx
@@ -123,7 +123,7 @@ def streamline_mapping(streamlines, affine=None,
 
     """
     cdef:
-        cnp.int_t[:, :] voxel_indices
+        cnp.npy_intp[:, :] voxel_indices
 
     lin, offset = _mapping_to_voxel(affine)
     if mapping_as_streamlines:
@@ -398,7 +398,7 @@ def track_counts(tracks, vol_dims, vox_sizes=(1,1,1), return_elements=True):
     vox_sizes = np.asarray(vox_sizes).astype(np.double)
     n_voxels = np.prod(vol_dims)
     # output track counts array, flattened
-    cdef cnp.ndarray[cnp.int_t, ndim=1] tcs = \
+    cdef cnp.ndarray[cnp.npy_intp, ndim=1] tcs = \
         np.zeros((n_voxels,), dtype=int)
     # pointer to output track indices
     cdef cnp.npy_intp i
