21a3388d7bb257fe20f1529898770d1be610595e
Serge Koudoro <skab12@gmail.com>
2024-01-25 00:48:09 -0500

    [TEST] adds support of cython for pytest

--- dipy/conftest.py
+++ dipy/conftest.py
@@ -1,7 +1,10 @@
 """pytest initialization."""
+import importlib
+import warnings
+
 import numpy as np
 from packaging.version import Version
-import warnings
+import pytest
 
 """ Set numpy print options to "legacy" for new versions of numpy
  If imported into a file, pytest will run this before any doctests.
@@ -19,3 +22,48 @@ warnings.simplefilter(action="default", category=FutureWarning)
 warnings.simplefilter("always", category=UserWarning)
 # List of files that pytest should ignore
 collect_ignore = ["testing/decorators.py", "bench*.py", "**/benchmarks/*"]
+
+
+def pytest_collect_file(parent, file_path):
+    if file_path.suffix in [".pyx", ".so"] and  \
+       file_path.name.startswith("test"):
+        return PyxFile.from_parent(parent, path=file_path)
+
+
+class PyxFile(pytest.File):
+    def collect(self):
+        try:
+            mod_name = self.path.stem.split('.')[0]
+            base = self.parent.module.__name__
+            mod = importlib.import_module(f'{base}.{mod_name}')
+            for name in dir(mod):
+                item = getattr(mod, name)
+                if callable(item) and name.startswith("test_"):
+                    yield PyxItem.from_parent(self, name=name, test_func=item,
+                                              mod=mod)
+        except ImportError:
+            msg = (f"Import failed for {self.path}. Make sure you cython file "
+                   "has been compiled.")
+            raise PyxException(msg, self.path, 0)
+
+
+class PyxItem(pytest.Item):
+    def __init__(self, *, test_func, mod, **kwargs):
+        super().__init__(**kwargs)
+        self.mod = mod
+        self.test_func = test_func
+
+    def runtest(self):
+        """Called to execute the test item."""
+        self.test_func()
+
+    def repr_failure(self, excinfo):
+        """Called when self.runtest() raises an exception."""
+        return excinfo.value.args[0]
+
+    def reportinfo(self):
+        return self.path, 0, f"test: {self.name}"
+
+
+class PyxException(Exception):
+    """Custom exception for error reporting."""
