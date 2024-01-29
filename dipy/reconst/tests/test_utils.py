2db7c64791bc4998b022a58a2e92c315bee0b2fc
Serge Koudoro <skab12@gmail.com>
2024-01-29 16:30:37 -0500

    [NF] add function and cli to convert tensor in different format

--- dipy/reconst/tests/test_utils.py
+++ dipy/reconst/tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.testing as npt
-from dipy.reconst.utils import _roi_in_volume, _mask_from_roi
+from dipy.reconst.utils import _roi_in_volume, _mask_from_roi, convert_tensors
 
 
 def test_roi_in_volume():
@@ -50,3 +50,41 @@ def test_mask_from_roi():
     mask_gt[2, 0:5, 0:5] = 1
     roi_mask = _mask_from_roi(data_shape, roi_center, roi_radii)
     npt.assert_array_equal(roi_mask, mask_gt)
+
+
+def test_convert_tensor():
+    # Test case 1: Convert from 'dipy' to 'mrtrix'
+    tensor = np.array([[[[1, 2, 3, 4, 5, 6]]]])
+    converted_tensor = convert_tensors(tensor, 'dipy', 'mrtrix')
+    expected_tensor = np.array([[[[1, 3, 6, 2, 4, 5]]]])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
+
+    # Test case 2: Convert from 'mrtrix' to 'ants'
+    tensor = np.array([[[[1, 2, 3, 4, 5, 6]]]])
+    converted_tensor = convert_tensors(tensor, 'mrtrix', 'ants')
+    expected_tensor = np.array([[[[[1, 4, 2, 5, 6, 3]]]]])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
+
+    # Test case 3: Convert from 'ants' to 'fsl'
+    tensor = np.array([[[[1, 2, 3, 4, 5, 6]]]])
+    converted_tensor = convert_tensors(tensor, 'ants', 'fsl')
+    expected_tensor = np.array([[[[1, 2, 4, 3, 5, 6]]]])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
+
+    # Test case 4: Convert from 'fsl' to 'dipy'
+    tensor = np.array([[[[1, 2, 3, 4, 5, 6]]]])
+    converted_tensor = convert_tensors(tensor, 'fsl', 'dipy')
+    expected_tensor = np.array([[[[1, 2, 4, 3, 5, 6]]]])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
+
+    # Test case 5: Convert from 'dipy' to 'ants'
+    tensor = np.array([[[[1, 2, 3, 4, 5, 6]]]])
+    converted_tensor = convert_tensors(tensor, 'dipy', 'ants')
+    expected_tensor = np.array([[[[[1, 2, 3, 4, 5, 6]]]]])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
+
+    # Test case 6: Convert from 'ants' to 'dipy'
+    tensor = np.array([[[[[1, 2, 3, 4, 5, 6]]]]])
+    converted_tensor = convert_tensors(tensor, 'ants', 'dipy')
+    expected_tensor = np.array([1, 2, 3, 4, 5, 6])
+    npt.assert_array_equal(converted_tensor, expected_tensor)
