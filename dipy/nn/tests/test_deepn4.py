32fb8d480dc121f0b4495ce4985eb1a59bc85b40
praitayini <praitayini@gmail.com>
2024-02-24 19:25:57 -0600

    Implement deep learning N4 correction scripts and tests
    - Added deepn4.py for model inference
    - Created test case in test_deepn4.py
    - Updated fetcher.py to support deepn4 wights and inputs/outputs
    - Ensured PEP8 style compliance

---
+++ dipy/nn/tests/test_deepn4.py
@@ -0,0 +1,40 @@
+import pytest
+
+from dipy.data import get_fnames
+from dipy.utils.optpkg import optional_package
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+tf, have_tf, _ = optional_package('tensorflow', min_version='2.0.0')
+tfa, have_tfa, _ = optional_package('tensorflow_addons')
+
+if have_tf and have_tfa:
+    from dipy.nn.deepn4 import DeepN4
+
+
+@pytest.mark.skipif(not all([have_tf, have_tfa]), reason='Requires TensorFlow \
+                                                          , TensorFlow_addons')
+def test_default_weights():
+    file_names = get_fnames('deepn4_test_data')
+    input_arr = np.load(file_names[0])['img']
+    input_affine_arr = np.load(file_names[0])['affine']
+    target_arr = np.load(file_names[1])['corr']
+
+    deepn4_model = DeepN4()
+    deepn4_model.fetch_default_weights()
+    results_arr = deepn4_model.predict(input_arr, input_affine_arr)
+    assert_almost_equal(results_arr, target_arr, decimal=1)
+
+
+@pytest.mark.skipif(not all([have_tf, have_tfa]), reason='Requires TensorFlow \
+                                                          , TensorFlow_addons')
+def test_default_weights_batch():
+    file_names = get_fnames('deepn4_test_data')
+    input_arr = np.load(file_names[0])['img']
+    input_affine_arr = np.load(file_names[0])['affine']
+    target_arr = np.load(file_names[1])['corr']
+
+    deepn4_model = DeepN4()
+    deepn4_model.fetch_default_weights()
+    results_arr = deepn4_model.predict(input_arr, input_affine_arr)
+    assert_almost_equal(results_arr, target_arr, decimal=1)
