24e43e08b7e901d132fba8a5fb967d83291a6679
Serge Koudoro <skab12@gmail.com>
2024-03-09 00:34:28 -0500

    bump minor version

--- tools/version_utils.py
+++ tools/version_utils.py
@@ -20,8 +20,8 @@ import argparse
 
 MAJOR = 1
 MINOR = 9
-MICRO = 0
-ISRELEASED = True  # False
+MICRO = 1
+ISRELEASED = False
 IS_RELEASE_BRANCH = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
