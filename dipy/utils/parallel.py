9e1aa54913a0215b09bf2794d7e68757ad690d9d
Asa Gilmore <asaagilmore@gmail.com>
2024-03-27 14:46:27 -0700

    added back dependencies, use tempfile for ray spill

--- dipy/utils/parallel.py
+++ dipy/utils/parallel.py
@@ -3,14 +3,16 @@ import shutil
 import numpy as np
 from dipy.utils.optpkg import optional_package
 import json
+import tempfile
+import multiprocessing
 
-joblib, has_joblib, _ = optional_package("joblib")
-dask, has_dask, _ = optional_package("dask")
-ray, has_ray, _ = optional_package("ray")
+ray, has_ray, _ = optional_package('ray')
+joblib, has_joblib, _ = optional_package('joblib')
+dask, has_dask, _ = optional_package('dask')
 
 
 def paramap(func, in_list, out_shape=None, n_jobs=-1, engine="ray",
-            backend=None, func_args=None, func_kwargs=None,
+            backend=None, func_args=None, clean_spill=True, func_kwargs=None,
             **kwargs):
     """
     Maps a function to a list of inputs in parallel.
@@ -37,13 +39,17 @@ def paramap(func, in_list, out_shape=None, n_jobs=-1, engine="ray",
     backend : str, optional
         What joblib or dask backend to use. For joblib, the default is "loky".
         For dask the default is "threading".
+    clean_spill : bool, optional
+        If True, clean up the spill directory after the computation is done.
+        Only applies to "ray" engine. Otherwise has no effect.
+        Default: True.
     func_args : list, optional
         Positional arguments to `func`.
     func_kwargs : dict, optional
         Keyword arguments to `func`.
     kwargs : dict, optional
         Additional arguments to pass to either joblib.Parallel
-        or dask.compute depending on the engine used.
+        or dask.compute, or ray.remote depending on the engine used.
         Default: {}
 
     Returns
@@ -94,29 +100,23 @@ def paramap(func, in_list, out_shape=None, n_jobs=-1, engine="ray",
         if not has_ray:
             raise ray()
 
-        if not ray.is_initialized():
-            ray.init(_system_config={
-                "object_spilling_config": json.dumps(
-                    {"type": "filesystem", "params": {"directory_path":
-                     "/tmp/spill"}},
-                )
-            },)
+        if clean_spill:
+            tmp_dir = tempfile.TemporaryDirectory()
 
-        func = ray.remote(func)
+            if not ray.is_initialized():
+                ray.init(_system_config={
+                    "object_spilling_config": json.dumps(
+                        {"type": "filesystem", "params": {"directory_path":
+                         tmp_dir.name}},
+                    )
+                },)
+
+        func = ray.remote(func, **kwargs)
         results = ray.get([func.remote(ii, *func_args, **func_kwargs)
                           for ii in in_list])
 
-        clean_spill = kwargs.get('clean_spill', False)
         if clean_spill:
-            for filename in os.listdir("/tmp/spill"):
-                file_path = os.path.join("/tmp/spill", filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-                except Exception as e:
-                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+            shutil.rmtree(tmp_dir.name)
 
     elif engine == "serial":
         results = []
