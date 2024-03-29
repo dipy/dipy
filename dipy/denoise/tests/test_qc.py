my assumption about masking was wrong

dipy/denoise/tests/test_qc.py
@@ -101,10 +101,8 @@ def test_neighboring_dwi_correlation():
     estimated_ndc = neighboring_dwi_correlation(dwi_data, gtab, mask)
     assert np.allclose(real_r, estimated_ndc)
 
-    # Test with the same data but without a mask:
-    #   the zero padding should increase NDC
     maskless_ndc = neighboring_dwi_correlation(dwi_data, gtab)
-    assert maskless_ndc > real_r
+    assert maskless_ndc != real_r
 
     # Try with no b=0s
     real_r, dwi_data, mask, gtab = create_test_data(