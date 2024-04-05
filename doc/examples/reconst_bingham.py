13b7c2dbbfd6216794a54a79b1cbdc5e66394fc9
Julio Villalon <neurobarranquilla@hotmail.com>
2024-04-04 20:21:05 -0700

    Small changes to the example.

--- doc/examples/reconst_bingham.py
+++ doc/examples/reconst_bingham.py
@@ -7,8 +7,8 @@ This example shows how to recontruct Bingham functions from orientation
 distribution functions (ODFs). Reconstructed Bingham functions can be useful to
 quantify properties from ODFs such as fiber dispersion [1]_,[2]_.
 
-To begin, let's import the relevant functions and load a data consisting of 10
-b0s and 150 non-b0s with a b-value of 2000.
+To begin, let us import the relevant functions and load a data consisting of 10
+b0s and 150 non-b0s with a b-value of 2000s/mm2.
 """
 
 import matplotlib.pyplot as plt
@@ -33,30 +33,30 @@ gtab = gradient_table(bvals, bvecs)
 # To properly fit Bingham functions, we recommend the use of a larger number of
 # directions to sample the ODFs. For this, we load a `sphere` class instance
 # containing 724 directions sampling a 3D sphere. We further subdivide the
-# faces of this `sphere` representation in 2, to get 11554 direction
+# faces of this `sphere` representation into 2, to get 11554 directions.
 
 sphere = get_sphere('repulsion724')
 sphere = sphere.subdivide(2)
 
 nd = sphere.vertices.shape[0]
-print('The number of directions in the sphere is {}'.format(nd))
+print('The number of directions on the sphere is {}'.format(nd))
 
 ###############################################################################
 # Step 1. ODF estimation
 # =================================================
 #
-# Before fitting Bingham functions, we need to reconstruct orientation
+# Before fitting Bingham functions, we must reconstruct orientation
 # distribution functions. In this example, ODFs will be reconstructed using
 # the Constrained Spherical Deconvolution (CSD) method [3]_.
 # In the main tutorial of CSD (see
 # :ref:`sphx_glr_examples_built_reconstruction_reconst_csd.py`), several
 # strategies to define the fiber response function are discussed. Here, for
-# the sake of simplicity, the use the response function estimates from a local
-# brain regions:
+# the sake of simplicity, we will use the response function estimates from a
+# local brain regions:
 
 response, ratio = auto_response_ssst(gtab, data, roi_radii=10, fa_thr=0.7)
 
-# Let's now compute the ODFs using this response function:
+# Let us now compute the ODFs using this response function:
 
 csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order_max=8)
 
@@ -67,7 +67,7 @@ data_small = data[20:50, 55:85, 38:39]
 csd_fit = csd_model.fit(data_small)
 
 ###############################################################################
-# Let's visualize the odfs
+# Let us visualize the ODFs
 
 csd_odf = csd_fit.odf(sphere)
 
@@ -79,7 +79,7 @@ fodf_spheres = actor.odf_slicer(csd_odf, sphere=sphere, scale=0.9,
                                 norm=False, colormap='plasma')
 scene.add(fodf_spheres)
 
-print('Saving illustration as csd_odfs.png')
+print('Saving the illustration as csd_odfs.png')
 window.record(scene, out_path='csd_odfs.png', size=(600, 600))
 if interactive:
     window.show(scene)
@@ -92,7 +92,7 @@ if interactive:
 #
 # Step 2. Bingham fitting and Metrics
 # =================================================
-# Now that we have some ODFs, let's fit the Bingham functions to them by using
+# Now that we have some ODFs, let us fit the Bingham functions to them by using
 # the function `bingham_from_odf`:
 
 BinghamMetrics = bingham_from_odf(csd_odf, sphere)
@@ -110,7 +110,7 @@ fodf_spheres = actor.odf_slicer(bim_odf, sphere=sphere, scale=0.9,
                                 norm=False, colormap='plasma')
 scene.add(fodf_spheres)
 
-print('Saving illustration as Bingham_odfs.png')
+print('Saving the illustration as Bingham_odfs.png')
 window.record(scene, out_path='Bingham_odfs.png', size=(600, 600))
 if interactive:
     window.show(scene)
@@ -118,7 +118,7 @@ if interactive:
 ###############################################################################
 # .. rst-class:: centered small fst-italic fw-semibold
 #
-# Bingham functions fitted to CSD's ODFs.
+# Bingham functions fitted to CSD fiber ODFs.
 #
 # Alternatively to fit Bingham functions to sampled ODFs, DIPY also contains
 # the function `bingham_from_sh` to perform Bingham fitting from the ODF's
@@ -143,7 +143,7 @@ BinghamMetrics = bingham_from_sh(sh_coeff, sphere, 8)
 
 FD_ODF_l1 = BinghamMetrics.fd[:, :, 0, 0]
 FD_ODF_l2 = BinghamMetrics.fd[:, :, 0, 1]
-FD_total = BinghamMetrics.tfd[:, :, 0]
+FD_total = BinghamMetrics.gfd[:, :, 0]
 
 fig1, ax = plt.subplots(1, 3, figsize=(16, 4))
 
@@ -154,7 +154,7 @@ im1 = ax[1].imshow(FD_ODF_l2[:, -1:1:-1].T, vmin=0, vmax=2)
 ax[1].set_title('FD ODF lobe 2')
 
 im2 = ax[2].imshow(FD_total[:, -1:1:-1].T, vmin=0, vmax=2)
-ax[2].set_title('FD total')
+ax[2].set_title('FD ODF global')
 
 fig1.colorbar(im0, ax=ax[0])
 fig1.colorbar(im1, ax=ax[1])
@@ -190,13 +190,13 @@ ODI2[FD_total < 0.5] = 0
 fig2, ax = plt.subplots(1, 3, figsize=(15, 5))
 
 im0 = ax[0].imshow(ODI1[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[0].set_title('ODI1 (lobe 1)')
+ax[0].set_title('ODI_1 (lobe 1)')
 
 im1 = ax[1].imshow(ODI2[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[1].set_title('ODI2  (lobe 1)')
+ax[1].set_title('ODI_2  (lobe 1)')
 
 im2 = ax[2].imshow(ODIt[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[2].set_title('ODIt  (lobe 1)')
+ax[2].set_title('ODI_total  (lobe 1)')
 
 fig2.colorbar(im0, ax=ax[0])
 fig2.colorbar(im1, ax=ax[1])
@@ -206,10 +206,11 @@ fig2.colorbar(im2, ax=ax[2])
 # .. rst-class:: centered small fst-italic fw-semibold
 #
 # The figure shows from left to right: 1) ODI of the largest ODF lobe along
-# the Axis with Greater Dispersion (direction in which fibers exhibit the most
-# variability in orientation); 2) ODI of the largest ODF lobe along the Axis
-# with Lesser Dispersion (directions in which fiber orientations are more
-# uniform); and 3) Average ODI of the largest ODF lobe across noth axes.
+# the axis with greater dispersion (direction in which fibers exhibit the most
+# variability in orientation); 2) ODI of the largest ODF lobe along the axis
+# with lesser dispersion (directions in which fiber orientations are more
+# uniform); and 3) Average or total ODI of the largest ODF lobe across both
+# axes.
 #
 # Above, we focused on the largest ODF's lobe, representing the most pronounced
 # fiber population within a voxel. However, this methodology is not limited to
@@ -230,13 +231,13 @@ ODI2[FD_total < 0.5] = 0
 fig3, ax = plt.subplots(1, 3, figsize=(15, 5))
 
 im0 = ax[0].imshow(ODI1[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[0].set_title('ODI1 (lobe 2)')
+ax[0].set_title('ODI_1 (lobe 2)')
 
 im1 = ax[1].imshow(ODI2[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[1].set_title('ODI2  (lobe 2)')
+ax[1].set_title('ODI_2  (lobe 2)')
 
 im2 = ax[2].imshow(ODIt[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[2].set_title('ODIt  (lobe 2)')
+ax[2].set_title('ODI_total  (lobe 2)')
 
 fig3.colorbar(im0, ax=ax[0])
 fig3.colorbar(im1, ax=ax[1])
@@ -246,18 +247,18 @@ fig3.colorbar(im2, ax=ax[2])
 # .. rst-class:: centered small fst-italic fw-semibold
 #
 # The figure shows from left to right: 1) ODI of the second-largest ODF lobe
-# along the Axis with Greater Dispersion (direction in which fibers exhibit the
+# along the axis with greater dispersion (direction in which fibers exhibit the
 # most variability in orientation); 2) ODI of the second-largest ODF lobe along
-# the Axis with Lesser Dispersion (directions in which fiber orientations are
+# the axis with lesser dispersion (directions in which fiber orientations are
 # more uniform); and 3) Average ODI of the second-largest ODF lobe across noth
 # axes. In this figure, regions of white matter that contain only a single
 # fiber  population display ODI estimates of zero, corresponding to ODF
 # profiles lacking a second ODF lobe.
 #
-# BinghamMetric can also be used to compute the averaged ODI quantities across
-# all ODF lobes (see below). The averaged quantitaties are computed by
-# weigthing each ODF lobe with their respective FD value. These quantities
-# are plotted in the following figure.
+# BinghamMetrics can also be used to compute the average ODI quantities across
+# all ODF lobes aka. al global ODI (see below). The average quantitaties are
+# computed by weigthing each ODF lobe with their respective FD value.
+# These quantities are plotted in the following figure.
 
 ODIt = BinghamMetrics.godi_total[:, :, 0]
 ODI1 = BinghamMetrics.godi_1[:, :, 0]
@@ -270,13 +271,13 @@ ODI2[FD_total < 0.5] = 0
 fig4, ax = plt.subplots(1, 3, figsize=(15, 5))
 
 im0 = ax[0].imshow(ODI1[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[0].set_title('ODI1 (global)')
+ax[0].set_title('ODI_1 (global)')
 
 im1 = ax[1].imshow(ODI2[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[1].set_title('ODI2  (global)')
+ax[1].set_title('ODI_2  (global)')
 
 im2 = ax[2].imshow(ODIt[:, -1:1:-1].T, vmin=0, vmax=0.2)
-ax[2].set_title('ODIt  (global)')
+ax[2].set_title('ODI_total  (global)')
 
 fig4.colorbar(im0, ax=ax[0])
 fig4.colorbar(im1, ax=ax[1])
@@ -285,9 +286,9 @@ fig4.colorbar(im2, ax=ax[2])
 ###############################################################################
 # .. rst-class:: centered small fst-italic fw-semibold
 #
-# The figure shows from left to right: 1) weighted-averaged ODI1 along all ODF
-# lobes; 2) weighted-averaged ODI2 along all ODF lobe; 3) weighted-averaged
-# ODI_total along all ODF lobes.
+# The figure shows from left to right: 1) weighted-averaged ODI_1 along all ODF
+# lobes; 2) weighted-averaged ODI_2 along all ODF lobe; 3) weighted-average
+# ODI_total across all ODF lobes.
 #
 # References
 # ----------
