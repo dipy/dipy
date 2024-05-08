8a5b653fbc112e8dc2e2adbc8f7f814233849fff
Gabriel Girard <girard.gabriel@gmail.com>
2024-05-08 11:37:12 -0400

    fixed rebase

--- dipy/direction/peaks.py
+++ dipy/direction/peaks.py
@@ -671,7 +671,7 @@ def reshape_peaks_for_visualization(peaks):
     if isinstance(peaks, PeaksAndMetrics):
         peaks = peaks.peak_dirs
 
-    return peaks.reshape(np.append(peaks.shape[:-2], -1)).astype('float32')
+    return peaks.reshape(np.append(peaks.shape[:-2], -1)).astype("float32")
 
 
 def peaks_from_positions(
@@ -741,4 +741,3 @@ def peaks_from_positions(
         peaks_arr[i, :nbr_peaks, :] = peaks[:nbr_peaks, :]
 
     return peaks_arr
-
