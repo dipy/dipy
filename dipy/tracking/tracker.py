8623e75223bd2f5e9f9742daf69564e5a82744c0
Gabriel Girard <girard.gabriel@gmail.com>
2024-07-18 14:25:13 -0400

    DOC

--- dipy/tracking/tracker.py
+++ dipy/tracking/tracker.py
@@ -12,7 +12,9 @@ def deterministic_directions():
 def eudx_directions():
     pass
 
-def build_stopping_criterion(mask=None, check_point=None, include=None, exclude=None):
+
+def build_stopping_criterion(mask=None, check_point=None, include=None,
+                             exclude=None):
     sc_element = {
         "mask": mask,
         "wm_map": None,
@@ -20,34 +22,39 @@ def build_stopping_criterion(mask=None, check_point=None, include=None, exclude=
         "csf_map": None,
         "checkpoint": check_point,
         "include": include,
-        "exclude": exclude
+        "exclude": exclude,
     }
     return sc_element
-
 
 
 def generate_streamlines():
     pass
 
 
-def generate_tractogram_py(seeds, streamline_generator=generate_streamlines, postprocess_streamline=None):
+def generate_tractogram_py(seeds, streamline_generator=generate_streamlines,
+                           postprocess_streamline=None):
     for s in seeds:
-        streamline  = streamline_generator(s, stopping_criterion, direction_getter, max_cross, step_size, maxlen, fixedstep)
+        streamline = streamline_generator(s, stopping_criterion,
+                                          direction_getter, max_cross,
+                                          step_size, maxlen, fixedstep)
         if postprocess_streamline is not None:
-            streamline  = postprocess_streamline(streamlines)
+            streamline = postprocess_streamline(streamlines)
         yield streamline
 
 
-def build_tractogram_algorithm(streamline_generator, tracker_func, pmf_gen_func,
-                               stopping_criterion_func, postprocess_streamline=None):
+def build_tractogram_algorithm(streamline_generator, tracker_func,
+                               pmf_gen_func, stopping_criterion_func,
+                               postprocess_streamline=None):
 
-    def generate_tractogram(seed_positions, seed_directions, tracker_parameters):
+    def generate_tractogram(seed_positions, seed_directions,
+                            tracker_parameters):
         for s in len(seed_positions):
-            streamline  = streamline_generator(seed_positions[i], seed_directions[i],
-                                               tracker_func, pmf_gen_func,
-                                               stopping_criterion_func)
+            streamline = streamline_generator(seed_positions[i],
+                                              seed_directions[i],
+                                              tracker_func, pmf_gen_func,
+                                              stopping_criterion_func)
             if postprocess_streamline is not None:
-                streamline  = postprocess_streamline(streamlines)
+                streamline = postprocess_streamline(streamlines)
             yield streamline
 
     return generate_tractogram
@@ -63,23 +70,24 @@ def local_tracking(seed_positons, seed_directions, *,
                                            buffer_prec=buffer_perc):
             yield streamlines
 
-    return generate_tractogram_py(seed_positons, seed_directions, tracker_parameters)
+    return generate_tractogram_py(seed_positons, seed_directions,
+                                  tracker_parameters)
 
 
-probabilistic_tracking = partial(local_tracking, tractogram_func=generate_tractogram)
-probabilistic_tracking_python = partial(local_tracking, tractogram_func=generate_tractogram_py)
+probabilistic_tracking = partial(local_tracking,
+                                 tractogram_func=generate_tractogram)
+probabilistic_tracking_python = partial(local_tracking,
+                                        tractogram_func=generate_tractogram_py)
 
-deterministic_tracking = partial(local_tracking, direction_getter='deterministic')
-eudx_tracking = partial(local_tracking, direction_getter='eudx')
-fact_tracking = partial(local_tracking, direction_getter='fact')
-ptt_tracking = partial(local_tracking, direction_getter='ptt')
-pft_tracking = partial(local_tracking, direction_getter='pft')
-
-
-def test_probabilistic_tracking():
-    pass
+deterministic_tracking = partial(
+    local_tracking, direction_getter="deterministic")
+eudx_tracking = partial(local_tracking, direction_getter="eudx")
+fact_tracking = partial(local_tracking, direction_getter="fact")
+ptt_tracking = partial(local_tracking, direction_getter="ptt")
+pft_tracking = partial(local_tracking, direction_getter="pft")
 
 
 if __name__ == "__main__":
     my_generate_tractogram = build_tractogram_algorithm()
-    streamlines = local_tracking(p_seed, d_seed, params, tractogram_func=my_generate_tractogram)
+    streamlines = local_tracking(
+        p_seed, d_seed, params, tractogram_func=my_generate_tractogram)
