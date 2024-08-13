ec4d5431aed4ece582f8affcd1c8a270ac655c9d
Ariel Rokem <arokem@gmail.com>
2024-08-13 13:59:38 -0700

    Test that parallelization of reconst can take sequences of kwargs.

--- dipy/reconst/tests/test_multi_voxel.py
+++ dipy/reconst/tests/test_multi_voxel.py
@@ -194,27 +194,35 @@ def test_multi_voxel_fit(rng):
     fit = model.fit(many_voxels, another_kwarg="foo", engine="serial")
 
     for verbose in [True, False]:
-        # If parallelization engines are installed use them to test:
-        if has_joblib:
-            fit = model.fit(
-                many_voxels,
-                verbose=verbose,
-                another_kwarg="foo",
-                engine="joblib",
-            )
-            npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
+        # Test with single value kwarg, or sequence of kwarg values
+        for another_kwarg in ["foo", len(many_voxels) * ["foo"]]:
+            # If parallelization engines are installed use them to test:
+            if has_joblib:
+                fit = model.fit(
+                    many_voxels,
+                    verbose=verbose,
+                    another_kwarg=another_kwarg,
+                    engine="joblib",
+                )
+                npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
 
-        if has_dask:
-            fit = model.fit(
-                many_voxels, verbose=verbose, another_kwarg="foo", engine="dask"
-            )
-            npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
+            if has_dask:
+                fit = model.fit(
+                    many_voxels,
+                    verbose=verbose,
+                    another_kwarg=another_kwarg,
+                    engine="dask",
+                )
+                npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
 
-        if has_ray:
-            fit = model.fit(
-                many_voxels, verbose=verbose, another_kwarg="foo", engine="ray"
-            )
-            npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
+            if has_ray:
+                fit = model.fit(
+                    many_voxels,
+                    verbose=verbose,
+                    another_kwarg=another_kwarg,
+                    engine="ray",
+                )
+                npt.assert_equal(fit.predict(S0=S0), np.ones(many_voxels.shape) * S0)
 
     # Test with a mask
     mask = np.zeros((3, 3, 3)).astype("bool")
