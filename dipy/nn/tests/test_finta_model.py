c55a65035a5206a3ceeb8a7c25de2d3be437329d
itellaetxe <inigotellaetxe@gmail.com>
2024-08-22 11:32:45 +0200

    TEST+STYLE: Adds more tests to FINTA AE and reformats code
    STYLE -> Default values removed from docstrings; Raw docstrings removed; Removed single quotes in favor of double quotes; Improves explanation of Encoder and Decoder blocks; Limits lines to 88 chars; Adds missing docstrings to from_config and get_config methods; Includes FINTA references in Encoder, Decoder, and AutoEncoder class; TEST-> Adds tests to ReflectionPadding1D layer, and tests to control for initialization valuess.

--- dipy/nn/tests/test_finta_model.py
+++ dipy/nn/tests/test_finta_model.py
@@ -14,20 +14,21 @@ if have_tf:
     from dipy.nn.finta_model import Encoder, Decoder
     from dipy.nn.finta_model import (
         IncrFeatStridedConvFCUpsampReflectPadAE as AE)
+    from keras.layers import Layer
+    from keras import Sequential
 
 
 class DummyData():
-    def __init__(self, latent_space_dims: int = 32):
-        """
-        Generate three streamlines, aligned with the x, y, and z axes. Each streamline has 256 points.
-        Generate a latent vector of the given size.
+    def __init__(self, latent_space_dims=32):
+        """Generate three streamlines, aligned with the x, y, and z axes. Each
+        streamline has 256 points. Generate a latent vector of the input size.
 
         Parameters
         ----------
         latent_space_dims : int, optional
             Number of dimensions of the latent space vector
-            Default: 32
         """
+
         streamlines = []
         latent_vectors = []
         for i in range(3):
@@ -43,7 +44,6 @@ class DummyData():
             latent_vectors.append(latent_vector)
 
         self.streamlines = np.concatenate(streamlines, axis=0)
-
         self.latent_vectors = np.concatenate(latent_vectors, axis=0)
 
 
@@ -57,12 +57,51 @@ def test_encoder_parameter_initialization():
     assert test_encoder.latent_space_dims == 10
     assert test_encoder.kernel_size == 3
     k_weight_init = np.sqrt(1 / (3 * test_encoder.kernel_size))
-    npt.assert_almost_equal(test_encoder.k_conv1d_weights_initializer,
+    npt.assert_almost_equal(test_encoder.k_conv1d_weight_init,
                             k_weight_init, decimal=5)
 
 
 @pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
-def test_encoder_call():
+def test_encoder_decoder_wrong_kernel_size():
+    with pytest.raises(ValueError) as excinfo:
+        Encoder(latent_space_dims=10, kernel_size=6)
+
+    assert str(excinfo.value) == "Kernel size must be between 1 and 5."
+    assert excinfo.type == ValueError
+
+    with pytest.raises(ValueError) as excinfo:
+        Decoder(encoder_out_size=[8, 1024], kernel_size=6)
+
+    assert str(excinfo.value) == "Kernel size must be between 1 and 5."
+    assert excinfo.type == ValueError
+
+
+@pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
+def test_encoder_ae_wrong_streamline_size_call():
+    bad_streamlines = np.zeros((10, 128, 3))
+
+    # Instantiate the Encoder and the AE
+    test_encoder = Encoder(latent_space_dims=10, kernel_size=3)
+    test_ae = AE(latent_space_dims=10, kernel_size=3)
+
+    # Encoder
+    with pytest.raises(ValueError) as excinfo:
+        test_encoder(bad_streamlines)
+
+    assert "Input streamlines in Encoder must have 256 points." in str(excinfo.value)
+    assert excinfo.type == ValueError
+
+    # AE
+    with pytest.raises(ValueError) as excinfo:
+        test_ae(bad_streamlines)
+
+    exc_str = str(excinfo.value)
+    assert "Input streamlines to AutoEncoder must have 256 points." in exc_str
+    assert excinfo.type == ValueError
+
+
+@pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
+def test_encoder_call_correct():
     latent_space_dims = 10
     test_encoder = Encoder(latent_space_dims=latent_space_dims)
     data = DummyData().streamlines
@@ -89,7 +128,7 @@ def test_decoder_parameter_initialization():
 
 
 @pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
-def test_decoder_call():
+def test_decoder_call_correct():
     test_decoder = Decoder(encoder_out_size=[8, 1024])
     data = DummyData(latent_space_dims=10).latent_vectors
     output = test_decoder(data)
@@ -101,7 +140,7 @@ def test_decoder_call():
     assert test_decoder.built is True
 
 
-# Test the AutoEncoder fully
+# Test the full AutoEncoder
 
 @pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
 def test_ae_calling_output_shape():
@@ -121,6 +160,29 @@ def test_ae_calling_output_shape():
 
     # Check that the autoencoder is built
     assert test_ae.built is True
+
+
+@pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
+def test_ae_fit_wrong_streamline_size():
+    bad_streamlines = np.zeros((10, 128, 3))
+
+    test_ae = AE(latent_space_dims=10, kernel_size=3)
+
+    # Compile the model
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5)
+    loss = tf.keras.losses.MeanSquaredError()
+    test_ae.compile(optimizer=optimizer, loss=loss)
+    # Fit the model
+    input_train_data = tf.convert_to_tensor(bad_streamlines)
+
+    with pytest.raises(ValueError) as excinfo:
+        test_ae.fit(x=input_train_data,
+                    y=input_train_data,
+                    batch_size=1,
+                    epochs=3)
+
+    assert excinfo.type == ValueError
+    assert "Training streamlines must have 256 points." in str(excinfo.value)
 
 
 @pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
@@ -171,3 +233,25 @@ def test_ae_train_weight_saving_and_loading():
     # Cleanup the weights and the model
     os.remove(path_weights)
     os.remove(path_model)
+
+
+# Test reflection padding layer
+@pytest.mark.skipif(not have_tf, reason="Requires TensorFlow")
+def test_pre_pad():
+    # Test passing a layer, check that the output is a keras.Sequential
+    test_layer = tf.keras.layers.Conv1D(32, 3)
+    padded_layer = utils.pre_pad(test_layer)
+
+    assert isinstance(padded_layer, Sequential)
+    # Pass an input to the padded_layer to cover the call method
+    input_data = np.random.rand(10, 256, 3)
+    padded_layer(input_data)
+
+    # Test passing a thing that is not a layer
+    with pytest.raises(ValueError) as excinfo:
+        utils.pre_pad("not_a_layer")
+
+    expected_error_str = ("Input must be an instance of keras.layers.Layer."
+                          " Pass a layer to apply ReflectionPadding1D to.")
+    assert str(excinfo.value) == expected_error_str
+    assert excinfo.type == ValueError
