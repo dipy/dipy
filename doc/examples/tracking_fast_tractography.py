b6e7748e910712b411bc3c0af6b70bccd92cdc45
Gabriel Girard <girard.gabriel@gmail.com>
2024-08-29 12:12:49 -0400

    DOC - clarified fast tracking example

--- doc/examples/tracking_fast_tractography.py
+++ doc/examples/tracking_fast_tractography.py
@@ -25,6 +25,7 @@ from dipy.core.sphere import HemiSphere
 from dipy.data import get_fnames, get_sphere
 from dipy.direction.peaks import peaks_from_positions
 from dipy.direction.pmf import SimplePmfGen
+from dipy.io.image import load_nifti, load_nifti_data
 from dipy.io.stateful_tractogram import Space, StatefulTractogram
 from dipy.io.streamline import load_tractogram, save_trk
 from dipy.reconst.shm import sh_to_sf
@@ -43,10 +44,20 @@ interactive = False
 # Prepare the synthetic DiSCo data for fast tracking. The ground-truth
 # connectome will be use to evaluate tractography performances.
 fnames = get_fnames("disco1")
-GT_connectome = np.loadtxt(fnames[35])
+disco1_fnames = [f.split("/")[-1] for f in fnames]
+
+GT_connectome_fname = fnames[disco1_fnames.index(
+    "DiSCo1_Connectivity_Matrix_Cross-Sectional_Area.txt")]
+GT_connectome = np.loadtxt(GT_connectome_fname)
+
+# select the non-zero connections of the upper triangular part of the connectome
 connectome_mask = np.tril(np.ones(GT_connectome.shape), -1) > 0
-labels_img = nib.load(fnames[23])
-labels = np.round(labels_img.get_fdata()).astype(int)
+
+# load the
+labels_fname = fnames[disco1_fnames.index("highRes_DiSCo1_ROIs.nii.gz")]
+labels, affine, labels_img = load_nifti(labels_fname, return_img=True)
+labels = labels.astype(int)
+
 GT_streams = load_tractogram(fnames[39], reference=labels_img).streamlines
 if has_fury:
     scene = window.Scene()
@@ -58,7 +69,7 @@ if has_fury:
 
 plt.imshow(GT_connectome, origin="lower",
            cmap="viridis", interpolation="nearest")
-plt.axis('off')
+plt.axis("off")
 plt.savefig("connectome_ground_truth.png")
 plt.close()
 ###############################################################################
@@ -70,8 +81,8 @@ plt.close()
 ###############################################################################
 # Prepare ODFs
 sphere = get_sphere("repulsion724")
-GT_SH_img = nib.load(fnames[20])
-GT_SH = GT_SH_img.get_fdata()
+GT_SH_fname = fnames[disco1_fnames.index("highRes_DiSCo1_Strand_ODFs.nii.gz")]
+GT_SH = load_nifti_data(GT_SH_fname)
 GT_ODF = sh_to_sf(GT_SH, sphere, sh_order_max=12,
                   basis_type="tournier07", legacy=False)
 GT_ODF[GT_ODF < 0] = 0
@@ -93,16 +104,17 @@ if has_fury:
 
 ###############################################################################
 # Tracking mask, seed positions and initial directions
-mask = nib.load(fnames[25]).get_fdata()
+mask_fname = fnames[disco1_fnames.index("highRes_DiSCo1_mask.nii.gz")]
+mask = load_nifti_data(mask_fname)
 sc = BinaryStoppingCriterion(mask)
 
-affine = nib.load(fnames[25]).affine
 voxel_size = np.ones(3)
-seed_mask = nib.load(fnames[34]).get_fdata()
+seed_fname = fnames[disco1_fnames.index("highRes_DiSCo1_ROIs-mask.nii.gz")]
+seed_mask = load_nifti_data(seed_fname)
 seed_mask = binary_erosion(seed_mask * mask, iterations=1)
 seeds_positions = random_seeds_from_mask(seed_mask,
                                          affine,
-                                         seeds_count=5000,
+                                         seeds_count=10000,
                                          seed_count_per_voxel=False)
 
 peaks = peaks_from_positions(
@@ -112,13 +124,13 @@ seeds, initial_directions = seeds_directions_pairs(
 
 plt.imshow(seed_mask[:, :, 17], origin="lower",
            cmap="gray", interpolation="nearest")
-plt.axis('off')
+plt.axis("off")
 plt.title('Seeding Mask')
 plt.savefig("seeding_mask.png")
 plt.close()
 plt.imshow(mask[:, :, 17], origin="lower",
            cmap="gray", interpolation="nearest")
-plt.axis('off')
+plt.axis("off")
 plt.title('Tracking Mask')
 plt.savefig("tracking_mask.png")
 plt.close()
@@ -129,7 +141,7 @@ plt.close()
 # DiSCo seeding (left) and tracking (right) masks.
 
 ###############################################################################
-# Perform fast deterministic tractography
+# Perform fast deterministic tractography using 1 thread (cpu)
 det_params = generate_tracking_parameters("det",
                                           max_len=500,
                                           step_size=0.2,
@@ -141,17 +153,11 @@ streamline_generator = generate_tractogram(seeds,
                                            initial_directions,
                                            sc,
                                            det_params,
-                                           pmf_gen
-                                           )
+                                           pmf_gen,
+                                           nbr_threads=1)
 det_streams = Streamlines(streamline_generator)
-sft = StatefulTractogram(det_streams, GT_SH_img, Space.RASMM)
+sft = StatefulTractogram(det_streams, labels_img, Space.RASMM)
 save_trk(sft, "tractogram_fast_deterministic.trk")
-
-# Compare the estimated connectivity with the ground-truth connectivity
-connectome = connectivity_matrix(det_streams, affine, labels)[1:, 1:]
-r, _ = pearsonr(GT_connectome[connectome_mask].flatten(),
-                connectome[connectome_mask].flatten())
-print("DiSCo ground-truth correlation (fast deterministic tractography): ", r)
 
 if has_fury:
     scene = window.Scene()
@@ -161,9 +167,14 @@ if has_fury:
     if interactive:
         window.show(scene)
 
-plt.imshow(connectome, origin="lower",
-           cmap="viridis", interpolation="nearest")
-plt.axis('off')
+# Compare the estimated connectivity with the ground-truth connectivity
+connectome = connectivity_matrix(det_streams, affine, labels)[1:, 1:]
+r, _ = pearsonr(GT_connectome[connectome_mask].flatten(),
+                connectome[connectome_mask].flatten())
+print("DiSCo ground-truth correlation (fast deterministic tractography): ", r)
+
+plt.imshow(connectome, origin="lower", cmap="viridis", interpolation="nearest")
+plt.axis("off")
 plt.savefig("connectome_deterministic.png")
 plt.close()
 ###############################################################################
@@ -173,30 +184,24 @@ plt.close()
 # DiSCo Deterministic tractogram and corresponding connectome.
 
 ###############################################################################
-# Perform fast probabilistic tractography
+# Perform fast probabilistic tractography using 4 threads (cpus)
 prob_params = generate_tracking_parameters("prob",
                                            max_len=500,
                                            step_size=0.2,
                                            voxel_size=voxel_size,
-                                           max_angle=20
-                                           )
+                                           max_angle=20)
 
 # Prepare the streamline generator
 streamline_generator = generate_tractogram(seeds,
                                            initial_directions,
                                            sc,
                                            prob_params,
-                                           pmf_gen
+                                           pmf_gen,
+                                           nbr_threads=4
                                            )
 prob_streams = Streamlines(streamline_generator)
-sft = StatefulTractogram(prob_streams, GT_SH_img, Space.RASMM)
+sft = StatefulTractogram(prob_streams, labels_img, Space.RASMM)
 save_trk(sft, "tractogram_fast_probabilistic.trk")
-
-# Compare the estimated connectivity with the ground-truth connectivity
-connectome = connectivity_matrix(prob_streams, affine, labels)[1:, 1:]
-r, _ = pearsonr(GT_connectome[connectome_mask].flatten(),
-                connectome[connectome_mask].flatten())
-print("DiSCo ground-truth correlation (fast probabilistic tractography): ", r)
 
 if has_fury:
     scene = window.Scene()
@@ -206,9 +211,14 @@ if has_fury:
     if interactive:
         window.show(scene)
 
-plt.imshow(connectome, origin="lower",
-           cmap="viridis", interpolation="nearest")
-plt.axis('off')
+# Compare the estimated connectivity with the ground-truth connectivity
+connectome = connectivity_matrix(prob_streams, affine, labels)[1:, 1:]
+r, _ = pearsonr(GT_connectome[connectome_mask].flatten(),
+                connectome[connectome_mask].flatten())
+print("DiSCo ground-truth correlation (fast probabilistic tractography): ", r)
+
+plt.imshow(connectome, origin="lower", cmap="viridis", interpolation="nearest")
+plt.axis("off")
 plt.savefig("connectome_probabilistic.png")
 plt.close()
 ###############################################################################
@@ -218,7 +228,7 @@ plt.close()
 # DiSCo Probabilistic tractogram and corresponding connectome.
 
 ###############################################################################
-# Perform fast paralle transport tractography tractography
+# Perform fast parallel transport tractography tractography using all threads (cpus)
 ptt_params = generate_tracking_parameters("ptt",
                                           max_len=500,
                                           step_size=0.2,
@@ -232,29 +242,27 @@ streamline_generator = generate_tractogram(seeds,
                                            initial_directions,
                                            sc,
                                            ptt_params,
-                                           pmf_gen
+                                           pmf_gen,
+                                           nbr_threads=0
                                            )
 ptt_streams = Streamlines(streamline_generator)
-sft = StatefulTractogram(ptt_streams, GT_SH_img, Space.RASMM)
+sft = StatefulTractogram(ptt_streams, labels_img, Space.RASMM)
 save_trk(sft, "tractogram_fast_ptt.trk")
+
+if has_fury:
+    scene = window.Scene()
+    scene.add(actor.line(ptt_streams, colors=colormap.line_colors(ptt_streams)))
+    window.record(scene=scene, out_path="tractogram_fast_ptt.png", size=(800, 800))
+    if interactive:
+        window.show(scene)
 
 # Compare the estimated connectivity with the ground-truth connectivity
 connectome = connectivity_matrix(ptt_streams, affine, labels)[1:, 1:]
 r, _ = pearsonr(GT_connectome[connectome_mask].flatten(),
                 connectome[connectome_mask].flatten())
 print("DiSCo ground-truth correlation (fast PTT tractography): ", r)
-
-if has_fury:
-    scene = window.Scene()
-    scene.add(actor.line(ptt_streams, colors=colormap.line_colors(ptt_streams)))
-    window.record(
-        scene=scene, out_path="tractogram_fast_ptt.png", size=(800, 800))
-    if interactive:
-        window.show(scene)
-
-plt.imshow(connectome, origin="lower",
-           cmap="viridis", interpolation="nearest")
-plt.axis('off')
+plt.imshow(connectome, origin="lower", cmap="viridis", interpolation="nearest")
+plt.axis("off")
 plt.savefig("connectome_ptt.png")
 plt.close()
 ###############################################################################
