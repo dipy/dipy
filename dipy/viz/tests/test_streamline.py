21cacf50d46c4bb4ba9713fd159358f031b3d249
Bramsh Q Chandio <bramshq@gmail.com>
2024-09-04 18:03:55 -0700

    Update test_streamline.py

--- dipy/viz/tests/test_streamline.py
+++ dipy/viz/tests/test_streamline.py
@@ -16,6 +16,8 @@ from dipy.utils.optpkg import optional_package
 
 _, have_matplotlib, _ = optional_package("matplotlib")
 fury, have_fury, _ = optional_package("fury", min_version="0.10.0")
+pd, have_pd, _ = optional_package("pandas")
+
 if have_fury:
     from dipy.viz import window
     from dipy.viz.streamline import (
@@ -84,7 +86,7 @@ def test_incorrect_view():
 
 
 @pytest.mark.skipif(
-    not have_fury or not have_matplotlib, reason="Requires FURY and Matplotlib"
+    not have_fury or not have_matplotlib or not have_pd, reason="Requires FURY, Matplotlib and Pandas"
 )
 def test_bundlewarp_viz():
     with tempfile.TemporaryDirectory() as temp_dir:
