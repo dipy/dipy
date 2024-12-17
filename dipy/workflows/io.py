a88cbee081b51a51e141a1c5c9e617d5a8b07c45
Serge Koudoro <skab12@gmail.com>
2024-12-17 12:01:03 -0500

    NF: add dipy math workflow

--- dipy/workflows/io.py
+++ dipy/workflows/io.py
@@ -21,8 +21,11 @@ from dipy.io.streamline import load_tractogram, save_tractogram
 from dipy.reconst.shm import convert_sh_descoteaux_tournier
 from dipy.reconst.utils import convert_tensors
 from dipy.tracking.streamlinespeed import length
+from dipy.utils.optpkg import optional_package
 from dipy.utils.tractogram import concatenate_tractogram
 from dipy.workflows.workflow import Workflow
+
+ne, have_ne, _ = optional_package("numexpr")
 
 
 class IoInfoFlow(Workflow):
@@ -775,3 +778,125 @@ class PamToNiftisFlow(Workflow):
                 fname_qa=oqa,
             )
             logging.info(msg)
+
+
+class MathFlow(Workflow):
+    @classmethod
+    def get_short_name(cls):
+        return "math_flow"
+
+    def run(
+        self, operation, input_files, dtype=None, out_dir="", out_file="math_out.nii.gz"
+    ):
+        """Perform mathematical operations on volume input files.
+
+        This workflow allows the user to perform mathematical operations on
+        multiple input files. e.g. to add two volumes together, subtract one:
+        ``dipy_math "vol1 + vol2 - vol3" t1.nii.gz t1_a.nii.gz t1_b.nii.gz``
+        The input files must be in Nifti format and have the same shape.
+
+        Parameters
+        ----------
+        operation : string
+            Mathematical operation to perform. supported operators are:
+                - Bitwise operators (and, or, not, xor): ``&, |, ~, ^``
+                - Comparison operators: ``<, <=, ==, !=, >=, >``
+                - Unary arithmetic operators: ``-``
+                - Binary arithmetic operators: ``+, -, *, /, **, %, <<, >>``
+            Supported functions are:
+                - ``where(bool, number1, number2) -> number``: number1 if the bool
+                  condition is true, number2 otherwise.
+                - ``{sin,cos,tan}(float|complex) -> float|complex``: trigonometric sine,
+                  cosine or tangent.
+                - ``{arcsin,arccos,arctan}(float|complex) -> float|complex``:
+                  trigonometric inverse sine, cosine or tangent.
+                - ``arctan2(float1, float2) -> float``: trigonometric inverse tangent of
+                  float1/float2.
+                - ``{sinh,cosh,tanh}(float|complex) -> float|complex``: hyperbolic
+                  sine, cosine or tangent.
+                - ``{arcsinh,arccosh,arctanh}(float|complex) -> float|complex``:
+                  hyperbolic inverse sine, cosine or tangent.
+                - ``{log,log10,log1p}(float|complex) -> float|complex``: natural,
+                  base-10 and log(1+x) logarithms.
+                - ``{exp,expm1}(float|complex) -> float|complex``: exponential and
+                  exponential minus one.
+                - ``sqrt(float|complex) -> float|complex``: square root.
+                - ``abs(float|complex) -> float|complex``: absolute value.
+                - ``conj(complex) -> complex``: conjugate value.
+                - ``{real,imag}(complex) -> float``: real or imaginary part of complex.
+                - ``complex(float, float) -> complex``: complex from real and imaginary
+                  parts.
+                - ``contains(np.str, np.str) -> bool``: returns True for every string
+                  in op1 that contains op2.
+        input_files : variable string
+            Any number of Nifti1 files
+        dtype : string, optional
+            Data type of the resulting file.
+        out_dir : string, optional
+            Output directory
+        out_file : string, optional
+            Name of the resulting file to be saved.
+        """
+        vol_dict = {}
+        ref_affine = None
+        ref_shape = None
+        info_msg = ""
+        have_errors = False
+        for i, fname in enumerate(input_files, start=1):
+            if not os.path.isfile(fname):
+                logging.error(f"Input file {fname} does not exist.")
+                raise SystemExit()
+
+            if not (fname.endswith(".nii.gz") or fname.endswith(".nii")):
+                msg = (
+                    f"Wrong volume type: {fname}. Only Nifti files are supported"
+                    " (*.nii or *.nii.gz)."
+                )
+                logging.error(msg)
+                raise SystemExit()
+
+            data, affine = load_nifti(fname)
+            vol_dict[f"vol{i}"] = data
+            info_msg += f"{fname}:\n- vol index: {i}\n- shape: {data.shape}"
+            info_msg += f"\n- affine:\n{affine}\n"
+            if ref_affine is None:
+                ref_affine = affine
+                ref_shape = data.shape
+                continue
+
+            have_errors = (
+                have_errors
+                or not np.all(np.isclose(ref_affine, affine, rtol=1e-05, atol=1e-08))
+                or not np.array_equal(ref_shape, data.shape)
+            )
+
+        if have_errors:
+            logging.warning(info_msg)
+            msg = "All input files must have the same shape and affine matrix."
+            logging.error(msg)
+            raise SystemExit()
+
+        try:
+            res = ne.evaluate(operation, local_dict=vol_dict)
+        except KeyError as e:
+            msg = (
+                f"Impossible key {e} in the operation. You have {len(input_files)}"
+                f" volumes available with the following keys: {list(vol_dict.keys())}"
+            )
+            logging.error(msg)
+            raise SystemExit() from e
+
+        if dtype:
+            try:
+                res = res.astype(dtype)
+            except TypeError as e:
+                msg = (
+                    f"Impossible to cast to {dtype}. Check possible numpy type here:"
+                    "https://numpy.org/doc/stable/reference/arrays.interface.html"
+                )
+                logging.error(msg)
+                raise SystemExit() from e
+
+        out_fname = os.path.join(out_dir, out_file)
+        logging.info(f"Saving result to {out_fname}")
+        save_nifti(out_fname, res, affine)
