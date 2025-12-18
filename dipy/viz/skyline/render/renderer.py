c684ec4934621e1abd0570400a53e880f79658f1
Maharshi Gor <gor.maharshi@gmail.com>
2025-12-18 15:24:45 -0500

    WIP: Added skyline

---
+++ dipy/viz/skyline/render/renderer.py
@@ -0,0 +1,78 @@
+import sys
+
+from fury import window
+
+from dipy.utils.logging import logger
+
+
+class Visualization:
+    def __init__(self, render_callback):
+        self._render_callback = render_callback
+
+    def render(self):
+        if self._render_callback is not None:
+            self._render_callback()
+
+    @property
+    def actor(self):
+        raise NotImplementedError("Subclasses must implement the actor property.")
+
+
+def create_window(*, visualizer_type="standalone", size=(800, 600), screen_config=None):
+    """Create a FURY ShowManager based on the visualizer type.
+
+    This function enables the creation of different types of windows for visualizations.
+    This will be used to create multi-window structure in the skyline visualizer,
+    where multiple instances can co-exist.
+
+    Parameters
+    ----------
+    visualizer_type : str, optional
+        Type of visualizer to create. The options are:
+        - "standalone": A standalone window with full interactivity.
+        - "gui": A Qt-based GUI window.
+        - "jupyter": An inline Jupyter notebook visualizer.
+        - "stealth": An offscreen visualizer without GUI.
+    size : tuple, optional
+        Size of the window
+    screen_config : list, optional
+        Defines the screen layout. Can be a list of integers (vertical/horizontal
+        sections) or a list of explicit bounding box tuples (x, y, w, h).
+
+    Returns
+    -------
+    ShowManager
+        An instance of FURY's ShowManager configured according to the
+        specified visualizer type.
+    """
+    if visualizer_type == "standalone":
+        window_type = "default"
+    elif visualizer_type == "gui":
+        window_type = "qt"
+    elif visualizer_type == "jupyter":
+        window_type = "jupyter"
+    elif visualizer_type == "stealth":
+        window_type = "offscreen"
+    else:
+        logger.error(
+            f"Visualizer type '{visualizer_type}' is not recognized. "
+            "Please provide one of the following: "
+            "'standalone', 'gui', 'jupyter', 'stealth'."
+        )
+        sys.exit(1)
+
+    if visualizer_type != "stealth":
+        show_m = window.ShowManager(
+            size=size,
+            window_type=window_type,
+            screen_config=screen_config,
+            imgui=True,
+            imgui_draw_function=lambda: None,
+            pixel_ratio=2,
+        )
+    else:
+        show_m = window.ShowManager(
+            size=size, window_type=window_type, screen_config=screen_config, imgui=False
+        )
+
+    return show_m
