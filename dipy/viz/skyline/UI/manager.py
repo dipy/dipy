de7cb53a2b99c72e11792777684cdaefcd8384b4
maharshigor <gor.maharshi@gmail.com>
2025-12-22 23:13:06 -0500

    WIP: Render Group introduced.

--- dipy/viz/skyline/UI/manager.py
+++ dipy/viz/skyline/UI/manager.py
@@ -25,7 +25,7 @@ class UIWindow:
         self._section_open = {}
         hello_imgui.set_assets_folder(str(SKYLINE_HOME))
         hello_imgui.load_font_ttf_with_font_awesome_icons(
-            f"{FONT.parent.name}/{FONT.name}", 18
+            f"{FONT.parent.name}/{FONT.name}", 20
         )
         imgui.push_style_color(
             imgui.Col_.window_bg, imgui.get_color_u32(THEME["background"])
