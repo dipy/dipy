105ed46e0dbf57f09ca3f35c971bcd90085abacf
maharshigor <gor.maharshi@gmail.com>
2026-01-07 21:15:33 -0500

    WIP: Skyline CLI added.
    - dipy_skyline is available to use.
    - `--image` option is required to be used to pass space separated file names.

--- dipy/viz/skyline/app.py
+++ dipy/viz/skyline/app.py
@@ -1,4 +1,5 @@
-from dipy.io.image import load_nifti
+from pathlib import Path
+
 from dipy.viz.skyline.UI.manager import UIWindow
 from dipy.viz.skyline.render.renderer import create_window
 from dipy.viz.skyline.render.slicer import Slicer
@@ -12,7 +13,7 @@ class Skyline:
         self.UI_window = UIWindow("Slicer Controls")
 
         if images is not None:
-            for img, affine, _ in images:
+            for idx, (img, affine, path) in enumerate(images):
                 slicer = Slicer(
                     img,
                     affine=affine,
@@ -20,16 +21,7 @@ class Skyline:
                     interpolation="linear",
                 )
                 self.windows[0].screens[0].scene.add(slicer.actor)
-                self.UI_window.add("HARDI Slicer", slicer.render_widgets)
+                fname = Path(path).stem if path is not None else f"Image {idx}"
+                self.UI_window.add(fname, slicer.render_widgets)
         self.windows[0]._imgui.set_gui(self.UI_window.render)
         self.windows[0].start()
-
-
-if __name__ == "__main__":
-    file_path = "~/.dipy/stanford_hardi/HARDI150.nii.gz"
-    img, affine = load_nifti(file_path)
-    print("Affine:\n", affine)
-    print("Data shape: ", img.shape)
-    skyline = Skyline(
-        visualizer_type="standalone", images=[(img[..., 0], affine, file_path)]
-    )
