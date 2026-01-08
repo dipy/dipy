b0c79fcd6b26cf37acc7ff078dfba8b315eb411e
Maharshi Gor <gor.maharshi@gmail.com>
2026-01-08 15:33:57 -0500

    WIP: Static Interaction Panel.
    - Adds static interaction panel
    - Resizes with the change of height.
    - Fixed width.

--- dipy/viz/skyline/UI/manager.py
+++ dipy/viz/skyline/UI/manager.py
@@ -1,7 +1,7 @@
 from imgui_bundle import hello_imgui, imgui
 
 from dipy.viz.skyline.UI.elements import render_section_header
-from dipy.viz.skyline.UI.theme import FONT, SKYLINE_HOME, THEME
+from dipy.viz.skyline.UI.theme import ASSETS, FONT, THEME
 
 
 class UIManager:
@@ -14,7 +14,7 @@ class UIManager:
 
 class UIWindow:
     def __init__(
-        self, title, *, default_open=True, flags=0, pos=(50, 50), size=(400, 400)
+        self, title, *, default_open=True, flags=0, pos=(0, 0), size=(400, 400)
     ):
         self.title = title
         self.is_open = default_open
@@ -23,9 +23,9 @@ class UIWindow:
         self.size = size
         self._sections = {}
         self._section_open = {}
-        hello_imgui.set_assets_folder(str(SKYLINE_HOME))
+        hello_imgui.set_assets_folder(str(ASSETS))
         hello_imgui.load_font_ttf_with_font_awesome_icons(
-            f"{FONT.parent.name}/{FONT.name}", 20
+            str(FONT.relative_to(ASSETS)), 20
         )
         imgui.push_style_color(
             imgui.Col_.window_bg, imgui.get_color_u32(THEME["background"])
@@ -43,13 +43,13 @@ class UIWindow:
 
     def render(self):
         if self.pos is not None:
-            imgui.set_next_window_pos(
-                (self.pos[0], self.pos[1]), imgui.Cond_.first_use_ever
-            )
+            imgui.set_next_window_pos(self.pos)
         if self.size is not None:
-            imgui.set_next_window_size((self.size[0], self.size[1]))
+            imgui.set_next_window_size(self.size, imgui.Cond_.always)
 
-        computed_flags = self.flags | imgui.WindowFlags_.no_collapse
+        computed_flags = (
+            self.flags | imgui.WindowFlags_.no_collapse | imgui.WindowFlags_.no_resize
+        )
         open_flag = imgui.begin(self.title, None, computed_flags)
         self.is_open = open_flag
 
@@ -60,6 +60,21 @@ class UIWindow:
                 is_open = render_section_header(name, is_open=is_open)
                 self._section_open[name] = is_open
                 if is_open:
-                    renderer()
+                    padding = 20
+                    imgui.begin_group()
+                    imgui.dummy((0, padding / 2))
+                    imgui.push_style_var(
+                        imgui.StyleVar_.window_padding, (padding, padding / 2)
+                    )
+                    child_flags = (
+                        imgui.ChildFlags_.always_use_window_padding
+                        | imgui.ChildFlags_.auto_resize_y
+                    )
+                    if imgui.begin_child(f"{name}_content_child", (0, 0), child_flags):
+                        renderer()
+                    imgui.end_child()
+                    imgui.pop_style_var()
+                    imgui.dummy((0, padding / 2))
+                    imgui.end_group()
                 imgui.pop_id()
         imgui.end()
