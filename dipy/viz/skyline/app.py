1f332780bae9cbc6b6c44921211a4267f4415c54
Maharshi Gor <gor.maharshi@gmail.com>
2026-01-14 16:30:56 -0500

    WIP: Two disk slider added.
    - Intensities slider added.

--- dipy/viz/skyline/app.py
+++ dipy/viz/skyline/app.py
@@ -1,53 +1,54 @@
 from pathlib import Path
 
-from fury.lib import EventType
 from fury.window import update_viewports
 
 from dipy.viz.skyline.UI.manager import UIWindow
+from dipy.viz.skyline.render.image import Image3D
 from dipy.viz.skyline.render.renderer import create_window
-from dipy.viz.skyline.render.slicer import Slicer
 
 
 class Skyline:
     def __init__(self, visualizer_type="standalone", images=None):
         self.windows = []
-        size = (1200, 1000)
-        ui_size = (400, size[1])
+        self.size = (1200, 1000)
+        self.ui_size = (400, self.size[1])
         self.windows.append(
             create_window(
                 visualizer_type=visualizer_type,
-                size=size,
+                size=self.size,
                 screen_config=[
-                    (0, 0, ui_size[0], ui_size[1]),
-                    (ui_size[0], 0, size[0] - ui_size[0], size[1]),
+                    (0, 0, self.ui_size[0], self.ui_size[1]),
+                    (self.ui_size[0], 0, self.size[0] - self.ui_size[0], self.size[1]),
                 ],
             )
         )
 
-        self.UI_window = UIWindow("Slicer Controls", size=ui_size)
-        self.windows[0].renderer.add_event_handler(
-            lambda event: self.handle_resize((event.width, event.height)),
-            EventType.RESIZE,
-        )
+        self.UI_window = UIWindow("Image Controls", size=self.ui_size)
+        self.windows[0].resize_callback(self.handle_resize)
 
         if images is not None:
             for idx, (img, affine, path) in enumerate(images):
-                slicer = Slicer(
+                image3d = Image3D(
                     img,
                     affine=affine,
                     render_callback=self.windows[0].render,
                     interpolation="linear",
                 )
-                self.windows[0].screens[1].scene.add(slicer.actor)
+                self.windows[0].screens[1].scene.add(image3d.actor)
                 fname = Path(path).name if path is not None else f"Image {idx}"
-                self.UI_window.add(fname, slicer.render_widgets)
+                self.UI_window.add(fname, image3d.render_widgets)
         self.windows[0]._imgui.set_gui(self.UI_window.render)
         self.windows[0].start()
 
     def handle_resize(self, size):
+        self.size = size
+        self.ui_size = (400, self.size[1])
         update_viewports(
             self.windows[0].screens,
-            [(0, 0, 400, size[1]), (400, 0, size[0] - 400, size[1])],
+            [
+                (0, 0, self.ui_size[0], size[1]),
+                (self.ui_size[0], 0, size[0] - self.ui_size[0], size[1]),
+            ],
         )
-        self.UI_window.size = (400, size[1])
+        self.UI_window.size = (self.ui_size[0], size[1])
         self.windows[0].render()
--- dipy/viz/skyline/render/image.py
+++ dipy/viz/skyline/render/image.py
@@ -1,12 +1,20 @@
+import sys
+
 from fury.actor import set_group_opacity, show_slices, volume_slicer
 from imgui_bundle import imgui
 import numpy as np
 
-from dipy.viz.skyline.UI.elements import render_group, segmented_switch, thin_slider
+from dipy.utils.logging import logger
+from dipy.viz.skyline.UI.elements import (
+    render_group,
+    segmented_switch,
+    thin_slider,
+    two_disk_slider,
+)
 from dipy.viz.skyline.render.renderer import Visualization
 
 
-class Slicer(Visualization):
+class Image3D(Visualization):
     def __init__(
         self,
         volume,
@@ -15,35 +23,74 @@ class Slicer(Visualization):
         interpolation="linear",
         render_callback=None,
         opacity=100,
+        rgb=False,
         value_percentiles=(2, 98),
     ):
         super().__init__(render_callback=render_callback)
-        self.volume = volume
+        self.dwi = volume
         self.affine = affine
-        if self.volume.ndim == 4:
-            self.volume = self.volume[..., 0]
-        self.slicer = volume_slicer(
-            self.volume,
-            affine=affine,
-            interpolation=interpolation,
-        )
+
+        if (
+            rgb
+            and self.dwi.ndim == 4
+            and (self.dwi.shape[3] != 3 and self.dwi.shape[3] != 4)
+        ):
+            logger.error(
+                "When specifying rgb=True, the last dimension of the volume "
+                "must be 3 (RGB) or 4 (RGBA)."
+            )
+            sys.exit(1)
+        self.rgb = rgb
+
+        self._volume_idx = 0
+        self.interpolation = interpolation or "linear"
+        self._value_percentiles = value_percentiles
+
+        self._create_slicer_actor()
         self.bounds = self.slicer.get_bounding_box()
         self.state = np.mean(self.bounds, axis=0).astype(int)
         show_slices(self.slicer, self.state)
         self.opacity = opacity
-        self.interpolation = interpolation or "linear"
 
         self.slicer.add_event_handler(self._pick_voxel, "pointer_down")
 
     def _pick_voxel(self, event):
         info = event.pick_info
         intensity_interpolated = np.asarray(info["rgba"].rgb).mean()
-        intensity_raw = self.volume[info["index"]]
+        intensity_raw = self.dwi[info["index"]]
         print(
             f"Voxel {info['index']}:"
             f"\nInterpolated intensity: {intensity_interpolated:.2f}"
             f"\nRaw intensity: {intensity_raw}"
         )
+
+    def _create_slicer_actor(self):
+        if self.dwi.ndim == 4 and not self.rgb:
+            volume = self.dwi[..., self._volume_idx]
+            self.value_range = self._value_range_from_percentile(volume)
+            self.slicer = volume_slicer(
+                volume,
+                affine=self.affine,
+                interpolation=self.interpolation,
+                value_range=self.value_range,
+                alpha_mode="blend",
+                depth_write=True,
+            )
+        else:
+            self.value_range = self._value_range_from_percentile(self.dwi)
+            self.slicer = volume_slicer(
+                self.dwi,
+                affine=self.affine,
+                interpolation=self.interpolation,
+                value_range=self.value_range,
+                alpha_mode="blend",
+                depth_write=True,
+            )
+
+    def _value_range_from_percentile(self, volume):
+        p_low, p_high = self._value_percentiles
+        vmin, vmax = np.percentile(volume, (p_low, p_high))
+        return vmin, vmax
 
     @property
     def actor(self):
@@ -63,6 +110,28 @@ class Slicer(Visualization):
         if changed:
             self.opacity = new
             set_group_opacity(self.slicer, self.opacity / 100.0)
+
+        imgui.spacing()
+        volume_for_range = (
+            self.dwi[..., self._volume_idx]
+            if self.dwi.ndim == 4 and not self.rgb
+            else self.dwi
+        )
+        intensity_changed, new_percentiles = two_disk_slider(
+            "Intensities",
+            self._value_percentiles,
+            0,
+            100,
+            text_format=".1f",
+            step=1,
+            min_gap=0.1,
+            display_values=self.value_range,
+        )
+        if intensity_changed:
+            self._value_percentiles = new_percentiles
+            self.value_range = self._value_range_from_percentile(volume_for_range)
+            for actor in self.slicer.children:
+                actor.material.clim = self.value_range
 
         imgui.spacing()
         axis_labels = ("X", "Y", "Z")
