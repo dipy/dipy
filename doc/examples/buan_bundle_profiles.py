a5facac68bccd7f612142bcb1b1240164ec067f2
BramshQamar <bramshq@gmail.com>
2026-01-16 17:58:48 -0500

    remove os

--- doc/examples/buan_bundle_profiles.py
+++ doc/examples/buan_bundle_profiles.py
@@ -1,8 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jan 16 17:25:11 2026
+================================
+BUAN bundle profiles (lite)
+================================
 
-@author: bramshqamar
+This example shows how to compute **weighted mean bundle profiles**
+using the BUAN "lite" functionality.
+
+Compared to full BUAN profiles (streamline-by-streamline), the lite version
+summarizes each bundle into a mean profile, where each streamline contributes
+according to a weight based on its distance to a model bundle centroid.
+
+The output is useful for users who prefer working with **mean profiles**
+while keeping BUAN-style along-tract segmentation.
+
 """
+
+import numpy as np
+
+from dipy.data import fetch_bundles_2_subjects, read_bundles_2_subjects
+from dipy.tracking.streamline import length, transform_streamlines
+from dipy.viz import actor, window
+
+fetch_bundles_2_subjects()
+dix = read_bundles_2_subjects(
+    subj_id="subj_1", metrics=["fa"], bundles=["cst.right"]
+)
+
+###############################################################################
+# Store fractional anisotropy.
+
+fa = dix["fa"]
+
+###############################################################################
+# Store grid to world transformation matrix.
+
+affine = dix["affine"]
+
+###############################################################################
+# Store the corticospinal tract. A bundle is a list of streamlines.
+
+bundle = dix["CST.left"]
 
