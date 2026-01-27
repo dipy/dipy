9d74239bd90251ec85f52d456307918dee6a0615
maharshigor <gor.maharshi@gmail.com>
2026-01-27 15:00:42 -0500

    WIP: Dipy Skyline title added.

--- dipy/viz/skyline/UI/manager.py
+++ dipy/viz/skyline/UI/manager.py
@@ -32,6 +32,8 @@ class UIWindow:
         self._section_open = {}
         self._render_callback = render_callback
         self.logo_tex_ref = logo_tex_ref
+        self.logo_size = (48, 48)
+        self._title_text = "DIPY SKYLINE"
         if render_callback is None:
             self.render_callback = lambda: None
 
@@ -61,11 +63,48 @@ class UIWindow:
             imgui.set_next_window_size(self.size, imgui.Cond_.always)
 
         computed_flags = (
-            self.flags | imgui.WindowFlags_.no_collapse | imgui.WindowFlags_.no_resize
+            self.flags
+            | imgui.WindowFlags_.no_collapse
+            | imgui.WindowFlags_.no_resize
+            | imgui.WindowFlags_.no_title_bar
         )
         imgui.begin(self.title, None, computed_flags)
         imgui.push_id("logo")
-        imgui.image(self.logo_tex_ref, (64, 64))
+        imgui.push_font(None, 36)
+
+        draw_list = imgui.get_foreground_draw_list()
+        spacing = 10
+        available_width = imgui.get_content_region_avail().x
+
+        org_start = imgui.get_cursor_screen_pos()
+        start = (org_start.x, org_start.y + spacing)
+
+        text_width, text_height = imgui.calc_text_size(self._title_text)
+
+        total_width = self.logo_size[0] + spacing + text_width
+        offset_x = max((available_width - total_width) * 0.5, 0.0)
+        start = (start[0] + offset_x, start[1])
+
+        draw_list.add_image(
+            self.logo_tex_ref,
+            start,
+            (start[0] + self.logo_size[0], start[1] + self.logo_size[1]),
+        )
+
+        title_text_pos = (
+            start[0] + self.logo_size[0] + spacing,
+            start[1] + (self.logo_size[1] - text_height) * 0.5,
+        )
+        text_color = imgui.get_color_u32(THEME["text"])
+        draw_list.add_text(title_text_pos, text_color, self._title_text)
+
+        rect_min = (start[0] - spacing, start[1] + self.logo_size[1] + spacing)
+        rect_max = (start[0] + total_width + spacing, rect_min[1] + 1)
+        draw_list.add_rect_filled(rect_min, rect_max, text_color, 0, 0)
+
+        imgui.set_cursor_screen_pos(org_start)
+        imgui.dummy((available_width, self.logo_size[1] + spacing * 3 + 1))
+        imgui.pop_font()
         imgui.pop_id()
         is_removed = [False] * len(self._sections)
         for idx, (name, renderer) in enumerate(self._sections.items()):
@@ -84,3 +123,7 @@ class UIWindow:
     @property
     def sections(self):
         return self._sections
+
+    @property
+    def section_open_states(self):
+        return self._section_open
