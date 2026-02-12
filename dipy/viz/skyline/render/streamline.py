5a5d370ac0bc457d1242166fce66b4fe0a3cc115
Maharshi Gor <gor.maharshi@gmail.com>
2026-02-12 14:07:48 -0500

    NF: streamlines added.

--- dipy/viz/skyline/render/streamline.py
+++ dipy/viz/skyline/render/streamline.py
@@ -5,20 +5,37 @@ from dipy.viz.skyline.render.renderer import Visualization
 
 class Streamline3D(Visualization):
     def __init__(
-        self, name, sft, *, line_type="tube", color=(1, 0, 0), render_callback=None
+        self,
+        name,
+        sft,
+        *,
+        line_type="line",
+        color=(1, 0, 0),
+        device=None,
+        render_callback=None,
     ):
         super().__init__(name, render_callback)
         self.sft = sft
         self.color = color
+        self.device = device
         self._create_streamline_actor(line_type)
 
     def _create_streamline_actor(self, line_type):
         if line_type == "tube":
             self._actor = streamtube(
-                self.sft.streamlines, radius=0.1, colors=self.color
+                self.sft.streamlines,
+                radius=0.1,
+                colors=self.color,
+                flat_shading=False,
+                segments=3,
             )
         elif line_type == "line":
-            self._actor = streamlines(self.sft.streamlines, colors=self.color)
+            self._actor = streamlines(
+                self.sft.streamlines,
+                colors=self.color,
+                thickness=1,
+                outline_thickness=1,
+            )
 
     @property
     def actor(self):
