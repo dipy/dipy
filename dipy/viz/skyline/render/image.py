903b39fba618865ad8791d1cda1b0832b68f510a
Maharshi Gor <gor.maharshi@gmail.com>
2026-02-18 16:46:32 -0500

    WIP: Added voxel intensity picker
    - WIP: Added voxel intensity picker
      - updated height of the combobox.
    - WIP: `.tck` and other format files supported

--- dipy/viz/skyline/render/image.py
+++ dipy/viz/skyline/render/image.py
@@ -10,6 +10,7 @@ from dipy.viz.skyline.UI.elements import (
     thin_slider,
     two_disk_slider,
 )
+from dipy.viz.skyline.UI.theme import THEME
 from dipy.viz.skyline.render.renderer import Visualization
 
 
@@ -49,21 +50,53 @@ class Image3D(Visualization):
         self._value_percentiles = value_percentiles
         self._colormap_options = ("Gray", "Inferno", "Magma", "Plasma", "Viridis")
         self.colormap = colormap
+        self._picked_voxel = None
+        self._picked_intensity = None
 
         self._create_slicer_actor()
         self.opacity = opacity
 
-        self._slicer.add_event_handler(self._pick_voxel, "pointer_down")
-
     def _pick_voxel(self, event):
         info = event.pick_info
-        intensity_interpolated = np.asarray(info["rgba"].rgb).mean()
-        intensity_raw = self.dwi[info["index"]]
-        print(
-            f"Voxel {info['index']}:"
-            f"\nInterpolated intensity: {intensity_interpolated:.2f}"
-            f"\nRaw intensity: {intensity_raw}"
-        )
+        voxel = info["index"]
+        self._picked_voxel = voxel
+        self._picked_intensity = self._active_volume()[voxel]
+
+    def _active_volume(self):
+        return self.dwi[..., self._volume_idx] if self._has_directions else self.dwi
+
+    # def _render_voxel_info_panel(self):
+    #     voxel = str(self._picked_voxel) if self._picked_voxel is not None else ""
+    #     intensity = (
+    #         str(self._picked_intensity) if self._picked_intensity is not None else ""
+    #     )
+
+    #     # panel_height = 32.0
+    #     # panel_width = max(1.0, imgui.get_content_region_avail().x)
+    #     # imgui.invisible_button("##voxel_info_panel", (panel_width, panel_height), 0)
+
+    #     # panel_min = imgui.get_item_rect_min()
+    #     # panel_max = imgui.get_item_rect_max()
+    #     # draw_list = imgui.get_window_draw_list()
+
+    #     # background_color = imgui.get_color_u32(THEME["background"])
+    #     label_color = imgui.get_color_u32(THEME["text"])
+    #     value_color = imgui.get_color_u32(THEME["primary"])
+
+    #     # draw_list.add_rect_filled(panel_min, panel_max, background_color, 4.0)
+
+    #     # text_y = panel_min.y + (panel_height - imgui.get_text_line_height()) * 0.5
+    #     # cursor_x = panel_min.x
+    #     # voxel_text = f"({voxel[0]}, {voxel[1]}, {voxel[2]})"
+    #     # segments = (
+    #     #     ("Voxel ", label_color),
+    #     #     (voxel_text, value_color),
+    #     #     ("    Intensity ", label_color),
+    #     #     (f"{intensity:.2f}", value_color),
+    #     # )
+    #     # for text, color in segments:
+    #     #     draw_list.add_text((cursor_x, text_y), color, text)
+    #     #     cursor_x += imgui.calc_text_size(text).x
 
     def _create_slicer_actor(self):
         if self._has_directions:
@@ -90,6 +123,7 @@ class Image3D(Visualization):
         self._apply_colormap(self.colormap)
         self.bounds = self._slicer.get_bounding_box()
         self.state = np.mean(self.bounds, axis=0)
+        self._slicer.add_event_handler(self._pick_voxel, "pointer_down")
         show_slices(self._slicer, self.state)
         self.render()
 
@@ -191,10 +225,30 @@ class Image3D(Visualization):
 
         imgui.spacing()
         colormap_changed, new_cmap = dropdown(
-            "Colormap", self._colormap_options, self.colormap
+            "Colormap", self._colormap_options, self.colormap, height=26
         )
         if colormap_changed:
             self._apply_colormap(new_cmap)
+
+        imgui.spacing()
+
+        voxel = str(self._picked_voxel) if self._picked_voxel is not None else ""
+        intensity = (
+            str(self._picked_intensity) if self._picked_intensity is not None else ""
+        )
+        value_color = THEME["primary"]
+        label_color = THEME["text"]
+        intesity_pos_x = imgui.get_content_region_avail().x * 0.5
+        imgui.push_id("voxel_info")
+        imgui.text_colored(label_color, "Voxel ")
+        imgui.same_line()
+        imgui.text_colored(value_color, voxel)
+        imgui.same_line()
+        imgui.set_cursor_pos_x(intesity_pos_x)
+        imgui.text_colored(label_color, "Intensity ")
+        imgui.same_line()
+        imgui.text_colored(value_color, intensity)
+        imgui.pop_id()
 
         imgui.spacing()
         imgui.spacing()
