ce37c7dfea435f4bfdae57f7ea6334983e4a6f5a
Maharshi Gor <gor.maharshi@gmail.com>
2026-03-10 15:03:29 -0400

    NF: Updated buan colors

--- dipy/viz/skyline/render/streamline.py
+++ dipy/viz/skyline/render/streamline.py
@@ -1,3 +1,4 @@
+import colorsys
 from pathlib import Path
 import time
 
@@ -8,14 +9,41 @@ from imgui_bundle import imgui
 import numpy as np
 
 from dipy.segment.clustering import qbx_and_merge
-from dipy.tracking.streamline import length as streamline_length
+from dipy.stats.analysis import assignment_map
+from dipy.tracking.streamline import (
+    length as streamline_length,
+)
 from dipy.viz.skyline.UI.elements import (
     create_numeric_input,
     segmented_switch,
+    toggle_button,
     uploader,
 )
 from dipy.viz.skyline.io import load_npy
 from dipy.viz.skyline.render.renderer import Visualization
+
+
+def apply_buan_colors(
+    streamlines, buan_pvals, *, hue=(0.0, 0.1), saturation=(0.8, 0.2)
+):
+    n = len(buan_pvals)
+    indx = assignment_map(streamlines, streamlines, n)
+    buan_color_pvals = buan_pvals[indx].astype(np.float32)
+
+    lut = np.zeros((n, 3), dtype=np.float32)
+
+    h = np.interp(np.arange(n), [0, n - 1], hue)
+    s = np.interp(np.arange(n), [0, n - 1], saturation)
+    for i in range(n):
+        r, g, b = colorsys.hsv_to_rgb(h[i], s[i], 0.8)
+        lut[i] = (r, g, b)
+
+    buan_color_idx = np.interp(
+        buan_color_pvals, [buan_pvals.min(), buan_pvals.max()], [0, n - 1]
+    ).astype(int)
+    buan_colors = lut[buan_color_idx]
+
+    return buan_colors
 
 
 def create_streamline_visualization(
@@ -29,6 +57,7 @@ def create_streamline_visualization(
     render_callback=None,
     colormap=None,
     tract_colors=None,
+    switch_render_callback=None,
 ):
     """Create streamline visualization from input
 
@@ -56,6 +85,8 @@ def create_streamline_visualization(
         String options are 'random' for random colors for each tractogram,
         'direction'  for directionally colored streamlines.
         For example, a value of (1, 0, 0) would mean the red color.
+    switch_render_callback : callable, optional
+        Callback function to switch rendering type, used for cluster visualization.
 
     Returns
     -------
@@ -83,6 +114,7 @@ def create_streamline_visualization(
             line_type=line_type,
             render_callback=render_callback,
             colormap=colormap,
+            switch_render_callback=switch_render_callback,
         )
 
     if tract_colors is not None:
@@ -102,29 +134,34 @@ def create_streamline_visualization(
         line_type=line_type,
         color=color,
         render_callback=render_callback,
+        switch_render_callback=switch_render_callback,
     )
 
 
 def create_streamline(lines, *, color=(1, 0, 0), line_type="line", segments=4):
-    if isinstance(color, str) and color == "direction":
+    if isinstance(color, str) and color == "direction" and lines:
         color = line_colors(lines)
     if line_type == "tube":
+        if len(color) != len(lines) and color.ndim == 2:
+            points_per_line = [len(line) for line in lines]
+            if color.shape[0] == sum(points_per_line):
+                color = np.split(color, np.cumsum(points_per_line)[:-1])
         tubes = streamtube(
             lines=lines,
-            radius=0.3,
+            radius=0.5,
             colors=color,
             segments=segments,
         )
         tubes.material.side = "front"
         return tubes
     elif line_type == "line":
-        if len(color) == len(lines):
+        if len(color) == len(lines) and color.ndim == 2:
             color = np.repeat(color, [len(line) for line in lines], axis=0)
         lines = streamlines(
             lines=lines,
             colors=color,
-            thickness=3,
-            outline_thickness=0.5,
+            thickness=5,
+            outline_thickness=0.4,
             outline_color=(0.15, 0.15, 0.15),
         )
         lines.material.aa = True
@@ -140,19 +177,22 @@ class Streamline3D(Visualization):
         line_type="line",
         color=(1, 0, 0),
         render_callback=None,
+        switch_render_callback=None,
     ):
         super().__init__(name, render_callback)
         self.sft = sft
         self.color = color
         self._line_type = line_type
-        self._buan_colors_file = False
-        self._create_streamline_actor(line_type)
+        self._buan_pvals_file = False
+        self._buan_pvals_data = None
+        self._switch_render_callback = switch_render_callback
+        self._create_streamline_actor()
 
-    def _create_streamline_actor(self, line_type):
+    def _create_streamline_actor(self):
         self._actor = create_streamline(
             lines=self.sft.streamlines,
             color=self.color,
-            line_type=line_type,
+            line_type=self._line_type,
         )
 
     @property
@@ -160,10 +200,14 @@ class Streamline3D(Visualization):
         return self._actor
 
     def render_widgets(self):
+        changed, is_clustered = toggle_button(False, label="Cluster")
+        if changed:
+            if self._switch_render_callback is not None:
+                self._switch_render_callback(self, is_clustered)
         changed, new = segmented_switch("Line Type", ["Line", "Tube"], self._line_type)
         if changed:
-            self._line_type = new
-            self._create_streamline_actor(new.lower())
+            self._line_type = new.lower()
+            self._create_streamline_actor()
             self.render()
 
         imgui.spacing()
@@ -171,25 +215,34 @@ class Streamline3D(Visualization):
 
         def handle_color_change(fname):
             if fname is not None:
-                self._buan_colors_file = Path(fname[0]).name
-                self._buan_colors_data = load_npy(fname[0])
-                print(f"BUAN colors shape {self._buan_colors_data.shape}")
-                print(
-                    f"BUAN colors range {self._buan_colors_data.min()} - {self._buan_colors_data.max()}"
+                self._buan_pvals_file = Path(fname[0]).name
+                self._buan_pvals_data = load_npy(fname[0])
+                self.color = apply_buan_colors(
+                    self.sft.streamlines, self._buan_pvals_data
                 )
+                self._create_streamline_actor()
+                self.render()
 
         uploader(
             "Upload BUAN P values",
             callback=handle_color_change,
             extension="*.npy",
-            selected=self._buan_colors_file,
-            type="buan_colors",
+            selected=self._buan_pvals_file,
+            type="buan_pvals",
         )
 
 
 class ClusterStreamline3D(Visualization):
     def __init__(
-        self, name, sft, thr, *, line_type="line", render_callback=None, colormap=None
+        self,
+        name,
+        sft,
+        thr,
+        *,
+        line_type="line",
+        render_callback=None,
+        colormap=None,
+        switch_render_callback=None,
     ):
         super().__init__(name, render_callback=render_callback)
 
@@ -206,6 +259,7 @@ class ClusterStreamline3D(Visualization):
         self._actor = Group()
         self._pending_thr = None
         self._thr_changed_at = None
+        self._switch_render_callback = switch_render_callback
         self._recluster_debounce_sec = 1.0
         self._perform_clustering()
         self.size = int(np.min(self._sizes)) if self._sizes.size else 0
@@ -338,6 +392,10 @@ class ClusterStreamline3D(Visualization):
         return self._actor
 
     def render_widgets(self):
+        changed, is_clustered = toggle_button(True, label="Cluster")
+        if changed:
+            if self._switch_render_callback is not None:
+                self._switch_render_callback(self, is_clustered)
         threshold_value = (
             self._pending_thr if self._pending_thr is not None else self.thr
         )
