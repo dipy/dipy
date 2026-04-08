3a3ca1b95f3ef9b79ec610de780ac6395ea7f8ae
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2026-04-08 11:09:02 +0100

    Added test for classifying heavily masked tissue

--- dipy/segment/tests/test_mrf.py
+++ dipy/segment/tests/test_mrf.py
@@ -410,6 +410,14 @@ def test_classify():
     npt.assert_(seg_final.max() == nclasses)
     npt.assert_(seg_final.min() == 0.0)
 
+    # Now we test what happens with heavily masked data
+    masked_image = np.copy(image)
+    masked_image[masked_image.shape[0] // 2:, :, :] = 0
+    seg_init, seg_final, PVE = imgseg.classify(masked_image, nclasses, beta)
+
+    npt.assert_(seg_final.max() == nclasses)
+    npt.assert_(seg_final.min() == 0.0)
+
     # Next we test saving the history of accumulated energies from ICM
     imgseg = TissueClassifierHMRF(save_history=True)
 
